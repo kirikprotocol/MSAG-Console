d0a40f2d6c52d648b32340cd2d63c1f436f3db60
makarov <>
2002-02-11 10:04:43 +0000

    :rded

---
+++ src/storetest/StoreTest.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <OCIMessageStore.h>
+
+int main(void) 
+{
+    using namespace smsc::store;
+    using namespace smsc::store::oci;
+
+    OCIMessageStoreFactory factory;
+    OCIStoreConfig* config = new OCIStoreConfig("ORCL", "smsc", "smsc");
+    
+    MessageStore* store = factory.createMessageStore(config);
+    
+    try {
+        store->open();
+	printf("Connect Ok !\n");
+    } 
+    catch (exception& ae) {
+	printf("Exception : %s\n", ae.what());
+    }
+    
+    if (store) delete store;
+    if (config) delete config;
+    
+    return 0;
+}
