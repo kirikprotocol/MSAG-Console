cc580fd927709a1a8fd68752ba023bbef10acc99
makarov <>
2002-02-12 15:26:32 +0000

    design was changed

---
+++ src/store/ConnectionManager.cpp
@@ -0,0 +1,121 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <oci.h>
+#include "ConnectionManager.h"
+
+namespace smsc { namespace store 
+{
+/* ----------------------------- StoreConfig --------------------------- */
+StoreConfig::StoreConfig(const char* db, const char* usr, const char* pwd)
+{
+    userName = strdup(usr);
+    userPwd = strdup(pwd);
+    dbName = strdup(db);
+}
+StoreConfig::~StoreConfig() 
+{
+    if (userName) free(userName);
+    if (userPwd) free(userPwd);
+    if (dbName) free(dbName);
+}
+/* ----------------------------- StoreConfig --------------------------- */
+
+/* ----------------------------- ConnectionPool ------------------------ */
+SingleConnectionPool::SingleConnectionPool(StoreConfig* _config)
+    throw(ResourceAllocationException, AuthenticationException)
+        : ConnectionPool(_config), conn(0L), lock(false)
+{
+    //create mutex here
+    connect();
+}
+
+SingleConnectionPool::~SingleConnectionPool()
+{
+    disconnect();
+    // destory mutex here
+}
+
+void SingleConnectionPool::connect()
+    throw(ResourceAllocationException, AuthenticationException)
+{
+    if (!conn) 
+    {
+        if (!config) {
+            throw AuthenticationException();
+        }
+        if (!(conn = new Connection(0))) {
+            throw ResourceAllocationException();
+        }
+             
+        const char* userName = config->getUserName();
+        const char* userPwd = config->getUserPwd();
+        const char* dbName = config->getDbName();
+
+        if (userName && userPwd && dbName)
+        {
+            sword       status;
+
+            /* initialize the mode to be the threaded environment */
+            status = OCIEnvCreate(&(conn->envhp), OCI_THREADED, (dvoid *)0,
+                                   0, 0, 0, (size_t) 0, (dvoid **)0);
+            if (status != OCI_SUCCESS || !(conn->envhp)) 
+            {
+                throw ResourceAllocationException();
+            }
+
+            /* allocate an error handle */
+            status = OCIHandleAlloc ((dvoid *)(conn->envhp), 
+                                     (dvoid **)&(conn->errhp),
+                                      OCI_HTYPE_ERROR, 0, (dvoid **) 0);
+            if (status != OCI_SUCCESS || !(conn->errhp)) 
+            {
+                throw ResourceAllocationException();
+            }
+        
+            // logon to server database (allocate a service handle) 
+            status = OCILogon(conn->envhp, conn->errhp, &(conn->svchp),
+                                (OraText*)userName, strlen(userName),
+                                (OraText*)userPwd, strlen(userPwd),
+                                (OraText*)dbName, strlen(dbName));
+            if (status != OCI_SUCCESS || !(conn->svchp))
+            {
+                // free envirounment handle (error handle will be freed too)
+                //checkerror(conn.errhp, status);
+                (void) OCIHandleFree(conn->envhp, OCI_HTYPE_ENV);
+                throw AuthenticationException();
+            }
+        } 
+        else {
+            throw AuthenticationException();
+        }
+    }
+}
+ 
+void SingleConnectionPool::disconnect()
+{
+    if (conn && conn->envhp && conn->errhp && conn->svchp)
+    {
+        // logoff from database server
+        (void) OCILogoff(conn->svchp, conn->errhp);
+        
+        // free envirounment handle (error & service handles will be freed too)
+        (void) OCIHandleFree(conn->envhp, OCI_HTYPE_ENV);
+        delete conn; conn = 0L;
+    }
+}
+
+Connection* SingleConnectionPool::getConnection()
+{
+    // wait mutex here
+    return conn;
+}
+
+void SingleConnectionPool::freeConnection(Connection* connection)
+{
+    // free mutex here
+}
+/* ----------------------------- ConnectionPool ------------------------ */
+
+}}
+
---
+++ src/store/ConnectionManager.h
@@ -0,0 +1,95 @@
+#ifndef CONNECTION_MANAGER_DECLARATIONS
+#define CONNECTION_MANAGER_DECLARATIONS
+
+#include <oci.h>
+
+#include "StoreExceptions.h"
+
+namespace smsc { namespace store 
+{
+    class StoreConfig
+    {
+    private:
+        
+        char*   userName;
+        char*   userPwd;
+        char*   dbName;
+
+    public:
+
+        StoreConfig(const char* db, const char* usr, const char* pwd);
+        virtual ~StoreConfig();
+
+        inline const char* getUserName() { return userName; };
+        inline const char* getUserPwd() { return userPwd; };
+        inline const char* getDbName() { return dbName; };
+    };
+    
+    struct Connection
+    {
+	    OCIEnv*         envhp;  // OCI envirounment handle
+        OCISvcCtx*      svchp;  // OCI service handle
+	    OCIError*       errhp;	// OCI error handle
+		
+	    Connection(int _id) 
+	        : id(_id), envhp(0L), svchp(0L), errhp(0L) {};
+	    Connection(int _id, OCIEnv* _envhp, OCISvcCtx* _svchp, OCIError* _errhp)
+	        : id(_id), envhp(_envhp), svchp(_svchp), errhp(_errhp) {};
+	
+	    virtual ~Connection() {};
+    
+    private:
+	    int	id;
+    };
+    
+    class ConnectionPool
+    {
+    protected:
+
+	    StoreConfig*	config;
+        
+	    ConnectionPool(StoreConfig* _config) 
+            throw(ResourceAllocationException, AuthenticationException) 
+                : config(_config) {};
+
+    public:
+	
+	    virtual ~ConnectionPool() { 
+            if (config) delete config; 
+        };
+	
+	    virtual void connect() 
+            throw(ResourceAllocationException, AuthenticationException) = 0;
+	    virtual void disconnect() = 0;
+
+	    virtual Connection* getConnection() = 0;
+	    virtual void freeConnection(Connection* connection) = 0;
+    };
+    
+    class SingleConnectionPool : public ConnectionPool
+    {
+    private:
+    
+	    Connection*	conn;
+	    bool        lock; // Replace with mutex
+
+    public:
+    
+	    SingleConnectionPool(StoreConfig* _config)
+            throw(ResourceAllocationException, AuthenticationException);
+
+	    virtual ~SingleConnectionPool();
+
+	    virtual void connect()
+            throw(ResourceAllocationException, AuthenticationException);
+	    virtual void disconnect();
+
+	    virtual Connection* getConnection();
+	    virtual void freeConnection(Connection* connection);
+    };
+
+}}
+
+#endif
+
+
