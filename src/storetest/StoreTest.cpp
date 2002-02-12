8f7edea4e654772222313585fb101bf372a7b59f
makarov <>
2002-02-12 15:15:03 +0000

    *** empty log message ***

--- src/storetest/StoreTest.cpp
+++ src/storetest/StoreTest.cpp
@@ -1,28 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <OCIMessageStore.h>
+#include <StoreManager.h>
 
 int main(void) 
 {
     using namespace smsc::store;
-    using namespace smsc::store::oci;
-
-    OCIMessageStoreFactory factory;
-    OCIStoreConfig* config = new OCIStoreConfig("ORCL", "smsc", "smsc");
     
-    MessageStore* store = factory.createMessageStore(config);
+    MessageStore* store;
     
     try {
-        store->open();
-	printf("Connect Ok !\n");
+        store = StoreManager::getInstance();
+        printf("Connect Ok !\n");
     } 
-    catch (exception& ae) {
-	printf("Exception : %s\n", ae.what());
+    catch (exception& exc) {
+        printf("Exception : %s\n", exc.what());
+        return -1;
     }
-    
-    if (store) delete store;
-    if (config) delete config;
     
     return 0;
 }
