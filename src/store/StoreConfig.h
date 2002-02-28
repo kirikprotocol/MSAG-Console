4bc9ae3f4b27e47fb26a79a383e21fc6959d2793
makarov <>
2002-02-28 13:37:11 +0000

    initial version

---
+++ src/store/StoreConfig.h
@@ -0,0 +1,23 @@
+#ifndef STORE_CONFIG_DECLARATIONS
+#define STORE_CONFIG_DECLARATIONS
+
+namespace smsc { namespace store 
+{
+    class StoreConfig
+    {
+	public:
+
+        virtual ~StoreConfig();
+
+        virtual const char* getDBInstance() = 0;
+		virtual const char* getDBUserName() = 0;
+		virtual const char* getDBUserPassword() = 0;
+		
+		virtual int getMaxConnectionsCount() = 0;
+		virtual int getInitConnectionsCount() = 0;
+    };
+}}
+
+#endif
+
+
