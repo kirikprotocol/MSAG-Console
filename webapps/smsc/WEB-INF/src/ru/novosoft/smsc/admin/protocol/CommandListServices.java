5ab257fc31094e682c730813601bb4f16d7a0c1f
igork <>
2002-04-09 13:44:36 +0000

    no message

---
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/protocol/CommandListServices.java
@@ -0,0 +1,19 @@
+// Decompiled by Jad v1.5.5.3. Copyright 1997-98 Pavel Kouznetsov.
+// Jad home page:      http://web.unicom.com.cy/~kpd/jad.html
+// Decompiler options: packimports(3) printdflt 
+// Source File Name:   CommandListServices.java
+
+package ru.novosoft.smsc.admin.protocol;
+
+
+// Referenced classes of package ru.novosoft.smsc.admin.protocol:
+//      Command
+
+public class CommandListServices extends Command
+{
+
+  public CommandListServices()
+  {
+    super("list_services");
+  }
+}
