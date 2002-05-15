5237c170304645057e710adae0be888a0b364677
bryz <>
2002-05-15 14:45:55 +0000

    ���� ��� ������������full scan

--- sql/testing/msg_store_test_cases.sql
+++ sql/testing/msg_store_test_cases.sql
@@ -26,3 +26,9 @@ lock table sms_msg in exclusive mode
 commit
 
 
+--ѕроверка на использование индексов, full scan
+select stat.statistic#, nam.name, sum(stat.value) from v$sesstat stat, v$session ses, v$statname nam
+where stat.sid = ses.sid and stat.statistic# = nam.statistic# and stat.statistic# in (40,44,112,158,159,160,161,162,163,164,165,166,167,168,169,172,173,174,175,176,177,178,179,180,181,188,189,190,191,192) and
+ses.machine='smsc' and ses.osuser='bryz'
+group by stat.statistic#, nam.name having sum(stat.value) > 0
+order by stat.statistic#
