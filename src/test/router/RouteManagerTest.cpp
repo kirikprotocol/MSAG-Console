5597c7652997ddb835ab253038f18a987214c5cd
bryz <>
2002-05-16 11:03:12 +0000

    CR: CM for SMSC - ������ �outeManager::lookup()

--- src/test/router/RouteManagerTest.cpp
+++ src/test/router/RouteManagerTest.cpp
@@ -14,40 +14,79 @@ using smsc::smeman::SmeProxy;
 using smsc::test::sms::SmsUtil;
 
 static Category& log = Logger::getCategory("RouteManagerTest");
+SmeManagerTestCases tcSme;
+RouteManagerTestCases tcRoute;
+RouteRegistry routeReg;
+
+void addRouteNotMatch(const Address& origAddr, const Address& destAddr,
+	int smeNum, bool needProxy, int routeNum)
+{
+	SmeInfo sme;
+	SmeProxy* proxy = NULL;
+	cout << *tcSme.addCorrectSme(&sme, smeNum) << endl;
+	if (needProxy)
+	{
+		cout << *tcSme.registerCorrectSmeProxy(sme.systemId, &proxy) << endl;
+	}
+	TestRouteData routeData(origAddr, destAddr, proxy);
+	if (routeNum < 1000)
+	{
+		cout << *tcRoute.addCorrectRouteNotMatch(sme.systemId, &routeData, routeNum) << endl;
+	}
+	else
+	{
+		cout << *tcRoute.addCorrectRouteNotMatch2(sme.systemId, &routeData, routeNum) << endl;
+	}
+	routeReg.putRoute(routeData);
+}
+
+void addRouteMatch(const Address& origAddr, const Address& destAddr,
+	int smeNum, bool needProxy, int routeNum)
+{
+	SmeInfo sme;
+	SmeProxy* proxy = NULL;
+	cout << *tcSme.addCorrectSme(&sme, smeNum) << endl;
+	if (needProxy)
+	{
+		cout << *tcSme.registerCorrectSmeProxy(sme.systemId, &proxy) << endl;
+	}
+	TestRouteData routeData(origAddr, destAddr, proxy);
+	cout << *tcRoute.addCorrectRouteMatch(sme.systemId, &routeData, routeNum) << endl;
+	routeReg.putRoute(routeData);
+}
 
 void executeTest()
 {
-	SmeManagerTestCases tcSme;
-	RouteManagerTestCases tcRoute;
-	RouteRegistry routeReg;
 /*
-addCorrectSme(3)->
-addCorrectRouteMatch(46)->
-lookupRoute(1){104}
+addCorrectSme(10)->
+registerCorrectSmeProxy(1)->
+addCorrectRouteNotMatch(25)->
+
+addCorrectSme(6)->
+registerCorrectSmeProxy(1)->
+addCorrectRouteMatch(18)->
+
+addCorrectSme(5)->
+registerCorrectSmeProxy(1)->
+addCorrectRouteMatch(1)->
+lookupRoute(1){101}
 */
-	SmeInfo sme;
-	cout << *tcSme.addCorrectSme(&sme, 3) << endl;
-
-	SmeProxy* proxy;
-	cout << *tcSme.registerCorrectSmeProxy(sme.systemId, &proxy) << endl;
-
 	Address origAddr, destAddr;
 	SmsUtil::setupRandomCorrectAddress(&origAddr);
 	SmsUtil::setupRandomCorrectAddress(&destAddr);
-	TestRouteData routeData(origAddr, destAddr, proxy);
-	cout << *tcRoute.addCorrectRouteMatch(sme.systemId, &routeData, 46) << endl;
-	routeReg.putRoute(routeData);
-	
+
+	addRouteNotMatch(origAddr, destAddr, 10, true, 25);
+	addRouteMatch(origAddr, destAddr, 6, true, 18);
+	addRouteMatch(origAddr, destAddr, 5, true, 1);
 	cout << *tcRoute.lookupRoute(routeReg, origAddr, destAddr) << endl;
 
 /*
-	cout << *tcSme.addCorrectSme(&sme, 1) << endl;
-	cout << *tcSme.registerCorrectSmeProxy(sme.systemId) << endl;
-	cout << *tcRoute.addCorrectRoute(sme.systemId, &routeData, 1) << endl;
-	cout << *tcRoute.addCorrectRoute2(sme.systemId, &routeData, 1) << endl;
-	cout << *tcRoute.addIncorrectRoute(sme.systemId, *routeData.route, 1) << endl;
-	cout << *tcRoute.lookupRoute(origAddr, destAddr, routeData) << endl;
-	cout << *tcRoute.iterateRoutes(routeData) << endl;
+	cout << *tcRoute.addCorrectRouteMatch(sme.systemId, &routeData, 1) << endl;
+	cout << *tcRoute.addCorrectRouteNotMatch(sme.systemId, &routeData, 1) << endl;
+	cout << *tcRoute.addCorrectRouteNotMatch2(sme.systemId, &routeData, 1) << endl;
+	cout << *tcRoute.addIncorrectRoute(sme.systemId, routeInfo, 1) << endl;
+	cout << *tcRoute.lookupRoute(routeReg, origAddr, destAddr) << endl;
+	cout << *tcRoute.iterateRoutes(routeReg) << endl;
 */
 
 }
