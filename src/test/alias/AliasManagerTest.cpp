0935b01376d468afb2dfdffc2b87b3a6571befc8
bryz <>
2002-05-20 04:35:58 +0000

    CR: CM for SMSC - AliasManager::AddressToAlias() �� ����

--- src/test/alias/AliasManagerTest.cpp
+++ src/test/alias/AliasManagerTest.cpp
@@ -6,6 +6,7 @@ using smsc::util::Logger;
 using smsc::sms::Address;
 using smsc::alias::AliasManager;
 using smsc::test::sms::SmsUtil;
+using smsc::test::core::AliasRegistry;
 using smsc::test::core::TestAliasData;
 using smsc::test::alias::AliasManagerTestCases;
 using namespace smsc::test::util;
@@ -15,21 +16,29 @@ static Category& log = Logger::getCategory("AliasManagerTest");
 void executeTest()
 {
 /*
-addCorrectAliasException(3)->
-addCorrectAliasMatch(10){100}
+addCorrectAliasNotMatchAddress(1)->
+addCorrectAliasException(1)->
+findAliasByAddress(1){100}
 */
 	AliasManager aliasMan;
 	AliasManagerTestCases tc(&aliasMan);
+	AliasRegistry aliasReg;
 
 	Address alias, addr;
 	SmsUtil::setupRandomCorrectAddress(&alias);
 	SmsUtil::setupRandomCorrectAddress(&addr);
 
+	/*
 	TestAliasData data(alias, addr);
-	cout << *tc.addCorrectAliasException(&data, 3) << endl;
+	cout << *tc.addCorrectAliasNotMatchAddress(&data, 1) << endl;
+	aliasReg.putAlias(data);
+	*/
 
 	TestAliasData data2(alias, addr);
-	cout << *tc.addCorrectAliasMatch(&data2, 10) << endl;
+	cout << *tc.addCorrectAliasException(&data2, 1) << endl;
+	aliasReg.putAlias(data2);
+
+	cout << *tc.findAliasByAddress(aliasReg, addr) << endl;
 /*
 	cout << *tc.addCorrectAliasMatch(&data, 1) << endl;
 	cout << *tc.addCorrectAliasNotMatchAddress(&data, 1) << endl;
@@ -37,10 +46,8 @@ addCorrectAliasMatch(10){100}
 	cout << *tc.addCorrectAliasException(&data, 1) << endl;
 	cout << *tc.addIncorrectAlias() << endl;
 	cout << *tc.deleteAliases() << endl;
-	cout << *tc.findAliasByAddress(const AliasRegistry& aliasReg,
-		const Address& addr) << endl;
-	cout << *tc.findAddressByAlias(const AliasRegistry& aliasReg,
-		const Address& alias) << endl;
+	cout << *tc.findAliasByAddress(aliasReg, addr) << endl;
+	cout << *tc.findAddressByAlias(aliasReg, alias) << endl;
 	cout << *tc.iterateAliases(const AliasRegistry& aliasReg) << endl;
 */
 
