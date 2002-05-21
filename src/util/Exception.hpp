e5c0199fc8a3aaa4cc0361981a4fab8971a0cc7f
skv <>
2002-05-21 06:34:31 +0000

    ... in arguments of constructor

--- src/util/Exception.hpp
+++ src/util/Exception.hpp
@@ -3,13 +3,35 @@
 
 #include <exception>
 #include <string>
+#include <stdarg.h>
+#include <stdio.h>
 
 namespace smsc{
 namespace util{
 
 class Exception:public std::exception{
 public:
-  Exception(const char* msg):message(msg){}
+  Exception(const char* fmt,...)
+  {
+    int size=strlen(fmt)*4;
+    char *buf=new char[size];
+    va_list arglist;
+    int res;
+    do{
+      va_start(arglist,fmt);
+      res=vsnprintf( buf, size,fmt,arglist);
+      va_end(arglist);
+      if(res<0)
+      {
+        delete [] buf;
+        size-=res;
+        size+=4;
+        buf=new char[size];
+      }
+    }while(res<0);
+    message=buf;
+    delete [] buf;
+  }
   virtual ~Exception()throw(){}
   virtual const char* what()const throw(){return message.c_str();}
 protected:
