de89ecb872171d77ed090b1c63c68d0e3ece19e3
bryz <>
2002-05-23 14:34:08 +0000

    ���etFirstPendingSubmitSmPdu() ���removePdu() �utPdu() ���aitTimeMap

--- src/test/core/PduRegistry.cpp
+++ src/test/core/PduRegistry.cpp
@@ -12,6 +12,7 @@ void PduRegistry::putPdu(PduData& data)
 	idMap[pduData->smsId] = pduData;
 	seqNumMap[pduData->pdu->get_sequenceNumber()] = pduData;
 	msgRefMap[pduData->msgRef] = pduData;
+	waitTimeMap.insert(WaitTimeMapPair(pduData->waitTime, pduData));
 }
 
 PduData* PduRegistry::getPdu(uint16_t msgRef)
@@ -50,8 +51,25 @@ bool PduRegistry::removePdu(const PduData& pduData)
 	idMap.erase(pduData.smsId);
 	seqNumMap.erase(pduData.pdu->get_sequenceNumber());
 	msgRefMap.erase(pduData.msgRef);
+	WaitTimeMap::iterator it = waitTimeMap.find(pduData.waitTime);
+	for (; it != waitTimeMap.end() && it->first == pduData.waitTime; it++)
+	{
+		if (it->second->pdu->get_sequenceNumber() ==
+			pduData.pdu->get_sequenceNumber() &&
+			it->second->msgRef == pduData.msgRef)
+		{
+			waitTimeMap.erase(it);
+			break;
+		}
+	}
 	delete pduData.pdu;
 	return true;
+}
+
+PduData* PduRegistry::getFirstPendingSubmitSmPdu(time_t waitTime)
+{
+    WaitTimeMap::iterator it = waitTimeMap.lower_bound(waitTime);
+	return (it == waitTimeMap.end() ? NULL : it->second);
 }
 
 }
