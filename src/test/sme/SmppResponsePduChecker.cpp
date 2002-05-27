28394e788a080a3454f6843c3aecd8cc604c461a
bryz <>
2002-05-27 11:19:15 +0000

    �������

--- src/test/sme/SmppResponsePduChecker.cpp
+++ src/test/sme/SmppResponsePduChecker.cpp
@@ -1,18 +1,22 @@
 #include "SmppResponsePduChecker.hpp"
+#include "test/smpp/SmppUtil.hpp"
 
 namespace smsc {
 namespace test {
 namespace sme {
 
+using smsc::test::smpp::SmppUtil;
 using namespace smsc::smpp::SmppCommandSet; //constants
+using namespace smsc::smpp::SmppStatusSet; //constants
 
-SmppResponsePduChecker::SmppResponsePduChecker()
-{
-}
+SmppResponsePduChecker::SmppResponsePduChecker(PduRegistry* _pduReg,
+	const RouteChecker* _routeChecker)
+	: pduReg(_pduReg), routeChecker(_routeChecker) {}
 	
 vector<int> SmppResponsePduChecker::checkSubmitSmResp(
-	PduSubmitSm& pdu, PduSubmitSmResp& respPdu)
+	PduData* pduData, PduSubmitSmResp& respPdu)
 {
+	__require__(pduData && pduData->pdu);
 	vector<int> res;
 	if (respPdu.get_header().get_commandLength() != 81)
 	{
@@ -23,17 +27,92 @@ vector<int> SmppResponsePduChecker::checkSubmitSmResp(
 		res.push_back(2);
 	}
 	if (respPdu.get_header().get_sequenceNumber() !=
-		pdu.get_header().get_sequenceNumber())
+		pduData->pdu->get_sequenceNumber())
 	{
 		res.push_back(3);
 	}
-	//для commandStatus нужны более детальные проверки
-	if (respPdu.get_header().get_commandStatus())
+	if (pduData->pdu->get_commandId() != SUBMIT_SM)
 	{
-		res.push_back(-respPdu.get_header().get_commandStatus());
+		res.push_back(4);
+	}
+	PduSubmitSm* pdu = reinterpret_cast<PduSubmitSm*>(pduData->pdu);
+	time_t validTime =
+		SmppUtil::string2time(pdu->get_message().get_validityPeriod());
+	time_t waitTime =
+		SmppUtil::string2time(pdu->get_message().get_scheduleDeliveryTime());
+	switch (respPdu.get_header().get_commandStatus())
+	{
+		case ESME_ROK: //No Error
+			if (!routeChecker->checkExistsReachableRoute(
+				pdu->get_message().get_dest(), false)) //без проверки на bound sme
+			{
+				res.push_back(11);
+			}
+			if (validTime < time(NULL))
+			{
+				res.push_back(12);
+			}
+			if (validTime > __maxValidPeriod__)
+			{
+				res.push_back(13);
+			}
+			if (validTime < waitTime)
+			{
+				res.push_back(14);
+			}
+			//если данная pdu замещает предыдущую pdu
+			for (PduData* replaceData = pduData->replacePdu; replaceData; )
+			{
+				PduData* nextReplaceData = replaceData->replacePdu;
+				//replaceData->responseFlag
+				replaceData->deliveryFlag = true;
+				replaceData->deliveryReceiptFlag = true;
+				replaceData->intermediateNotificationFlag = true;
+				if (replaceData->complete())
+				{
+					pduReg->removePdu(replaceData);
+				}
+				replaceData = nextReplaceData;
+			}
+			break;
+		case ESME_RINVDSTADR: //Invalid Dest Addr
+			if (!routeChecker->checkExistsUnreachableRoute(
+				pdu->get_message().get_dest(), false)) //без проверки на bound sme
+			{
+				res.push_back(15);
+			}
+			break;
+		case ESME_RINVSCHED: //Invalid Scheduled Delivery Time
+			if (validTime < waitTime)
+			{
+				res.push_back(16);
+			}
+			break;
+		case ESME_RINVEXPIRY: //Invalid message validity period
+			if (validTime > time(NULL) && validTime <= __maxValidPeriod__)
+			{
+				res.push_back(17);
+			}
+			break;
+		default:
+			res.push_back(-respPdu.get_header().get_commandStatus());
+	}
+	if (respPdu.get_header().get_commandStatus() != ESME_ROK)
+	{
+		pduReg->removePdu(pduData);
 	}
 	return res;
 }
+
+/*
+ESME_RINVDSTADR 0x0000000B Invalid Dest Addr
+ESME_RINVSCHED 0x00000061 Invalid Scheduled Delivery Time
+ESME_RINVEXPIRY 0x00000062 Invalid message validity period (Expiry time)
+bool SmppTestCases::checkMissingDeliveryPdu(PduData* pduData)
+{
+	return ();
+}
+*/
 
 }
 }
--- src/test/sme/SmppResponsePduChecker.hpp
+++ src/test/sme/SmppResponsePduChecker.hpp
@@ -2,6 +2,8 @@
 #define TEST_SME_SMPP_RESPONSE_PDU_CHECKER
 
 #include "smpp/smpp_structures.h"
+#include "test/core/PduRegistry.hpp"
+#include "test/core/RouteChecker.hpp"
 #include <vector>
 
 namespace smsc {
@@ -9,15 +11,22 @@ namespace test {
 namespace sme {
 
 using std::vector;
+using smsc::test::core::PduRegistry;
+using smsc::test::core::RouteChecker;
+using smsc::test::core::PduData;
 using namespace smsc::smpp; //pdu
 
 class SmppResponsePduChecker
 {
 public:
-	SmppResponsePduChecker();
+	SmppResponsePduChecker(PduRegistry* pduReg, const RouteChecker* routeChecker);
 	~SmppResponsePduChecker() {}
 	
-	vector<int> checkSubmitSmResp(PduSubmitSm& pdu, PduSubmitSmResp& respPdu);
+	vector<int> checkSubmitSmResp(PduData* pduData, PduSubmitSmResp& respPdu);
+
+private:
+	const RouteChecker* routeChecker;
+	PduRegistry* pduReg;
 };
 
 }
