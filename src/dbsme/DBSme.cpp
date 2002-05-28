9d1289248b41e6d8b70f50ce3bad585a825a8d69
makarov <>
2002-05-28 09:09:58 +0000

    sql job & io first raw versions were added

--- src/dbsme/DBSme.cpp
+++ src/dbsme/DBSme.cpp
@@ -9,6 +9,7 @@
 #include "CommandProcessor.h"
 
 #include <dbsme/jobs/SampleJob.h>
+#include <dbsme/jobs/SQLJob.h>
 
 int main(void) 
 {
@@ -19,13 +20,15 @@ int main(void)
     using smsc::util::config::ConfigView;
     using smsc::util::config::ConfigException;
     
-    const char* OCI_DS_FACTORY_IDENTITY = "OCI";
     const char* DEFAULT_JOB_IDENTITY = "default";
     const char* SAMPLE_JOB_IDENTITY = "sample1";
+    const char* SQL_JOB_IDENTITY = "sql-job";
     
     SampleJobFactory _sampleJobFactory;
+    SQLJobFactory _sqlJobFactory;
     JobFactory::registerFactory(&_sampleJobFactory, SAMPLE_JOB_IDENTITY);
     JobFactory::registerFactory(&_sampleJobFactory, DEFAULT_JOB_IDENTITY);
+    JobFactory::registerFactory(&_sqlJobFactory, SQL_JOB_IDENTITY);
 
     ConfigView  *dsConfig, *cpConfig;
     try 
@@ -38,12 +41,13 @@ int main(void)
 
         cpConfig = new ConfigView(manager, "Applications.DBSme");
         CommandProcessor processor(cpConfig);
-        Command command;
         
+        Command command;
         const char* toAddressStr = "1111111";
         Address toAddress(strlen(toAddressStr), 0, 0, toAddressStr);
         command.setToAddress(toAddress);
-        command.setJobName(SAMPLE_JOB_IDENTITY);
+        //command.setJobName(SAMPLE_JOB_IDENTITY);
+        command.setJobName(SQL_JOB_IDENTITY);
 
         processor.process(command);
     } 
