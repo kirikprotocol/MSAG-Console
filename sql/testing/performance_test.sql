efc83faf87231294991b32d0b592aa8e9f06b94c
bryz <>
2002-07-30 10:59:30 +0000

    ��� ���� �������

--- sql/testing/performance_test.sql
+++ sql/testing/performance_test.sql
@@ -1,15 +1,13 @@
 create or replace procedure insert_record(msg sms_msg%rowtype) is
 begin
-	insert into sms_msg (id,st,mr,oa_ton,oa_npi,oa_val,src_msc,src_imsi,
-		src_sme_n,da_ton,da_npi,da_val,dst_msc,dst_imsi,dst_sme_n,wait_time,
-		valid_time,submit_time,attempts,last_result,last_try_time,next_try_time,
-		svc_type,dr,arc,pri,pid,udhi,dcs,udl,ud,rcpt_id,esm_class)
-	values (msg.id,msg.st,msg.mr,msg.oa_ton,msg.oa_npi,msg.oa_val,msg.src_msc,
-		msg.src_imsi,msg.src_sme_n,msg.da_ton,msg.da_npi,msg.da_val,msg.dst_msc,
-		msg.dst_imsi,msg.dst_sme_n,msg.wait_time,msg.valid_time,msg.submit_time,
-		msg.attempts,msg.last_result,msg.last_try_time,msg.next_try_time,
-		msg.svc_type,msg.dr,msg.arc,msg.pri,msg.pid,msg.udhi,msg.dcs,msg.udl,
-		msg.ud,msg.rcpt_id,msg.esm_class);
+	insert into sms_msg (id,st,submit_time,valid_time,attempts,last_result,
+		last_try_time,next_try_time,oa,da,dda,mr,svc_type,arc,dr,br,src_msc,
+		src_imsi,src_sme_n,dst_msc,dst_imsi,dst_sme_n,body_len,body)
+	values (msg.id,msg.st,msg.submit_time,msg.valid_time,msg.attempts,
+		msg.last_result,msg.last_try_time,msg.next_try_time,msg.oa,msg.da,
+		msg.dda,msg.mr,msg.svc_type,msg.arc,msg.dr,msg.br,msg.src_msc,
+		msg.src_imsi,msg.src_sme_n,msg.dst_msc,msg.dst_imsi,msg.dst_sme_n,
+		msg.body_len,msg.body);
 end;
 /
 
@@ -41,37 +39,31 @@ begin
 		--createSms()
 		msg.id := to_char(i, 'fm0999999999999999');
 		msg.st := 0;
-		msg.mr := 69;
-		msg.oa_ton := 101;
-		msg.oa_npi := 102;
-		msg.oa_val := '123456789012345678901';
-		msg.src_msc := '123456789012345678901';
-		msg.src_imsi := '123456789012345678901';
-		msg.src_sme_n := 123456789;
-		msg.da_ton := 103;
-		msg.da_npi := 104;
-		msg.da_val := '123456789012345678901';
-		msg.dst_msc := '123456789012345678901';
-		msg.dst_imsi := '123456789012345678901';
-		msg.dst_sme_n := 123456789;
-		msg.wait_time := sysdate;
-		msg.valid_time := sysdate;
 		msg.submit_time := sysdate;
+		msg.valid_time := sysdate;
 		msg.attempts := 5;
 		msg.last_result := 105;
 		msg.last_try_time := sysdate;
 		msg.next_try_time := sysdate;
+		msg.oa := '101.101.12345678901234567890';
+		msg.da := '102.102.12345678901234567890';
+		msg.dda := '103.103.12345678901234567890';
+		msg.mr := 69;
 		msg.svc_type := '123456';
-		msg.dr := 106;
 		msg.arc := 'Y';
-		msg.pri := 107;
-		msg.pid := 108;
-		msg.udhi := 'Y';
-		msg.dcs := 109;
-		msg.udl := 200;
-		msg.ud := hextoraw('313839453435338414c4947495946475559464359544448474b4d54f4a494849464448544b485495554f5949465554464c494a4f49555494f594f4947484b55484b4c554748594a46555446545548544459525345524145575847564e42494a54f4a4f549484959475954445246434e424859465954445254525344545344554755544446595244544452434b42484b4c4a494f4849475546544452534454455346484c4a4f4954a554f4946544454444a48564b48554f5949484c49484759464a54464a4c4c4c4');
-		msg.rcpt_id := msg.id;
-		msg.esm_class := 110;
+		msg.dr := 106;
+		msg.br := 101;
+		msg.src_msc := '123456789012345678901';
+		msg.src_imsi := '123456789012345678901';
+		msg.src_sme_n := 123456789;
+		msg.dst_msc := '123456789012345678901';
+		msg.dst_imsi := '123456789012345678901';
+		msg.dst_sme_n := 123456789;
+		--msg.body_len := 550; -- = 18*12 (int fileds) + 254 + 8 (short_message) + 64 + 8 (receipted_message_id)
+		--msg.body := hextoraw('313839453435338414c4947495946475559464359544448474b4d54f4a494849464448544b485495554f5949465554464c494a4f49555494f594f4947484b55484b4c554748594a46555446545548544459525345524145575847564e42494a54f4a4f549484959475954445246434e424859465954445254525344545344554755544446595244544452434b42484b4c4a494f4849475546544452534454455346484c4a4f4954a554f4946544454444a48564b48554f5949484c49484759464a54464a4c4c4c4313839453435338414c4947495946475559464359544448474b4d54f4a494849464448544b485495554f5949465554464c494a4f49555494f594f4947484b55484b4c554748594a46555446545548544459525345524145575847564e42494a54f4a4f549484959475954445246434e424859465954445254525344545344554755544446595244544452434b42484b4c4a494f4849475546544452534454455346484c4a4f4954a554f4946544454444a48564b48554f5949484c49484759464a54464a4c4c4c4313839453435338414c4947495946475559464359544448474b4d54f4a494849464448544b485495554f5949465554464c494a4f49555494f594f4947484b55484b4c554748594a46555446545548544459525345524145575847564e42494a54f4a4f549484959475954445246434e424859465954445254525344545344554755544446595244544452434b42484b4c4a494f4849471');
+		msg.body_len := 200;
+		msg.body := hextoraw('313839453435338414c4947495946475559464359544448474b4d54f4a494849464448544b485495554f5949465554464c494a4f49555494f594f4947484b55484b4c554748594a46555446545548544459525345524145575847564e42494a54f4a4f549484959475954445246434e424859465954445254525344545344554755544446595244544452434b42484b4c4a494f4849475546544452534454455346484c4a4f4954a554f4946544454444a48564b48554f5949484c49484759464a54464a4c4c4c4');
+
 		insert_record(msg);
 		commit;
 		--changeSmsState()
