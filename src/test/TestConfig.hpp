10fc5c9874244a5ac3cfd8e1dccee38ee61e17fc
bryz <>
2002-08-19 13:32:22 +0000

    ���axWaitTime �axDeliveryPeriod

--- src/test/TestConfig.hpp
+++ src/test/TestConfig.hpp
@@ -9,12 +9,14 @@ namespace test {
 using smsc::sms::Address;
 
 //конфигурационный параметр core/reschedule_table
-const int rescheduleTimes[] = {20, 10};
-const int rescheduleSize = sizeof(rescheduleTimes) / sizeof(int);
+const int rescheduleTimes[] = {30, 20};
+const int rescheduleSize = sizeof(rescheduleTimes) / sizeof(*rescheduleTimes);
 
 const int timeCheckAccuracy = 5; //5 сек
 const int sequentialPduInterval = 10; //10 сек
-const int maxValidPeriod = 24 * 3600; //1 сутки
+const int maxValidPeriod = 86400; //1 сутки
+const int maxWaitTime = 60; //max(waitTime)
+const int maxDeliveryPeriod = 120; //max(validTime - waitTime)
 
 //адрес SC
 const uint8_t smscAddrTon = 3;
