6911d966272646430ebf53586da775889bca78dd
igork <>
2002-08-26 12:14:54 +0000

    bugfix: alias attribute "hide" maked really optional

--- src/util/config/alias/aliasconf.cpp
+++ src/util/config/alias/aliasconf.cpp
@@ -114,7 +114,8 @@ AliasConfig::status AliasConfig::load(const char * const filename)
       }
       //record->alias = attrs.getNameItem("alias").getNodeValue().transcode();
       {
-				record->hide=!strcmp(attrs.getNamedItem("hide").getNodeValue().transcode(),"true");
+        DOM_Node hide_attr_node = attrs.getNamedItem("hide");
+				record->hide=!hide_attr_node.isNull() && !strcmp(node.getNodeValue().transcode(),"true");
 				__trace2__("record->hide: %s",record->hide?"true":"false");
         char* dta = attrs.getNamedItem("alias").getNodeValue().transcode();
         record->aliasValue = new char[21]; memset(record->aliasValue,0,21);
