73c0fe2240e605c80332818de434efb498ed204e
igork <>
2002-09-03 09:06:30 +0000

    no message

--- src/system/smscmain.cpp
+++ src/system/smscmain.cpp
@@ -1,34 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <exception>
+#include <sys/stat.h>
 #include "system/smsc.hpp"
 #include "alias/aliasman.h"
 #include "util/config/alias/aliasconf.h"
 #include "util/config/route/RouteConfig.h"
+#include <admin/service/ComponentManager.h>
+#include <admin/service/AdminSocketManager.h>
+#include <admin/smsc_service/SmscComponent.h>
+#include <admin/smsc_service/SmscShutdownHandler.h>
+
+bool file_exist(const char * const filename)
+{
+  struct stat buf;
+  return stat(filename, &buf) == 0;
+}
+
+char get_filename_result[128];
+const char * const get_filename(const char * const file_to_find)
+throw (smsc::admin::AdminException)
+{
+  if (file_exist(file_to_find)) {
+    strcpy(get_filename_result, file_to_find);
+    return get_filename_result;
+  } else {
+    char buf[strlen(file_to_find)+8+1];
+    strcpy(buf, "../conf/");
+    strcat(buf, file_to_find);
+    if (file_exist(buf)) {
+      strcpy(get_filename_result, buf);
+      return get_filename_result;
+    }
+  }
+  char message[strlen(file_to_find) + 64];
+  sprintf(message, "File \"%s\" not found", file_to_find); 
+  throw smsc::admin::AdminException(message);
+}
 
 int main(int argc,char* argv[])
 {
   try{
-    smsc::system::Smsc *app=new smsc::system::Smsc;
     smsc::system::SmscConfigs cfgs;
-    smsc::util::config::Manager::init("config.xml");
+    smsc::util::config::Manager::init(get_filename("config.xml"));
     cfgs.cfgman=&cfgs.cfgman->getInstance();
     smsc::util::config::smeman::SmeManConfig smemancfg;
-    smemancfg.load("sme.xml");
+    smemancfg.load(get_filename("sme.xml"));
     cfgs.smemanconfig=&smemancfg;
     smsc::util::config::alias::AliasConfig aliascfg;
-    aliascfg.load("aliases.xml");
+    aliascfg.load(get_filename("aliases.xml"));
     cfgs.aliasconfig=&aliascfg;
     smsc::util::config::route::RouteConfig rc;
-    rc.load("routes.xml");
+    rc.load(get_filename("routes.xml"));
     cfgs.routesconfig=&rc;
 
-    app->init(cfgs);
-    app->run();
+		in_port_t servicePort = 0;
+		if (argc > 1)
+		{
+			servicePort = atoi(argv[1]);
+		}
+		if (servicePort == 0)
+		{
+			servicePort = cfgs.cfgman->getInt("admin.port");
+		}
+    if (servicePort == 0) {
+      fprintf(stderr,"WARNING: admin port not specified, admin module disabled - smsc is not administrable\n");
+      
+      smsc::system::Smsc *app=new smsc::system::Smsc;
+      
+      app->init(cfgs);
+      app->run();
+      
+      fprintf(stderr,"quiting smsc\n");
+      delete app;
+      fprintf(stderr,"app deleted\n");
+    } else {
+      using namespace smsc::admin::smsc_service;
+      using smsc::util::config::Manager;
 
-    fprintf(stderr,"quiting smsc\n");
-    delete app;
-    fprintf(stderr,"app deleted\n");
+      // init Admin part
+      SmscComponent smsc_component(cfgs);
+      ComponentManager::registerComponent(&smsc_component);
+
+      SmscShutdownHandler shutdown_handler(smsc_component);
+      SmscShutdownHandler::registerShutdownHandler(&shutdown_handler);
+
+      // start
+      smsc_component.runSmsc();
+      AdminSocketManager::start(cfgs.cfgman->getString("admin.host"), servicePort);
+      fprintf(stderr,"smsc started");
+
+      //running
+      AdminSocketManager::WaitFor();
+
+      fprintf(stderr,"smsc stopped, finishing\n");
+      // stopped
+      smsc_component.stopSmsc();
+      Manager::deinit();
+      
+      fprintf(stderr,"smsc finished\n");
+    }
+
   }catch(std::exception& e)
   {
     fprintf(stderr,"top level exception: %s\n",e.what());
