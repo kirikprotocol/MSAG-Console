da4057ce0cdbc8d6f0df925ca8f057a0c4ad628e
bryz <>
2002-09-17 14:24:55 +0000

    ������� �hpp -> cpp

---
+++ src/test/dbsme/DateFormatter.cpp
@@ -0,0 +1,133 @@
+#include "DateFormatter.hpp"
+
+namespace smsc {
+namespace test {
+namespace dbsme {
+
+void DateFormatter::processToken(ostream& res, const string& token, const tm& lt) const
+{
+	//Abbreviated weekday name
+	if (token == "w")
+	{
+		char str[16];
+		strftime(str, sizeof(str), "%a", &lt);
+		res << str;
+	}
+	//Full weekday name
+	else if (token == "W")
+	{
+		char str[16];
+		strftime(str, sizeof(str), "%A", &lt);
+		res << str;
+	}
+	else if (token == "d")
+	{
+		res << lt.tm_mday;
+	}
+	else if (token == "dd")
+	{
+		res << (lt.tm_mday < 10 ? "0" : "");
+		res << lt.tm_mday;
+	}
+	else if (token == "M")
+	{
+		res << (lt.tm_mon + 1);
+	}
+	else if (token == "MM")
+	{
+		res << (lt.tm_mon < 9 ? "0" : "");
+		res << (lt.tm_mon + 1);
+	}
+	//Abbreviated month name
+	else if (token == "MMM")
+	{
+		char str[16];
+		strftime(str, sizeof(str), "%b", &lt);
+		res << str;
+	}
+	//Full month name
+	else if (token == "MMMM")
+	{
+		char str[16];
+		strftime(str, sizeof(str), "%B", &lt);
+		res << str;
+	}
+	//Year without century
+	else if (token == "yy")
+	{
+		res << (lt.tm_year % 100 < 10 ? "0" : "");
+		res << (lt.tm_year % 100);
+	}
+	else if (token == "yyyy")
+	{
+		res << lt.tm_year + 1900;
+	}
+	else if (token == "h")
+	{
+		res << lt.tm_hour;
+	}
+	else if (token == "hh")
+	{
+		res << (lt.tm_hour < 10 ? "0" : "");
+		res << lt.tm_hour;
+	}
+	else if (token == "m")
+	{
+		res << lt.tm_min;
+	}
+	else if (token == "mm")
+	{
+		res << (lt.tm_min < 10 ? "0" : "");
+		res << lt.tm_min;
+	}
+	else if (token == "s")
+	{
+		res << lt.tm_sec;
+	}
+	else if (token == "ss")
+	{
+		res << (lt.tm_sec < 10 ? "0" : "");
+		res << lt.tm_sec;
+	}
+	//Current locale's A.M./P.M. indicator
+	else if (token == "t")
+	{
+		char str[8];
+		strftime(str, sizeof(str), "%p", &lt);
+		res << str;
+	}
+	else
+	{
+		res << token;
+	}
+}
+
+const string DateFormatter::format(time_t t) const
+{
+	//for (int i = 0; i < fmt.length(); i++)
+	ostringstream res;
+	tm lt;
+	localtime_r(&t, &lt);
+	string::const_iterator it_begin = fmt.begin();
+	for (string::const_iterator it = fmt.begin(); it != fmt.end(); it++)
+	{
+		if (!isalpha(*it))
+		{
+			string token(it_begin, it);
+			//cout << "token: " << token << endl;
+			processToken(res, token, lt);
+			res << *it;
+			it_begin = it;
+			it_begin++;
+		}
+	}
+	string token(it_begin, fmt.end());
+	//cout << "token: " << token << endl;
+	processToken(res, token, lt);
+	return res.str();
+}
+
+}
+}
+}
+
