236680d6b245239b79e715a1026122505c9590ea
bryz <>
2002-09-23 14:31:52 +0000

    ���n8, int64, ldbl

--- sql/testing/db_sme.sql
+++ sql/testing/db_sme.sql
@@ -2,12 +2,15 @@ drop table db_sme_test;
 create table db_sme_test
 (  
 	id number(10),
+	int8 number(3),
 	int16 number(5),
 	int32 number(10),
+	int64 number(20),
 	flt number(5,2),
 	dbl number(5,2),
-	dt date,
-	str varchar2(200)
+	ldbl number(5,2),
+	str varchar2(200),
+	dt date
 ) tablespace smsc_data;
 
 create unique index db_sme_test_idx on db_sme_test (id)
