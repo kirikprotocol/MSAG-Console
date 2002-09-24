bbef5dd0e468b0dc8be3a5955d689faa347f7ce6
bryz <>
2002-09-24 10:43:26 +0000

    no message

---
+++ src/test/dbsme/NumberFormatter.hpp
@@ -0,0 +1,63 @@
+#ifndef TEST_DBSME_NUMBER_FORMATTER
+#define TEST_DBSME_NUMBER_FORMATTER
+
+#include <string>
+
+namespace smsc {
+namespace test {
+namespace dbsme {
+
+using std::string;
+
+struct NumberFormatter
+{
+	virtual const string format(double val) const = NULL;
+};
+
+class FloatFormatter : public NumberFormatter
+{
+	char fmt[16];
+public:
+	FloatFormatter(int precision)
+	{
+		__require__(precision >= 0);
+		sprintf(fmt, "%%.%dlf", precision);
+	}
+	virtual const string format(double val) const
+	{
+		char res[32];
+		sprintf(res, fmt, val);
+		return res;
+	}
+};
+
+class DoubleFormatter : public NumberFormatter
+{
+	char fmt[16];
+public:
+	DoubleFormatter(int digits, bool exponent)
+	{
+		__require__(digits > 0);
+		if (exponent)
+		{
+			sprintf(fmt, "%%.%dle", digits - 1);
+		}
+		else
+		{
+			sprintf(fmt, "%%.%dlg", digits - 1);
+		}
+	}
+	virtual const string format(double val) const
+	{
+		char res[32];
+		sprintf(res, fmt, val);
+		return res;
+	}
+};
+
+}
+}
+}
+
+#endif /* TEST_DBSME_NUMBER_FORMATTER */
+
