545785643a5c0e8f937fce82d2640e99647f704c
bryz <>
2002-09-24 10:42:30 +0000

    ����� ��

--- src/test/dbsme/DbSmeFormatJobTestCases.cpp
+++ src/test/dbsme/DbSmeFormatJobTestCases.cpp
@@ -108,34 +108,49 @@ const string DbSmeDateFormatJobTestCases::processJobFirstOutput(const string& te
 	return "";
 }
 
-DbSmeTestRecord* DbSmeOtherFormatJobTestCases::createJobInput(
-	bool params)
+DbSmeTestRecord* DbSmeOtherFormatJobTestCases::newDefInput()
+{
+	DbSmeTestRecord* rec = new DbSmeTestRecord();
+	rec->setInt8(8);
+	rec->setInt16(16);
+	rec->setInt32(32);
+	rec->setInt64(64);
+	rec->setFloat(16.16);
+	rec->setDouble(32.32);
+	rec->setLongDouble(64.64);
+	rec->setString("xxx");
+	return rec;
+}
+
+DbSmeTestRecord* DbSmeOtherFormatJobTestCases::createValuesJobInput()
 {
 	__decl_tc__;
+	__tc__("submitDbSmeCmd.correct.job.select"); __tc_ok__;
 	DbSmeTestRecord* rec = new DbSmeTestRecord();
 	rec->setJob("OtherFormatJob");
-	rec->setDefInput(new DbSmeTestRecord());
-	rec->getDefInput()->setInt16(16);
-	rec->getDefInput()->setInt32(32);
-	rec->getDefInput()->setFloat(16.16);
-	rec->getDefInput()->setDouble(32.32);
-	rec->getDefInput()->setString("xxx");
+	rec->setDefInput(newDefInput());
+	setRandomInputInt8(rec);
+	setRandomInputInt16(rec);
+	setRandomInputInt32(rec);
+	setRandomInputInt64(rec);
+	setRandomInputFloat(rec);
+	setRandomInputDouble(rec);
+	setRandomInputLongDouble(rec);
+	setRandomInputString(rec, rand0(1));
+	return rec;
+}
+
+DbSmeTestRecord* DbSmeOtherFormatJobTestCases::createDefaultsJobInput()
+{
+	__decl_tc__;
 	__tc__("submitDbSmeCmd.correct.job.select"); __tc_ok__;
-	if (params)
-	{
-		setRandomInputInt16(rec);
-		setRandomInputInt32(rec);
-		setRandomInputFloat(rec);
-		setRandomInputDouble(rec);
-		setRandomInputString(rec, rand0(1));
-	}
-	else
-	{
-		__tc__("submitDbSmeCmd.correct.input.noParams"); __tc_ok__;
-		__tc__("submitDbSmeCmd.correct.defaultInput.int"); __tc_ok__;
-		__tc__("submitDbSmeCmd.correct.defaultInput.float"); __tc_ok__;
-		__tc__("submitDbSmeCmd.correct.defaultInput.string"); __tc_ok__;
-	}
+	DbSmeTestRecord* rec = new DbSmeTestRecord();
+	rec->setJob("OtherFormatJob");
+	rec->setDefInput(newDefInput());
+	__tc__("submitDbSmeCmd.correct.input.noParams"); __tc_ok__;
+	__tc__("submitDbSmeCmd.correct.defaultInput.int"); __tc_ok__;
+	__tc__("submitDbSmeCmd.correct.defaultInput.float"); __tc_ok__;
+	__tc__("submitDbSmeCmd.correct.defaultInput.string"); __tc_ok__;
 	return rec;
 }
 
@@ -147,30 +162,41 @@ const string DbSmeOtherFormatJobTestCases::processJobFirstOutput(const string& t
 	__tc__("processDbSmeRes.select.singleRecord"); __tc_ok__;
 	bool res = true;
 	ostringstream os;
-	os << endl << "from-address: '" << getOutputFromAddress(rec) << "';" << endl;
+	os << endl << "job-name: '" << getOutputJobName(rec) << "'" << endl;
+	os << "from-address: /" << getOutputFromAddress(rec) << "/" << endl;
+	os << "to-address: \\" << getOutputToAddress(rec) << "\\" << endl;
 	string str = getOutputString(rec, NULL, res);
 	os << "string: \"" << str << "\"," << endl;
 	__tc__("processDbSmeRes.output.string.left"); __tc_ok__;
 	__tc__("processDbSmeRes.output.string.right"); __tc_ok__;
 	__tc__("processDbSmeRes.output.string.center"); __tc_ok__;
-	if (str.length() < 30)
+	const int fieldLen = 20;
+	if (str.length() < fieldLen)
 	{
-		os << "string_right : " << string(30 - str.length(), ' ') << str << "," << endl;
-		int tmp = (30 - str.length()) / 2;
-		os << "string_center: " << string(tmp, ' ') << str <<
-			string(30 - tmp - str.length(), ' ') << "," << endl;
-		os << "string_left  : " << str << string(30 - str.length(), ' ') << ";" << endl;
+		os << "string_right : _" << string(fieldLen - str.length(), ' ') <<
+			str << ";" << endl;
+		int tmp = (fieldLen - str.length()) / 2;
+		os << "string_center: -" << string(tmp, ' ') << str <<
+			string(fieldLen - tmp - str.length(), ' ') << "." << endl;
+		os << "string_left  : +" << str <<
+			string(fieldLen - str.length(), ' ') << "?" << endl;
 	}
 	else
 	{
-		os << "string_right : " << str << "," << endl;
-		os << "string_center: " << str << "," << endl;
-		os << "string_left  : " << str << ";" << endl;
+		os << "string_right : _" << str << ";" << endl;
+		os << "string_center: -" << str << "." << endl;
+		os << "string_left  : +" << str << "?" << endl;
 	}
-	os << "int16: (--$" << getOutputInt16(rec, NULL, res) << "--)," << endl;
-	os << "int32: (++$" << getOutputInt32(rec, NULL, res) << "++)," << endl;
-	os << "float: (**$" << getOutputFloat(rec, NULL, res) << "**)," << endl;
-	os << "double: (::$" << getOutputDouble(rec, NULL, res) << "::);" << endl;
+	os << "int8: $" << getOutputInt8(rec, NULL, res) << "!" << endl;
+	os << "int16: <" << getOutputInt16(rec, NULL, res) << "@" << endl;
+	os << "int32: >" << getOutputInt32(rec, NULL, res) << "#" << endl;
+	os << "int64: (" << getOutputInt16(rec, NULL, res) << ")" << endl;
+	static const FloatFormatter ff(1);
+	static const DoubleFormatter df(3, true);
+	static const DoubleFormatter ldf(2, false);
+	os << "float: *" << getOutputFloat(rec, NULL, ff, res) << "%" << endl;
+	os << "double: &" << getOutputDouble(rec, NULL, df, res) << "|" << endl;
+	os << "long-double: =" << getOutputLongDouble(rec, NULL, ldf, res) << "^" << endl;
 	__require__(res);
 	return os.str();
 }
