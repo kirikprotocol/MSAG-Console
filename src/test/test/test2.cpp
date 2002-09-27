ed71177c991cdef17970e97281fc23e22eb951b4
bryz <>
2002-09-27 10:59:33 +0000

    no message

--- src/test/test/test2.cpp
+++ src/test/test/test2.cpp
@@ -5,8 +5,10 @@ using namespace smsc::test::sms;
 
 int main(int argc, char* argv[])
 {
-	SMS sms;
-	SmsUtil::setupRandomCorrectSms(&sms);
-	sms.messageBody.getBuffer();
+	SMS sms1, sms2;
+	SmsUtil::setupRandomCorrectSms(&sms1);
+	uint8_t* buf = sms1.getMessageBody().getBuffer();
+	int len = sms1.getMessageBody().getBufferLength();
+	sms2.getMessageBody().setBuffer(buf, len);
 	return 0;
 }
