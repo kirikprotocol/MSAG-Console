4b8d730210e5f239ea449ab0ee1d265408c9c50a
chen <>
2002-10-02 03:46:43 +0000

    *** empty log message ***

--- src/system/mapio/MapDialog.cpp
+++ src/system/mapio/MapDialog.cpp
@@ -6,6 +6,57 @@ using namespace std;
 using namespace smsc::sms;
 using namespace smsc::smeman;
   
+#define TP_VP_NONE  0
+#define TP_VP_REL   1
+#define TP_VP_ENCH  2
+#define TP_VP_ABS   3
+
+#pragma pack(1)
+
+struct SMS_SUMBMIT_FORMAT_HEADER{
+  unsigned mg_type_ind:2;
+  unsigned reject_dupl:1;
+  unsigned tp_vp:2;
+  unsigned reply_path:1;
+  unsigned udhi:1;
+  unsigned srr:1;
+  unsigned mr:8;
+};
+
+struct MAP_SMS_ADDRESS{
+  unsigned char len;
+  //unsigned char tonpi;
+  union{
+    struct{
+      unsigned npi:4;
+      unsigned ton:3;
+      unsigned reserved_1:1;
+    }st;
+    unsigned char tonpi;
+  };
+  unsigned char val[10];
+};
+
+void ConvAddrMap2Smc(const MAP_SMS_ADDRESS* ma,Address* sa){
+  sa->setTypeOfNumder(ma->st.ton);
+  sa->setNumberingPlan(ma->st.npi);
+  if ( ma->len != 0 ){
+    char sa_val[21] = {0,};
+    for ( int i=0; i<ma->len;){
+      sa_val[i]=ma->val[i>>1]&0x0f+0x30;
+      ++i;
+      if ( i < ma->len ){
+        sa_val[i] = (ma->val[i>>1]>>4)+0x30;
+        ++i;
+      }else break;
+    }
+    sa->setValue(ma->len,sa_val);
+  }else{
+    char c = 0;
+    sa->setValue(0,&c);
+  }
+}
+
 USHORT_T  MapDialog::Et96MapV2ForwardSmMOInd( 
     ET96MAP_LOCAL_SSN_T lssn, 
     ET96MAP_DIALOGUE_ID_T dialogId,
@@ -17,6 +68,39 @@ USHORT_T  MapDialog::Et96MapV2ForwardSmMOInd(
   __trace2__("MAP::MapDialog::Et96MapV2ForwardSmMOInd");
   setInvokeId(invokeId);
   SMS sms;
+  Address src_addr;
+  Address dest_addr;
+  SMS_SUMBMIT_FORMAT_HEADER* ssfh = (SMS_SUMBMIT_FORMAT_HEADER*)ud->signalInfo;
+  __trace2__("MAP::DIALOG::ForwardReaq: MR(8) = 0x%x",ssfh->mr);
+  __trace2__("MAP::DIALOG::ForwardReaq: MSG_TYPE_IND(2) = 0x%x",ssfh->mg_type_ind);
+  __trace2__("MAP::DIALOG::ForwardReaq: MSG_VPF(2) = 0x%x",ssfh->tp_vp);
+  __trace2__("MAP::DIALOG::ForwardReaq: MSG_UDHI(1) = %d",ssfh->udhi);
+  __trace2__("MAP::DIALOG::ForwardReaq: MSG_REJECT_DUPL(1) = %d",ssfh->reject_dupl);
+  __trace2__("MAP::DIALOG::ForwardReaq: MSG_REPLY_PATH(1) = %d",ssfh->reply_path);
+  __trace2__("MAP::DIALOG::ForwardReaq: MSG_SRR(1) = %d",ssfh->srr);
+  MAP_SMS_ADDRESS* msa = (MAP_SMS_ADDRESS*)(ud->signalInfo+2);
+  __trace2__("MAP::DIALOG::ForwardReaq: MSA tonpi.va(8):0x%x, ton(3):0x%x, npi(4):0x%x, len(8):%d",
+             msa->tonpi,msa->st.ton,msa->st.npi,msa->len);
+  //__trace2__("MAP::DIALOG::ForwardReaq: user_data_len = user_data_len");
+  unsigned msa_len = mas->length;
+  if ( msa_len%2 ) mas_len+1;
+  msa_len = mas_len/2+2;
+  unsigned char* user_data_len = (unsigned char*)(ud->signalInfo+2+ssfh->tp_vp?7:1+msa->len+2);
+  unsigned char* user_data = user_data_len+1;
+  unsigned char protocol_id = *(unsigned char*)(ud->signalInfo+2+msa->len);
+  unsigned char user_data_coding = *(unsigned char*)(ud->signalInfo+2+msa->len+1);
+  __trace2__("MAP::DIALOG::ForwardReaq: protocol_id = 0x%x",protocol_id);
+  __trace2__("MAP::DIALOG::ForwardReaq: user_data_len = %d",user_data_len);
+  __trace2__("MAP::DIALOG::ForwardReaq: user_data_encoding = 0x%x",data_coding);
+  sms.setBinProperty(Tag::SMPP_SM_MESSAGE,user_data,user_data_len);
+  sms.setIntProperty(Tag::SMPP_SM_LENGTH,user_dta_len);
+  sms.setIntProperty(Tag::SMPP_DATA_CODING,user_data_coding);
+  sms.setIntProperty(Tag::SMPP_PROTOCOL_ID,protocol_id);
+  sms.setMessageReference(ssfh->mr);
+  ConvAddrMap2Smc((MAP_SMS_ADDRESS*)srcAddr->addr,src_addr);
+  sms.setOriginatingAddress(src_addr);
+  ConvAddrMap2Smc(msa,dest_addr);
+  sms.setDestinationAddress(dest_addr);
   MapProxy* proxy = MapDialogContainer::getInstance()->getProxy();
   SmscCommand cmd = SmscCommand::makeSumbmitSm(sms,((uint32_t)dialogId)&0xffff);
   proxy->putIncomingCommand(cmd);
