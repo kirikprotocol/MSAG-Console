0dd1b77093e18a5f87f66af35379b49947bf2436
bryz <>
2002-10-10 11:33:12 +0000

    no message

---
+++ src/test/test/decoder/decoder.cpp
@@ -0,0 +1,32 @@
+#include "test/util/TextUtil.hpp"
+#include "util/debug.h"
+#include <iostream>
+
+using namespace std;
+using namespace smsc::test::util;
+
+int main(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		cout << "Usage: test <dataCoding> <bytes>" << endl;
+		return 0;
+	}
+	uint8_t dataCoding = atoi(argv[1]);
+	const char* input = argv[2];
+	cout << "dataCoding = " << dataCoding << ", input = " << input << endl;
+	string tmp;
+	istringstream is(input);
+	is >> hex;
+	while (!is.eof())
+	{
+		int ch;
+		is >> ch;
+		tmp += (unsigned char) ch;
+		//__trace2__("ch = %d", ch);
+	}
+	//__trace2__("tmp len = %d", tmp.length());
+	const string str = decode(tmp.c_str(), tmp.length(), dataCoding);
+	cout << "output = \"" << str << "\"" << endl;
+	return 0;
+}
