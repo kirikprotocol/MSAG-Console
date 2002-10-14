cfb261729e41ae22879d927d2e7399f7384e3aa0
chen <>
2002-10-14 14:50:49 +0000

    *** empty log message ***

--- src/system/mapio/MapDialog.cpp
+++ src/system/mapio/MapDialog.cpp
@@ -515,6 +515,7 @@ ET96MAP_SM_RP_UI_T* mkDeliverPDU(SMS* sms,ET96MAP_SM_RP_UI_T* pdu)
         if ( encoding == MAP_8BIT_ENCODING ) value |= 0x4;
       }
     }
+    __trace2__("MAP::mkDeliveryPDU: user data coding = 0x%x",value);
     *pdu_ptr++ = value;
   }
   {
