5728f7c0d454e79119cdf6f61065be9ade438ec5
skv <>
2002-10-18 08:01:40 +0000

    first version

---
+++ src/util/polltrace/polltrace.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+//#include "debug.h"
+#include <string>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <poll.h>
+#include "core/synchronization/Mutex.hpp"
+#include "core/buffers/XHash.hpp"
+#include "core/threads/Thread.hpp"
+
+using namespace smsc::util;
+using namespace smsc::core::synchronization;
+using namespace smsc::core::buffers;
+using namespace smsc::core::threads;
+
+struct PollEnterInfo{
+  time_t t;
+  void *ra;
+};
+
+XHash<int,PollEnterInfo> stats;
+Mutex mtx;
+FILE *f;
+int cnt=0;
+int stopping=0;
+
+class DumpThread:public Thread{
+public:
+  int Execute()
+  {
+    while(!stopping)
+    {
+      sleep(1);
+      mtx.Lock();
+      fprintf(f,"Dump started at %d\n",time(NULL));
+      stats.First();
+      int key;
+      PollEnterInfo *pi;
+      while(stats.Next(key,pi))
+      {
+        fprintf(f,"RA=%p, StartTime=%d\n",pi->ra,pi->t);
+      }
+      fprintf(f,"Dump finished at %d\n\n",time(NULL));
+      fflush(f);
+      mtx.Unlock();
+    }
+    return 0;
+  }
+}dumpthr;
+
+
+typedef int (*pollFn)(struct pollfd fds[], nfds_t nfds, int timeout);
+
+pollFn oldpoll;
+
+extern "C"
+int _poll(struct pollfd fds[], nfds_t nfds, int timeout)
+{
+  mtx.Lock();
+  int id=cnt++;
+  PollEnterInfo pi;
+  pi.t=time(NULL);
+  pi.ra=__builtin_return_address(0);
+  stats[id]=pi;
+  mtx.Unlock();
+  int rv=oldpoll(fds,nfds,timeout);
+  mtx.Lock();
+  stats.Delete(id);
+  mtx.Unlock();
+  return rv;
+}
+
+struct Dummy{
+Dummy()
+{
+  void* dlhandle = dlopen("/usr/lib/libc.so", RTLD_LAZY);
+  if(!dlhandle)
+  {
+    fprintf(stderr,"dlopen failed\n");
+    return;
+  }
+  oldpoll = (pollFn)dlsym(dlhandle, "_poll");
+  if(!oldpoll)
+  {
+    fprintf(stderr,"dlsym failed\n");
+    return;
+  }
+  f=fopen("polltrace.log","wt");
+  dumpthr.Start();
+}
+~Dummy()
+{
+  fclose(f);
+  stopping=1;
+  dumpthr.WaitFor();
+}
+}dummy;
