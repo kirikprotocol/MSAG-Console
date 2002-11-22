6f5b0d2a46b68ce4c1cf6b25d66ac4ecc74be7bf
skv <>
2002-11-22 09:33:46 +0000

    pq

---
+++ src/core/buffers/PriorityQueue.hpp
@@ -0,0 +1,100 @@
+#ifndef __SMSC_CORE_BUFFERS_PRIORITY_QUEUE_HPP__
+#define __SMSC_CORE_BUFFERS_PRIORITY_QUEUE_HPP__
+
+#include "core/buffers/Array.hpp"
+
+namespace smsc{
+namespace core{
+namespace buffers{
+
+template <class T,class C,int min,int max>
+class PriorityQueue{
+public:
+  PriorityQueue()
+  {
+    counts.Init(max-min+1);
+    queue.Init(max-min+1);
+    for(int i=0;i<=max-min;i++)
+    {
+      counts[i]=0;
+    }
+    count=0;
+    minuse=0;
+    maxuse=0;
+    processed=0;
+  }
+  ~PriorityQueue()
+  {
+  }
+
+  PriorityQueue& Push(const T& item,int priority)
+  {
+    if(priority<min)priority=min;
+    if(priority>max)priority=max;
+    int idx=priority-min;
+    if(count)
+    {
+      if(idx<minuse)minuse=idx;
+      if(idx>maxuse)maxuse=idx;
+    }else
+    {
+      minuse=maxuse=idx;
+    }
+    queue[idx].Push(item);
+    count++;
+    return *this;
+  }
+
+  bool Pop(T& item)
+  {
+    if(count==0)return false;
+    int best=minuse,i;
+    double bestval=(double)counts[minuse]/((minuse+min)>0?(minuse+min):1);
+
+    for(i=minuse+1;i<=maxuse;i++)
+    {
+      if(queue[i].Count()==0)
+      {
+        continue;
+      }
+      double val=(double)counts[i]/((i+min)>0?(i+min):1);
+      if(val<bestval)
+      {
+        best=i;
+        bestval=val;
+      }
+    }
+    counts[best]++;
+    //if(counts[best]>=best)counts[best]=0;
+    queue[best].Pop(item);
+    count--;
+    if(count>0)
+    {
+      while(queue[minuse].Count()==0)minuse++;
+      while(queue[maxuse].Count()==0)maxuse--;
+    }
+    processed++;
+    if(processed>=(max-min)*100)
+    {
+      for(i=0;i<=max-min;i++)
+      {
+        counts[i]=0;
+      }
+      processed=0;
+    }
+    return true;
+  }
+  int Count(){return count;}
+protected:
+  Array<C> queue;
+  Array<int> counts;
+  int minuse,maxuse;
+  int count;
+  int processed;
+};
+
+};//buffers
+};//core
+};//smsc
+
+#endif
