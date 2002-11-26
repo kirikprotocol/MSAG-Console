ebb5c9f01c5e35573d9e4f9b462d20a83ffc6330
bryz <>
2002-11-26 11:51:27 +0000

    ���erviceId, ���routeId

--- src/test/core/RouteUtil.cpp
+++ src/test/core/RouteUtil.cpp
@@ -7,6 +7,8 @@ namespace smsc {
 namespace test {
 namespace core {
 
+using smsc::core::synchronization::Mutex;
+using smsc::core::synchronization::MutexGuard;
 using smsc::test::sms::SmsUtil;
 using smsc::test::sms::operator<<;
 using smsc::test::sms::operator!=;
@@ -16,68 +18,52 @@ using namespace smsc::test::util;
 
 void RouteUtil::setupRandomCorrectRouteInfo(RouteInfo* route)
 {
+	static Mutex mutex;
+	static int routeId = 1;
+	MutexGuard mguard(mutex);
 	route->priority = rand2(RoutePriorityMin, RoutePriorityMax);
 	route->billing = rand0(10);
-	route->paid = rand0(10);
 	route->archived = rand0(3);
 	//route->enabling = rand0(2);
-	route->routeId = (RouteId) rand1(INT_MAX);
+	route->serviceId = rand0(INT_MAX);
+	ostringstream s;
+	s << "route_" << routeId++;
+	route->routeId = s.str();
 	//route->smeSystemId = ...
 	//route->source = ...
 	//route->dest = ...
 }
 
+#define __check__(errCode, field) \
+	if (route1.field != route2.field) { res.push_back(errCode); }
+	
 vector<int> RouteUtil::compareRoutes(const RouteInfo& route1,
 	const RouteInfo& route2)
 {
 	vector<int> res;
-	if (route1.priority != route2.priority)
-	{
-		res.push_back(1);
-	}
-	if (route1.billing != route2.billing)
-	{
-		res.push_back(2);
-	}
-	if (route1.paid != route2.paid)
-	{
-		res.push_back(3);
-	}
-	if (route1.archived != route2.archived)
-	{
-		res.push_back(4);
-	}
-	if (route1.routeId != route2.routeId)
-	{
-		res.push_back(5);
-	}
-	if (route1.smeSystemId != route2.smeSystemId)
-	{
-		res.push_back(6);
-	}
-	if (route1.source != route2.source)
-	{
-		res.push_back(7);
-	}
-	if (route1.dest != route2.dest)
-	{
-		res.push_back(8);
-	}
+	__check__(1, priority);
+	__check__(2, billing);
+	__check__(3, archived);
+	__check__(4, enabling);
+	__check__(5, serviceId);
+	__check__(6, routeId);
+	__check__(7, smeSystemId);
+	__check__(8, source);
+	__check__(9, dest);
 	return res;
 }
 
 ostream& operator<< (ostream& os, const RouteInfo& route)
 {
 	os << "routeId = " << route.routeId;
-	os << ", smeSystemId = " << route.smeSystemId << "(" <<
-		route.smeSystemId.length() << ")";
+	os << ", smeSystemId = " << route.smeSystemId;
 	os << ", source = " << route.source;
 	os << ", dest = " << route.dest;
 	os << ", enabling = " << route.enabling;
 	os << ", priority = " << route.priority;
 	os << ", billing = " << (route.billing ? "true" : "false");
-	os << ", paid = " << (route.paid ? "true" : "false");
 	os << ", archived = " << (route.archived ? "true" : "false");
+	os << ", serviceId = " << route.serviceId;
 	return os;
 }
 
