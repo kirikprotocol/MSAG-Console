87dfa28ae409a84cbc562238e274eceaaf68f999
skv <>
2002-12-30 08:44:40 +0000

    �Y�VS��ݽ��C��҃��d����� IN�Kҿt�BśG��(�}u������qj������FdC����,'�)w�>��Y�����ֻ����>vuK泲7����N�^�,�����������@

--- config/scripts/start_smsc.sh
+++ config/scripts/start_smsc.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
+echo $$ >pid
+
 while true;
 do
-./smsc 2>err.log
+./smsc 2>err.log &
+PID=$!
+export PID
+trap "kill -2 $PID" SIGINT
+wait $PID
 if [ -f core ]; then
   DATE=`perl -e '($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);printf("%04d%02d%02d%02d%02d%02d",$year+1900,$mon,$mday,$hour,$min,$sec)'`
   mkdir "$DATE"
   mv core "$DATE"
   mv err.log "$DATE"
   mv smsc.log "$DATE"
-fi
-if [ -f stop_smsc_flag ]; then
-  rm stop_smsc_flag
-  break
+  mv smsc "$DATA"
 fi
 done
--- config/scripts/stop_smsc.sh
+++ config/scripts/stop_smsc.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-touch stop_smsc_flag
-kill `ps -efa|grep $USER|grep ./smsc|awk -e '{print \$2}'`
+kill -2 `cat pid`
