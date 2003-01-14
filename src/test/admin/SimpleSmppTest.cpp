540881f633e3205f25234248887e3c9ba9f05ef2
bryz <>
2003-01-14 15:47:58 +0000

    �����

--- src/test/admin/SimpleSmppTest.cpp
+++ src/test/admin/SimpleSmppTest.cpp
@@ -1,5 +1,6 @@
 #include "sme/SmppBase.hpp"
 #include "test/smpp/SmppUtil.hpp"
+#include "test/util/TextUtil.hpp"
 #include "readline/readline.h"
 #include "readline/history.h"
 #include <map>
@@ -9,16 +10,21 @@ using smsc::sms::Address;
 using namespace std;
 using namespace smsc::sme;
 using namespace smsc::smpp::SmppCommandSet;
+using namespace smsc::smpp::DataCoding;
 using namespace smsc::test::smpp;
+using namespace smsc::test::util;
 
 class SimpleSmppListener : public SmppPduEventListener
 {
 	const string systemId;
 	SmppSession* session;
+	uint32_t respCommandStatus;
 
 public:
-	SimpleSmppListener(const string& _systemId) : systemId(_systemId) {}
+	SimpleSmppListener(const string& _systemId)
+		: systemId(_systemId), respCommandStatus(0x0) {}
 	void setSession(SmppSession* _session) { session = _session; }
+	void setDeliveryResp(uint32_t commandStatus) { respCommandStatus = commandStatus; }
 	virtual void handleEvent(SmppHeader* pdu);
 	virtual void handleError(int errorCode);
 };
@@ -44,7 +50,9 @@ public:
 	void select(const string& systemId);
 	void list();
 	void unbind();
-	void submit(const string& src, const string& dest);
+	void submit(const string& src, const string& dest,
+		uint8_t registeredDelivery, bool ussd);
+	void setDeliveryResp(uint32_t commandStatus);
 };
 
 #define __check_exception__ \
@@ -67,7 +75,12 @@ void SimpleSmppTest::bind(const string& systemId, const string& passwd)
 		data->listener->setSession(data->session);
 		data->session->connect();
 		sessions[systemId] = data;
+		cout << "connected successfully" << endl;
 		select(systemId);
+	}
+	catch (SmppConnectException& e)
+	{
+		cout << "connect exception: " << e.what() << endl;
 	}
 	__check_exception__
 }
@@ -114,71 +127,115 @@ void SimpleSmppTest::unbind()
 	__check_exception__
 }
 
-void SimpleSmppTest::submit(const string& src, const string& dest)
+void SimpleSmppTest::submit(const string& src, const string& dest,
+	uint8_t registeredDelivery, bool ussd)
 {
 	static uint16_t msgRef = 1;
 	__check_selected__;
 	try
 	{
 		PduSubmitSm pdu;
-		SmppUtil::setupRandomCorrectSubmitSmPdu(&pdu, true);
+		SmppUtil::setupRandomCorrectSubmitSmPdu(&pdu, true, OPT_ALL & ~OPT_USSD_SERVICE_OP);
 		PduAddress addr;
 		const Address srcAddr(src.c_str());
 		const Address destAddr(dest.c_str());
 		pdu.get_message().set_source(*SmppUtil::convert(srcAddr, &addr));
 		pdu.get_message().set_dest(*SmppUtil::convert(destAddr, &addr));
+		pdu.get_message().set_registredDelivery(registeredDelivery);
 		//неменденная доставка
 		pdu.get_message().set_scheduleDeliveryTime("");
 		pdu.get_message().set_validityPeriod("");
 		pdu.get_optional().set_userMessageReference(++msgRef);
+		if (ussd)
+		{
+			pdu.get_optional().set_ussdServiceOp(1);
+		}
 		PduSubmitSmResp* respPdu = data->session->getSyncTransmitter()->submit(pdu);
 		dumpPdu("submit", selectedId, reinterpret_cast<SmppHeader*>(&pdu));
-		char res[128];
-		sprintf(res, "submit: sme = %s, seqNum = %u, commandStatus = 0x%x, msgRef = %u",
-			selectedId.c_str(), pdu.get_header().get_sequenceNumber(),
-			respPdu->get_header().get_commandStatus(),
-			(uint32_t) pdu.get_optional().get_userMessageReference());
-		cout << res << endl;
+		uint8_t dc = pdu.get_message().get_dataCoding();
+		cout << "submit: sme = " << selectedId <<
+			", seqNum = " << dec << pdu.get_header().get_sequenceNumber() <<
+			", commandStatus = 0x" << hex << respPdu->get_header().get_commandStatus() <<
+			", msgRef = " << dec << (int) pdu.get_optional().get_userMessageReference() <<
+			", ussd = " << (ussd ? "yes" : "no") <<
+			", dc = " << (int) dc;
+		if (dc == DEFAULT || dc == UCS2 || dc == SMSC7BIT)
+		{
+			bool udhi = pdu.get_message().get_esmClass() & ESM_CLASS_UDHI_INDICATOR;
+			int udlLen = !udhi ? 0 :
+				1 + (unsigned char) *pdu.get_message().get_shortMessage();
+			const char* text = pdu.get_message().get_shortMessage() + udlLen;
+			int len = pdu.get_message().size_shortMessage() - udlLen;
+			cout << ", udhi = " << (udhi ? "yes" : "no") <<
+				", sm = " << decode(text, len, dc, false);
+		}
+		cout << endl;
 	}
 	__check_exception__
+}
+
+void SimpleSmppTest::setDeliveryResp(uint32_t commandStatus)
+{
+	__check_selected__;
+	data->listener->setDeliveryResp(commandStatus);
 }
 
 void SimpleSmppListener::handleEvent(SmppHeader* pdu)
 {
 	__require__(pdu);
 	dumpPdu("handleEvent", systemId, pdu);
-	char res[128];
 	switch (pdu->get_commandId())
 	{
 		case GENERIC_NACK:
-			sprintf(res, "handle generick_nack: sme = %s, seqNum = %u, commandStatus = 0x%x",
-				systemId.c_str(), pdu->get_sequenceNumber(), pdu->get_commandStatus());
+			cout << "handle generick_nack: sme = " << systemId <<
+				", seqNum = " << dec << pdu->get_sequenceNumber() <<
+				", commandStatus = 0x" << hex << pdu->get_commandStatus() << endl;
 			break;
 		case SUBMIT_SM_RESP:
-			sprintf(res, "handle submit_sm_resp: sme = %s, seqNum = %u, commandStatus = 0x%x",
-				systemId.c_str(), pdu->get_sequenceNumber(), pdu->get_commandStatus());
+			cout << "handle submit_sm_resp: sme = " << systemId <<
+				", seqNum = " << dec << pdu->get_sequenceNumber() <<
+				", commandStatus = 0x" << hex << pdu->get_commandStatus() << endl;
 			break;
 		case DELIVERY_SM:
 			{
 				PduDeliverySm* p = reinterpret_cast<PduDeliverySm*>(pdu);
 				__require__(p->get_optional().has_userMessageReference());
-				sprintf(res, "handle delivery_sm: sme = %s, msgRef = %u",
-					systemId.c_str(), (uint32_t) p->get_optional().get_userMessageReference());
+				uint8_t dc = p->get_message().get_dataCoding();
+				uint16_t msgRef = p->get_optional().get_userMessageReference();
+				cout << "handle deliver_sm: sme = " << systemId <<
+					", msgRef = " << dec << (int) msgRef <<
+					", dc = " << (int) dc;
+				if (dc == DEFAULT || dc == UCS2 || dc == SMSC7BIT)
+				{
+					bool udhi = p->get_message().get_esmClass() & ESM_CLASS_UDHI_INDICATOR;
+					int udlLen = !udhi ? 0 :
+						1 + (unsigned char) *p->get_message().get_shortMessage();
+					const char* text = p->get_message().get_shortMessage() + udlLen;
+					int len = p->get_message().size_shortMessage() - udlLen;
+					cout << ", udhi = " << (udhi ? "yes" : "no") <<
+						", sm = " << decode(text, len, dc, false);
+				}
+				cout << endl;
+				PduDeliverySmResp respPdu;
+				respPdu.get_header().set_commandStatus(respCommandStatus);
+				respPdu.get_header().set_sequenceNumber(pdu->get_sequenceNumber());
+				session->getAsyncTransmitter()->sendDeliverySmResp(respPdu);
+				dumpPdu("send resp", systemId, reinterpret_cast<SmppHeader*>(&respPdu));
+				cout << "deliver resp: sme = " << systemId <<
+					", seqNum = " << dec << respPdu.get_header().get_sequenceNumber() <<
+					", commandStatus = 0x" << hex << respPdu.get_header().get_commandStatus() << endl;
 			}
 			break;
 		default:
-			sprintf(res, "handle pdu: sme = %s, commandId = 0x%x",
-				systemId.c_str(), pdu->get_commandId());
+			cout << "handle pdu: sme = " << systemId <<
+				", commandId = 0x" << hex << pdu->get_commandId() << endl;
 	}
-	cout << res << endl;
 }
 
 void SimpleSmppListener::handleError(int errorCode)
 {
-	char res[128];
-	sprintf(res, "handle error: sme = %s, errorCode = %d",
-		systemId.c_str(), errorCode);
-	cout << res << endl;
+	cout << "handle error: sme = " << systemId <<
+		", errorCode = " << dec << errorCode << endl;
 }
 
 void executeTest(const string& smscHost, int smscPort)
@@ -186,7 +243,6 @@ void executeTest(const string& smscHost, int smscPort)
 	SimpleSmppTest test(smscHost, smscPort);
 	//обработка команд консоли
 	bool help = true;
-	string cmdLine, cmd;
 	while(true)
 	{
 		//хелп
@@ -197,12 +253,19 @@ void executeTest(const string& smscHost, int smscPort)
 			cout << "select <systemId> - select sme" << endl;
 			cout << "list - list sessions" << endl;
 			cout << "unbind - unbind selected sme" << endl;
-			cout << "submit <src> <dest> - submit sms" << endl;
+			cout << "submit <src> <dest> [report=none|full|failure] [ussd=no|yes] - submit sms" << endl;
+			cout << "set resp [commandStatus=0] - set delivery_sm_resp command status" << endl;
 			cout << "quit - quit test" << endl;
 			continue;
 		}
-		cmdLine = readline(">");
+		const char* cmdLine = readline(">");
+		if (!cmdLine)
+		{
+			return;
+		}
+		add_history(cmdLine);
 		istringstream is(cmdLine);
+		string cmd;
 		is >> cmd;
 		if (cmd == "bind")
 		{
@@ -238,11 +301,58 @@ void executeTest(const string& smscHost, int smscPort)
 		}
 		else if (cmd == "submit")
 		{
-			string src, dest;
+			string src, dest, report, ussd;
 			is >> src;
 			is >> dest;
-			test.submit(src, dest);
+			is >> report;
+			is >> ussd;
+			uint8_t registeredDelivery;
+			bool ussdFlag;
+			//report
+			if (report == "" || report == "none")
+			{
+				registeredDelivery = NO_SMSC_DELIVERY_RECEIPT;
+			}
+			else if (report == "full")
+			{
+				registeredDelivery = FINAL_SMSC_DELIVERY_RECEIPT;
+			}
+			else if (report == "failure")
+			{
+				registeredDelivery = FAILURE_SMSC_DELIVERY_RECEIPT;
+			}
+			else
+			{
+				cout << "invalid report option" << endl;
+				continue;
+			}
+			//ussd
+			if (ussd == "" || ussd == "no")
+			{
+				ussdFlag = false;
+			}
+			else if (ussd == "yes")
+			{
+				ussdFlag = true;
+			}
+			else
+			{
+				cout << "invalid ussd flag" << endl;
+				continue;
+			}
+			test.submit(src, dest, registeredDelivery, ussdFlag);
 			continue;
+		}
+		else if (cmd == "set")
+		{
+			is >> cmd;
+			if (cmd == "resp")
+			{
+				uint32_t commandStatus = 0x0;
+				is >> commandStatus;
+				test.setDeliveryResp(commandStatus);
+				continue;
+			}
 		}
 		else if (cmd == "quit")
 		{
