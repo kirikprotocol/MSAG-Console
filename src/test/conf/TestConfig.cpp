20cac88b7c58e34818f23bfa0cbc7b75e89387a1
bryz <>
2003-01-27 15:05:40 +0000

    ��

--- src/test/conf/TestConfig.cpp
+++ src/test/conf/TestConfig.cpp
@@ -17,7 +17,8 @@ TestConfig::TestConfig()
 	intMap["timeCheckAccuracy"] = 5; //5 сек
 	intMap["missingPduCheckInterval"] = 20; //20 сек
 	intMap["scCmdTimeout"] = 8; //макс время жизни команды в SC
-	intMap["smeInactivityTime"] = 10; //период неактивности sme
+	intMap["smeInactivityTime"] = 10; //период неактивности sme до начала enquire_link
+	intMap["smeInactivityTimeOut"] = 15; //закрывается соединение после начала enquire_link
 	intMap["proxyTimeout"] = 2; //таймаут для помирания прокси
 	intMap["maxValidPeriod"] = 150; //150 сек
 	intMap["maxWaitTime"] = 60; //max(waitTime)
