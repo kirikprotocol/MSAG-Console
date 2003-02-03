af749a30cbe1ea55d6d77c5f87ace413a0decd54
bryz <>
2003-02-03 15:23:31 +0000

    ����� ����

--- src/test/recoder/decoder.cpp
+++ src/test/recoder/decoder.cpp
@@ -8,9 +8,9 @@ using namespace smsc::test::util;
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	if (argc < 3)
 	{
-		cout << "Usage: test <dataCoding> <bytes>" << endl;
+		cout << "Usage: test <dataCoding> <bytes> [-t]" << endl;
 		return 0;
 	}
 	uint8_t dataCoding = atoi(argv[1]);
@@ -31,5 +31,13 @@ int main(int argc, char* argv[])
 	//__trace2__("tmp len = %d", tmp.length());
 	const string str = decode(tmp.c_str(), tmp.length(), dataCoding, false);
 	cout << "output = \"" << str << "\"" << endl;
+	if (argc == 4 && dataCoding == 8)
+	{
+		char buf[(int) (1.5 * str.length())];
+		int len = Transliterate(str.c_str(), str.length(),
+			CONV_ENCODING_CP1251, buf, sizeof(buf));
+		buf[len] = 0;
+		cout << "translit = \"" << buf << "\"" << endl;
+	}
 	return 0;
 }
