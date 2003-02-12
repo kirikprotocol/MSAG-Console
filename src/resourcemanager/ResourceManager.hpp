5d34c2b9b3d7241dd6b59c955f61a22483e920e6
skv <>
2003-02-12 10:17:30 +0000

    required methods

---
+++ src/resourcemanager/ResourceManager.hpp
@@ -0,0 +1,24 @@
+#ifndef __RESOURCE_MANAGER_HPP__
+#define __RESOURCE_MANAGER_HPP__
+
+#include <string>
+
+namespace smsc{
+namespace resourcemanager{
+
+class ResourceManager{
+public:
+  // возвращает строку из сетингов для определленой локали и ключа.
+  string getSetting(const string& locale,const string& key);
+  // возвращает строку из сетингов для дефолтной локали и ключа.
+  string getSetting(const string& key );
+  // возвращает строку из ресурса для определленой локали и ключа.
+  string getString(const string& locale, const string& key);
+  // возвращает строку из ресурса для дефолтной локали и ключа.
+  string getString(const string& key);
+};
+
+};//resourcemanager
+};//smsc
+
+#endif
