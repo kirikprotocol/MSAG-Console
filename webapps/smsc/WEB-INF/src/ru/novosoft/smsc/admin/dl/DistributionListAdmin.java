f8c505d79530858bb0164b10b61e4dd42ba097ae
makarov <>
2003-02-26 07:09:31 +0000

    initial version 2

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/dl/DistributionListAdmin.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/dl/DistributionListAdmin.java
@@ -8,24 +8,37 @@
 package ru.novosoft.smsc.admin.dl;
 
 import ru.novosoft.smsc.admin.AdminException;
+import ru.novosoft.smsc.admin.dl.exceptions.*;
 
 import java.util.List;
 
 public interface DistributionListAdmin
 {
-    public void addPrincipal(Principal prc) throws AdminException;
+    public void addPrincipal(Principal prc)
+        throws AdminException, PrincipalAlreadyExistsException;
 
-    public void addMember(String dlname, String address) throws AdminException;
-    public void deleteMember(String dlname, String address) throws AdminException;
-    public void removeMembers(String dlname) throws AdminException;
-    public List members(String dlname, String submitter) throws AdminException;
+    public void addMember(String dlname, String address)
+        throws AdminException, MemberAlreadyExistsException;
+    public void deleteMember(String dlname, String address)
+        throws AdminException, MemberNotExistsException;
+    public void removeMembers(String dlname)
+        throws AdminException, ListNotExistsException;
+    public List members(String dlname, String submitter)
+        throws AdminException, ListNotExistsException, IllegalSubmitterException;
 
-    public void grantPosting(String dlname, String address) throws AdminException;
-    public void revokePosting(String dlname, String address) throws AdminException;
+    public void grantPosting(String dlname, String address)
+        throws AdminException, ListNotExistsException;
+    public void revokePosting(String dlname, String address)
+        throws AdminException, ListNotExistsException;
 
-    public void addDistributionList(DistributionList dl) throws AdminException;
-    public void deleteDistributionList(String dlname) throws AdminException;
+    public void addDistributionList(DistributionList dl)
+        throws AdminException, ListAlreadyExistsException;
+    public void deleteDistributionList(String dlname)
+        throws AdminException, ListNotExistsException;
 
-    public boolean checkPermission(String dlname, String address) throws AdminException;
-    public List list() throws AdminException;
+    public boolean checkPermission(String dlname, String address)
+        throws AdminException, ListNotExistsException;
+
+    public List list()
+        throws AdminException;
 }
--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/dl/DistributionListManager.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/dl/DistributionListManager.java
@@ -9,10 +9,14 @@ package ru.novosoft.smsc.admin.dl;
 
 import ru.novosoft.smsc.admin.smsc_service.Smsc;
 import ru.novosoft.smsc.admin.AdminException;
+import ru.novosoft.smsc.admin.dl.exceptions.*;
 
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DistributionListManager implements DistributionListAdmin
 {
@@ -27,31 +31,70 @@ public class DistributionListManager implements DistributionListAdmin
         "SELECT NVL(COUNT(*), 0) FROM DL_PRINCIPALS WHERE ADDRESS=?";
     private final static String ADD_PRINCIPAL_SQL =
         "INSERT INTO DL_PRINCIPALS (ADDRESS, MAX_LST, MAX_EL) VALUES (?, ?, ?)";
-    public void addPrincipal(Principal prc) throws AdminException {
+    public void addPrincipal(Principal prc)
+        throws AdminException, PrincipalAlreadyExistsException
+    {
+        Connection connection = null;
+        try
+        {
+          connection = ds.getConnection();
+          PreparedStatement stmt = connection.prepareStatement(CHECK_PRINCIPAL_SQL);
+          stmt.setString(1, prc.getAddress());
+          ResultSet rs = stmt.executeQuery();
+          if (!rs.next() || rs.getInt(1) != 0)
+            throw new PrincipalAlreadyExistsException(prc);
 
+          stmt = connection.prepareStatement(ADD_PRINCIPAL_SQL);
+          stmt.setString(1, prc.getAddress());
+          stmt.setInt(2, prc.getMaxLists());
+          stmt.setInt(3, prc.getMaxElements());
+          stmt.executeUpdate();
+          connection.commit();
+          connection.close();
+        }
+        catch (Exception exc)
+        {
+          exc.printStackTrace();
+          try {
+              if (connection != null) {
+                connection.rollback();
+                connection.close();
+              }
+          } catch (Exception cexc) { cexc.printStackTrace(); }
+          throw new AdminException(exc.getMessage());
+        }
     }
 
     private final static String CHECK_MEMBER_SQL =
-        "SELECT NVL(COUNT(*), 0) FROM DL_MEMBERS WHERE ADDRESS= ? AND LIST=?";
+        "SELECT NVL(COUNT(*), 0) FROM DL_MEMBERS WHERE LIST=? AND ADDRESS=?";
     private final static String ADD_MEMBER_SQL =
-        "INSERT INTO FROM DL_MEMBERS (ADDRESS, LIST) VALUES (?, ?)";
-    public void addMember(String dlname, String address) throws AdminException {
+        "INSERT INTO DL_MEMBERS (LIST, ADDRESS) VALUES (?, ?)";
+    public void addMember(String dlname, String address)
+        throws AdminException, MemberAlreadyExistsException
+    {
     }
 
     private final static String DELETE_MEMBER_SQL =
-        "DELETE FROM DL_MEMBERS WHERE ADDRESS= ? AND LIST=?";
-    public void deleteMember(String dlname, String address) throws AdminException {
+        "DELETE FROM DL_MEMBERS WHERE LIST=? AND ADDRESS= ?";
+    public void deleteMember(String dlname, String address)
+        throws AdminException, MemberNotExistsException
+    {
     }
 
     private final static String REMOVE_MEMBERS_SQL =
-        "DELETE FROM DL_MEMBERS WHERE AND LIST=?";
-    public void removeMembers(String dlname) throws AdminException {
+        "DELETE FROM DL_MEMBERS WHERE LIST=?";
+    public void removeMembers(String dlname)
+        throws AdminException, ListNotExistsException
+    {
     }
 
     private final static String GET_MEMBERS_SQL =
         "SELECT ADDRESS FROM DL_MEMBERS WHERE LIST=?";
-    public List members(String dlname, String submitter) throws AdminException {
+    public List members(String dlname, String submitter)
+        throws AdminException, ListNotExistsException, IllegalSubmitterException
+    {
         List list = new ArrayList();
+        // get members first than check submitter
         return list;
     }
 
@@ -59,25 +102,51 @@ public class DistributionListManager implements DistributionListAdmin
         "SELECT NVL(COUNT(*), 0) FROM DL_SUBMITTERS WHERE ADDRESS=? AND LIST=?";
     private final static String ADD_SUBMITTER_SQL =
         "INSERT INTO DL_SUBMITTERS (ADDRESS, LIST) VALUES (?, ?)";
-    public void grantPosting(String dlname, String address) throws AdminException {
+    public void grantPosting(String dlname, String address)
+        throws AdminException, ListNotExistsException
+    {
     }
 
     private final static String DELETE_SUBMITTER_SQL =
         "DELETE FROM DL_SUBMITTERS WHERE ADDRESS=? AND LIST=?";
-    public void revokePosting(String dlname, String address) throws AdminException {
+    public void revokePosting(String dlname, String address)
+        throws AdminException, ListNotExistsException
+    {
     }
 
-    public void addDistributionList(DistributionList dl) throws AdminException {
+    private final static String CHECK_DL_SQL =
+        "SELECT NVL(COUNT(*), 0) FROM DL_SET WHERE LIST=?";
+    private final static String ADD_DL_SQL =
+        "INSERT INTO DL_SET (LIST, SYS, MAX_EL, OWNER) VALUES (?, ?, ?, ?)";
+    public void addDistributionList(DistributionList dl)
+        throws AdminException, ListAlreadyExistsException
+    {
     }
 
-    public void deleteDistributionList(String dlname) throws AdminException {
+    private final static String DELETE_DL_SUB_SQL =
+        "DELETE FROM DL_SUBMITTERS WHERE LIST=?";
+    private final static String DELETE_DL_MEM_SQL =
+        "DELETE FROM DL_MEMBERS WHERE LIST=?";
+    private final static String DELETE_DL_SQL =
+        "DELETE FROM DL_SET WHERE LIST=?";
+    public void deleteDistributionList(String dlname)
+        throws AdminException, ListNotExistsException
+    {
     }
 
-    public boolean checkPermission(String dlname, String address) throws AdminException {
+    private final static String CHECK_PERM_SQL =
+        "SELECT NVL(COUNT(*), 0) FROM DL_SUBMITTERS WHERE LIST=? AND ADDRESS=?";
+    public boolean checkPermission(String dlname, String address)
+        throws AdminException, ListNotExistsException
+    {
         return false;
     }
 
-    public List list() throws AdminException {
+    private final static String LIST_DL_SQL =
+        "SELECT LIST, SYS, MAX_EL, OWNER FROM DL_SET";
+    public List list()
+        throws AdminException
+    {
         List list = new ArrayList();
         return list;
     }
