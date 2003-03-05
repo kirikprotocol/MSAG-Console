47546ac2bcc355174de97ea1dc36c6f5d07166e7
igork <>
2003-03-05 16:32:44 +0000

    no message

--- src/resourcemanager/test/functionality_test.cpp
+++ src/resourcemanager/test/functionality_test.cpp
@@ -6,6 +6,6 @@ using namespace smsc::resourcemanager;
 int main(int argc, char *argv[])
 {
 	ResourceManager::init("en_en, ru_ru, blah_blah", "blah_blah");
-	ResourceManager manager = ResourceManager::getInstance();
-	manager.dump(std::cout);
+	const ResourceManager* manager = ResourceManager::getInstance();
+	manager->dump(std::cout);
 }
