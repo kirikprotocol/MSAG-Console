d16645038c622a3ae74bd09c726b58e60d7bbfb5
skv <>
2003-03-11 08:56:01 +0000

    some changes

--- config/scripts/start_smsc.sh
+++ config/scripts/start_smsc.sh
@@ -1,20 +1,27 @@
 #! /bin/bash
+export LC_ALL=ru_RU.ANSI1251            
+export NLS_LANG=RUSSIAN_CIS.CL8MSWIN1251
 
 echo $$ >pid
-
+if [ -f logs/smsc.err ]; then
+  mv logs/smsc.err logs/smsc.err.old
+fi
 while true;
 do
-./smsc 2>err.log &
+./smsc 1>/dev/null 2>>logs/smsc.err&
 PID=$!
 export PID
-trap "kill -2 $PID" SIGINT
+trap "kill -2 $PID;wait $PID" SIGINT
 wait $PID
 if [ -f core ]; then
-  DATE=`perl -e '($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);printf("%04d%02d%02d%02d%02d%02d",$year+1900,$mon,$mday,$hour,$min,$sec)'`
+  DATE=`perl -e '($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);printf("%04d%02d%02d%02d%02d%02d",$year+1900,$mon+1,$mday,$hour,$min,$sec)'`
   mkdir "$DATE"
   mv core "$DATE"
-  mv err.log "$DATE"
+  mv logs/smsc.err "$DATE"
+  if [ -f logs/smsc.err.old ]; then
+    mv logs/smsc.err.old "$DATE"
+  fi
   mv smsc.log "$DATE"
-  mv smsc "$DATA"
+  cp -p ./bin/smsc "$DATE"
 fi
 done
