ce6f0ac870fef8ccd3d7d4b4e8ef38b034c9bd52
skv <>
2003-07-28 10:00:19 +0000

    initial release

---
+++ src/system/traffic_control.cpp
@@ -0,0 +1,163 @@
+#include "util/debug.h"
+#include "system/traffic_control.hpp"
+#include "system/smsc.hpp"
+#include "store/StoreManager.h"
+
+namespace smsc{
+namespace system{
+
+using namespace smsc::profiler;
+
+
+bool TrafficControl::processCommand(SmscCommand& cmd)
+{
+  MutexGuard g(mtx);
+  switch(cmd->get_commandId())
+  {
+    case SUBMIT:
+    case FORWARD:
+    {
+      bool isReceipt=cmd->get_commandId()!=FORWARD &&
+                     cmd->get_sms()->hasStrProperty(Tag::SMPP_RECEIPTED_MESSAGE_ID);
+
+      if(!isReceipt)
+      {
+        double tcnt=totalCounter.Get()+1;
+        __debug2__(log,"TC: totcalCount=%lf",tcnt);
+        if(tcnt/cfg.shapeTimeFrame>cfg.maxSmsPerSecond)
+        {
+          __info2__(log,"TC: deny - SHAPING LIMIT!!! speed=%lf",tcnt/cfg.shapeTimeFrame);
+          return false;
+        }
+      }
+
+      Address dst;
+      SMS *sms;
+      SMS _sms;
+
+      if(cmd->get_commandId()==SUBMIT)
+      {
+        sms=cmd->get_sms();
+      }else
+      {
+        try{
+          cfg.store->retriveSms(cmd->get_forwardMsgId(),_sms);
+        }catch(...)
+        {
+          __warn2__(log,"TC: failed to retrieve sms with id=%lld",cmd->get_forwardMsgId());
+          return false;
+        }
+        sms=&_sms;
+      }
+
+      if(!cfg.smsc->AliasToAddress(sms->getDestinationAddress(),dst))
+      {
+        dst=sms->getDestinationAddress();
+      }
+      SmeProxy *src_proxy=cmd.getProxy();
+
+      smsc::router::RouteInfo ri;
+      SmeIndex dstIdx;
+      SmeProxy *dest_proxy;
+      bool has_route = cfg.smsc->routeSms(sms->getOriginatingAddress(),
+                                      dst,dstIdx,dest_proxy,&ri);
+      if(!has_route)return true;
+
+      if(!dest_proxy)return true;
+
+
+      if(!isReceipt)
+      {
+        time_t lookAhead=time(NULL)+cfg.lookAheadTime;
+
+        IntTimeSlotCounter *dcnt=getTSC(deliverCnt,dstIdx);
+        IntTimeSlotCounter *rcnt=getTSC(responseCnt,dstIdx);
+
+        int deliveryCount=dcnt->Get();
+        int responseCount=rcnt->Get();
+
+        if(deliveryCount-responseCount>cfg.protectThreshold)
+        {
+          __info2__(log,"TC: deny - protect threshold limit for %s: %d-%d",dest_proxy->getSystemId(),deliveryCount,responseCount);
+          return false;
+        }
+
+        int scount=cfg.smsc->GetSmeScheduleCount(dstIdx,lookAhead);
+
+        double speed=(double)responseCount*cfg.lookAheadTime/cfg.protectTimeFrame;
+
+        if(deliveryCount-responseCount<=cfg.allowedDeliveryFailures &&
+           speed<scount)
+        {
+          __info2__(log,"TC: deny - protect schedule limit for %s: %lf - %d",dest_proxy->getSystemId(),speed,scount);
+          return false;
+        }
+
+        if(cmd->get_commandId()==SUBMIT && !sms->hasStrProperty(Tag::SMPP_RECEIPTED_MESSAGE_ID))
+        {
+          smsc::profiler::Profile profile=cfg.smsc->getProfiler()->lookup(sms->getOriginatingAddress());
+
+          bool regdel=(sms->getIntProperty(Tag::SMPP_REGISTRED_DELIVERY)&0x3)==1 ||
+                      (sms->getIntProperty(Tag::SMPP_REGISTRED_DELIVERY)&0x3)==2 ||
+                       sms->getIntProperty(Tag::SMSC_STATUS_REPORT_REQUEST);
+
+          if(regdel || profile.reportoptions!=ProfileReportOptions::ReportNone)
+          {
+            SmeIndex idx=src_proxy->getSmeIndex();
+            IntTimeSlotCounter *dsrccnt=getTSC(deliverCnt,idx);
+            IntTimeSlotCounter *rsrccnt=getTSC(responseCnt,idx);
+            int deliveryCount=dsrccnt->Get();
+            int responseCount=rsrccnt->Get();
+
+            if(deliveryCount-responseCount>cfg.protectThreshold)
+            {
+              __info2__(log,"TC: deny - receipt protect threshold limit for %s: %d-%d",
+                src_proxy->getSystemId(),deliveryCount,responseCount);
+              return false;
+            }
+
+            int scount=cfg.smsc->GetSmeScheduleCount(idx,lookAhead);
+            double speed=(double)responseCount*cfg.lookAheadTime/cfg.protectTimeFrame;
+
+            if(deliveryCount-responseCount<=cfg.allowedDeliveryFailures &&
+               speed<scount)
+            {
+              __info2__(log,"TC: deny - protect schedule limit for %s: %lf - %d",src_proxy->getSystemId(),speed,scount);
+              return false;
+            }
+
+            dsrccnt->Inc();
+            __debug2__(log,"TC: receipt inc for %s",src_proxy->getSystemId());
+          }
+        }
+
+        if(!sms->hasStrProperty(Tag::SMPP_RECEIPTED_MESSAGE_ID))
+        {
+          __debug2__(log,"TC: inc for %s",dest_proxy->getSystemId());
+          dcnt->Inc();
+        }
+      }
+
+      totalCounter.Inc();
+      __debug2__(log,"TC: inc total count(%d)",totalCounter.Get());
+
+      return true;
+
+    }break;
+    case DELIVERY_RESP:
+      if(GET_STATUS_TYPE(cmd->get_resp()->get_status())==CMD_OK ||
+         GET_STATUS_TYPE(cmd->get_resp()->get_status())==CMD_ERR_PERM ||
+         GET_STATUS_TYPE(cmd->get_resp()->get_status())==CMD_ERR_FATAL)
+      {
+        __debug2__(log,"TC: delresp for %s",cmd.getProxy()->getSystemId());
+        getTSC(responseCnt,cmd.getProxy()->getSmeIndex())->Inc();
+      }
+    default:
+      return true;
+  }
+  return false;
+}
+
+
+};//system
+};//smsc
---
+++ src/system/traffic_control.hpp
@@ -0,0 +1,71 @@
+#ifndef __SMSC_SYSTEM_TRAFFIC_CONTROL_HPP__
+#define __SMSC_SYSTEM_TRAFFIC_CONTROL_HPP__
+
+#include "util/timeslotcounter.hpp"
+#include "core/buffers/IntHash.hpp"
+#include "smeman/smsccmd.h"
+#include "core/synchronization/Mutex.hpp"
+#include "logger/Logger.h"
+
+namespace smsc{
+
+namespace store{
+class MessageStore;
+};
+
+namespace system{
+
+class SmeManager;
+
+using smsc::smeman::SmscCommand;
+using smsc::core::buffers::IntHash;
+using smsc::smeman::SmeIndex;
+using namespace smsc::core::synchronization;
+
+class Smsc;
+
+class TrafficControl{
+protected:
+  typedef smsc::util::TimeSlotCounter<> IntTimeSlotCounter;
+public:
+  struct TrafficControlConfig{
+    int maxSmsPerSecond;
+    int protectThreshold;
+    int allowedDeliveryFailures;
+    time_t protectTimeFrame;
+    time_t shapeTimeFrame;
+    time_t lookAheadTime;
+    Smsc *smsc;
+    smsc::store::MessageStore *store;
+  };
+  TrafficControl(const TrafficControlConfig& cfg):
+    cfg(cfg),
+    totalCounter(cfg.shapeTimeFrame,10)
+  {
+    log=&smsc::util::Logger::getCategory("smsc.tc");
+  }
+
+
+  bool processCommand(SmscCommand& cmd);
+
+  IntTimeSlotCounter* getTSC(IntHash<IntTimeSlotCounter*>& hash,SmeIndex idx)
+  {
+    IntTimeSlotCounter** pptr=hash.GetPtr(idx);
+    if(pptr)return *pptr;
+    IntTimeSlotCounter* ptr=new IntTimeSlotCounter(cfg.protectTimeFrame);
+    hash.Insert(idx,ptr);
+    return ptr;
+  }
+
+protected:
+  TrafficControlConfig cfg;
+  IntHash<IntTimeSlotCounter*> deliverCnt,responseCnt;
+  IntTimeSlotCounter totalCounter;
+  Mutex mtx;
+  log4cpp::Category* log;
+};
+
+};//system
+};//smsc
+
+#endif
