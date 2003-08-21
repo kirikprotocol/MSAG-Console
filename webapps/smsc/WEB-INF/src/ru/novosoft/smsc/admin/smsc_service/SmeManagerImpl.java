45d11c1597e05596231b09440abd3f076f148ef1
igork <>
2003-08-21 12:50:26 +0000

    *** empty log message ***

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsc_service/SmeManagerImpl.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsc_service/SmeManagerImpl.java
@@ -19,98 +19,94 @@ import java.util.*;
  * Date: Jan 30, 2003
  * Time: 3:19:25 PM
  */
-public class SmeManagerImpl implements SmeManager
-{
-	private Category logger = Category.getInstance(this.getClass());
-	private SMEList smes = null;
-	private File smeConfigFile = null;
-	private Smsc smsc;
+public class SmeManagerImpl implements SmeManager {
+  private Category logger = Category.getInstance(this.getClass());
+  private SMEList smes = null;
+  private File smeConfigFile = null;
+  private Smsc smsc;
 
-	public SmeManagerImpl(Smsc smsc) throws AdminException
-	{
-		this.smsc = smsc;
-		try
-		{
-			smeConfigFile = new File(WebAppFolders.getSmscConfFolder(), "sme.xml");
-			Document smesDoc = Utils.parse(new FileReader(smeConfigFile));
-			smes = new SMEList(smesDoc.getDocumentElement());
-		}
-		catch (FactoryConfigurationError error)
-		{
-			logger.error("Couldn't configure xml parser factory", error);
-			throw new AdminException("Couldn't configure xml parser factory: " + error.getMessage());
-		}
-		catch (ParserConfigurationException e)
-		{
-			logger.error("Couldn't configure xml parser", e);
-			throw new AdminException("Couldn't configure xml parser: " + e.getMessage());
-		}
-		catch (SAXException e)
-		{
-			logger.error("Couldn't parse", e);
-			throw new AdminException("Couldn't parse: " + e.getMessage());
-		}
-		catch (IOException e)
-		{
-			logger.error("Couldn't read sme config", e);
-			throw new AdminException("Couldn't read sme config: " + e.getMessage());
-		}
-		catch (NullPointerException e)
-		{
-			logger.error("Couldn't parse sme config", e);
-			throw new AdminException("Couldn't parse sme config: " + e.getMessage());
-		}
-	}
+  public SmeManagerImpl(Smsc smsc) throws AdminException
+  {
+    this.smsc = smsc;
+    try {
+      smeConfigFile = new File(WebAppFolders.getSmscConfFolder(), "sme.xml");
+      Document smesDoc = Utils.parse(new FileReader(smeConfigFile));
+      smes = new SMEList(smesDoc.getDocumentElement());
+    } catch (FactoryConfigurationError error) {
+      logger.error("Couldn't configure xml parser factory", error);
+      throw new AdminException("Couldn't configure xml parser factory: " + error.getMessage());
+    } catch (ParserConfigurationException e) {
+      logger.error("Couldn't configure xml parser", e);
+      throw new AdminException("Couldn't configure xml parser: " + e.getMessage());
+    } catch (SAXException e) {
+      logger.error("Couldn't parse", e);
+      throw new AdminException("Couldn't parse: " + e.getMessage());
+    } catch (IOException e) {
+      logger.error("Couldn't read sme config", e);
+      throw new AdminException("Couldn't read sme config: " + e.getMessage());
+    } catch (NullPointerException e) {
+      logger.error("Couldn't parse sme config", e);
+      throw new AdminException("Couldn't parse sme config: " + e.getMessage());
+    }
+  }
 
-	public synchronized void removeAllIfSme(Collection serviceIds) throws AdminException
-	{
-		for (Iterator i = serviceIds.iterator(); i.hasNext();)
-		{
-			String serviceId = (String) i.next();
-			if (contains(serviceId))
-			{
-				remove(serviceId);
-			}
-		}
-	}
+  public synchronized void removeAllIfSme(Collection serviceIds) throws AdminException
+  {
+    for (Iterator i = serviceIds.iterator(); i.hasNext();) {
+      String serviceId = (String) i.next();
+      if (contains(serviceId)) {
+        remove(serviceId);
+      }
+    }
+  }
 
-	public synchronized SME remove(String id) throws AdminException
-	{
-		SME removedSme = smes.remove(id);
-		save();
-		if (smsc.getInfo().getStatus() == ServiceInfo.STATUS_RUNNING)
-			smsc.smeRemove(id);
-		return removedSme;
-	}
+  public synchronized SME remove(String id) throws AdminException
+  {
+    SME removedSme = smes.remove(id);
+    save();
+    if (smsc.getInfo().getStatus() == ServiceInfo.STATUS_RUNNING)
+      smsc.smeRemove(id);
+    return removedSme;
+  }
 
-	public synchronized boolean contains(String id)
-	{
-		return smes.contains(id);
-	}
+  public synchronized boolean contains(String id)
+  {
+    return smes.contains(id);
+  }
 
-	public synchronized SME get(String id) throws AdminException
-	{
-		return smes.get(id);
-	}
+  public synchronized SME get(String id) throws AdminException
+  {
+    return smes.get(id);
+  }
 
-	public synchronized SME add(String id, int priority, byte type, int typeOfNumber, int numberingPlan, int interfaceVersion, String systemType, String password, String addrRange, int smeN, boolean wantAlias, boolean forceDC, int timeout, String receiptSchemeName, boolean disabled, byte mode) throws AdminException
-	{
-		return add(new SME(id, priority, type, typeOfNumber, numberingPlan, interfaceVersion, systemType, password, addrRange, smeN, wantAlias, forceDC, timeout, receiptSchemeName, disabled, mode));
-	}
+  public synchronized SME add(String id, int priority, byte type, int typeOfNumber, int numberingPlan, int interfaceVersion, String systemType, String password, String addrRange, int smeN, boolean wantAlias, boolean forceDC, int timeout, String receiptSchemeName, boolean disabled, byte mode) throws AdminException
+  {
+    return add(new SME(id, priority, type, typeOfNumber, numberingPlan, interfaceVersion, systemType, password, addrRange, smeN, wantAlias, forceDC, timeout, receiptSchemeName, disabled, mode));
+  }
 
-	public synchronized SME add(SME newSme) throws AdminException
-	{
-		smes.add(newSme);
-		save();
-		if (smsc.getInfo().getStatus() == ServiceInfo.STATUS_RUNNING)
-			smsc.smeAdd(newSme);
-		return newSme;
-	}
+  public synchronized SME add(SME newSme) throws AdminException
+  {
+    smes.add(newSme);
+    save();
+    if (smsc.getInfo().getStatus() == ServiceInfo.STATUS_RUNNING)
+      smsc.smeAdd(newSme);
+    return newSme;
+  }
 
-	public synchronized List getSmeNames()
-	{
-		return new SortedList(smes.getNames());
-	}
+  public synchronized List getSmeNames()
+  {
+    return new SortedList(smes.getNames(), new Comparator() {
+      public int compare(Object o1, Object o2)
+      {
+        if (o1 instanceof String && o2 instanceof String) {
+          String s1 = (String) o1;
+          String s2 = (String) o2;
+          return s1.compareToIgnoreCase(s2);
+        } else
+          return 0;
+      }
+    });
+  }
 
 /*
 	public SMEList getSmes()
@@ -119,39 +115,36 @@ public class SmeManagerImpl implements SmeManager
 	}
 */
 
-	public synchronized void save() throws AdminException
-	{
-		try
-		{
-			PrintWriter out = new PrintWriter(new FileOutputStream(smeConfigFile), true);
-			Functions.storeConfigHeader(out, "records", "SmeRecords.dtd");
-			smes.store(out);
-			Functions.storeConfigFooter(out, "records");
-			out.flush();
-			out.close();
-		}
-		catch (FileNotFoundException e)
-		{
-			throw new AdminException("Couldn't save new smes settings: Couldn't write to destination config file: " + e.getMessage());
-		}
-	}
+  public synchronized void save() throws AdminException
+  {
+    try {
+      PrintWriter out = new PrintWriter(new FileOutputStream(smeConfigFile), true);
+      Functions.storeConfigHeader(out, "records", "SmeRecords.dtd");
+      smes.store(out);
+      Functions.storeConfigFooter(out, "records");
+      out.flush();
+      out.close();
+    } catch (FileNotFoundException e) {
+      throw new AdminException("Couldn't save new smes settings: Couldn't write to destination config file: " + e.getMessage());
+    }
+  }
 
-	public SmeStatus smeStatus(String id) throws AdminException
-	{
-		return smsc.getSmeStatus(id);
-	}
+  public SmeStatus smeStatus(String id) throws AdminException
+  {
+    return smsc.getSmeStatus(id);
+  }
 
-	public synchronized SME update(SME newSme) throws AdminException
-	{
-		SME updatedSme = smes.update(newSme);
-		save();
-		if (smsc.getInfo().getStatus() == ServiceInfo.STATUS_RUNNING)
-			smsc.smeUpdate(updatedSme);
-		return updatedSme;
-	}
+  public synchronized SME update(SME newSme) throws AdminException
+  {
+    SME updatedSme = smes.update(newSme);
+    save();
+    if (smsc.getInfo().getStatus() == ServiceInfo.STATUS_RUNNING)
+      smsc.smeUpdate(updatedSme);
+    return updatedSme;
+  }
 
-	public void disconnectSmes(List smeIdsToDisconnect) throws AdminException
-	{
-		smsc.disconnectSmes(smeIdsToDisconnect);
-	}
+  public void disconnectSmes(List smeIdsToDisconnect) throws AdminException
+  {
+    smsc.disconnectSmes(smeIdsToDisconnect);
+  }
 }
