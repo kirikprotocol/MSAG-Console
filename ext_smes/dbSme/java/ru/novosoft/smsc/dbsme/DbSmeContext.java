6b6b19c162591907eb8860c0fb84269f7588df96
igork <>
2003-08-27 12:31:49 +0000

    no message

--- ext_smes/dbSme/java/ru/novosoft/smsc/dbsme/DbSmeContext.java
+++ ext_smes/dbSme/java/ru/novosoft/smsc/dbsme/DbSmeContext.java
@@ -15,62 +15,58 @@ import java.io.InputStream;
  * Time: 4:50:30 PM
  */
 public class DbSmeContext {
-	private static DbSmeContext dbSmeContext = null;
+  private static DbSmeContext dbSmeContext = null;
 
-	public static synchronized DbSmeContext getInstance(SMSCAppContext appContext)
-	{
-		if (dbSmeContext == null)
-			dbSmeContext = new DbSmeContext(appContext);
-		return dbSmeContext;
-	}
+  public static synchronized DbSmeContext getInstance(SMSCAppContext appContext)
+  {
+    if (dbSmeContext == null)
+      dbSmeContext = new DbSmeContext(appContext);
+    return dbSmeContext;
+  }
 
-	private boolean configChanged = false;
+  private boolean configChanged = false;
 
-	public DbSmeContext(SMSCAppContext appContext)
-	{
-		this.configChanged = false;
-		File tempConfigFile = new File(WebAppFolders.getWorkFolder(), "dbSme.config.xml.tmp");
-		if (tempConfigFile.exists()) {
-			File origConfigFile = null;
-			try {
-				origConfigFile = new File(WebAppFolders.getServiceFolder(appContext.getHostsManager().getServiceInfo(Constants.DBSME_SME_ID).getHost(), Constants.DBSME_SME_ID), "conf/config.xml");
-			} catch (Exception e) {
-				System.out.println("Couldn't get original config, nested: " + e.getMessage());
-			}
-         if (origConfigFile.exists())
-			{
-				if (origConfigFile.length() != tempConfigFile.length())
-				{
-					this.configChanged = true;
-					return ;
-				}
-				try {
-					InputStream temp = new BufferedInputStream(new FileInputStream(tempConfigFile));
-					InputStream orig = new BufferedInputStream(new FileInputStream(origConfigFile));
-					for (int readedTemp = temp.read(); readedTemp != -1; readedTemp = temp.read())
-					{
-						if (readedTemp != orig.read())
-						{
-							configChanged = true;
-							temp.close();
-							orig.close();
-							return;
-						}
-					}
-				} catch (Exception e) {
-					System.out.println("Couldn't compare origianl and saved configs, nested: " + e.getMessage());
-				}
-			}
-		}
-	}
+  public DbSmeContext(SMSCAppContext appContext)
+  {
+    this.configChanged = false;
+    File tempConfigFile = new File(WebAppFolders.getWorkFolder(), "dbSme.config.xml.tmp");
+    if (tempConfigFile.exists()) {
+      File origConfigFile = null;
+      try {
+        origConfigFile = new File(appContext.getHostsManager().getServiceInfo(Constants.DBSME_SME_ID).getServiceFolder(), "conf/config.xml");
+      } catch (Exception e) {
+        System.out.println("Couldn't get original config, nested: " + e.getMessage());
+      }
+      if (origConfigFile.exists()) {
+        if (origConfigFile.length() != tempConfigFile.length()) {
+          this.configChanged = true;
+          return;
+        }
+        try {
+          InputStream temp = new BufferedInputStream(new FileInputStream(tempConfigFile));
+          InputStream orig = new BufferedInputStream(new FileInputStream(origConfigFile));
+          for (int readedTemp = temp.read(); readedTemp != -1; readedTemp = temp.read()) {
+            if (readedTemp != orig.read()) {
+              configChanged = true;
+              temp.close();
+              orig.close();
+              return;
+            }
+          }
+        } catch (Exception e) {
+          System.out.println("Couldn't compare origianl and saved configs, nested: " + e.getMessage());
+        }
+      }
+    }
+  }
 
-	public boolean isConfigChanged()
-	{
-		return configChanged;
-	}
+  public boolean isConfigChanged()
+  {
+    return configChanged;
+  }
 
-	public void setConfigChanged(boolean configChanged)
-	{
-		this.configChanged = configChanged;
-	}
+  public void setConfigChanged(boolean configChanged)
+  {
+    this.configChanged = configChanged;
+  }
 }
--- ext_smes/dbSme/java/ru/novosoft/smsc/dbsme/DbsmeBean.java
+++ ext_smes/dbSme/java/ru/novosoft/smsc/dbsme/DbsmeBean.java
@@ -20,127 +20,127 @@ import java.util.Set;
  */
 public class DbsmeBean extends IndexBean {
 
-	public static final int RESULT_OVERVIEW = IndexBean.PRIVATE_RESULT;
-	public static final int RESULT_PARAMS = IndexBean.PRIVATE_RESULT + 1;
-	public static final int RESULT_DRIVERS = IndexBean.PRIVATE_RESULT + 2;
-	public static final int RESULT_PROVIDERS = IndexBean.PRIVATE_RESULT + 3;
-	public static final int RESULT_PROVIDER = IndexBean.PRIVATE_RESULT + 4;
-	protected static final int PRIVATE_RESULT = IndexBean.PRIVATE_RESULT + 5;
+  public static final int RESULT_OVERVIEW = IndexBean.PRIVATE_RESULT;
+  public static final int RESULT_PARAMS = IndexBean.PRIVATE_RESULT + 1;
+  public static final int RESULT_DRIVERS = IndexBean.PRIVATE_RESULT + 2;
+  public static final int RESULT_PROVIDERS = IndexBean.PRIVATE_RESULT + 3;
+  public static final int RESULT_PROVIDER = IndexBean.PRIVATE_RESULT + 4;
+  protected static final int PRIVATE_RESULT = IndexBean.PRIVATE_RESULT + 5;
 
-	private String menuSelection = null;
-	protected Config config = null;
+  private String menuSelection = null;
+  protected Config config = null;
 
-	protected int init(List errors)
-	{
-		int result = super.init(errors);
-		if (result != RESULT_OK)
-			return result;
+  protected int init(List errors)
+  {
+    int result = super.init(errors);
+    if (result != RESULT_OK)
+      return result;
 
-		try {
-			File tempConfigFile = new File(WebAppFolders.getWorkFolder(), "dbSme.config.xml.tmp");
-			if (!tempConfigFile.exists()) {
-				File origConfigFile = new File(WebAppFolders.getServiceFolder(appContext.getHostsManager().getServiceInfo(Constants.DBSME_SME_ID).getHost(), Constants.DBSME_SME_ID), "conf/config.xml");
-				if (!origConfigFile.exists()) {
-					logger.error("Couldn't find DBSME config file (" + origConfigFile.getAbsolutePath() + ")");
-					return error(DBSmeErrors.error.couldntFindConfig, origConfigFile.getAbsolutePath());
-				}
-				InputStream in = new BufferedInputStream(new FileInputStream(origConfigFile));
-				OutputStream out = new BufferedOutputStream(new FileOutputStream(tempConfigFile));
-				for (int readedByte = in.read(); readedByte >= 0; readedByte = in.read())
-					out.write(readedByte);
-				in.close();
-				out.close();
-			}
+    try {
+      File tempConfigFile = new File(WebAppFolders.getWorkFolder(), "dbSme.config.xml.tmp");
+      if (!tempConfigFile.exists()) {
+        File origConfigFile = new File(appContext.getHostsManager().getServiceInfo(Constants.DBSME_SME_ID).getServiceFolder(), "conf/config.xml");
+        if (!origConfigFile.exists()) {
+          logger.error("Couldn't find DBSME config file (" + origConfigFile.getAbsolutePath() + ")");
+          return error(DBSmeErrors.error.couldntFindConfig, origConfigFile.getAbsolutePath());
+        }
+        InputStream in = new BufferedInputStream(new FileInputStream(origConfigFile));
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(tempConfigFile));
+        for (int readedByte = in.read(); readedByte >= 0; readedByte = in.read())
+          out.write(readedByte);
+        in.close();
+        out.close();
+      }
 
-			config = new Config(tempConfigFile);
-		} catch (Throwable e) {
-			logger.error("Couldn't get DBSME config", e);
-			return error(DBSmeErrors.error.couldntGetConfig, e);
-		}
+      config = new Config(tempConfigFile);
+    } catch (Throwable e) {
+      logger.error("Couldn't get DBSME config", e);
+      return error(DBSmeErrors.error.couldntGetConfig, e);
+    }
 
-		return RESULT_OK;
-	}
+    return RESULT_OK;
+  }
 
-	public int process(SMSCAppContext appContext, List errors, Principal loginedPrincipal)
-	{
-		int result = super.process(appContext, errors, loginedPrincipal);
-		if (result != RESULT_OK)
-			return result;
+  public int process(SMSCAppContext appContext, List errors, Principal loginedPrincipal)
+  {
+    int result = super.process(appContext, errors, loginedPrincipal);
+    if (result != RESULT_OK)
+      return result;
 
-		if ("overview".equalsIgnoreCase(menuSelection))
-			return RESULT_OVERVIEW;
-		if ("params".equalsIgnoreCase(menuSelection))
-			return RESULT_PARAMS;
-		if ("drivers".equalsIgnoreCase(menuSelection))
-			return RESULT_DRIVERS;
-		if ("providers".equalsIgnoreCase(menuSelection))
-			return RESULT_PROVIDERS;
+    if ("overview".equalsIgnoreCase(menuSelection))
+      return RESULT_OVERVIEW;
+    if ("params".equalsIgnoreCase(menuSelection))
+      return RESULT_PARAMS;
+    if ("drivers".equalsIgnoreCase(menuSelection))
+      return RESULT_DRIVERS;
+    if ("providers".equalsIgnoreCase(menuSelection))
+      return RESULT_PROVIDERS;
 
-		return RESULT_OK;
-	}
+    return RESULT_OK;
+  }
 
-	public byte getServiceStatus()
-	{
-		try {
-			return appContext.getHostsManager().getServiceInfo(Constants.DBSME_SME_ID).getStatus();
-		} catch (AdminException e) {
-			logger.error("Couldn't get DBSME status, nested: " + e.getMessage(), e);
-			return ServiceInfo.STATUS_UNKNOWN;
-		}
-	}
+  public byte getServiceStatus()
+  {
+    try {
+      return appContext.getHostsManager().getServiceInfo(Constants.DBSME_SME_ID).getStatus();
+    } catch (AdminException e) {
+      logger.error("Couldn't get DBSME status, nested: " + e.getMessage(), e);
+      return ServiceInfo.STATUS_UNKNOWN;
+    }
+  }
 
-	public Set getParameterNames()
-	{
-		return config.getParameterNames();
-	}
+  public Set getParameterNames()
+  {
+    return config.getParameterNames();
+  }
 
-	public int getInt(String paramName)
-	{
-		try {
-			return config.getInt(paramName);
-		} catch (Exception e) {
-			logger.debug("Couldn't get int parameter \"" + paramName + "\", nested: " + e.getMessage(), e);
-			return 0;
-		}
-	}
+  public int getInt(String paramName)
+  {
+    try {
+      return config.getInt(paramName);
+    } catch (Exception e) {
+      logger.debug("Couldn't get int parameter \"" + paramName + "\", nested: " + e.getMessage(), e);
+      return 0;
+    }
+  }
 
-	public String getString(String paramName)
-	{
-		try {
-			return config.getString(paramName);
-		} catch (Exception e) {
-			logger.debug("Couldn't get string parameter \"" + paramName + "\", nested: " + e.getMessage(), e);
-			return "";
-		}
-	}
+  public String getString(String paramName)
+  {
+    try {
+      return config.getString(paramName);
+    } catch (Exception e) {
+      logger.debug("Couldn't get string parameter \"" + paramName + "\", nested: " + e.getMessage(), e);
+      return "";
+    }
+  }
 
-	public boolean getBool(String paramName)
-	{
-		try {
-			return config.getBool(paramName);
-		} catch (Exception e) {
-			logger.debug("Couldn't get boolean parameter \"" + paramName + "\", nested: " + e.getMessage(), e);
-			return false;
-		}
-	}
+  public boolean getBool(String paramName)
+  {
+    try {
+      return config.getBool(paramName);
+    } catch (Exception e) {
+      logger.debug("Couldn't get boolean parameter \"" + paramName + "\", nested: " + e.getMessage(), e);
+      return false;
+    }
+  }
 
-	public Object getParameter(String paramName)
-	{
-		return config.getParameter(paramName);
-	}
+  public Object getParameter(String paramName)
+  {
+    return config.getParameter(paramName);
+  }
 
-	public String getMenuSelection()
-	{
-		return menuSelection;
-	}
+  public String getMenuSelection()
+  {
+    return menuSelection;
+  }
 
-	public void setMenuSelection(String menuSelection)
-	{
-		this.menuSelection = menuSelection;
-	}
+  public void setMenuSelection(String menuSelection)
+  {
+    this.menuSelection = menuSelection;
+  }
 
-	public Set getSectionChildSectionNames(String sectionName)
-	{
-		return config.getSectionChildSectionNames(sectionName);
-	}
+  public Set getSectionChildSectionNames(String sectionName)
+  {
+    return config.getSectionChildSectionNames(sectionName);
+  }
 }
