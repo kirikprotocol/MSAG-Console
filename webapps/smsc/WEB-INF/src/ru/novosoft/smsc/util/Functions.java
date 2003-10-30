961598081cd5d7cb6a20beca03b2f15f99a86e9c
igork <>
2003-10-30 11:53:44 +0000

    *** empty log message ***

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/util/Functions.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/util/Functions.java
@@ -1,12 +1,15 @@
 package ru.novosoft.smsc.util;
 
 import ru.novosoft.smsc.jsp.SMSCAppContext;
+import ru.novosoft.smsc.admin.AdminException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.*;
+
+import org.apache.log4j.Category;
 
 /**
  * Created by igork
@@ -15,91 +18,89 @@ import java.util.*;
  */
 public class Functions
 {
-	public static boolean recursiveDeleteFolder(File folder)
-	{
-		String[] childNames = folder.list();
-		if (childNames != null)
-		{
-			for (int i = 0; i < childNames.length; i++)
-			{
-				File child = new File(folder, childNames[i]);
-				if (child.isDirectory())
-					recursiveDeleteFolder(child);
-				else
-					child.delete();
-			}
-		}
-		return folder.delete();
-	}
+  private static Category logger = Category.getInstance(Functions.class);
 
-	public static void unZipFileFromArchive(File folderUnpackTo, String name, ZipInputStream zin)
-			throws IOException
-	{
-		File file = new File(folderUnpackTo, name);
-		file.getParentFile().mkdirs();
-		OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
-		for (int i = 0; (i = zin.read()) != -1; out.write(i)) ;
-		out.close();
-	}
+  public static boolean recursiveDeleteFolder(File folder)
+  {
+    String[] childNames = folder.list();
+    if (childNames != null) {
+      for (int i = 0; i < childNames.length; i++) {
+        File child = new File(folder, childNames[i]);
+        if (child.isDirectory())
+          recursiveDeleteFolder(child);
+        else
+          child.delete();
+      }
+    }
+    return folder.delete();
+  }
 
-	public static void unZipArchive(File folderUnpackTo, InputStream in)
-			throws IOException
-	{
-		ZipInputStream zin = new ZipInputStream(in);
-		for (ZipEntry e = zin.getNextEntry(); e != null; e = zin.getNextEntry())
-		{
-			if (!e.isDirectory())
-				unZipFileFromArchive(folderUnpackTo, e.getName(), zin);
-		}
-		zin.close();
-		in.close();
-	}
+  public static void unZipFileFromArchive(File folderUnpackTo, String name, ZipInputStream zin)
+          throws IOException
+  {
+    File file = new File(folderUnpackTo, name);
+    file.getParentFile().mkdirs();
+    OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+    for (int i = 0; (i = zin.read()) != -1; out.write(i)) ;
+    out.close();
+  }
 
-	public static File saveFileToTemp(InputStream in,
-												 String prefix, //"SMSC_SME_distrib_"
-												 String sufffix //".zip.tmp"
-												 )
-			throws IOException
-	{
-		File tmpFile = File.createTempFile(prefix, sufffix);
-		OutputStream out = new BufferedOutputStream(new FileOutputStream(tmpFile));
+  public static void unZipArchive(File folderUnpackTo, InputStream in)
+          throws IOException
+  {
+    ZipInputStream zin = new ZipInputStream(in);
+    for (ZipEntry e = zin.getNextEntry(); e != null; e = zin.getNextEntry()) {
+      if (!e.isDirectory())
+        unZipFileFromArchive(folderUnpackTo, e.getName(), zin);
+    }
+    zin.close();
+    in.close();
+  }
 
-		byte buffer[] = new byte[2048];
-		for (int readed = 0; (readed = in.read(buffer)) > -1;)
-		{
-			out.write(buffer, 0, readed);
-		}
+  public static File saveFileToTemp(InputStream in,
+                                    String prefix, //"SMSC_SME_distrib_"
+                                    String sufffix //".zip.tmp"
+                                    )
+          throws IOException
+  {
+    File tmpFile = File.createTempFile(prefix, sufffix);
+    OutputStream out = new BufferedOutputStream(new FileOutputStream(tmpFile));
 
-		in.close();
-		out.close();
-		return tmpFile;
-	}
+    byte buffer[] = new byte[2048];
+    for (int readed = 0; (readed = in.read(buffer)) > -1;) {
+      out.write(buffer, 0, readed);
+    }
 
-	public static PrintWriter storeConfigHeader(PrintWriter out, String docType, String dtdFile)
-	{
-		// C++ code doesn't know about other codings // System.getProperty("file.encoding");
-		return storeConfigHeader(out, docType, dtdFile, null);
-	}
+    in.close();
+    out.close();
+    return tmpFile;
+  }
 
-	public static PrintWriter storeConfigHeader(PrintWriter out, String docType, String dtdFile, String encoding)
-	{
-		out.println("<?xml version=\"1.0\" encoding=\"" + (encoding == null || encoding.length() == 0 ? "ISO-8859-1" : encoding) + "\"?>");
-		out.println("<!DOCTYPE " + docType + " SYSTEM \"file://" + dtdFile + "\">");
-		out.println();
-		out.println("<" + docType + ">");
-		return out;
-	}
+  public static PrintWriter storeConfigHeader(PrintWriter out, String docType, String dtdFile)
+  {
+    // C++ code doesn't know about other codings // System.getProperty("file.encoding");
+    return storeConfigHeader(out, docType, dtdFile, null);
+  }
 
-	public static PrintWriter storeConfigFooter(PrintWriter out, String docType)
-	{
-		out.println("</" + docType + ">");
-		return out;
-	}
+  public static PrintWriter storeConfigHeader(PrintWriter out, String docType, String dtdFile, String encoding)
+  {
+    out.println("<?xml version=\"1.0\" encoding=\"" + (encoding == null || encoding.length() == 0 ? "ISO-8859-1" : encoding) + "\"?>");
+    out.println("<!DOCTYPE " + docType + " SYSTEM \"file://" + dtdFile + "\">");
+    out.println();
+    out.println("<" + docType + ">");
+    return out;
+  }
 
-	public static SMSCAppContext getAppContext(HttpServletRequest request)
-	{
-		return (SMSCAppContext) request.getAttribute("appContext");
-	}
+  public static PrintWriter storeConfigFooter(PrintWriter out, String docType)
+  {
+    out.println("</" + docType + ">");
+    return out;
+  }
+
+  public static SMSCAppContext getAppContext(HttpServletRequest request)
+  {
+    return (SMSCAppContext) request.getAttribute("appContext");
+  }
 
   public static String collectionToString(final Collection collection, final String delimeter)
   {
@@ -127,10 +128,11 @@ public class Functions
   }
 
   private static int filenameCounter = 0;
+
   public static synchronized File createTempFilename(String prefix, String suffix, File directory)
   {
     File file = new File(directory, prefix + (filenameCounter++));
-    while(file.exists())
+    while (file.exists())
       file = new File(directory, prefix + (filenameCounter++));
     return file;
   }
@@ -144,5 +146,20 @@ public class Functions
     calendar.set(Calendar.SECOND, 0);
     calendar.set(Calendar.MILLISECOND, 0);
     return calendar.getTime();
+  }
+
+  public static String getServiceId(String servletPath) throws AdminException
+  {
+    final String prefix = "/esme_";
+    if (servletPath == null || !servletPath.startsWith(prefix)) {
+      logger.error("Call getServiceId(String servletPath) with incorrect parameter \"" + servletPath + "\"");
+      throw new AdminException("Call getServiceId(String servletPath) with incorrect parameter \"" + servletPath + "\"");
+    }
+    final int secondSlashIndex = servletPath.indexOf('/', prefix.length());
+    if (secondSlashIndex < prefix.length()) {
+      logger.error("Call getServiceId(String servletPath) with incorrect parameter \"" + servletPath + "\"");
+      throw new AdminException("Call getServiceId(String servletPath) with incorrect parameter \"" + servletPath + "\"");
+    }
+    return servletPath.substring(prefix.length(), secondSlashIndex);
   }
 }
