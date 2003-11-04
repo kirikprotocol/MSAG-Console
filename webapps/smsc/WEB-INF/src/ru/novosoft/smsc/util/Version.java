a65fcc010ed4cfa8ed778200f9f98a7d16b8ed74
igork <>
2003-11-04 14:06:10 +0000

    *** empty log message ***

---
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/util/Version.java
@@ -0,0 +1,16 @@
+package ru.novosoft.smsc.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: igork
+ * Date: 04.11.2003
+ * Time: 20:00:52
+ */
+public abstract class Version
+{
+  public static final int VER_MAJOR = 1;
+  public static final int VER_MINOR = 0;
+  public static final int VER_BUILD = 2;
+  public static final String VER_DATE = "Tue Nov  4 10:51:36 2003";
+  public static final String VER_STR = "Sibinco SMSC Control Center ver " + VER_MAJOR + "." + VER_MINOR + " build " + VER_BUILD + " date " + VER_DATE;
+}
