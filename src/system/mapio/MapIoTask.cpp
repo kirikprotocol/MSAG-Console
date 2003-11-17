04f467032e08a7144da3d886555056c6b92a3d94
serge <>
2003-11-17 14:51:35 +0000

    Fixed bug in 'not all binded'

--- src/system/mapio/MapIoTask.cpp
+++ src/system/mapio/MapIoTask.cpp
@@ -157,14 +157,10 @@ void MapIoTask::dispatcher()
     }
 */
     if ( result == MSG_TIMEOUT ) {
-	if ( __global_bind_counter != CORRECT_BIND_COUNTER ){
-          if( ++bindTimer > MAX_BIND_TIMEOUT ) {
-      	    result = MSG_BROKEN_CONNECTION;
-	    __map_warn2__("MAP:: not all binders binded in %d seconds. Restarting...", MAX_BIND_TIMEOUT);
-          }
-
-        }
-        continue;
+	if ( __global_bind_counter == CORRECT_BIND_COUNTER ) continue;
+        if( ++bindTimer <= MAX_BIND_TIMEOUT ) continue;
+	result = MSG_BROKEN_CONNECTION;
+	__map_warn2__("MAP:: not all binders binded in %d seconds. Restarting...", MAX_BIND_TIMEOUT);
     }
     if ( result == MSG_BROKEN_CONNECTION ){
       __map_warn2__("Broken connection");
