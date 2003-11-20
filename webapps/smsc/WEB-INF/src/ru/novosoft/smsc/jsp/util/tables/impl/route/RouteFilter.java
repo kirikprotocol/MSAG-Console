bf9f1adf3d3b5ac34389c493c1946574ce41669b
igork <>
2003-11-20 10:10:50 +0000

    *** empty log message ***

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/util/tables/impl/route/RouteFilter.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/util/tables/impl/route/RouteFilter.java
@@ -15,246 +15,240 @@ import java.util.*;
 
 public class RouteFilter implements Filter
 {
-	private Set src_subjects = null;
-	private MaskList src_masks = null;
-	private Set dst_subjects = null;
-	private MaskList dst_masks = null;
-	private List smes = null;
-	private boolean intersection = false;
-	private static final int ALLOWED = 0;
-	private static final int NOT_ALLOWED = 1;
-	private static final int UNKNOWN = 2;
+  private Set src_subjects = null;
+  private MaskList src_masks = null;
+  private Set dst_subjects = null;
+  private MaskList dst_masks = null;
+  private List smes = null;
+  private boolean intersection = false;
+  private static final int ALLOWED = 0;
+  private static final int NOT_ALLOWED = 1;
+  private static final int UNKNOWN = 2;
 
-	public RouteFilter()
-	{
-		src_subjects = new HashSet();
-		src_masks = new MaskList();
-		dst_subjects = new HashSet();
-		dst_masks = new MaskList();
-		smes = new Vector();
-	}
+  public RouteFilter()
+  {
+    src_subjects = new HashSet();
+    src_masks = new MaskList();
+    dst_subjects = new HashSet();
+    dst_masks = new MaskList();
+    smes = new Vector();
+  }
 
-	public RouteFilter(boolean isIntersection,
-							 Set source_selected,
-							 String src_masks,
-							 Set destination_selected,
-							 String dst_masks,
-							 List smes,
-							 SubjectList allSubjects)
-	{
-		intersection = isIntersection;
-		// get sources
-		this.src_subjects = source_selected;
-		this.src_masks = new MaskList(src_masks);
+  public RouteFilter(boolean isIntersection,
+                     Set source_selected,
+                     String src_masks,
+                     Set destination_selected,
+                     String dst_masks,
+                     List smes)
+  {
+    intersection = isIntersection;
+    // get sources
+    this.src_subjects = source_selected;
+    this.src_masks = new MaskList(src_masks);
 
-		// get destinations
-		this.dst_subjects = destination_selected;
-		this.dst_masks = new MaskList(dst_masks);
+    // get destinations
+    this.dst_subjects = destination_selected;
+    this.dst_masks = new MaskList(dst_masks);
 
-		this.smes = smes;
-	}
+    this.smes = smes;
+  }
 
-	public boolean isEmpty()
-	{
-		return this.src_subjects.isEmpty() && this.src_masks.isEmpty()
-				  && this.dst_subjects.isEmpty() && this.dst_masks.isEmpty()
-				  && this.smes.isEmpty();
-	}
+  public boolean isEmpty()
+  {
+    return this.src_subjects.isEmpty() && this.src_masks.isEmpty()
+            && this.dst_subjects.isEmpty() && this.dst_masks.isEmpty()
+            && this.smes.isEmpty();
+  }
 
-	protected static int isSubjectAllowed(Set subjects, String subj)
-	{
-		if (subjects.isEmpty())
-			return UNKNOWN;
-		if (subjects.contains(subj))
-			return ALLOWED;
-		else
-			return NOT_ALLOWED;
-	}
+  protected static int isSubjectAllowed(Set subjects, String subj)
+  {
+    if (subjects.isEmpty())
+      return UNKNOWN;
+    if (subjects.contains(subj))
+      return ALLOWED;
+    else
+      return NOT_ALLOWED;
+  }
 
-	protected static int isMaskAllowed(MaskList masksSet, String mask)
-	{
-		if (masksSet.isEmpty())
-			return UNKNOWN;
+  protected static int isMaskAllowed(MaskList masksSet, String maskStr)
+  {
+    if (masksSet.isEmpty())
+      return UNKNOWN;
 
-		for (Iterator i = masksSet.iterator(); i.hasNext();)
-		{
-			Mask m = (Mask) i.next();
-			if (mask.startsWith(m.getMask()))
-				return ALLOWED;
-		}
-		return NOT_ALLOWED;
-	}
+    final Mask maskToTest;
+    try {
+      maskToTest = new Mask(maskStr);
+    } catch (AdminException e) {
+      return UNKNOWN;
+    }
 
-	protected static int isSourceAllowed(Set subjects, MaskList masks, Source src)
-	{
-		if (src.isSubject())
-			return isSubjectAllowed(subjects, src.getName());
-		else
-			return isMaskAllowed(masks, src.getName());
-	}
+    for (Iterator i = masksSet.iterator(); i.hasNext();) {
+      Mask m = (Mask) i.next();
+      if (m.addressConfirm(maskToTest))
+        return ALLOWED;
+    }
+    return NOT_ALLOWED;
+  }
 
-	protected int isSourcesAllowed(SourceList srcs)
-	{
-		if (src_subjects.isEmpty() && src_masks.isEmpty())
-			return UNKNOWN;
+  protected static int isSourceAllowed(Set subjects, MaskList masks, Source src)
+  {
+    if (src.isSubject())
+      return isSubjectAllowed(subjects, src.getName());
+    else
+      return isMaskAllowed(masks, src.getName());
+  }
 
-		for (Iterator i = srcs.iterator(); i.hasNext();)
-		{
-			switch (isSourceAllowed(src_subjects, src_masks, (Source) i.next()))
-			{
-				case ALLOWED:
-					if (!intersection)
-						return ALLOWED;
-					break;
+  protected int isSourcesAllowed(SourceList srcs)
+  {
+    if (src_subjects.isEmpty() && src_masks.isEmpty())
+      return UNKNOWN;
 
-				case NOT_ALLOWED:
-					if (intersection)
-						return NOT_ALLOWED;
-					break;
+    for (Iterator i = srcs.iterator(); i.hasNext();) {
+      switch (isSourceAllowed(src_subjects, src_masks, (Source) i.next())) {
+        case ALLOWED:
+          if (!intersection)
+            return ALLOWED;
+          break;
 
-				case UNKNOWN:
-					// do nothing
-					break;
+        case NOT_ALLOWED:
+          if (intersection)
+            return NOT_ALLOWED;
+          break;
 
-				default:
-					throw new InternalError("unknown state");
-			}
-		}
-		return intersection ? ALLOWED : NOT_ALLOWED;
-	}
+        case UNKNOWN:
+          // do nothing
+          break;
 
-	protected int isDestinationsAllowed(DestinationList dsts)
-	{
-		if (dst_subjects.isEmpty() && dst_masks.isEmpty())
-			return UNKNOWN;
+        default:
+          throw new InternalError("unknown state");
+      }
+    }
+    return intersection ? ALLOWED : NOT_ALLOWED;
+  }
 
-		for (Iterator i = dsts.iterator(); i.hasNext();)
-		{
-			switch (isSourceAllowed(dst_subjects, dst_masks, (Source) i.next()))
-			{
-				case ALLOWED:
-					if (!intersection)
-						return ALLOWED;
-					break;
+  protected int isDestinationsAllowed(DestinationList dsts)
+  {
+    if (dst_subjects.isEmpty() && dst_masks.isEmpty())
+      return UNKNOWN;
 
-				case NOT_ALLOWED:
-					if (intersection)
-						return NOT_ALLOWED;
-					break;
+    for (Iterator i = dsts.iterator(); i.hasNext();) {
+      switch (isSourceAllowed(dst_subjects, dst_masks, (Source) i.next())) {
+        case ALLOWED:
+          if (!intersection)
+            return ALLOWED;
+          break;
 
-				case UNKNOWN:
-					// do nothing
-					break;
+        case NOT_ALLOWED:
+          if (intersection)
+            return NOT_ALLOWED;
+          break;
 
-				default:
-					throw new InternalError("unknown state");
-			}
-		}
-		return intersection ? ALLOWED : NOT_ALLOWED;
-	}
+        case UNKNOWN:
+          // do nothing
+          break;
 
-	protected int isSMEsAllowed(DestinationList dsts)
-	{
-		if (smes.isEmpty())
-			return UNKNOWN;
+        default:
+          throw new InternalError("unknown state");
+      }
+    }
+    return intersection ? ALLOWED : NOT_ALLOWED;
+  }
 
-		for (Iterator i = dsts.iterator(); i.hasNext();)
-		{
-			Destination dst = (Destination) i.next();
-			if (intersection)
-			{
-				if (!smes.contains(dst.getSme().getId()))
-					return NOT_ALLOWED;
-			}
-			else
-			{
-				if (smes.contains(dst.getSme().getId()))
-					return ALLOWED;
-			}
-		}
-		return intersection ? ALLOWED : NOT_ALLOWED;
-	}
+  protected int isSMEsAllowed(DestinationList dsts)
+  {
+    if (smes.isEmpty())
+      return UNKNOWN;
 
-	public boolean isItemAllowed(DataItem item)
-	{
-		if (isEmpty())
-			return true;
+    for (Iterator i = dsts.iterator(); i.hasNext();) {
+      Destination dst = (Destination) i.next();
+      if (intersection) {
+        if (!smes.contains(dst.getSme().getId()))
+          return NOT_ALLOWED;
+      } else {
+        if (smes.contains(dst.getSme().getId()))
+          return ALLOWED;
+      }
+    }
+    return intersection ? ALLOWED : NOT_ALLOWED;
+  }
 
-		SourceList srcs = (SourceList) item.getValue("sources");
-		DestinationList dsts = (DestinationList) item.getValue("destinations");
+  public boolean isItemAllowed(DataItem item)
+  {
+    if (isEmpty())
+      return true;
 
-		if (intersection)
-		{
-			return isSourcesAllowed(srcs) != NOT_ALLOWED
-					  && isDestinationsAllowed(dsts) != NOT_ALLOWED
-					  && isSMEsAllowed(dsts) != NOT_ALLOWED;
-		}
-		else
-		{
-			return isSourcesAllowed(srcs) == ALLOWED
-					  || isDestinationsAllowed(dsts) == ALLOWED
-					  || isSMEsAllowed(dsts) == ALLOWED;
-		}
-	}
+    SourceList srcs = (SourceList) item.getValue("sources");
+    DestinationList dsts = (DestinationList) item.getValue("destinations");
+
+    if (intersection) {
+      return isSourcesAllowed(srcs) != NOT_ALLOWED
+              && isDestinationsAllowed(dsts) != NOT_ALLOWED
+              && isSMEsAllowed(dsts) != NOT_ALLOWED;
+    } else {
+      return isSourcesAllowed(srcs) == ALLOWED
+              || isDestinationsAllowed(dsts) == ALLOWED
+              || isSMEsAllowed(dsts) == ALLOWED;
+    }
+  }
 
 
-	public String[] getSourceSubjectNames()
-	{
-		return (String[])src_subjects.toArray(new String[0]);
-	}
+  public String[] getSourceSubjectNames()
+  {
+    return (String[]) src_subjects.toArray(new String[0]);
+  }
 
-	public String[] getSourceMaskStrings()
-	{
-		return (String[])src_masks.getNames().toArray(new String[0]);
-	}
+  public String[] getSourceMaskStrings()
+  {
+    return (String[]) src_masks.getNames().toArray(new String[0]);
+  }
 
-	public String[] getDestinationSubjectNames()
-	{
-		return (String[])dst_subjects.toArray(new String[0]);
-	}
+  public String[] getDestinationSubjectNames()
+  {
+    return (String[]) dst_subjects.toArray(new String[0]);
+  }
 
-	public String[] getDestinationMaskStrings()
-	{
-		return (String[])dst_masks.getNames().toArray(new String[0]);
-	}
+  public String[] getDestinationMaskStrings()
+  {
+    return (String[]) dst_masks.getNames().toArray(new String[0]);
+  }
 
-	public String[] getSmeIds()
-	{
-		return (String[])smes.toArray(new String[0]);
-	}
+  public String[] getSmeIds()
+  {
+    return (String[]) smes.toArray(new String[0]);
+  }
 
-	public boolean isIntersection()
-	{
-		return intersection;
-	}
+  public boolean isIntersection()
+  {
+    return intersection;
+  }
 
-	public void setSourceSubjectNames(String[] srcSubjs)
-	{
-		this.src_subjects = new HashSet(Arrays.asList(srcSubjs));
-	}
+  public void setSourceSubjectNames(String[] srcSubjs)
+  {
+    this.src_subjects = new HashSet(Arrays.asList(srcSubjs));
+  }
 
-	public void setSourceMaskStrings(String[] srcMasks) throws AdminException
-	{
-		this.src_masks = new MaskList(srcMasks);
-	}
+  public void setSourceMaskStrings(String[] srcMasks) throws AdminException
+  {
+    this.src_masks = new MaskList(srcMasks);
+  }
 
-	public void setDestinationSubjectNames(String[] dstSubjs)
-	{
-		this.dst_subjects = new HashSet(Arrays.asList(dstSubjs));
-	}
+  public void setDestinationSubjectNames(String[] dstSubjs)
+  {
+    this.dst_subjects = new HashSet(Arrays.asList(dstSubjs));
+  }
 
-	public void setDestinationMaskStrings(String[] dstMasks) throws AdminException
-	{
-		this.dst_masks = new MaskList(dstMasks);
-	}
+  public void setDestinationMaskStrings(String[] dstMasks) throws AdminException
+  {
+    this.dst_masks = new MaskList(dstMasks);
+  }
 
-	public void setSmeIds(String[] smes)
-	{
-		this.smes = Arrays.asList(smes);
-	}
+  public void setSmeIds(String[] smes)
+  {
+    this.smes = Arrays.asList(smes);
+  }
 
-	public void setIntersection(boolean intersection)
-	{
-		this.intersection = intersection;
-	}
+  public void setIntersection(boolean intersection)
+  {
+    this.intersection = intersection;
+  }
 }
--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/util/tables/impl/subject/SubjectFilter.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/util/tables/impl/subject/SubjectFilter.java
@@ -7,6 +7,7 @@ package ru.novosoft.smsc.jsp.util.tables.impl.subject;
 
 import ru.novosoft.smsc.admin.AdminException;
 import ru.novosoft.smsc.admin.route.MaskList;
+import ru.novosoft.smsc.admin.route.Mask;
 import ru.novosoft.smsc.jsp.util.tables.DataItem;
 import ru.novosoft.smsc.jsp.util.tables.Filter;
 
@@ -15,86 +16,92 @@ import java.util.*;
 
 public class SubjectFilter implements Filter
 {
-	private Set names = new HashSet();
-	private List smes = new LinkedList();
-	private MaskList masks = new MaskList();
+  private Set names = new HashSet();
+  private List smes = new LinkedList();
+  private MaskList masks = new MaskList();
 
-	public SubjectFilter()
-	{
-	}
+  public SubjectFilter()
+  {
+  }
 
-	public SubjectFilter(Set names, List smeIds, String masks)
-	{
-		this.names = names;
-		this.smes = smeIds;
-		this.masks = new MaskList(masks);
-	}
+  public SubjectFilter(Set names, List smeIds, String masks)
+  {
+    this.names = names;
+    this.smes = smeIds;
+    this.masks = new MaskList(masks);
+  }
 
-	public boolean isEmpty()
-	{
-		return this.names.isEmpty() && this.smes.isEmpty() && this.masks.isEmpty();
-	}
+  public boolean isEmpty()
+  {
+    return this.names.isEmpty() && this.smes.isEmpty() && this.masks.isEmpty();
+  }
 
-	private boolean isMaskAllowed(String mask)
-	{
-		for (Iterator i = masks.getNames().iterator(); i.hasNext();)
-		{
-			if (mask.startsWith((String) i.next()))
-				return true;
-		}
-		return false;
-	}
+  private boolean isMaskAllowed(String maskStr)
+  {
+    final Mask mask;
+    try {
+      mask = new Mask(maskStr);
+    } catch (AdminException e) {
+      return false;
+    }
 
-	private boolean isAnyMaskAllowed(Vector item_masks)
-	{
-		for (Iterator i = item_masks.iterator(); i.hasNext();)
-		{
-			if (isMaskAllowed((String) i.next()))
-				return true;
-		}
-		return false;
-	}
+    for (Iterator i = masks.iterator(); i.hasNext();) {
+      final Mask m = (Mask) i.next();
+      if (m.addressConfirm(mask))
+        return true;
+    }
+    return false;
+  }
 
-	public boolean isItemAllowed(DataItem item)
-	{
-		if (isEmpty())
-			return true;
+  private boolean isAnyMaskAllowed(Vector item_masks)
+  {
+    for (Iterator i = item_masks.iterator(); i.hasNext();) {
+      if (isMaskAllowed((String) i.next()))
+        return true;
+    }
+    return false;
+  }
 
-		String item_name = (String) item.getValue("Name");
-		String item_sme = (String) item.getValue("Default SME");
-		Vector item_masks = (Vector) item.getValue("Masks");
-		return (names.isEmpty() || names.contains(item_name))
-				  && (smes.isEmpty() || smes.contains(item_sme))
-				  && (masks.isEmpty() || isAnyMaskAllowed(item_masks));
-	}
+  public boolean isItemAllowed(DataItem item)
+  {
+    if (isEmpty())
+      return true;
 
-	public List getMaskStrings()
-	{
-		return masks.getNames();
-	}
+    String item_name = (String) item.getValue("Name");
+    String item_sme = (String) item.getValue("Default SME");
+    Vector item_masks = (Vector) item.getValue("Masks");
+    return (names.isEmpty() || names.contains(item_name))
+            && (smes.isEmpty() || smes.contains(item_sme))
+            && (masks.isEmpty() || isAnyMaskAllowed(item_masks));
+  }
 
-	public Set getNames()
-	{
-		return names;
-	}
+  public List getMaskStrings()
+  {
+    return masks.getNames();
+  }
 
-	public List getSmeIds()
-	{
-		return smes;
-	}
+  public Set getNames()
+  {
+    return names;
+  }
 
-	public void setMasks(String[] newMasks) throws AdminException
-	{
-		this.masks = new MaskList(newMasks);
-	}
+  public List getSmeIds()
+  {
+    return smes;
+  }
 
-	public void setNames(String[] newNames)
-	{
-		this.names = new HashSet(Arrays.asList(newNames));
-	}
+  public void setMasks(String[] newMasks) throws AdminException
+  {
+    this.masks = new MaskList(newMasks);
+  }
 
-	public void setSmes(String[] newSmes)
-	{
-		this.smes = Arrays.asList(newSmes);
-	}
+  public void setNames(String[] newNames)
+  {
+    this.names = new HashSet(Arrays.asList(newNames));
+  }
+
+  public void setSmes(String[] newSmes)
+  {
+    this.smes = Arrays.asList(newSmes);
+  }
 }
