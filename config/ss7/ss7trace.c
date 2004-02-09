b4f94b55651cf68b495c5c217855972723ca86fe
vic <>
2004-02-09 15:54:49 +0000

    *** empty log message ***

--- config/ss7/ss7trace.c
+++ config/ss7/ss7trace.c
@@ -26,13 +26,14 @@ modesc moduledesc[] = {{"TCAP",   4    },
 		       {"ISUP",   12   },
                        {"MTPL2",  13   },
                        {"ETSIMAP",107  },
+                       {"FEIF01", 67   },
                        {"",       65535}};
 
-enum {UNBOUND,BOUND,SIDLE,SINIT,SRUN,LINKRUN} status;
+enum {UNBOUND,BOUND,RUNNING,TRACEOK} status;
 
 void usage() {
-  printf("Usage:   ss7run cfgname modname [modname...]\n");
-  printf("Example: ss7run ss7.cnf ETSIMAP TCAP ISUP SCCP MTPL3 MTPL2\n");
+  printf("Usage:   ss7trace modname traceon|traceoff\n");
+  printf("Example: ss7trace ETSIMAP traceoff\n");
 }
 
 int verify(int argc, char* argv[]) {
@@ -40,23 +41,25 @@ int verify(int argc, char* argv[]) {
   USHORT_T res = 0;
   USHORT_T i,j,id;
 
-  if (argc < 3) {
+  if (argc != 3) {
     usage();
     return -1;
   }
 
-  for(i=0; i< argc - 2; i++) {
-    for(j = 0; j < sizeof(moduledesc)/sizeof(modesc); j++) {
-      id = moduledesc[j].id;
-      if (strcasecmp(argv[i+2],moduledesc[j].name) == 0) break;
-    }
-    if (id == 65535) {
-      fprintf(stderr,"FAILED: unsupported module %s\n",argv[i+2]);
-      res = -1;
-      break;
-    }
+  for(j = 0; j < sizeof(moduledesc)/sizeof(modesc); j++) {
+    id = moduledesc[j].id;
+    if (strcasecmp(argv[1],moduledesc[j].name) == 0) break;
+  }
+  if (id == 65535) {
+    fprintf(stderr,"FAILED: unsupported module %s\n",argv[1]);
+    return -1;
   }
 
+  if (strcasecmp(argv[2],"traceon") != 0 &&
+      strcasecmp(argv[2],"traceoff") !=0) {
+    fprintf(stderr,"FAILED: unsupported operation %s\n",argv[2]);
+    return -1;
+  }
   return res;
 }
 int main(int argc, char* argv[]) {
@@ -72,7 +75,7 @@ int main(int argc, char* argv[]) {
     return res;
   }
 
-  if ((res = ss7run(argv[1],argc-2,&argv[2])) != 0) {
+  if ((res = ss7trace(argv[1],argv[2])) != 0) {
     deinitcp();
     return res;
   }
@@ -83,17 +86,17 @@ int initcp() {
   USHORT_T res;
   if ( (res = EINSS7CpMsgInitNoSig(QUEUE_SIZE)) != MSG_INIT_OK) {
     fprintf(stderr, "FAILED: initializing message handling. code %hu \n", res);
-    return -1;
+    return res;
   }
 
   if ( (res = MsgOpen(USERID)) != MSG_OPEN_OK) {
     fprintf(stderr, "FAILED: opening message port for user %d. code %hu \n", USERID, res);
-    return -1;
+    return res;
   }
 
   if( (res = MsgConn( USERID, MGMT_ID)) != RETURN_OK) {
     fprintf(stderr, "FAILED: connecting to user %d. code %hu\n", MGMT_ID, res);
-    return -1;
+    return res;
   }
   return 0;
 }
@@ -106,7 +109,7 @@ void deinitcp() {
 }
 
 
-int ss7run(char* cfg, USHORT_T modnum, char* argv[]) {
+int ss7trace(char* modname, char* operation) {
 
   USHORT_T res;
   USHORT_T i,j,id;
@@ -116,7 +119,7 @@ int ss7run(char* cfg, USHORT_T modnum, char* argv[]) {
   res = EINSS7_MgmtApiSendBindReq(USERID,MGMT_ID,6,WAIT);
   if (res != EINSS7_MGMTAPI_RETURN_OK) {
     fprintf(stderr, "FAILED: binding to user %d. code %hu\n", MGMT_ID, res);
-    return -1;
+    return res;
   }
 
   if (status != BOUND) {
@@ -135,75 +138,38 @@ int ss7run(char* cfg, USHORT_T modnum, char* argv[]) {
 
   if (res != EINSS7_MGMTAPI_RETURN_OK) {
     fprintf(stderr, "FAILED: ss7 stack state query. code %hu\n", res);
-    return -1;
+    return res ;
   }
 
-  if (status != SIDLE) {
-    fprintf(stderr, "FAILED: ss7 stack isn't IDLE.\n");
+  if (status != RUNNING) {
+    fprintf(stderr, "FAILED: ss7 stack isn't RUNNING.\n");
     return -1 ;
   }
 
   /* Проводим инициализацию модулей */
 
-  modids = malloc(modnum*2);
-  for(i=0; i< modnum; i++) {
-    for(j = 0; j < sizeof(moduledesc)/sizeof(modesc); j++) {
-      id = moduledesc[j].id;
-      if (strcasecmp(argv[i],moduledesc[j].name) == 0) break;
-    }
-    modids[i*2] = (UCHAR_T)(id & 0xFF);
-    modids[i*2+1] = (UCHAR_T)(id >>8);
+  for(j = 0; j < sizeof(moduledesc)/sizeof(modesc); j++) {
+    id = moduledesc[j].id;
+    if (strcasecmp(modname,moduledesc[j].name) == 0) break;
   }
 
-  res = EINSS7_MgmtApiSendInitReq(USERID, /*senderID*/
-                                  MGMT_ID, /*receiverID*/
-                                  strlen(cfg)+1, /*lengthOfFilename,*/
-                                  cfg, /* fileName_p*/
-                                  modnum, /*noOfModules*/
-                                  modids,
-                                  WAIT);
-  free(modids);
-  if (res != EINSS7_MGMTAPI_RETURN_OK) {
-    fprintf(stderr, "FAILED: ss7 stack init. code %hu\n", res);
-    return -1;
-  }
-
-  if (status != SINIT) {
-    fprintf(stderr, "FAILED: ss7 stack init.\n");
-    return -1 ;
-  }
-
-  res = EINSS7_MgmtApiSendStartReq(USERID, /*senderID*/
-                                   MGMT_ID, /*receiverID*/
-                                   WAIT);
-
-  if (res != EINSS7_MGMTAPI_RETURN_OK) {
-    fprintf(stderr, "FAILED: ss7 stack start. code %hu\n", res);
-    return -1;
-  }
-
-  if (status != SRUN) {
-    fprintf(stderr, "FAILED: ss7 stack start.\n");
-    return -1;
-  }
   res = EINSS7_MgmtApiSendOrderReq(USERID, /*senderID*/
                                    MGMT_ID, /*receiverID*/
-                                   6, /*moduleID = MTPL3*/
-                                   11, /*orderID = start all links*/
+                                   id, /*moduleID*/
+                                   strcasecmp(operation,"traceoff")?3:4, /*orderID = start all links*/
                                    0, /*lengthOfInfo*/
                                    NULL, /* orderInfo_p*/
                                    WAIT);
 
   if (res != EINSS7_MGMTAPI_RETURN_OK) {
-    fprintf(stderr, "FAILED: start all links. code %hu\n", res);
-    return -1;
+    fprintf(stderr, "FAILED: trace order request. code %hu\n", res);
+    return res ;
   }
 
-  if (status != LINKRUN) {
-    fprintf(stderr, "FAILED: start all links.\n");
+  if (status != TRACEOK) {
+    fprintf(stderr, "FAILED: trace order request.\n");
     return -1 ;
   }
-  return 0;
 }
 
 /*********************************************************************/
@@ -243,7 +209,7 @@ USHORT_T EINSS7_MgmtApiHandleMgmtConf(UCHAR_T typeOfService,
     default: printf("UNKNOWN typeOfService\n");
   };
 */
-  if (status == BOUND && typeOfService == 9 && *data_p == 0) status = SIDLE;
+  if (status == BOUND && typeOfService == 9 && *data_p == 4) status = RUNNING;
   return MSG_OK;
 }
                                              
@@ -251,35 +217,39 @@ USHORT_T EINSS7_MgmtApiHandleInitConf(USHORT_T moduleId,
                                       UCHAR_T result,
                                       ULONG_T offset)
 {
+  printf("EINSS7_MgmtApiHandleInitConf\n");
 /*
   printf("INIT %s MODULE=%d OFFSET=%d\n",
           getResultDescription(result),
           moduleId,
           offset);
-*/
+
   if (status == SIDLE && result == 0) {
     status = SINIT;
   }
   else {
     fprintf(stderr, "FAILED: ss7 stack init for module %d file offset %d\n", moduleId,offset);
   }
+*/
   return MSG_OK;
 }
 
 USHORT_T EINSS7_MgmtApiHandleStartConf(USHORT_T moduleId,
                                        UCHAR_T result)
 {
+  printf("EINSS7_MgmtApiHandleStartConf\n");
 /*
   printf("START %s MODULE=%d\n",
           getResultDescription(result),
           moduleId);
-*/
+
   if (status == SINIT && result == 0) {
     status = SRUN;
   }
   else {
     fprintf(stderr, "FAILED: ss7 stack start for module %d\n", moduleId);
   }
+*/
   return MSG_OK;
 }
 
@@ -317,9 +287,9 @@ USHORT_T EINSS7_MgmtApiHandleOrderConf(USHORT_T moduleId,
           buf);
   free(buf);
 */
-  if (status == SRUN && 
-      orderId == 11  && 
-      orderResult == 0) status = LINKRUN;
+  if (status == RUNNING && 
+      (orderId == 3 || orderId == 4)  && 
+      orderResult == 0) status = TRACEOK;
   return MSG_OK;
 }
 
