a7f181931ed13c14450559ca75e8a34b2c13bcd8
igork <>
2004-02-24 11:42:01 +0000

    implement locale codepage for xml

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/route/RouteSubjectManagerImpl.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/route/RouteSubjectManagerImpl.java
@@ -21,108 +21,121 @@ import java.io.*;
 import java.util.Iterator;
 
 
-public class RouteSubjectManagerImpl implements RouteSubjectManager {
-    private RouteList routes = null;
-    private SubjectList subjects = null;
-    private Category logger = Category.getInstance(this.getClass());
-    private SmeManager smeManager = null;
+public class RouteSubjectManagerImpl implements RouteSubjectManager
+{
+  private RouteList routes = null;
+  private SubjectList subjects = null;
+  private Category logger = Category.getInstance(this.getClass());
+  private SmeManager smeManager = null;
 
-    private final static String SMSC_ROUTES_PRIMARY_CONFIG = "routes.xml";
-    private final static String SMSC_ROUTES_TEMPORAL_CONFIG = "routes_.xml";
-    private final static String SMSC_ROUTES_TRACEABLE_CONFIG = "routes__.xml";
+  private final static String SMSC_ROUTES_PRIMARY_CONFIG = "routes.xml";
+  private final static String SMSC_ROUTES_TEMPORAL_CONFIG = "routes_.xml";
+  private final static String SMSC_ROUTES_TRACEABLE_CONFIG = "routes__.xml";
 
-    public RouteSubjectManagerImpl(SmeManager smeManager) throws AdminException {
-        this.smeManager = smeManager;
-        load();
-        trace();
+  public RouteSubjectManagerImpl(SmeManager smeManager) throws AdminException
+  {
+    this.smeManager = smeManager;
+    load();
+    trace();
+  }
+
+  public RouteList getRoutes()
+  {
+    return routes;
+  }
+
+  public SubjectList getSubjects()
+  {
+    return subjects;
+  }
+
+  public boolean isSmeUsed(String smeId)
+  {
+    for (Iterator i = routes.iterator(); i.hasNext();) {
+      Route route = (Route) i.next();
+      if (route.getDestinations().isSmeUsed(smeId))
+        return true;
     }
+    return false;
+  }
 
-    public RouteList getRoutes() {
-        return routes;
+  synchronized public boolean hasSavedConfiguration()
+  {
+    final File smscConfFolder = WebAppFolders.getSmscConfFolder();
+    File config = new File(smscConfFolder, SMSC_ROUTES_TEMPORAL_CONFIG);
+    return config.exists();
+  }
+
+  synchronized public void loadFromFile(String fileName) throws AdminException
+  {
+    try {
+      final File smscConfFolder = WebAppFolders.getSmscConfFolder();
+      File config = new File(smscConfFolder, fileName);
+
+      Document routesDoc = Utils.parse(config.getAbsolutePath());
+      subjects = new SubjectList(routesDoc.getDocumentElement(), smeManager);
+      routes = new RouteList(routesDoc.getDocumentElement(), subjects, smeManager);
+    } catch (FactoryConfigurationError error) {
+      logger.error("Couldn't configure xml parser factory", error);
+      throw new AdminException("Couldn't configure xml parser factory: " + error.getMessage());
+    } catch (ParserConfigurationException e) {
+      logger.error("Couldn't configure xml parser", e);
+      throw new AdminException("Couldn't configure xml parser: " + e.getMessage());
+    } catch (SAXException e) {
+      logger.error("Couldn't parse", e);
+      throw new AdminException("Couldn't parse: " + e.getMessage());
+    } catch (IOException e) {
+      logger.error("Couldn't perform IO operation", e);
+      throw new AdminException("Couldn't perform IO operation: " + e.getMessage());
+    } catch (NullPointerException e) {
+      logger.error("Couldn't parse", e);
+      throw new AdminException("Couldn't parse: " + e.getMessage());
     }
+  }
 
-    public SubjectList getSubjects() {
-        return subjects;
+  private void saveToFile(String filename) throws AdminException
+  {
+    try {
+      final File file = new File(WebAppFolders.getSmscConfFolder(), filename);
+      final File newFile = Functions.createNewFilenameForSave(file);
+      PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(newFile), Functions.getLocaleEncoding()));
+      Functions.storeConfigHeader(out, "routes", "routes.dtd", Functions.getLocaleEncoding());
+      subjects.store(out);
+      routes.store(out);
+      Functions.storeConfigFooter(out, "routes");
+      out.flush();
+      out.close();
+      Functions.renameNewSavedFileToOriginal(newFile, file);
+    } catch (FileNotFoundException e) {
+      throw new AdminException("Couldn't save new routes settings: Couldn't write to destination config filename: " + e.getMessage());
+    } catch (IOException e) {
+      logger.error("Couldn't save new routes settings", e);
+      throw new AdminException("Couldn't save new routes settings: " + e.getMessage());
     }
+  }
 
-    public boolean isSmeUsed(String smeId) {
-        for (Iterator i = routes.iterator(); i.hasNext();) {
-            Route route = (Route) i.next();
-            if (route.getDestinations().isSmeUsed(smeId))
-                return true;
-        }
-        return false;
-    }
+  synchronized public void save() throws AdminException
+  {
+    saveToFile(SMSC_ROUTES_TEMPORAL_CONFIG);
+  }
 
-    synchronized public boolean hasSavedConfiguration() {
-        final File smscConfFolder = WebAppFolders.getSmscConfFolder();
-        File config = new File(smscConfFolder, SMSC_ROUTES_TEMPORAL_CONFIG);
-        return config.exists();
-    }
+  synchronized public void restore() throws AdminException
+  {
+    loadFromFile(SMSC_ROUTES_TEMPORAL_CONFIG);
+  }
 
-    synchronized public void loadFromFile(String fileName) throws AdminException {
-        try {
-            final File smscConfFolder = WebAppFolders.getSmscConfFolder();
-            File config = new File(smscConfFolder, fileName);
+  synchronized public void apply() throws AdminException
+  {
+    saveToFile(SMSC_ROUTES_PRIMARY_CONFIG);
+  }
 
-            Document routesDoc = Utils.parse(config.getAbsolutePath());
-            subjects = new SubjectList(routesDoc.getDocumentElement(), smeManager);
-            routes = new RouteList(routesDoc.getDocumentElement(), subjects, smeManager);
-        } catch (FactoryConfigurationError error) {
-            logger.error("Couldn't configure xml parser factory", error);
-            throw new AdminException("Couldn't configure xml parser factory: " + error.getMessage());
-        } catch (ParserConfigurationException e) {
-            logger.error("Couldn't configure xml parser", e);
-            throw new AdminException("Couldn't configure xml parser: " + e.getMessage());
-        } catch (SAXException e) {
-            logger.error("Couldn't parse", e);
-            throw new AdminException("Couldn't parse: " + e.getMessage());
-        } catch (IOException e) {
-            logger.error("Couldn't perform IO operation", e);
-            throw new AdminException("Couldn't perform IO operation: " + e.getMessage());
-        } catch (NullPointerException e) {
-            logger.error("Couldn't parse", e);
-            throw new AdminException("Couldn't parse: " + e.getMessage());
-        }
-    }
+  synchronized public void load() throws AdminException
+  {
+    loadFromFile(SMSC_ROUTES_PRIMARY_CONFIG);
+  }
 
-    private void saveToFile(String filename) throws AdminException {
-        try {
-            final File file = new File(WebAppFolders.getSmscConfFolder(), filename);
-            final File newFile = Functions.createNewFilenameForSave(file);
-            PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(newFile), Functions.getLocaleEncoding()));
-            Functions.storeConfigHeader(out, "routes", "routes.dtd", Functions.getLocaleEncoding());
-            subjects.store(out);
-            routes.store(out);
-            Functions.storeConfigFooter(out, "routes");
-            out.flush();
-            out.close();
-            Functions.renameNewSavedFileToOriginal(newFile, file);
-        } catch (FileNotFoundException e) {
-            throw new AdminException("Couldn't save new routes settings: Couldn't write to destination config filename: " + e.getMessage());
-        } catch (IOException e) {
-            logger.error("Couldn't save new routes settings", e);
-            throw new AdminException("Couldn't save new routes settings: " + e.getMessage());
-        }
-    }
-
-    synchronized public void save() throws AdminException {
-        saveToFile(SMSC_ROUTES_TEMPORAL_CONFIG);
-    }
-
-    synchronized public void restore() throws AdminException {
-        loadFromFile(SMSC_ROUTES_TEMPORAL_CONFIG);
-    }
-
-    synchronized public void apply() throws AdminException {
-        saveToFile(SMSC_ROUTES_PRIMARY_CONFIG);
-    }
-
-    synchronized public void load() throws AdminException {
-        loadFromFile(SMSC_ROUTES_PRIMARY_CONFIG);
-    }
-
-    synchronized public void trace() throws AdminException {
-        saveToFile(SMSC_ROUTES_TRACEABLE_CONFIG);
-    }
+  synchronized public void trace() throws AdminException
+  {
+    saveToFile(SMSC_ROUTES_TRACEABLE_CONFIG);
+  }
 }
