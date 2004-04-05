573a1a99753659787c702a4cf423530cc80fb61d
igork <>
2004-04-05 09:13:54 +0000

    getAliasByAddress method added

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/alias/AliasSet.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/alias/AliasSet.java
@@ -16,122 +16,109 @@ import ru.novosoft.smsc.jsp.util.tables.impl.alias.AliasQuery;
 import ru.novosoft.smsc.util.StringEncoderDecoder;
 
 import java.io.PrintWriter;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 
 public class AliasSet
 {
-	private Set aliases = new HashSet();
-	private AliasDataSource dataSource = new AliasDataSource();
-	private Category logger = Category.getInstance(this.getClass());
+  private Set aliases = new HashSet();
+  private AliasDataSource dataSource = new AliasDataSource();
+  private Category logger = Category.getInstance(this.getClass());
 
-	public AliasSet()
-	{
-	}
+  public AliasSet()
+  {
+  }
 
-	public AliasSet(Element aliasesElem)
-	{
-		NodeList aliasNodes = aliasesElem.getElementsByTagName("record");
-		for (int i = 0; i < aliasNodes.getLength(); i++)
-		{
-			Element aliasElem = (Element) aliasNodes.item(i);
-			try
-			{
-				add(new Alias(new Mask(aliasElem.getAttribute("addr")), new Mask(aliasElem.getAttribute("alias")), aliasElem.getAttribute("hide").equalsIgnoreCase("true")));
-			}
-			catch (AdminException e)
-			{
-				logger.error("Couldn't load alias \"" + aliasElem.getAttribute("alias") + "\"-->\"" + aliasElem.getAttribute("addr") + "\", skipped", e);
-			}
-		}
-	}
+  public AliasSet(Element aliasesElem)
+  {
+    NodeList aliasNodes = aliasesElem.getElementsByTagName("record");
+    for (int i = 0; i < aliasNodes.getLength(); i++) {
+      Element aliasElem = (Element) aliasNodes.item(i);
+      try {
+        add(new Alias(new Mask(aliasElem.getAttribute("addr")), new Mask(aliasElem.getAttribute("alias")), aliasElem.getAttribute("hide").equalsIgnoreCase("true")));
+      } catch (AdminException e) {
+        logger.error("Couldn't load alias \"" + aliasElem.getAttribute("alias") + "\"-->\"" + aliasElem.getAttribute("addr") + "\", skipped", e);
+      }
+    }
+  }
 
-	public PrintWriter store(PrintWriter out)
-	{
-		for (Iterator i = iterator(); i.hasNext();)
-		{
-			Alias a = (Alias) i.next();
-			out.println("  <record addr=\"" + StringEncoderDecoder.encode(a.getAddress().getMask()) + "\" alias=\"" + StringEncoderDecoder.encode(a.getAlias().getMask()) + "\" hide=\"" + (a.isHide() ? "true" : "false") + "\"/>");
-		}
-		return out;
-	}
+  public PrintWriter store(PrintWriter out)
+  {
+    for (Iterator i = iterator(); i.hasNext();) {
+      Alias a = (Alias) i.next();
+      out.println("  <record addr=\"" + StringEncoderDecoder.encode(a.getAddress().getMask()) + "\" alias=\"" + StringEncoderDecoder.encode(a.getAlias().getMask()) + "\" hide=\"" + (a.isHide() ? "true" : "false") + "\"/>");
+    }
+    return out;
+  }
 
-	public boolean add(Alias new_alias)
-	{
-		if (aliases.contains(new_alias))
-			return false;
+  public boolean add(Alias new_alias)
+  {
+    if (aliases.contains(new_alias))
+      return false;
 
-		if (new_alias.isHide())
-			for (Iterator i = aliases.iterator(); i.hasNext();)
-			{
-				Alias alias = (Alias) i.next();
-				if (alias.isHide() && alias.getAddress().equals(new_alias.getAddress()))
-					return false;
-			}
+    if (new_alias.isHide())
+      for (Iterator i = aliases.iterator(); i.hasNext();) {
+        Alias alias = (Alias) i.next();
+        if (alias.isHide() && alias.getAddress().equals(new_alias.getAddress()))
+          return false;
+      }
 
-		dataSource.add(new_alias);
-		return aliases.add(new_alias);
-	}
+    dataSource.add(new_alias);
+    return aliases.add(new_alias);
+  }
 
-	public Iterator iterator()
-	{
-		return aliases.iterator();
-	}
+  public Iterator iterator()
+  {
+    return aliases.iterator();
+  }
 
-	public boolean remove(Alias a)
-	{
-		dataSource.remove(a);
-		return aliases.remove(a);
-	}
+  public boolean remove(Alias a)
+  {
+    dataSource.remove(a);
+    return aliases.remove(a);
+  }
 
-	public boolean remove(String alias)
-	{
-		try
-		{
-			Alias a = new Alias(new Mask(alias), new Mask(alias), false);
-			dataSource.remove(a);
-			return aliases.remove(a);
-		}
-		catch (AdminException e)
-		{
-			logger.error("Couldn't remove alias \"" + alias + '"', e);
-			return false;
-		}
-	}
+  public boolean remove(String alias)
+  {
+    try {
+      Alias a = new Alias(new Mask(alias), new Mask(alias), false);
+      dataSource.remove(a);
+      return aliases.remove(a);
+    } catch (AdminException e) {
+      logger.error("Couldn't remove alias \"" + alias + '"', e);
+      return false;
+    }
+  }
 
-	public QueryResultSet query(AliasQuery query)
-	{
-		dataSource.clear();
-		for (Iterator i = aliases.iterator(); i.hasNext();)
-		{
-			Alias alias = (Alias) i.next();
-			dataSource.add(alias);
-		}
-		return dataSource.query(query);
-	}
+  public QueryResultSet query(AliasQuery query)
+  {
+    dataSource.clear();
+    for (Iterator i = aliases.iterator(); i.hasNext();) {
+      Alias alias = (Alias) i.next();
+      dataSource.add(alias);
+    }
+    return dataSource.query(query);
+  }
 
-	public boolean contains(Alias a)
-	{
-		return aliases.contains(a);
-	}
+  public boolean contains(Alias a)
+  {
+    return aliases.contains(a);
+  }
 
-	public Alias get(String aliasString)
-	{
-		for (Iterator i = aliases.iterator(); i.hasNext();)
-		{
-			Alias alias = (Alias) i.next();
-			if (alias.getAlias().getMask().equals(aliasString))
-				return alias;
-		}
-		return null;
-	}
+  public Alias get(String aliasString)
+  {
+    for (Iterator i = aliases.iterator(); i.hasNext();) {
+      Alias alias = (Alias) i.next();
+      if (alias.getAlias().getMask().equals(aliasString))
+        return alias;
+    }
+    return null;
+  }
 
-	public int size()
-	{
-		return aliases.size();
-	}
+  public int size()
+  {
+    return aliases.size();
+  }
 
   public boolean isContainsAlias(Mask aliasMask)
   {
@@ -141,5 +128,22 @@ public class AliasSet
         return true;
     }
     return false;
+  }
+
+  public Alias getAliasByAddress(Mask address)
+  {
+    Alias result = null;
+    for (Iterator i = aliases.iterator(); i.hasNext();) {
+      Alias alias = (Alias) i.next();
+      if (alias.isHide()) {
+        if (alias.getAddress().addressConfirm(address)) {
+          if (result == null)
+            result = alias;
+          else if (alias.getAddress().getQuestionsCount() < result.getAddress().getQuestionsCount())
+            result = alias;
+        }
+      }
+    }
+    return result;
   }
 }
