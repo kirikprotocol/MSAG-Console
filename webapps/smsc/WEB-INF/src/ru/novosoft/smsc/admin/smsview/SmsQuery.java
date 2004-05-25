155e9342a1d6f29c48298c37a6790e16879eda28
igork <>
2004-05-25 12:03:50 +0000

    From Date set to now in constructor

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsview/SmsQuery.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsview/SmsQuery.java
@@ -9,6 +9,8 @@ package ru.novosoft.smsc.admin.smsview;
  * @version 1.0
  */
 
+import ru.novosoft.smsc.util.Functions;
+
 import java.util.Date;
 
 public class SmsQuery
@@ -16,56 +18,172 @@ public class SmsQuery
   public final static int SMS_OPERATIVE_STORAGE_TYPE = 100;
   public final static int SMS_ARCHIVE_STORAGE_TYPE = 200;
 
-  private int    storageType = SMS_ARCHIVE_STORAGE_TYPE;
-  private int    rowsMaximum = 500;
+  private int storageType = SMS_ARCHIVE_STORAGE_TYPE;
+  private int rowsMaximum = 500;
   private String abonentAddress = "*";
-  private String fromAddress    = "*";
-  private String toAddress      = "*";
-  private String smeId    = "*";
+  private String fromAddress = "*";
+  private String toAddress = "*";
+  private String smeId = "*";
   private String srcSmeId = "*";
   private String dstSmeId = "*";
   private String routeId = "*";
   private String smsId = "*";
-  private Date   fromDate = new Date();
-  private Date   tillDate = new Date();
+  private Date fromDate = Functions.truncateTime(new Date());
+  private Date tillDate = new Date();
   private String sortBy = "Date";
-  private boolean fromDateEnabled = false;
+  private boolean fromDateEnabled = true;
   private boolean tillDateEnabled = false;
 
-  public int getStorageType() { return storageType; }
-  public void setStorageType(int type) { storageType = type; }
-  public int getRowsMaximum() { return rowsMaximum; }
-  public void setRowsMaximum(int max) { rowsMaximum = max; }
+  public int getStorageType()
+  {
+    return storageType;
+  }
 
-  public String getAbonentAddress() { return abonentAddress; }
-  public void setAbonentAddress(String address) { abonentAddress = address; }
-  public String getFromAddress() { return fromAddress; }
-  public void setFromAddress(String address) { fromAddress = address; }
-  public String getToAddress() { return toAddress; }
-  public void setToAddress(String address) { toAddress = address; }
+  public void setStorageType(int type)
+  {
+    storageType = type;
+  }
 
-  public String getSmeId() { return smeId; }
-  public void setSmeId(String id) { smeId = id; }
-  public String getSrcSmeId() { return srcSmeId; }
-  public void setSrcSmeId(String id) { srcSmeId = id; }
-  public String getDstSmeId() { return dstSmeId; }
-  public void setDstSmeId(String id) { dstSmeId = id; }
-  public String getRouteId() { return routeId; }
-  public void setRouteId(String id) { routeId = id; }
-  public String getSmsId() { return smsId; }
-  public void setSmsId(String id) { smsId = id; }
+  public int getRowsMaximum()
+  {
+    return rowsMaximum;
+  }
 
-  public void setFromDateEnabled(boolean enabled) { fromDateEnabled = enabled; }
-  public boolean getFromDateEnabled() { return fromDateEnabled; }
-  public Date getFromDate() { return fromDate; }
-  public void setFromDate(Date date) { fromDate = date; }
+  public void setRowsMaximum(int max)
+  {
+    rowsMaximum = max;
+  }
 
-  public void setTillDateEnabled(boolean enabled) { tillDateEnabled = enabled; }
-  public boolean getTillDateEnabled() { return tillDateEnabled; }
-  public Date getTillDate() { return tillDate; }
-  public void setTillDate(Date date) { tillDate = date; }
+  public String getAbonentAddress()
+  {
+    return abonentAddress;
+  }
 
-  public String getSortBy() { return sortBy; }
-  public void setSortBy(String by) { sortBy = by; }
+  public void setAbonentAddress(String address)
+  {
+    abonentAddress = address;
+  }
 
-};
+  public String getFromAddress()
+  {
+    return fromAddress;
+  }
+
+  public void setFromAddress(String address)
+  {
+    fromAddress = address;
+  }
+
+  public String getToAddress()
+  {
+    return toAddress;
+  }
+
+  public void setToAddress(String address)
+  {
+    toAddress = address;
+  }
+
+  public String getSmeId()
+  {
+    return smeId;
+  }
+
+  public void setSmeId(String id)
+  {
+    smeId = id;
+  }
+
+  public String getSrcSmeId()
+  {
+    return srcSmeId;
+  }
+
+  public void setSrcSmeId(String id)
+  {
+    srcSmeId = id;
+  }
+
+  public String getDstSmeId()
+  {
+    return dstSmeId;
+  }
+
+  public void setDstSmeId(String id)
+  {
+    dstSmeId = id;
+  }
+
+  public String getRouteId()
+  {
+    return routeId;
+  }
+
+  public void setRouteId(String id)
+  {
+    routeId = id;
+  }
+
+  public String getSmsId()
+  {
+    return smsId;
+  }
+
+  public void setSmsId(String id)
+  {
+    smsId = id;
+  }
+
+  public void setFromDateEnabled(boolean enabled)
+  {
+    fromDateEnabled = enabled;
+  }
+
+  public boolean getFromDateEnabled()
+  {
+    return fromDateEnabled;
+  }
+
+  public Date getFromDate()
+  {
+    return fromDate;
+  }
+
+  public void setFromDate(Date date)
+  {
+    fromDate = date;
+  }
+
+  public void setTillDateEnabled(boolean enabled)
+  {
+    tillDateEnabled = enabled;
+  }
+
+  public boolean getTillDateEnabled()
+  {
+    return tillDateEnabled;
+  }
+
+  public Date getTillDate()
+  {
+    return tillDate;
+  }
+
+  public void setTillDate(Date date)
+  {
+    tillDate = date;
+  }
+
+  public String getSortBy()
+  {
+    return sortBy;
+  }
+
+  public void setSortBy(String by)
+  {
+    sortBy = by;
+  }
+
+}
+
+;
