b08c6ebfacfce531452f965fd3f8469d26479972
serge <>
2004-06-07 08:38:36 +0000

    *** empty log message ***

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/util/Version.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/util/Version.java
@@ -9,8 +9,8 @@ package ru.novosoft.smsc.util;
 public abstract class Version
 {
   public static final int VER_MAJOR = 1;
-  public static final int VER_MINOR = 0;
-  public static final int VER_BUILD = 1606;
+  public static final int VER_MINOR = 7;
+  public static final int VER_BUILD = 1704;
   public static final String VER_DATE = "Wed Nov 12 17:28:45 2003";
   public static final String VER_STR = "Sibinco SMSC Control Center ver " + VER_MAJOR + "." + VER_MINOR + " build " + VER_BUILD + " date " + VER_DATE;
 }
