8dfb4e91d779f5da9e2c60d6cd464eceb82a1173
hedgehog <>
2004-06-11 12:40:27 +0000

    *** empty log message ***

--- src/acls/interfaces.h
+++ src/acls/interfaces.h
@@ -12,12 +12,12 @@ typedef std::string    AclName;
 typedef std::string    AclDescription;
 typedef unsigned long  AclIdent;
 typedef const char*    AclPChar;
-typedef pair<AclIdent,AclName> AclNamedIdent; 
+typedef std::pair<AclIdent,AclName> AclNamedIdent; 
 
 enum AclCacheType { 
-  ACT_UNKNOWN,
-  ACT_DBSDIRECT,
-  ACT_FULLCACHE,
+  ACT_UNKNOWN   = '0',
+  ACT_DBSDIRECT = '1',
+  ACT_FULLCACHE = '2'
 };
 
 struct AclInfo
@@ -30,14 +30,23 @@ struct AclInfo
 
 struct AclEditor
 {
-  virtual void    enumerate(vector<AclNamedIdent>& result) = 0;
+  virtual void    enumerate(std::vector<AclNamedIdent>& result) = 0;
   virtual AclInfo getInfo(AclIdent) = 0;  
   virtual bool    remove(AclIdent) = 0;
-  virtual void    create(AclIdent,const char* aclname,const char* descr,const vector<AclPhoneNumber>& phones = vector<AclPhoneNumber>(),AclCacheType act = ACT_DBSDIRECT) = 0;
-  virtual bool    lookupByPrefix(AclIdent,const char* prefix,vector<AclPhoneNumber>&) = 0;
+  virtual void    create(
+                        AclIdent,
+                        const char* aclname,
+                        const char* descr,
+                        const std::vector<AclPhoneNumber>& phones = std::vector<AclPhoneNumber>(),
+                        AclCacheType act = ACT_DBSDIRECT) = 0;
+  virtual bool    lookupByPrefix(AclIdent,const char* prefix,std::vector<AclPhoneNumber>&) = 0;
   virtual bool    removePhone(AclIdent,const AclPhoneNumber&) = 0;
   virtual void    addPhone(AclIdent,const AclPhoneNumber&) = 0;
-  virtual void    updateAclInfo(AclIdent,const char* aclname,const char* acldesc,AclCacheType act = ACT_UNKNOWN) = 0;
+  virtual void    updateAclInfo(
+                        AclIdent,
+                        const char* aclname,
+                        const char* acldesc,
+                        AclCacheType act = ACT_UNKNOWN) = 0;
 };
 
 struct AclLookuper
