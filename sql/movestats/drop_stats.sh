53fd2480f9345949a4c8ba8d14ca13d5607f7644
serge <>
2004-07-16 08:59:13 +0000

    *** empty log message ***

---
+++ sql/movestats/drop_stats.sh
@@ -0,0 +1,12 @@
+if [ "$1" = "" ] ; then 
+  echo Usage: drop_stats.sh period connect_string
+  echo Examp: drop_stats.sh 20041019 \"smsc/smsc@ORCL\"
+  exit 1
+fi
+if [ "$2" = "" ] ; then 
+  echo Usage: drop_stats.sh period connect_string
+  echo Examp: drop_stats.sh 20041019 \"smsc/smsc@ORCL\"
+  exit 1
+fi
+sed -e s/--PERIOD--/$1/ stats_drop_template.sql > stats_drop.sql
+sqlplus $2 @stats_drop.sql
---
+++ sql/movestats/move_stats.sh
@@ -0,0 +1,12 @@
+if [ "$1" = "" ] ; then 
+  echo Usage: drop_stats.sh period connect_string
+  echo Examp: drop_stats.sh 20041019 \"smsc/smsc@ORCL\"
+  exit 1
+fi
+if [ "$2" = "" ] ; then 
+  echo Usage: drop_stats.sh period connect_string
+  echo Examp: drop_stats.sh 20041019 \"smsc/smsc@ORCL\"
+  exit 1
+fi
+sed -e s/--PERIOD--/$1/ stats_template.sql > stats.sql
+sqlplus $2 @stats.sql
