950a0ffd8b51596f9b5ac5417a9201ffe7d4ac01
serge <>
2004-07-16 09:16:13 +0000

    *** empty log message ***

--- sql/movestats/drop_stats.sh
+++ sql/movestats/drop_stats.sh
@@ -8,7 +8,7 @@ if [ "$2" = "" ] ; then
   echo Examp: drop_stats.sh 20041019 \"smsc/smsc@ORCL\"
   exit 1
 fi
-sed -e s/--PERIOD--/$1/ stats_drop_template.sql > stats_drop.sql
+sed -e s/--PERIOD--/$1/g stats_drop_template.sql > stats_drop.sql
 sqlplus $2 @stats_drop.sql
 
 rm -f stats_drop.sql
--- sql/movestats/move_stats.sh
+++ sql/movestats/move_stats.sh
@@ -8,7 +8,7 @@ if [ "$2" = "" ] ; then
   echo Examp: drop_stats.sh 20041019 \"smsc/smsc@ORCL\"
   exit 1
 fi
-sed -e s/--PERIOD--/$1/ stats_template.sql > stats.sql
+sed -e s/--PERIOD--/$1/g stats_template.sql > stats.sql
 sqlplus $2 @stats.sql
 
 rm -f stats.sql
