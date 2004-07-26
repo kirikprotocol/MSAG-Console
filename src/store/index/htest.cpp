37ea15485716d1a2ea3a88c010402e59b0d199c2
skv <>
2004-07-26 12:31:34 +0000

    test of disk hash

---
+++ src/store/index/htest.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "core/buffers/DiskHash.hpp"
+#include "SmsKeys.hpp"
+#include "logger/Logger.h"
+
+using namespace smsc::core::buffers;
+
+typedef DiskHash<Int64Key,IdLttKey> SmsIdDiskHash;
+
+int main(int argc,char* argv[])
+{
+  smsc::logger::Logger::Init();
+  SmsIdDiskHash h;
+  h.Create("test.dat",1000,true);
+  for(int i=0;i<1000000;i++)
+  {
+    if((i%1000)==0)
+    {
+      printf("%d\n",i);
+    }
+    h.Insert(i,IdLttKey(i,i));
+  }
+
+  return 0;
+}
