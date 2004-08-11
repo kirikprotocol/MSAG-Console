af24f3865392b1dfe12c78c955ddf48296baad78
serge <>
2004-08-11 11:18:12 +0000

    build 1707

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/util/Version.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/util/Version.java
@@ -10,7 +10,7 @@ public abstract class Version
 {
   public static final int VER_MAJOR = 1;
   public static final int VER_MINOR = 7;
-  public static final int VER_BUILD = 1706;
-  public static final String VER_DATE = "Tue Jul 13 15:23:34 2004";
+  public static final int VER_BUILD = 1707;
+  public static final String VER_DATE = "Wed Aug 11 18:18:14 2004";
   public static final String VER_STR = "Sibinco SMSC Control Center ver " + VER_MAJOR + "." + VER_MINOR + " build " + VER_BUILD + " date " + VER_DATE;
 }
