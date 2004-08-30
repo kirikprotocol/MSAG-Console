8b6509751beb4b9a5a40ff6cfde003d561d6d5f7
igork <>
2004-08-30 11:05:33 +0000

    Provider Id for users added

--- webapps/smsc_lib/src/ru/sibinco/lib/backend/users/User.java
+++ webapps/smsc_lib/src/ru/sibinco/lib/backend/users/User.java
@@ -9,9 +9,7 @@ import java.util.*;
 
 
 /**
- * Created by igork
- * Date: 03.03.2004
- * Time: 19:23:59
+ * Created by igork Date: 03.03.2004 Time: 19:23:59
  */
 public class User implements Principal
 {
@@ -30,13 +28,15 @@ public class User implements Principal
   private String homePhone = null;
   private String cellPhone = null;
   private String email = null;
+  private long providerId;
 
 
-  public User(String login, String password, String[] roles, String firstName, String lastName, String dept, String workPhone, String homePhone, String cellPhone, String email)
+  public User(final String login, final String password, final String[] roles, final String firstName, final String lastName, final String dept,
+              final String workPhone, final String homePhone, final String cellPhone, final String email, final long providerId)
   {
     this.login = login;
     this.password = password;
-    this.roles = new TreeSet(Arrays.asList(roles == null ? new String[0] : roles));
+    this.roles = new TreeSet(Arrays.asList(null == roles ? new String[0] : roles));
     this.firstName = firstName;
     this.lastName = lastName;
     this.dept = dept;
@@ -44,25 +44,26 @@ public class User implements Principal
     this.homePhone = homePhone;
     this.cellPhone = cellPhone;
     this.email = email;
+    this.providerId = providerId;
 
-    if (this.firstName == null) this.firstName = "";
-    if (this.lastName == null) this.lastName = "";
-    if (this.dept == null) this.dept = "";
-    if (this.workPhone == null) this.workPhone = "";
-    if (this.homePhone == null) this.homePhone = "";
-    if (this.cellPhone == null) this.cellPhone = "";
-    if (this.email == null) this.email = "";
+    if (null == this.firstName) this.firstName = "";
+    if (null == this.lastName) this.lastName = "";
+    if (null == this.dept) this.dept = "";
+    if (null == this.workPhone) this.workPhone = "";
+    if (null == this.homePhone) this.homePhone = "";
+    if (null == this.cellPhone) this.cellPhone = "";
+    if (null == this.email) this.email = "";
   }
 
-  public User(Element userElem)
+  public User(final Element userElem)
   {
     this.login = userElem.getAttribute("login");
     this.password = userElem.getAttribute("password");
 
     this.roles = new TreeSet();
-    NodeList roleList = userElem.getElementsByTagName("role");
+    final NodeList roleList = userElem.getElementsByTagName("role");
     for (int i = 0; i < roleList.getLength(); i++) {
-      Element roleElem = (Element) roleList.item(i);
+      final Element roleElem = (Element) roleList.item(i);
       this.roles.add(roleElem.getAttribute("name"));
     }
 
@@ -73,14 +74,19 @@ public class User implements Principal
     this.homePhone = userElem.getAttribute("homePhone");
     this.cellPhone = userElem.getAttribute("cellPhone");
     this.email = userElem.getAttribute("email");
+    try {
+      this.providerId = Long.decode(userElem.getAttribute("long providerId")).longValue();
+    } catch (NumberFormatException e) {
+      this.providerId = -1;
+    }
 
-    if (this.firstName == null) this.firstName = "";
-    if (this.lastName == null) this.lastName = "";
-    if (this.dept == null) this.dept = "";
-    if (this.workPhone == null) this.workPhone = "";
-    if (this.homePhone == null) this.homePhone = "";
-    if (this.cellPhone == null) this.cellPhone = "";
-    if (this.email == null) this.email = "";
+    if (null == this.firstName) this.firstName = "";
+    if (null == this.lastName) this.lastName = "";
+    if (null == this.dept) this.dept = "";
+    if (null == this.workPhone) this.workPhone = "";
+    if (null == this.homePhone) this.homePhone = "";
+    if (null == this.cellPhone) this.cellPhone = "";
+    if (null == this.email) this.email = "";
   }
 
   public String getName()
@@ -99,7 +105,7 @@ public class User implements Principal
    * @param o object to compare
    * @return
    */
-  public boolean equals(Object o)
+  public boolean equals(final Object o)
   {
     if (this == o) return true;
     if (!(o instanceof User)) return false;
@@ -128,9 +134,10 @@ public class User implements Principal
     result += " homePhone=\"" + StringEncoderDecoder.encode(homePhone) + '"';
     result += " cellPhone=\"" + StringEncoderDecoder.encode(cellPhone) + '"';
     result += " email=\"" + StringEncoderDecoder.encode(email) + '"';
+    result += " providerId=\"" + providerId + '"';
     result += ">\n";
     for (Iterator i = roles.iterator(); i.hasNext();) {
-      String role = (String) i.next();
+      final String role = (String) i.next();
       result += "\t\t<role name=\"" + StringEncoderDecoder.encode(role) + "\"/>\n";
     }
     result += "\t</user>\n";
@@ -143,7 +150,7 @@ public class User implements Principal
     return login;
   }
 
-  public void setLogin(String login)
+  public void setLogin(final String login)
   {
     this.login = login;
   }
@@ -153,7 +160,7 @@ public class User implements Principal
     return password;
   }
 
-  public void setPassword(String password)
+  public void setPassword(final String password)
   {
     this.password = password;
   }
@@ -163,9 +170,9 @@ public class User implements Principal
     return roles;
   }
 
-  public void setRoles(Collection roles)
+  public void setRoles(final Collection roles)
   {
-    this.roles = roles == null ? new TreeSet() : new TreeSet(roles);
+    this.roles = null == roles ? new TreeSet() : new TreeSet(roles);
   }
 
   public String getFirstName()
@@ -173,7 +180,7 @@ public class User implements Principal
     return firstName;
   }
 
-  public void setFirstName(String firstName)
+  public void setFirstName(final String firstName)
   {
     this.firstName = firstName;
   }
@@ -183,7 +190,7 @@ public class User implements Principal
     return lastName;
   }
 
-  public void setLastName(String lastName)
+  public void setLastName(final String lastName)
   {
     this.lastName = lastName;
   }
@@ -193,7 +200,7 @@ public class User implements Principal
     return dept;
   }
 
-  public void setDept(String dept)
+  public void setDept(final String dept)
   {
     this.dept = dept;
   }
@@ -203,7 +210,7 @@ public class User implements Principal
     return workPhone;
   }
 
-  public void setWorkPhone(String workPhone)
+  public void setWorkPhone(final String workPhone)
   {
     this.workPhone = workPhone;
   }
@@ -213,7 +220,7 @@ public class User implements Principal
     return homePhone;
   }
 
-  public void setHomePhone(String homePhone)
+  public void setHomePhone(final String homePhone)
   {
     this.homePhone = homePhone;
   }
@@ -223,7 +230,7 @@ public class User implements Principal
     return cellPhone;
   }
 
-  public void setCellPhone(String cellPhone)
+  public void setCellPhone(final String cellPhone)
   {
     this.cellPhone = cellPhone;
   }
@@ -233,19 +240,29 @@ public class User implements Principal
     return email;
   }
 
-  public void setEmail(String email)
+  public void setEmail(final String email)
   {
     this.email = email;
   }
 
-  public void grantRole(String roleName)
+  public void grantRole(final String roleName)
   {
     roles.add(roleName);
   }
 
-  public void revokeRole(String roleName)
+  public void revokeRole(final String roleName)
   {
     roles.remove(roleName);
+  }
+
+  public long getProviderId()
+  {
+    return providerId;
+  }
+
+  public void setProviderId(final long providerId)
+  {
+    this.providerId = providerId;
   }
 }
 
