d41f09cbe192f8af56a620ec137fba0320e6a4c9
serge <>
2004-09-08 08:45:40 +0000

    *** empty log message ***

---
+++ ext_smes/MCIProf/sql/oracle_drop.sql
@@ -0,0 +1,15 @@
+drop index results_question;
+drop index results_rdate;
+drop table results;
+
+drop sequence informmessages_seq;
+drop trigger informmessages_trigger;
+drop table informmessages;
+
+drop table dispatch;
+
+drop index smppstats_rdate;
+drop table smppstats;
+
+drop index sesstrack_rdate;
+drop table sesstrack;
