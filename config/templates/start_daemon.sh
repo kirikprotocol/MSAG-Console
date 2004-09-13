c1034f6a38427d9238dd0babe8051ced546bca48
serge <>
2004-09-13 09:04:19 +0000

    no message

--- config/templates/start_daemon.sh
+++ config/templates/start_daemon.sh
@@ -6,6 +6,7 @@ export LC_ALL=@LC_ALL@
 export LANG=@LC_ALL@
 export LANGUAGE=@LC_ALL@
 export NLS_LANG=@NLS_LANG@
+export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
 cd @daemon.dir@
 nohup bin/daemon.sh daemon.pid &
 echo $! > daemon.pid
