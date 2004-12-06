7ba59369ae7542ba6f8a07243cc17f6fd304ddb7
serge <>
2004-12-06 16:44:10 +0000

    *** empty log message ***

--- src/system/mapio/MapProxy.h
+++ src/system/mapio/MapProxy.h
@@ -98,7 +98,7 @@ public:
   {
     {
       MutexGuard g(mutex);
-      if(cmd.cmdid = SUBMIT && cmdinqueue.Count()==MAP_PROXY_QUEUE_LIMIT)
+      if( cmd.cmdid = SUBMIT && cmdinqueue.Count()==MAP_PROXY_QUEUE_LIMIT )
       {
         __mapproxy_trace__("putIncomingCommand: proxy queue limit exceded");
         throw ProxyQueueLimitException();
