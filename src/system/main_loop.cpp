a564a559e2a0ea2a3984da6efa7d19057c18bcdb
serge <>
2004-12-08 10:47:22 +0000

    *** empty log message ***

--- src/system/main_loop.cpp
+++ src/system/main_loop.cpp
@@ -203,27 +203,10 @@ void Smsc::mainLoop()
     // main "delay" cycle
 
     int eqsize,equnsize;
-    eventqueue.getStats(eqsize,equnsize);
     while(frame.size())
     {
       int cntInstant=tcontrol->getTotalCount();
-//      int cntSmooth=tcontrol->getTotalCountLong();
-      /*if(cntInstant+1>maxsms && cntSmooth+1000<=maxsms*smt*1000)
-      {
-        __info2__(log,"cnt=%d, smooth_cnt=%d, submitCnt=%d",cntInstant,cntSmooth,submitCount);
-      }*/
-
-      /*
-      if(
-          (cntInstant+1<=maxsms*stf ||
-            (
-              cntSmooth+1000<=maxsms*smt*1000 &&
-              cntInstant+1<=maxsms*stf+maxsms*stf*20/100
-            )
-          ) &&
-          eqsize+1<=eventQueueLimit
-        )
-        */
+      eventqueue.getStats(eqsize,equnsize);
       if(cntInstant+1<=maxsms*stf && equnsize+1<=eventQueueLimit)
       {
         SmscCommand cmd=frame.back();
