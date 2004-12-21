a33396ef3690f627db2676cdc1b17578259693ac
makar <>
2004-12-21 12:36:40 +0000

    lines & indents

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/console/commands/AliasAlterCommand.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/console/commands/AliasAlterCommand.java
@@ -22,52 +22,51 @@ import ru.novosoft.smsc.admin.journal.Action;
 
 public class AliasAlterCommand implements Command
 {
-    private String address = null;
-    private String alias = null;
-    private boolean hide = false;
-    private boolean hideSet = false;
+  private String address = null;
+  private String alias = null;
+  private boolean hide = false;
+  private boolean hideSet = false;
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-    public void setHide(boolean hide) {
-        this.hide = hide; hideSet = true;
-    }
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  public void setAlias(String alias) {
+    this.alias = alias;
+  }
+  public void setHide(boolean hide) {
+    this.hide = hide; hideSet = true;
+  }
 
-    public void process(CommandContext ctx)
-    {
-        String out = "Alias '"+alias+"'";
-        Alias smscAlias = ctx.getSmsc().getAliases().get(alias);
-        if (smscAlias != null) {
-            try {
-                Alias newAlias = new Alias(new Mask(address), new Mask(alias),
-                        (hideSet) ? hide:smscAlias.isHide());
-                boolean ok = ctx.getSmsc().getAliases().modify(new Alias(new Mask(alias), new Mask(alias), false), newAlias, new Action("console user", Constants.CONSOLE_SESSION_ID)); //todo: pass console user name to method
-                if (ok) {
-                    ctx.setMessage(out+" altered");
-                    ctx.setStatus(CommandContext.CMD_OK);
-                } else {
-                    ctx.getSmsc().getAliases().add(smscAlias, new Action("console user", Constants.CONSOLE_SESSION_ID)); //todo: pass console user name to method
-                    ctx.setMessage(out+" already exists or has equivalent");
-                    ctx.setStatus(CommandContext.CMD_PROCESS_ERROR);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                ctx.setMessage("Couldn't alter "+out+". Cause: "+e.getMessage());
-                ctx.setStatus(CommandContext.CMD_PROCESS_ERROR);
-            }
+  public void process(CommandContext ctx)
+  {
+    String out = "Alias '"+alias+"'";
+    Alias smscAlias = ctx.getSmsc().getAliases().get(alias);
+    if (smscAlias != null) {
+      try {
+        Alias newAlias = new Alias(new Mask(address), new Mask(alias), (hideSet) ? hide:smscAlias.isHide());
+        boolean ok = ctx.getSmsc().getAliases().modify(new Alias(new Mask(alias), new Mask(alias), false), newAlias, new Action("console user", Constants.CONSOLE_SESSION_ID)); //todo: pass console user name to method
+        if (ok) {
+          ctx.setMessage(out+" altered");
+          ctx.setStatus(CommandContext.CMD_OK);
         } else {
-            ctx.setMessage(out+" not found");
-            ctx.setStatus(CommandContext.CMD_PROCESS_ERROR);
+          ctx.getSmsc().getAliases().add(smscAlias, new Action("console user", Constants.CONSOLE_SESSION_ID)); //todo: pass console user name to method
+          ctx.setMessage(out+" already exists or has equivalent");
+          ctx.setStatus(CommandContext.CMD_PROCESS_ERROR);
         }
+      } catch (Exception e) {
+        e.printStackTrace();
+        ctx.setMessage("Couldn't alter "+out+". Cause: "+e.getMessage());
+        ctx.setStatus(CommandContext.CMD_PROCESS_ERROR);
+      }
+    } else {
+      ctx.setMessage(out+" not found");
+      ctx.setStatus(CommandContext.CMD_PROCESS_ERROR);
     }
+  }
 
-    public String getId() {
-        return "ALIAS_ALTER";
-    }
+  public String getId() {
+    return "ALIAS_ALTER";
+  }
 }
 
 
