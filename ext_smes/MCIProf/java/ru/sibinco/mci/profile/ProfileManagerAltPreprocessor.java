52fb74f312c46ea2ef3ea341173ae6464eaed3c7
makar <>
2005-01-17 14:29:09 +0000

    extra log added

--- ext_smes/MCIProf/java/ru/sibinco/mci/profile/ProfileManagerAltPreprocessor.java
+++ ext_smes/MCIProf/java/ru/sibinco/mci/profile/ProfileManagerAltPreprocessor.java
@@ -30,6 +30,7 @@ public class ProfileManagerAltPreprocessor extends ProfileManagerState implement
     if (msg == null)
       throw new ProcessingException("Profile option is undefined", ErrorCode.PAGE_EXECUTOR_EXCEPTION);
     msg = msg.trim();
+    logger.debug("Alt pre-processor got message: '"+msg+"'");
 
     if (!msg.equals(Constants.OPTION_EXIT))
     {
@@ -37,15 +38,18 @@ public class ProfileManagerAltPreprocessor extends ProfileManagerState implement
       try { switchReason = Integer.parseInt(msg); } catch(NumberFormatException e) {
         throw new ProcessingException("Profile option "+msg+" is invalid", ErrorCode.PAGE_EXECUTOR_EXCEPTION);
       }
+      logger.debug("Alt pre-processor option: "+switchReason);
       String activeReasons = (String)state.getAttribute(Constants.ATTR_REASONS);
       if (activeReasons == null)
         throw new ProcessingException("Failed to locate '"+Constants.ATTR_REASONS+"' attribute",
                                       ErrorCode.PAGE_EXECUTOR_EXCEPTION);
       activeReasons = activeReasons.toUpperCase();
+      logger.debug("Alt pre-processor activeReasons: '"+activeReasons+"'");
       try
       {
         ProfileInfo info = getProfileInfo(state);
         int counter = 0; int oldEventMask = info.getEventMask();
+        logger.debug("Alt pre-processor old mask: "+oldEventMask);
         if      ((activeReasons.indexOf('B') != -1) && switchReason == ++counter)
           info.eventMask = switchEventMask(oldEventMask, ProfileInfo.MASK_BUSY);
         else if ((activeReasons.indexOf('N') != -1) && switchReason == ++counter)
@@ -54,8 +58,13 @@ public class ProfileManagerAltPreprocessor extends ProfileManagerState implement
           info.eventMask = switchEventMask(oldEventMask, ProfileInfo.MASK_ABSENT);
         else if ((activeReasons.indexOf('U') != -1) && switchReason == ++counter)
           info.eventMask = switchEventMask(oldEventMask, ProfileInfo.MASK_UNCOND);
+        logger.debug("Alt pre-processor new mask: "+oldEventMask);
 
-        if (info.eventMask != oldEventMask) setProfileInfo(state, info);
+        if (info.eventMask != oldEventMask) {
+          setProfileInfo(state, info);
+          logger.debug("Alt pre-processor mask changed. Old: "+
+                       oldEventMask+", New: "+info.eventMask+". Counter="+counter);
+        }
         else logger.warn("Event mask is kept unchanged (mask="+oldEventMask+", msg='"+msg+"')");
         state.removeAttribute(Constants.ATTR_ERROR);
       }
