dc926e33c7669d611bcc0d08e5c0359c2d3b3c1c
skv <>
2005-03-24 13:15:36 +0000

    append and other

--- src/core/buffers/TmpBuf.hpp
+++ src/core/buffers/TmpBuf.hpp
@@ -1,6 +1,8 @@
 #ifndef __SMSC_CORE_BUFFERS_HPP__
 #define __SMSC_CORE_BUFFERS_HPP__
 
+#include <algorithm>
+
 namespace smsc{
 namespace core{
 namespace buffers{
@@ -21,29 +23,78 @@ public:
       heapSize=0;
       realBuf=stackBuf;
     }
+    pos=0;
   }
 
-  T* getSize(int size)
+  T* setSize(int size)
   {
     if(size>SZ)
     {
       if(size>heapSize)
       {
-        delete [] heapBuf;
+        if(heapBuf)delete [] heapBuf;
         heapBuf=new T[size];
         heapSize=size;
         realBuf=heapBuf;
       }
-    }else
-    {
-      realBuf=stackBuf;
     }
     return realBuf;
   }
+
+  void Append(const T* data,int count)
+  {
+    if(count==0)return;
+    if(pos+count>SZ)
+    {
+      if(heapBuf)
+      {
+        if(pos+count>heapSize)
+        {
+          heapSize=(pos+count)*2;
+          T* tmp=new T[heapSize];
+          std::copy(realBuf,realBuf+pos,tmp);
+          delete [] realBuf;
+          realBuf=tmp;
+        }
+      }else
+      {
+        heapSize=(pos+count)*2;
+        heapBuf=new T[heapSize];
+        std::copy(realBuf,realBuf+pos,heapBuf);
+        realBuf=heapBuf;
+      }
+    }
+    std::copy(data,data+count,realBuf+pos);
+    pos+=count;
+  }
+
+  void SetPos(int newpos)
+  {
+    pos=newpos;
+  }
+
+  int GetPos()
+  {
+    return pos;
+  }
+
+  void Read(T* dst,int count)
+  {
+    std::copy(realBuf+pos,realBuf+pos+count,dst);
+    pos+=count;
+  }
+
+  T* GetCurPtr()
+  {
+    return realBuf+pos;
+  }
+
+
   ~TmpBuf()
   {
     if(heapBuf)delete [] heapBuf;
   }
+
   inline operator T* ()
   {
     return realBuf;
@@ -63,6 +114,7 @@ protected:
   T* heapBuf;
   T* realBuf;
   int heapSize;
+  int pos;
 };
 
 }//namespace buffers
