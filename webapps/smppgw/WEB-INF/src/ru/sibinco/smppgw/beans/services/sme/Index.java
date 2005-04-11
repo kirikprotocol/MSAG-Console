6a825f0ff8283ac8f98c1d32219d3cdce071d3e5
andreybig <>
2005-04-11 06:52:19 +0000

    added registration Smsc

--- webapps/smppgw/WEB-INF/src/ru/sibinco/smppgw/beans/services/sme/Index.java
+++ webapps/smppgw/WEB-INF/src/ru/sibinco/smppgw/beans/services/sme/Index.java
@@ -5,6 +5,7 @@ import ru.sibinco.lib.backend.protocol.Proxy;
 import ru.sibinco.lib.bean.TabledBean;
 import ru.sibinco.smppgw.Constants;
 import ru.sibinco.smppgw.backend.Gateway;
+import ru.sibinco.smppgw.backend.sme.GwSme;
 import ru.sibinco.smppgw.beans.SmppgwJspException;
 import ru.sibinco.smppgw.beans.TabledBeanImpl;
 
@@ -25,9 +26,15 @@ public class Index extends TabledBeanImpl implements TabledBean
   {
     final Gateway gateway = appContext.getGateway();
     final Map smes = appContext.getGwSmeManager().getSmes();
+    final Map smscs = appContext.getSmscsManager().getSmscs();
     for (Iterator i = checkedSet.iterator(); i.hasNext();) {
       final String smeId = (String) i.next();
       try {
+        GwSme sme= (GwSme)smes.get(smeId);
+        if (sme.isSmsc()) {
+         gateway.unregSmsc(smeId);
+         smscs.remove(smeId);
+        }
         gateway.deleteSme(smeId);
         smes.remove(smeId);
       } catch (SibincoException e) {
