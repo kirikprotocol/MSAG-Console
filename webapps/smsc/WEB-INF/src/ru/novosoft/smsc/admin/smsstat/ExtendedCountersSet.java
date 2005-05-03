c055d6a0d83b9f7484ca554bdd5b7e4fd45e488a
makar <>
2005-05-03 13:00:31 +0000

    no message

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsstat/ExtendedCountersSet.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsstat/ExtendedCountersSet.java
@@ -14,41 +14,41 @@ import java.util.TreeMap;
  */
 public class ExtendedCountersSet extends CountersSet
 {
-  private TreeMap err=new TreeMap();
+    private TreeMap err=new TreeMap();
 
-  public ExtendedCountersSet() {}
-  public ExtendedCountersSet(long accepted, long rejected, long delivered,
-                             long failed, long rescheduled, long temporal, long i, long o)
-  {
-    super(accepted, rejected, delivered, failed, rescheduled, temporal, i, o);
-  }
+    public ExtendedCountersSet() {}
+    public ExtendedCountersSet(long accepted, long rejected, long delivered,
+                               long failed, long rescheduled, long temporal, long i, long o)
+    {
+        super(accepted, rejected, delivered, failed, rescheduled, temporal, i, o);
+    }
 
-  public void incError(int errcode, long count)
-  {
-      Integer key = new Integer(errcode);
-      ErrorCounterSet set = (ErrorCounterSet)err.get(key);
-      if (set == null) err.put(key, new ErrorCounterSet(errcode, count));
-      else set.increment(count);
-  }
-  public void putErr(int errcode, ErrorCounterSet set)
-  {
-    err.put(new Integer(errcode), set);
-  }
+    public void incError(int errcode, long count)
+    {
+        Integer key = new Integer(errcode);
+        ErrorCounterSet set = (ErrorCounterSet)err.get(key);
+        if (set == null) err.put(key, new ErrorCounterSet(errcode, count));
+        else set.increment(count);
+    }
+    public void putErr(int errcode, ErrorCounterSet set)
+    {
+        err.put(new Integer(errcode), set);
+    }
     public void addAllErr(Collection err)
-  {
-    err.addAll(err);
-  }
-  public Collection getErrors()
-  {
-    return err.values();
-  }
-  public TreeMap getErrorsMap()
-   {
-     return err;
-   }
+    {
+        err.addAll(err);
+    }
+    public Collection getErrors()
+    {
+        return err.values();
+    }
+    public TreeMap getErrorsMap()
+    {
+        return err;
+    }
 
-  public ErrorCounterSet getErr(int errcode)
-  {
-    return (ErrorCounterSet)err.get(new Integer(errcode));
-  }
+    public ErrorCounterSet getErr(int errcode)
+    {
+        return (ErrorCounterSet)err.get(new Integer(errcode));
+    }
 }
