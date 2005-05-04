e3cc10956a80ded799ee687c2c7db9ddb2b1d044
makar <>
2005-05-04 06:11:12 +0000

    some methods added

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsstat/ExportSettings.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/smsstat/ExportSettings.java
@@ -15,25 +15,50 @@ public class ExportSettings
     private String password = null;
     private String tablesPrefix = null;
 
+    public ExportSettings() {}
     public ExportSettings(String source, String driver, String user, String password, String tablesPrefix) {
         this.source = source;  this.driver = driver;
         this.user = user; this.password = password;
         this.tablesPrefix = tablesPrefix;
     }
 
-    public String getTablesPrefix() {
-        return tablesPrefix;
+    public boolean isEmpty() {
+        return (source == null || source.length() <= 0 || driver == null || driver.length() <= 0 ||
+                user == null || user.length() <= 0 || password == null ||
+                tablesPrefix == null || tablesPrefix.length() <= 0);
     }
     public String getSource() {
         return source;
     }
+    public void setSource(String source) {
+        this.source = source;
+    }
+
     public String getDriver() {
         return driver;
     }
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
     public String getUser() {
         return user;
     }
+    public void setUser(String user) {
+        this.user = user;
+    }
+
     public String getPassword() {
         return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTablesPrefix() {
+        return tablesPrefix;
+    }
+    public void setTablesPrefix(String tablesPrefix) {
+        this.tablesPrefix = tablesPrefix;
     }
 }
