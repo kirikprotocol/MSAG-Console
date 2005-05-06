12a52fa2a26acfb98232f34bc9358b98027bd1af
makar <>
2005-05-06 08:14:08 +0000

    added

---
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/console/commands/misc/SmsExportCommand.java
@@ -0,0 +1,49 @@
+package ru.novosoft.smsc.admin.console.commands.misc;
+
+import ru.novosoft.smsc.admin.console.Command;
+import ru.novosoft.smsc.admin.console.CommandContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: makar
+ * Date: 05.05.2005
+ * Time: 19:08:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmsExportCommand implements Command
+{
+    // export messages [to driver source user pass table_name]
+
+    public void process(CommandContext ctx)
+    {
+        try {
+            throw new Exception("Not implemented yet");
+            //ctx.setStatus(CommandContext.CMD_OK);
+        } catch (Exception e) {
+            ctx.setMessage("Couldn't export messages. Cause: "+e.getMessage());
+            ctx.setStatus(CommandContext.CMD_PROCESS_ERROR);
+        }
+    }
+
+    public void setSource(String source) {
+
+    }
+    public void setDriver(String driver) {
+
+    }
+    public void setUser(String user) {
+
+    }
+    public void setPassword(String password) {
+
+    }
+    public void setTableName(String tableName) {
+
+    }
+
+    public String getId() {
+        return "SMS_EXPORT";
+    }
+
+}
+
