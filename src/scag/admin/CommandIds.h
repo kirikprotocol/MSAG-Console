d4a785145fadc1681f4b741d49fe607217b77732
loomox <>
2005-05-14 07:11:40 +0000

    *** empty log message ***

---
+++ src/scag/admin/CommandIds.h
@@ -0,0 +1,39 @@
+// 
+// File:   CommandIds.h
+// Author: igork
+//
+// Created on 27 Май 2004 г., 15:59
+//
+
+#ifndef _SCAG_ADMIN_CommandIds_H
+#define	_SCAG_ADMIN_CommandIds_H
+
+#include "admin/protocol/Command.h"
+
+namespace smsc {
+namespace scag {
+namespace admin {
+  
+namespace CommandIds {
+  enum IDS {
+    unknown = smsc::admin::protocol::Command::undefined,
+    apply,
+    updateSmeInfo,
+    addSme,
+    statusSme,
+    deleteSme,
+    traceRoute,
+    loadRoutes,
+    regSmsc,
+    unregSme,
+    modifySmsc
+  };
+}
+
+}
+}
+}
+
+
+#endif	/* _SCAG_ADMIN_CommandIds_H */
+
