ec88232a56fb3666d16278936e6775b9b411eee7
starkom <>
2005-05-30 11:38:17 +0000

    no message

--- webapps/scripts/calendar_msg_en.js
+++ webapps/scripts/calendar_msg_en.js
@@ -1,2 +1,3 @@
+var isLocalized = "true";
 var calendarMonths = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 var calendarWD = new Array("Su","Mo","Tu","We","Th","Fr","Sa","Su");
--- webapps/scripts/calendar_msg_ru.js
+++ webapps/scripts/calendar_msg_ru.js
@@ -1,2 +1,3 @@
+var isLocalized = "true";
 var calendarMonths = new Array("Январь","Февраль","Март","Апрель","May","June","July","August","September","October","November","December");
 var calendarWD = new Array("Вс","Пн","Вт","Ср","Чт","Пт","Сб","Вс");
