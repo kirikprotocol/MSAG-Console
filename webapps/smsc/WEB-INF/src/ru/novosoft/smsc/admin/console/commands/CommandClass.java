732d659f315ba682cd41288724f5c047fe5b5fa3
starkom <>
2005-06-07 08:11:59 +0000

    no message

---
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/admin/console/commands/CommandClass.java
@@ -0,0 +1,36 @@
+package ru.novosoft.smsc.admin.console.commands;
+
+import ru.novosoft.smsc.admin.console.Command;
+import ru.novosoft.smsc.admin.console.CommandContext;
+import ru.novosoft.smsc.admin.Constants;
+import ru.novosoft.smsc.admin.journal.SubjectTypes;
+import ru.novosoft.smsc.admin.journal.Actions;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: starkom
+ * Date: 27.05.2005
+ * Time: 15:12:21
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class CommandClass implements Command
+{
+	public abstract void process(CommandContext ctx);
+	public abstract String getId();
+
+	public void updateJournalAndStatuses(CommandContext ctx, String userName)
+	{
+	}
+
+	protected void journalAppend(CommandContext ctx, String userName, final byte subjectType, final String subjectId, final byte action)
+	{
+		ctx.getJournal().append(userName, Constants.CONSOLE_SESSION_ID, subjectType, subjectId, action);
+	}
+
+	protected void journalAppend(CommandContext ctx, String userName, final byte subjectType, final String subjectId, final byte action, final String additionalKey, final String additionalValue)
+	{
+		ctx.getJournal().append(userName, Constants.CONSOLE_SESSION_ID, subjectType, subjectId, action, additionalKey, additionalValue);
+	}
+}
