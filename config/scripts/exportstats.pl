58a012c69164c8db3ea4517e79c14c9e4d7cd9e1
skv <>
2005-07-07 06:53:43 +0000

    select added

--- config/scripts/exportstats.pl
+++ config/scripts/exportstats.pl
@@ -14,19 +14,22 @@ my $sck=IO::Socket::INET->new(PeerHost => $ARGV[0],
                               );
 die "Connect failed:$!" unless $sck;
 die "Username:password expected" unless $ARGV[1]=~/^(.+):(.+)$/;
-getline($sck);
+getline($sck,60);
 $sck->write($1."\n");
-getline($sck);
+getline($sck,60);
 $sck->write($2."\n");
-getline($sck);
+getline($sck,60);
 my @tm=localtime(time-24*60*60);
 my $date=sprintf("%02d.%02d.%04d",$tm[3],$tm[4]+1,$tm[5]+1900);
 $sck->write("export stats for $date\n");
-getline($sck);
+getline($sck,10*60);
 $sck->write("quit");
 exit(0);
 
 sub getline{
+  my $sel=IO::Select->new();
+  $sel->add($_[0]);
+  die "Read timed out" unless $sel->can_read($_[1]);
   my $l=readline($_[0]);
   die "$!" unless defined($l);
   die "Error response:$l" unless $l=~/^\+/;
