7649fb97928d6dbe3c3af2f582a8110bc05f919c
loomox <>
2005-07-25 09:05:06 +0000

    *** empty log message ***

---
+++ src/scag/re/actions/ActionContext.cpp
@@ -0,0 +1,64 @@
+#include "ActionContext.h"
+#include <iostream>
+
+using namespace std;
+
+namespace scag { namespace re { namespace actions 
+{
+    using smsc::core::buffers::Hash;
+    using scag::re::RuleStatus;
+
+    using namespace scag::util::properties;
+
+
+FieldType ActionContext::Separate(const std::string& var,const char *& name)
+{
+    if (var.size()<=1) return ftUnknown;
+    
+    name = var.c_str() + 1;
+
+    if (var[0] == ftLocal) return ftLocal;
+    if (var[0] == ftConst) return ftConst;
+    if (var[0] == ftField) return ftField;
+    if (var[0] == ftSession) return ftSession;
+
+    return ftUnknown;
+}
+
+
+Property* ActionContext::getProperty(const std::string& var)
+{
+    FieldType prefix;
+    const char * name;
+    Property * p = 0;
+
+    prefix = Separate(var,name);
+
+    switch (prefix) 
+    {
+    case ftLocal:
+        if (!variables.Exists(name)) 
+        {
+            Property property;
+            variables.Insert(name,property);
+        }
+        return variables.GetPtr(name);
+        break;
+
+    case ftConst:
+        //TODO: how to return a value without const_cast?
+        p = const_cast<Property *>(constants.GetPtr(name));
+        return p;
+        break;
+
+    case ftField: return command.getProperty(name);
+    case ftSession: return session.getProperty(name);
+
+    default:
+        return 0;
+    }
+
+}
+
+}}}
+
