a0fbb72a0f4c58addbb4865fdd72a2934c65fb81
vic <>
2005-07-28 15:11:06 +0000

    no message

--- src/inman/inap/session.hpp
+++ src/inman/inap/session.hpp
@@ -1,4 +1,4 @@
-// $Id$
+#ident "$Id$"
 // INAP cессия
 
 #ifndef __SMSC_INMAN_INAP_SESSION__
@@ -7,7 +7,6 @@
 #include <map>
 
 #include "ss7cp.h"
-#include "et94inapcs1plusapi.h"
 
 #include "core/threads/Thread.hpp"
 #include "core/synchronization/Event.hpp"
@@ -23,7 +22,7 @@ namespace inap {
 
 static const USHORT_T MSG_RECV_TIMEOUT     = 1000;
 static const USHORT_T MSG_INIT_MAX_ENTRIES = 512;
-static const USHORT_T TCAP_DIALOG_MIN_ID   = 0;
+static const USHORT_T TCAP_DIALOG_MIN_ID   = 1;
 static const USHORT_T TCAP_DIALOG_MAX_ID   = 1000;
 
 class Dialog;
@@ -39,6 +38,10 @@ class Session : public Thread
         void        setState(State_T newState);
         UCHAR_T     getSSN() const;
 
+        SS7_ADDRESS_T scfAddr;
+        SS7_ADDRESS_T inmanAddr;
+        AC_NAMEREQ_T  ac;
+
         virtual     Dialog*  openDialog();
         virtual     Dialog*  findDialog(USHORT_T id);
         virtual     void       closeDialog(Dialog* pDlg);
@@ -48,17 +51,18 @@ class Session : public Thread
         typedef      std::map<USHORT_T, Dialog*> DialogsMap_T;
 
         Session(UCHAR_T SSN);
+        Session(UCHAR_T ssn, const char* scfNum, const char* inmanNum);
         DialogsMap_T dialogs;
 
         virtual     ~Session();
         virtual int  Execute();
 
-        State_T     state;
-        UCHAR_T     SSN;
-        USHORT_T    lastDialogId;
-        Event       started;
-        Event       stopped;
-        Mutex       lock;
+        State_T       state;
+        UCHAR_T       SSN;
+        USHORT_T      lastDialogId;
+        Event         started;
+        Event         stopped;
+        Mutex         lock;
         volatile BOOLEAN_T running;
 };
 
