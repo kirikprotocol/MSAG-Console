0e3626525bfcbe1f510de19ea6929a43304e09d4
vic <>
2005-07-28 16:12:11 +0000

    no message

--- src/inman/inap/callbacks.hpp
+++ src/inman/inap/callbacks.hpp
@@ -1,80 +1,199 @@
-#include "ss7cp.h"
-#include "et94inapcs1plusapi.h"
+#ident "$Id$"
 
+#include "ss7cp.h"
+#include "i97tcapapi.h"
 extern "C"
 {
 
-USHORT_T E94InapAbortInd(       UCHAR_T         ssn,
-                                USHORT_T        dialogueId,
-                                UCHAR_T         qSrvc,
-                                UCHAR_T         priority,
-                                ABORT_T         *abort_sp );
+USHORT_T EINSS7_I97TBindConf(   UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                UCHAR_T          bindResult);
 
-USHORT_T E94InapPAbortInd(      UCHAR_T         ssn,
-                                USHORT_T        dialogueId,
-                                UCHAR_T         qSrvc,
-                                UCHAR_T         priority,
-                                UCHAR_T         pAbortCause );
+USHORT_T EINSS7_I97TUniInd(     UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                UCHAR_T          priOrder,
+                                UCHAR_T          qualityOfService,
+                                UCHAR_T          destAdrLength,
+                                UCHAR_T          *destAdr_p,
+                                UCHAR_T          orgAdrLength,
+                                UCHAR_T          *orgAdr_p,
+                                UCHAR_T          compPresent,
+                                UCHAR_T          appContextLength,
+                                UCHAR_T          *appContext_p,
+                                USHORT_T         userInfoLength,
+                                UCHAR_T          *userInfo_p);
 
-USHORT_T E94InapBeginInd(       UCHAR_T         ssn,
-                                USHORT_T        dialogueId,
-                                UCHAR_T         qSrvc,
-                                UCHAR_T         priority,
-                                SS7_ADDRESS_T   *destAddress_sp,
-                                SS7_ADDRESS_T   *origAddress_sp,
-                                AC_NAMEREQ_T    *acName_sp,
-                                USHORT_T        uInfoLen,
-                                UCHAR_T         *uInfo_p,
-                                USHORT_T        noOfComponents,
-                                COMP_T          *comp_p );
+USHORT_T EINSS7_I97TBeginInd(   UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          priOrder,
+                                UCHAR_T          qualityOfService,
+                                UCHAR_T          destAdrLength,
+                                UCHAR_T          *destAdr_p,
+                                UCHAR_T          orgAdrLength,
+                                UCHAR_T          *orgAdr_p,
+                                UCHAR_T          compPresent,
+                                UCHAR_T          appContextLength,
+                                UCHAR_T          *appContext_p,
+                                USHORT_T         userInfoLength,
+                                UCHAR_T          *userInfo_p);
 
-USHORT_T E94InapBindConf(       UCHAR_T         ssn,
-                                UCHAR_T         bindResult );
+USHORT_T EINSS7_I97TContinueInd(UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          priOrder,
+                                UCHAR_T          qualityOfService,
+                                UCHAR_T          compPresent,
+                                UCHAR_T          appContextLength,
+                                UCHAR_T          *appContext_p,
+                                USHORT_T         userInfoLength,
+                                UCHAR_T          *userInfo_p);
 
-USHORT_T E94InapDataInd(        UCHAR_T         ssn,
-                                USHORT_T        dialogueId,
-                                UCHAR_T         qSrvc,
-                                UCHAR_T         priority,
-                                UCHAR_T         acShort,
-                                USHORT_T        uInfoLen,
-                                UCHAR_T         *uInfo_p,
-                                USHORT_T        noOfComponents,
-                                COMP_T          *comp_p );
+USHORT_T EINSS7_I97TEndInd(     UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          priOrder,
+                                UCHAR_T          qualityOfService,
+                                UCHAR_T          compPresent,
+                                UCHAR_T          appContextLength,
+                                UCHAR_T          *appContext_p,
+                                USHORT_T         userInfoLength,
+                                UCHAR_T          *userInfo_p);
 
-USHORT_T E94InapEndInd(         UCHAR_T         ssn,
-                                USHORT_T        dialogueId,
-                                UCHAR_T         qSrvc,
-                                UCHAR_T         priority,
-                                UCHAR_T         acShort,
-                                USHORT_T        uInfoLen,
-                                UCHAR_T         *uInfo_p,
-                                USHORT_T        noOfComponents,
-                                COMP_T          *comp_p );
+USHORT_T EINSS7_I97TUAbortInd(  UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          priOrder,
+                                UCHAR_T          qualityOfService,
+                                USHORT_T         abortInfoLength,
+                                UCHAR_T          *abortInfo_p,
+                                UCHAR_T          appContextLength,
+                                UCHAR_T          *appContext_p,
+                                USHORT_T         userInfoLength,
+                                UCHAR_T          *userInfo_p);
 
-USHORT_T E94InapErrorInd(       UCHAR_T         ssn,
-                                USHORT_T        dialogueId,
-                                UCHAR_T         einReason,
-                                BOOLEAN_T       invokeIdUsed,
-                                UCHAR_T         invokeId,
-                                UCHAR_T         disconnectIndicator );
+USHORT_T EINSS7_I97TPAbortInd(  UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          priOrder,
+                                UCHAR_T          qualityOfService,
+                                UCHAR_T          abortCause);
 
-USHORT_T E94InapNoticeInd(      UCHAR_T         ssn,
-                                USHORT_T        dialogueId,
-                                UCHAR_T         priority,
-                                UCHAR_T         acShort,
-                                UCHAR_T         reportCause,
-                                UCHAR_T         returnIndicator,
-                                USHORT_T        relatedDialogueId,
-                                USHORT_T        noOfComponents,
-                                COMP_T          *comp_p );
+USHORT_T EINSS7_I97TNoticeInd(  UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          reportCause,
+                                UCHAR_T          returnIndicator,
+                                USHORT_T         relDialogueId,
+                                UCHAR_T          segmentationIndicator,
+                                UCHAR_T          destAdrLength,
+                                UCHAR_T          *destAdr_p,
+                                UCHAR_T          orgAdrLength,
+                                UCHAR_T          *orgAdr_p);
 
-USHORT_T E94InapStateInd(       UCHAR_T         ssn,
-                                UCHAR_T         userStatus,
-                                UCHAR_T         affectedSsn,
-                                ULONG_T         affectedSpc,
-                                ULONG_T         localSpc );
+USHORT_T EINSS7_I97TInvokeInd(  UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeId,
+                                UCHAR_T          lastComponent,
+                                UCHAR_T          linkedIdUsed,
+                                UCHAR_T          linkedId,
+                                UCHAR_T          operationTag,
+                                USHORT_T         operationLength,
+                                UCHAR_T          *operationCode_p,
+                                USHORT_T         paramLength,
+                                UCHAR_T          *parameters_p);
 
-USHORT_T E94InapIndError(       USHORT_T        indErrorCode,
-                                MSG_T           *msg_sp );
+USHORT_T EINSS7_I97TResultNLInd(UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeId,
+                                UCHAR_T          lastComponent,
+                                UCHAR_T          operationTag,
+                                USHORT_T         operationLength,
+                                UCHAR_T          *operationCode_p,
+                                USHORT_T         paramLength,
+                                UCHAR_T          *parameters_p);
+
+USHORT_T EINSS7_I97TResultLInd( UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeId,
+                                UCHAR_T          lastComponent,
+                                UCHAR_T          operationTag,
+                                USHORT_T         operationLength,
+                                UCHAR_T          *operationCode_p,
+                                USHORT_T         paramLength,
+                                UCHAR_T          *parameters_p);
+
+USHORT_T EINSS7_I97TUErrorInd(  UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeId,
+                                UCHAR_T          lastComponent,
+                                UCHAR_T          errorCodeTag,
+                                USHORT_T         errorCodeLength,
+                                UCHAR_T          *errorCode_p,
+                                USHORT_T         paramLength,
+                                UCHAR_T          *parameters_p);
+
+USHORT_T EINSS7_I97TURejectInd( UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeIdUsed,
+                                UCHAR_T          invokeId,
+                                UCHAR_T          lastComponent,
+                                UCHAR_T          problemCodeTag,
+                                UCHAR_T          problemCode);
+
+USHORT_T EINSS7_I97TLRejectInd( UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeIdUsed,
+                                UCHAR_T          invokeId,
+                                UCHAR_T          problemCodeTag,
+                                UCHAR_T          problemCode);
+
+USHORT_T EINSS7_I97TRRejectInd( UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeIdUsed,
+                                UCHAR_T          invokeId,
+                                UCHAR_T          lastComponent,
+                                UCHAR_T          problemCodeTag,
+                                UCHAR_T          problemCode);
+
+USHORT_T EINSS7_I97TLCancelInd( UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                USHORT_T         dialogueId,
+                                UCHAR_T          invokeId);
+
+USHORT_T EINSS7_I97TStateInd(   UCHAR_T          ssn,
+                                USHORT_T         userId,
+                                EINSS7INSTANCE_T tcapInstanceId,
+                                UCHAR_T          userState,
+                                UCHAR_T          affectedSsn,
+                                ULONG_T          affectedSpc,
+                                ULONG_T          localSpc,
+                                UCHAR_T          subsysMultiplicityInd);
+
+USHORT_T EINSS7_I97TIndError(   USHORT_T         errorCode,
+                                MSG_T            *msg_sp);
 
 }
