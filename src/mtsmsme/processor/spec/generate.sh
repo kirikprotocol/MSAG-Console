6c6c69c5a19a121991dce3be1406108f04d0636c
vic <>
2005-09-01 13:45:17 +0000

    no message

---
+++ src/mtsmsme/processor/spec/generate.sh
@@ -0,0 +1,3 @@
+#cd ../codec
+#../spec/asn1c -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
+#../spec/asn1c -S /export/home/vic/asn1c/skeletons -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
