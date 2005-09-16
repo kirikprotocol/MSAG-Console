93914e9d568204813520df4b3b4f82bc3a49a608
skv <>
2005-09-16 03:39:33 +0000

    fixed length string

---
+++ src/core/buffers/FixedLengthString.hpp
@@ -0,0 +1,80 @@
+#ifndef __SMSC_CORE_BUFFERS_FIXEDLENGTHSTRING_HPP__
+#define __SMSC_CORE_BUFFERS_FIXEDLENGTHSTRING_HPP__
+
+#include <string.h>
+
+namespace smsc{
+namespace core{
+namespace buffers{
+
+template <int N>
+struct FixedLengthString{
+  char str[N];
+  FixedLengthString()
+  {
+    str[0]=0;
+  }
+  FixedLengthString(const char* argStr)
+  {
+    *this=argStr;
+  }
+  FixedLengthString(const FixedLengthString& argStr)
+  {
+    *this=argStr.str;
+  }
+  FixedLengthString& operator=(const char* argStr)
+  {
+    strncpy(str,argStr,N);
+    str[N-1]=0;
+    return *this;
+  }
+
+  template <int M>
+  bool operator==(const FixedLengthString<M>& argStr)const
+  {
+    return !strcmp(str,argStr.str);
+  }
+
+  bool operator==(const char* argStr)const
+  {
+    return !strcmp(str,argStr);
+  }
+
+  template <int M>
+  bool operator<(const FixedLengthString<M>& argStr)const
+  {
+    return strcmp(str,argStr.str)<0;
+  }
+
+
+  bool operator<(const char* argStr)const
+  {
+    return strcmp(str,argStr)<0;
+  }
+
+  operator char*()
+  {
+    return str;
+  }
+  operator const char*()const
+  {
+    return str;
+  }
+
+  char operator[](int index)const
+  {
+    return str[index];
+  }
+  char& operator[](int index)
+  {
+    return str[index];
+  }
+
+};
+
+}//namespace buffers
+}//namespace core
+}//namespace smsc
+
+
+#endif
