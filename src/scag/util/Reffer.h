1c0ccf3c044a8158b2e8c8bb088e3f9a5fc9813f
vita <>
2005-09-27 09:10:53 +0000

    *** empty log message ***

---
+++ src/scag/util/Reffer.h
@@ -0,0 +1,56 @@
+#ifndef Reffer_dot_h
+#define Reffer_dot_h
+
+// TODO: move to uti    l
+template<class T>
+class Reffer
+{
+  Mutex sync_;
+  T* manager_;
+  unsigned refCounter_;
+public:
+  T* operator->() { return manager_;}
+  Reffer(T* manager)
+  {
+    refCounter_ = 1;
+    manager_ = manager;
+  }
+  void Release()
+  {
+    unsigned counter;
+    {
+      MutexGuard g(sync_);
+      counter = --refCounter_;
+    }
+    if ( counter == 0 )
+    {
+      delete this;
+    }
+  }
+  Reffer* AddRef(){
+    MutexGuard g(sync_);
+    ++refCounter_;
+    return this;
+  }
+  static Reffer* Create(T* t) {return new Reffer(t);}
+protected:
+  virtual ~Reffer() {delete manager_;}
+private:
+  Reffer& operator = (const Reffer&);
+  Reffer(const Reffer&);
+};
+
+template<class T>
+class RefferGuard
+{
+  mutable Reffer<T>* reffer_;
+public:
+  RefferGuard(Reffer<T>* reffer) : reffer_(reffer) { reffer_->AddRef(); }
+  RefferGuard(const RefferGuard& g) : reffer_(g.reffer_){g.reffer_=0;}
+  ~RefferGuard() { if ( reffer_ != 0 ) reffer_->Release(); reffer_ = 0; }
+  Reffer<T>& operator->() { return *reffer_; }
+private:
+  RefferGuard& operator = (const RefferGuard&);
+};
+
+#endif
