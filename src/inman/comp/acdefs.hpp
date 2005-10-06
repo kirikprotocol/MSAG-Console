1157daf33fc413f9f2eee8545616a7bde59e82cd
gvroman <>
2005-10-06 10:15:33 +0000

    *** empty log message ***

--- src/inman/comp/acdefs.hpp
+++ src/inman/comp/acdefs.hpp
@@ -2,10 +2,22 @@
 #ifndef __SMSC_INMAN_COMP_APPCTXDEFS__
 #define __SMSC_INMAN_COMP_APPCTXDEFS__
 
-typedef const char * ApplicationContextId;
-//extern ApplicationContextId const _AC_oid[];
+#include "inman/comp/operfactory.hpp"
+
+using smsc::inman::comp::OperationFactoryInstanceT;
+
+namespace smsc {
+namespace ac {
 
 #define id_ac_cap4_sms_AC			0
 #define id_ac_map_networkUnstructuredSs_v2	1
+
+typedef OperationFactoryInstanceT <id_ac_cap4_sms_AC> 
+    CAP4SMSFactory;
+typedef OperationFactoryInstanceT <id_ac_map_networkUnstructuredSs_v2>
+    MAPUSS2Factory;
+
+} //ac
+} //smsc
 
 #endif /* __SMSC_INMAN_COMP_APPCTXDEFS__ */
