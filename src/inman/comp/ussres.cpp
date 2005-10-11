d09c04c298d3e87f9116e88ec90fbaac6d963ce3
gvroman <>
2005-10-11 04:46:28 +0000

    *** empty log message ***

--- src/inman/comp/ussres.cpp
+++ src/inman/comp/ussres.cpp
@@ -32,14 +32,14 @@ const vector<unsigned char>& ProcessUSSRequestRes::getUSSData(void)
 
 void ProcessUSSRequestRes::setUSSData(unsigned char * data, unsigned size)
 {
-    _uSSdata.resize(size);
+    _uSSdata.clear();
     _uSSdata.insert(_uSSdata.begin(), data, data + size);
 }
 
 void ProcessUSSRequestRes::decode(const vector<unsigned char>& buf)
 {
-    USSD_Res_t *	dcmd = NULL;	/* decoded structure */
-    asn_dec_rval_t	drc;		/* Decoder return value  */
+    USSD_Res_t *        dcmd = NULL;    /* decoded structure */
+    asn_dec_rval_t      drc;            /* Decoder return value  */
 
     drc = ber_decode(0, &asn_DEF_USSD_Res, (void **)&dcmd, &buf[0], buf.size());
     ASNCODEC_LOG_DEC(drc, asn_DEF_USSD_Res, "mapUSS");
@@ -56,18 +56,22 @@ void ProcessUSSRequestRes::decode(const vector<unsigned char>& buf)
 
 void ProcessUSSRequestRes::encode(vector<unsigned char>& buf)
 {
-    asn_enc_rval_t	er;
+    asn_enc_rval_t      er;
     /* construct USSD_Arg */
-    USSD_Res_t	 	cmd;
+    USSD_Res_t          cmd;
+    uint8_t             fdcsbuf;
+    uint8_t             fussdsbuf[MAP_MAX_USSD_StringLength];
 
 
     memset(&cmd, 0x00, sizeof(cmd)); //clear optionals and asn_ctx
     cmd.ussd_DataCodingScheme.size = 1;
+    cmd.ussd_DataCodingScheme.buf = &fdcsbuf;
     cmd.ussd_DataCodingScheme.buf[0] = _dCS;
     
     /* prepare ussd string */
     cmd.ussd_String.size = _uSSdata.size();
     assert(cmd.ussd_String.size < MAP_MAX_USSD_StringLength);
+    cmd.ussd_String.buf = &fussdsbuf[0];
     memcpy(cmd.ussd_String.buf, &_uSSdata[0], cmd.ussd_String.size);
 
     smsc_log_component(compLogger, &asn_DEF_USSD_Res, &cmd); 
