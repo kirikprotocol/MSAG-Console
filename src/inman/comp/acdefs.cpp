89b84ff803b59f5e0d32e3898b75ff63ffcb59a9
vic <>
2005-10-17 08:39:55 +0000

    add register method to session

--- src/inman/comp/acdefs.cpp
+++ src/inman/comp/acdefs.cpp
@@ -8,7 +8,7 @@ static char const ident[] = "$Id$";
 namespace smsc {
 namespace ac {
 
-#define MAX_AC_IDX 	id_ac_map_networkUnstructuredSs_v2
+#define MAX_AC_IDX  id_ac_map_networkUnstructuredSs_v2
 
 static APP_CONTEXT_T const  _OIDS[] = {
      {0, {0}}
@@ -17,9 +17,9 @@ static APP_CONTEXT_T const  _OIDS[] = {
 //      cap3OE(21) ac(3) 61 }
     ,{ 7, {0x04, 0x00, 0x00, 0x01, 0x15, 0x03, 0x3D}}
 //id_ac_map_networkUnstructuredSs_version2:
-//	{ itu-t(0) identified-organization(4) etsi(0) mobileDomain(0) gsm-Network(1)
+//  { itu-t(0) identified-organization(4) etsi(0) mobileDomain(0) gsm-Network(1)
 //        networkUnstructuredSs(19) version2(2) }
-    ,{ 6, {0x04, 0x00, 0x00, 0x01, 0x13, 0x02}}
+    ,{7,{0x04,0x00,0x00,0x01,0x00,0x13,0x02,}}
 };
 
 /* ************************************************************************** *
@@ -29,7 +29,7 @@ static APP_CONTEXT_T const  _OIDS[] = {
 const APP_CONTEXT_T * ACOID::OIDbyIdx(unsigned ac_idx)
 {
     if (ac_idx && (ac_idx <= MAX_AC_IDX)) {
-	return &_OIDS[ac_idx];
+  return &_OIDS[ac_idx];
     }
     return NULL;
 }
@@ -38,8 +38,8 @@ int ACOID::equal(const APP_CONTEXT_T* ac1, const APP_CONTEXT_T* ac2)
 {
     if (ac1->acLen && (ac1->acLen == ac2->acLen)) {
         int i;
-	for (i = ac1->acLen - 1; ((i >= 0) && (ac1->ac[i] == ac2->ac[i])) ; i--);
-	return (!i);
+  for (i = ac1->acLen - 1; ((i >= 0) && (ac1->ac[i] == ac2->ac[i])) ; i--);
+  return (!i);
     }
     return 0;
 }
@@ -48,7 +48,7 @@ int ACOID::equal(const APP_CONTEXT_T* ac1, const APP_CONTEXT_T* ac2)
 unsigned ACOID::Idx4OID(const APP_CONTEXT_T *oid)
 {
     for (unsigned i = 1; i <= MAX_AC_IDX; i++) {
-	if ((oid == &_OIDS[i]) || equal(oid, &_OIDS[i])) {
+  if ((oid == &_OIDS[i]) || equal(oid, &_OIDS[i])) {
             return i;
         }
     }
@@ -57,4 +57,3 @@ unsigned ACOID::Idx4OID(const APP_CONTEXT_T *oid)
 
 } //ac
 } //smsc
-
