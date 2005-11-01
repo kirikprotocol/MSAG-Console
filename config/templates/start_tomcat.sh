271511f29e6c9ef44a32674e62190c6c2061dc9e
serge <>
2005-11-01 09:32:26 +0000

    *** empty log message ***

--- config/templates/start_tomcat.sh
+++ config/templates/start_tomcat.sh
@@ -6,6 +6,6 @@ fi
 export LC_ALL=@LC_ALL@
 export LANG=@LC_ALL@
 export LANGUAGE=@LC_ALL@
-export NLS_LANG=@NSL_LANG@
+export NLS_LANG=@NLS_LANG@
 tomcat/tomcat_runner.sh run tomcat.pid &
 echo $! > tomcat.pid
