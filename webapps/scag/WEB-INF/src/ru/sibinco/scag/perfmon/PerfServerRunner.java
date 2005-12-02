c6a212d354c349c610495c1099a848a9c4af7f1c
igor <>
2005-12-02 05:57:02 +0000

    no message

--- webapps/scag/WEB-INF/src/ru/sibinco/scag/perfmon/PerfServerRunner.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/perfmon/PerfServerRunner.java
@@ -78,9 +78,8 @@ public class PerfServerRunner extends Thread {
   protected void readSnap(InputStream istream, PerfSnap snap)
       throws IOException {
     int len = inbuf.readNetworkInt(istream);
-    //inbuf.fill(istream, len - 4);
-    inbuf.fill(istream, len);
-    snap.init2(inbuf);
+    inbuf.fill(istream, len - 4);
+    snap.init(inbuf);
   }
 
   Object shutSemaphore = new Object();
