0c849a6ebbb38be6eac7bfc99dba2d9ec5770699
gvroman <>
2005-12-05 11:33:26 +0000

    Full control over sending commands

--- src/inman/inman_test.cpp
+++ src/inman/inman_test.cpp
@@ -38,6 +38,34 @@ typedef struct {
 } Abonent;
 
 
+class INDialog {
+public:
+    typedef enum { dIdle = 0, dCharged = 1, dApproved, dReported } INState;
+
+    INDialog(unsigned int id, AbonentType ab_type, bool ussd_op,
+             bool batch_mode = false,
+             DeliverySmsResult_t delivery = smsc::inman::interaction::DELIVERY_SUCCESSED)
+        : did(id), state(INDialog::dIdle), abType(ab_type), ussdOp(ussd_op)
+        , batchMode(batch_mode), dlvrRes(delivery)
+    {}
+
+    void setState(INState new_state) { state = new_state; }
+    INState getState(void) { return state; }
+    DeliverySmsResult_t getDlvrResult(void) { return dlvrRes; }
+
+    bool    isBatchMode(void) { return batchMode; }
+
+protected:
+    unsigned int        did;
+    INState             state;
+    bool                batchMode;
+    DeliverySmsResult_t dlvrRes;
+    AbonentType         abType;
+    bool                ussdOp;
+};
+
+
+
 static const Abonent  _abonents[2] = {
     //Nezhinsky phone(prepaid):
      { abPrepaid, ".1.1.79139343290", "250013900405871" }
@@ -48,19 +76,21 @@ static const Abonent  _abonents[2] = {
 class Facade : public Thread, public SmscHandler
 {
 protected:
+    typedef std::map<unsigned int, INDialog*> INDialogsMap;
+
     bool                _running;
     unsigned            dialogId;
     Socket*             socket;
     ObjectPipe*         pipe;
     Logger*             logger;
-    DeliverySmsResult_t delivery;
     AbonentType         abType;
     bool                _ussdOp;
+    INDialogsMap        _Dialogs;
 
 public:
     Facade(const char* host, int port)
         : logger(Logger::getInstance("smsc.InFacade"))
-        , dialogId(0), abType(abPrepaid), _running (false), _ussdOp(false) 
+        , dialogId(0), abType(abPrepaid), _running (false), _ussdOp(false)
     {
         std::string msg;
         msg = format("InFacade: connecting to InManager at %s:%d...\n", host, port);
@@ -88,9 +118,13 @@ public:
         _running = false; //stop thread 
         delete pipe;
         delete socket; 
-    }
 
-    DeliverySmsResult_t getDeliveryStatus(void) { return delivery; }
+        INDialogsMap::const_iterator it;
+        for (it = _Dialogs.begin(); it != _Dialogs.end(); it++) {
+            INDialog * dlg = (*it).second;
+            delete dlg;
+        }
+    }
 
     bool isUssdOp(void) { return _ussdOp; }
     void setUssdOp(bool op) { _ussdOp = op; }
@@ -101,14 +135,32 @@ public:
     bool isRunning(void) const { return _running; }
     unsigned getNextDialogId(void) { return ++dialogId; }
 
-    void sendChargeSms(DeliverySmsResult_t deliveryStatus)
+    unsigned int initDialog(unsigned int did = 0, bool batch_mode = false,
+             DeliverySmsResult_t delivery = smsc::inman::interaction::DELIVERY_SUCCESSED)
     {
-        //set delivery status to send on ChargeSmsResult
-        delivery = deliveryStatus;
+        if (!did)
+            did = getNextDialogId();
+        INDialog * dlg = new INDialog(did, abType, _ussdOp, batch_mode, delivery);
+        _Dialogs.insert(INDialogsMap::value_type(did, dlg));
+        return did;
+    }
+
+    INDialog * findDialog(unsigned int did)
+    {
+        INDialogsMap::const_iterator it = _Dialogs.find(did);
+
+        if (it != _Dialogs.end())
+            return (*it).second;
+        return NULL;
+    }
+
+
+    void sendChargeSms(unsigned int dlgId)
+    {
         //compose ChargeSms
         ChargeSms op;
 
-        op.setDialogId(getNextDialogId());
+        op.setDialogId(dlgId);
 
         op.setDestinationSubscriberNumber( ".1.1.79139859489" ); // missing for MT
 
@@ -131,31 +183,54 @@ public:
         op.setMsgId(0x010203040506);
         op.setServiceOp(_ussdOp ? 0 : -1);
 
-        pipe->send(&op); 
+        std::string msg = format("Sending ChargeSms [dlgId: %u] ..\n", op.getDialogId());
+        prompt(msg);
+        pipe->send(&op);
+
+        INDialog * dlg = findDialog(dlgId);
+        assert(dlg);
+        if (dlg->getState() == INDialog::dIdle)
+            dlg->setState(INDialog::dCharged);
+        else {
+            msg =  format("WRN: Dialog[%u] state is %u!\n", dlg->getState());
+            prompt(msg);
+        }
     }
 
-    void sendDeliverySmsResult(unsigned int dlgId)
+    void sendDeliverySmsResult(unsigned int dlgId, DeliverySmsResult_t deliveryStatus)
     {
-        DeliverySmsResult   op(delivery);
+        DeliverySmsResult   op(deliveryStatus);
         op.setDialogId(dlgId);
-        std::string msg = format("Sending DeliverySmsResult: DELIVERY_%s\n",
-                                !delivery ? "DELIVERY_SUCCESSED" : "DELIVERY_FAILED");
+        std::string msg = format("Sending DeliverySmsResult: DELIVERY_%s [dlgId: %u]\n",
+                                 !deliveryStatus ? "SUCCESSED" : "FAILED", dlgId);
         prompt(msg);
-        if (!delivery) { //fill optional fields
+        if (!deliveryStatus) { //fill optional fields
             op.setDestIMSI("250013901464251");
             op.setDestMSC(".1.1.79139860001");
             op.setDestSMEid("DST_MAP_PROXY");
             op.setDeliveryTime(time(NULL));
+        }
+        
+        INDialog * dlg = findDialog(dlgId);
+        if (dlg) {
+            if (dlg->getState() == INDialog::dApproved)
+                dlg->setState(INDialog::dReported);
+            else {
+                msg =  format("WRN: Dialog[%u] state is %u!\n", dlg->getState());
+                prompt(msg);
+            }
+        } else {
+            msg =  format("WRN: Dialog[%u] is inknown!\n", dlgId);
+            prompt(msg);
         }
         pipe->send(&op);
     }
 
     void onChargeSmsResult(ChargeSmsResult* result)
     {
-        assert(result);
-        assert(result->getObjectId() == smsc::inman::interaction::CHARGE_SMS_RESULT_TAG);
-        std::string msg = format("ChargeSmsResult: CHARGING_%sPOSSIBLE",
-                ( result->GetValue() == smsc::inman::interaction::CHARGING_POSSIBLE ) ?
+        unsigned int did = result->getDialogId();
+        std::string msg = format("Dialog[%u] got ChargeSmsResult: CHARGING_%sPOSSIBLE", did,
+                (result->GetValue() == smsc::inman::interaction::CHARGING_POSSIBLE ) ?
                  "" : "NOT_");
         if (result->GetRPCause())
             msg += format(", RPCause: %u", (unsigned)result->GetRPCause());
@@ -166,10 +241,24 @@ public:
         else if (result->GetTCAPError())
             msg += ", TCAP dialog error";
         msg += "\n";
-
         prompt(msg);
-        if (result->GetValue() == smsc::inman::interaction::CHARGING_POSSIBLE)
-            sendDeliverySmsResult(result->getDialogId());
+
+        INDialog * dlg = findDialog(did);
+        if (dlg) {
+            if (dlg->getState() == INDialog::dCharged) {
+                dlg->setState(INDialog::dApproved);
+                if (dlg->isBatchMode()) {
+                    if (result->GetValue() == smsc::inman::interaction::CHARGING_POSSIBLE)
+                        sendDeliverySmsResult(did, dlg->getDlvrResult());
+                }
+            } else {
+                msg =  format("ERR: Dialog[%u] was not Charged!", did);
+                prompt(msg);
+            }
+        } else {
+            msg =  format("ERR: Dialog[%u] is inknown!", did);
+            prompt(msg);
+        }
     }
 
     // Thread entry point
@@ -183,9 +272,13 @@ public:
             int n = select(  socket->getSocket()+1, &read, 0, 0, 0 );
             if ( n > 0 ) {
                 SmscCommand* cmd = static_cast<SmscCommand*>(pipe->receive());
-                if (cmd)
-                    cmd->handle( this );
-                else { //socket closed or socket error
+                if (cmd) {
+                    if (cmd->getObjectId() == smsc::inman::interaction::CHARGE_SMS_RESULT_TAG)
+                        cmd->handle(this);
+                    else
+                        fprintf(stdout, "ERR: unknown command recieved: %u\n",
+                                cmd->getObjectId());
+                } else { //socket closed or socket error
                     _running = false;
                 }
             }
@@ -206,31 +299,91 @@ public:
 
 static Facade* _pFacade = 0;
 
+void cmd_charge(Console&, const std::vector<std::string> &args)
+{
+    if (_pFacade->isRunning()) {
+        unsigned int did = 0;
+
+        if (args.size() > 1) {
+            did = (unsigned int)atoi(args[1].c_str());
+            if (!did) {
+                fprintf(stdout, "ERR: bad dialog id specified (%s)!\n", args[1].c_str());
+                return;
+            }
+        }
+        if (!_pFacade->findDialog(did))
+            did = _pFacade->initDialog(did);
+        _pFacade->sendChargeSms(did);
+    } else
+        throw ConnectionClosedException();
+}
 
 
 void cmd_chargeOk(Console&, const std::vector<std::string> &args)
 {
-    if (_pFacade->isRunning())
-        _pFacade->sendChargeSms(smsc::inman::interaction::DELIVERY_SUCCESSED);
-    else
+    if (_pFacade->isRunning()) {
+        unsigned int did = _pFacade->initDialog(0, true,
+                            smsc::inman::interaction::DELIVERY_SUCCESSED);
+        _pFacade->sendChargeSms(did);
+    } else
         throw ConnectionClosedException();
 }
 
 void cmd_chargeErr(Console&, const std::vector<std::string> &args)
 {
-    if (_pFacade->isRunning())
-        _pFacade->sendChargeSms(smsc::inman::interaction::DELIVERY_FAILED);
-    else
+    if (_pFacade->isRunning()) {
+        unsigned int did = _pFacade->initDialog(0, true,
+                            smsc::inman::interaction::DELIVERY_FAILED);
+        _pFacade->sendChargeSms(did);
+    } else
         throw ConnectionClosedException();
 }
 
-void cmd_delivery(Console&, const std::vector<std::string> &args)
+
+void cmd_reportOk(Console&, const std::vector<std::string> &args)
 {
-    if (_pFacade->isRunning())
-        _pFacade->sendDeliverySmsResult(_pFacade->getNextDialogId());
-    else
+    if (_pFacade->isRunning()) {
+        unsigned int did = 0;
+
+        if (args.size() > 1) //create new dialog
+            did = (unsigned int)atoi(args[1].c_str());
+
+        if (!did) {
+            fprintf(stdout, "ERR: bad dialog id specified (%s)!\n", args[1].c_str());
+            return;
+        }
+
+        if (!_pFacade->findDialog(did))
+            fprintf(stdout, "WRN: unknown dialog id specified (%s)!\n", args[1].c_str());
+
+        _pFacade->sendDeliverySmsResult(did,
+                smsc::inman::interaction::DELIVERY_SUCCESSED);
+    } else
         throw ConnectionClosedException();
 }
+
+void cmd_reportErr(Console&, const std::vector<std::string> &args)
+{
+    if (_pFacade->isRunning()) {
+        unsigned int did = 0;
+
+        if (args.size() > 1) //create new dialog
+            did = (unsigned int)atoi(args[1].c_str());
+
+        if (!did) {
+            fprintf(stdout, "ERR: bad dialog id specified (%s)!\n", args[1].c_str());
+            return;
+        }
+
+        if (!_pFacade->findDialog(did))
+            fprintf(stdout, "WRN: unknown dialog id specified (%s)!\n", args[1].c_str());
+
+        _pFacade->sendDeliverySmsResult(did,
+                smsc::inman::interaction::DELIVERY_SUCCESSED);
+    } else
+        throw ConnectionClosedException();
+}
+
 
 void cmd_config(Console&, const std::vector<std::string> &args)
 {
@@ -238,9 +391,6 @@ void cmd_config(Console&, const std::vector<std::string> &args)
         AbonentType ab = _pFacade->getAbonent();
         fprintf(stdout, "Current Abonent: %s (%s)\n", _abonents[ab].addr,
                                  (ab == abPrepaid) ? "prepaid":"postpaid");
-        fprintf(stdout, "  deliveryStatus to report: %s\n", 
-                (_pFacade->getDeliveryStatus() == smsc::inman::interaction::DELIVERY_SUCCESSED) ?
-                 "SUCCESSED" : "FAILED");
         fprintf(stdout, "  bearerType to use: dp%s\n",
                 _pFacade->isUssdOp() ? "USSD" : "SMS");
     } else
@@ -289,20 +439,26 @@ void cmd_dpussd(Console&, const std::vector<std::string> &args)
 int main(int argc, char** argv)
 {
     if ( argc != 3 ) {
-        fprintf( stderr, "Usage: %s <host> <port>\n", argv[0] );
+        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0] );
+        exit(1);
+    }
+    int port = atoi( argv[2]);
+    if (!port) {
+        fprintf(stderr, "ERR: bad port specified (%s) !", argv[2]);
         exit(1);
     }
     const char* host = argv[1];
-    int port = atoi( argv[2]);
 
     Logger::Init();
     try {
         _pFacade = new Facade(host, port);
 
         Console console;
-        console.addItem( "chargeOk", cmd_chargeOk );
-        console.addItem( "chargeErr",  cmd_chargeErr );
-        console.addItem( "delivery",  cmd_delivery );
+        console.addItem( "charge", cmd_charge ); //chargeSMS only
+        console.addItem( "chargeOk", cmd_chargeOk ); //chargeSMS -> reportOk
+        console.addItem( "chargeErr",  cmd_chargeErr );//chargeSMS -> reportErr
+        console.addItem( "reportOk",  cmd_reportOk );
+        console.addItem( "reportErr",  cmd_reportErr );
         console.addItem( "prepaid",  cmd_prepaid );
         console.addItem( "postpaid",  cmd_postpaid );
         console.addItem( "dpsms",  cmd_dpsms );
