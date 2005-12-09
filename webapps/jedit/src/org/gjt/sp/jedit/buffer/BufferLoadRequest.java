1d2a24c048dc1e75e4071c4b2a892f1cc2cdb02d
andreybig <>
2005-12-09 08:42:36 +0000

    added getting errors from scag

--- webapps/jedit/src/org/gjt/sp/jedit/buffer/BufferLoadRequest.java
+++ webapps/jedit/src/org/gjt/sp/jedit/buffer/BufferLoadRequest.java
@@ -37,6 +37,8 @@ import java.net.MalformedURLException;
 import org.gjt.sp.jedit.io.*;
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.util.*;
+import sidekick.SideKickPlugin;
+import errorlist.DefaultErrorSource;
 //}}}
 
 /**
@@ -46,48 +48,52 @@ import org.gjt.sp.util.*;
  */
 public class BufferLoadRequest extends BufferIORequest
 {
- //{{{ BufferLoadRequest constructor
- /**
-  * Creates a new buffer I/O request.
-  * @param view The view
-  * @param buffer The buffer
-  * @param session The VFS session
-  * @param vfs The VFS
-  * @param path The path
-  */
- public BufferLoadRequest(View view, Buffer buffer,
-  Object session, VFS vfs, String path)
- {
-  super(view,buffer,session,vfs,path);
- } //}}}
+  //{{{ BufferLoadRequest constructor
+  /**
+   * Creates a new buffer I/O request.
+   * @param view The view
+   * @param buffer The buffer
+   * @param session The VFS session
+   * @param vfs The VFS
+   * @param path The path
+   */
+  public BufferLoadRequest(View view, Buffer buffer,
+                           Object session, VFS vfs, String path)
+  {
+    super(view,buffer,session,vfs,path);
+  } //}}}
 
   public void _createInputStream(Object session, String path,
- Buffer buffer, long length,boolean markers) throws IOException ,FileNotFoundException
- {
+                                 Buffer buffer, long length,boolean markers) throws IOException ,FileNotFoundException
+  {
 
     URL url=null;
     HttpURLConnection c=null;InputStream _in=null;
     int command=jEdit.getParseXml();
-   if (jEdit.getBooleanProperty("bufferWorkWithId")) command=jEdit.getLoadRule();
-   if (buffer!=null && buffer.getBooleanProperty("newRule")) command=jEdit.getLoadNewRule();
-   String content="?username="+jEdit.username+"&password="+jEdit.password+"&command="+command+"&file="+path;
-   System.out.println("BufferLoadRequest _createInputStream content= "+content);
+    if (jEdit.getBooleanProperty("bufferWorkWithId")) command=jEdit.getLoadRule();
+    if (buffer!=null && buffer.getBooleanProperty("newRule")) command=jEdit.getLoadNewRule();
+    String content="?username="+jEdit.username+"&password="+jEdit.password+"&command="+command+"&file="+path;
+    System.out.println("BufferLoadRequest _createInputStream content= "+content);
     try {
       url=new URL(jEdit.servletUrl,content);
       c=(HttpURLConnection) url.openConnection();
       c.connect(); String status=c.getHeaderField("status");
-     /* if (jEdit.getBooleanProperty("bufferWorkWithId")) {
-        String len=c.getHeaderField("length");
-        System.out.println("BufferLoadRequest._createInputStream len= "+len);
-        length=0;//Long.parseLong(len);
-      }  */
       System.out.println("BufferLoadRequest _createInputStream status= "+status);
-      if (!status.equals("ok")) throw new FileNotFoundException(status);
+      if (!status.equals("ok"))  {
+        if (jEdit.getBooleanProperty("bufferWorkWithId")) {
+          DefaultErrorSource errorSource=SideKickPlugin.getErrorSource(view);
+          int errorType=c.getHeaderFieldInt("errorType",0); String _path=c.getHeaderField("id");
+          int lineIndex=c.getHeaderFieldInt("lineIndex",0); int start=c.getHeaderFieldInt("start",0);
+          int end=c.getHeaderFieldInt("end",0);
+          errorSource.addError(errorType,_path,lineIndex,start,end,status);
+        }
+        else throw new FileNotFoundException(status);
+      }
       _in=c.getInputStream();
       if(_in == null)
-     return;
-     if (!markers) read(autodetect(_in),length,false);
-     else readMarkers(buffer,_in);
+        return;
+      if (!markers) read(autodetect(_in),length,false);
+      else readMarkers(buffer,_in);
     } catch (MalformedURLException e) {
       e.printStackTrace();
       /*  }  catch (ProtocolException e) {
@@ -95,174 +101,174 @@ public class BufferLoadRequest extends BufferIORequest
       */ }
     finally{
       try {
-      if (_in!=null) _in.close();
-      if (c!=null) c.disconnect();
-        } catch (IOException e) {
-          e.printStackTrace();
+        if (_in!=null) _in.close();
+        if (c!=null) c.disconnect();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  } //}}}
+
+  //{{{ run() method
+  public void run()
+  {
+    InputStream in = null;
+
+    try
+    {
+      try
+      {
+        setAbortable(true);
+        long length = 0;
+        if (!jEdit.getBooleanProperty("bufferWorkWithId")) {
+
+          String[] args = { vfs.getFileName(path) };
+          if(!buffer.isTemporary())
+          {
+            setStatus(jEdit.getProperty("vfs.status.load",args));
+            setProgressValue(0);
+          }
+
+          path = vfs._canonPath(session,path,view);
+
+          VFSFile entry = vfs._getFile(session,path,view);
+          if(entry != null)
+            length = entry.getLength();
+          else
+            length = 0L;
+
         }
-    }
- } //}}}
-
- //{{{ run() method
- public void run()
- {
-  InputStream in = null;
-
-  try
-  {
-   try
-   {
-     setAbortable(true);
-    long length = 0;
-    if (!jEdit.getBooleanProperty("bufferWorkWithId")) {
-
-    String[] args = { vfs.getFileName(path) };
-    if(!buffer.isTemporary())
-    {
-     setStatus(jEdit.getProperty("vfs.status.load",args));
-     setProgressValue(0);
-    }
-
-    path = vfs._canonPath(session,path,view);
-
-    VFSFile entry = vfs._getFile(session,path,view);
-    if(entry != null)
-     length = entry.getLength();
-    else
-     length = 0L;
-
-    }
-  /*  in = vfs._createInputStream(session,path,
-     false,view);
-    if(in == null)
-     return;
-      */
+        /*  in = vfs._createInputStream(session,path,
+       false,view);
+      if(in == null)
+       return;
+        */
         _createInputStream(session,path,buffer,length,false);
-    //read(autodetect(in),length,false);
-    buffer.setNewFile(false);
-   }
-   catch(CharConversionException ch)
-   {
-  //  Log.log(Log.ERROR,this,ch);
-    Object[] pp = { buffer.getProperty(Buffer.ENCODING),
-     ch.toString() };
-    VFSManager.error(view,path,"ioerror.encoding-error",pp);
-       ch.printStackTrace();
-    buffer.setBooleanProperty(ERROR_OCCURRED,true);
-   }
-   catch(UnsupportedEncodingException uu)
-   {
-  //  Log.log(Log.ERROR,this,uu);
-    Object[] pp = { buffer.getProperty(Buffer.ENCODING),
-     uu.toString() };
-    VFSManager.error(view,path,"ioerror.encoding-error",pp);
-       uu.printStackTrace();
-    buffer.setBooleanProperty(ERROR_OCCURRED,true);
-   }
-   catch(IOException io)
-   {
-   // Log.log(Log.ERROR,this,io);
-    Object[] pp = { io.toString() };
-    VFSManager.error(view,path,"ioerror.read-error",pp);
+        //read(autodetect(in),length,false);
+        buffer.setNewFile(false);
+      }
+      catch(CharConversionException ch)
+      {
+        //  Log.log(Log.ERROR,this,ch);
+        Object[] pp = { buffer.getProperty(Buffer.ENCODING),
+                ch.toString() };
+        VFSManager.error(view,path,"ioerror.encoding-error",pp);
+        ch.printStackTrace();
+        buffer.setBooleanProperty(ERROR_OCCURRED,true);
+      }
+      catch(UnsupportedEncodingException uu)
+      {
+        //  Log.log(Log.ERROR,this,uu);
+        Object[] pp = { buffer.getProperty(Buffer.ENCODING),
+                uu.toString() };
+        VFSManager.error(view,path,"ioerror.encoding-error",pp);
+        uu.printStackTrace();
+        buffer.setBooleanProperty(ERROR_OCCURRED,true);
+      }
+      catch(IOException io)
+      {
+        // Log.log(Log.ERROR,this,io);
+        Object[] pp = { io.toString() };
+        VFSManager.error(view,path,"ioerror.read-error",pp);
         io.printStackTrace();
-    buffer.setBooleanProperty(ERROR_OCCURRED,true);
-   }
-   catch(OutOfMemoryError oom)
-   {
-   // Log.log(Log.ERROR,this,oom);
-    VFSManager.error(view,path,"out-of-memory-error",null);
+        buffer.setBooleanProperty(ERROR_OCCURRED,true);
+      }
+      catch(OutOfMemoryError oom)
+      {
+        // Log.log(Log.ERROR,this,oom);
+        VFSManager.error(view,path,"out-of-memory-error",null);
         oom.printStackTrace();
-    buffer.setBooleanProperty(ERROR_OCCURRED,true);
-   }
+        buffer.setBooleanProperty(ERROR_OCCURRED,true);
+      }
 
-   if(jEdit.getBooleanProperty("persistentMarkers") && !jEdit.getBooleanProperty("bufferWorkWithId"))
-   {
-    try
-    {
-     String[] args = { vfs.getFileName(path) };
-     if(!buffer.isTemporary())
-      setStatus(jEdit.getProperty("vfs.status.load-markers",args));
-     setAbortable(true);
+      if(jEdit.getBooleanProperty("persistentMarkers") && !jEdit.getBooleanProperty("bufferWorkWithId"))
+      {
+        try
+        {
+          String[] args = { vfs.getFileName(path) };
+          if(!buffer.isTemporary())
+            setStatus(jEdit.getProperty("vfs.status.load-markers",args));
+          setAbortable(true);
 
-    // in = vfs._createInputStream(session,markersPath,true,view);
-      _createInputStream(session,markersPath,buffer,0,true);
-         // if(in != null)
-     // readMarkers(buffer,in);
+          // in = vfs._createInputStream(session,markersPath,true,view);
+          _createInputStream(session,markersPath,buffer,0,true);
+          // if(in != null)
+          // readMarkers(buffer,in);
+        }
+        catch(IOException io)
+        {
+          // ignore
+        }
+      }
     }
-    catch(IOException io)
+    catch(WorkThread.Abort a)
     {
-     // ignore
+      if(in != null)
+      {
+        try
+        {
+          in.close();
+        }
+        catch(IOException io)
+        {
+        }
+      }
+
+      buffer.setBooleanProperty(ERROR_OCCURRED,true);
     }
-   }
-  }
-  catch(WorkThread.Abort a)
+    finally
+    {
+      try
+      {
+        vfs._endVFSSession(session,view);
+      }
+      catch(IOException io)
+      {
+        // Log.log(Log.ERROR,this,io);
+        String[] pp = { io.toString() };
+        VFSManager.error(view,path,"ioerror.read-error",pp);
+
+        buffer.setBooleanProperty(ERROR_OCCURRED,true);
+      }
+      catch(WorkThread.Abort a)
+      {
+        buffer.setBooleanProperty(ERROR_OCCURRED,true);
+      }
+    }
+  } //}}}
+
+  //{{{ readMarkers() method
+  private void readMarkers(Buffer buffer, InputStream _in)
+          throws IOException
   {
-   if(in != null)
-   {
-    try
-    {
-     in.close();
-    }
-    catch(IOException io)
-    {
-    }
-   }
-
-   buffer.setBooleanProperty(ERROR_OCCURRED,true);
-  }
-  finally
-  {
-   try
-   {
-    vfs._endVFSSession(session,view);
-   }
-   catch(IOException io)
-   {
-   // Log.log(Log.ERROR,this,io);
-    String[] pp = { io.toString() };
-    VFSManager.error(view,path,"ioerror.read-error",pp);
-
-    buffer.setBooleanProperty(ERROR_OCCURRED,true);
-   }
-   catch(WorkThread.Abort a)
-   {
-    buffer.setBooleanProperty(ERROR_OCCURRED,true);
-   }
-  }
- } //}}}
-
- //{{{ readMarkers() method
- private void readMarkers(Buffer buffer, InputStream _in)
-  throws IOException
- {
     // For `reload' command
-  buffer.removeAllMarkers();
+    buffer.removeAllMarkers();
 
-  BufferedReader in = new BufferedReader(new InputStreamReader(_in));
+    BufferedReader in = new BufferedReader(new InputStreamReader(_in));
 
-  try
-  {
-   String line;
+    try
+    {
+      String line;
       while((line = in.readLine()) != null)
-   {
-    // compatibility kludge for jEdit 3.1 and earlier
-    if(!line.startsWith("!"))
-     continue;
+      {
+        // compatibility kludge for jEdit 3.1 and earlier
+        if(!line.startsWith("!"))
+          continue;
 
-    // malformed marks file?
-    if(line.length() == 0)
-     continue;
-    
-    char shortcut = line.charAt(1);
-    int start = line.indexOf(';');
-    int end = line.indexOf(';',start + 1);
-    int position = Integer.parseInt(line.substring(start + 1,end));
-    buffer.addMarker(shortcut,position);
-   }
-  }
-  finally
-  {
-   in.close();
-  }
- } //}}}
+        // malformed marks file?
+        if(line.length() == 0)
+          continue;
+
+        char shortcut = line.charAt(1);
+        int start = line.indexOf(';');
+        int end = line.indexOf(';',start + 1);
+        int position = Integer.parseInt(line.substring(start + 1,end));
+        buffer.addMarker(shortcut,position);
+      }
+    }
+    finally
+    {
+      in.close();
+    }
+  } //}}}
 }
