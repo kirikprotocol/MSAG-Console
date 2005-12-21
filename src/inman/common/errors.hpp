536847a4c1a049b051f575db4d39db5127016793
gvroman <>
2005-12-21 10:17:58 +0000

    base exception for Inman parts

--- src/inman/common/errors.hpp
+++ src/inman/common/errors.hpp
@@ -1,3 +1,4 @@
+#ident "$Id$"
 #ifndef __SMSC_INMAN_COMMON_ERRORS_HPP__
 #define __SMSC_INMAN_COMMON_ERRORS_HPP__
 
@@ -8,67 +9,67 @@
 
 #include "inman/common/util.hpp"
 
+using smsc::inman::common::format;
+
 namespace smsc {
 namespace inman {
 namespace common {
 
-class CustomError : public std::exception
+class CustomException : public std::exception
 {
-    protected:
-        std::string message;
+protected:
+    int         errCode;
+    std::string message;
+    std::string desc;
+    std::string exId;
 
-    public:	
-        CustomError() throw()
-        { }
-        CustomError(const std::string& msg) throw()
-            : message( msg )
-        { }
-
-        ~CustomError() throw()
-        { }
-
-        const char* what() const throw()
-        {
-            return message.c_str();
+public:
+    CustomException(const char * msg, int err_code = 0, const char * err_desc = NULL)
+        : errCode(err_code)
+    {
+        if (msg)
+            message += msg;
+        if (err_desc) {
+            desc += err_desc;
+            message += ": ";
+            message += err_desc;
         }
+        if (errCode)
+            format(message, "(code: %u)", errCode);
+    }
+    ~CustomException() throw()
+    {}
+
+    void setExcId(const char * ids) { exId += ids; }
+    const char * getExcId(void) const { return exId.c_str(); }
+
+    const char* what() const throw() { return message.c_str(); }
+
+    int getErrorCode(void) const { return errCode; }
+    const char* getErrorDesc(void) const  { return desc.c_str(); }
 };
 
-class SystemError : public CustomError
+
+//system errors based on 'errno'
+class SystemError : public CustomException
 {
-    protected:
-        int errorCode;
+public:
+    SystemError(const char * msg, int err_code = errno)
+        : CustomException(msg, err_code, err_code ? strerror(err_code) : NULL)
+    {
+        setExcId("SystemError");
+    }
 
-    public:
-        SystemError(const char * msg) throw()
-            : errorCode( errno )
-        {
-            format(message, "%s : %s (0x%02X)", msg, getErrorString(), errorCode);
-        }
-
-        SystemError(std::string & msg) throw()
-            : errorCode( errno )
-        {
-            format(message, "%s : %s (0x%02X)", msg.c_str(), getErrorString(), errorCode);
-        }
-
-        void Init(const char * msg)
-        {
-            format(message, "%s : %s (0x%02X)", msg, getErrorString(), errorCode = errno);
-        }
-
-        int getErrorCode() const
-        {
-	    return errorCode;
-        }
-
-        const char* getErrorString() const
-        {
-            return strerror(errorCode);
-        }
+    SystemError(std::string & msg, int err_code = errno)
+        : CustomException(msg.c_str(), err_code, err_code ? strerror(err_code) : NULL)
+    {
+        setExcId("SystemError");
+    }
 };
 
-}
-}
-}
+} //common
+} //inman
+} //smsc
 
-#endif
+#endif /* __SMSC_INMAN_COMMON_ERRORS_HPP__ */
+
