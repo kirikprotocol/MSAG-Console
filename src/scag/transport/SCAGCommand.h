f0aea6e4295457a4a674718fd97c279c1d17ac36
skv <>
2006-01-12 07:03:37 +0000

    tabs?

--- src/scag/transport/SCAGCommand.h
+++ src/scag/transport/SCAGCommand.h
@@ -5,7 +5,7 @@
 
 namespace scag { namespace transport
 {
-    enum TransportType 
+    enum TransportType
     {
         SMPP = 1, HTTP = 2, MMS = 3, WAP = 4
     };
@@ -13,16 +13,16 @@ namespace scag { namespace transport
     class SCAGCommand
     {
     public:
-    
-        virtual TransportType getType() const = 0;
-	
-        virtual int getRuleId() const = 0;
-        virtual void setRuleId(int ruleId) = 0;
-	
-    	virtual int64_t getOperationId() const = 0;
-    	virtual void setOperationId(int64_t op) = 0;
-    
-    	virtual ~SCAGCommand() {};
+
+      virtual TransportType getType() const = 0;
+
+      virtual int getRuleId() const = 0;
+      virtual void setRuleId(int ruleId) = 0;
+
+      virtual int64_t getOperationId() const = 0;
+      virtual void setOperationId(int64_t op) = 0;
+
+      virtual ~SCAGCommand() {};
     };
 
 }}
