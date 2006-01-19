6400b7302a7fdc5b2e4ee342dfefc87dd98476f3
skv <>
2006-01-19 06:57:16 +0000

    implementation

--- src/scag/sessions/SessionStore.cpp
+++ src/scag/sessions/SessionStore.cpp
@@ -1,35 +1,108 @@
 #include "SessionStore.h"
 
-namespace scag { namespace sessions 
+namespace scag{
+namespace sessions{
+
+void SessionStore::init(const std::string& dir,SessionLoadCallback cb,void* dataPtr)
 {
+  log=smsc::logger::Logger::getInstance("sesstore");
 
-    SessionPtr currSession;
+  std::string idxFile="sessoionstore.idx";
+  std::string binFile="sessoionstore.bin";
 
-    void SessionStore::init(const std::string& dir,SessionLoadCallback cb,void* data)
-    {
-    
-    }
-    
-    SessionPtr SessionStore::getSession(const CSessionKey& sessionKey)
-    {
-        return currSession;
-    }
+  if( dir.length()>0 && ( dir[dir.length()-1]=='\\' || dir[dir.length()-1]=='/') )
+  {
+    idxFile=dir+idxFile;
+    binFile=dir+binFile;
+  }else
+  {
+    idxFile=dir+'/'+idxFile;
+    binFile=dir+'/'+binFile;
+  }
 
-    SessionPtr SessionStore::newSession(const CSessionKey& sessionKey)
-    {
-        currSession = new Session(sessionKey);
+  if(!buf::File::Exists(idxFile.c_str()))
+  {
+    dhash.Create(idxFile.c_str(),100000,false);
+  }else
+  {
+    dhash.Open(idxFile.c_str());
+  }
+  if(!buf::File::Exists(binFile.c_str()))
+  {
+    pfile.Create(binFile.c_str(),256,25000);
+  }else
+  {
+    pfile.Open(binFile.c_str());
+  }
 
-        return currSession;
-    }
-    
-    void SessionStore::deleteSession(const CSessionKey& sessionKey)
-    {
-    }
-    
-    void SessionStore::updateSession(SessionPtr session)
-    {
-    }
-    
-   
-}}
+  File::offset_type idx=0,ridx;
+  std::vector<unsigned char> data;
+  while((idx=pfile.Read(idx,data,&ridx))!=0)
+  {
+    Session s=Session(CSessionKey());
+    SessionBuffer sb(data.size());
+    sb.Append((char*)&data[0],data.size());
+    s.Deserialize(sb);
+    cb(dataPtr,&s);
+  }
+}
 
+SessionPtr SessionStore::getSession(const CSessionKey& sessionKey)
+{
+  sync::MutexGuard mg(mtx);
+  OffsetValue off;
+  if(!dhash.LookUp(sessionKey,off))
+  {
+    return SessionPtr(0);
+  }
+  std::vector<unsigned char> data;
+  pfile.Read(off.value,data);
+  SessionPtr s(new Session(sessionKey));
+  SessionBuffer sb(data.size());
+  sb.Append((char*)&data[0],data.size());
+  s->Deserialize(sb);
+  return s;
+}
+
+SessionPtr SessionStore::newSession(const CSessionKey& sessionKey)
+{
+  SessionBuffer sb;
+  SessionPtr s(new Session(sessionKey));
+  s->Serialize(sb);
+  sync::MutexGuard mg(mtx);
+  OffsetValue val(pfile.Append(sb.get(),sb.GetPos()));
+  smsc_log_debug(log,"newSession:%s:%08llx",DiskSessionKey(sessionKey).toString().c_str(),val.value);
+  dhash.Insert(sessionKey,val);
+  return s;
+}
+
+void SessionStore::deleteSession(const CSessionKey& sessionKey)
+{
+  sync::MutexGuard mg(mtx);
+  OffsetValue off;
+  if(!dhash.LookUp(sessionKey,off))
+  {
+    smsc_log_info(log,"Attempt to delete session that doesn't exists:%s",DiskSessionKey(sessionKey).toString().c_str());
+    return;
+  }
+  smsc_log_debug(log,"delSession:%s:%08llx",DiskSessionKey(sessionKey).toString().c_str(),off.value);
+  pfile.Delete(off.value);
+  dhash.Delete(sessionKey);
+}
+
+void SessionStore::updateSession(SessionPtr session)
+{
+  sync::MutexGuard mg(mtx);
+  OffsetValue off;
+  if(!dhash.LookUp(session->getSessionKey(),off))
+  {
+    throw smsc::util::Exception("SessionStore::updateSession - Session with key %s not found",DiskSessionKey(session->getSessionKey()).toString().c_str());
+  }
+  smsc_log_debug(log,"updateSession:%s:%08llx",DiskSessionKey(session->getSessionKey()).toString().c_str(),off.value);
+  SessionBuffer sb;
+  session->Serialize(sb);
+  pfile.Update(off.value,sb.get(),sb.GetPos());
+}
+
+}
+}
--- src/scag/sessions/SessionStore.h
+++ src/scag/sessions/SessionStore.h
@@ -7,28 +7,115 @@
 #include "Session.h"
 #include "core/buffers/RefPtr.hpp"
 #include "core/synchronization/Mutex.hpp"
+#include "core/buffers/DiskHash.hpp"
+#include "core/buffers/PageFile.hpp"
 
-namespace scag { namespace sessions
+namespace scag{
+namespace sessions{
+
+
+typedef smsc::core::buffers::RefPtr<Session,smsc::core::synchronization::Mutex> SessionPtr;
+typedef void (*SessionLoadCallback)(void*,Session*);
+
+namespace buf=smsc::core::buffers;
+namespace sync=smsc::core::synchronization;
+
+
+class SessionStore
 {
-    typedef smsc::core::buffers::RefPtr<Session,smsc::core::synchronization::Mutex> SessionPtr;
+public:
 
-    typedef void (*SessionLoadCallback)(void*,Session*);
+  SessionStore() {};
+  ~SessionStore() {};
 
-    class SessionStore
+  void init(const std::string& dir,SessionLoadCallback cb,void* data);
+
+  SessionPtr getSession(const CSessionKey& sessionKey);
+
+  SessionPtr newSession(const CSessionKey& sessionKey);
+  void deleteSession(const CSessionKey& sessionKey);
+  void updateSession(SessionPtr session);
+
+protected:
+    smsc::logger::Logger* log;
+
+  class DiskSessionKey{
+  protected:
+  public:
+    Address  addr;
+    uint16_t umr;
+    DiskSessionKey(){}
+    DiskSessionKey(const CSessionKey& key):addr(key.abonentAddr),umr(key.USR){}
+
+    bool operator==(const DiskSessionKey& that)const
     {
-    public:
+      return addr==that.addr && umr==that.umr;
+    }
 
-        SessionStore() {};
-        ~SessionStore() {};
+    std::string toString()
+    {
+      std::string str=addr.toString();
+      char buf[16];
+      sprintf(buf,":%hu",umr);
+      str+=buf;
+      return str;
+    }
+    static uint32_t Size()
+    {
+      return 1+1+sizeof(AddressValue)+2;
+    }
+    void Read(File& f)
+    {
+      addr.type=f.ReadByte();
+      addr.plan=f.ReadByte();
+      f.Read(addr.value,sizeof(addr.value));
+      addr.length=strlen(addr.value);
+      umr=f.ReadNetInt16();
+    }
+    void Write(File& f)const
+    {
+      f.WriteByte(addr.type);
+      f.WriteByte(addr.plan);
+      f.Write(addr.value,sizeof(addr.value));
+      f.WriteNetInt16(umr);
+    }
+    uint32_t HashCode(uint32_t attempt)const
+    {
+      uint32_t res=(addr.type+addr.plan)*umr;
+      res=crc32(res,addr.value,addr.length);
+      for(;attempt>0;attempt--)
+      {
+        res+=umr;
+        res=crc32(res,addr.value,addr.length);
+      }
+      return res;
+    }
+  };
 
-        void init(const std::string& dir,SessionLoadCallback cb,void* data);
+  struct OffsetValue{
+    File::offset_type value;
 
-        SessionPtr getSession(const CSessionKey& sessionKey);
+    OffsetValue():value(0){}
+    OffsetValue(File::offset_type argValue):value(argValue){}
+    OffsetValue(const OffsetValue& src):value(src.value){}
 
-        SessionPtr newSession(const CSessionKey& sessionKey);
-        void deleteSession(const CSessionKey& sessionKey);
-        void updateSession(SessionPtr session);
-    };
-}}
+    static uint32_t Size(){return sizeof(File::offset_type);}
+    void Read(File& f)
+    {
+      value=f.ReadNetInt64();
+    }
+    void Write(File& f)const
+    {
+      f.WriteNetInt64(value);
+    }
+  };
+
+  buf::PageFile pfile;
+  buf::DiskHash<DiskSessionKey,OffsetValue> dhash;
+  sync::Mutex mtx;
+};
+
+}
+}
 
 #endif // SCAG_SESSIONS_STORE
