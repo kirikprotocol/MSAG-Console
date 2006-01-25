f083f91499d94dd3a36029cfe2a84f797c3cceed
igor <>
2006-01-25 09:16:39 +0000

    changed class Daemon

--- webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/gw/status/Status.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/gw/status/Status.java
@@ -1,10 +1,10 @@
 package ru.sibinco.scag.beans.gw.status;
 
 import ru.sibinco.lib.SibincoException;
-import ru.sibinco.lib.backend.daemon.Daemon;
-import ru.sibinco.lib.backend.service.ServiceInfo;
 import ru.sibinco.scag.beans.SCAGBean;
 import ru.sibinco.scag.beans.SCAGJspException;
+import ru.sibinco.scag.backend.service.ServiceInfo;
+import ru.sibinco.scag.backend.daemon.Daemon;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,30 +13,28 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by igork Date: 27.07.2004 Time: 17:17:59
  */
-public class Status extends SCAGBean
-{
-  private byte gwStatus = ServiceInfo.STATUS_UNKNOWN;
+public class Status extends SCAGBean {
 
-  public void process(final HttpServletRequest request, final HttpServletResponse response) throws SCAGJspException
-  {
-    super.process(request, response);
-    final Daemon gwDaemon = appContext.getGwDaemon();
-    try {
-      gwDaemon.refreshServices(appContext.getGwSmeManager());
-      final Object gwService = gwDaemon.getServiceInfo(appContext.getGateway().getId());
-      if (gwService instanceof ServiceInfo) {
-        final ServiceInfo info = (ServiceInfo) gwService;
-        gwStatus = info.getStatus();
-      }
-    } catch (SibincoException e) {
-      logger.error("Could not refresh services", e);
-    } catch (NullPointerException e) {
-      logger.error("Could not get GW daemon");
+    private byte scagStatus = ServiceInfo.STATUS_UNKNOWN;
+
+    public void process(final HttpServletRequest request, final HttpServletResponse response) throws SCAGJspException {
+        super.process(request, response);
+        final Daemon scagDaemon = appContext.getScagDaemon();
+        try {
+            scagDaemon.refreshServices(appContext.getSmppManager());
+            final Object gwService = scagDaemon.getServiceInfo(appContext.getGateway().getId());
+            if (gwService instanceof ServiceInfo) {
+                final ServiceInfo info = (ServiceInfo) gwService;
+                scagStatus = info.getStatus();
+            }
+        } catch (SibincoException e) {
+            logger.error("Could not refresh services", e);
+        } catch (NullPointerException e) {
+            logger.error("Could not get SCAG daemon");
+        }
     }
-  }
 
-  public byte getGwStatus()
-  {
-    return gwStatus;
-  }
+    public byte getScagStatus() {
+        return scagStatus;
+    }
 }
