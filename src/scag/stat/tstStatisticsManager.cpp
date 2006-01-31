a63dd590a682d3f1527d9150836d494f1d221601
green <>
2006-01-31 07:20:42 +0000

    *** empty log message ***

---
+++ src/scag/stat/tstStatisticsManager.cpp
@@ -0,0 +1,151 @@
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#ifdef _WIN32
+#include <stdint.h>
+#else
+#include <inttypes.h>
+#endif
+
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>  
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include <scag/util/singleton/Singleton.h>
+#include "tstStatisticsManager.h"
+
+namespace scag { 
+namespace statx {
+
+bool  StatisticsManager::inited = false;
+using namespace scag::util::singleton;
+using scag::config::StatManConfig;
+
+
+typedef SingletonHolder<StatisticsManager> SingleSM;
+
+Statistics& Statistics::Instance()
+{
+    if (!StatisticsManager::inited) 
+    {
+        MutexGuard guard(StatisticsManager::initLock);
+        if (!StatisticsManager::inited) 
+            throw std::runtime_error("Statistics not inited!");
+    }
+    return SingleSM::Instance();
+}
+void StatisticsManager::init(const StatManConfig& statManConfig)
+{
+    if (!StatisticsManager::inited)
+    {
+        MutexGuard guard(StatisticsManager::initLock);
+        if (!StatisticsManager::inited) {
+            StatisticsManager& sm = SingleSM::Instance();
+
+            sm.Start();
+            StatisticsManager::inited = true;
+        }
+    }
+}
+
+
+
+StatisticsManager::StatisticsManager()
+    : Statistics(), Thread(), 
+      logger(Logger::getInstance("scag.stat.StatisticsManager")),
+      currentIndex(0), bExternalFlush(false), isStarted(false) 
+{
+}
+StatisticsManager::~StatisticsManager()
+{
+  Stop();
+  WaitFor();
+
+}
+ 
+void StatisticsManager::incError(IntHash<int>& hash, int errcode){}
+
+void StatisticsManager::registerEvent(const SmppStatEvent& se){}
+
+void StatisticsManager::registerEvent(const HttpStatEvent& se){}
+    
+bool StatisticsManager::checkTraffic(std::string routeId, CheckTrafficPeriod period, int64_t value)
+{
+      
+    return false;
+}
+
+int StatisticsManager::Execute()
+{
+
+    
+    isStarted = true;     
+
+    while (isStarted)
+    {
+        int toSleep = calculateToSleep();
+        smsc_log_debug(logger, "Execute() >> Start wait %d", toSleep);
+        awakeEvent.Wait(toSleep); // Wait for next hour begins ...
+        smsc_log_debug(logger, "Execute() >> End wait");
+
+        int flushIndex = switchCounters();
+        flushCounters(flushIndex);
+        flushHttpCounters(flushIndex);
+
+        flushTraffic();
+        flushHttpTraffic();
+
+        if(bExternalFlush) isStarted = false;
+        bExternalFlush = false;
+        smsc_log_debug(logger, "Execute() >> Flushed");
+    }
+
+    smsc_log_debug(logger, "Execute() exited");
+    return 0;
+}
+
+void StatisticsManager::Stop()
+{
+
+    smsc_log_debug(logger, "PerformanceServer is shutdowninig...");
+    sender.Stop();
+    smsc_log_debug(logger, "PerformanceServer is shutdowned");
+
+    if (isStarted)
+    {
+        bExternalFlush = true;
+        awakeEvent.Signal();
+    }
+}
+
+void StatisticsManager::Start()
+{
+    
+    smsc_log_debug(logger, "PerformanceServer is starting...");
+    sender.Start();
+    smsc_log_debug(logger, "PerformanceServer is started");
+
+    isStarted = true;
+    Thread::Start();
+}
+
+
+
+
+void StatisticsManager::reportGenPerformance(PerformanceData * data){}
+void StatisticsManager::getSmppPerfData(uint64_t *cnt){}
+void StatisticsManager::reportSvcPerformance(){}
+void StatisticsManager::reportScPerformance(){}
+void StatisticsManager::addSvcSocket(Socket * socket){}
+void StatisticsManager::addScSocket(Socket * socket){}
+void StatisticsManager::addGenSocket(Socket * socket){}
+
+
+
+}//namespace statx
+}//namespace scag
---
+++ src/scag/stat/tstStatisticsManager.h
@@ -0,0 +1,96 @@
+#ifndef SMSC_SCAG_STATX_STATISTICS_MANAGER
+#define SMSC_SCAG_STATX_STATISTICS_MANAGER
+
+#include <core/synchronization/Mutex.hpp>
+#include <core/synchronization/Event.hpp>
+#include <core/buffers/IntHash.hpp>
+#include <core/buffers/Hash.hpp>
+#include <core/buffers/TmpBuf.hpp>
+#include <core/buffers/Array.hpp>
+#include <core/threads/Thread.hpp>
+
+#include <logger/Logger.h>
+
+#include <util/timeslotcounter.hpp>
+#include <util/config/Config.h>
+
+#include "Statistics.h"
+#include "routemap.h"
+#include "TrafficRecord.h"
+#include <core/buffers/File.hpp>
+#include "scag/config/stat/StatManConfig.h"
+
+#include "util/timeslotcounter.hpp"
+#include "core/network/Socket.hpp"
+#include "Sender.h"
+#include "Performance.h"
+
+namespace scag {
+
+namespace statx {
+
+    using namespace smsc::core::threads;
+    using namespace smsc::core::synchronization;
+    using namespace scag::stat;
+
+    using smsc::core::buffers::File;
+    using smsc::core::buffers::Array;
+    using scag::config::StatManConfig;
+
+    using smsc::core::buffers::IntHash;
+    using smsc::core::buffers::Hash;
+
+    using smsc::logger::Logger;
+    using smsc::util::config::Config;
+    using scag::stat::SmppStatEvent;
+
+
+    using smsc::util::TimeSlotCounter;
+    using smsc::core::network::Socket;
+
+    class StatisticsManager : public Statistics, public PerformanceListener, public PerformanceServer, public Thread
+    {
+    friend class Statistics;
+    private:
+ 
+        static bool  inited;
+        static Mutex initLock;
+
+    protected:
+
+        smsc::logger::Logger    *logger;
+
+        bool    isStarted;
+
+    private:
+
+        void Stop();
+        void Start();
+
+    public:
+
+        static void init(const StatManConfig& statManCfg);    
+        virtual int Execute();
+
+        virtual void registerEvent(const SmppStatEvent& se);
+        virtual void registerEvent(const HttpStatEvent& se);
+
+        bool checkTraffic(std::string routeId, CheckTrafficPeriod period, int64_t value);
+
+        virtual void getSmppPerfData(uint64_t *cnt);
+        //virtual void getHttpPerfData(uint64_t *cnt);
+        virtual void reportGenPerformance(PerformanceData * data);
+        virtual void reportSvcPerformance();
+        virtual void reportScPerformance();
+        virtual void addSvcSocket(Socket * socket);
+        virtual void addScSocket(Socket * socket);
+        virtual void addGenSocket(Socket * socket);
+
+        StatisticsManager();
+        virtual ~StatisticsManager();
+    };
+
+}//namespace statx
+}//namespace scag
+
+#endif //SMSC_STATX_STATISTICS_MANAGER
