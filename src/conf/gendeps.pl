13fe1279fcef8db27c4553d3a662d2e6b4303d2f
serge <>
2006-02-01 11:37:49 +0000

    *** empty log message ***

--- src/conf/gendeps.pl
+++ src/conf/gendeps.pl
@@ -18,6 +18,7 @@ die "SMSC_BUILDDIR not defined" unless $builddir;
 my $pdir=substr($dir,length($srcdir)+1);
 my $lib=$pdir;
 $lib=~s#/#-#g;
+print "Lib=$lib;pdir=$pdir\n"; 
 
 open(OUT,">>$builddir/deps/$pdir/.binaries-rules")
  or die "Failed to open $builddir/deps/$pdir/.binaries-rules";
@@ -72,6 +73,9 @@ while(<IN>)
   $gccflags=~s/-library=stlport4//;
   #print STDERR "Exec:g++ -MM $gccflags $srcdir/$pdir/$src.cpp\n";
   my $dep=`g++ -MM $gccflags $srcdir/$pdir/$src.cpp`;
+  $dep=~s/^#.*?$//mg;
+  $dep=~s/^[\x0a\x0d]+//;
+  print STDERR "lib=$lib\ndep=$dep\n";
   print OUT "$builddir/obj/lib$lib/$dep" if $dep;
   print OUT "\n\n";
 }
