1075800a2f63a087e7e9eb1ac287ab6400eedb61
green <>
2006-02-09 10:44:51 +0000

    *** empty log message ***

---
+++ src/scag/gt/scag/scagx.h
@@ -0,0 +1,25 @@
+#ifndef SCAG_X_H
+#define SCAG_X_H
+namespace scag{
+class Scag
+{
+public:
+  Scag():stopFlag(false){};
+  ~Scag();
+  void init(){};
+  void run()
+  {
+   while(!stopFlag)
+   {
+      sleep(1);
+   }
+
+  };
+  void stop(){stopFlag=true;}
+protected:
+  bool stopFlag;
+
+};
+
+}//scag
+#endif
