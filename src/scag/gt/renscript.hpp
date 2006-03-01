a0ab16071ca73f4475817db5f0799a0540223914
green <>
2006-03-01 08:51:42 +0000

    *** empty log message ***

--- src/scag/gt/renscript.hpp
+++ src/scag/gt/renscript.hpp
@@ -597,6 +597,7 @@ static JSFunctionSpec Global_functions[] = {
  {"InitReInstance",_initReInstance,1},
  {"InitBillInstance",_initBillInstance,1},
  {"InitSessionManagerInstance",_initSessionManagerInstance,1},
+ {"InitStatisticsInstance",_initStatInstance,1},
  {"UpdateRule",_updaterule,1},
  {"DeleteRule",_deleterule,1},
  {"RuleRun",_rulerun,1},
