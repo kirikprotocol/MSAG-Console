932ec178a1c132d8359fb9b60018d8c5abbbbc4b
loomox <>
2006-03-22 11:42:52 +0000

    *** empty log message ***

---
+++ src/scag/re/CommandBrige.cpp
@@ -0,0 +1,64 @@
+
+#include "CommandBrige.h"
+#include <scag/sessions/Session.h>
+
+namespace scag { namespace re {
+
+
+void CommandBrige::makeTrafficEvent(SmppCommand& command, int handlerType, scag::sessions::CSessionPrimaryKey& sessionPrimaryKey, SACC_TRAFFIC_INFO_EVENT_t& ev)
+{
+    ev.Header.cCommandId = handlerType;
+        
+    ev.Header.cProtocolId = 1;
+    ev.Header.iServiceId = command.getServiceId();
+    ev.Header.iServiceProviderId = 1;
+
+    long now;
+    time(&now);
+
+    ev.Header.lDateTime = now;
+
+    const char * str = getAbonentAddr(command).toString().c_str();
+    sprintf((char *)ev.Header.pAbonentNumber,"%s",str);
+
+    ev.Header.sCommandStatus = 1;
+    ev.Header.sEventType = 1;
+
+    ev.iOperatorId = 1;
+
+    std::string unicodeSTR = getMessageBody(command);
+    memcpy(ev.pMessageText, unicodeSTR.data(), unicodeSTR.size()); 
+
+    sprintf((char *)ev.pSessionKey,"%s/%d", sessionPrimaryKey.abonentAddr.toString().c_str(),sessionPrimaryKey.BornMicrotime);
+}
+
+void CommandBrige::makeBillEvent(int serviceId, std::string& abonentAddr, int billCommand, SACC_BILLING_INFO_EVENT_t& ev)
+{
+    ev.Header.cCommandId = billCommand;
+
+    ev.Header.cProtocolId = 1;
+    ev.Header.iServiceId = serviceId;
+    ev.Header.iServiceProviderId = 1;
+
+    long now;
+    time(&now);
+
+    ev.Header.lDateTime = now;
+
+    sprintf((char *)ev.Header.pAbonentNumber,"%s",abonentAddr.c_str());
+
+    ev.Header.sCommandStatus = 1;
+    ev.Header.sEventType = 1;
+
+
+    ev.fBillingSumm = 100;
+    ev.iMediaResourceType = 100;
+    ev.iOperatorId = 1;
+    ev.iPriceCatId = 1;
+    sprintf((char *)ev.pBillingCurrency,"%s","$");
+}
+
+
+}}
+
+
