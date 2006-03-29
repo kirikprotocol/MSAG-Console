70175dccf5ae752e3dcd459d0c9fbf837a76efe2
gvroman <>
2006-03-29 09:26:02 +0000

    version number header

---
+++ src/inman/version.hpp
@@ -0,0 +1,11 @@
+#ident "$Id$"
+
+#ifndef __SMSC_INMAN_VERSION__
+#define __SMSC_INMAN_VERSION__
+
+const unsigned INMAN_VER_HIGH    = 0;    //major verison
+const unsigned INMAN_VER_LOW     = 3;    //minor version
+const unsigned INMAN_VER_FIX     = 0;    //bug fix version
+
+#endif /* __SMSC_INMAN_VERSION__ */
+
