3614983bd56c5d789c57bd1e0b08ec83b4005471
starkom <>
2006-03-30 08:41:38 +0000

    fixed bug with null-pointer exception in editing user with empty roles set

--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/smsc/users/UsersAdd.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/smsc/users/UsersAdd.java
@@ -8,73 +8,63 @@ package ru.novosoft.smsc.jsp.smsc.users;
 
 import ru.novosoft.smsc.admin.journal.Actions;
 import ru.novosoft.smsc.admin.journal.SubjectTypes;
-import ru.novosoft.smsc.admin.users.User;
 import ru.novosoft.smsc.admin.preferences.UserPreferences;
+import ru.novosoft.smsc.admin.users.User;
 import ru.novosoft.smsc.jsp.SMSCErrors;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public class UsersAdd
-        extends UsersEditBean
-{
-	protected int init(List errors)
-	{
-		int result = super.init(errors);
-		if (result != RESULT_OK) return result;
+        extends UsersEditBean {
+    protected int init(List errors) {
+        int result = super.init(errors);
+        if (result != RESULT_OK) return result;
 
-		if (login == null || firstName == null || lastName == null)
-		{
-			login = "";
-			password = "";
-			confirmPassword = "";
-			setRoles(new String[0]);
-			prefsNames = UserPreferences.getDefaultPrefsNames();
-			prefsValues = UserPreferences.getDefaultPrefsValues();
-			firstName = "";
-			lastName = "";
-			dept = "";
-			workPhone = "";
-			homePhone = "";
-			cellPhone = "";
-			email = "";
-		}
-		return RESULT_OK;
-	}
+        if (login == null || firstName == null || lastName == null) {
+            login = "";
+            password = "";
+            confirmPassword = "";
+            setRoles(new String[0]);
+            prefsNames = UserPreferences.getDefaultPrefsNames();
+            prefsValues = UserPreferences.getDefaultPrefsValues();
+            firstName = "";
+            lastName = "";
+            dept = "";
+            workPhone = "";
+            homePhone = "";
+            cellPhone = "";
+            email = "";
+        }
+        return RESULT_OK;
+    }
 
-	protected int save(final HttpServletRequest request)
-	{
-		if (login == null || login.trim().length() == 0)
-		return error(SMSCErrors.error.users.loginNotDefined);
+    protected int save(final HttpServletRequest request) {
+        if (login == null || login.trim().length() == 0)
+            return error(SMSCErrors.error.users.loginNotDefined);
 
-		User user = userManager.getUser(login);
-		if (user == null)
-		{
-			if (password == null || password.trim().length() == 0 || confirmPassword == null || confirmPassword.length() == 0)
-				return error(SMSCErrors.error.users.passwordNotDefined);
-			if (!password.trim().equals(confirmPassword.trim()))
-				return error(SMSCErrors.error.users.passwordNotConfirmed);
+        User user = userManager.getUser(login);
+        if (user == null) {
+            if (password == null || password.trim().length() == 0 || confirmPassword == null || confirmPassword.length() == 0)
+                return error(SMSCErrors.error.users.passwordNotDefined);
+            if (!password.trim().equals(confirmPassword.trim()))
+                return error(SMSCErrors.error.users.passwordNotConfirmed);
 
-			UserPreferences prefs = new UserPreferences();
-			prefs.setValues(prefsNames, prefsValues);
-			if (userManager.addUser(new User(login, password.trim(), roles, firstName, lastName, dept, workPhone, homePhone, cellPhone, email, prefs)))
-			{
+            UserPreferences prefs = new UserPreferences();
+            prefs.setValues(prefsNames, prefsValues);
+            if (userManager.addUser(new User(login, password.trim(), roles, firstName, lastName, dept, workPhone, homePhone, cellPhone, email, prefs))) {
                 request.getSession().setAttribute("USER_LOGIN_ADD_EDIT", login);
-				journalAppend(SubjectTypes.TYPE_user, login, Actions.ACTION_ADD);
-				appContext.getStatuses().setUsersChanged(true);
-				return RESULT_DONE;
-			}
-		else
-			return error(SMSCErrors.error.unknown);
-		}
-		else
-		{
-			return error(SMSCErrors.error.users.userAlreadyExists, login);
-		}
-	}
+                journalAppend(SubjectTypes.TYPE_user, login, Actions.ACTION_ADD);
+                appContext.getStatuses().setUsersChanged(true);
+                return RESULT_DONE;
+            } else
+                return error(SMSCErrors.error.unknown);
+        } else {
+            return error(SMSCErrors.error.users.userAlreadyExists, login);
+        }
+    }
 
-  public boolean isNew()
-  {
-    return true;
-  }
+    public boolean isNew() {
+        return true;
+    }
 }
--- webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/smsc/users/UsersEdit.java
+++ webapps/smsc/WEB-INF/src/ru/novosoft/smsc/jsp/smsc/users/UsersEdit.java
@@ -8,99 +8,87 @@ package ru.novosoft.smsc.jsp.smsc.users;
 
 import ru.novosoft.smsc.admin.journal.Actions;
 import ru.novosoft.smsc.admin.journal.SubjectTypes;
-import ru.novosoft.smsc.admin.users.User;
 import ru.novosoft.smsc.admin.preferences.UserPreferences;
+import ru.novosoft.smsc.admin.users.User;
 import ru.novosoft.smsc.jsp.SMSCErrors;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
-public class UsersEdit extends UsersEditBean
-{
-	protected int init(List errors)
-	{
-		int result = super.init(errors);
-		if (result != RESULT_OK) return result;
+public class UsersEdit extends UsersEditBean {
+    protected int init(List errors) {
+        int result = super.init(errors);
+        if (result != RESULT_OK) return result;
 
-		if (firstName == null || lastName == null)
-		{
-			if (login == null || login.trim().length() == 0)
-			{
-				setRoles(new String[0]);
-				prefsNames = UserPreferences.getDefaultPrefsNames();
-				prefsValues = UserPreferences.getDefaultPrefsValues();
-				login = password = confirmPassword = firstName = lastName = dept = workPhone = homePhone = cellPhone = email = "";
-				return error(SMSCErrors.error.users.loginNotDefined);
-			}
-			else
-			{
-				User user = userManager.getUser(login);
-				if (user == null)
-				{
-					setRoles(new String[0]);
-					prefsNames = UserPreferences.getDefaultPrefsNames();
-					prefsValues = UserPreferences.getDefaultPrefsValues();
-					password = confirmPassword = firstName = lastName = dept = workPhone = homePhone = cellPhone = email = "";
-					return error(SMSCErrors.error.users.userNotFound, login);
-				}
-				else
-				{
-					password = "";
-					confirmPassword = "";
-					setRoles((String[]) user.getRoles().toArray(new String[0]));
-					prefsNames  = UserPreferences.getDefaultPrefsNames();
-					prefsValues = user.getPrefs().getPrefsValues();
-					firstName   = user.getFirstName();
-					lastName    = user.getLastName();
-					dept        = user.getDept();
-					workPhone   = user.getWorkPhone();
-					homePhone   = user.getHomePhone();
-					cellPhone   = user.getCellPhone();
-					email       = user.getEmail();
-				}
-			}
-		}
-		return RESULT_OK;
-	}
-
-  protected int save(final HttpServletRequest request)
-  {
-    if (login == null || login.trim().length() == 0)
-      return error(SMSCErrors.error.users.loginNotDefined);
-
-    User user = userManager.getUser(login);
-    if (user == null) { // add new user
-      return error(SMSCErrors.error.users.userNotFound, login);
+        if (firstName == null || lastName == null) {
+            if (login == null || login.trim().length() == 0) {
+                setRoles(new String[0]);
+                prefsNames = UserPreferences.getDefaultPrefsNames();
+                prefsValues = UserPreferences.getDefaultPrefsValues();
+                login = password = confirmPassword = firstName = lastName = dept = workPhone = homePhone = cellPhone = email = "";
+                return error(SMSCErrors.error.users.loginNotDefined);
+            } else {
+                User user = userManager.getUser(login);
+                if (user == null) {
+                    setRoles(new String[0]);
+                    prefsNames = UserPreferences.getDefaultPrefsNames();
+                    prefsValues = UserPreferences.getDefaultPrefsValues();
+                    password = confirmPassword = firstName = lastName = dept = workPhone = homePhone = cellPhone = email = "";
+                    return error(SMSCErrors.error.users.userNotFound, login);
+                } else {
+                    password = "";
+                    confirmPassword = "";
+                    setRoles((String[]) user.getRoles().toArray(new String[0]));
+                    prefsNames = UserPreferences.getDefaultPrefsNames();
+                    prefsValues = user.getPrefs().getPrefsValues();
+                    firstName = user.getFirstName();
+                    lastName = user.getLastName();
+                    dept = user.getDept();
+                    workPhone = user.getWorkPhone();
+                    homePhone = user.getHomePhone();
+                    cellPhone = user.getCellPhone();
+                    email = user.getEmail();
+                }
+            }
+        }
+        return RESULT_OK;
     }
-    else {
-      if ((password == null || password.trim().length() == 0) && (confirmPassword == null || confirmPassword.length() == 0)) {
-        password = confirmPassword = user.getPassword().trim();
-      }
-      if (password == null || confirmPassword == null || !password.trim().equals(confirmPassword.trim()))
-        return error(SMSCErrors.error.users.passwordNotConfirmed);
 
-		user.setLogin(login);
-		user.setPassword(password);
-		user.setRoles(Arrays.asList(roles));
-		user.setFirstName(firstName);
-		user.setLastName(lastName);
-		user.setDept(dept);
-		user.setWorkPhone(workPhone);
-		user.setHomePhone(homePhone);
-		user.setCellPhone(cellPhone);
-		user.setEmail(email);
-		user.getPrefs().setValues(prefsNames, prefsValues);
-      journalAppend(SubjectTypes.TYPE_user, login, Actions.ACTION_MODIFY);
-      appContext.getStatuses().setUsersChanged(true);
-      request.getSession().setAttribute("USER_LOGIN_ADD_EDIT", login);
-      return RESULT_DONE;
+    protected int save(final HttpServletRequest request) {
+        if (login == null || login.trim().length() == 0)
+            return error(SMSCErrors.error.users.loginNotDefined);
+
+        User user = userManager.getUser(login);
+        if (user == null) { // add new user
+            return error(SMSCErrors.error.users.userNotFound, login);
+        } else {
+            if ((password == null || password.trim().length() == 0) && (confirmPassword == null || confirmPassword.length() == 0)) {
+                password = confirmPassword = user.getPassword().trim();
+            }
+            if (password == null || confirmPassword == null || !password.trim().equals(confirmPassword.trim()))
+                return error(SMSCErrors.error.users.passwordNotConfirmed);
+
+            user.setLogin(login);
+            user.setPassword(password);
+            user.setRoles(Arrays.asList(roles == null ? new String[0] : roles));
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setDept(dept);
+            user.setWorkPhone(workPhone);
+            user.setHomePhone(homePhone);
+            user.setCellPhone(cellPhone);
+            user.setEmail(email);
+            user.getPrefs().setValues(prefsNames, prefsValues);
+            journalAppend(SubjectTypes.TYPE_user, login, Actions.ACTION_MODIFY);
+            appContext.getStatuses().setUsersChanged(true);
+            request.getSession().setAttribute("USER_LOGIN_ADD_EDIT", login);
+            return RESULT_DONE;
+        }
     }
-  }
 
-  public boolean isNew()
-  {
-    return false;
-  }
+    public boolean isNew() {
+        return false;
+    }
 
 }
