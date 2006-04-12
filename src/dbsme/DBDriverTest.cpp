158391c7822f3babd58baf9ff7a367a633bacc1a
makar <>
2006-04-12 09:00:20 +0000

    DB DRV testing

--- src/dbsme/DBDriverTest.cpp
+++ src/dbsme/DBDriverTest.cpp
@@ -8,9 +8,106 @@
 #include <db/DataSource.h>
 #include <db/DataSourceLoader.h>
 
+#include <core/threads/ThreadedTask.hpp>
+#include <core/threads/ThreadPool.hpp>
+
 #include <exception>
 using std::exception;
+using namespace smsc::core::threads;
+using namespace smsc::db;
 
+static smsc::logger::Logger *logger = 0;
+
+static const char* SQL_1 = "INSERT INTO SMS_TEST (TEMP, STR, DT) VALUES (:TEMP, :STR, :DT)";
+static const char* SQL_2 = "SELECT * FROM SMS_TEST";
+static const char* SQL_3 = "UPDATE SMS_TEST SET TEMP=TEMP";
+
+class Executor : public ThreadedTask
+{
+private:
+
+    EventMonitor waitMonitor;
+    DataSource* ds;
+    int threadNum;
+    bool threadIdle;
+
+public:
+
+    Executor(DataSource* _ds, int thn, bool thi) 
+        : ThreadedTask(), ds(_ds), threadNum(thn), threadIdle(thi) {};
+
+    virtual const char* taskName() { return "Executor"; };
+
+    virtual void stop() 
+    {
+        ThreadedTask::stop();
+        MutexGuard guard(waitMonitor);
+        waitMonitor.notify();
+    };
+
+    int Execute() 
+    {
+        try
+        {
+            while (!isStopping) 
+            {
+                Connection* connection = ds->getConnection();
+                if (!connection) {
+                    smsc_log_warn(logger, "%d Get connection failed", threadNum);
+                    return -1;
+                }
+
+                if (!threadIdle) 
+                {
+                    {
+                        std::auto_ptr<Statement> s1guard(connection->createStatement(SQL_1));
+                        Statement* statement1 = s1guard.get();
+
+                        statement1->setInt64(1, 0xffffffffffffffffLL);
+                        statement1->setString(2, "Testing string value");
+                        statement1->setDateTime(3, 1000000);
+                        statement1->executeUpdate();
+
+                        connection->commit();
+                    }
+                    {
+                        std::auto_ptr<Statement> s2guard(connection->createStatement(SQL_2));
+                        Statement* statement2 = s2guard.get();
+                        if (statement2) 
+                        {
+                            std::auto_ptr<ResultSet> rsguard(statement2->executeQuery());
+                            ResultSet* rs = rsguard.get(); 
+
+                            int count = 0;
+                            while (rs && rs->fetchNext()) 
+                            {
+                                int64_t inum64 = rs->getInt64(1);
+                                //smsc_log_debug(logger, "%d 64i : %llx value selected", threadNum, inum64);
+
+                                if (!rs->isNull(2))
+                                {
+                                    const char* str = rs->getString(2);
+                                    //smsc_log_debug(logger, "%d str %d: \"%s\" value selected", threadNum, ++count, str);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                ds->freeConnection(connection);
+
+                MutexGuard guard(waitMonitor);
+                waitMonitor.wait(100);
+            }
+        }
+        catch (std::exception& exc) {
+            smsc_log_error(logger, "Executor %d error: %s", threadNum, exc.what());
+        }
+        return 0;
+    };
+};
+
+const int MAX_DB_POOL_THREADS = 10;
 int main(void) 
 {
     using namespace smsc::db;
@@ -21,100 +118,32 @@ int main(void)
 
     const char* OCI_DS_FACTORY_IDENTITY = "OCI";
     
-    const char* sql1 = "INSERT INTO SMS_TEST (TEMP, STR, DT)"
-                        " VALUES (:TEMP, :STR, :DT)";
-    const char* sql2 = "SELECT * FROM SMS_TEST";
-    const char* sql3 = "UPDATE SMS_TEST SET TEMP=TEMP";
-    
-    DataSourceLoader::loadupDataSourceFactory(
-        "../db/oci/libdb_oci.so", OCI_DS_FACTORY_IDENTITY);
+    smsc::logger::Logger::Init();
+    logger = Logger::getInstance("DBSme.test");
+
+    DataSourceLoader::loadupDataSourceFactory("../db/oci/libdb_oci.so", OCI_DS_FACTORY_IDENTITY);
 
     try 
     {
         Manager::init("config.xml");
-        DataSource* ds = 
-            DataSourceFactory::getDataSource(OCI_DS_FACTORY_IDENTITY);
+        DataSource* ds = DataSourceFactory::getDataSource(OCI_DS_FACTORY_IDENTITY);
         if (ds)
         {
-            ConfigView* config = 
-                new ConfigView(Manager::getInstance(),
-                    "DBSme.DataProviders.SampleDataProvider1.DataSource");
-            
+            ConfigView* config = new ConfigView(Manager::getInstance(), "DBSme.DataProviders.DataSource");
             ds->init(config);
-            printf("Init Ok !\n");
-            Connection* connection = ds->getConnection();
-            if (connection)
-            {
-                Statement* statement1 = connection->createStatement(sql1);
-                if (statement1)
-                {
-                    statement1->setInt64(1, 0xffffffffffffffffLL);
-                    statement1->setString(2, "Testing string value");
-                    statement1->setDateTime(3, 1000000);
-                    statement1->executeUpdate();
-                     
-                    /*for (int i=0; i<10; i++)
-                    {
-                        statement1->setInt64(1, -(0x0123456789000000+i));
-                        statement1->setString(2, "Testing string value");
-                        statement1->setDateTime(3, i*1000000);
-                        int rows = statement1->executeUpdate();
-                    }*/
-                    
-                    connection->commit();
-                    delete statement1;
-                }
-                
-                Statement* statement2 = connection->createStatement(sql2);
-                if (statement2)
-                {
-                    ResultSet* rs = statement2->executeQuery();
-                    if (rs)
-                    {
-                        int count = 0;
-                        while (rs->fetchNext())
-                        {
-                            /*int8_t inum8 = rs->getInt8(1);
-                            printf("8i  : %d value selected\n", inum8);
-                            int16_t inum16 = rs->getInt16(1);
-                            printf("16i : %d value selected\n", inum16);
-                            int32_t inum32 = rs->getInt32(1);
-                            printf("32i : %d value selected\n", inum32);*/
-                            int64_t inum64 = rs->getInt64(1);
-                            printf("64i : %llx value selected\n", inum64);
+            smsc_log_info(logger, "DS Init Ok, starting threads...");
 
-                            /*uint8_t unum8 = rs->getUint8(1);
-                            printf("8u  : %d value selected\n", unum8);
-                            uint16_t unum16 = rs->getUint16(1);
-                            printf("16u : %d value selected\n", unum16);
-                            uint32_t unum32 = rs->getUint32(1);
-                            printf("32u : %d value selected\n", unum32);*/
-                            /*uint64_t unum64 = rs->getUint64(1);
-                            printf("64u : %llx value selected\n", unum64);*/
-
-                            if (!rs->isNull(2))
-                            {
-                                const char* str = rs->getString(2);
-                                printf("str %d: \"%s\" value selected\n", 
-                                       ++count, str);
-                            }
-                        }
-                        delete rs;
-                    }
-                    delete statement2;
-                }
-                
-                /*Statement* statement3 = connection->createStatement(sql3);
-                if (statement3)
-                {
-                    int wdTimer = ds->startTimer(connection, 10);
-                    int rows = statement3->executeUpdate();
-                    ds->stopTimer(wdTimer);
-                    delete statement3;
-                }*/
-
-                ds->freeConnection(connection);
+            ThreadPool pool; pool.setMaxThreads(MAX_DB_POOL_THREADS);
+            for (int i=0; i<MAX_DB_POOL_THREADS; i++) {
+                pool.startTask(new Executor(ds, i, (bool)(i%2)));
             }
+
+            Event stopEvent;
+            stopEvent.Wait(300000);
+            smsc_log_info(logger, "Stopping...");
+            pool.shutdown();
+            smsc_log_info(logger, "Stopped, deliting ds...");
+
             delete ds;
             if (config) delete config;
         }
@@ -125,5 +154,6 @@ int main(void)
         return -1;
     }
     
+    smsc_log_info(logger, "All stopped ok.");
     return 0;
 }
