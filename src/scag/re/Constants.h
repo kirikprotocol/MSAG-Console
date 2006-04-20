50d88b95a992017b0e8ac54b2292bb111980090c
loomox <>
2006-04-20 11:43:30 +0000

    *** empty log message ***

---
+++ src/scag/re/Constants.h
@@ -0,0 +1,28 @@
+#ifndef _SCAG_RE_CONSTANTS_
+#define _SCAG_RE_CONSTANTS_
+
+namespace scag { namespace re {
+
+
+namespace Const
+{
+
+
+enum TransportConstants
+{
+
+    USSD = 1,
+    SMS  = 2,
+    MMS  = 3,
+    WAP  = 4,
+    HTTP = 5
+};
+
+
+
+}
+
+
+}}
+
+#endif
