afd87f3f2ef382b25644fc8cd94b4a92c2789e6e
gvroman <>
2006-04-24 07:46:18 +0000

    asn1c-0.9.19 xer patch

---
+++ src/inman/codec_inc/map/InformationNotAvailableParam.h
@@ -0,0 +1,41 @@
+#ident "$Id$"
+
+#ifndef	_InformationNotAvailableParam_H_
+#define	_InformationNotAvailableParam_H_
+
+
+#include <asn_application.h>
+
+/* Including external dependencies */
+#include <constr_SEQUENCE.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Forward declarations */
+struct ExtensionContainer;
+
+/* InformationNotAvailableParam */
+typedef struct InformationNotAvailableParam {
+	struct ExtensionContainer	*extensionContainer	/* OPTIONAL */;
+	/*
+	 * This type is extensible,
+	 * possible extensions are below.
+	 */
+	
+	/* Context for parsing across buffer boundaries */
+	asn_struct_ctx_t _asn_ctx;
+} InformationNotAvailableParam_t;
+
+/* Implementation */
+extern asn_TYPE_descriptor_t asn_DEF_InformationNotAvailableParam;
+
+/* Referred external types */
+#include <ExtensionContainer.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _InformationNotAvailableParam_H_ */
---
+++ src/inman/codec_inc/map/TeleservNotProvParam.h
@@ -0,0 +1,41 @@
+#ident "$Id$"
+
+#ifndef	_TeleservNotProvParam_H_
+#define	_TeleservNotProvParam_H_
+
+
+#include <asn_application.h>
+
+/* Including external dependencies */
+#include <constr_SEQUENCE.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Forward declarations */
+struct ExtensionContainer;
+
+/* TeleservNotProvParam */
+typedef struct TeleservNotProvParam {
+	struct ExtensionContainer	*extensionContainer	/* OPTIONAL */;
+	/*
+	 * This type is extensible,
+	 * possible extensions are below.
+	 */
+	
+	/* Context for parsing across buffer boundaries */
+	asn_struct_ctx_t _asn_ctx;
+} TeleservNotProvParam_t;
+
+/* Implementation */
+extern asn_TYPE_descriptor_t asn_DEF_TeleservNotProvParam;
+
+/* Referred external types */
+#include <ExtensionContainer.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _TeleservNotProvParam_H_ */
