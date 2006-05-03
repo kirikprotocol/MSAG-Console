ddae5fdd0f70e97927ca0684cc1ce0e5fa192b6f
firs <>
2006-05-03 09:10:40 +0000

    *** empty log message ***

--- src/scag/re/actions/ActionLog.cpp
+++ src/scag/re/actions/ActionLog.cpp
@@ -13,36 +13,33 @@ bool ActionLog::FinishXMLSubSection(const std::string& name)
     return true;
 }
 
+std::string ActionLog::ttToStr(TransportType t)
+{
+    if(t == 1)
+        return "smpp";
+    else if(t == 2)
+        return "http";
+    else if(t == 3)
+        return "mms";
+    return "unknown";
+}
+
 void ActionLog::init(const SectionParams& params,PropertyObject propertyObject)
 {
-    logger = Logger::getInstance("scag.re");
-
-/*
-    if (!params.Exists("level")) throw SCAGException("Action 'log': missing 'level' parameter");
-    if (!params.Exists("category")) throw SCAGException("Action 'log': missing 'category' parameter");
-    if (!params.Exists("message")) throw SCAGException("Action 'log': missing 'message' parameter");
-*/
-    FieldType ft;
-    std::string temp;
     bool bExist;
 
-    ft = CheckParameter(params, propertyObject, "log", "level", true, true, temp, bExist);
-    std::string sLevel = ConvertWStrToStr(temp);
+    if (!params.Exists("category")) throw SCAGException("Action 'log': missing 'category' parameter");
 
-    if (sLevel == "error") level = lgError;
-    else if (sLevel == "warn") level = lgWarning;
-    else if (sLevel == "info") level = lgInfo;
-    else if (sLevel == "debug") level = lgDebug;
-    else throw SCAGException("Action 'log': invalid value for 'level' parameter");
+    const char * name = 0;
+    strCategory = ConvertWStrToStr(params["category"]);
+    std::string s = "rule." + ttToStr(propertyObject.transport) + "." + strCategory;
 
-    ftCategory = CheckParameter(params, propertyObject, "log", "category", true, true, wstrCategory, bExist);
-    strCategory = ConvertWStrToStr(wstrCategory);
+    logger = Logger::getInstance(s.c_str());
 
     ftMessage = CheckParameter(params, propertyObject, "log", "message", true, true, wstrMsg, bExist);
     strMsg = ConvertWStrToStr(wstrMsg);
 
-
-    smsc_log_debug(logger,"Action 'log':: init...");
+    smsc_log_debug(logger,"Action 'log':: inited... level=%d, category %s, transport=%s", level, strCategory.c_str(), ttToStr(propertyObject.transport).c_str());
 }
 
 bool ActionLog::run(ActionContext& context)
@@ -50,55 +47,28 @@ bool ActionLog::run(ActionContext& context)
     Property * p1 = 0;
     Property * p2 = 0;
 
-    std::string s1,s2;
-    std::string wstr;
+    std::string s2;
 
-    if (ftCategory != ftUnknown) 
+    if (ftMessage != ftUnknown)  
     {
-        p1 = context.getProperty(strCategory);
-        if (!p1) 
+        if (!(p2 = context.getProperty(strMsg))) 
         {
-            smsc_log_warn(logger,"Action 'log': invalid property '%s' to log", strCategory.c_str());
-            return true;
-        }
-        wstr = p1->getStr();
-        s1 = FormatWStr(wstr);
-    } else s1 = FormatWStr(wstrCategory);
-
-    if (ftMessage!=ftUnknown)  
-    {
-        p2 = context.getProperty(strMsg);
-
-        if (!p2) 
-        {
-            smsc_log_warn(logger,"Action 'log': invalid property '%s' to log", FormatWStr(wstrMsg).c_str());
+            smsc_log_warn(logger,"Action 'log': invalid property '%s' to log", strMsg.c_str());
             return true;
         }
 
-        wstr = p2->getStr();
-        s2 = FormatWStr(wstr);
-
-    } else s2 = FormatWStr(wstrMsg);
-
-    std::string logstr = s1 + ": " + s2;
+        s2 = ConvertWStrToStr(p2->getStr());
+    } 
+    else
+        s2 = strMsg;
 
     switch (level) 
     {
-    case lgError: 
-        smsc_log_error(logger,logstr);
-        break;
-    case lgWarning:
-        smsc_log_warn(logger,logstr);
-        break;
-
-    case lgInfo:
-        smsc_log_info(logger,logstr);
-        break;
-    case lgDebug:
-        smsc_log_debug(logger,logstr);
-        break;
+        case lgError: smsc_log_error(logger,s2); break;
+        case lgWarning: smsc_log_warn(logger,s2); break;
+        case lgInfo: smsc_log_info(logger,s2); break;
+        case lgDebug: smsc_log_debug(logger,s2); break;
     }
-
 
     return true;
 }
@@ -107,8 +77,5 @@ ActionLog::~ActionLog()
 {
     //smsc_log_debug(logger, "'log' action released");
 }
-
-
-
 
 }}}
