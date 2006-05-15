2e5f03475cc6bb38d1d90680897b50fb932605e1
loomox <>
2006-05-15 11:50:36 +0000

    *** empty log message ***

--- src/scag/scag.cpp
+++ src/scag/scag.cpp
@@ -586,8 +586,8 @@ void Scag::init()
   sm.releaseSession(sessionPtr);
 
   ////////////////////////// FOR TEST 
-   */                                
-                      
+                                   
+    */                  
                                     
     //********************************************************
     //************** HttpManager initialization **************
