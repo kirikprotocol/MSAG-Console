95540b6c537813e3e3da4994053278f341c797b9
gvroman <>
2006-05-23 09:53:35 +0000

    MAP ATSI dialog

---
+++ src/inman/inap/map_atih/DlgMapATSI.cpp
@@ -0,0 +1,214 @@
+static char const ident[] = "$Id$";
+
+#include <assert.h>
+
+#include "inman/inap/map_atih/DlgMapATSI.hpp"
+
+namespace smsc {
+namespace inman {
+namespace inap {
+namespace atih {
+
+using smsc::inman::comp::atih::MAPATIH_OpCode;
+
+/* ************************************************************************** *
+ * class MapATSIDlg implementation:
+ * ************************************************************************** */
+MapATSIDlg::MapATSIDlg(SSNSession* pSession, ATSIhandler * atsi_handler,
+                        Logger * uselog/* = NULL*/)
+    : atsiHdl(atsi_handler), session(pSession), atsiId(0), dialog(NULL)
+    , logger(uselog)
+{
+    assert(atsi_handler && pSession);
+    _atsiState.value = 0;
+    if (!logger)
+        logger = Logger::getInstance("smsc.inman.Inap.MapATSIDlg");
+}
+
+MapATSIDlg::~MapATSIDlg()
+{
+    endATSI();
+}
+
+void MapATSIDlg::endATSI(void)
+{
+    MutexGuard  grd(_sync);
+    atsiHdl = NULL;
+    endTCap();
+}
+
+/* ------------------------------------------------------------------------ *
+ * ATSIcontractor interface
+ * ------------------------------------------------------------------------ */
+void MapATSIDlg::subsciptionInterrogation(const char * subcr_adr,
+                    bool imsi/* = false*/, USHORT_T timeout/* = 0*/) throw(CustomException)
+{
+    MutexGuard  grd(_sync);
+    TonNpiAddress   tnAdr;
+    if (!tnAdr.fromText(subcr_adr))
+        throw CustomException("inalid subscriberID", -1, subcr_adr);
+
+    dialog = session->openDialog(tnAdr.getSignals());
+    if (!dialog)
+        throw CustomException("MapATSI[0]: Unable to create TC Dialog");
+    if (timeout)
+        dialog->setInvokeTimeout(timeout);
+    dialog->addListener(this);
+    atsiId = (unsigned)dialog->getId();
+
+    ATSIArg     arg;
+    arg.setSCFaddress(session->getOwnAdr());
+    if (!imsi)
+        arg.setSubscriberId(tnAdr);
+    else
+        arg.setSubscriberId(subcr_adr, imsi);
+    arg.setRequestedCSI(RequestedCAMEL_SubscriptionInfo_o_CSI);
+
+    Invoke* op = dialog->initInvoke(MAPATIH_OpCode::anyTimeSubscriptionInterrogation, this);
+    op->setParam(&arg);
+    dialog->sendInvoke(op);
+
+    dialog->beginDialog();
+    _atsiState.s.ctrInited = MAP_OPER_INITED;
+}
+
+/* ------------------------------------------------------------------------ *
+ * InvokeListener interface
+ * ------------------------------------------------------------------------ */
+void MapATSIDlg::onInvokeResult(Invoke* op, TcapEntity* res)
+{
+    unsigned do_end = 0;
+    {
+        MutexGuard  grd(_sync);
+        smsc_log_debug(logger, "MapATSI[%u]: Invoke[%u:%u] got a returnResult: %u",
+            atsiId, (unsigned)op->getId(), (unsigned)op->getOpcode(),
+            (unsigned)res->getOpcode());
+
+        _atsiState.s.ctrResulted = 1;
+        endTCap();
+        ATSIRes * resComp = static_cast<ATSIRes *>(res->getParam());
+        atsiHdl->onATSIResult(resComp);
+        do_end  = _atsiState.s.ctrFinished;
+    }
+    if (do_end)
+        atsiHdl->onEndATSI(0, smsc::inman::errOk);
+}
+
+//Called if Operation got ResultError
+void MapATSIDlg::onInvokeError(Invoke *op, TcapEntity * resE)
+{
+    {
+        MutexGuard  grd(_sync);
+        smsc_log_error(logger, "MapATSI[%u]: Invoke[%u:%u] got a returnError: %u",
+            atsiId, (unsigned)op->getId(), (unsigned)op->getOpcode(),
+            (unsigned)resE->getOpcode());
+
+        _atsiState.s.ctrInited = MAP_OPER_FAIL;
+        endTCap();
+    }
+    atsiHdl->onEndATSI(resE->getOpcode(), smsc::inman::errMAP);
+}
+
+//Called if Operation got L_CANCEL, possibly while waiting result
+void MapATSIDlg::onInvokeLCancel(Invoke *op)
+{
+    {
+        MutexGuard  grd(_sync);
+        smsc_log_error(logger, "MapATSI[%u]: Invoke[%u:%u] got a LCancel",
+            atsiId, (unsigned)op->getId(), (unsigned)op->getOpcode());
+        endTCap();
+    }
+    atsiHdl->onEndATSI(MapATSIDlg::atsiServiceResponse, smsc::inman::errMAPuser);
+}
+
+/* ------------------------------------------------------------------------ *
+ * DialogListener interface
+ * ------------------------------------------------------------------------ */
+//SCF sent ContinueDialog.
+void MapATSIDlg::onDialogContinue(bool compPresent)
+{
+    MutexGuard  grd(_sync);
+    if (!compPresent)
+        smsc_log_error(logger, "MapATSI[%u]: missing component in TC_CONT_IND", atsiId);
+     //else wait for ongoing Invoke result/error
+    return;
+}
+
+//TCAP indicates DialogPAbort: either due to TC layer error on SCF/HLR side or
+//because of local TC dialog timeout is expired (no T_END_IND from HLR).
+void MapATSIDlg::onDialogPAbort(UCHAR_T abortCause)
+{
+    {
+        MutexGuard  grd(_sync);
+        _atsiState.s.ctrAborted = 1;
+        smsc_log_error(logger, "MapATSI[%u]: P_ABORT at state 0x%x", atsiId, _atsiState.value);
+        endTCap();
+    }
+    atsiHdl->onEndATSI(abortCause, smsc::inman::errTCAP);
+}
+
+//SCF sent DialogUAbort (some logic error on SCF side).
+void MapATSIDlg::onDialogUAbort(USHORT_T abortInfo_len, UCHAR_T *pAbortInfo,
+                                    USHORT_T userInfo_len, UCHAR_T *pUserInfo)
+{
+    {
+        MutexGuard  grd(_sync);
+        _atsiState.s.ctrAborted = 1;
+        smsc_log_error(logger, "MapATSI[%u]: U_ABORT at state 0x%x", atsiId, _atsiState.value);
+        endTCap();
+    }
+    atsiHdl->onEndATSI(Dialog::tcUserGeneralError, smsc::inman::errTCuser);
+}
+
+//SCF sent DialogEnd, it's either succsesfull contract completion,
+//or some logic error (f.ex. timeout expiration) on SSF side.
+void MapATSIDlg::onDialogREnd(bool compPresent)
+{
+    InmanErrorType layer = smsc::inman::errOk;
+    unsigned char  errcode = 0;
+    {
+        MutexGuard  grd(_sync);
+        _atsiState.s.ctrFinished = 1;
+        if (!compPresent) {
+            endTCap();
+            if (!_atsiState.s.ctrResulted) {
+                smsc_log_error(logger, "MapATSI[%u]: T_END_IND, state 0x%x", atsiId, _atsiState.value);
+                layer = smsc::inman::errMAPuser;
+                errcode = MapATSIDlg::atsiServiceResponse;
+            }
+        }
+    }
+    if (!compPresent)
+        atsiHdl->onEndATSI(errcode, layer);
+    //else wait for ongoing Invoke result/error
+}
+
+/* ------------------------------------------------------------------------ *
+ * Private/protected methods
+ * ------------------------------------------------------------------------ */
+//ends TC dialog, releases Dialog()
+void MapATSIDlg::endTCap(void)
+{
+    if (dialog) {
+        dialog->removeListener(this);
+        if (!(dialog->getState().value & TC_DLG_CLOSED_MASK)) {
+            //see 3GPP 29.078 14.1.2.1.3 smsSSF-to-gsmSCF SMS related messages
+            try {
+                dialog->endDialog(); // do TC_BasicEnd if still active
+                smsc_log_debug(logger, "MapATSI[%u]: T_END_REQ, state: 0x%x", atsiId,
+                                _atsiState.value);
+            } catch (std::exception & exc) {
+                smsc_log_error(logger, "MapATSI[%u]: T_END_REQ: %s", atsiId, exc.what());
+                dialog->releaseAllInvokes();
+            }
+        }
+        session->releaseDialog(dialog);
+        dialog = NULL;
+    }
+}
+
+} //atih
+} //inap
+} //inman
+} //smsc
+
---
+++ src/inman/inap/map_atih/DlgMapATSI.hpp
@@ -0,0 +1,99 @@
+#ident "$Id$"
+// mapATIH service: ATSI dialog implementation (over TCAP dialog)
+
+#ifndef __SMSC_INMAN_INAP_MAP_ATSI__
+#define __SMSC_INMAN_INAP_MAP_ATSI__
+
+#include "core/synchronization/Mutex.hpp"
+#include "inman/inerrcodes.hpp"
+#include "inman/inap/session.hpp"
+#include "inman/inap/dialog.hpp"
+#include "inman/comp/map_atih/MapATSIComps.hpp"
+
+using smsc::core::synchronization::Mutex;
+
+using smsc::inman::InmanErrorType;
+using smsc::inman::_InmanErrorSource;
+
+using smsc::inman::comp::atih::ATSIArg;
+using smsc::inman::comp::atih::ATSIRes;
+using smsc::inman::inap::SSNSession;
+using smsc::inman::inap::Dialog;
+using smsc::inman::inap::DialogListener;
+using smsc::inman::inap::InvokeListener;
+
+namespace smsc {
+namespace inman {
+namespace inap {
+namespace atih {
+
+class ATSIhandler { // SCF <- HLR */
+public: 
+    virtual void onATSIResult(ATSIRes* arg) = 0;
+    //dialog finalization/error handling:
+    //if errLayer != errOk, dialog is aborted by reason = errcode
+    virtual void onEndATSI(unsigned char ercode, InmanErrorType errLayer) = 0;
+};
+
+#define MAP_OPER_INITED 0x2 //'10'B  on BIG-ENDIAN
+#define MAP_OPER_FAIL   0x3 //'11'B  on BIG-ENDIAN 
+typedef union {
+    unsigned short value;
+    struct {
+        unsigned int ctrInited : 2;
+        unsigned int ctrResulted : 1;
+        unsigned int ctrAborted : 1;
+        unsigned int ctrFinished : 1;
+    } s;
+} ATSIState;
+
+//NOTE: DlgATSI doesn't maintain own timer for operations, it uses instead the 
+//innate timer of the SS7 stack for Invoke lifetime.
+class MapATSIDlg : DialogListener, InvokeListener { // SCF -> HLR
+public:
+    MapATSIDlg(SSNSession* pSession, ATSIhandler * atsi_handler, Logger * uselog = NULL);
+    virtual ~MapATSIDlg();
+
+    enum {
+        atsiServiceResponse = 0
+    };
+    void subsciptionInterrogation(const char * subcr_adr,
+                        bool imsi = false, USHORT_T timeout = 0) throw(CustomException);
+
+    void endATSI(void);
+
+protected:
+    friend class smsc::inman::inap::Dialog;
+    // DialogListener interface
+    void onDialogInvoke(Invoke* op, bool lastComp) { }
+    void onDialogContinue(bool compPresent);
+    void onDialogPAbort(UCHAR_T abortCause);
+    void onDialogREnd(bool compPresent);
+    void onDialogUAbort(USHORT_T abortInfo_len, UCHAR_T *pAbortInfo,
+                        USHORT_T userInfo_len, UCHAR_T *pUserInfo);
+
+    // InvokeListener interface (no OPs with returnResult defined)
+    void onInvokeResult(Invoke* inv, TcapEntity* res);
+    void onInvokeError(Invoke* inv, TcapEntity* resE);
+    void onInvokeResultNL(Invoke* inv, TcapEntity* res) { }
+    void onInvokeLCancel(Invoke* inv);
+
+private:
+    void endTCap(void); //ends TC dialog, releases Dialog()
+
+    Mutex       _sync;
+    unsigned    atsiId;
+    Dialog*     dialog;     //TCAP dialog
+    SSNSession* session;    //TCAP dialogs factory
+    Logger*     logger;
+    ATSIhandler * atsiHdl;
+    ATSIState   _atsiState;  //current state of mapATSI dialog
+};
+
+} //atih
+} //inap
+} //inman
+} //smsc
+
+#endif /* __SMSC_INMAN_INAP_MAP_ATSI__ */
+
