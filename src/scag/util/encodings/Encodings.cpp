c7563421a3560e8577885a47397cad8955e0ebf5
loomox <>
2006-05-31 09:51:33 +0000

    *** empty log message ***

--- src/scag/util/encodings/Encodings.cpp
+++ src/scag/util/encodings/Encodings.cpp
@@ -1,8 +1,19 @@
-#include "Decoder.h"
+#include "Encodings.h"
 
 
-namespace scag { namespace util {
+namespace scag { namespace util { namespace encodings {
+
+    void Convertor::UCS2ToUTF8(unsigned char * usc2buff, int usc2len, std::string& utf8str)
+    {
+    }
+    
+    void Convertor::GSM7BitToUTF8(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& utf8str)
+    {
+    }
+    
+    void Convertor::Latin1ToUTF8(unsigned char * latin1Buff, int latin1BuffLen, std::string& utf8str)
+    {
+    }
 
 
-
-}}
+}}}
--- src/scag/util/encodings/Encodings.h
+++ src/scag/util/encodings/Encodings.h
@@ -1,19 +1,21 @@
-#ifndef _SCAG_UTIL_DECODER_H_
-#define _SCAG_UTIL_DECODER_H_
+#ifndef _SCAG_UTIL_ENCODINGS_H_
+#define _SCAG_UTIL_ENCODINGS_H_
 
-namespace scag { namespace util { namespace encodings
+#include <string>
+
+namespace scag { namespace util { namespace encodings {
 
 struct Convertor
 {
 
     static void UCS2ToUTF8(unsigned char * usc2buff, int usc2len, std::string& utf8str);
-    static void 7BitToUTF8(unsigned char * 7BitBuff, int 7BitBuffLen, std::string& utf8str);
-    static void Latin1ToUTF8(unsigned char * latin1Buff, int latin1BuffLen, std::string& utf8str);
     
+    static void GSM7BitToUTF8(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& utf8str);
+    
+    static void Latin1ToUTF8(unsigned char * latin1Buff, int latin1BuffLen, std::string& utf8str);
+}; 
 
-} 
 
-
-}}
+}}}
 
 #endif
