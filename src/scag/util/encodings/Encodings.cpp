0e87deeef73109da1cf21572e251387b870cc4df
loomox <>
2006-05-31 10:22:42 +0000

    *** empty log message ***

--- src/scag/util/encodings/Encodings.cpp
+++ src/scag/util/encodings/Encodings.cpp
@@ -3,17 +3,29 @@
 
 namespace scag { namespace util { namespace encodings {
 
-    void Convertor::UCS2ToUTF8(unsigned char * usc2buff, int usc2len, std::string& utf8str)
-    {
-    }
-    
-    void Convertor::GSM7BitToUTF8(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& utf8str)
-    {
-    }
-    
-    void Convertor::Latin1ToUTF8(unsigned char * latin1Buff, int latin1BuffLen, std::string& utf8str)
-    {
-    }
+void Convertor::UCS2ToUTF8(unsigned char * usc2buff, int usc2len, std::string& utf8str)
+{
+}
+
+void Convertor::GSM7BitToUTF8(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& utf8str)
+{
+}
+
+void Convertor::MultibyteToUTF8(unsigned char * latin1Buff, int latin1BuffLen, ConvEncodingEnum encoding, std::string& utf8str)
+{
+}
+
+void Convertor::UTF8ToUSC2(unsigned char * utf8buff, int utf8len, std::string& ucs2str)
+{
+}
+
+void Convertor::GSM7BitToUCS2(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& ucs2str)
+{
+}
+
+void Convertor::MultibyteToUCS2(unsigned char * latin1Buff, int latin1BuffLen, ConvEncodingEnum encoding, std::string& ucs2str)
+{
+}
 
 
 }}}
--- src/scag/util/encodings/Encodings.h
+++ src/scag/util/encodings/Encodings.h
@@ -2,6 +2,7 @@
 #define _SCAG_UTIL_ENCODINGS_H_
 
 #include <string>
+#include <util/recoder/recode_dll.h>
 
 namespace scag { namespace util { namespace encodings {
 
@@ -9,10 +10,14 @@ struct Convertor
 {
 
     static void UCS2ToUTF8(unsigned char * usc2buff, int usc2len, std::string& utf8str);
-    
     static void GSM7BitToUTF8(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& utf8str);
-    
-    static void Latin1ToUTF8(unsigned char * latin1Buff, int latin1BuffLen, std::string& utf8str);
+    static void MultibyteToUTF8(unsigned char * latin1Buff, int latin1BuffLen, ConvEncodingEnum encoding, std::string& utf8str);
+
+    static void UTF8ToUSC2(unsigned char * utf8buff, int utf8len, std::string& ucs2str);
+    static void GSM7BitToUCS2(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& ucs2str);
+    static void MultibyteToUCS2(unsigned char * latin1Buff, int latin1BuffLen, ConvEncodingEnum encoding, std::string& ucs2str);
+
+
 }; 
 
 
