3b9551d8ce05626bb9fe2972ca552b4cced73623
loomox <>
2006-06-01 05:52:37 +0000

    *** empty log message ***

--- src/scag/util/encodings/Encodings.cpp
+++ src/scag/util/encodings/Encodings.cpp
@@ -5,6 +5,7 @@ namespace scag { namespace util { namespace encodings {
 
 void Convertor::UCS2ToUTF8(unsigned char * usc2buff, int usc2len, std::string& utf8str)
 {
+
 }
 
 void Convertor::GSM7BitToUTF8(unsigned char * gsm7BitBuff, int gsm7BitBuffLen, std::string& utf8str)
