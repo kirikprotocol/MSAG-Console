0ac1362991160cf03bf5eb5930dfeb68b9a10dc0
igor <>
2006-06-05 04:39:29 +0000

    added constructor with additional param

--- webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/EditException.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/EditException.java
@@ -6,17 +6,24 @@ package ru.sibinco.scag.beans;
  * Date: 15.03.2004
  * Time: 16:12:48
  */
-public class EditException extends SCAGJspException
-{
-  private final String editId;
+public class EditException extends SCAGJspException {
+    private final String editId;
+    private String param;
 
-  public EditException(String editId)
-  {
-    this.editId = editId;
-  }
+    public EditException(String editId) {
+        this.editId = editId;
+    }
 
-  public String getEditId()
-  {
-    return editId;
-  }
+    public EditException(String editId, String param) {
+        this.editId = editId;
+        this.param = param;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public String getEditId() {
+        return editId;
+    }
 }
