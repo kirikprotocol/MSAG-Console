cdc605e3198349201adaa45fee6edbf3a34bdcba
igor <>
2006-06-05 04:37:59 +0000

    added Http Subjects

--- webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/routing/subjects/Edit.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/routing/subjects/Edit.java
@@ -2,115 +2,239 @@ package ru.sibinco.scag.beans.routing.subjects;
 
 import ru.sibinco.lib.SibincoException;
 import ru.sibinco.lib.backend.route.Mask;
-import ru.sibinco.lib.backend.route.MaskList;
+import ru.sibinco.lib.backend.users.User;
 import ru.sibinco.lib.backend.util.Functions;
 import ru.sibinco.lib.backend.util.SortedList;
-import ru.sibinco.lib.backend.users.User;
 import ru.sibinco.scag.Constants;
-import ru.sibinco.scag.backend.endpoints.svc.Svc;
-import ru.sibinco.scag.backend.endpoints.centers.Center;
-import ru.sibinco.scag.backend.routing.Subject;
 import ru.sibinco.scag.backend.SCAGAppContext;
+import ru.sibinco.scag.backend.endpoints.centers.Center;
+import ru.sibinco.scag.backend.endpoints.svc.Svc;
+import ru.sibinco.scag.backend.routing.MaskList;
+import ru.sibinco.scag.backend.routing.Subject;
+import ru.sibinco.scag.backend.routing.http.HttpRoutingManager;
+import ru.sibinco.scag.backend.routing.http.HttpSubject;
+import ru.sibinco.scag.backend.routing.http.HttpSite;
 import ru.sibinco.scag.backend.status.StatMessage;
 import ru.sibinco.scag.backend.status.StatusManager;
-import ru.sibinco.scag.beans.DoneException;
-import ru.sibinco.scag.beans.EditBean;
-import ru.sibinco.scag.beans.SCAGJspException;
+import ru.sibinco.scag.backend.transport.Transport;
+import ru.sibinco.scag.beans.*;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
+import java.util.*;
 
 
 /**
  * Created by igork Date: 20.04.2004 Time: 15:51:47
  */
-public class Edit extends EditBean {
+public class Edit extends TabledEditBeanImpl {//EditBean {
+
     private String name;
     private String defaultSme;
     private String[] masks = new String[0];
+    private String[] address = new String[0];
+
     private String description;
+
+    protected long transportId = Transport.SMPP_TRANSPORT_ID;
+    private String httpSubjId = null;
+    private String httpSiteId = null;
+    private int subjectType;
+    private Map sites = new HashMap();
+    private String mbAddSite;
+
 
     public String getId() {
         return name;
     }
 
-    protected void load(final String loadId) throws SCAGJspException {
-        final Subject subject = (Subject) appContext.getScagRoutingManager().getSubjects().get(loadId);
-        if (null != subject) {
-            name = subject.getName();
-            defaultSme = null != subject.getSvc() ? subject.getSvc().getId() : null;
-            if(defaultSme == null){
-                defaultSme = null != subject.getCenter() ? subject.getCenter().getId() : null;
-            }
-            description = subject.getNotes();
+    protected Collection getDataSource() {
+        return sites.values();
+    }
 
-            final List maskList = new ArrayList();
-            for (Iterator i = subject.getMasks().iterator(); i.hasNext();) {
-                final Mask mask = (Mask) i.next();
-                maskList.add(mask.getMask());
+    protected void delete() throws SCAGJspException {
+        appContext.getHttpRoutingManager().deleteSite(getId(), getLoginedPrincipal().getName(), checkedSet);
+    }
+
+    public void process(HttpServletRequest request, HttpServletResponse response) throws SCAGJspException {
+        super.process(request, response);
+
+        if (mbDelete != null) {
+            delete();
+            load(getId());
+        }
+
+        if (getId() == null && !isAdd())
+            load(getEditId());
+
+        if (getMbAddSite() != null) {
+            throw new AddChildException(request.getContextPath() + "/routing/subjects/site", getId());
+        }
+
+        super.process(request, response);
+    }
+
+    protected void load(final String loadId) throws SCAGJspException {
+
+        if (transportId == Transport.SMPP_TRANSPORT_ID) {
+            final Subject subject = (Subject) appContext.getScagRoutingManager().getSubjects().get(loadId);
+            if (null != subject) {
+                name = subject.getName();
+                defaultSme = null != subject.getSvc() ? subject.getSvc().getId() : null;
+                if (defaultSme == null) {
+                    defaultSme = null != subject.getCenter() ? subject.getCenter().getId() : null;
+                }
+                description = subject.getNotes();
+
+                final List maskList = new ArrayList();
+                for (Iterator i = subject.getMasks().iterator(); i.hasNext();) {
+                    final Mask mask = (Mask) i.next();
+                    maskList.add(mask.getMask());
+                }
+                maskList.clear();
+
             }
-            masks = (String[]) maskList.toArray(new String[0]);
-            maskList.clear();
+        } else if (transportId == Transport.HTTP_TRANSPORT_ID) {
+            if (getSubjectType() == HttpRoutingManager.HTTP_SUBJECT_TYPE) {
+                HttpSubject httpSubject = (HttpSubject) appContext.getHttpRoutingManager().getSubjects().get(getHttpSubjId());
+                if (null != httpSubject) {
+                    name = httpSubject.getName();
+                    final List addressList = new ArrayList();
+                    for (Iterator i = httpSubject.getMasks().iterator(); i.hasNext();) {
+                        final Mask mask = (Mask) i.next();
+                        addressList.add(mask.getMask());
+                    }
+                    address = (String[]) addressList.toArray(new String[addressList.size()]);
+                    addressList.clear();
+                }
+            } else if (getSubjectType() == HttpRoutingManager.HTTP_SITE_TYPE) {
+
+                HttpSite httpSite = (HttpSite) appContext.getHttpRoutingManager().getSites().get(getHttpSiteId());
+                if (null != httpSite) {
+                    sites = httpSite.getSites();
+                    name = httpSite.getName();
+                }
+            }
         }
     }
 
     protected void save() throws SCAGJspException {
         String messagetxt = "";
         masks = Functions.trimStrings(masks);
+        address = Functions.trimStrings(address);
+
         final Map subjects = appContext.getScagRoutingManager().getSubjects();
+        final Map httpSubjects = appContext.getHttpRoutingManager().getSubjects();
+        final Map httpSites = appContext.getHttpRoutingManager().getSites();
         final Svc defSvc = (Svc) appContext.getSmppManager().getSvcs().get(defaultSme);
         final Center defCenter = (Center) appContext.getSmppManager().getCenters().get(defaultSme);
-        if (defSvc == null && defCenter == null)
-            throw new SCAGJspException(Constants.errors.routing.subjects.DEFAULT_SME_NOT_FOUND, defaultSme);
+        if (transportId == Transport.SMPP_TRANSPORT_ID) {
+            if (defSvc == null && defCenter == null)
+                throw new SCAGJspException(Constants.errors.routing.subjects.DEFAULT_SME_NOT_FOUND, defaultSme);
+        }
 
         if (isAdd()) {
-            if (subjects.containsKey(name))
-                throw new SCAGJspException(Constants.errors.routing.subjects.SUBJECT_ALREADY_EXISTS, name);
-            try {
-                subjects.put(name, defSvc == null ? new Subject(name, defCenter, masks, description) : new Subject(name, defSvc, masks, description));
-                messagetxt = "Added new subject: '" + name + "'.";
-            } catch (SibincoException e) {
-                logger.debug("Could not create new subject", e);
-                throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_CREATE, e);
-            }
-        } else {
-            if (!getEditId().equals(name)) {
+            if (transportId == Transport.SMPP_TRANSPORT_ID) {
                 if (subjects.containsKey(name))
                     throw new SCAGJspException(Constants.errors.routing.subjects.SUBJECT_ALREADY_EXISTS, name);
-                subjects.remove(getEditId());
                 try {
                     subjects.put(name, defSvc == null ? new Subject(name, defCenter, masks, description) : new Subject(name, defSvc, masks, description));
-
+                    messagetxt = "Added new subject: '" + name + "'.";
                 } catch (SibincoException e) {
                     logger.debug("Could not create new subject", e);
                     throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_CREATE, e);
                 }
-            } else {
-                final Subject subject = (Subject) subjects.get(name);
-                if (defSvc != null) {
-                    subject.setSvc(defSvc);
-                    subject.setCenter(null);
+            } else if (transportId == Transport.HTTP_TRANSPORT_ID) {
+                if (getSubjectType() == HttpRoutingManager.HTTP_SUBJECT_TYPE) {
+                    if (httpSubjects.containsKey(name))
+                        throw new SCAGJspException(Constants.errors.routing.subjects.HTTP_SUBJECT_ALREADY_EXISTS, name);
+                    try {
+                        httpSubjects.put(name, new HttpSubject(name, address));
+                        messagetxt = "Added new http subject: '" + name + "'.";
+                    } catch (SibincoException e) {
+                        logger.debug("Could not create new http subject", e);
+                        throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_CREATE_HTTP_SUBJECT, e);
+                    }
+                } else if (getSubjectType() == HttpRoutingManager.HTTP_SITE_TYPE) {
+                    if (httpSites.containsKey(name))
+                        throw new SCAGJspException(Constants.errors.routing.sites.HTTP_SITE_ALREADY_EXISTS, name);//
+                    try {
+                        httpSites.put(name, new HttpSite(name));
+                        messagetxt = "Added new http subject site: '" + name + "'.";
+                    } catch (SibincoException e) {
+                        logger.debug("Could not create new http subject site", e);
+                        throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_CREATE_HTTP_SUBJECT_SITE, e);
+                    }
+                }
+            }
+        } else {
+            if (transportId == Transport.SMPP_TRANSPORT_ID) {
+                if (!getEditId().equals(name)) {
+                    if (subjects.containsKey(name))
+                        throw new SCAGJspException(Constants.errors.routing.subjects.SUBJECT_ALREADY_EXISTS, name);
+                    subjects.remove(getEditId());
+                    try {
+                        subjects.put(name, defSvc == null ? new Subject(name, defCenter, masks, description) : new Subject(name, defSvc, masks, description));
+
+                    } catch (SibincoException e) {
+                        logger.debug("Could not create new subject", e);
+                        throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_CREATE, e);
+                    }
                 } else {
-                    subject.setCenter(defCenter);
-                    subject.setSvc(null);
+                    final Subject subject = (Subject) subjects.get(name);
+                    if (defSvc != null) {
+                        subject.setSvc(defSvc);
+                        subject.setCenter(null);
+                    } else {
+                        subject.setCenter(defCenter);
+                        subject.setSvc(null);
+                    }
+                    try {
+                        subject.setMasks(new MaskList(masks));
+                    } catch (SibincoException e) {
+                        logger.debug("Could not set masks list for subject", e);
+                        throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_SET_MASKS, e);
+                    }
+                    subject.setNotes(description);
+                    messagetxt = "Changed subject: '" + subject.getName() + "'.";
                 }
-                try {
-                    subject.setMasks(new MaskList(masks));
-                } catch (SibincoException e) {
-                    logger.debug("Could not set masks list for subject", e);
-                    throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_SET_MASKS, e);
+            } else if (transportId == Transport.HTTP_TRANSPORT_ID) {
+                if (getSubjectType() == HttpRoutingManager.HTTP_SUBJECT_TYPE) {
+                    if (!getId().equals(name)) {
+                        if (httpSubjects.containsKey(name))
+                            throw new SCAGJspException(Constants.errors.routing.subjects.HTTP_SUBJECT_ALREADY_EXISTS, name);
+                        httpSubjects.remove(getId());
+                        try {
+                            httpSubjects.put(name, new HttpSubject(name, address));
+                        } catch (SibincoException e) {
+                            logger.debug("Could not create new subject", e);
+                            throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_CREATE, e);
+                        }
+                    } else {
+                        final HttpSubject httpSubject = (HttpSubject) httpSubjects.get(name);
+                        try {
+                            httpSubject.setMasks(new MaskList(address));
+                        } catch (SibincoException e) {
+                            logger.debug("Could not set masks list http for subject", e);
+                            throw new SCAGJspException(Constants.errors.routing.subjects.COULD_NOT_SET_MASKS, e);
+                        }
+                        messagetxt = "Changed http subject: '" + httpSubject.getName() + "'.";
+                    }
+                } else if (getSubjectType() == HttpRoutingManager.HTTP_SITE_TYPE) {
+
                 }
-                subject.setNotes(description);
-                 messagetxt = "Changed subject: '" + subject.getName() + "'.";
             }
         }
-        appContext.getScagRoutingManager().setRoutesChanged(true);
-        appContext.getScagRoutingManager().setChangedByUser(getUser(appContext).getName());
         StatMessage message = new StatMessage(getUser(appContext).getName(), "Subject", messagetxt);
-        appContext.getScagRoutingManager().addStatMessages(message);
+        if (transportId == Transport.SMPP_TRANSPORT_ID) {
+            appContext.getScagRoutingManager().setRoutesChanged(true);
+            appContext.getScagRoutingManager().setChangedByUser(getUser(appContext).getName());
+            appContext.getScagRoutingManager().addStatMessages(message);
+        } else if (transportId == Transport.HTTP_TRANSPORT_ID) {
+            appContext.getHttpRoutingManager().setRoutesChanged(true);
+            appContext.getHttpRoutingManager().addStatMessages(message);
+        }
         StatusManager.getInstance().addStatMessages(message);
         throw new DoneException();
     }
@@ -162,6 +286,14 @@ public class Edit extends EditBean {
         this.masks = masks;
     }
 
+    public String[] getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String[] address) {
+        this.address = address;
+    }
+
     public String getDescription() {
         return description;
     }
@@ -169,4 +301,45 @@ public class Edit extends EditBean {
     public void setDescription(final String description) {
         this.description = description;
     }
+
+    public long getTransportId() {
+        return transportId;
+    }
+
+    public void setTransportId(long transportId) {
+        this.transportId = transportId;
+    }
+
+    public String getHttpSubjId() {
+        return httpSubjId;
+    }
+
+    public void setHttpSubjId(String httpSubjId) {
+        this.httpSubjId = httpSubjId;
+    }
+
+    public String getHttpSiteId() {
+        return httpSiteId;
+    }
+
+    public void setHttpSiteId(String httpSiteId) {
+        this.httpSiteId = httpSiteId;
+    }
+
+    public int getSubjectType() {
+        return subjectType;
+    }
+
+    public void setSubjectType(int subjectType) {
+        this.subjectType = subjectType;
+    }
+
+    public String getMbAddSite() {
+        return mbAddSite;
+    }
+
+    public void setMbAddSite(String mbAddSite) {
+        this.mbAddSite = mbAddSite;
+    }
+
 }
--- webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/routing/subjects/Index.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/routing/subjects/Index.java
@@ -1,9 +1,16 @@
 package ru.sibinco.scag.beans.routing.subjects;
 
 import ru.sibinco.lib.bean.TabledBean;
-import ru.sibinco.scag.beans.TabledBeanImpl;
+import ru.sibinco.lib.backend.util.SortedList;
+import ru.sibinco.lib.backend.util.SortByPropertyComparator;
+import ru.sibinco.scag.beans.*;
+import ru.sibinco.scag.backend.transport.Transport;
+import ru.sibinco.scag.backend.routing.http.HttpRoutingManager;
 
-import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.*;
 
 
 /**
@@ -11,11 +18,186 @@ import java.util.Collection;
  */
 public class Index extends TabledBeanImpl implements TabledBean {
 
+    private List siteItems = new ArrayList();
+    private List subjectItems = new ArrayList();
+    private String mbHttpSubjEdit = null;
+    private String mbHttpSiteEdit = null;
+    private String mbAddHttpSubj = null;
+    private String mbAddHttpSite = null;
+    private int subjectType;
+    private String mbDeleteHttpSubj;
+    private String mbDeleteHttpSite;
+
+    protected int transportId =  -1;
+
     protected Collection getDataSource() {
-        return appContext.getScagRoutingManager().getSubjects().values();
+        if (transportId == Transport.SMPP_TRANSPORT_ID) {
+            return appContext.getScagRoutingManager().getSubjects().values();
+        } else if (transportId == Transport.HTTP_TRANSPORT_ID) {
+            return appContext.getHttpRoutingManager().getSites().values();
+        } else if (transportId == Transport.MMS_TRANSPORT_ID) {
+            return appContext.getScagRoutingManager().getSubjects().values();
+        } else {
+            return new HashMap().values();
+        }
+    }
+
+    public void process(HttpServletRequest request, HttpServletResponse response) throws SCAGJspException {
+
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("HTTP_TRANSPORT") == null) {
+            session.setAttribute("HTTP_TRANSPORT", new Integer(Transport.SMPP_TRANSPORT_ID));
+        } else {
+            Integer tTransportId = (Integer) session.getAttribute("HTTP_TRANSPORT");
+            if (tTransportId.intValue() != transportId && transportId != -1) {
+                session.setAttribute("HTTP_TRANSPORT", new Integer(transportId));
+            }
+        }
+        transportId = Integer.parseInt(String.valueOf(session.getAttribute("HTTP_TRANSPORT")));
+
+        if (mbDeleteHttpSite != null) {
+            super.process(request, response);
+            deleteHttpSite();
+        }
+        if (mbDeleteHttpSubj != null) {
+            super.process(request, response);
+            deleteHttpSubject();
+        }
+        if (mbAddHttpSubj != null)
+            throw new AddSiteSubjectException(Integer.toString(Transport.HTTP_TRANSPORT_ID),
+                    Integer.toString(HttpRoutingManager.HTTP_SUBJECT_TYPE));
+        if (mbAddHttpSite != null)
+            throw new AddSiteSubjectException(Integer.toString(Transport.HTTP_TRANSPORT_ID),
+                    Integer.toString(HttpRoutingManager.HTTP_SITE_TYPE));
+
+        super.process(request, response);
+
+        if (sort == null)
+            sort = getDefaultSort();
+        final SortedList results = new SortedList(getDataSource(), new SortByPropertyComparator(sort));
+        totalSize = results.size();
+        if (totalSize > startPosition)
+            tabledItems = results.subList(startPosition, Math.min(totalSize, startPosition + pageSize));
+        else
+            tabledItems = new LinkedList();
+        if (transportId == Transport.HTTP_TRANSPORT_ID) {
+            final SortedList results2 = new SortedList(appContext.getHttpRoutingManager().getSubjects().values(), new SortByPropertyComparator(sort));
+            totalSize = results2.size();
+            if (totalSize > startPosition)
+                subjectItems = results2.subList(startPosition, Math.min(totalSize, startPosition + pageSize));
+            else
+                subjectItems = new LinkedList();
+        }
+        if (transportId == Transport.HTTP_TRANSPORT_ID) {
+            final SortedList results3 = new SortedList(appContext.getHttpRoutingManager().getSites().values(), new SortByPropertyComparator(sort));
+            totalSize = results3.size();
+            if (totalSize > startPosition)
+                siteItems = results3.subList(startPosition, Math.min(totalSize, startPosition + pageSize));
+            else
+                siteItems = new LinkedList();
+        }
     }
 
     protected void delete() {
         appContext.getScagRoutingManager().deleteSubjects(getLoginedPrincipal().getName(), checkedSet);
     }
+
+    protected void deleteHttpSite() {
+        appContext.getHttpRoutingManager().deleteSubjectsSite(getLoginedPrincipal().getName(), checkedSet);
+    }
+
+    protected void deleteHttpSubject() {
+        appContext.getHttpRoutingManager().deleteSubjects(getLoginedPrincipal().getName(), checkedSet);
+    }
+
+    public int getTransportId() {
+        return transportId;
+    }
+
+    public void setTransportId(int transportId) {
+        this.transportId = transportId;
+    }
+
+    public String[] getTransportIds() {
+        return Transport.transportIds;
+    }
+
+    public String[] getTransportTitles() {
+        return Transport.transportTitles;
+    }
+
+    public void setSiteItems(List siteItems) {
+        this.siteItems = siteItems;
+    }
+
+    public List getSiteItems() {
+        return siteItems;
+    }
+
+    public List getSubjectItems() {
+        return subjectItems;
+    }
+
+    public void setSubjectItems(List subjectItems) {
+        this.subjectItems = subjectItems;
+    }
+
+    public String getMbHttpSubjEdit() {
+        return mbHttpSubjEdit;
+    }
+
+    public void setMbHttpSubjEdit(String mbHttpSubjEdit) {
+        this.mbHttpSubjEdit = mbHttpSubjEdit;
+    }
+
+    public String getMbAddHttpSubj() {
+        return mbAddHttpSubj;
+    }
+
+    public void setMbAddHttpSubj(String mbAddHttpSubj) {
+        this.mbAddHttpSubj = mbAddHttpSubj;
+    }
+
+    public String getMbAddHttpSite() {
+        return mbAddHttpSite;
+    }
+
+    public void setMbAddHttpSite(String mbAddHttpSite) {
+        this.mbAddHttpSite = mbAddHttpSite;
+    }
+
+    public int getSubjectType() {
+        return subjectType;
+    }
+
+    public void setSubjectType(int subjectType) {
+        this.subjectType = subjectType;
+    }
+
+    public String getMbHttpSiteEdit() {
+        return mbHttpSiteEdit;
+    }
+
+    public void setMbHttpSiteEdit(String mbHttpSiteEdit) {
+        this.mbHttpSiteEdit = mbHttpSiteEdit;
+    }
+
+    public String getMbDeleteHttpSubj() {
+        return mbDeleteHttpSubj;
+    }
+
+    public void setMbDeleteHttpSubj(String mbDeleteHttpSubj) {
+        this.mbDeleteHttpSubj = mbDeleteHttpSubj;
+    }
+
+    public String getMbDeleteHttpSite() {
+        return mbDeleteHttpSite;
+    }
+
+    public void setMbDeleteHttpSite(String mbDeleteHttpSite) {
+        this.mbDeleteHttpSite = mbDeleteHttpSite;
+    }
+
+
 }
