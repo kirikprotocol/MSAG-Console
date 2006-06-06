6479b25a89532f33390c176733f918e3cde8d8b9
skv <>
2006-06-06 11:54:28 +0000

    timezone manager. need route.xml and timezones.xml to work

---
+++ src/system/common/TimeZoneMan.cpp
@@ -0,0 +1,72 @@
+#include "system/common/TimeZoneMan.hpp"
+#include "util/config/Config.h"
+#include "util/config/route/RouteConfig.h"
+#include <xercesc/dom/DOM.hpp>
+#include "util/xml/DOMTreeReader.h"
+
+namespace smsc{
+namespace system{
+namespace common{
+
+TimeZoneManager* TimeZoneManager::instance=0;
+
+
+static int getOffsetValue(const smsc::util::config::Config& cfg,const char* param)
+{
+  const char* val=cfg.getString(param);
+  int rv;
+  if(sscanf(val,"%d,",&rv)!=1)throw smsc::util::Exception("Invalid value for timezone:%s",val);
+  return rv;
+}
+
+void TimeZoneManager::Reload()
+{
+  using namespace xercesc;
+  using namespace smsc::util::xml;
+  using namespace smsc::util::config;
+  Config cfg;
+  DOMTreeReader reader;
+  DOMDocument *document = reader.read(cfgTzFileName.c_str());
+  if (document && document->getDocumentElement())
+  {
+    DOMElement *elem = document->getDocumentElement();
+    cfg.parse(*elem);
+  }
+  else
+  {
+    throw smsc::util::Exception("Parse result is null:%s",cfgTzFileName.c_str());
+  }
+
+  route::RouteConfig rcfg;
+  rcfg.load(cfgRouteFileName.c_str());
+
+  sync::MutexGuard mg(mtx);
+  offsetMap.clear();
+  defaultOffset=getOffsetValue(cfg,"default_timezone");
+  std::auto_ptr<CStrSet> masks(cfg.getChildStrParamNames("masks"));
+  char buf[128];
+  for(CStrSet::iterator it=masks->begin();it!=masks->end();it++)
+  {
+    snprintf(buf,128,"masks.%s",it->c_str());
+    offsetMap.insert(OffsetMap::value_type(it->c_str(),getOffsetValue(cfg,buf)));
+  }
+  std::auto_ptr<CStrSet> subjs(cfg.getChildStrParamNames("subjects"));
+  for(CStrSet::iterator it=subjs->begin();it!=subjs->end();it++)
+  {
+    snprintf(buf,128,"subjects.%s",it->c_str());
+    try{
+      route::Subject& subj=rcfg.getSubject(it->c_str());
+      for(route::MaskVector::const_iterator sit=subj.getMasks().begin();sit!=subj.getMasks().end();sit++)
+      {
+        offsetMap.insert(OffsetMap::value_type(sit->c_str(),getOffsetValue(cfg,buf)));
+      }
+    }catch(smsc::core::buffers::HashInvalidKeyException& e)
+    {
+      throw smsc::util::Exception("Unknown subject:%s",it->c_str());
+    }
+  }
+}
+
+}//common
+}//system
+}//smsc
---
+++ src/system/common/TimeZoneMan.hpp
@@ -0,0 +1,88 @@
+#ifndef __SMSC_SYSTEM_COMMON_TIMEZONEMAN_HPP_
+#define __SMSC_SYSTEM_COMMON_TIMEZONEMAN_HPP_
+
+#include <string>
+#include <map>
+#include "core/synchronization/Mutex.hpp"
+#include "sms/sms.h"
+#include "util/Exception.hpp"
+
+namespace smsc{
+namespace system{
+namespace common{
+
+namespace sync=smsc::core::synchronization;
+
+class TimeZoneManager{
+public:
+  static void Init(const char* tzFileName,const char* routeFileName)
+  {
+    instance=new TimeZoneManager;
+    instance->cfgTzFileName=tzFileName;
+    instance->cfgRouteFileName=routeFileName;
+    instance->Reload();
+  }
+
+  static TimeZoneManager& getInstance()
+  {
+    if(!instance)throw smsc::util::Exception("TimeZoneManager wasn't initiated");
+    return *instance;
+  }
+
+  void Reload();
+
+  int getTimeZone(const smsc::sms::Address& addr)const
+  {
+    sync::MutexGuard mg(mtx);
+    OffsetMap::const_iterator it=offsetMap.find(addr);
+    if(it==offsetMap.end())return defaultOffset;
+    return it->second;
+  }
+
+  static void Shutdown()
+  {
+    delete instance;
+    instance=0;
+  }
+
+protected:
+
+  static TimeZoneManager* instance;
+
+  struct MaskComparator{
+    bool operator()(const smsc::sms::Address& addra,const smsc::sms::Address& addrb)const
+    {
+      if(addra.type<addrb.type)return true;
+      if(addra.type>addrb.type)return false;
+      if(addra.plan<addrb.plan)return true;
+      if(addra.plan>addrb.plan)return false;
+      const char* a=addra.value;
+      const char* b=addrb.value;
+      while(*a && *b)
+      {
+        if(*a=='?' || *b=='?')return addra.length<addrb.length;//strlen(a)<strlen(b);
+        if(*a<*b)return true;
+        a++;
+        b++;
+      }
+      return !*a && *b;
+    }
+  };
+
+
+  std::string cfgTzFileName;
+  std::string cfgRouteFileName;
+  mutable sync::Mutex mtx;
+  typedef std::map<smsc::sms::Address,int,MaskComparator> OffsetMap;
+  OffsetMap offsetMap;
+
+  int defaultOffset;
+
+
+};
+
+}//common
+}//system
+}//smsc
+
+#endif
