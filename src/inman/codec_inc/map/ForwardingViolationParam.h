13b78bf56a61984f65c774f3f62bc3e2d1306d38
gvroman <>
2006-06-21 07:54:55 +0000

    MAP-SEND-ROUTING-INFO types are added

---
+++ src/inman/codec_inc/map/ForwardingViolationParam.h
@@ -0,0 +1,41 @@
+#ident "$Id$"
+
+#ifndef	_ForwardingViolationParam_H_
+#define	_ForwardingViolationParam_H_
+
+
+#include <asn_application.h>
+
+/* Including external dependencies */
+#include <constr_SEQUENCE.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Forward declarations */
+struct ExtensionContainer;
+
+/* ForwardingViolationParam */
+typedef struct ForwardingViolationParam {
+	struct ExtensionContainer	*extensionContainer	/* OPTIONAL */;
+	/*
+	 * This type is extensible,
+	 * possible extensions are below.
+	 */
+	
+	/* Context for parsing across buffer boundaries */
+	asn_struct_ctx_t _asn_ctx;
+} ForwardingViolationParam_t;
+
+/* Implementation */
+extern asn_TYPE_descriptor_t asn_DEF_ForwardingViolationParam;
+
+/* Referred external types */
+#include <ExtensionContainer.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _ForwardingViolationParam_H_ */
---
+++ src/inman/codec_inc/map/NoSubscriberReplyParam.h
@@ -0,0 +1,41 @@
+#ident "$Id$"
+
+#ifndef	_NoSubscriberReplyParam_H_
+#define	_NoSubscriberReplyParam_H_
+
+
+#include <asn_application.h>
+
+/* Including external dependencies */
+#include <constr_SEQUENCE.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Forward declarations */
+struct ExtensionContainer;
+
+/* NoSubscriberReplyParam */
+typedef struct NoSubscriberReplyParam {
+	struct ExtensionContainer	*extensionContainer	/* OPTIONAL */;
+	/*
+	 * This type is extensible,
+	 * possible extensions are below.
+	 */
+	
+	/* Context for parsing across buffer boundaries */
+	asn_struct_ctx_t _asn_ctx;
+} NoSubscriberReplyParam_t;
+
+/* Implementation */
+extern asn_TYPE_descriptor_t asn_DEF_NoSubscriberReplyParam;
+
+/* Referred external types */
+#include <ExtensionContainer.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _NoSubscriberReplyParam_H_ */
