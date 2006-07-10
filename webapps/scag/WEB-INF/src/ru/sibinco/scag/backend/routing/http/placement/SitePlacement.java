09acfff76b5cbab9667d80a990750c11069b64b5
igor <>
2006-07-10 08:00:43 +0000

    added

---
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/backend/routing/http/placement/SitePlacement.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2006 SibInco Inc. All Rights Reserved.
+ */
+
+package ru.sibinco.scag.backend.routing.http.placement;
+
+import ru.sibinco.lib.SibincoException;
+
+
+/**
+ * The <code>SitePlacement</code> class represents
+ * <p><p/>
+ * <p/>
+ * Date: 06.07.2006
+ * Time: 17:20:06
+ *
+ * @author &lt;a href="mailto:igor@sibinco.ru"&gt;Igor Klimenko&lt;/a&gt;
+ */
+public class SitePlacement extends Placement{
+
+    public SitePlacement(String type, String name) throws SibincoException {
+        super(type, name);
+    }
+}
