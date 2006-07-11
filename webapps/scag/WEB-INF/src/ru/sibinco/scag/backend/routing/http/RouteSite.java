9a1d7349e023d3f08b11299bc610c46b50e97347
igor <>
2006-07-11 10:15:07 +0000

    added placements

--- webapps/scag/WEB-INF/src/ru/sibinco/scag/backend/routing/http/RouteSite.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/backend/routing/http/RouteSite.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 
 import ru.sibinco.lib.SibincoException;
 import ru.sibinco.lib.backend.util.StringEncoderDecoder;
+import ru.sibinco.scag.backend.routing.http.placement.SitePlacement;
 
 /**
  * The <code>RouteSite</code> class represents
@@ -27,6 +28,8 @@ public class RouteSite {
 
     private Map siteSubjects = new HashMap();
     private Map sites = new HashMap();
+    private List usrPlace = new ArrayList();
+    private List addressPlace = new ArrayList();
 
 
     public RouteSite(Map siteSubjects, Map sites) {
@@ -57,24 +60,73 @@ public class RouteSite {
             site.setPathLinks((String[]) list.toArray(new String[list.size()]));
             sites.put(site.getHost(), site);
         }
+        NodeList placementList = routeElement.getElementsByTagName("placement");
+        if (placementList != null) {
+            Element usrplacementElement = (Element) placementList.item(0);
+            Element addrplacementElement = (Element) placementList.item(1);
+            if (usrplacementElement != null) {
+                NodeList usrPlaceNodeList = usrplacementElement.getElementsByTagName("usr_place");
+                if (usrPlaceNodeList != null) {
+                    for (int i = 0; i < usrPlaceNodeList.getLength(); i++) {
+                        Element usrPlaceElement = (Element) usrPlaceNodeList.item(i);
+                        final SitePlacement sitePlacement = new SitePlacement(
+                                usrPlaceElement.getAttribute("type"),
+                                usrPlaceElement.getAttribute("name"));
+                        usrPlace.add(sitePlacement);
+                    }
+                }
+            }
+            if (addrplacementElement != null) {
+                NodeList addressPlaceNodeList = addrplacementElement.getElementsByTagName("address_place");
+                if (addressPlaceNodeList != null) {
+                    for (int i = 0; i < addressPlaceNodeList.getLength(); i++) {
+                        Element addressPlaceElement = (Element) addressPlaceNodeList.item(i);
+                        final SitePlacement sitePlacement = new SitePlacement(
+                                addressPlaceElement.getAttribute("type"),
+                                addressPlaceElement.getAttribute("name"));
+                        addressPlace.add(sitePlacement);
+                    }
+                }
+            }
+        }
     }
 
     public PrintWriter store(PrintWriter out) {
         out.println("    <sites>");
         for (Iterator i = siteSubjects.values().iterator(); i.hasNext();) {
             final HttpSite httpSite = (HttpSite) i.next();
-            out.println("      <site_subject id=\"" + StringEncoderDecoder.encode(httpSite.getName()) + "\"/>");
+            out.println("        <site_subject id=\"" + StringEncoderDecoder.encode(httpSite.getName()) + "\"/>");
         }
         for (Iterator i = sites.values().iterator(); i.hasNext();) {
             final Site site = (Site) i.next();
-            out.println("      <site host=\"" + StringEncoderDecoder.encode(site.getHost()) + "\""  +
+            out.println("        <site host=\"" + StringEncoderDecoder.encode(site.getHost()) + "\"" +
                     " port=\"" + site.getPort() + "\">");
             for (int j = 0; j < site.getPathLinks().length; j++) {
-                out.println("           <path value=\"" + StringEncoderDecoder.encode(site.getPathLinks()[j]) + "\"/>");
+                out.println("            <path value=\"" + StringEncoderDecoder.encode(site.getPathLinks()[j]) + "\"/>");
             }
-            out.println("      </site>");
+            out.println("        </site>");
         }
+        out.println();
+        out.println("        <placement>");
+        for (Iterator i = addressPlace.iterator(); i.hasNext();) {
+            final SitePlacement placement = (SitePlacement) i.next();
+            out.println(new StringBuffer().append("            <address_place type=\"").
+                    append(StringEncoderDecoder.encode(placement.getType())).append("\" ").
+                    append(" name=\"").append(StringEncoderDecoder.encode(placement.getName())).
+                    append("\" />").toString());
+        }
+        out.println();
+        for (Iterator i = usrPlace.iterator(); i.hasNext();) {
+            final SitePlacement placement = (SitePlacement) i.next();
+            out.println(new StringBuffer().append("            <usr_place type=\"").
+                    append(StringEncoderDecoder.encode(placement.getType())).
+                    append("\" ").append(" name=\"").append(StringEncoderDecoder.encode(placement.getName())).
+                    append("\" />").toString());
+        }
+        out.println("        </placement>");
+        out.println();
         out.println("    </sites>");
+        out.println();
         return out;
     }
 
@@ -111,5 +163,13 @@ public class RouteSite {
             siteList.add(site.getHost());
         }
         return (String[]) siteList.toArray(new String[siteList.size()]);
+    }
+
+    public List getUsrPlace() {
+        return usrPlace;
+    }
+
+    public List getAddressPlace() {
+        return addressPlace;
     }
 }
