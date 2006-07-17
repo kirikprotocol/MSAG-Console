c585a828f2af558071810f688e8d2e50f6220cbb
skv <>
2006-07-17 08:17:44 +0000

    expiration fix

--- src/system/state_machine.cpp
+++ src/system/state_machine.cpp
@@ -2691,6 +2691,7 @@ StateType StateMachine::forward(Tuple& t)
     smsc->getScheduler()->InvalidSms(t.msgId);
     return UNKNOWN_STATE;
   }
+  smsc_log_debug(smsLog,"orgMSC=%s, orgIMSI=%s",sms.originatingDescriptor.msc,sms.originatingDescriptor.imsi);
   INFwdSmsChargeResponse::ForwardContext ctx;
   ctx.allowDivert=t.command->get_forwardAllowDivert();
   ctx.reschedulingForward=t.command->is_reschedulingForward();
@@ -2711,6 +2712,33 @@ StateType StateMachine::forward(Tuple& t)
     }
   }
 
+  if(sms.getState()==EXPIRED_STATE)
+  {
+    smsc_log_warn(smsLog, "FWD: sms in expired state msgId=%lld",t.msgId);
+    sms.setLastResult(Status::EXPIRED);
+    smsc->getScheduler()->InvalidSms(t.msgId);
+    return EXPIRED_STATE;
+  }
+
+  time_t now=time(NULL);
+  if((sms.getValidTime()<=now && sms.getLastResult()!=0) || //expired or
+     RescheduleCalculator::calcNextTryTime(now,sms.getLastResult(),sms.getAttemptsCount())==-1) //max attempts count reached
+  {
+    sms.setLastResult(Status::EXPIRED);
+    smsc->registerStatisticalEvent(StatEvents::etRescheduled,&sms);
+    smsc->registerStatisticalEvent(StatEvents::etUndeliverable,&sms);
+    try{
+      smsc->getScheduler()->InvalidSms(t.msgId);
+      store->changeSmsStateToExpired(t.msgId);
+    }catch(...)
+    {
+      __warning__("FORWARD: failed to change state to expired");
+    }
+    info2(smsLog, "FWD: %lld expired (valid:%u - now:%u)",t.msgId,sms.getValidTime(),now);
+    sendFailureReport(sms,t.msgId,EXPIRED_STATE,"expired");
+    return EXPIRED_STATE;
+  }
+
   if(sms.getIntProperty(Tag::SMSC_CHARGINGPOLICY)==Smsc::chargeOnDelivery  && sms.billingRecord)
   {
     try{
@@ -2722,7 +2750,7 @@ StateType StateMachine::forward(Tuple& t)
       smsc->registerStatisticalEvent(StatEvents::etRescheduled,&sms);
       smsc->registerStatisticalEvent(StatEvents::etDeliverErr,&sms);
       Descriptor d;
-      changeSmsStateToEnroute(sms,t.msgId,d,Status::SMENOTCONNECTED,rescheduleSms(sms));
+      changeSmsStateToEnroute(sms,t.msgId,d,Status::NOCONNECTIONTOINMAN,rescheduleSms(sms));
       return ENROUTE_STATE;
     }
     return CHARGINGFWD_STATE;
@@ -2755,45 +2783,7 @@ StateType StateMachine::forwardChargeResp(Tuple& t)
     return ERROR_STATE;
   }
 
-  if(sms.getState()==EXPIRED_STATE)
-  {
-    smsc_log_warn(smsLog, "FWD: sms in expired state msgId=%lld",t.msgId);
-    sms.setLastResult(Status::EXPIRED);
-    smsc->getScheduler()->InvalidSms(t.msgId);
-    try{
-      smsc->ReportDelivery(inDlgId,sms,true,Smsc::chargeOnDelivery);
-    }catch(std::exception& e)
-    {
-      smsc_log_warn(smsLog,"ReportDelivery for %lld failed:'%s'",t.msgId,e.what());
-    }
-    return EXPIRED_STATE;
-  }
   time_t now=time(NULL);
-  if((sms.getValidTime()<=now && sms.getLastResult()!=0) || //expired or
-     RescheduleCalculator::calcNextTryTime(now,sms.getLastResult(),sms.getAttemptsCount())==-1) //max attempts count reached
-  {
-    sms.setLastResult(Status::EXPIRED);
-    smsc->registerStatisticalEvent(StatEvents::etRescheduled,&sms);
-    smsc->registerStatisticalEvent(StatEvents::etUndeliverable,&sms);
-    try{
-      smsc->getScheduler()->InvalidSms(t.msgId);
-      store->changeSmsStateToExpired(t.msgId);
-    }catch(...)
-    {
-      __warning__("FORWARD: failed to change state to expired");
-    }
-    info2(smsLog, "FWD: %lld expired (valid:%u - now:%u)",t.msgId,sms.getValidTime(),now);
-
-    sendFailureReport(sms,t.msgId,EXPIRED_STATE,"expired");
-    try{
-      smsc->ReportDelivery(inDlgId,sms,true,Smsc::chargeOnDelivery);
-    }catch(std::exception& e)
-    {
-      smsc_log_warn(smsLog,"ReportDelivery for %lld failed:'%s'",t.msgId,e.what());
-    }
-
-    return EXPIRED_STATE;
-  }
 
   if(sms.hasIntProperty(Tag::SMSC_MERGE_CONCAT) && sms.getIntProperty(Tag::SMSC_MERGE_CONCAT)!=3)
   {
