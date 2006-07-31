20064f37525a843c22d52a2117d7ffcdd3f013fc
igor <>
2006-07-31 09:06:28 +0000

    added logging

---
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/gw/logging/Logging.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright (c) 2006 SibInco Inc. All Rights Reserved.
+ */
+
+package ru.sibinco.scag.beans.gw.logging;
+
+import ru.sibinco.lib.SibincoException;
+import ru.sibinco.lib.backend.util.SortedList;
+import ru.sibinco.scag.Constants;
+import ru.sibinco.scag.beans.EditBean;
+import ru.sibinco.scag.beans.SCAGJspException;
+import ru.sibinco.scag.util.Comparator_CaseInsensitive;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+/**
+ * The <code>Logging</code> class represents
+ * <p><p/>
+ * Date: 21.07.2006
+ * Time: 11:50:29
+ *
+ * @author &lt;a href="mailto:igor@sibinco.ru"&gt;Igor Klimenko&lt;/a&gt;
+ */
+public class Logging extends EditBean {
+    public static final String catParamNamePrefix = "category_";
+    private LoggerCategoryInfo rootCategory;
+    private String[] priorities = {"FATAL", "ERROR", "WARN", "INFO", "DEBUG", "NOTSET"};
+    private LoggerCategoryInfo[] rootCategories;
+    private String category;
+
+    public void process(final HttpServletRequest request, final HttpServletResponse response) throws SCAGJspException {
+        super.process(request, response);
+        if (getMbSave() != null) {
+            save(request.getParameterMap());
+        }
+        init();
+    }
+
+    public class LoggerCategoryInfo implements Comparable {
+        private String name;
+        private String fullName;
+        private String priority;
+        private Map childs;
+
+        public LoggerCategoryInfo(String name, String fullName, String priority) {
+            this.name = name;
+            this.fullName = fullName;
+            this.priority = priority;
+            this.childs = new TreeMap(new Comparator_CaseInsensitive());
+        }
+
+        public void addChild(String childFullName, String childPriority) {
+            logger.debug(new StringBuffer().append("Adding child: name=\"").append(fullName).
+                    append("\" priority=").append(priority).append(", childName=\"").append(childFullName).
+                    append("\" childPriority=").append(childPriority).toString());
+            if (isRoot()) {
+                final int dotPos = childFullName.indexOf('.');
+                if (dotPos > 0) {
+                    final String childName = childFullName.substring(0, dotPos);
+                    LoggerCategoryInfo child = getOrCreateChild(childName, childName, "NOTSET");
+                    child.addChild(childFullName, childPriority);
+                } else {
+                    childs.put(childFullName, new LoggerCategoryInfo(childFullName, childFullName, childPriority));
+                }
+            } else {
+                if (childFullName.length() > fullName.length() && childFullName.startsWith(fullName) && childFullName.charAt(fullName.length()) == '.')
+                {
+                    final int beginIndex = fullName.length() + 1;
+                    final int endIndex = childFullName.indexOf('.', beginIndex);
+                    final String childName = endIndex > 0 ? childFullName.substring(beginIndex, endIndex) : childFullName.substring(beginIndex);
+                    if (endIndex > 0) {
+                        LoggerCategoryInfo child = getOrCreateChild(childName, childFullName.substring(0, endIndex), "NONSET");
+                        child.addChild(childFullName, childPriority);
+                    } else {
+                        LoggerCategoryInfo child = getOrCreateChild(childName, childFullName, childPriority);
+                        child.priority = childPriority;
+                    }
+                } else {
+                    logger.debug("Incorrect LogerCategoryInfo.addChild algorithm");
+                }
+            }
+        }
+
+        private LoggerCategoryInfo getOrCreateChild(String childName, String childFullName, String childPriority) {
+            LoggerCategoryInfo child = (LoggerCategoryInfo) childs.get(childName);
+            if (child == null) {
+                child = new LoggerCategoryInfo(childName, childFullName, childPriority);
+                childs.put(childName, child);
+            }
+            return child;
+        }
+
+        public boolean isRoot() {
+            return fullName == null || fullName.length() == 0;
+        }
+
+
+        public boolean hasChilds() {
+            return !childs.isEmpty();
+        }
+
+        public boolean isHasChilds() {
+            return !childs.isEmpty();
+        }
+
+        public int compareTo(Object o) {
+            if (o instanceof LoggerCategoryInfo) {
+                LoggerCategoryInfo info = (LoggerCategoryInfo) o;
+                return name.compareTo(info.name);
+            } else
+                return 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public String getPriority() {
+            return priority;
+        }
+
+        public Map getChilds() {
+            return childs;
+        }
+    }
+
+    protected void init() {
+        try {
+            Map logCategories = appContext.getScag().getLogCategories();
+            String rootPriority = (String) logCategories.remove("");
+            if (rootPriority == null) rootPriority = "NOTSET";
+            rootCategory = new LoggerCategoryInfo("", "", rootPriority);
+            Collection keys = new SortedList(logCategories.keySet());
+            for (Iterator iterator = keys.iterator(); iterator.hasNext();) {
+                String key = (String) iterator.next();
+                String value = (String) logCategories.get(key);
+                rootCategory.addChild(key, value);
+            }
+
+        } catch (SibincoException e) {
+            rootCategory = new LoggerCategoryInfo("", "", "NOTSET");
+        }
+        rootCategories = getLoggerCategoryInfos(rootCategory);
+
+    }
+
+    public LoggerCategoryInfo[] getRootCategories() {
+        return rootCategories;
+    }
+
+    public LoggerCategoryInfo getRootCategory() {
+        return rootCategory;
+    }
+
+    public String getId() {
+        return null;
+    }
+
+    protected void load(final String loadId) throws SCAGJspException {
+    }
+
+    protected void save() throws SCAGJspException {
+    }
+
+    protected void save(Map parameters) throws SCAGJspException {
+        Map cats = new HashMap();
+        for (Iterator iterator = parameters.entrySet().iterator(); iterator.hasNext();) {
+            Map.Entry entry = (Map.Entry) iterator.next();
+            String paramName = (String) entry.getKey();
+            if (paramName.startsWith(catParamNamePrefix)) {
+                cats.put(paramName.substring(catParamNamePrefix.length()), getParamValue(entry.getValue()));
+                logger.info("cat param: " + paramName + ":=" + getParamValue(entry.getValue()));
+            } else {
+                logger.info("WRONG param: " + paramName + ":=" + getParamValue(entry.getValue()));
+            }
+
+        }
+        try {
+            appContext.getScag().setLogCategories(cats);
+        } catch (SibincoException e) {
+            throw new SCAGJspException(Constants.errors.logging.COULDNT_SET_LOGCATS, e);
+        }
+    }
+
+    private String getParamValue(Object value) {
+        if (value instanceof String)
+            return (String) value;
+        else if (value instanceof String[]) {
+            String result = "";
+            final String[] values = (String[]) value;
+            for (int i = 0; i < values.length; i++) {
+                result += values[i];
+            }
+            return result;
+        } else
+            return value.toString();
+    }
+
+    public String[] getPriorities() {
+        return priorities;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private LoggerCategoryInfo[] getLoggerCategoryInfos(LoggerCategoryInfo rootCategory) {
+        List result = new ArrayList();
+        result.add(rootCategory);
+        if (rootCategory.hasChilds()) {
+            for (Iterator i = rootCategory.getChilds().values().iterator(); i.hasNext();) {
+                LoggerCategoryInfo child = (Logging.LoggerCategoryInfo) i.next();
+                result.add(child);
+                if (child.hasChilds()) {
+                    getLoggerCategoryInfos(child);
+                }
+            }
+        }
+        return (LoggerCategoryInfo[]) result.toArray(new LoggerCategoryInfo[result.size()]);
+    }
+}
