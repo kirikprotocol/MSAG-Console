6a24f2c9e8e0993b7d6d9ebaad927f9193a7546e
dym <>
2006-08-09 05:53:42 +0000

    no message

--- webapps/jedit/src/sidekick/SideKickPlugin.java
+++ webapps/jedit/src/sidekick/SideKickPlugin.java
@@ -200,7 +200,7 @@ public class SideKickPlugin extends EBPlugin
  {
    if(worker == null)
   {
-   worker = new WorkThreadPool("SideKick",2);
+   worker = new WorkThreadPool("SideKick",4);
    worker.start();
   }
   worker.addWorkRequest(run,inAWT);
