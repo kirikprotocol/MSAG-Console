59f811753c5bdda6d75f7de5852cf2cb21124e1b
miker <>
2006-08-28 09:46:46 +0000

    *** empty log message ***

--- src/mcisme/datfileconvert.cpp
+++ src/mcisme/datfileconvert.cpp
@@ -4,119 +4,129 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 const int MAX_EVENTS = 50;
+const int RECORDS_COUNT = 1000;
+const int ADDRESS_LEN = 10;
 
-//	бв а п бвагЄвга  "б®ЎлвЁҐ ¤«п  Ў®­Ґ­в " 
+//	старая структура "событие для абонента" 
 struct event_cell_ver0
 {
 	uint32_t        date;
 	uint8_t         id;
-	uint8_t		calling_num[10];
+	uint8_t			calling_num[ADDRESS_LEN];
 	uint8_t         reserved[5];
 };
 
-//	бв а п бвагЄвга  д ©«  б®ЎлвЁ© ¤«п  Ў®­Ґ­в®ў
+//	старая структура файла событий для абонентов
 struct dat_file_cell_ver0
 {
 	uint8_t         event_count;
-	uint8_t		inaccessible_num[10];
+	uint8_t			inaccessible_num[ADDRESS_LEN];
 	uint8_t         reserved[5];
 	event_cell_ver0 events[MAX_EVENTS];
 };
 
-//	­®ў п бвагЄвга  "б®ЎлвЁҐ ¤«п  Ў®­Ґ­в " 
+//	новая структура "событие для абонента" 
 struct event_cell_ver1
 {
-	uint64_t         date;
+	uint64_t		date;
 	uint8_t         id;
-	uint8_t		calling_num[10];
+	uint8_t			calling_num[ADDRESS_LEN];
 	uint8_t         reserved[5];
 };
 
-//	­®ў п бвагЄвга  д ©«  б®ЎлвЁ© ¤«п  Ў®­Ґ­в®ў
+//	новая структура файла событий для абонентов
 struct dat_file_cell_ver1
 {
-	uint64_t	schedTime;
-	uint8_t         event_count;
-	uint8_t		inaccessible_num[10];
-	uint8_t         reserved[5];
+	uint64_t		schedTime;
+	uint8_t			event_count;
+	uint16_t		last_error;
+	uint8_t			inaccessible_num[ADDRESS_LEN];
+	uint8_t         reserved[3];
 	event_cell_ver1 events[MAX_EVENTS];
 };
 
 int convert(const char* src_path, const char* dst_path)
 {
-	int src;
-	int dst;
+	int	src, dst;
 
 	if(-1 == (src = open(src_path, O_RDONLY)))
 	{
 		perror("Error opening source file");
 		return 1;
 	}
-	if(-1 == (dst = open(dst_path, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)))
+	if(-1 == (dst = open(dst_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)))
 	{
 		perror("Error opening destination file");
+		close(src);
 		return 1;
 	}
-/*	uint32_t	rb, i, count; //, cell_num=0;
-	off_t		size;
-	dat_file_cell	AbntEvents;
 
-	idx_file.SeekEnd(0);
-	if(0 == (size = idx_file.Pos()))
+	const uint32_t		src_buf_size = sizeof(dat_file_cell_ver0) * RECORDS_COUNT;
+	const uint32_t		dst_buf_size = sizeof(dat_file_cell_ver1) * RECORDS_COUNT;
+	uint8_t				src_buf[src_buf_size ];
+	uint8_t				dst_buf[dst_buf_size ];
+	dat_file_cell_ver0*	dat0;
+	dat_file_cell_ver1*	dat1;
+	uint32_t			rb, count;
+	off_t				src_file_size = lseek(src, 0, SEEK_END);
+	
+	if(0 == lseek(src, 0, SEEK_END))
 	{
-		smsc_log_debug(logger, "FSStorage: FSStorage: Nothing to load - idx_file is empty.\n");
-		delete[] buf;
+		printf("Source file is empty\n");
+		close(src);
+		close(dst);
 		return 0;
 	}
-	idx_file.Seek(0);
-		
-	while(rb = idx_file.Read((void*)buf, buf_size))
+
+	if( src_file_size != ((src_file_size/sizeof(dat_file_cell_ver0))*sizeof(dat_file_cell_ver0)))
 	{
-//		if( (size - idx_file.Pos())/sizeof(idx_file_cell) < 1000)
-		count = rb / sizeof(idx_file_cell);
-		if( (count * sizeof(idx_file_cell)) != rb)
-		{
-			delete[] buf; 
-			return 1;
-		}
-		for(i = 0; i < count; i++)
-		{
-			if(0 == memcmp((void*)&buf[i*sizeof(idx_file_cell)], (void*)zero_idx_cell, sizeof(idx_file_cell)))
-			{
-				freeCells.AddCell(cell_num);
-			}
-			else
-			{
-				AbntAddr abnt(&buf[i*sizeof(idx_file_cell)]);
-				LoadAbntEvents(abnt, &AbntEvents);
-				hashAbnt.Insert(abnt, cell_num);
-				pDeliveryQueue->Schedule(abnt, false, AbntEvents.schedTime);
-			}
-			cell_num++;
-		}
+		printf("Source file corrupted\n");
+		close(src);
+		close(dst);
+		return 1;
 	}
-*/
+
+	lseek(src, 0, SEEK_SET);
+		
+	while(rb = read(src, (void*)src_buf, src_buf_size))
+	{
+		count = rb / sizeof(dat_file_cell_ver0);
+		for(int i = 0; i < count; i++)
+		{
+			dat0 = (dat_file_cell_ver0*)&src_buf[i*sizeof(dat_file_cell_ver0)];
+			dat1 = (dat_file_cell_ver1*)&dst_buf[i*sizeof(dat_file_cell_ver1)];
+
+			dat1->event_count = dat0->event_count;
+			dat1->schedTime = -1;
+			dat1->event_count = dat0->event_count;
+			memcpy((void*)&(dat1->inaccessible_num), (void*)&(dat0->inaccessible_num), ADDRESS_LEN);
+
+			for(int j = 0; j < MAX_EVENTS; j++)
+			{
+				dat1->events[j].date = dat0->events[j].date;
+				dat1->events[j].id = dat0->events[j].id;
+				memcpy((void*)&(dat1->events[j].calling_num), (void*)&(dat0->events[j].calling_num), ADDRESS_LEN);
+			}
+		}
+		write(dst, dst_buf, dst_buf_size);
+	}
+	close(src);
+	close(dst);
   return 1;
 }
 
 
 int main(int argc,char* argv[])
 {
-  if(argc==1)
+  if(argc != 3)
   {
-    printf("%s converts old 32-bit format data_file to new 64-bit format\n",argv[0]);
+    printf("%s converts old 32-bit format data_file to new 64-bit format\n", argv[0]);
+	printf("Usage: %s source_file destinationfile\n", argv[0]);
     return -1;
   }
-
-  printf("00%%\n");
-  for(int i=0; i <=100; i++)
-  {
-	sleep(1);
-	printf("%c%c%.2d\n", 8,8, i);
-  }
-
-
+  convert(argv[1], argv[2]);
   return 1;
 }
