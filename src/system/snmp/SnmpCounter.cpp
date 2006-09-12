db7c74f014eff29326788a3d33a1e04accd98b95
skv <>
2006-09-12 11:56:34 +0000

    snmp counters

---
+++ src/system/snmp/SnmpCounter.cpp
@@ -0,0 +1,131 @@
+#include "SnmpCounter.hpp"
+#include "util/xml/DOMTreeReader.h"
+#include "util/xml/utilFunctions.h"
+
+namespace smsc{
+namespace system{
+
+using namespace xercesc;
+using namespace smsc::util::xml;
+
+const char* cntNames[]={
+"accepted",
+"rejected",
+"retried",
+"delivered",
+"failed",
+"temperror",
+"0x014",
+"0x058",
+"0x00d",
+"SDP",
+"other"
+};
+
+static Hash<int> cntNameToIdx;
+
+struct InitCntNameToIdx{
+  InitCntNameToIdx()
+  {
+    for(int i=0;i<sizeof(cntNames)/sizeof(cntNames[0]);i++)
+    {
+      cntNameToIdx.Insert(cntNames[i],i);
+    }
+  }
+}cntNameToIdxIniter;
+
+
+static void ParseRecord(DOMNode* record,SnmpCounter::SeverityLimits& lim)
+{
+  for(DOMNode *node = record->getFirstChild(); node != 0; node = node->getNextSibling())
+  {
+    if (node->getNodeType() != DOMNode::ELEMENT_NODE)
+    {
+      continue;
+    }
+
+    DOMNamedNodeMap *attrs = node->getAttributes();
+    if(!attrs)
+    {
+      throw smsc::util::Exception("Invalid snmp.xml configuration file");
+    }
+    XmlStr cntName(attrs->getNamedItem(XmlStr("name"))->getNodeValue());
+    const int* idxPtr=cntNameToIdx.GetPtr(cntName.c_str());
+    if(!idxPtr)
+    {
+      throw smsc::util::Exception("Unknown counter '%s' in snmp.xml",cntName.c_str());
+    }
+    XmlStr content(node->getTextContent());
+    if(sscanf(content.c_str(),"%d,%d,%d,%d",
+              &lim.values[*idxPtr][0],
+              &lim.values[*idxPtr][1],
+              &lim.values[*idxPtr][2],
+              &lim.values[*idxPtr][3])!=4)
+    {
+      throw smsc::util::Exception("Failed to parse severity limits for counter '%s' in snmp.xml:%s",cntName.c_str(),content.c_str());
+    }
+  }
+}
+
+void SnmpCounter::LoadCfg(const char* fileName)
+{
+  sync::MutexGuard mg(cfgMtx);
+  DOMTreeReader reader;
+  DOMDocument* doc=reader.read(fileName);
+  DOMElement *elem = doc->getDocumentElement();
+  DOMNodeList *list = elem->getElementsByTagName(XmlStr("default"));
+  DOMNode *record = list->item(0);
+  ParseRecord(record,defSvrtLim);
+  list = elem->getElementsByTagName(XmlStr("object"));
+  int listLength=list->getLength();
+  svrtHash.Empty();
+  for(int i=0;i<listLength;i++)
+  {
+    record=list->item(i);
+    DOMNamedNodeMap *attrs = record->getAttributes();
+    XmlStr objId(attrs->getNamedItem(XmlStr("id"))->getNodeValue());
+    XmlStr enabled(attrs->getNamedItem(XmlStr("enabled"))->getNodeValue());
+    SeverityLimits lmt;
+    lmt=defSvrtLim;
+    lmt.enabled=enabled=="true";
+    ParseRecord(record,lmt);
+    svrtHash.Insert(objId.c_str(),lmt);
+  }
+}
+
+void SnmpCounter::Check(const char* name,Counters& cnt,Severities& svrt)
+{
+  static const char* alarmIdName[cntCountersNumber]=
+  {
+    "Accepted",
+    "Rejected",
+    "Retried",
+    "Delivered",
+    "Failed",
+    "TempError",
+    "0x014",
+    "0x058",
+    "0x00d",
+    "SDP",
+    "Other",
+  };
+  char msg[1024];
+  char alarmId[64];
+  using smsc::snmp::SnmpAgent;
+  for(int i=0;i<cntCountersNumber;i++)
+  {
+    SeverityType curSvrt=GetSeverityValue(name,i,cnt.values[i]);
+    if(svrt.values[i]!=curSvrt)
+    {
+      sprintf(alarmId,"%s_%s",name,alarmIdName[i]);
+      sprintf(msg,"%s SMPP %s Threshold crossed (AlarmId=%s; severity=%d)",curSvrt==SnmpAgent::NORMAL?"CLEARED":"ACTIVE",alarmId,alarmId,curSvrt);
+      smsc::snmp::SnmpAgent::trap(alarmId,"SMPP",curSvrt,msg);
+      svrt.values[i]=curSvrt;
+    }
+    cnt.values[i]=0;
+  }
+}
+
+
+} //system
+} //smsc
---
+++ src/system/snmp/SnmpCounter.hpp
@@ -0,0 +1,185 @@
+#ifndef __SMSC_SYSTEM_SNMP_COUNTERS_HPP__
+#define __SMSC_SYSTEM_SNMP_COUNTERS_HPP__
+
+#include "core/synchronization/EventMonitor.hpp"
+#include "core/buffers/Hash.hpp"
+#include "core/threads/Thread.hpp"
+#include "logger/Logger.h"
+#include "snmp/SnmpAgent.hpp"
+
+namespace smsc{
+namespace system{
+
+namespace buf=smsc::core::buffers;
+namespace sync=smsc::core::synchronization;
+
+class SnmpCounter:public smsc::core::threads::Thread{
+public:
+  static void Init(const char* cfgFile)
+  {
+    instance=new SnmpCounter;
+    instance->LoadCfg(cfgFile);
+    instance->Start();
+  }
+  static void Shutdown()
+  {
+    instance->Stop();
+    instance->WaitFor();
+    delete instance;
+    instance=0;
+  }
+  static SnmpCounter& getInstance()
+  {
+    return *instance;
+  }
+
+  void LoadCfg(const char* fileName);
+
+  enum CounterId{
+    cntAccepted,
+    cntRejected,
+    cntRetried,
+    cntDelivered,
+    cntFailed,
+    cntTempError,
+    cntErr0x14,
+    cntErr0x58,
+    cntErr0xd,
+    cntErrSDP,
+    cntErrOther,
+    cntCountersNumber
+  };
+
+  void incCounter(CounterId id,const char* smeId)
+  {
+    if(id>=cntCountersNumber)
+    {
+      smsc_log_warn(smsc::logger::Logger::getInstance("snmpcnt"),"INVALID VALUE FOR COUNTER ID:%d",id);
+      return;
+    }
+    sync::MutexGuard mg(mon);
+    totalCnt.values[id]++;
+    Counters* ptr=smeCnt.GetPtr(smeId);
+    if(!ptr)
+    {
+      ptr=smeCnt.SetItem(smeId,Counters());
+    }
+    ptr->values[id]++;
+  }
+
+  struct SeverityLimits
+  {
+    int values[cntCountersNumber][4];
+    bool enabled;
+    SeverityLimits()
+    {
+      memset(values,0,sizeof(values));
+      enabled=true;
+    }
+  };
+
+protected:
+  struct Counters
+  {
+    Counters()
+    {
+      memset(values,0,sizeof(values));
+    }
+    uint32_t values[cntCountersNumber];
+  };
+
+  typedef smsc::snmp::SnmpAgent::alertSeverity SeverityType;
+
+  struct Severities
+  {
+    Severities()
+    {
+      for(int i=0;i<cntCountersNumber;i++)
+      {
+        values[i]=smsc::snmp::SnmpAgent::NORMAL;
+      }
+    }
+    SeverityType values[cntCountersNumber];
+  };
+
+  Counters totalCnt;
+  Severities totalSvrt;
+  buf::Hash<Counters> smeCnt;
+  buf::Hash<Severities> smeSvrt;
+  sync::EventMonitor mon;
+  bool isStopping;
+
+  sync::Mutex cfgMtx;
+  buf::Hash<SeverityLimits> svrtHash;
+  SeverityLimits defSvrtLim;
+
+  SnmpCounter():isStopping(false)
+  {
+  }
+  static SnmpCounter* instance;
+
+  void Stop()
+  {
+    sync::MutexGuard mg(mon);
+    isStopping=true;
+    mon.notify();
+  }
+
+  int Execute()
+  {
+    sync::MutexGuard mg(mon);
+    while(!isStopping)
+    {
+      mon.wait(5*60*1000);
+      if(isStopping)break;
+      Check("GLOBAL",totalCnt,totalSvrt);
+      char* name;
+      Counters* cnt;
+      smeCnt.First();
+      while(smeCnt.Next(name,cnt))
+      {
+        Severities* svrt=smeSvrt.GetPtr(name);
+        if(!svrt)
+        {
+          svrt=smeSvrt.SetItem(name,Severities());
+        }
+        Check(name,*cnt,*svrt);
+      }
+    }
+    return 0;
+  }
+
+  SeverityType GetSeverityValue(const char* name,int cntIdx,uint32_t cntValue)
+  {
+    sync::MutexGuard mg(cfgMtx);
+    SeverityLimits *limPtr=svrtHash.GetPtr(name);
+    if(!limPtr)limPtr=&defSvrtLim;
+    if(!limPtr->enabled)return smsc::snmp::SnmpAgent::NORMAL;
+    int* limArr=limPtr->values[cntIdx];
+    int rv=smsc::snmp::SnmpAgent::NORMAL;
+    if(limArr[0]<limArr[1])
+    {
+      for(int i=0;i<4;i++)
+      {
+        if(cntValue>limArr[i])break;
+        rv++;
+      }
+    }else
+    {
+      for(int i=0;i<4;i++)
+      {
+        if(cntValue<limArr[i])break;
+        rv++;
+      }
+    }
+    return (SeverityType)rv;
+  }
+
+  void Check(const char* name,Counters& cnt,Severities& svrt);
+
+};
+
+} //system
+} //smsc
+
+#endif
