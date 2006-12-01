1f538e6a90ddc7b6a51a3184822052b20ea3bcab
loomox <>
2006-12-01 12:13:40 +0000

    *** empty log message ***

--- src/scag/re/smpp/SmppAdapter.h
+++ src/scag/re/smpp/SmppAdapter.h
@@ -180,8 +180,6 @@ namespace scag { namespace re { namespace smpp
         //virtual void fillRespOperation(smsc::inman::interaction::DeliverySmsResult& op, TariffRec& tariffRec);
 
 
-        virtual SCAGCommand& getSCAGCommand() { return command; }
-        
         virtual ~SmppCommandAdapter();
         static AccessType CheckAccess(int handlerType,const std::string& name);
         //virtual SMS& getSMS();
