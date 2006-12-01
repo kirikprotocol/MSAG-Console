ee05ea98b02777c886e2678adc5c1e4a9cf61425
gvroman <>
2006-12-01 09:42:16 +0000

    MAP-PROCESS-UNSTRUCTURED-SS-REQUEST Dialog

---
+++ src/inman/inap/map_uss/DlgMapUSS.cpp
@@ -0,0 +1,287 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "$Id$";
+#endif /* MOD_IDENT_OFF */
+/* ************************************************************************* *
+ * MAP-PROCESS-UNSTRUCTURED-SS-REQUEST service:
+ * dialog implementation (over TCAP dialog)
+ * ************************************************************************* */
+#include <assert.h>
+
+#include "inman/inap/map_uss/DlgMapUSS.hpp"
+using smsc::inman::comp::uss::ProcessUSSRequestArg;
+using smsc::inman::comp::uss::MAPUSS_OpCode;
+
+#include "inman/common/adrutil.hpp"
+using smsc::cvtutil::packMAPAddress2OCTS;
+
+#include "util/BinDump.hpp"
+using smsc::util::DumpHex;
+
+namespace smsc {
+namespace inman {
+namespace inap {
+namespace uss {
+
+static void makeUI(std::vector<unsigned char> & ui, const TonNpiAddress & msadr,
+                   const TonNpiAddress & own_adr)
+{
+    //BER encoded OID
+    static const UCHAR_T ui_oid[] = {06,0x07,0x04,0x00,0x00,0x01,0x01,0x01,0x01,0xA0};
+
+    unsigned char addrOcts[sizeof(TONNPI_ADDRESS_OCTS) + 1];
+    ui.clear();
+
+    unsigned addrLen = packMAPAddress2OCTS(msadr, (TONNPI_ADDRESS_OCTS*)addrOcts);
+    ui.insert(ui.begin(), addrOcts, addrOcts + addrLen);
+    ui.insert(ui.begin(), (unsigned char)addrLen);
+    ui.insert(ui.begin(), 0x82);
+
+    addrLen = packMAPAddress2OCTS(own_adr, (TONNPI_ADDRESS_OCTS*)addrOcts);
+    ui.insert(ui.begin(), addrOcts, addrOcts + addrLen);
+    ui.insert(ui.begin(), (unsigned char)addrLen);
+    ui.insert(ui.begin(), 0x81);
+
+    addrLen = ui.size();
+    ui.insert(ui.begin(), (unsigned char)addrLen);
+    ui.insert(ui.begin(), 0xA0);
+
+    addrLen = ui.size();
+    ui.insert(ui.begin(), (unsigned char)addrLen);
+    ui.insert(ui.begin(), ui_oid, ui_oid + sizeof(ui_oid));
+
+    addrLen = ui.size();
+    ui.insert(ui.begin(), (unsigned char)addrLen);
+    ui.insert(ui.begin(), 0x28);
+}
+
+/* ************************************************************************** *
+ * class MapUSSDlg implementation:
+ * ************************************************************************** */
+MapUSSDlg::MapUSSDlg(TCSessionSR* pSession, USSDhandler * res_handler,
+                        Logger * uselog/* = NULL*/)
+    : resHdl(res_handler), session(pSession), dlgId(0), dialog(NULL)
+    , logger(uselog)
+{
+    assert(res_handler && pSession);
+    dlgState.value = 0;
+    if (!logger)
+        logger = Logger::getInstance("smsc.inman.inap");
+}
+
+MapUSSDlg::~MapUSSDlg()
+{
+    endMapDlg();
+}
+
+void MapUSSDlg::endMapDlg(void)
+{
+    MutexGuard  grd(_sync);
+    resHdl = NULL;
+    endTCap();
+}
+
+/* ------------------------------------------------------------------------ *
+ * MAP-PROCESS-UNSTRUCTURED-SS-REQUEST interface
+ * ------------------------------------------------------------------------ */
+void MapUSSDlg::requestSS(std::vector<unsigned char> & rq_data, unsigned char dcs,
+                    const char * subsc_adr/* = NULL*/) throw (CustomException)
+{
+    TonNpiAddress msAdr;
+    if (subsc_adr && !msAdr.fromText(subsc_adr))
+        throw CustomException("MapUSS[%u]: invalid subscrAdr: %s", subsc_adr);
+    requestSS(rq_data, dcs, subsc_adr ? &msAdr : NULL);
+}
+
+void MapUSSDlg::requestSS(std::vector<unsigned char> & rq_data, unsigned char dcs,
+                    const TonNpiAddress * subsc_adr/* = NULL*/) throw (CustomException)
+{
+    //create TCAP Dialog
+    MutexGuard  grd(_sync);
+    dialog = session->openDialog();
+    if (!dialog)
+        throw CustomException("MapUSS: Unable to create TC Dialog");
+    dialog->addListener(this);
+    dlgId = dialog->getId();
+
+    //create Component for TCAP invoke
+    ProcessUSSRequestArg    arg;
+
+    if (dcs == PLAIN_LATIN1_DCS) { //plain text
+        arg.setUSSData(&rq_data[0], rq_data.size());
+        smsc_log_debug(logger, "MapUSS[%u]: USS request: %.*s" , dlgId,
+                       rq_data.size(), &rq_data[0]);
+    } else {
+        arg.setRAWUSSData(dcs, &rq_data[0], rq_data.size());
+        smsc_log_debug(logger, "MapUSS[%u]: USS request: 0x%s", dlgId,
+                        DumpHex(rq_data.size(), &rq_data[0]).c_str());
+    }
+    Invoke* op = dialog->initInvoke(MAPUSS_OpCode::processUSS_Request, this);
+    op->setParam(&arg);
+    dialog->sendInvoke(op);
+
+    //GVR NOTE: though MAP specifies that msISDN address in USS request may
+    //present in component portion of TCAP invoke, the Ericsson tools transfers
+    //it only in user info section.
+    if (subsc_adr) {    //prepare user info
+        std::vector<unsigned char>   ui4;
+        makeUI(ui4, *subsc_adr,  session->getOwnAdr());
+        dialog->beginDialog(&ui4[0], ui4.size()); //throws
+    } else
+        dialog->beginDialog(); //throws
+    dlgState.s.ctrInited = MAP_OPER_INITED;
+}
+
+/* ------------------------------------------------------------------------ *
+ * InvokeListener interface
+ * ------------------------------------------------------------------------ */
+void MapUSSDlg::onInvokeResultNL(Invoke* op, TcapEntity* res)
+{
+    MutexGuard  grd(_sync);
+    smsc_log_warn(logger, "MapUSS[%u]: Invoke[%u:%u] got a ResultNL: %u",
+        dlgId, (unsigned)op->getId(), (unsigned)op->getOpcode(),
+        (unsigned)res->getOpcode());
+
+    dlgState.s.ctrResulted = MAP_OPER_INITED;
+    reqRes.reset((MAPUSS2CompAC*)(res->getParam()));
+    res->setParam(NULL);
+}
+
+void MapUSSDlg::onInvokeResult(Invoke* op, TcapEntity* res)
+{
+    unsigned do_end = 0;
+    {
+        MutexGuard  grd(_sync);
+        smsc_log_debug(logger, "MapUSS[%u]: Invoke[%u:%u] got a Result: %u",
+            dlgId, (unsigned)op->getId(), (unsigned)op->getOpcode(),
+            (unsigned)res->getOpcode());
+
+        dlgState.s.ctrResulted = MAP_OPER_DONE;
+        reqRes.reset((MAPUSS2CompAC*)(res->getParam()));
+        res->setParam(NULL);
+        resHdl->onMapResult(reqRes.get());
+        if ((do_end = dlgState.s.ctrFinished) != 0)
+            endTCap();
+    }
+    if (do_end)
+        resHdl->onEndMapDlg(0, smsc::inman::errOk);
+}
+
+//Called if Operation got ResultError
+void MapUSSDlg::onInvokeError(Invoke *op, TcapEntity * resE)
+{
+    {
+        MutexGuard  grd(_sync);
+        smsc_log_error(logger, "MapUSS[%u]: Invoke[%u:%u] got a Error: %u",
+            dlgId, (unsigned)op->getId(), (unsigned)op->getOpcode(),
+            (unsigned)resE->getOpcode());
+
+        dlgState.s.ctrInited = MAP_OPER_FAIL;
+        endTCap();
+    }
+    resHdl->onEndMapDlg(resE->getOpcode(), smsc::inman::errMAP);
+}
+
+//Called if Operation got L_CANCEL, possibly while waiting result
+void MapUSSDlg::onInvokeLCancel(Invoke *op)
+{
+    {
+        MutexGuard  grd(_sync);
+        smsc_log_error(logger, "MapUSS[%u]: Invoke[%u:%u] got a LCancel",
+            dlgId, (unsigned)op->getId(), (unsigned)op->getOpcode());
+        endTCap();
+    }
+    resHdl->onEndMapDlg(MapUSSDlg::ussServiceResponse, smsc::inman::errMAPuser);
+}
+
+/* ------------------------------------------------------------------------ *
+ * DialogListener interface
+ * ------------------------------------------------------------------------ */
+//SCF sent ContinueDialog.
+void MapUSSDlg::onDialogContinue(bool compPresent)
+{
+    MutexGuard  grd(_sync);
+    if (!compPresent)
+        smsc_log_error(logger, "MapUSS[%u]: missing component in TC_CONT_IND", dlgId);
+     //else wait for ongoing Invoke result/error
+    return;
+}
+
+//TCAP indicates DialogPAbort: either due to TC layer error on SCF/HLR side or
+//because of local TC dialog timeout is expired (no T_END_IND from HLR).
+void MapUSSDlg::onDialogPAbort(UCHAR_T abortCause)
+{
+    {
+        MutexGuard  grd(_sync);
+        dlgState.s.ctrAborted = 1;
+        smsc_log_error(logger, "MapUSS[%u]: P_ABORT at state 0x%x", dlgId, dlgState.value);
+        endTCap();
+    }
+    resHdl->onEndMapDlg(abortCause, smsc::inman::errTCAP);
+}
+
+//SCF sent DialogUAbort (some logic error on SCF side).
+void MapUSSDlg::onDialogUAbort(USHORT_T abortInfo_len, UCHAR_T *pAbortInfo,
+                                    USHORT_T userInfo_len, UCHAR_T *pUserInfo)
+{
+    {
+        MutexGuard  grd(_sync);
+        dlgState.s.ctrAborted = 1;
+        smsc_log_error(logger, "MapUSS[%u]: U_ABORT at state 0x%x", dlgId, dlgState.value);
+        endTCap();
+    }
+    resHdl->onEndMapDlg((abortInfo_len == 1) ? *pAbortInfo : Dialog::tcUserGeneralError,
+                       smsc::inman::errTCuser);
+}
+
+//SCF sent DialogEnd, it's either succsesfull contract completion,
+//or some logic error (f.ex. timeout expiration) on SSF side.
+void MapUSSDlg::onDialogREnd(bool compPresent)
+{
+    InmanErrorType layer = smsc::inman::errOk;
+    unsigned char  errcode = 0;
+    {
+        MutexGuard  grd(_sync);
+        dlgState.s.ctrFinished = 1;
+        if (!compPresent) {
+            endTCap();
+            if (!dlgState.s.ctrResulted) {
+                smsc_log_error(logger, "MapUSS[%u]: T_END_IND, state 0x%x", dlgId, dlgState.value);
+                layer = smsc::inman::errMAPuser;
+                errcode = MapUSSDlg::ussServiceResponse;
+            }
+        }
+    }
+    if (!compPresent)
+        resHdl->onEndMapDlg(errcode, layer);
+    //else wait for ongoing Invoke result/error
+}
+
+/* ------------------------------------------------------------------------ *
+ * Private/protected methods
+ * ------------------------------------------------------------------------ */
+//ends TC dialog, releases Dialog()
+void MapUSSDlg::endTCap(void)
+{
+    if (dialog) {
+        dialog->removeListener(this);
+        if (!(dialog->getState().value & TC_DLG_CLOSED_MASK)) {
+            //see 3GPP 29.078 14.1.2.1.3 smsSSF-to-gsmSCF SMS related messages
+            try {
+                dialog->endDialog(); // do TC_BasicEnd if still active
+                smsc_log_debug(logger, "MapUSS[%u]: T_END_REQ, state: 0x%x", dlgId,
+                                dlgState.value);
+            } catch (std::exception & exc) {
+                smsc_log_error(logger, "MapUSS[%u]: T_END_REQ: %s", dlgId, exc.what());
+                dialog->releaseAllInvokes();
+            }
+        }
+        session->releaseDialog(dialog);
+        dialog = NULL;
+    }
+}
+
+} //chsri
+} //inap
+} //inman
+} //smsc
+
---
+++ src/inman/inap/map_uss/DlgMapUSS.hpp
@@ -0,0 +1,108 @@
+#ident "$Id$"
+/* ************************************************************************* *
+ * MAP-PROCESS-UNSTRUCTURED-SS-REQUEST service:
+ * dialog implementation (over TCAP dialog)
+ * ************************************************************************* */
+#ifndef __SMSC_INMAN_INAP_MAP_USS__
+#define __SMSC_INMAN_INAP_MAP_USS__
+
+#include "inman/inerrcodes.hpp"
+using smsc::inman::InmanErrorType;
+using smsc::inman::_InmanErrorSource;
+
+#include "core/synchronization/Mutex.hpp"
+using smsc::core::synchronization::Mutex;
+
+#include "inman/inap/session.hpp"
+#include "inman/inap/dialog.hpp"
+using smsc::inman::inap::TCSessionSR;
+using smsc::inman::inap::Dialog;
+using smsc::inman::inap::DialogListener;
+using smsc::inman::inap::InvokeListener;
+
+#include "inman/comp/map_uss/MapUSSComps.hpp"
+using smsc::inman::comp::uss::MAPUSS2CompAC;
+
+#define PLAIN_LATIN1_DCS 0xF4 // 1111 0100
+
+namespace smsc {
+namespace inman {
+namespace inap {
+namespace uss {
+
+//USS Dialog Handler
+class USSDhandler { //  <- gsmSCF */
+public: 
+    virtual void onMapResult(MAPUSS2CompAC* arg) = 0;
+    //dialog finalization/error handling:
+    //if errLayer != errOk, dialog is aborted by reason = errcode
+    virtual void onEndMapDlg(unsigned short ercode, InmanErrorType errLayer) = 0;
+};
+
+//USS Dialog state
+#define MAP_OPER_INITED 0x2 //'10'B  on BIG-ENDIAN
+#define MAP_OPER_FAIL   0x3 //'11'B  on BIG-ENDIAN 
+#define MAP_OPER_DONE   0x3 //'11'B  on BIG-ENDIAN 
+typedef union {
+    unsigned short value;
+    struct {
+        unsigned int ctrInited : 2;
+        unsigned int ctrResulted : 2; // INITED - ResultNL, DONE - ResultL
+        unsigned int ctrAborted : 1;
+        unsigned int ctrFinished : 1;
+    } s;
+} USSDState;
+
+//NOTE: MapCHSRI doesn't maintain own timer for operations, it uses instead the 
+//innate timer of the SS7 stack for Invoke lifetime.
+class MapUSSDlg : DialogListener, InvokeListener { //  -> gsmSCF
+public:
+    MapUSSDlg(TCSessionSR* pSession, USSDhandler * res_handler, Logger * uselog = NULL);
+    virtual ~MapUSSDlg();
+
+    enum {
+        ussServiceResponse = 0xFF
+    };
+    void requestSS(std::vector<unsigned char> & rq_data, unsigned char dcs,
+                    const char * subsc_adr = NULL) throw (CustomException);
+    void requestSS(std::vector<unsigned char> & rq_data, unsigned char dcs,
+                    const TonNpiAddress * subsc_adr = NULL) throw (CustomException);
+
+    void endMapDlg(void);
+
+protected:
+    friend class smsc::inman::inap::Dialog;
+    // DialogListener interface
+    void onDialogInvoke(Invoke* op, bool lastComp) { }
+    void onDialogContinue(bool compPresent);
+    void onDialogPAbort(UCHAR_T abortCause);
+    void onDialogREnd(bool compPresent);
+    void onDialogUAbort(USHORT_T abortInfo_len, UCHAR_T *pAbortInfo,
+                        USHORT_T userInfo_len, UCHAR_T *pUserInfo);
+
+    // InvokeListener interface
+    void onInvokeResult(Invoke* inv, TcapEntity* res);
+    void onInvokeError(Invoke* inv, TcapEntity* resE);
+    void onInvokeResultNL(Invoke* inv, TcapEntity* res);
+    void onInvokeLCancel(Invoke* inv);
+
+private:
+    void endTCap(void); //ends TC dialog, releases Dialog()
+
+    Mutex       _sync;
+    unsigned    dlgId;
+    Dialog*     dialog;     //TCAP dialog
+    TCSessionSR* session;   //TCAP dialogs factory
+    Logger*     logger;
+    USSDhandler * resHdl;   //request result handler
+    USSDState   dlgState;   //current state of dialog
+    std::auto_ptr<MAPUSS2CompAC> reqRes;
+};
+
+} //uss
+} //inap
+} //inman
+} //smsc
+
+#endif /* __SMSC_INMAN_INAP_MAP_USS__ */
+
