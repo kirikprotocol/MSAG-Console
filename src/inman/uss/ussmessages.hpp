47b20a6380a097ca144e5819887d7a4fcd25dda3
shulga <>
2007-01-19 10:09:08 +0000

    added support for client request with specified IN platform number

--- src/inman/uss/ussmessages.hpp
+++ src/inman/uss/ussmessages.hpp
@@ -1,8 +1,9 @@
 #ident "$Id$"
 
-#ifndef __SMSC_INMAN_INTERACTION_USSMESSAGES__
-#define __SMSC_INMAN_INTERACTION_USSMESSAGES__
+#ifndef __SMSC_INMAN_USS_USSMESSAGES__
+#define __SMSC_INMAN_USS_USSMESSAGES__
 
+#include <logger/Logger.h>
 #include "util/Factory.hpp"
 using smsc::util::FactoryT;
 
@@ -14,6 +15,9 @@ using smsc::inman::interaction::ObjectBuffer;
 using smsc::inman::interaction::SerializableObjectAC;
 using smsc::inman::interaction::SerializerITF;
 
+#include <sstream>
+
+#include <util/BinDump.hpp>
 
 //NOTE: USSman considers the USS request encoding with DCS == 0xF4 as plain LATIN1 text
 #define USSMAN_LATIN1_DCS 0xF4 // 1111 0100
@@ -42,15 +46,16 @@ struct USS2CMD {
 class SerializerUSS : public SerializerITF, 
                     public FactoryT<unsigned short, SerializableObjectAC> {
 public:
-    virtual ~SerializerUSS() { }
+  virtual ~SerializerUSS() { }
 
-    SerializablePacketAC * deserialize(ObjectBuffer & in) throw(SerializerException);
-    SerializablePacketAC * deserialize(std::auto_ptr<ObjectBuffer>& p_in) throw(SerializerException);
+  SerializablePacketAC * deserialize(ObjectBuffer & in) throw(SerializerException);
+  SerializablePacketAC * deserialize(std::auto_ptr<ObjectBuffer>& p_in) throw(SerializerException);
 
-    static SerializerUSS* getInstance();
+  static SerializerUSS* getInstance();
 
 protected:
-    SerializerUSS();
+  SerializerUSS();
+  smsc::logger::Logger* _logger;
 };
 
 /*
@@ -79,6 +84,7 @@ public:
     void serialize(ObjectBuffer& out_buf) throw(SerializerException)
     {
         out_buf << at(0)->Id();
+        out_buf << dlgId;
         at(0)->save(out_buf);
     }
 };
@@ -107,50 +113,98 @@ typedef std::vector<unsigned char> USSDATA_T;
 //Abstract class for USSman commands
 class USSMessageAC : public SerializableObjectAC {
 public:
-    USSMessageAC(unsigned short msgTag) : SerializableObjectAC(msgTag)
-    { _status = 0; }
-    virtual ~USSMessageAC() {}
+  USSMessageAC(unsigned short msgTag) : SerializableObjectAC(msgTag),
+                                        _logger(smsc::logger::Logger::getInstance("smsc.ussbalance"))
+  {}
+  virtual ~USSMessageAC() {}
 
-    //SerializableObjectAC interface:
-    void load(ObjectBuffer &in) throw(SerializerException);
-    void save(ObjectBuffer &out) const;
+  //SerializableObjectAC interface:
+  virtual void load(ObjectBuffer &in) throw(SerializerException);
+  virtual void save(ObjectBuffer &out) const;
 
-    //assigns USS data, that is plain LATIN1 text,
-    void setUSSData(const unsigned char * data, unsigned size);
-    //assigns USS data encoded according to CBS coding scheme (UCS2, GSM 7bit, etc)
-    void setRAWUSSData(unsigned char dcs, const USSDATA_T& ussdata);
+  //assigns USS data, that is plain LATIN1 text,
+  void setUSSData(const unsigned char * data, unsigned size);
+  void setUSSData(const USSDATA_T& data);
 
-    void setMSISDNadr(const TonNpiAddress& msadr) { _msAdr = msadr; }
-    void setMSISDNadr(const char * adrStr) throw (CustomException);
+  //assigns USS data encoded according to CBS coding scheme (UCS2, GSM 7bit, etc)
+  void setRAWUSSData(unsigned char dcs, const USSDATA_T& ussdata);
 
-    void setStatus(const unsigned short& status) { _status = status; }
+  void setMSISDNadr(const TonNpiAddress& msadr) { _msAdr = msadr; }
+  void setMSISDNadr(const char * adrStr) throw (CustomException);
 
-    const USSDATA_T& getUSSData(void) const { return _ussData; }
-    const TonNpiAddress& getMSISDNadr(void) const { return _msAdr; }
-    unsigned char    getDCS(void) const     { return _dCS; }
-    unsigned short   getStatus(void) const  { return _status; }
+  const USSDATA_T& getUSSData(void) const { return _ussData; }
+  const TonNpiAddress& getMSISDNadr(void) const { return _msAdr; }
+  unsigned char    getDCS(void) const     { return _dCS; }
 
+  virtual std::string toString() const {
+    std::ostringstream obuf;
+    obuf << "msAddr=[" <<_msAdr.toString()
+         << "],dCS=[" << static_cast<uint32_t>(_dCS)
+         << "],ussData=[" << smsc::util::DumpHex(_ussData.size(), &_ussData[0])
+         << "]";
+    return obuf.str();
+  }
 protected:
-    unsigned char   _dCS;
-    USSDATA_T       _ussData;
-    TonNpiAddress   _msAdr;
-    unsigned short  _status; //only for PROCESS_USS_RESULT_TAG
+  unsigned char   _dCS;
+  USSDATA_T       _ussData;
+  TonNpiAddress   _msAdr;
+  smsc::logger::Logger* _logger; // for debug
 };
 
 
 class USSRequestMessage : public USSMessageAC {
 public:
-    USSRequestMessage() : USSMessageAC(USS2CMD::PROCESS_USS_REQUEST_TAG) {}
-    ~USSRequestMessage() {}
+  USSRequestMessage() : USSMessageAC(USS2CMD::PROCESS_USS_REQUEST_TAG) {}
+  ~USSRequestMessage() {}
+
+  //SerializableObjectAC interface:
+  virtual void load(ObjectBuffer &in) throw(SerializerException);
+  virtual void save(ObjectBuffer &out) const;
+
+  void set_IN_ISDNaddr(const TonNpiAddress& inaddr) { _inAddr = inaddr; }
+  void set_IN_ISDNaddr(const char * addrStr) throw (CustomException);
+  void set_IN_SSN(unsigned char inSSN) { _inSSN = inSSN; }
+
+  const TonNpiAddress& get_IN_ISDNaddr(void) const { return _inAddr; }
+  unsigned char    get_IN_SSN(void) const     { return _inSSN; }
+
+  virtual std::string toString() const {
+    std::ostringstream obuf(USSMessageAC::toString());
+    obuf << ",IN_SSN=[" << static_cast<uint32_t>(_inSSN)
+         << "],INAddr=[" << _inAddr.toString()
+         << "]";
+    return obuf.str();
+  }
+private:
+  unsigned char _inSSN;
+  TonNpiAddress _inAddr;
 };
 
 
 class USSResultMessage : public USSMessageAC {
 public:
-    USSResultMessage() : USSMessageAC(USS2CMD::PROCESS_USS_RESULT_TAG) {}
-    ~USSResultMessage() {};
+  USSResultMessage() : USSMessageAC(USS2CMD::PROCESS_USS_RESULT_TAG) {}
+  ~USSResultMessage() {};
 
-    bool  getUSSDataAsLatin1Text(std::string & str);
+  virtual void load(ObjectBuffer &in) throw(SerializerException);
+  virtual void save(ObjectBuffer &out) const;
+
+  bool  getUSSDataAsLatin1Text(std::string & str);
+  unsigned short   getStatus(void) const  { return _status; }
+  void setStatus(const unsigned short& status) { _status = status; }
+
+  virtual std::string toString() const {
+    std::ostringstream obuf;
+    obuf << "status=[" << _status
+         << "]";
+    if ( !_status )
+      obuf << "," 
+           << USSMessageAC::toString();
+    return obuf.str();
+  }
+
+private:
+  unsigned short  _status;
 };
 
 // --------------------------------------------------------- //
@@ -172,4 +226,4 @@ public:
 } //inman
 } //smsc
 
-#endif /* __SMSC_INMAN_INTERACTION_USSMESSAGES__ */
+#endif /* __SMSC_INMAN_USS_USSMESSAGES__ */
