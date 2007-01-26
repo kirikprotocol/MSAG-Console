d3eefdf35dd9b4105f1a77489bd3d7df8d692a1b
skv <>
2007-01-26 12:50:50 +0000

    check/fix

--- src/sme/SmppBase.hpp
+++ src/sme/SmppBase.hpp
@@ -165,7 +165,7 @@ public:
   }
   static void sigdisp(int sig)
   {
-    if(StaticHolder<0>::olddisp!=SIG_HOLD && StaticHolder<0>::olddisp!=SIG_ERR)
+    if(StaticHolder<0>::olddisp && StaticHolder<0>::olddisp!=SIG_HOLD && StaticHolder<0>::olddisp!=SIG_ERR)
     {
       StaticHolder<0>::olddisp(sig);
     }
