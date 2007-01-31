780ebe18a59e2c2b31e6c415649a971126a0e746
shulga <>
2007-01-31 11:03:40 +0000

    Catch std::exception and rethrow SQLException in DBEntityStorageConnection::createStatement

--- src/db/fileStorage/DBEntityStorageDataSource.cpp
+++ src/db/fileStorage/DBEntityStorageDataSource.cpp
@@ -95,111 +95,117 @@ Statement*
 DBEntityStorageConnection::createStatement(const char* sql)
   throw(SQLException)
 {
-  smsc_log_debug(_logger, "Enter DBEntityStorageConnection::createStatement,sql=[%s]",sql);
-  static size_t
-    del_infosme_id_map_len = strlen("DELETE FROM INFOSME_ID_MAPPING"),
-    sel_from_infosme_id_map_len = strlen("SELECT ID, TASK_ID FROM INFOSME_ID_MAPPING"),
-    ins_into_infosme_id_map_len = strlen("INSERT INTO INFOSME_ID_MAPPING"),
-    ins_into_infosme_gen_tasks_len = strlen("INSERT INTO INFOSME_GENERATING_TASKS"),
-    del_from_infosme_gen_tasks_len = strlen("DELETE FROM INFOSME_GENERATING_TASKS"),
-    del_from_infosme_tasks_stat_len = strlen("DELETE FROM INFOSME_TASKS_STAT"),
-    ins_into_infosme_tasks_stat_len = strlen("INSERT INTO INFOSME_TASKS_STAT"),
-    sel_from_infosme_t_len = strlen("SELECT ID, ABONENT, MESSAGE"),
-    create_index_len = strlen("CREATE INDEX"),
-    create_table_len = strlen("CREATE TABLE"),
-    drop_table_len = strlen("DROP TABLE"),
-    delete_keyword_len = strlen("DELETE"),
-    insert_into_len = strlen("INSERT INTO"),
-    update_keyword_len = strlen("UPDATE"),
-    infosme_t_fulltablescan_len = strlen("FULL_TABLE_SCAN FROM"),
-    infosme_tasks_stat_fulltablescan_len = strlen("FULL_TABLE_SCAN FROM INFOSME_TASKS_STAT");
+  try {
+    smsc_log_debug(_logger, "Enter DBEntityStorageConnection::createStatement,sql=[%s]",sql);
+    static size_t
+      del_infosme_id_map_len = strlen("DELETE FROM INFOSME_ID_MAPPING"),
+      sel_from_infosme_id_map_len = strlen("SELECT ID, TASK_ID FROM INFOSME_ID_MAPPING"),
+      ins_into_infosme_id_map_len = strlen("INSERT INTO INFOSME_ID_MAPPING"),
+      ins_into_infosme_gen_tasks_len = strlen("INSERT INTO INFOSME_GENERATING_TASKS"),
+      del_from_infosme_gen_tasks_len = strlen("DELETE FROM INFOSME_GENERATING_TASKS"),
+      del_from_infosme_tasks_stat_len = strlen("DELETE FROM INFOSME_TASKS_STAT"),
+      ins_into_infosme_tasks_stat_len = strlen("INSERT INTO INFOSME_TASKS_STAT"),
+      sel_from_infosme_t_len = strlen("SELECT ID, ABONENT, MESSAGE"),
+      create_index_len = strlen("CREATE INDEX"),
+      create_table_len = strlen("CREATE TABLE"),
+      drop_table_len = strlen("DROP TABLE"),
+      delete_keyword_len = strlen("DELETE"),
+      insert_into_len = strlen("INSERT INTO"),
+      update_keyword_len = strlen("UPDATE"),
+      infosme_t_fulltablescan_len = strlen("FULL_TABLE_SCAN FROM"),
+      infosme_tasks_stat_fulltablescan_len = strlen("FULL_TABLE_SCAN FROM INFOSME_TASKS_STAT");
 
-  if (!strncmp(sql,"DELETE FROM INFOSME_GENERATING_TASKS",del_from_infosme_gen_tasks_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_Generating_Tasks_By_TaskId");
+    if (!strncmp(sql,"DELETE FROM INFOSME_GENERATING_TASKS",del_from_infosme_gen_tasks_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_Generating_Tasks_By_TaskId");
 
-    return new Delete_from_InfoSme_Generating_Tasks_By_TaskId(StorageHelper::getInfoSme_GeneratingTasks_Storage());
-  } else if (!strncmp(sql,"INSERT INTO INFOSME_GENERATING_TASKS",ins_into_infosme_gen_tasks_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object InfoSme_Generating_Tasks_DBEntityStorage");
+      return new Delete_from_InfoSme_Generating_Tasks_By_TaskId(StorageHelper::getInfoSme_GeneratingTasks_Storage());
+    } else if (!strncmp(sql,"INSERT INTO INFOSME_GENERATING_TASKS",ins_into_infosme_gen_tasks_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object InfoSme_Generating_Tasks_DBEntityStorage");
 
-    return new Insert_into_InfoSme_Generating_Tasks(StorageHelper::getInfoSme_GeneratingTasks_Storage());
-  } else if (!strncmp(sql,"INSERT INTO INFOSME_ID_MAPPING",ins_into_infosme_id_map_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Insert_into_InfoSme_Id_Mapping");
+      return new Insert_into_InfoSme_Generating_Tasks(StorageHelper::getInfoSme_GeneratingTasks_Storage());
+    } else if (!strncmp(sql,"INSERT INTO INFOSME_ID_MAPPING",ins_into_infosme_id_map_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Insert_into_InfoSme_Id_Mapping");
 
-    return new Insert_into_InfoSme_Id_Mapping(StorageHelper::getInfoSme_Id_Mapping_Storage());
-  } else if (!strncmp(sql,"SELECT ID, TASK_ID FROM INFOSME_ID_MAPPING",sel_from_infosme_id_map_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object SelectInfoSme_Id_Mapping_SmscId_criterion");
+      return new Insert_into_InfoSme_Id_Mapping(StorageHelper::getInfoSme_Id_Mapping_Storage());
+    } else if (!strncmp(sql,"SELECT ID, TASK_ID FROM INFOSME_ID_MAPPING",sel_from_infosme_id_map_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object SelectInfoSme_Id_Mapping_SmscId_criterion");
 
-    return new SelectInfoSme_Id_Mapping_SmscId_criterion(StorageHelper::getInfoSme_Id_Mapping_Storage());
-  } else if (!strncmp(sql,"DELETE FROM INFOSME_ID_MAPPING",del_infosme_id_map_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_Id_Mapping_By_SmscId");
+      return new SelectInfoSme_Id_Mapping_SmscId_criterion(StorageHelper::getInfoSme_Id_Mapping_Storage());
+    } else if (!strncmp(sql,"DELETE FROM INFOSME_ID_MAPPING",del_infosme_id_map_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_Id_Mapping_By_SmscId");
 
-    return new Delete_from_InfoSme_Id_Mapping_By_SmscId(StorageHelper::getInfoSme_Id_Mapping_Storage());
-  } else if (!strncmp(sql,"DELETE FROM INFOSME_TASKS_STAT",del_from_infosme_tasks_stat_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_Tasks_Stat_By_TaskId");
+      return new Delete_from_InfoSme_Id_Mapping_By_SmscId(StorageHelper::getInfoSme_Id_Mapping_Storage());
+    } else if (!strncmp(sql,"DELETE FROM INFOSME_TASKS_STAT",del_from_infosme_tasks_stat_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_Tasks_Stat_By_TaskId");
 
-    return new Delete_from_InfoSme_Tasks_Stat_By_TaskId(StorageHelper::getInfoSme_Tasks_Stat_Storage());
-  } else if (!strncmp(sql,"INSERT INTO INFOSME_TASKS_STAT",ins_into_infosme_tasks_stat_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Insert_into_InfoSme_Tasks_Stat");
+      return new Delete_from_InfoSme_Tasks_Stat_By_TaskId(StorageHelper::getInfoSme_Tasks_Stat_Storage());
+    } else if (!strncmp(sql,"INSERT INTO INFOSME_TASKS_STAT",ins_into_infosme_tasks_stat_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Insert_into_InfoSme_Tasks_Stat");
 
-    return new Insert_into_InfoSme_Tasks_Stat(StorageHelper::getInfoSme_Tasks_Stat_Storage());
-  } else if ( !strncmp(sql, "FULL_TABLE_SCAN FROM INFOSME_TASKS_STAT", infosme_tasks_stat_fulltablescan_len)) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object SelectInfoSme_TasksStat_fullTableScan");
-    return new SelectInfoSme_TasksStat_fullTableScan(StorageHelper::getInfoSme_Tasks_Stat_Storage());
-  } else if ( !strncmp(sql, "CREATE INDEX", create_index_len) ) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object DDLStatement for CREATE INDEX");
+      return new Insert_into_InfoSme_Tasks_Stat(StorageHelper::getInfoSme_Tasks_Stat_Storage());
+    } else if ( !strncmp(sql, "FULL_TABLE_SCAN FROM INFOSME_TASKS_STAT", infosme_tasks_stat_fulltablescan_len)) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object SelectInfoSme_TasksStat_fullTableScan");
+      return new SelectInfoSme_TasksStat_fullTableScan(StorageHelper::getInfoSme_Tasks_Stat_Storage());
+    } else if ( !strncmp(sql, "CREATE INDEX", create_index_len) ) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object DDLStatement for CREATE INDEX");
 
-    return new DDLStatement();
-  } else if ( !strncmp(sql, "CREATE TABLE", create_table_len) ) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object DDLStatement for CREATE TABLE");
-    StorageHelper::createInfoSme_T_Storage(extractTableName(sql, "TABLE"));
+      return new DDLStatement();
+    } else if ( !strncmp(sql, "CREATE TABLE", create_table_len) ) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object DDLStatement for CREATE TABLE");
+      StorageHelper::createInfoSme_T_Storage(extractTableName(sql, "TABLE"));
 
-    return new DDLStatement();
-  } else if ( !strncmp(sql, "DROP TABLE", drop_table_len) ) {
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: call StorageHelper::deleteInfoSme_T_Storage, sql=[%s]", sql);
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: deleteInfoSme_T_Storage argument=[%s]", extractTableName(sql, "TABLE").c_str());
-    StorageHelper::deleteInfoSme_T_Storage(extractTableName(sql, "TABLE"));
-    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object DDLStatement for DROP TABLE");
-    return new DDLStatement();
-  } else {
-    if ( !strncmp(sql, "DELETE", delete_keyword_len) ) {
-      InfoSme_T_DBEntityStorage*
-        infoSme_T_Storage = StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "FROM"));
-      if ( strstr(sql,"WHERE STATE=:NEW AND ABONENT=:ABONENT") ) {
-        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_T_By_StateAndAbonent");
-        return new Delete_from_InfoSme_T_By_StateAndAbonent(infoSme_T_Storage);
-      } else if ( strstr(sql, "WHERE STATE=:NEW") ) {
-        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_T_By_State");
-        return new Delete_from_InfoSme_T_By_State(infoSme_T_Storage);
-      } else if ( strstr(sql, "WHERE ID=:ID") ) {
-        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_T_By_Id");
-        return new Delete_from_InfoSme_T_By_Id(infoSme_T_Storage);
+      return new DDLStatement();
+    } else if ( !strncmp(sql, "DROP TABLE", drop_table_len) ) {
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: call StorageHelper::deleteInfoSme_T_Storage, sql=[%s]", sql);
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: deleteInfoSme_T_Storage argument=[%s]", extractTableName(sql, "TABLE").c_str());
+      StorageHelper::deleteInfoSme_T_Storage(extractTableName(sql, "TABLE"));
+      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object DDLStatement for DROP TABLE");
+      return new DDLStatement();
+    } else {
+      if ( !strncmp(sql, "DELETE", delete_keyword_len) ) {
+        InfoSme_T_DBEntityStorage*
+          infoSme_T_Storage = StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "FROM"));
+        if ( strstr(sql,"WHERE STATE=:NEW AND ABONENT=:ABONENT") ) {
+          smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_T_By_StateAndAbonent");
+          return new Delete_from_InfoSme_T_By_StateAndAbonent(infoSme_T_Storage);
+        } else if ( strstr(sql, "WHERE STATE=:NEW") ) {
+          smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_T_By_State");
+          return new Delete_from_InfoSme_T_By_State(infoSme_T_Storage);
+        } else if ( strstr(sql, "WHERE ID=:ID") ) {
+          smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Delete_from_InfoSme_T_By_Id");
+          return new Delete_from_InfoSme_T_By_Id(infoSme_T_Storage);
+        }
+      } else if ( !strncmp(sql, "INSERT INTO", insert_into_len) ) {
+        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Insert_into_InfoSme_T");
+        return new Insert_into_InfoSme_T(StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "INTO")));
+      } else if ( !strncmp(sql, "UPDATE", update_keyword_len) ) {
+        InfoSme_T_DBEntityStorage*
+          infoSme_T_Storage = StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "UPDATE"));
+        if ( strstr(sql, "SET STATE=:NEW WHERE STATE=:WAIT") ) {
+          smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_NewState_By_OldState");
+          return new Update_InfoSme_T_Set_NewState_By_OldState(infoSme_T_Storage);
+        } else if ( strstr(sql, "STATE=:STATE WHERE ID=:ID") ) {
+          smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_State_By_Id");
+          return new Update_InfoSme_T_Set_State_By_Id(infoSme_T_Storage);
+        } else if ( strstr(sql, "STATE=:NEW, SEND_DATE=:SEND_DATE WHERE ID=:ID") ) {
+          smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_StateAndSendDate_By_Id");
+          return new Update_InfoSme_T_Set_StateAndSendDate_By_Id(infoSme_T_Storage);
+        } else if ( strstr(sql, "STATE=:ENROUTE WHERE ID=:ID AND STATE=:WAIT") ) {
+          smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_State_By_IdAndState");
+          return new Update_InfoSme_T_Set_State_By_IdAndState(infoSme_T_Storage);
+        }
+      } else if (!strncmp(sql, "SELECT ID, ABONENT, MESSAGE", sel_from_infosme_t_len)) {
+        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object SelectInfoSme_T_stateAndSendDate_criterion");
+        return new SelectInfoSme_T_stateAndSendDate_criterion(StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "FROM")));
+      } else if (traceIf("FULL_TABLE_SCAN FROM") && !strncmp(sql, "FULL_TABLE_SCAN FROM", infosme_t_fulltablescan_len)) {
+        return new SelectInfoSme_T_fullTableScan(StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "FROM")));
       }
-    } else if ( !strncmp(sql, "INSERT INTO", insert_into_len) ) {
-      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Insert_into_InfoSme_T");
-      return new Insert_into_InfoSme_T(StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "INTO")));
-    } else if ( !strncmp(sql, "UPDATE", update_keyword_len) ) {
-      InfoSme_T_DBEntityStorage*
-        infoSme_T_Storage = StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "UPDATE"));
-      if ( strstr(sql, "SET STATE=:NEW WHERE STATE=:WAIT") ) {
-        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_NewState_By_OldState");
-        return new Update_InfoSme_T_Set_NewState_By_OldState(infoSme_T_Storage);
-      } else if ( strstr(sql, "STATE=:STATE WHERE ID=:ID") ) {
-        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_State_By_Id");
-        return new Update_InfoSme_T_Set_State_By_Id(infoSme_T_Storage);
-      } else if ( strstr(sql, "STATE=:NEW, SEND_DATE=:SEND_DATE WHERE ID=:ID") ) {
-        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_StateAndSendDate_By_Id");
-        return new Update_InfoSme_T_Set_StateAndSendDate_By_Id(infoSme_T_Storage);
-      } else if ( strstr(sql, "STATE=:ENROUTE WHERE ID=:ID AND STATE=:WAIT") ) {
-        smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object Update_InfoSme_T_Set_State_By_IdAndState");
-        return new Update_InfoSme_T_Set_State_By_IdAndState(infoSme_T_Storage);
-      }
-    } else if (!strncmp(sql, "SELECT ID, ABONENT, MESSAGE", sel_from_infosme_t_len)) {
-      smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: return object SelectInfoSme_T_stateAndSendDate_criterion");
-      return new SelectInfoSme_T_stateAndSendDate_criterion(StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "FROM")));
-    } else if (traceIf("FULL_TABLE_SCAN FROM") && !strncmp(sql, "FULL_TABLE_SCAN FROM", infosme_t_fulltablescan_len)) {
-      return new SelectInfoSme_T_fullTableScan(StorageHelper::getInfoSme_T_Storage(extractTableName(sql, "FROM")));
     }
+  } catch (std::exception& ex) {
+    smsc_log_debug(_logger, "DBEntityStorageConnection::createStatement::: catch std::exception [%s]", ex.what());
+    throw SQLException(ex.what());
   }
+
   throw SQLException("DBEntityStorageConnection::createStatement::: unknown statement");
 }
 
@@ -238,6 +244,7 @@ InfoSme_T_DBEntityStorage* StorageHelper::getInfoSme_T_Storage(const std::string
     } else
       iter = insResult.first;
   }
+
   smsc_log_debug(logger, "StorageHelper::getInfoSme_T_Storage::: found storage for tableName=[%s]", tableName.c_str());
   return iter->second;
 }
