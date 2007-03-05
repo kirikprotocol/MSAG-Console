b901387edb8bbb104aab2565b2a35eefd6dd5167
firs <>
2007-03-05 11:40:46 +0000

    *** empty log message ***

--- src/scag/pers/client_test.cpp
+++ src/scag/pers/client_test.cpp
@@ -18,7 +18,7 @@ using namespace scag::pers::client;
 using namespace scag::pers;
 using namespace scag::config;
 
-#define ITER_CNT 40000
+#define ITER_CNT 10000
 
 extern "C" static void atExitHandler(void)
 {
