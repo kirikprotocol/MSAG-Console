11115b5a73191c9553948b183c064eaa2411d9c9
shulga <>
2007-03-07 10:09:52 +0000

    responseObjectBuffer is filled over call to Write method instead of Append method as it have been done

--- src/inman/uss/test_ussbalance.cpp
+++ src/inman/uss/test_ussbalance.cpp
@@ -121,123 +121,123 @@ static void init_logger()
 int main(int argc, char** argv)
 {
   try {
-  init_logger();
-  const char *  cfgFile = "test_ussbalance.cfg";
-  if (argc > 1) {
-    if ( !strcmp(argv[1], "-v") ) {
-      printVersion(); return 0;
-    } else if ( !strcmp(argv[1], "-h") ) {
-      printUsage(argv[0]); return 0;
-    } else
-      cfgFile = argv[1];
-  }
-
-  UssTestServiceConfig cfg;
-  try {
-    smsc::util::config::Manager::init(cfgFile);
-    smsc::util::config::Manager& manager = smsc::util::config::Manager::getInstance();
-    cfg.read(manager);
-  } catch (smsc::util::config::ConfigException& exc) {
-    fprintf(stderr, "Configuration invalid: %s. Exiting", exc.what());
-    return 1;
-  }
-
-  struct sockaddr_in peerAddr;
-  memset(&peerAddr, 0, sizeof(peerAddr));
-  peerAddr.sin_family = AF_INET;
-  peerAddr.sin_port = htons(cfg.peerPort);
-
-  int sockfd;
-  if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
-    perror("inet_pton failed");
-    return 1;
-  }
-
-  ssize_t st = inet_pton(AF_INET, cfg.peerHost.c_str(), &peerAddr.sin_addr);
-  if ( st < 0 ) {
-    perror("inet_pton failed");
-    return 1;
-  }
-
-  if ( connect(sockfd,(sockaddr*)&peerAddr,sizeof(peerAddr)) < 0 ) {
-    perror("connect failed");
-    return 1;
-  }
-
-  uint8_t buf[1000];
-  uint16_t cmdId = htons(1);
-  uint32_t reqId = htonl(10);
-  uint8_t dcs = 0xF4;
-
-  uint8_t ussData[] = "*100#";
-  uint8_t ussDataLen = strlen((char*)ussData);
-
-  //  uint8_t msAddr[] = "+79133821781";
-  uint8_t msAddrLen = cfg.msAddr.size();
-
-  //  uint8_t inAddr[] = "+79139860005";
-  uint8_t inAddrLen = cfg.inAddr.size();
-
-  uint8_t inSSN = 6;
-
-  size_t offset=0;
-  size_t dataSz = appendDataToBuf(buf, (uint8_t*)&cmdId, sizeof(cmdId), &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)&reqId, sizeof(reqId), &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)&dcs, sizeof(dcs), &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)&ussDataLen, sizeof(ussDataLen), &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)ussData, ussDataLen, &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)&msAddrLen, sizeof(msAddrLen), &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)cfg.msAddr.c_str(), msAddrLen, &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)&inSSN, sizeof(inSSN), &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)&inAddrLen, sizeof(inAddrLen), &offset);
-  dataSz += appendDataToBuf(buf, (uint8_t*)cfg.inAddr.c_str(), inAddrLen, &offset);
-
-  uint32_t netDataSz = htonl(dataSz);
-  if ( writen(sockfd, (uchar_t*)&netDataSz, sizeof(netDataSz)) != sizeof(netDataSz) ) {
-    printf("write: errno=%d\n", errno);
-    perror("write failed");
-    return 1;
-  }
-
-  if ( writen(sockfd, (uchar_t*)buf, dataSz) != dataSz ) {
-    printf("write: errno=%d\n", errno);
-    perror("write failed");
-    return 1;
-  }
-
-  if ( readn(sockfd, (uchar_t*)&netDataSz, sizeof(netDataSz)) != sizeof(netDataSz) ) {
-    printf("read: errno=%d\n", errno);
-    perror("read failed");
-    return 1;
-  }
-  dataSz = ntohl(netDataSz);
-
-  std::vector<uint8_t> responseBuf(dataSz);
-
-  if ((dataSz=readn(sockfd, &responseBuf[0], dataSz)) < 0) {
-    printf("read(responseBuf): errno=%d\n", errno);
-    perror("read failed");
-    return 1;
-  }
-
-  //write(1, &responseBuf[0], dataSz);
-  smsc::inman::interaction::SerializerUSS* serializer = smsc::inman::interaction::SerializerUSS::getInstance();
-
-  smsc::inman::interaction::ObjectBuffer responseObjectBuffer(1024);
-  responseObjectBuffer.Append(&responseBuf[0], responseBuf.size());
-
-  smsc::inman::interaction::USSPacketAC *ussPacket = static_cast<smsc::inman::interaction::USSPacketAC*>(serializer->deserialize(responseObjectBuffer));
-  smsc::inman::interaction::USSResultMessage *message = static_cast<smsc::inman::interaction::USSResultMessage*>(ussPacket->pCmd());
-  if ( message->getStatus() == smsc::inman::interaction::USS2CMD::STATUS_USS_REQUEST_OK ) {
-    std::string ussAsString;
-    if ( message->getUSSDataAsLatin1Text(ussAsString) )
-      printf("Got response: %s\n", ussAsString.c_str());
-    else {
-      smsc::inman::interaction::USSDATA_T resultUssData = message->getUSSData();
-      printf("Got response: %s\n", smsc::util::DumpHex(resultUssData.size(), &resultUssData[0]).c_str());
+    init_logger();
+    const char *  cfgFile = "test_ussbalance.cfg";
+    if (argc > 1) {
+      if ( !strcmp(argv[1], "-v") ) {
+        printVersion(); return 0;
+      } else if ( !strcmp(argv[1], "-h") ) {
+        printUsage(argv[0]); return 0;
+      } else
+        cfgFile = argv[1];
     }
-  } else 
-    fprintf(stderr, "Got error\n");
+
+    UssTestServiceConfig cfg;
+    try {
+      smsc::util::config::Manager::init(cfgFile);
+      smsc::util::config::Manager& manager = smsc::util::config::Manager::getInstance();
+      cfg.read(manager);
+    } catch (smsc::util::config::ConfigException& exc) {
+      fprintf(stderr, "Configuration invalid: %s. Exiting", exc.what());
+      return 1;
+    }
+
+    struct sockaddr_in peerAddr;
+    memset(&peerAddr, 0, sizeof(peerAddr));
+    peerAddr.sin_family = AF_INET;
+    peerAddr.sin_port = htons(cfg.peerPort);
+
+    int sockfd;
+    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+      perror("inet_pton failed");
+      return 1;
+    }
+
+    ssize_t st = inet_pton(AF_INET, cfg.peerHost.c_str(), &peerAddr.sin_addr);
+    if ( st < 0 ) {
+      perror("inet_pton failed");
+      return 1;
+    }
+
+    if ( connect(sockfd,(sockaddr*)&peerAddr,sizeof(peerAddr)) < 0 ) {
+      perror("connect failed");
+      return 1;
+    }
+
+    uint8_t buf[1000];
+    uint16_t cmdId = htons(1);
+    uint32_t reqId = htonl(10);
+    uint8_t dcs = 0xF4;
+
+    uint8_t ussData[] = "*100#";
+    uint8_t ussDataLen = strlen((char*)ussData);
+
+    //  uint8_t msAddr[] = "+79133821781";
+    uint8_t msAddrLen = cfg.msAddr.size();
+
+    //  uint8_t inAddr[] = "+79139860005";
+    uint8_t inAddrLen = cfg.inAddr.size();
+
+    uint8_t inSSN = 6;
+
+    size_t offset=0;
+    size_t dataSz = appendDataToBuf(buf, (uint8_t*)&cmdId, sizeof(cmdId), &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)&reqId, sizeof(reqId), &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)&dcs, sizeof(dcs), &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)&ussDataLen, sizeof(ussDataLen), &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)ussData, ussDataLen, &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)&msAddrLen, sizeof(msAddrLen), &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)cfg.msAddr.c_str(), msAddrLen, &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)&inSSN, sizeof(inSSN), &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)&inAddrLen, sizeof(inAddrLen), &offset);
+    dataSz += appendDataToBuf(buf, (uint8_t*)cfg.inAddr.c_str(), inAddrLen, &offset);
+
+    printf("Make balance request for ISDN#=%s\n",cfg.msAddr.c_str());
+    uint32_t netDataSz = htonl(dataSz);
+    if ( writen(sockfd, (uchar_t*)&netDataSz, sizeof(netDataSz)) != sizeof(netDataSz) ) {
+      fprintf(stderr,"write: errno=%d\n", errno);
+      perror("write failed");
+      return 1;
+    }
+
+    if ( writen(sockfd, (uchar_t*)buf, dataSz) != dataSz ) {
+      fprintf(stderr,"write: errno=%d\n", errno);
+      perror("write failed");
+      return 1;
+    }
+
+    if ( readn(sockfd, (uchar_t*)&netDataSz, sizeof(netDataSz)) != sizeof(netDataSz) ) {
+      fprintf(stderr,"read: errno=%d\n", errno);
+      perror("read failed");
+      return 1;
+    }
+    dataSz = ntohl(netDataSz);
+
+    std::vector<uint8_t> responseBuf(dataSz);
+
+    if ((dataSz=readn(sockfd, &responseBuf[0], dataSz)) < 0) {
+      fprintf(stderr,"read(responseBuf): errno=%d\n", errno);
+      perror("read failed");
+      return 1;
+    }
+
+    smsc::inman::interaction::SerializerUSS* serializer = smsc::inman::interaction::SerializerUSS::getInstance();
+
+    smsc::inman::interaction::ObjectBuffer responseObjectBuffer(1024);
+    responseObjectBuffer.Write(0, &responseBuf[0], responseBuf.size());
+
+    smsc::inman::interaction::USSPacketAC *ussPacket = static_cast<smsc::inman::interaction::USSPacketAC*>(serializer->deserialize(responseObjectBuffer));
+    smsc::inman::interaction::USSResultMessage *message = static_cast<smsc::inman::interaction::USSResultMessage*>(ussPacket->pCmd());
+    if ( message->getStatus() == smsc::inman::interaction::USS2CMD::STATUS_USS_REQUEST_OK ) {
+      std::string ussAsString;
+      if ( message->getUSSDataAsLatin1Text(ussAsString) )
+        printf("Got response: %s\n", ussAsString.c_str());
+      else {
+        smsc::inman::interaction::USSDATA_T resultUssData = message->getUSSData();
+        printf("Got response: %s\n", smsc::util::DumpHex(resultUssData.size(), &resultUssData[0]).c_str());
+      }
+    } else 
+      fprintf(stderr, "Got error\n");
   } catch (std::exception& ex) {
     fprintf(stderr, "Catch exception=[%s]\n",ex.what());
     return 1;
