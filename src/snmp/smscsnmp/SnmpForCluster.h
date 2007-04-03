5c8890967ab783442f7b622c6cf4e5d570781a9d
skv <>
2007-04-03 11:40:22 +0000

    ; fix

--- src/snmp/smscsnmp/SnmpForCluster.h
+++ src/snmp/smscsnmp/SnmpForCluster.h
@@ -9,13 +9,13 @@
   SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
   scds_get_resource_type_name(scds_handle),1,\
   "CLEARED SYSTEM %s Started (AlarmId=%s; severity=1)",\
-  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)) \
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)); \
   }else \
   { \
   SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
   scds_get_resource_type_name(scds_handle),1,\
   "ACTIVE SYSTEM %s Start failed (AlarmId=%s; severity=5)",\
-  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)) \
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)); \
   }
 
 #define SNMP_REPORT_SERVICE_STOP \
@@ -24,13 +24,13 @@
   SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
   scds_get_resource_type_name(scds_handle),5,\
   "ACTIVE SYSTEM %s Stopped (AlarmId=%s; severity=5)",\
-  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle))\
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)); \
   }else \
   {\
   SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
   scds_get_resource_type_name(scds_handle),5,\
   "ACTIVE SYSTEM %s Stop failed (AlarmId=%s; severity=5)",\
-  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle))\
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)); \
   }
 
 #define SNMP_REPORT_SERVICE_FAILURE \
