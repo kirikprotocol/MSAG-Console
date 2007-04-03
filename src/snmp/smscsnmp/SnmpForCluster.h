ec7578cdb27dd2d5af371019fd71144d3ab46c85
skv <>
2007-04-03 10:45:07 +0000

    fixes

--- src/snmp/smscsnmp/SnmpForCluster.h
+++ src/snmp/smscsnmp/SnmpForCluster.h
@@ -4,16 +4,34 @@
 #include <rgm/libdsdev.h>
 
 #define SNMP_REPORT_SERVICE_START \
+  if(rc==SCHA_ERR_NOERR) \
+  { \
   SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
   scds_get_resource_type_name(scds_handle),1,\
   "CLEARED SYSTEM %s Started (AlarmId=%s; severity=1)",\
-  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle))
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)) \
+  }else \
+  { \
+  SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
+  scds_get_resource_type_name(scds_handle),1,\
+  "ACTIVE SYSTEM %s Start failed (AlarmId=%s; severity=5)",\
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)) \
+  }
 
 #define SNMP_REPORT_SERVICE_STOP \
+  if(rc==SCHA_ERR_NOERR) \
+  { \
   SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
   scds_get_resource_type_name(scds_handle),5,\
   "ACTIVE SYSTEM %s Stopped (AlarmId=%s; severity=5)",\
-  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle))
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle))\
+  }else \
+  {\
+  SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
+  scds_get_resource_type_name(scds_handle),5,\
+  "ACTIVE SYSTEM %s Stop failed (AlarmId=%s; severity=5)",\
+  scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle))\
+  }
 
 #define SNMP_REPORT_SERVICE_FAILURE \
   if(rc==SCDS_PROBE_COMPLETE_FAILURE) \
@@ -22,7 +40,7 @@
     scds_get_resource_type_name(scds_handle),5,\
     "ACTIVE SYSTEM %s Failure (AlarmId=%s; severity=5)",\
     scds_get_resource_type_name(scds_handle),scds_get_resource_type_name(scds_handle)); \
-  }else \
+  }else if(rc!=0)\
   { \
   SnmpTrap(scds_get_resource_type_name(scds_handle),"SYSTEM",\
   scds_get_resource_type_name(scds_handle),5,\
--- src/snmp/smscsnmp/SnmpTrap.h
+++ src/snmp/smscsnmp/SnmpTrap.h
@@ -1,6 +1,10 @@
 #ifndef __SMSC_SNMP_SMSCSNMP_SNMPTRAP_H__
 #define __SMSC_SNMP_SMSCSNMP_SNMPTRAP_H__
 
-extern "C" void SnmpTrap(const char* prefix,const char* obj,const char* alarmId,int svrt,const char* fmt,...);
+extern
+#ifdef __cplusplus
+"C"
+#endif
+void SnmpTrap(const char* prefix,const char* obj,const char* alarmId,int svrt,const char* fmt,...);
 
 #endif
