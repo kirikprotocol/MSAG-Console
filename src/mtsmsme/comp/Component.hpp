92f5d4f23627704480f065116e828267bc62c164
vic <>
2007-04-11 09:20:20 +0000

    *** empty log message ***

---
+++ src/mtsmsme/comp/Component.hpp
@@ -0,0 +1,21 @@
+#ident "$Id$"
+#ifndef __SMSC_MTSMSME_COMP_COMP_HPP__
+#define __SMSC_MTSMSME_COMP_COMP_HPP__
+
+#include <vector>
+
+namespace smsc{ namespace mtsmsme{ namespace comp{
+
+//This is the base class for types of arguments, results and errors of
+//OPERATIONs, which are transferred through TCAP.
+
+using std::vector;
+class CompIF {
+public:
+    virtual void encode(vector<unsigned char>& buf) = 0;
+    virtual void decode(const vector<unsigned char>& buf) = 0;
+};
+
+/* namespace comp */ } /* namespace mtsmsme */ } /* namespace smsc */ }
+
+#endif /* __SMSC_MTSMSME_COMP_COMP_HPP__  */
