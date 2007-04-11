77c339cbca314d848497f988e07a18d5d4885ba3
vic <>
2007-04-11 08:52:26 +0000

    *** empty log message ***

--- src/mtsmsme/processor/spec/generate.sh
+++ src/mtsmsme/processor/spec/generate.sh
@@ -1,3 +1,6 @@
-#cd ../codec
+cd ../ncodec
+rm -rf *.[ch]
 #../spec/asn1c -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
-#../spec/asn1c -S /export/home/vic/asn1c/skeletons -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
+../spec/asn1c -S /home/vic/projects/asn1c/skeletons -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
+find . -type l  -exec rm {} \;
+rm Makefile.am.sample
