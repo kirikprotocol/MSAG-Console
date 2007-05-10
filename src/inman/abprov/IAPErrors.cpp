1afb69466ac18dbbfbf342e1987e724429eac5a6
gvroman <>
2007-05-10 07:29:08 +0000

    abonent provider errors for URCRegistry

---
+++ src/inman/abprov/IAPErrors.cpp
@@ -0,0 +1,20 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "$Id$";
+#endif /* MOD_IDENT_OFF */
+/* ************************************************************************* *
+ * Abonent Provider query status definition and description.
+ * ************************************************************************* */
+#ifndef URCREGISTRY_GLOBAL_INIT
+#include "inman/abprov/IAPErrors.hpp"
+
+namespace smsc {
+namespace inman {
+namespace iaprvd { //(I)NMan (A)bonent (P)roviders
+
+_RCS_IAPQStatusINIT();
+
+} //iaprvd
+} //inman
+} //smsc
+#endif /* URCREGISTRY_GLOBAL_INIT */
+
---
+++ src/inman/abprov/IAPErrors.hpp
@@ -0,0 +1,50 @@
+#ident "$Id$"
+/* ************************************************************************* *
+ * Abonent Provider query status definition and description.
+ * ************************************************************************* */
+#ifndef __SMSC_INMAN_IAPRVD_ERRORS__
+#define __SMSC_INMAN_IAPRVD_ERRORS__
+
+#include "util/URCdb/URCRegistry.hpp"
+using smsc::util::URCSpacePTR;
+
+namespace smsc {
+namespace inman {
+namespace iaprvd { //(I)NMan (A)bonent (P)roviders
+
+struct IAPQStatus {
+    enum Code {
+        iqOk = 0,       //query completed
+        iqCancelled,    //query has been cancelled
+        iqTimeout,      //query is timed out
+        iqError         //query execution error
+    };
+};
+extern const char * rc2Txt_IAPQStatus(uint32_t err_code);
+extern URCSpacePTR  _RCS_IAPQStatus;
+
+#define FDECL_rc2Txt_IAPQStatus()    \
+const char * rc2Txt_IAPQStatus(uint32_t err_code) { \
+    switch (err_code) { \
+    case IAPQStatus::iqOk: return "query completed"; \
+    case IAPQStatus::iqCancelled: return "query has been cancelled"; \
+    case IAPQStatus::iqTimeout: return "query is timed out"; \
+    case IAPQStatus::iqError: return "query execution error"; \
+    default:; } \
+    return "unknown IAP query status"; \
+}
+
+#define ODECL_RCS_IAPQStatus() FDECL_rc2Txt_IAPQStatus() \
+URCSpacePTR  _RCS_IAPQStatus("iapQueryRC", IAPQStatus::iqOk, \
+                                IAPQStatus::iqError, rc2Txt_IAPQStatus)
+
+/* ------------------------------------------------------------------------- *
+ * Macro for all return code spaces global initialization
+ * ------------------------------------------------------------------------- */
+#define _RCS_IAPQStatusINIT()  ODECL_RCS_IAPQStatus()
+
+} //iaprvd
+} //inman
+} //smsc
+#endif /* __SMSC_INMAN_IAPRVD_ERRORS__ */
+
