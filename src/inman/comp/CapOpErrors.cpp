a562a2578d37c1d2ea14deec6c620c8d126129bd
gvroman <>
2007-05-10 07:32:29 +0000

    CAP operation ids and errors

---
+++ src/inman/comp/CapOpErrors.cpp
@@ -0,0 +1,20 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "$Id$";
+#endif /* MOD_IDENT_OFF */
+/* ************************************************************************* *
+ * CAP operations error codes according to inman/codec_inc/cap_err_codes.asn
+ * ************************************************************************* */
+#ifndef URCREGISTRY_GLOBAL_INIT
+#include "inman/comp/CapOpErrors.hpp"
+
+namespace smsc {
+namespace inman {
+namespace comp {
+
+_RCS_CapOpErrorsINIT();
+
+} //comp
+} //inman
+} //smsc
+#endif /* URCREGISTRY_GLOBAL_INIT */
+
