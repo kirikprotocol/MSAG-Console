9eee4f6a13b885fee04b4e1e135d1f2675844d03
gvroman <>
2007-05-10 07:34:12 +0000

    MO SM transfer reject causes

---
+++ src/inman/comp/cap_sms/MOSM_RPCauses.cpp
@@ -0,0 +1,19 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "$Id$";
+#endif /* MOD_IDENT_OFF */
+/* ************************************************************************* *
+ * 3GPP TS 24.011 Annex E-2: RP-cause definition mobile originating SM-transfer.
+ * ************************************************************************* */
+#ifndef URCREGISTRY_GLOBAL_INIT
+#include "inman/comp/cap_sms/MOSM_RPCauses.hpp"
+
+namespace smsc {
+namespace inman {
+namespace comp {
+
+_RCS_MOSM_RPCauseINIT();
+
+}//namespace comp
+}//namespace inman
+}//namespace smsc
+#endif /* URCREGISTRY_GLOBAL_INIT */
