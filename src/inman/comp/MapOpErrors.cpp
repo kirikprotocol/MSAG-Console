844c25681d9cac7d744ccd4741ca26ea6c0821bc
gvroman <>
2007-05-10 07:32:45 +0000

    MAP operation ids and errors

---
+++ src/inman/comp/MapOpErrors.cpp
@@ -0,0 +1,20 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "$Id$";
+#endif /* MOD_IDENT_OFF */
+/* ************************************************************************* *
+ * MAP operations error codes according to inman/codec_inc/map_errors.asn
+ * ************************************************************************* */
+#ifndef URCREGISTRY_GLOBAL_INIT
+#include "inman/comp/MapOpErrors.hpp"
+
+namespace smsc {
+namespace inman {
+namespace comp {
+
+_RCS_MapOpErrorsINIT();
+
+} //comp
+} //inman
+} //smsc
+#endif /* URCREGISTRY_GLOBAL_INIT */
+
