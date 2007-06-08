f7f25e6f644bc564f39d3fd367ed8fe3f422c58a
shulga <>
2007-06-08 07:24:43 +0000

    framework for working with berkeley db

---
+++ src/db/bdb_framework/StorableDbRecord_Iface.cpp
@@ -0,0 +1,12 @@
+#include "StorableDbRecord_Iface.hpp"
+#include <string>
+#include <stdexcept>
+
+namespace smsc {
+namespace db {
+namespace bdb_framework {
+
+const size_t
+StorableDbRecord_Iface::EMPTY_STRING_SIZE;
+
+}}}
