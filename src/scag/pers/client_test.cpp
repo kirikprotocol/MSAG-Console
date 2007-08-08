dd1d33cd2ace4d284a4546679f15c8dd26588694
firs <>
2007-08-08 07:40:21 +0000

    *** empty log message ***

--- src/scag/pers/client_test.cpp
+++ src/scag/pers/client_test.cpp
@@ -18,7 +18,7 @@ using namespace scag::pers::client;
 using namespace scag::pers;
 using namespace scag::config;
 
-#define ITER_CNT 1
+#define ITER_CNT 1000
 
 extern "C" static void atExitHandler(void)
 {
@@ -37,9 +37,11 @@ int main(int argc, char* argv[])
     try{
         ConfigManager::Init();
         
-        PersClient::Init("phoenix", 1222, 60, 5);
+        PersClient::Init(argc > 1 ? argv[1] : "localhost", argc > 2 ? atoi(argv[2]) : 22890, 60, 5);
+        smsc_log_debug(logger, "Host: %s:%d. Requests: %d", argc > 1 ? argv[1] : "localhost", argc > 2 ? atoi(argv[2]) : 22890, 45 * ITER_CNT);
         PersClient& pc = PersClient::Instance();
         Property prop;
+        SerialBuffer sb;
 
         time_t t = time(NULL);
 
@@ -121,24 +123,125 @@ int main(int argc, char* argv[])
             pc.GetProperty(PT_SERVICE, i + 1, tv, prop);
             smsc_log_debug(logger,  ">>SERVICE: get int(after inc mod) %s", prop.toString().c_str());
 
-            pc.DelProperty(PT_SERVICE, i + 1, tv);
-            smsc_log_debug(logger,  ">>ABONENT: del int(int)");
+//batch
+            pc.PrepareBatch(sb);
+
+            prop.setInt(tv, 234567, FIXED, -1, 20);
+            pc.SetPropertyPrepare(PT_ABONENT, s, prop, sb);
+
+            prop.setBool(tvb, false, R_ACCESS, -1, 25);
+            pc.SetPropertyPrepare(PT_ABONENT, s, prop, sb);
+
+            prop.setString(tvs, "test_string", W_ACCESS, -1, 25);
+            pc.SetPropertyPrepare(PT_ABONENT, s, prop, sb);
+
+            prop.setDate(tvd, 111111, INFINIT, -1, 25);
+            pc.SetPropertyPrepare(PT_ABONENT, s, prop, sb);
+
+            pc.GetPropertyPrepare(PT_ABONENT, s, tv, sb);
+            pc.GetPropertyPrepare(PT_ABONENT, s, tvb, sb);
+            pc.GetPropertyPrepare(PT_ABONENT, s, tvs, sb);
+            pc.GetPropertyPrepare(PT_ABONENT, s, tvd, sb);
+
+            prop.setInt(tv, -123, FIXED, -1, 20);
+            pc.IncPropertyPrepare(PT_ABONENT, s, prop, sb);
+            pc.GetPropertyPrepare(PT_ABONENT, s, tv, sb);
+			
+            pc.DelPropertyPrepare(PT_ABONENT, s, tv, sb);
+
+            prop.setInt(tv, 234567, FIXED, -1, 20);
+            pc.SetPropertyPrepare(PT_SERVICE, i + 1, prop, sb);
+
+            prop.setBool(tvb, false, R_ACCESS, -1, 25);
+            pc.SetPropertyPrepare(PT_PROVIDER, i + 1, prop, sb);
+
+            prop.setString(tvs, "test_string", W_ACCESS, -1, 25);
+            pc.SetPropertyPrepare(PT_OPERATOR, i + 1, prop, sb);
+
+            prop.setDate(tvd, 111111, INFINIT, -1, 25);
+            pc.SetPropertyPrepare(PT_OPERATOR, i + 1, prop, sb);
+
+            pc.GetPropertyPrepare(PT_SERVICE, i + 1, tv, sb);
+
+            pc.GetPropertyPrepare(PT_PROVIDER, i + 1, tvb, sb);
+
+            pc.GetPropertyPrepare(PT_OPERATOR, i + 1, tvs, sb);
+
+            pc.GetPropertyPrepare(PT_OPERATOR, i + 1, tvd, sb);
+
+            prop.setInt(tv, -123, FIXED, -1, 20);
+            pc.IncModPropertyPrepare(PT_SERVICE, i + 1, prop, 10, sb);
+            pc.GetPropertyPrepare(PT_SERVICE, i + 1, tv, sb);
+
+            pc.DelPropertyPrepare(PT_SERVICE, i + 1, tv, sb);
+
+            pc.FinishPrepareBatch(22, sb);
+            pc.RunBatch(sb);
+
+            pc.SetPropertyResult(sb);
+            pc.SetPropertyResult(sb);
+            pc.SetPropertyResult(sb);
+            pc.SetPropertyResult(sb);
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>ABONENT %s: get int %s", s, prop.toString().c_str());
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>ABONENT %s: get bool %s", s, prop.toString().c_str());
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>ABONENT: get string %s", prop.toString().c_str());
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>ABONENT: get string1 %s", prop.toString().c_str());
+
+            pc.IncPropertyResult(sb);
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>ABONENT: get int(after inc) %s", prop.toString().c_str());
+			
+            pc.DelPropertyResult(sb);
+            smsc_log_debug(logger,  "BATCH >>ABONENT: del int");
+
+            pc.SetPropertyResult(sb);
+            pc.SetPropertyResult(sb);
+            pc.SetPropertyResult(sb);
+            pc.SetPropertyResult(sb);
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>SERVICE: get int %s", prop.toString().c_str());
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>OPERATOR: get bool %s", prop.toString().c_str());
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>OPERATOR: get string %s", prop.toString().c_str());
+
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>OPERATOR: get date %s", prop.toString().c_str());
+
+            int kkk = pc.IncModPropertyResult(sb);
+            pc.GetPropertyResult(prop, sb);
+            smsc_log_debug(logger,  "BATCH >>SERVICE: get int(after inc mod)(%d) %s", kkk, prop.toString().c_str());
+
+            pc.DelPropertyResult(sb);
+            smsc_log_debug(logger,  "BATCH >>ABONENT: del int(int)");
+            
             }
             catch(PersClientException &e)
             {
                 smsc_log_error(logger, "persclientexception: %s", e.what());
             }
 
-            if(pc.DelProperty(PT_SERVICE, i + 1, tv))
+/*            if(pc.DelProperty(PT_SERVICE, i + 1, tv))
 	            smsc_log_debug(logger,  ">>ABONENT: del int(int)");
 			else
                 smsc_log_debug(logger, "property already deleted...");
-
+*/
             smsc_log_debug(logger,  "end");
         }
         t = time(NULL) - t;
         if(t)
-            smsc_log_error(logger,  "timings: %d sec, %d req/s", t, 11*ITER_CNT/t); //24
+            smsc_log_error(logger,  "timings: %d sec, %d req/s", t, 45 * ITER_CNT/t); //24
     }
     catch (PersClientException& exc) 
     {
