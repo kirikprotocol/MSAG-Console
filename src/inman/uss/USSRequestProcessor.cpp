46e6d047d32aeadb889638dfaa499a9d70967f9c
shulga <>
2007-10-31 09:12:43 +0000

    send uss data for Latin dc as ascii string

--- src/inman/uss/USSRequestProcessor.cpp
+++ src/inman/uss/USSRequestProcessor.cpp
@@ -1,5 +1,8 @@
 #include "USSRequestProcessor.hpp"
 #include <inman/inap/dispatcher.hpp>
+#include <inman/common/cvtutil.hpp>
+#include <vector>
+#include <sys/types.h>
 
 namespace smsc {
 namespace inman {
@@ -8,7 +11,7 @@ namespace uss {
 USSRequestProcessor::USSRequestProcessor(smsc::inman::interaction::Connect* conn,
                                          const UssService_CFG& cfg)
   : _conn(conn), _logger(Logger::getInstance("smsc.uss.BalanceService")),
-    _mapSess(NULL), _mapDialog(NULL), _cfg(cfg), _dialogId(0) {}
+    _mapSess(NULL), _mapDialog(NULL), _cfg(cfg), _dialogId(0), _resultAsLatin1(false), _dcs(0) {}
 
 void
 USSRequestProcessor::setDialogId(uint32_t dialogId)
@@ -61,8 +64,12 @@ USSRequestProcessor::handleRequest(const smsc::inman::interaction::USSRequestMes
   try {
     _mapDialog = new smsc::inman::inap::uss::MapUSSDlg(_mapSess, this);
 
-    _mapDialog->requestSS(requestObject->getUSSData(),  requestObject->getDCS(),
-                          &(requestObject->getMSISDNadr()));
+    if ( requestObject->getFlg() == smsc::inman::interaction::USSMessageAC::PREPARED_USS_REQ )
+      _mapDialog->requestSS(requestObject->getUSSData(),  requestObject->getDCS(),
+                            &(requestObject->getMSISDNadr()));
+    else if ( requestObject->getFlg() == smsc::inman::interaction::USSMessageAC::LATIN1_USS_TEXT )
+      _mapDialog->requestSS(requestObject->getLatin1Text(),
+                            &(requestObject->getMSISDNadr()));
 
     _msISDNAddr = requestObject->getMSISDNadr();
   } catch (std::exception & ex) {
@@ -74,16 +81,17 @@ USSRequestProcessor::handleRequest(const smsc::inman::interaction::USSRequestMes
 
 void USSRequestProcessor::onMapResult(smsc::inman::comp::uss::MAPUSS2CompAC* arg)
 {
-  _resultUssData = arg->getUSSData();
-  _dcs = arg->getDCS();
-
-  std::string ussAsString;
-  if ( arg->getUSSDataAsLatin1Text(ussAsString) )
+  if ( arg->getUSSDataAsLatin1Text(_resultUssAsString) ) {
+    _resultAsLatin1 = true;
     smsc_log_debug(_logger, "USSRequestProcessor::onMapResult::: got USSData=[%s]",
-                   ussAsString.c_str());
-  else
+                   _resultUssAsString.c_str());
+  } else {
+    _resultUssData = arg->getUSSData();
+    _dcs = arg->getDCS();
+
     smsc_log_debug(_logger, "USSRequestProcessor::onMapResult::: got USSData=[%s]",
                    smsc::util::DumpHex(_resultUssData.size(), &_resultUssData[0]).c_str());
+  }
 }
 
 //dialog finalization/error handling:
@@ -94,9 +102,23 @@ void USSRequestProcessor::onEndMapDlg(RCHash ercode/* =0*/)
   if (!ercode) {
     // success and send result 
     // process std::vector result
+    if ( _resultAsLatin1 )
+      resultPacket.Cmd().setUSSData(_resultUssAsString.c_str(), _resultUssAsString.size());
+    else {
+      smsc::cbs::CBS_DCS cbs;
+      if (smsc::cbs::parseCBS_DCS(_dcs, cbs) == smsc::cbs::CBS_DCS::dcUCS2 && 
+          !cbs.compressed &&
+          !cbs.UDHind ) {
+        smsc_log_debug(_logger, "USSRequestProcessor::onEndMapDlg::: got UCS2 encoded message response");
+        if ( cbs.lngPrefix == smsc::cbs::CBS_DCS::lng4UCS2 )
+          resultPacket.Cmd().setUCS2USSData(std::vector<uint8_t>(&_resultUssData[0]+2,
+                                                                &_resultUssData[0]+_resultUssData.size()));
+        else
+          resultPacket.Cmd().setUCS2USSData(_resultUssData);
+      } else
+        resultPacket.Cmd().setRAWUSSData(_dcs, _resultUssData);
+    }
     resultPacket.Cmd().setStatus(smsc::inman::interaction::USS2CMD::STATUS_USS_REQUEST_OK);
-    // resultPacket.Cmd().setUSSData(_resultUssData);
-    resultPacket.Cmd().setRAWUSSData(_dcs, _resultUssData);
     resultPacket.Cmd().setMSISDNadr(_msISDNAddr);
   } else {
       smsc_log_error(_logger, "USSRequestProcessor::onEndMapDlg: error %u: %s", _logger,
