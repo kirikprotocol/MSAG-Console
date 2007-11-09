ca815c8e42f7462f6bac2a5cddf74196115e5171
mixim <>
2007-11-09 06:57:13 +0000

    updateSmeInfo -> updateSme

--- webapps/scag/WEB-INF/src/ru/sibinco/scag/backend/protocol/commands/endpoints/UpdateSvcInfo.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/backend/protocol/commands/endpoints/UpdateSvcInfo.java
@@ -17,7 +17,7 @@ import ru.sibinco.scag.backend.endpoints.svc.Svc;
 public class UpdateSvcInfo extends SvcCommand {
 
     public UpdateSvcInfo(final Svc svc) throws SibincoException {
-        super("updateSmeInfo", "command_gw.dtd");
+        super("updateSme", "command_gw.dtd");
         fillParams(svc);
     }
 }
