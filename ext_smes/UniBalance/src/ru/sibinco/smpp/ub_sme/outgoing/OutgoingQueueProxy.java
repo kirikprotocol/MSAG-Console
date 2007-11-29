d6fa7ddf8faf594f84ce650fe91ca21349a9a9f9
cyber_mind <>
2007-11-29 08:58:23 +0000

    no message

--- ext_smes/UniBalance/src/ru/sibinco/smpp/ub_sme/outgoing/OutgoingQueueProxy.java
+++
@@ -1,7 +0,0 @@
-package ru.sibinco.smpp.ub_sme.outgoing;
-
-public interface OutgoingQueueProxy {
-    public void addOutgoingObject(Response response);
-
-    public boolean registerMessageStatusListener(MessageStatusListener listener);
-}
