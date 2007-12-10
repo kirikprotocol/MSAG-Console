06a1181ee8c53fa4af8724b27d822a7f472cb77c
cyber_mind <>
2007-12-10 07:18:24 +0000

    remove profile lang

--- ext_smes/UniBalance/src/ru/sibinco/smpp/ub_sme/mg/MGState.java
+++ ext_smes/UniBalance/src/ru/sibinco/smpp/ub_sme/mg/MGState.java
@@ -21,11 +21,9 @@ public class MGState implements StateInterface, IAdvertisingResponseHandler {
     public static final byte BE_RESP_WAIT = 4;
     public static final byte BE_RESP_ERR = 5;
     public static final byte BE_RESP_OK = 6;
-    /*
     public static final byte PROF_ERR = 7;
     public static final byte PROF_OK = 8;
     public static final byte PROF_WAIT = 9;
-    */
     protected int mgState;
     protected String mgBalance;
     protected String message;
