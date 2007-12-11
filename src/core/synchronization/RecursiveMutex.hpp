d4593c52347470f3f0bb2c4eae23d5c2cd8537a4
gvroman <>
2007-12-11 10:06:42 +0000

    RecursiveMutex becomes successor of Mutex

--- src/core/synchronization/RecursiveMutex.hpp
+++ src/core/synchronization/RecursiveMutex.hpp
@@ -1,19 +1,19 @@
+#pragma ident "$Id$"
 #ifndef __CORE_SYNCHRONIZATION_RECURSIVEMUTEX_HPP__
 #define __CORE_SYNCHRONIZATION_RECURSIVEMUTEX_HPP__
 
-#include <pthread.h>
-
-#include "core/synchronization/MutexGuard.hpp"
-
+#include <string>
 #include <stdexcept>
+
+#include "core/synchronization/Mutex.hpp"
+
 namespace smsc{
 namespace core{
 namespace synchronization{
 
-
 class Event;
 
-class RecursiveMutex{
+class RecursiveMutex : public Mutex {
 public:
   RecursiveMutex()
   {
@@ -28,25 +28,13 @@ public:
     pthread_mutex_destroy(&mutex);
     pthread_mutexattr_destroy(&attr);
   }
-  void Lock()
-  {
-    pthread_mutex_lock(&mutex);
-  }
-  void Unlock()
-  {
-    pthread_mutex_unlock(&mutex);
-  }
-  bool TryLock()
-  {
-    return pthread_mutex_trylock(&mutex)==0;
-  }
+
 protected:
-  pthread_mutex_t mutex;
   pthread_mutexattr_t attr;
 
   RecursiveMutex(const RecursiveMutex&);
   void operator=(const RecursiveMutex&);
-};//Mutex
+};
 
 typedef MutexGuardTmpl<RecursiveMutex> RecursiveMutexGuard;
 
@@ -54,4 +42,5 @@ typedef MutexGuardTmpl<RecursiveMutex> RecursiveMutexGuard;
 }//core
 }//smsc
 
-#endif
+#endif /* __CORE_SYNCHRONIZATION_RECURSIVEMUTEX_HPP__ */
+
