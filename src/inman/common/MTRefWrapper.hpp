abab062a30490ad054d0cab69f2be5d6b266f71a
gvroman <>
2007-12-12 09:50:01 +0000

    *** empty log message ***

---
+++ src/inman/common/MTRefWrapper.hpp
@@ -0,0 +1,76 @@
+#pragma ident "$Id$"
+/* ************************************************************************** *
+ * 
+ * ************************************************************************** */
+#ifndef __MT_REFWRAPPER_HPP__
+#define __MT_REFWRAPPER_HPP__
+
+#include <pthread.h>
+
+namespace smsc {
+namespace core {
+namespace synchronization {
+
+// ********************************************************************
+// 
+// NOTE: Requires an external sync object.
+// ********************************************************************
+template <class _TArg>
+class MTRefWrapper_T {
+protected:
+    _TArg   * first;
+    _TArg   * second;
+    pthread_t thrId;
+
+public:
+    MTRefWrapper_T(_TArg * use_ref = NULL)
+        : thrId((pthread_t)(-1))
+    {
+        first = use_ref;  second = 0;
+    }
+    ~MTRefWrapper_T()
+    { }
+
+    void Reset(_TArg * use_ref)
+    {
+        thrId = (pthread_t)(-1);
+        first = use_ref;  second = 0;
+    }
+
+    inline _TArg * get(void) const { return first; }
+    inline _TArg * operator->() const { return first; }
+
+    _TArg * Lock(void)
+    {
+        if (first)
+            thrId = pthread_self();
+        return (second = first);
+    }
+    _TArg * UnLock(void)
+    {
+        first = second;
+        second = 0;
+        thrId = (pthread_t)(-1);
+        return first;
+    }
+    bool Unref(void)
+    {
+        if (!second) { //not locked
+            first = 0;
+            return true;
+        }
+        if (thrId == pthread_self()) {  //locked by same thread,
+            second = 0;                 //next Unlock() will perform Unref()
+            return true;
+        }
+        return false;
+    }
+};
+
+
+}//namespace synchronization
+}//namespace core
+}//namespace smsc
+
+#endif /* __MT_REFWRAPPER_HPP__ */
+
