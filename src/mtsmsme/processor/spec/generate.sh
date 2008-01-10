a4085fb2b28cb08ab6709f9d037df274b71df54b
vic <>
2008-01-10 10:25:31 +0000

    asn reorganization

--- src/mtsmsme/processor/spec/generate.sh
+++
@@ -1,6 +0,0 @@
-cd ../ncodec
-rm -rf *.[ch]
-#../spec/asn1c -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
-../spec/asn1c -S /home/vic/projects/asn1c/skeletons -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
-find . -type l  -exec rm {} \;
-rm Makefile.am.sample
--- src/mtsmsme/processor/spec/locupdgen.sh
+++
@@ -1,9 +0,0 @@
-cd ../ulcodec
-rm -rf *.[ch]
-#../spec/asn1c -fnative-types ../spec/ros.asn ../spec/dialogue.asn ../spec/mtsms.asn
-#../spec/asn1c
-#/home/gvroman/bin/asn1c-0.9.19/asn1c/asn1c 
-../spec/asn1c -pdu=all -S /home/vic/projects/asn1c/skeletons -fnative-types ../spec/ros.asn ../spec/map_ms_types.asn ../spec/map_cmn_types.asn ../spec/map_ext_types.asn ../spec/map_bs_code.asn ../spec/map_ts_code.asn ../spec/map_err_types.asn ../spec/ul.asn
-find . -type l  -exec rm {} \;
-rm Makefile.am.sample
-rm pdu_collection.c
