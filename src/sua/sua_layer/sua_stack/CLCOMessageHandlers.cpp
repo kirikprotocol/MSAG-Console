b31b7f0429614d9ff0922956db19aa8dc5eec5c6
shulga <>
2008-04-18 09:43:45 +0000

    added overloaded 'handle' method for CLDRMessage type argument; added processing of hop count parameter got in incoming CLDT message

--- src/sua/sua_layer/sua_stack/CLCOMessageHandlers.cpp
+++ src/sua/sua_layer/sua_stack/CLCOMessageHandlers.cpp
@@ -1,7 +1,9 @@
+#include <sua/communication/types.hpp>
 #include <sua/communication/LinkId.hpp>
 #include <sua/sua_layer/messages_router/MessagesRouter.hpp>
 #include <sua/sua_layer/messages_router/RoutingKey.hpp>
 #include <sua/communication/libsua_messages/EncapsulatedSuaMessage.hpp>
+#include <sua/communication/sua_messages/CLDRMessage.hpp>
 
 #include "CLCOMessageHandlers.hpp"
 
@@ -33,9 +35,30 @@ CLCOMessageHandlers::handle(const sua_messages::CLDTMessage& message, const comm
     smsc_log_info(_logger, "CLCOMessageHandlers::handle::: send encapsulated CLDTMessage [%s] to link with linkid=[%s]", messageForSending.toString().c_str(), outLinkId.getValue().c_str());
     _cMgr.send(outLinkId, messageForSending);
   } else {
-    smsc_log_info(_logger, "CLCOMessageHandlers::handle::: forward original CLDT message=[%s] to next SGP over link with linkid=[%s]", message.toString().c_str(), outLinkId.getValue().c_str());
-    _cMgr.send(outLinkId, message);
+    smsc_log_info(_logger, "CLCOMessageHandlers::handle::: forward original CLDT message=[%s] to next SGP over link with linkid=[%s]", message.toString().c_str(), linkId.getValue().c_str());
+    const sua_messages::TLV_SS7HopCount& ss7HopCount = message.getSS7HopCount();
+    if ( ss7HopCount.isSetValue() ) { 
+      uint8_t hopCountValue = ss7HopCount.getHopCountValue();
+      if ( --hopCountValue == 0 ) {
+        smsc_log_info(_logger, "CLCOMessageHandlers::handle::: hop count has been decremented to 0, throw away message =[%s]", message.toString().c_str());
+        sua_messages::CLDRMessage cldrMessage(message, communication::HOP_COUNTER_VIOLATION);
+        _cMgr.send(linkId, cldrMessage);
+        return;
+      } else {
+        sua_messages::CLDTMessage backForwardMessage(message);
+        backForwardMessage.setSS7HopCount(sua_messages::TLV_SS7HopCount(hopCountValue));
+        _cMgr.send(linkId, backForwardMessage);
+      }
+    } else {
+      smsc_log_info(_logger, "CLCOMessageHandlers::handle::: ss7hopCount parameter is missed in forwarding message, throw away original CLDT message");
+    }
   }
+}
+
+void
+CLCOMessageHandlers::handle(const sua_messages::CLDRMessage& message, const communication::LinkId& linkId)
+{
+  smsc_log_info(_logger, "CLCOMessageHandlers::handle::: handle CLDRMessage=[%s]", message.toString().c_str());
 }
 
 }
