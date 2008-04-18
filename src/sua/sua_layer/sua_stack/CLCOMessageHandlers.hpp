f0717d96199a110df04b8fd191fe7da414be6928
shulga <>
2008-04-18 09:42:34 +0000

    added overloaded 'handle' method for CLDRMessage type argument

--- src/sua/sua_layer/sua_stack/CLCOMessageHandlers.hpp
+++ src/sua/sua_layer/sua_stack/CLCOMessageHandlers.hpp
@@ -5,6 +5,7 @@
 # include <sua/utilx/Singleton.hpp>
 # include <sua/communication/LinkId.hpp>
 # include <sua/communication/sua_messages/CLDTMessage.hpp>
+# include <sua/communication/sua_messages/CLDRMessage.hpp>
 # include <sua/sua_layer/io_dispatcher/ConnectMgr.hpp>
 
 namespace sua_stack {
@@ -13,6 +14,7 @@ class CLCOMessageHandlers : public utilx::Singleton<CLCOMessageHandlers> {
 public:
   CLCOMessageHandlers();
   void handle(const sua_messages::CLDTMessage& message, const communication::LinkId& linkId);
+  void handle(const sua_messages::CLDRMessage& message, const communication::LinkId& linkId);
 private:
   smsc::logger::Logger* _logger;
   io_dispatcher::ConnectMgr& _cMgr;
