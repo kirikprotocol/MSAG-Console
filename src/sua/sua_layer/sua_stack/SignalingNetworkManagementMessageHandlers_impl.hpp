0f2d7f275eb66392b57d2a7e616db340352287e9
shulga <>
2008-05-21 05:12:48 +0000

    put file under version control

---
+++ src/sua/sua_layer/sua_stack/SignalingNetworkManagementMessageHandlers_impl.hpp
@@ -0,0 +1,22 @@
+template <class MSG>
+libsua_messages::N_STATE_IND_Message
+SignalingNetworkManagementMessageHandlers::make_N_STATE_Indication_message(const MSG& message, uint8_t userStatus)
+{
+  libsua_messages::N_STATE_IND_Message resultMesage;
+
+  sua_messages::ITU_PC affectedPointCode;
+  if ( message.getAffectedPointCode().getNextPC(&affectedPointCode) ) {
+    uint16_t pointCode;
+    memcpy(reinterpret_cast<uint8_t*>(&pointCode), affectedPointCode.getValue()+2, sizeof(pointCode));
+    pointCode = ntohs(pointCode);
+    resultMesage.setPointCode(pointCode);
+
+    resultMesage.setSSN(uint8_t(message.getSSN().getValue()));
+    resultMesage.setUserStatus(userStatus);
+    if ( message.getSMI().isSetValue() )
+      resultMesage.setSubsystemMultiplicityInd(uint8_t(message.getSMI().getValue()));
+
+    return resultMesage;
+  } else
+    throw smsc::util::Exception("SignalingNetworkManagementMessageHandlers::make_N_STATE_Indication_message::: affected point code is absent in original %s message", message.getMsgCodeTextDescription());
+}
