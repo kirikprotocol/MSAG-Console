a9989368a41cef8548d20ab51fba6bb5a58d09ca
shulga <>
2008-05-21 05:31:51 +0000

    removed registration of EncapsulatedSuaMessage and MErrorMessage from SuaLayerMessagesFactory; added registration of N_UNITDATA_REQ_Message in SuaLayerMessagesFactory

--- src/sua/sua_layer/sua_user_communication/initializer.cpp
+++ src/sua/sua_layer/sua_user_communication/initializer.cpp
@@ -1,24 +1,6 @@
 #include <sua/sua_layer/io_dispatcher/SuaLayerMessagesFactory.hpp>
 #include "MessagesHandlingDispatcher.hpp"
 
-namespace io_dispatcher {
-
-template <>
-class SuaLayerMessagesInstancer<sua_user_communication::EncapsulatedSuaMessage_HandlingDispatcher> : public utilx::MessageCreator<communication::Message> {
-public:
-  SuaLayerMessagesInstancer(uint32_t msgCode)
-    : _msgCode(msgCode) {}
-
-  virtual communication::Message* instance() {
-    utilx::alloc_mem_desc_t* mem_header = utilx::PreallocatedMemoryManager::getInstance().getMemory<utilx::PreallocatedMemoryManager::MEM_FOR_ENCAPSULATED_MESSAGE>();
-    return new (mem_header->allocated_memory) sua_user_communication::EncapsulatedSuaMessage_HandlingDispatcher(_msgCode);
-  }
-private:
-  uint32_t _msgCode;
-};
-
-}
-
 namespace sua_user_communication {
 
 bool registerMessageCreators()
@@ -29,19 +11,10 @@ bool registerMessageCreators()
      new io_dispatcher::SuaLayerMessagesInstancer<BindMessage_HandlingDispatcher>()
      );
 
-  for(uint32_t msgCode=libsua_messages::EncapsulatedSuaMessage::_MIN_MSG_CODE;
-      msgCode <= libsua_messages::EncapsulatedSuaMessage::_MAX_MSG_CODE;
-      ++msgCode)
-    io_dispatcher::SuaLayerMessagesFactory::getInstance().registerMessageCreator
-      (
-       libsua_messages::LibsuaMessage::getMessageIndex(msgCode),
-       new io_dispatcher::SuaLayerMessagesInstancer<EncapsulatedSuaMessage_HandlingDispatcher>(msgCode)
-       );
-
   io_dispatcher::SuaLayerMessagesFactory::getInstance().registerMessageCreator
     (
-     libsua_messages::LibsuaMessage::getMessageIndex(libsua_messages::MErrorMessage().getMsgCode()),
-     new io_dispatcher::SuaLayerMessagesInstancer<MErrorMessage_HandlingDispatcher>()
+     libsua_messages::LibsuaMessage::getMessageIndex(libsua_messages::N_UNITDATA_REQ_Message().getMsgCode()),
+     new io_dispatcher::SuaLayerMessagesInstancer<N_UNITDATA_REQ_Message_HandlingDispatcher>()
      );
 
   io_dispatcher::SuaLayerMessagesFactory::getInstance().registerMessageCreator
