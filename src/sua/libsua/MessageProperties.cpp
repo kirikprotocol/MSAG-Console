75fff91b5da022f7d63a964c82025488106304fe
shulga <>
2008-05-21 08:10:25 +0000

    constructor of MessageProperties class was modified

--- src/sua/libsua/MessageProperties.cpp
+++ src/sua/libsua/MessageProperties.cpp
@@ -3,8 +3,8 @@
 namespace libsua {
 
 MessageProperties::MessageProperties()
-  :  protocolClass(0), returnOnError(false), sequenceControlValue(0),
-     importance(0), messagePriority(0), correlationId(0), hopCount(0), fieldsMask(0)
+  :  returnOnError(false), sequenceControlValue(0),
+     importance(0), hopCount(0), fieldsMask(0)
 {}
 
 }
