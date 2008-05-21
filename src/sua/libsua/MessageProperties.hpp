77b69735464fa5024c94f79d3b170218689b7887
shulga <>
2008-05-21 08:09:45 +0000

    messagePriority, correlationId, segmentation and protocolClass data members was removed; SET_PROT_CLASS, SET_RETURN_ON_ERROR, SET_SEGMENTATION, SET_MESSAGE_PRIORITY constant was removed

--- src/sua/libsua/MessageProperties.hpp
+++ src/sua/libsua/MessageProperties.hpp
@@ -2,7 +2,6 @@
 # define __SUA_LIBSUA_MESSAGEPROPERTIES_HPP__ 1
 
 # include <sys/types.h>
-# include <sua/libsua/Segmentation.hpp>
 
 namespace libsua {
 
@@ -10,25 +9,15 @@ struct MessageProperties
 {
   MessageProperties();
 
-  uint8_t protocolClass;
-
   bool returnOnError;
 
   uint32_t sequenceControlValue;
 
   uint8_t importance;
 
-  uint8_t messagePriority;
-
-  uint32_t correlationId;
-
-  Segmentation segmentation;
-
   uint8_t hopCount;
 
-  typedef enum {SET_PROT_CLASS = 0x01, SET_RETURN_ON_ERROR = 0x02, SET_SEQUENCE_CONTROL = 0x04,
-                SET_SEGMENTATION = 0x08, SET_IMPORTANCE = 0x10, SET_MESSAGE_PRIORITY = 0x20,
-                SET_CORRELATION_ID = 0x40, SET_HOP_COUNT = 0x80} field_mask_t;
+  typedef enum {SET_SEQUENCE_CONTROL = 0x01, SET_IMPORTANCE = 0x02, SET_HOP_COUNT = 0x04} field_mask_t;
 
   uint32_t fieldsMask;
 };
