6668fbb855de7648e40aafe0535cecf80041eed9
shulga <>
2008-05-21 05:24:32 +0000

    adde Mutex _lock data member; added getLinkSetIds method; getLinkSet method declaration was modified

--- src/sua/sua_layer/sua_user_communication/LinkSetInfoRegistry.hpp
+++ src/sua/sua_layer/sua_user_communication/LinkSetInfoRegistry.hpp
@@ -7,6 +7,7 @@
 # include <logger/Logger.h>
 # include <sua/utilx/Singleton.hpp>
 # include <sua/communication/LinkId.hpp>
+# include <core/synchronization/Mutex.hpp>
 
 namespace sua_user_communication {
 
@@ -15,15 +16,18 @@ public:
   LinkSetInfoRegistry();
 
   void addAssociation(const communication::LinkId& linkSetId, const std::string& appId);
-  
-  const communication::LinkId& getLinkSet(const std::string& appId);
-  
+
+  // return true if 'appId --> linkSet' association was found, else return false.
+  bool getLinkSet(const std::string& appId, communication::LinkId* linkSetId) const;
+
   void removeAssociation(const communication::LinkId& linkSetId, const std::string& appId);
 
+  std::set<communication::LinkId> getLinkSetIds() const;
 private:
   smsc::logger::Logger* _logger;
   typedef std::map<communication::LinkId, std::set<std::string>* /*appIds*/> registry_t;
   registry_t _registry;
+  mutable smsc::core::synchronization::Mutex _lock;
 };
 
 }
