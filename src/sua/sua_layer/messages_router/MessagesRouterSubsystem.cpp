0a81f57d1f5d1d28c3b14650990593eb4f8f2b41
shulga <>
2008-06-03 05:28:35 +0000

    added methods fillUpRouteTable, makeAddressFamilyPrefix, processApplicationLinkSets, initializeLinkSets; removed methods initializeOutcomingLinkSets, initializeIncomingLinkSets, removeAsteriskFromPrefix. Added support of GT translation tables.

--- src/sua/sua_layer/messages_router/MessagesRouterSubsystem.cpp
+++ src/sua/sua_layer/messages_router/MessagesRouterSubsystem.cpp
@@ -5,11 +5,10 @@
 #include <sua/sua_layer/io_dispatcher/ConnectMgr.hpp>
 #include <sua/sua_layer/sua_user_communication/LinkSetInfoRegistry.hpp>
 #include <sua/sua_layer/sua_stack/LinkSetsRegistry.hpp>
-#include "MessagesRouter.hpp"
 #include "MessagesRouterSubsystem.hpp"
-
-messages_router::MessagesRouterSubsystem*
-utilx::Singleton<messages_router::MessagesRouterSubsystem*>::_instance;
+#include "GTTranslator.hpp"
+#include "GTTranslationTablesRegistry.hpp"
+#include "GTTranslationTable.hpp"
 
 namespace messages_router {
 
@@ -24,14 +23,6 @@ MessagesRouterSubsystem::start()
 void
 MessagesRouterSubsystem::stop()
 {
-}
-
-void
-MessagesRouterSubsystem::removeAsteriskFromPrefix(std::string* wildCardStr)
-{
-  std::string::size_type pos = wildCardStr->find('*');
-  if ( pos != std::string::npos )
-    wildCardStr->erase(pos);
 }
 
 io_dispatcher::LinkSet::linkset_mode_t
@@ -51,101 +42,148 @@ MessagesRouterSubsystem::convertStringToTrafficModeValue(const std::string& traf
 }
 
 bool
-MessagesRouterSubsystem::extractAddrPrefixAndSSN(const std::string& gt, std::string* destinationGTMask, uint8_t* destinationSSN, const char* where)
+MessagesRouterSubsystem::processApplicationLinkSets(const runtime_cfg::CompositeParameter* routingEntryCompositeParameter, const communication::LinkId& linkSetId, io_dispatcher::LinkSet::linkset_mode_t linkSetMode)
 {
-  std::string::size_type pos = gt.find(':');
+  bool result = false;
 
-  if ( pos != std::string::npos ) {
-    *destinationGTMask = gt.substr(0, pos);
-    removeAsteriskFromPrefix(destinationGTMask);
-    std::string destinationSSN_paramVal = gt.substr(pos+1);
-    *destinationSSN = static_cast<uint8_t>(strtol(destinationSSN_paramVal.c_str(), (char **)NULL, 10));
-    if ( *destinationSSN == 0 && errno == EINVAL )
-      throw smsc::util::Exception(std::string(where).append(" wrong SSN value format [%s]").c_str(), destinationSSN_paramVal.c_str());
-    return true;
-  } else {
-    *destinationGTMask = gt;
-    removeAsteriskFromPrefix(destinationGTMask);
-    return false;
-  }
-}
+  runtime_cfg::CompositeParameter::Iterator<runtime_cfg::Parameter> appIdIterator = routingEntryCompositeParameter->getIterator<runtime_cfg::Parameter>("application");
 
-void
-MessagesRouterSubsystem::initializeIncomingLinkSets(runtime_cfg::CompositeParameter& incomingRoutingKeysParameter)
-{
-  runtime_cfg::CompositeParameter::Iterator<runtime_cfg::CompositeParameter> incomingGTIterator = incomingRoutingKeysParameter.getIterator<runtime_cfg::CompositeParameter>("GT");
-
-  while(incomingGTIterator.hasElement()) {
-    const runtime_cfg::CompositeParameter* gtParams = incomingGTIterator.getCurrentElement();
-    const std::string& gt = gtParams->getValue();
-    communication::LinkId linkSetId(std::string("IN:") + gt);
-    runtime_cfg::CompositeParameter::Iterator<runtime_cfg::Parameter> appIdIterator = gtParams->getIterator<runtime_cfg::Parameter>("application_id");
-    while (appIdIterator.hasElement()) {
-      const std::string& appId=appIdIterator.getCurrentElement()->getValue();
-      smsc_log_info(_logger, "MessagesRouterSubsystem::initializeIncomingLinkSets::: add association for application=[%s] to linkset=[%s]", appId.c_str(), linkSetId.getValue().c_str());
-      sua_user_communication::LinkSetInfoRegistry::getInstance().addAssociation(linkSetId, appId);
-      appIdIterator.next();
-    }
-
-    io_dispatcher::LinkSet::linkset_mode_t linkSetMode = convertStringToTrafficModeValue(gtParams->getParameter<runtime_cfg::Parameter>("traffic_mode")->getValue(), "MessagesRouterSubsystem::initializeIncomingLinkSets: ");
-
+  if ( appIdIterator.hasElement() ) {
+    result = true;
     io_dispatcher::ConnectMgr::getInstance().registerLinkSet(linkSetId, linkSetMode);
-
-    std::string destinationGTPrefix;
-    uint8_t destinationSSN;
-    if ( extractAddrPrefixAndSSN(gt, &destinationGTPrefix, &destinationSSN, "MessagesRouterSubsystem::initializeIncomingLinkSets:::") )
-      MessagesRouter::getInstance().addIncomingRouteEntry(destinationGTPrefix, destinationSSN, linkSetId);
-    else
-      MessagesRouter::getInstance().addIncomingRouteEntry(destinationGTPrefix, linkSetId);
-
-    incomingGTIterator.next();
   }
 
+  while (appIdIterator.hasElement()) {
+    const std::string& appId = appIdIterator.getCurrentElement()->getValue();
+    smsc_log_info(_logger, "MessagesRouterSubsystem::processApplicationLinkSets::: add association for application=[%s] to linkset=[%s]", appId.c_str(), linkSetId.getValue().c_str());
+    sua_user_communication::LinkSetInfoRegistry::getInstance().addAssociation(linkSetId, appId);
+    appIdIterator.next();
+  }
+
+  return result;
 }
 
-void
-MessagesRouterSubsystem::initializeOutcomingLinkSets(runtime_cfg::CompositeParameter& outcomingRoutingKeysParameter)
+bool
+MessagesRouterSubsystem::processSgpLinkSets(const runtime_cfg::CompositeParameter* routingEntryCompositeParameter, const communication::LinkId& linkSetId, io_dispatcher::LinkSet::linkset_mode_t linkSetMode)
 {
-  runtime_cfg::CompositeParameter::Iterator<runtime_cfg::CompositeParameter> outcomingGTIterator = outcomingRoutingKeysParameter.getIterator<runtime_cfg::CompositeParameter>("GT");
+  bool result = false;
 
-  while(outcomingGTIterator.hasElement()) {
-    const runtime_cfg::CompositeParameter* gtParams = outcomingGTIterator.getCurrentElement();
+  runtime_cfg::CompositeParameter::Iterator<runtime_cfg::Parameter> sgpLinksIterator = routingEntryCompositeParameter->getIterator<runtime_cfg::Parameter>("sgp_link");
 
-    io_dispatcher::LinkSet::linkset_mode_t linkSetMode = convertStringToTrafficModeValue(gtParams->getParameter<runtime_cfg::Parameter>("traffic_mode")->getValue(), "MessagesRouterSubsystem::initializeOutcomingLinkSets: ");
-
-    const std::string& gt = gtParams->getValue();
-    communication::LinkId linkSetId(std::string("OUT:") + gt);
-
+  if ( sgpLinksIterator.hasElement() ) {
+    result = true;
     io_dispatcher::ConnectMgr::getInstance().registerLinkSet(linkSetId, linkSetMode);
     sua_stack::LinkSetsRegistry::getInstance().registerLinkSet(linkSetId);
-
-    runtime_cfg::CompositeParameter::Iterator<runtime_cfg::Parameter> sgpLinkIdIterator = gtParams->getIterator<runtime_cfg::Parameter>("link_id");
-    while (sgpLinkIdIterator.hasElement()) {
-      communication::LinkId sgpLinkId(sgpLinkIdIterator.getCurrentElement()->getValue());
-      smsc_log_info(_logger, "MessagesRouterSubsystem::initializeOutcomingLinkSets::: add link=[%s] to linkset=[%s]", sgpLinkId.getValue().c_str(), linkSetId.getValue().c_str());
-      sua_stack::LinkSetsRegistry::getInstance().registerLinkInLinkSet(linkSetId, sgpLinkId);
-      sgpLinkIdIterator.next();
-    }
-
-    std::string destinationGTPrefix;
-    uint8_t destinationSSN;
-    if ( extractAddrPrefixAndSSN(gt, &destinationGTPrefix, &destinationSSN, "MessagesRouterSubsystem::initializeOutcomingLinkSets:::") )
-      MessagesRouter::getInstance().addOutcomingRouteEntry(destinationGTPrefix, destinationSSN, linkSetId);
-    else
-      MessagesRouter::getInstance().addOutcomingRouteEntry(destinationGTPrefix, linkSetId);
-
-    outcomingGTIterator.next();
   }
+
+  while (sgpLinksIterator.hasElement()) {
+    communication::LinkId sgpLinkId(sgpLinksIterator.getCurrentElement()->getValue());
+    smsc_log_info(_logger, "MessagesRouterSubsystem::processSgpLinkSets::: add link=[%s] to linkset=[%s]", sgpLinkId.getValue().c_str(), linkSetId.getValue().c_str());
+    sua_stack::LinkSetsRegistry::getInstance().registerLinkInLinkSet(linkSetId, sgpLinkId);
+    sgpLinksIterator.next();
+  }
+
+  return result;
+}
+
+std::string
+MessagesRouterSubsystem::makeAddressFamilyPrefix(unsigned int gti,
+                                                 const std::string& gtMaskValue) const
+{
+  std::string gtAddressFamilyPrefix;
+
+  if ( gtMaskValue[0] == '+' && gti == 4 )
+    gtAddressFamilyPrefix = ".4.0.1.4";
+  else if ( gtMaskValue[0] >= 0x30 && gtMaskValue[0] <= 0x39 && gti == 1 )
+    gtAddressFamilyPrefix = ".4.0.1.0";
+  else {
+    std::string::size_type idx = gtMaskValue.rfind('.');
+    if ( idx != std::string::npos )
+      gtAddressFamilyPrefix = gtMaskValue.substr(0, idx);
+    else
+      throw smsc::util::Exception("MessagesRouterSubsystem::makeAddressFamilyPrefix::: can't determine address family prefix for gtMaskValue=[%s]", gtMaskValue.c_str());
+  }
+
+  return gtAddressFamilyPrefix;
+}
+
+void
+MessagesRouterSubsystem::fillUpRouteTable(const runtime_cfg::CompositeParameter* routingEntryCompositeParameter,
+                                          const communication::LinkId& linkSetId)
+{
+  runtime_cfg::CompositeParameter::Iterator<runtime_cfg::CompositeParameter>& routeInfoIterator = routingEntryCompositeParameter->getIterator<runtime_cfg::CompositeParameter>("route");
+  if ( !routeInfoIterator.hasElement() ) { // route to CLOUD - process default route
+    GTTranslationTable::addDefaultTranslationEntry(linkSetId);
+  } else
+    while(routeInfoIterator.hasElement()) {
+      const runtime_cfg::CompositeParameter* routeDescriptionCompositeParameter = routeInfoIterator.getCurrentElement();
+      runtime_cfg::Parameter* gtiConfigParameter = routeDescriptionCompositeParameter->getParameter<runtime_cfg::Parameter>("gti");
+      unsigned int gti = 4;
+      if ( gtiConfigParameter )
+        gti = atoi(gtiConfigParameter->getValue().c_str());
+
+      std::string gtMaskValue = routeDescriptionCompositeParameter->getParameter<runtime_cfg::Parameter>("gt")->getValue();
+
+      const std::string& gtAddressFamilyPrefix = makeAddressFamilyPrefix(gti, gtMaskValue);
+
+      smsc_log_debug(_logger, "MessagesRouterSubsystem::fillUpRouteTable::: process next route entry, gtMask=[%s]", gtMaskValue.c_str());
+      uint8_t destinationSSN = 0;
+      const runtime_cfg::Parameter* ssnConfigParam = routeDescriptionCompositeParameter->getParameter<runtime_cfg::Parameter>("ssn");
+      if ( ssnConfigParam ) {
+        const std::string& ssnValue = ssnConfigParam->getValue();
+
+        destinationSSN = static_cast<uint8_t>(strtol(ssnValue.c_str(), (char **)NULL, 10));
+        if ( destinationSSN == 0 && errno == EINVAL )
+          throw smsc::util::Exception("MessagesRouterSubsystem::fillUpRouteTable::: wrong SSN value format [%s]", ssnValue.c_str());
+      }
+
+      GTTranslationTable* translationTable = GTTranslationTablesRegistry::getInstance().getGTTranslationTable(gtAddressFamilyPrefix);
+      if ( !translationTable )
+        throw smsc::util::Exception("MessagesRouterSubsystem::fillUpRouteTable::: translation table not found for address family=[%s]", gtAddressFamilyPrefix.c_str());
+
+      translationTable->addTranslationEntry(gtMaskValue, destinationSSN, linkSetId);
+
+      routeInfoIterator.next();
+    }
+}
+
+void
+MessagesRouterSubsystem::initializeLinkSets(runtime_cfg::CompositeParameter& routingKeysParameter)
+{
+  runtime_cfg::CompositeParameter::Iterator<runtime_cfg::CompositeParameter> routingEntryIterator = routingKeysParameter.getIterator<runtime_cfg::CompositeParameter>("routingEntry");
+
+  while(routingEntryIterator.hasElement()) {
+    const runtime_cfg::CompositeParameter* routingEntryCompositeParameter = routingEntryIterator.getCurrentElement();
+    io_dispatcher::LinkSet::linkset_mode_t linkSetMode = convertStringToTrafficModeValue(routingEntryCompositeParameter->getParameter<runtime_cfg::Parameter>("traffic-mode")->getValue(), "MessagesRouterSubsystem::initializeIncomingLinkSets: ");
+    
+    communication::LinkId linkSetId(routingEntryCompositeParameter->getValue());
+
+    smsc_log_debug(_logger, "MessagesRouterSubsystem::initializeLinkSets::: linkSetId=[%s]", linkSetId.getValue().c_str());
+
+    fillUpRouteTable(routingEntryCompositeParameter,linkSetId);
+
+    if ( !processApplicationLinkSets(routingEntryCompositeParameter, linkSetId, linkSetMode) )
+      processSgpLinkSets(routingEntryCompositeParameter, linkSetId, linkSetMode);
+
+    routingEntryIterator.next();
+  }
+
 }
 
 void
 MessagesRouterSubsystem::initialize(runtime_cfg::RuntimeConfig& rconfig)
 {
-  MessagesRouter::init();
+  smsc_log_info(_logger, "try initialize MessagesRouterSubsystem");
+
+  GTTranslator::init();
+  GTTranslationTablesRegistry::init();
   sua_user_communication::LinkSetInfoRegistry::init();
   sua_stack::LinkSetsRegistry::init();
-  initializeIncomingLinkSets(rconfig.find<runtime_cfg::CompositeParameter>("config.incoming-routing-keys"));
-  initializeOutcomingLinkSets(rconfig.find<runtime_cfg::CompositeParameter>("config.outcoming-routing-keys"));
+
+  GTTranslationTablesRegistry::getInstance().registerGTTranslationTable(".4.0.1.4", new GTTranslationTable_Prefix_4_0_1_4());
+  GTTranslationTablesRegistry::getInstance().registerGTTranslationTable(".4.0.1.0", new GTTranslationTable_Prefix_4_0_1_0());
+
+  initializeLinkSets(rconfig.find<runtime_cfg::CompositeParameter>("config.routing-keys"));
 }
 
 const std::string&
