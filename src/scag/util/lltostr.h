fd444f3c68786d216803882506cf4b0e4dfb0acb
bukind <>
2008-06-16 03:52:28 +0000

    lltostr.h, strlcpy.h were added for systems that lacks corresponding functions

---
+++ src/scag/util/lltostr.h
@@ -0,0 +1,23 @@
+#ifndef __SCAG_UTIL_LLTOSTR_H
+# define __SCAG_UTIL_LLTOSTR_H 1
+
+# ifdef __GNUC__
+#include <sstream>
+namespace {
+
+  char* lltostr( long long int v, char* endptr ) 
+  {
+    std::ostringstream s;
+    s << v;
+    const std::string ss = s.str();
+    endptr -= ss.size();
+    memcpy( endptr, ss.c_str(), ss.size() );
+    return endptr;
+  }
+
+}
+#else
+#include <stdlib.h>
+#endif
+
+#endif /* __SCAG_UTIL_LLTOSTR_H */
