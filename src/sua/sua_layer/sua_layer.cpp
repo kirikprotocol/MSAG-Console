394844a53bbedee259245ba6cd003a35c7e92648
shulga <>
2008-06-20 06:26:37 +0000

    added _initialize_singleton_instances(() standalone function. in main(): added initializing of LMSubsystem

--- src/sua/sua_layer/sua_layer.cpp
+++ src/sua/sua_layer/sua_layer.cpp
@@ -1,23 +1,40 @@
-#include <sua/sua_layer/runtime_cfg/Parameter.hpp>
-#include <sua/sua_layer/runtime_cfg/CompositeParameter.hpp>
-#include <sua/sua_layer/runtime_cfg/RuntimeConfig.hpp>
-
-#include <sua/sua_layer/io_dispatcher/IODispatcherSubsystem.hpp>
-#include <sua/sua_layer/sua_stack/SuaStackSubsystem.hpp>
-#include <sua/sua_layer/sua_user_communication/SuaUserCommunicationSubsystem.hpp>
-#include <sua/sua_layer/messages_router/MessagesRouterSubsystem.hpp>
-#include <sua/sua_layer/io_dispatcher/ConnectMgr.hpp>
-
-#include <sua/utilx/SubsystemsManager.hpp>
 #include <logger/Logger.h>
 #include <exception>
+#include <iostream>
+#include <sua/utilx/SubsystemsManager.hpp>
 #include <util/config/Manager.h>
 
-#include <iostream>
+#include "runtime_cfg/Parameter.hpp"
+#include "runtime_cfg/CompositeParameter.hpp"
+#include "runtime_cfg/RuntimeConfig.hpp"
+
+#include "io_dispatcher/ConnectMgr.hpp"
+#include "io_dispatcher/IODispatcherSubsystem.hpp"
+#include "sua_stack/SuaStackSubsystem.hpp"
+#include "sua_user_communication/SuaUserCommunicationSubsystem.hpp"
+#include "messages_router/MessagesRouterSubsystem.hpp"
+#include "lm_subsystem/LMSubsystem.hpp"
+#include "lm_subsystem/InputCommandProcessor.hpp"
+
+static bool
+_initialize_singleton_instances()
+{
+  smsc::logger::Logger::Init();
+  utilx::SubsystemsManager::init();
+  io_dispatcher::IODispatcherSubsystem::init();
+  messages_router::MessagesRouterSubsystem::init();
+  sua_stack::SuaStackSubsystem::init();
+  lm_subsystem::LMSubsystem::init();
+  sua_user_communication::SuaUserCommunicationSubsystem::init();
+  runtime_cfg::RuntimeConfig::init();
+
+  return true;
+}
+
+static bool all_singleton_was_initialized = _initialize_singleton_instances();;
 
 int main(int argc, char** argv)
 {
-  smsc::logger::Logger::Init();
   smsc::logger::Logger *logger = smsc::logger::Logger::getInstance("init");
 
   const char* cfgFile;
@@ -32,33 +49,31 @@ int main(int argc, char** argv)
 
     smsc::util::config::ConfigView suaConfigView(manager, "SuaLayer");
 
-    runtime_cfg::RuntimeConfig::init();
     runtime_cfg::RuntimeConfig& rconfig = runtime_cfg::RuntimeConfig::getInstance();
     rconfig.initialize(&suaConfigView);
 
-    utilx::SubsystemsManager::init();
-
     smsc_log_info(logger, "main::: try to initialize all subsystems");
-    io_dispatcher::IODispatcherSubsystem::init();
+
     io_dispatcher::IODispatcherSubsystem::getInstance()->initialize(rconfig);
     smsc_log_info(logger, "main::: IODispatcherSubsystem has been initialized");
 
-    messages_router::MessagesRouterSubsystem::init();
     messages_router::MessagesRouterSubsystem::getInstance()->initialize(rconfig);
     smsc_log_info(logger, "main::: MessagesRouterSubsystem has been initialized");
 
-    sua_stack::SuaStackSubsystem::init();
     sua_stack::SuaStackSubsystem::getInstance()->initialize(rconfig);
     smsc_log_info(logger, "main::: SuaStackSubsystem has been initialized");
 
-    sua_user_communication::SuaUserCommunicationSubsystem::init();
     sua_user_communication::SuaUserCommunicationSubsystem::getInstance()->initialize(rconfig);
     smsc_log_info(logger, "main::: SuaUserCommunicationSubsystem has been initialized");
+
+    lm_subsystem::LMSubsystem::getInstance()->initialize(rconfig);
+    smsc_log_info(logger, "main::: LMSubsystem has been initialized");
 
     utilx::SubsystemsManager::getInstance()->initialize();
     utilx::SubsystemsManager::getInstance()->registerSubsystem(io_dispatcher::IODispatcherSubsystem::getInstance());
     utilx::SubsystemsManager::getInstance()->registerSubsystem(sua_user_communication::SuaUserCommunicationSubsystem::getInstance(), io_dispatcher::IODispatcherSubsystem::getInstance());
     utilx::SubsystemsManager::getInstance()->registerSubsystem(sua_stack::SuaStackSubsystem::getInstance(), io_dispatcher::IODispatcherSubsystem::getInstance());
+    utilx::SubsystemsManager::getInstance()->registerSubsystem(lm_subsystem::LMSubsystem::getInstance());
 
     smsc_log_info(logger, "main::: try start all subsystems");
     utilx::SubsystemsManager::getInstance()->startup();
