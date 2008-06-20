ac02d8e5b8c5291e3c710e7a37f8d2aac5518767
shulga <>
2008-06-20 08:22:00 +0000

    added overload getInterator and getParameter methods with without const modifier in theirs signatures

--- src/sua/sua_layer/runtime_cfg/CompositeParameter_impl.hpp
+++ src/sua/sua_layer/runtime_cfg/CompositeParameter_impl.hpp
@@ -1,14 +1,14 @@
 template<> inline
-CompositeParameter*
+const CompositeParameter*
 CompositeParameter::getParameter<CompositeParameter>(const std::string& parameterName) const
 {
   composite_parameters_t::const_iterator iter = _compositeParameters.find(parameterName);
-  if ( iter == _parameters.end() ) return NULL;
+  if ( iter == _compositeParameters.end() ) return NULL;
   return iter->second;
 }
 
 template<> inline
-Parameter*
+const Parameter*
 CompositeParameter::getParameter<Parameter>(const std::string& parameterName) const
 {
   parameters_t::const_iterator iter = _parameters.find(parameterName);
@@ -17,17 +17,35 @@ CompositeParameter::getParameter<Parameter>(const std::string& parameterName) co
 }
 
 template<> inline
-CompositeParameter::Iterator<Parameter>
-CompositeParameter::getIterator<Parameter>(const std::string& parameterName) const
+CompositeParameter*
+CompositeParameter::getParameter<CompositeParameter>(const std::string& parameterName)
 {
-  std::pair<parameters_t::const_iterator,  parameters_t::const_iterator> interval = _parameters.equal_range(parameterName);
+  composite_parameters_t::const_iterator iter = _compositeParameters.find(parameterName);
+  if ( iter == _compositeParameters.end() ) return NULL;
+  return iter->second;
+}
+
+template<> inline
+Parameter*
+CompositeParameter::getParameter<Parameter>(const std::string& parameterName)
+{
+  parameters_t::const_iterator iter = _parameters.find(parameterName);
+  if ( iter == _parameters.end() ) return NULL;
+  return iter->second;
+}
+
+template<> inline
+CompositeParameter::Iterator<Parameter>
+CompositeParameter::getIterator<Parameter>(const std::string& parameterName)
+{
+  std::pair<parameters_t::iterator,  parameters_t::iterator> interval = _parameters.equal_range(parameterName);
   return Iterator<Parameter>(interval.first, interval.second);
 }
 
 template<> inline
 CompositeParameter::Iterator<CompositeParameter>
-CompositeParameter::getIterator<CompositeParameter>(const std::string& parameterName) const
+CompositeParameter::getIterator<CompositeParameter>(const std::string& parameterName)
 {
-  std::pair<composite_parameters_t::const_iterator,  composite_parameters_t::const_iterator> interval = _compositeParameters.equal_range(parameterName);
+  std::pair<composite_parameters_t::iterator,  composite_parameters_t::iterator> interval = _compositeParameters.equal_range(parameterName);
   return Iterator<CompositeParameter>(interval.first, interval.second);
 }
