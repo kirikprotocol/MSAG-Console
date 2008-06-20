e9f32450137369a1bd0371c4cdd842b9a88f9bc6
shulga <>
2008-06-20 08:51:04 +0000

    replaced inheritance from utilx::Subsystem to inheritance from sua_layer::ApplicationSubsystem; added changeParameterEventHandler and addParameterEventHandler methods

--- src/sua/sua_layer/messages_router/MessagesRouterSubsystem.hpp
+++ src/sua/sua_layer/messages_router/MessagesRouterSubsystem.hpp
@@ -2,35 +2,52 @@
 # define __SUA_SUALAYER_MESSAGESROUTER_MESSAGESROUTERSUBSYSTEM_HPP__ 1
 
 # include <string>
-# include <sua/utilx/Subsystem.hpp>
+
 # include <sua/utilx/Singleton.hpp>
-# include <util/config/ConfigView.h>
-# include <netinet/in.h>
+# include <sua/sua_layer/ApplicationSubsystem.hpp>
 # include <sua/sua_layer/io_dispatcher/LinkSet.hpp>
+# include <sua/sua_layer/runtime_cfg/RuntimeConfig.hpp>
+# include <sua/sua_layer/runtime_cfg/ParameterObserver.hpp>
 
 namespace messages_router {
 
-class MessagesRouterSubsystem : public utilx::Subsystem,
+class MessagesRouterSubsystem : public sua_layer::ApplicationSubsystem,
+                                public runtime_cfg::ParameterObserver,
                                 public utilx::Singleton<MessagesRouterSubsystem*> {
 public:
   MessagesRouterSubsystem();
-  virtual void start();
-  virtual void stop();
+
   virtual void initialize(runtime_cfg::RuntimeConfig& rconfig);
-  virtual void waitForCompletion();
-  virtual const std::string& getName() const;
+
+  using ParameterObserver::addParameterEventHandler;
+  using ParameterObserver::changeParameterEventHandler;
+  using ParameterObserver::removeParameterEventHandler;
+
+  virtual void changeParameterEventHandler(const runtime_cfg::CompositeParameter& context,
+                                           const runtime_cfg::Parameter& modifiedParameter);
+
+  virtual void addParameterEventHandler(const runtime_cfg::CompositeParameter& context,
+                                        runtime_cfg::Parameter* addedParameter); // ok!
+
+  virtual runtime_cfg::CompositeParameter*
+  addParameterEventHandler(const runtime_cfg::CompositeParameter& context,
+                           runtime_cfg::CompositeParameter* addedParameter); // ok!
+
+  virtual void addParameterEventHandler(runtime_cfg::CompositeParameter* context,
+                                        runtime_cfg::Parameter* addedParameter); // ok!
+
 private:
   void initializeLinkSets(runtime_cfg::CompositeParameter& routingKeysParameter);
 
-  bool processApplicationLinkSets(const runtime_cfg::CompositeParameter* routingEntryCompositeParameter,
+  bool processApplicationLinkSets(runtime_cfg::CompositeParameter* routingEntryCompositeParameter,
                                   const communication::LinkId& linkSetId,
                                   io_dispatcher::LinkSet::linkset_mode_t linkSetMode);
 
-  bool processSgpLinkSets(const runtime_cfg::CompositeParameter* routingEntryCompositeParameter,
+  bool processSgpLinkSets(runtime_cfg::CompositeParameter* routingEntryCompositeParameter,
                           const communication::LinkId& linkSetId,
                           io_dispatcher::LinkSet::linkset_mode_t linkSetMode);
 
-  void fillUpRouteTable(const runtime_cfg::CompositeParameter* routingEntryCompositeParameter,
+  void fillUpRouteTable(runtime_cfg::CompositeParameter* routingEntryCompositeParameter,
                         const communication::LinkId& linkSetId);
 
   io_dispatcher::LinkSet::linkset_mode_t convertStringToTrafficModeValue(const std::string& trafficMode,
@@ -39,8 +56,7 @@ private:
   std::string makeAddressFamilyPrefix(unsigned int gti,
                                       const std::string& gtMaskValue) const;
 
-  std::string _name;
-  smsc::logger::Logger* _logger;
+  //  runtime_cfg::CompositeParameter* findRoutingEntryParameter(const runtime_cfg::CompositeParameter& context);
 };
 
 }
