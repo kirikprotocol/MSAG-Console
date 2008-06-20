7a81618c32f9b44b19ae6eac31e0073d384291d6
shulga <>
2008-06-20 06:34:10 +0000

    added changeParameterEventHandler method; pushed up getName() and waitForCompletion() methods to upper class

--- src/sua/sua_layer/io_dispatcher/IODispatcherSubsystem.cpp
+++ src/sua/sua_layer/io_dispatcher/IODispatcherSubsystem.cpp
@@ -1,3 +1,4 @@
+#include <sua/utilx/strtol.hpp>
 #include "IODispatcherSubsystem.hpp"
 #include "ConnectMgr.hpp"
 #include "IOEventProcessor.hpp"
@@ -6,7 +7,7 @@
 namespace io_dispatcher {
 
 IODispatcherSubsystem::IODispatcherSubsystem()
-  : _name("IODispatcherSubsystem"), _logger(smsc::logger::Logger::getInstance("io_dsptch")),
+  : sua_layer::ApplicationSubsystem("IODispatcherSubsystem", "io_dsptch"),
     _stateMachinesCount(0)
 {}
 
@@ -35,23 +36,30 @@ void
 IODispatcherSubsystem::initialize(runtime_cfg::RuntimeConfig& rconfig)
 {
   SuaLayerMessagesFactory::init();
-  //  LinkSetsManager::init();
   ConnectMgr::init();
 
   runtime_cfg::Parameter& stateMachinesCount = rconfig.find<runtime_cfg::Parameter>("config.state_machines_count");
-  _stateMachinesCount = atoi(stateMachinesCount.getValue().c_str());
-  if ( _stateMachinesCount <= 0 )
-    throw smsc::util::Exception("IODispatcherSubsystem::initialize::: invalid value of config.state_machines_count parameter = %d", _stateMachinesCount);
-}
-
-const std::string&
-IODispatcherSubsystem::getName() const
-{
-  return _name;
+  _stateMachinesCount = utilx::strtol(stateMachinesCount.getValue().c_str(), (char **)NULL, 10);
+  if ( _stateMachinesCount == 0 && errno )
+    throw smsc::util::Exception("IODispatcherSubsystem::initialize::: invalid value of config.state_machines_count parameter=[%s]", stateMachinesCount.getValue().c_str());
 }
 
 void
-IODispatcherSubsystem::waitForCompletion()
-{}
+IODispatcherSubsystem::changeParameterEventHandler(const runtime_cfg::CompositeParameter& context,
+                                                   const runtime_cfg::Parameter& modifiedParameter)
+{
+  if ( context.getFullName() == "config" ) {
+    runtime_cfg::RuntimeConfig& runtimeConfig = runtime_cfg::RuntimeConfig::getInstance();
+    runtime_cfg::CompositeParameter& rootConfigParam = runtimeConfig.find<runtime_cfg::CompositeParameter>("config");
+
+    if ( modifiedParameter.getFullName() != "state_machines_count" ) return;
+
+    smsc_log_debug(_logger, "IODispatcherSubsystem::handle::: handle modified parameter 'config.%s'='%s'", modifiedParameter.getName().c_str(), modifiedParameter.getValue().c_str());
+
+    runtime_cfg::Parameter* foundParam = rootConfigParam.getParameter<runtime_cfg::Parameter>(modifiedParameter.getName());
+    if ( foundParam )
+      foundParam->setValue(modifiedParameter.getValue());
+  }
+}
 
 }
