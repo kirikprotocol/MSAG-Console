4571af8d7b17e6b43b599f8a76776a36123c7118
shulga <>
2008-06-20 08:28:51 +0000

    put into repository

---
+++ src/sua/sua_layer/runtime_cfg/RuntimeConfig_impl.hpp
@@ -0,0 +1,26 @@
+template <> inline
+CompositeParameter&
+RuntimeConfig::find<CompositeParameter>(const std::string& parameterName) {
+  std::string leafParameterName;
+  CompositeParameter& nodeParameter = findLastNodeParameter(parameterName, &leafParameterName);
+
+  CompositeParameter* nodeParameterPtr = nodeParameter.getParameter<CompositeParameter>(leafParameterName);
+  if (  !nodeParameterPtr )
+    throw std::runtime_error(std::string("C::find<CompositeParameter>::: node parameter ") + parameterName + std::string(" is not found"));
+
+  return *nodeParameterPtr;
+
+}
+
+template <> inline
+Parameter&
+RuntimeConfig::find<Parameter>(const std::string& parameterName) {
+  std::string leafParameterName;
+  CompositeParameter& nodeParameter = findLastNodeParameter(parameterName, &leafParameterName);
+
+  Parameter* childParameterPtr = nodeParameter.getParameter<Parameter>(leafParameterName);
+  if (  !childParameterPtr )
+    throw std::runtime_error(std::string("C::find<Parameter>::: leaf parameter ") + parameterName + std::string(" is not found"));
+
+  return *childParameterPtr;
+}
