3f19f56f322cbfdc648311b85c24ed8c9ed5da86
shulga <>
2008-06-20 08:11:56 +0000

    replaced inheritance from utilx::Subsystem to inheritance from sua_layer::ApplicationSubsystem; added changeParameterEventHandler and addParameterEventHandler methods; pushed up getName(), waitForCompletion and start() methods to upper class

--- src/sua/sua_layer/sua_user_communication/SuaUserCommunicationSubsystem.hpp
+++ src/sua/sua_layer/sua_user_communication/SuaUserCommunicationSubsystem.hpp
@@ -2,27 +2,33 @@
 # define __SUA_SUALAYER_SUAUSERCOMMUNICATION_SUAUSERCOMMUNICATIONSUBSYSTEM_HPP__ 1
 
 # include <string>
-# include <sua/utilx/Subsystem.hpp>
+
 # include <sua/utilx/Singleton.hpp>
-# include <util/config/ConfigView.h>
-# include <logger/Logger.h>
-# include <netinet/in.h>
+# include <sua/sua_layer/ApplicationSubsystem.hpp>
+# include <sua/sua_layer/runtime_cfg/RuntimeConfig.hpp>
+# include <sua/sua_layer/runtime_cfg/ParameterObserver.hpp>
 
 namespace sua_user_communication {
 
-class SuaUserCommunicationSubsystem : public utilx::Subsystem,
+class SuaUserCommunicationSubsystem : public sua_layer::ApplicationSubsystem,
+                                      public runtime_cfg::ParameterObserver,
                                       public utilx::Singleton<SuaUserCommunicationSubsystem*> {
 public:
   SuaUserCommunicationSubsystem();
-  virtual void start();
   virtual void stop();
   virtual void initialize(runtime_cfg::RuntimeConfig& rconfig);
-  virtual void waitForCompletion();
-  virtual const std::string& getName() const;
+
+  using ParameterObserver::addParameterEventHandler;
+  using ParameterObserver::changeParameterEventHandler;
+  //  using ParameterObserver::removeParameterEventHandler;
+
+  virtual void addParameterEventHandler(const runtime_cfg::CompositeParameter& context,
+                                        runtime_cfg::Parameter* addedParameter);
+  virtual void changeParameterEventHandler(const runtime_cfg::CompositeParameter& context,
+                                           const runtime_cfg::Parameter& modifiedParameter);
+  //  virtual void removeParameterEventHandler(const runtime_cfg::Parameter& removedParameter);
 private:
-  std::string _name;
   char _acceptorName[128];
-  smsc::logger::Logger* _logger;
 };
 
 }
