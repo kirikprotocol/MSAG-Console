57c44c3c969b5406824afc38cf755d7bb648f5f9
shulga <>
2008-06-20 06:32:09 +0000

    replaced inheritance from utilx::Subsystem to inheritance from sua_layer::ApplicationSubsystem; added changeParameterEventHandler method; pushed up getName() and waitForCompletion() methods to upper class

--- src/sua/sua_layer/io_dispatcher/IODispatcherSubsystem.hpp
+++ src/sua/sua_layer/io_dispatcher/IODispatcherSubsystem.hpp
@@ -1,28 +1,29 @@
 #ifndef __SUA_SUALAYER_IODISPATCHER_IODISPATCHERSUBSYSTEM_HPP__
 # define __SUA_SUALAYER_IODISPATCHER_IODISPATCHERSUBSYSTEM_HPP__ 1
 
-# include <sua/utilx/Subsystem.hpp>
-# include <sua/utilx/Singleton.hpp>
-# include <util/config/ConfigView.h>
 # include <core/threads/ThreadPool.hpp>
-# include <logger/Logger.h>
+
+# include <sua/utilx/Singleton.hpp>
+# include <sua/sua_layer/ApplicationSubsystem.hpp>
+# include <sua/sua_layer/runtime_cfg/RuntimeConfig.hpp>
+# include <sua/sua_layer/runtime_cfg/ParameterObserver.hpp>
 
 namespace io_dispatcher {
 
-class IODispatcherSubsystem : public utilx::Subsystem,
+class IODispatcherSubsystem : public sua_layer::ApplicationSubsystem,
+                              public runtime_cfg::ParameterObserver,
                               public utilx::Singleton<IODispatcherSubsystem*> {
 public:
   IODispatcherSubsystem();
   virtual void start();
   virtual void stop();
   virtual void initialize(runtime_cfg::RuntimeConfig& rconfig);
-  virtual const std::string& getName() const;
-  virtual void waitForCompletion();
-private:
-  std::string _name;
 
+  using ParameterObserver::changeParameterEventHandler;
+  virtual void changeParameterEventHandler(const runtime_cfg::CompositeParameter& context,
+                                           const runtime_cfg::Parameter& modifiedParameter);
+private:
   smsc::core::threads::ThreadPool _threadsPool;
-  smsc::logger::Logger* _logger;
   int _stateMachinesCount;
 };
 
