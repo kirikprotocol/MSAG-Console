f8ab234b07d1141d802b6b6cbcb6e8c701d605c0
shulga <>
2008-06-24 04:53:53 +0000

    applyParametersChange and initializeConnectAcceptor methods was added; added "config.commit" user command processing

--- src/sua/sua_layer/sua_user_communication/SuaUserCommunicationSubsystem.cpp
+++ src/sua/sua_layer/sua_user_communication/SuaUserCommunicationSubsystem.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <util/Exception.hpp>
+#include <sua/utilx/strtol.hpp>
 #include <sua/sua_layer/io_dispatcher/ConnectMgr.hpp>
 #include <sua/communication/libsua_messages/initializer.hpp>
 
@@ -14,6 +15,9 @@
 
 namespace sua_user_communication {
 
+utilx::ThreadSpecificData<bool>
+SuaUserCommunicationSubsystem::_threadScopeModificationFlag;
+
 SuaUserCommunicationSubsystem::SuaUserCommunicationSubsystem()
   : sua_layer::ApplicationSubsystem("SuaUserCommunicationSubsystem", "sua_usr_cm")
 {}
@@ -22,7 +26,7 @@ void
 SuaUserCommunicationSubsystem::stop()
 {
   smsc_log_info(_logger, "SuaUserCommunicationSubsystem::stop::: shutdown subsystem");
-  delete io_dispatcher::ConnectMgr::getInstance().removeConnectAcceptor(_acceptorName);
+  delete io_dispatcher::ConnectMgr::getInstance().removeConnectAcceptor(_acceptorName, true);
 }
 
 void
@@ -39,24 +43,23 @@ SuaUserCommunicationSubsystem::initialize(runtime_cfg::RuntimeConfig& rconfig)
   libsua_messages::initialize();
   registerMessageCreators();
 
+  runtime_cfg::RuntimeConfig::getInstance().registerParameterObserver("config.commit", this);
   runtime_cfg::RuntimeConfig::getInstance().registerParameterObserver("config.local_ip", this);
   runtime_cfg::RuntimeConfig::getInstance().registerParameterObserver("config.local_port", this);
   runtime_cfg::RuntimeConfig::getInstance().registerParameterObserver("config.sua_applications", this);
+  runtime_cfg::RuntimeConfig::getInstance().registerParameterObserver("config.sua_applications.commit", this);
   runtime_cfg::RuntimeConfig::getInstance().registerParameterObserver("config.sua_applications.application", this);
 
   runtime_cfg::Parameter& listeningHostParameter = rconfig.find<runtime_cfg::Parameter>("config.local_ip");
   runtime_cfg::Parameter& listeningPortParameter = rconfig.find<runtime_cfg::Parameter>("config.local_port");
 
   std::string localHost = listeningHostParameter.getValue();
-  in_port_t localPort = (in_port_t)atoi(listeningPortParameter.getValue().c_str());
-  snprintf(_acceptorName, sizeof(_acceptorName), "libSuaAcptr_%s.%d", localHost.c_str(), localPort);
+  in_port_t localPort = (in_port_t)utilx::strtol(listeningPortParameter.getValue().c_str(), (char **)NULL, 10);
+  if ( localPort == 0 && errno )
+    throw smsc::util::Exception("SuaUserCommunicationSubsystem::initialize::: invalid config.local_port value");
 
-  char acceptorDescription[256];
-  snprintf(acceptorDescription, sizeof(acceptorDescription), "LibSuaConnectAcceptor for localHost=%s,localPort=%d", localHost.c_str(), localPort);
+  initializeConnectAcceptor(localHost, localPort);
 
-  io_dispatcher::ConnectMgr::getInstance().addConnectAcceptor(_acceptorName, new LibSuaConnectAcceptor(acceptorDescription, localHost, localPort));
-
-  smsc_log_info(_logger, "SuaUserCommunicationSubsystem::initialize::: added ConnectAcceptor with name [%s]", _acceptorName);
   runtime_cfg::CompositeParameter& sua_applications_parameter = rconfig.find<runtime_cfg::CompositeParameter>("config.sua_applications");
 
   runtime_cfg::CompositeParameter::Iterator<runtime_cfg::Parameter> iterator = sua_applications_parameter.getIterator<runtime_cfg::Parameter>("application");
@@ -65,7 +68,8 @@ SuaUserCommunicationSubsystem::initialize(runtime_cfg::RuntimeConfig& rconfig)
     const runtime_cfg::Parameter* parameter = iterator.getCurrentElement();
     const std::string& appId =  parameter->getValue();
     communication::LinkId linkSetId;
-    LinkSetInfoRegistry::getInstance().getLinkSet(appId, &linkSetId); // throws exception if there isn't registered appId
+    if ( !LinkSetInfoRegistry::getInstance().getLinkSet(appId, &linkSetId) )
+      throw smsc::util::Exception("SuaUserCommunicationSubsystem::initialize::: unknown application id value=[%s]", appId.c_str());
     iterator.next(); // pass to next element
   }
 }
@@ -79,7 +83,7 @@ SuaUserCommunicationSubsystem::addParameterEventHandler(const runtime_cfg::Compo
     runtime_cfg::CompositeParameter& suaApplicationsParam = runtimeConfig.find<runtime_cfg::CompositeParameter>("config.sua_applications");
 
     if ( addedParameter->getName() == "application" ) {
-      smsc_log_debug(_logger, "SuaUserCommunicationSubsystem::handle::: handle added parameter 'config.sua_applications.application'='%s'", addedParameter->getValue().c_str());
+      smsc_log_debug(_logger, "SuaUserCommunicationSubsystem::addParameterEventHandler::: handle added parameter 'config.sua_applications.application'='%s'", addedParameter->getValue().c_str());
 
       if ( !checkParameterExist(&suaApplicationsParam, addedParameter) )
         suaApplicationsParam.addParameter(addedParameter);
@@ -89,21 +93,65 @@ SuaUserCommunicationSubsystem::addParameterEventHandler(const runtime_cfg::Compo
 
 void
 SuaUserCommunicationSubsystem::changeParameterEventHandler(const runtime_cfg::CompositeParameter& context,
-                                                      const runtime_cfg::Parameter& modifiedParameter)
+                                                           const runtime_cfg::Parameter& modifiedParameter)
 {
   if ( context.getFullName() == "config" ) {
     runtime_cfg::RuntimeConfig& runtimeConfig = runtime_cfg::RuntimeConfig::getInstance();
     runtime_cfg::CompositeParameter& rootConfigParam = runtimeConfig.find<runtime_cfg::CompositeParameter>("config");
 
-    if ( modifiedParameter.getName() != "local_ip" &&
-         modifiedParameter.getName() != "local_port" )
-      return;
+    if ( modifiedParameter.getName() == "commit" )
+      applyParametersChange();
+    else {
+      if ( modifiedParameter.getName() != "local_ip" &&
+           modifiedParameter.getName() != "local_port" )
+        return;
 
-    smsc_log_debug(_logger, "SuaUserCommunicationSubsystem::handle::: handle modified parameter 'config.%s'='%s'", modifiedParameter.getName().c_str(), modifiedParameter.getValue().c_str());
-    runtime_cfg::Parameter* foundParam = rootConfigParam.getParameter<runtime_cfg::Parameter>(modifiedParameter.getName());
-    if ( foundParam )
+      smsc_log_debug(_logger, "SuaUserCommunicationSubsystem::changeParameterEventHandler::: handle modified parameter 'config.%s'='%s'", modifiedParameter.getName().c_str(), modifiedParameter.getValue().c_str());
+      runtime_cfg::Parameter* foundParam = rootConfigParam.getParameter<runtime_cfg::Parameter>(modifiedParameter.getName());
+      if ( !foundParam )
+        throw smsc::util::Exception("SuaUserCommunicationSubsystem::changeParameterEventHandler::: parameter '%s' not found in current configuration", modifiedParameter.getName().c_str());
+
       foundParam->setValue(modifiedParameter.getValue());
+      _threadScopeModificationFlag.set(true);
+    }
   }
+}
+
+void
+SuaUserCommunicationSubsystem::applyParametersChange()
+{
+  smsc_log_debug(_logger, "SuaUserCommunicationSubsystem::applyParametersChange::: Enter it");
+
+  if ( !_threadScopeModificationFlag.get(false) ) return;
+
+  _threadScopeModificationFlag.set(false);
+
+  runtime_cfg::RuntimeConfig& rconfig = runtime_cfg::RuntimeConfig::getInstance();
+
+  runtime_cfg::Parameter& listeningHostParameter = rconfig.find<runtime_cfg::Parameter>("config.local_ip");
+  runtime_cfg::Parameter& listeningPortParameter = rconfig.find<runtime_cfg::Parameter>("config.local_port");
+
+  std::string localHost = listeningHostParameter.getValue();
+  in_port_t localPort = (in_port_t)utilx::strtol(listeningPortParameter.getValue().c_str(), (char **)NULL, 10);
+  if ( localPort == 0 && errno )
+    throw smsc::util::Exception("SuaUserCommunicationSubsystem::applyParametersChange::: invalid config.local_port value");
+
+  delete io_dispatcher::ConnectMgr::getInstance().removeConnectAcceptor(_acceptorName, false); // false --> don't remove current active client's connections
+  initializeConnectAcceptor(localHost, localPort);
+}
+
+void
+SuaUserCommunicationSubsystem::initializeConnectAcceptor(const std::string& localHost,
+                                                         in_port_t localPort)
+{
+  snprintf(_acceptorName, sizeof(_acceptorName), "libSuaAcptr_%s.%d", localHost.c_str(), localPort);
+
+  char acceptorDescription[256];
+  snprintf(acceptorDescription, sizeof(acceptorDescription), "LibSuaConnectAcceptor for localHost=%s,localPort=%d", localHost.c_str(), localPort);
+
+  io_dispatcher::ConnectMgr::getInstance().addConnectAcceptor(_acceptorName, new LibSuaConnectAcceptor(acceptorDescription, localHost, localPort));
+
+  smsc_log_info(_logger, "SuaUserCommunicationSubsystem::initialize::: ConnectAcceptor with name [%s] was initialized", _acceptorName);
 }
 
 // void
