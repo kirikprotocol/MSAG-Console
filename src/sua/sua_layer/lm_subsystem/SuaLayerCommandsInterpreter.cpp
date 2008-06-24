aa2ab0ddde1d06b155d11443a89087ade3eb9fad
shulga <>
2008-06-24 04:40:35 +0000

    return type of interpretCommandLine method was modified; when user type "exit" or "quit" then create lm_commands::LM_SuaLayer_Commit and return it

--- src/sua/sua_layer/lm_subsystem/SuaLayerCommandsInterpreter.cpp
+++ src/sua/sua_layer/lm_subsystem/SuaLayerCommandsInterpreter.cpp
@@ -3,34 +3,41 @@
 #include "SuaApplicationsCommandsInterpreter.hpp"
 #include "SgpLinksCommandsInterpreter.hpp"
 #include "TranslationTableCommandsInterpreter.hpp"
+#include "lm_commands/LM_SuaLayer_Commit.hpp"
 #include "lm_commands/LM_LocalIpCommand.hpp"
 #include "lm_commands/LM_LocalTcpPortCommand.hpp"
 #include "lm_commands/LM_StateMachineCountCommand.hpp"
 
 namespace lm_subsystem {
 
-std::pair<lm_commands_refptr_t, lm_commands_interpreter_refptr_t>
+LM_CommandsInterpreter::interpretation_result
 SuaLayerCommandsInterpreter::interpretCommandLine(utilx::StringTokenizer& stringTokenizer)
 {
-  std::pair<lm_commands_refptr_t, lm_commands_interpreter_refptr_t> parseResult(lm_commands_refptr_t(NULL), lm_commands_interpreter_refptr_t(NULL));
+  interpretation_result parseResult(lm_commands_refptr_t(NULL), lm_commands_interpreter_refptr_t(NULL), false);
 
   if ( stringTokenizer.hasNextToken() ) {
     const std::string& tokenValue = utilx::toLowerCaseString(stringTokenizer.nextToken());
     if ( tokenValue == "sua-applications" ) {
-      parseResult.second = create_CommandsInterpreter<SuaApplicationsCommandsInterpreter>(stringTokenizer);
+      parseResult.command = new lm_commands::LM_SuaLayer_Commit();
+      parseResult.interpreter = create_CommandsInterpreter<SuaApplicationsCommandsInterpreter>(stringTokenizer);
     } else if ( tokenValue == "sgp-links" ) {
-      parseResult.second = create_CommandsInterpreter<SgpLinksCommandsInterpreter>(stringTokenizer);
+      parseResult.command = new lm_commands::LM_SuaLayer_Commit();
+      parseResult.interpreter = create_CommandsInterpreter<SgpLinksCommandsInterpreter>(stringTokenizer);
     } else if ( tokenValue == "translation-table" ) {
-      parseResult.second = create_CommandsInterpreter<TranslationTableCommandsInterpreter>(stringTokenizer);
-    } else if ( tokenValue == "local-ip" ) {
-      parseResult.first = create_LM_Command<lm_commands::LM_LocalIpCommand>(stringTokenizer);
-    } else if ( tokenValue == "local-tcp-port" ) {
-      parseResult.first = create_LM_Command<lm_commands::LM_LocalTcpPortCommand>(stringTokenizer);
-    } else if ( tokenValue == "state-machine-count" ) {
-      parseResult.first = create_LM_Command<lm_commands::LM_StateMachineCountCommand>(stringTokenizer);
-    } else if ( tokenValue != "exit" && tokenValue != "quit" )
+      parseResult.command = new lm_commands::LM_SuaLayer_Commit();
+      parseResult.interpreter = create_CommandsInterpreter<TranslationTableCommandsInterpreter>(stringTokenizer);
+    } else if ( tokenValue == "local-ip" )
+      parseResult.command = create_LM_Command<lm_commands::LM_LocalIpCommand>(stringTokenizer);
+    else if ( tokenValue == "local-tcp-port" )
+      parseResult.command = create_LM_Command<lm_commands::LM_LocalTcpPortCommand>(stringTokenizer);
+    else if ( tokenValue == "state-machine-count" )
+      parseResult.command = create_LM_Command<lm_commands::LM_StateMachineCountCommand>(stringTokenizer);
+    else if ( tokenValue == "exit" || tokenValue == "quit" ) {
+      parseResult.command = new lm_commands::LM_SuaLayer_Commit();
+      parseResult.popUpCurrentInterpreter = true;
+    } else
       throw InvalidCommandLineException("SuaLayerCommandsInterpreter::interpretCommandLine::: invalid input=[%s]", tokenValue.c_str());
-
+    
     return parseResult;
   } else
     throw InvalidCommandLineException("SuaLayerCommandsInterpreter::interpretCommandLine::: empty input");
