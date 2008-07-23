b906faaf3a9b639350b849adf31c32600ea254b8
bukind <>
2008-07-23 06:12:30 +0000

    queue pushCommand interface changed: second argument may be enum value RESERVE,MOVE,PUSH

--- src/scag/transport/SCAGCommand2.h
+++ src/scag/transport/SCAGCommand2.h
@@ -51,26 +51,38 @@ protected:
 };
 
 
+/// The queue interface.
+/// It is supposed that some commands may be kept into external sources,
+/// but they still should be counted.  To achieve this goal method
+/// pushCommand() accepts the second argument 'action', which
+/// tells what to do with a command.
 class SCAGCommandQueue
 {
 public:
+    enum {
+            RESERVE,    // reserve a place for a command (don't push)
+            MOVE,       // move command from external source (push, but don't count)
+            PUSH        // push command (push and count)
+    };
+
     virtual ~SCAGCommandQueue() {}
+
+    /// tell the queue to stop.
+    /// The queue then stop accepting command.
     virtual void stop() = 0;
 
     /// check if the queue is stopped
     virtual bool isStopping() const = 0;
 
+    /// push a command to a queue and return the resulting queue size.
+    /// \param cmd -- command, \param action -- what to do (see above).
     /// return queue size after command is added to queue or
     ///   unsigned(-1) if cmd cannot be added.
-    /// \param count -- do count the command.
     virtual unsigned pushCommand( scag::transport::SCAGCommand2* cmd,
-                                  bool count = true ) = 0;
+                                  int action = PUSH ) = 0;
 
     /// return 0 if the queue is stopped
     virtual scag::transport::SCAGCommand2* popCommand() = 0;
-
-    /// don't push the command but increment the count of commands
-    virtual void incrementCount( scag::transport::SCAGCommand2* cmd ) = 0;
 
 };
 
