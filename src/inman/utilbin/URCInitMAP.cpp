132c22b9835895528b924e38773ae3936f0c0ba3
gvroman <>
2008-07-29 08:32:41 +0000

    URC registry initialization for MAP services

---
+++ src/inman/utilbin/URCInitMAP.cpp
@@ -0,0 +1,32 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "$Id$";
+#endif /* MOD_IDENT_OFF */
+/* ************************************************************************* *
+ * Global return codes registry initialization for MAP services.
+ * ************************************************************************* */
+
+#ifdef URCREGISTRY_GLOBAL_INIT
+#include "inman/inap/SS7MsgErrors.hpp"
+#include "inman/inap/TCAPErrors.hpp"
+#include "inman/inap/TCDlgErrors.hpp"
+namespace smsc {
+namespace inman {
+namespace inap {
+_RCS_SS7_CP_INIT();
+_RCS_TCAPErrorsINIT();
+_RCS_TC_DialogINIT();
+} //inap
+} //inman
+} //smsc
+
+#include "inman/comp/MapOpErrors.hpp"
+namespace smsc {
+namespace inman {
+namespace comp {
+_RCS_MapOpErrorsINIT();
+} //comp
+} //inman
+} //smsc
+
+#endif /* URCREGISTRY_GLOBAL_INIT */
+
---
+++ src/inman/utilbin/URCInitMAP.hpp
@@ -0,0 +1,23 @@
+#pragma ident "$Id$"
+/* ************************************************************************* *
+ * Global return codes registry initialization for MAP services.
+ * ************************************************************************* */
+#ifndef __INMAN_URCREGISTRY_INIT_MAP_HPP
+#define __INMAN_URCREGISTRY_INIT_MAP_HPP
+
+
+#ifdef URCREGISTRY_GLOBAL_INIT
+#include "inman/inap/SS7MsgErrors.hpp"
+#include "inman/inap/TCAPErrors.hpp"
+#include "inman/inap/TCDlgErrors.hpp"
+#include "inman/comp/MapOpErrors.hpp"
+
+#  define URCRegistryInit4MAP() _RCS_SS7_CP_GET(); \
+_RCS_TCAPErrorsGET(); _RCS_TC_DialogGET(); _RCS_MapOpErrorsGET()
+
+#else /* URCREGISTRY_GLOBAL_INIT */
+#  define URCRegistryInit4MAP() /**/
+#endif /* URCREGISTRY_GLOBAL_INIT */
+
+#endif /* __INMAN_URCREGISTRY_INIT_MAP_HPP */
+
