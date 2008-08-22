7fbc4b95833dd4b9cad27932dcb5d834284caa0b
bukind <>
2008-08-22 10:17:31 +0000

    strlcpy for linux

---
+++ src/util/strlcpy.h
@@ -0,0 +1,22 @@
+#ifndef __UTIL_STRLCPY_H
+# define __UTIL_STRLCPY_H 1
+
+#include <string.h>
+# ifdef __GNUC__
+namespace {
+
+  size_t strlcpy( char* dst, const char* src, size_t dstsize )
+  {
+    const size_t lsrc = strlen(src);
+    if ( dstsize > 0 ) {
+      const size_t l = std::min(lsrc,dstsize-1);
+      memcpy( dst, src, l );
+      dst[l] = '\0';
+    }
+    return lsrc;
+  }
+
+}
+#endif
+
+#endif /* __UTIL_LLTOSTR_H */
