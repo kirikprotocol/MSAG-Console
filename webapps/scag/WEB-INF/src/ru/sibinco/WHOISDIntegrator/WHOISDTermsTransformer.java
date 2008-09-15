d4bedd093792cf2ac36d46c38ea200e610a5b2d4
mixim <>
2008-09-15 05:27:15 +0000

    added logs, refactored

--- webapps/scag/WEB-INF/src/ru/sibinco/WHOISDIntegrator/WHOISDTermsTransformer.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/WHOISDIntegrator/WHOISDTermsTransformer.java
@@ -27,38 +27,65 @@ public class WHOISDTermsTransformer {
   private final static String EndingToWHOISDTerms = "-->";
   public final static boolean DEBUG = false;
 
-  public static Map buildRules(LinkedList termAsList,String service, BufferedReader br, File xslFolder) throws Exception {
-    String content = "";
-    String line ="";
-    try {
-     logger.debug("!!!!!!!!!!!Start getting term!!!!!!!!!!");
-     while((line=br.readLine()) != null) {
-      content = content + line + "\n";
-       logger.debug(line);
-       termAsList.add(line);
-     }
-     logger.debug("!!!!!!!!!!!End getting term!!!!!!!!!!");
+    public static final String ENCODING_UTF = "UTF-8";
+
+//  public static Map buildRules_(LinkedList termAsList, String service, BufferedReader br, File xslFolder) throws Exception {
+//    String content = "";
+//      content = "";
+//    String line ="";
+//    try {
+//     logger.debug("!!!!!!!!!!!Start getting term!!!!!!!!!!");
+//        StringBuffer sb = new StringBuffer();
+//     while( ( line = br.readLine() ) != null) {
+//         sb.append( line ).append( "\n" );
+//         termAsList.add(line);
+//     }
+//     content = sb.toString();
+////     logContent(content, "cntbr");
+//     logger.debug("\n-----------------------------------------\ncontent=" + content + "\n---------------------------------------\n" +
+//             "!!!!!!!!!!!End getting term!!!!!!!!!!");
+//    } finally {
+//      if (br!=null) br.close();
+//    }
+//    logger.debug( "WHOISDTermsTransformer.buildRules(4) call buildRules(3)");
+//    return buildRules(service,content,xslFolder);
+//  }
+
+    public static Map buildRules(LinkedList termAsList,String service, BufferedReader br, File xslFolder) throws Exception {
+      String content = "";
+      String line ="";
+      try {
+       logger.debug("!!!!!!!!!!!Start getting term!!!!!!!!!!");
+       while((line=br.readLine()) != null) {
+        content = content + line + "\n";
+         logger.debug(line);
+         termAsList.add(line);
+       }
+       logger.debug("!!!!!!!!!!!End getting term!!!!!!!!!!");
+      }
+      finally {
+        if (br!=null) br.close();
+      }
+      return buildRules(service,content,xslFolder);
     }
-    finally {
-      if (br!=null) br.close();
-    }
-    return buildRules(service,content,xslFolder);
-  }
 
   public static Map buildRules(String service, String content, File xslFolder) throws Exception  {
+      logger.debug( "WHOISDTermsTransformer.buildRules(3) start" );
     HashMap rules = new HashMap(3);
     Rule newRule;
     String stylesheet = "terms.xsl";
     long id = Long.parseLong(service);
     HashMap transportToXSl = WHOISDRequest.transportToXSl;
     LinkedList termsresult = new LinkedList();
+//      logger.debug( "WHOISDTermsTransformer.buildRules(3) addBody() L65");
     addBody(termsresult,content, stylesheet, xslFolder,false);
     termsresult.remove(1);
     termsresult.add(1,HeaderToWHOISDTerms);
     termsresult.add(EndingToWHOISDTerms);
     Vector transports = new Vector(transportToXSl.keySet()) ;
-
+//      logger.debug( "WHOISDTermsTransformer.buildRules() for");
     for (Iterator i = transports.iterator() ;i.hasNext();) {
+        logger.debug( "WHOISDTermsTransformer.buildRules() for into");
         LinkedList ruleresult = new LinkedList();
         LinkedList header = new LinkedList();
         LinkedList body = new LinkedList();
@@ -83,23 +110,34 @@ public class WHOISDTermsTransformer {
 
 
   private static boolean addBody(LinkedList result,String content, String stylesheet, File xslFolder, boolean omit_declaration) throws Exception {
+      logger.debug( "WHOISDTermsTransformer.addBody() start" + "\ncontent=" + content );
     StringWriter sw = new StringWriter();
     StringReader sr = new StringReader(content);
     BufferedReader br = null;
     try {
-    TransformerFactory tFactory = TransformerFactory.newInstance();
-    Transformer transformer = tFactory.newTransformer(new StreamSource(new File(xslFolder,stylesheet)) ) ;
-    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, (omit_declaration)?"yes":"no");
-    transformer.transform(new StreamSource(sr), new StreamResult(sw));
-    br = new BufferedReader(new StringReader(sw.toString()));
-    String line;
-    while((line=br.readLine()) != null) {
-      result.add(line);
-    }
-    if (DEBUG) debugTransformation(result);
-     if (result.size()>2) return true;
+        TransformerFactory tFactory = TransformerFactory.newInstance();
+        Transformer transformer = tFactory.newTransformer(new StreamSource(new File(xslFolder,stylesheet)) ) ;
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, (omit_declaration)?"yes":"no");
+//        logContent(content, "cntbr");
+        transformer.transform(new StreamSource(sr), new StreamResult(sw));
+        br = new BufferedReader(new StringReader(sw.toString()));
+        String line;
+        while((line=br.readLine()) != null) {
+          result.add(line);
+        }
+//        logger.debug( "WHOISDTermsTransformer.addBody() end try 117");
+        if (DEBUG) debugTransformation(result);
+//        logger.debug( "WHOISDTermsTransformer.addBody() end try 119");
+        // debug code -------------------------
+//        for (int i = 0; i < result.size(); i++) {
+//            String s = (String) result.get(i);
+//            logContent(s, "cntbr");
+//        }
+        // -------------------------
+
+         if (result.size()>2) return true;
     } catch(TransformerException te) {
-      throw new WHOISDException("Can't build rules from term, reason: "+te.getMessage() + " " + te.getLocationAsString());
+        throw new WHOISDException("Can't build rules from term, reason: "+te.getMessage() + " " + te.getLocationAsString());
     }
     finally {
       if (sw!=null) sw.close();
@@ -109,7 +147,17 @@ public class WHOISDTermsTransformer {
     return false;
   }
 
-  private static void debugRuleBody(Rule newRule) {
+    private static void logContent(String content, String s) {
+        try {
+            Writer w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(System.currentTimeMillis() + s + ".log"), "UTF-8"));
+            w.write(content);
+            w.close();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    private static void debugRuleBody(Rule newRule) {
     System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     for (Iterator ii = newRule.getBody().iterator(); ii.hasNext(); )
      System.out.println(ii.next());
