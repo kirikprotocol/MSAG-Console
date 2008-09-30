153bf1f2680719198980c5ef4684bdcd052ffe46
alkhal <>
2008-09-30 03:01:34 +0000

    mysql

--- ext_smes/SmsQuiz/sql/mysql_create.sql
+++ ext_smes/SmsQuiz/sql/mysql_create.sql
@@ -2,7 +2,7 @@
 
 create table smsquiz_subscriptions
 (
-    address integer not null primary key ,
+    address varchar(32) not null primary key ,
     start_date timestamp not null,
     end_date timestamp
 ) engine=InnoDB character set utf8;
