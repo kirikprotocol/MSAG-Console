4008e3b23b604166561ba466f76dfe58bfa2e665
alkhal <>
2008-10-01 09:59:13 +0000

    Service PreTest

---
+++ ext_smes/SmsQuiz/test/ServiceTest.java
@@ -0,0 +1,52 @@
+import com.eyeline.sme.smpp.SMPPTransceiver;
+import com.eyeline.sme.handler.MessageHandler;
+import com.eyeline.sme.handler.HandlerException;
+import com.eyeline.utils.config.properties.PropertiesConfig;
+import com.eyeline.utils.config.ConfigException;
+
+import java.io.File;
+
+import ru.aurorisoft.smpp.SMPPException;
+import mobi.eyeline.smsquiz.manager.SubscriptionManager;
+import mobi.eyeline.smsquiz.storage.StorageException;
+import mobi.eyeline.smsquiz.storage.ConnectionPoolFactory;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * author: alkhal
+ */
+public class ServiceTest {
+    public static void init() {
+
+        try {
+            ConnectionPoolFactory.init("conf/config.xml");
+        } catch (StorageException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+        try {
+            SubscriptionManager.init("conf/smsquiz.properties");
+        } catch (StorageException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+      init();
+      try{
+          PropertiesConfig cfg = new PropertiesConfig();
+          cfg.load(new File("conf/smpp.properties"));
+          final SMPPTransceiver transceiver = new SMPPTransceiver(cfg, "");
+          transceiver.connect();
+          MessageHandler mh = new MessageHandler("conf/config.xml",transceiver.getInQueue(),transceiver.getOutQueue());
+          mh.start();
+      } catch (SMPPException e) {
+          e.printStackTrace();
+      } catch (ConfigException e) {
+          e.printStackTrace();
+      } catch (HandlerException e) {
+          e.printStackTrace();
+      }
+
+    }
+}
