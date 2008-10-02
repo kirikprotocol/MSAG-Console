1ecf3427273393754ee7cf4fd50d45ada527021f
alkhal <>
2008-10-02 03:33:12 +0000

    Service Test

---
+++ ext_smes/SmsQuiz/test/SmsQuizTest.java
@@ -0,0 +1,157 @@
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import mobi.eyeline.smsquiz.storage.ConnectionPoolFactory;
+import mobi.eyeline.smsquiz.storage.StorageException;
+import mobi.eyeline.smsquiz.manager.SubscriptionManager;
+import mobi.eyeline.smsquiz.manager.SubManagerException;
+import mobi.eyeline.smsquiz.service.SubscriptionOnSMPPService;
+import mobi.eyeline.smsquiz.service.SubscriptionOffSMPPService;
+import com.eyeline.sme.handler.config.ServicesConfig;
+import com.eyeline.sme.handler.config.ServicesConfigReader;
+import com.eyeline.sme.handler.config.ConfigException;
+import com.eyeline.sme.handler.HandlerException;
+import com.eyeline.sme.handler.RequestToServiceMap;
+import com.eyeline.sme.handler.SMPPService;
+import com.eyeline.sme.handler.SMPPRequest;
+import com.eyeline.sme.handler.services.SimpleResponder;
+import com.eyeline.sme.smpp.OutgoingQueue;
+import com.eyeline.sme.smpp.IncomingObject;
+
+import java.util.Properties;
+
+import ru.aurorisoft.smpp.SMPPException;
+import ru.aurorisoft.smpp.Message;
+
+/**
+ * author: alkhal
+ */
+public class SmsQuizTest {    
+
+
+    //private static SubscriptionOnSMPPService onSMPPService;
+    //private static SubscriptionOffSMPPService offSMPPService;
+   // private static SimpleResponder helpService;
+    private static ServicesConfig config;
+    private static RequestToServiceMap requestToServiceMap;
+
+    @BeforeClass
+    public static void init() {
+        try {
+            ConnectionPoolFactory.init("conf/config.xml");
+        } catch (StorageException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+        try {
+            SubscriptionManager.init();
+        } catch (SubManagerException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+        try {
+            config = ServicesConfigReader.readConfig("conf/services.xml");
+        } catch (ConfigException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+        try {
+            requestToServiceMap = new RequestToServiceMap(config,new OutgoingQueue(20));
+        } catch (HandlerException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testDefaultService() {
+        assertTrue(requestToServiceMap.getDefaultService().getClass().getName().equals(SimpleResponder.class.getName()));
+    }
+
+    @Test
+    public void testRequestMappingHelp() {
+        IncomingObject incObj = createIncObj("sdadsadasasd");
+        RequestToServiceMap.Entry e=requestToServiceMap.getEntry(incObj);
+        assertNull(e);
+    }
+    @Test
+    public void testRequestMappingOn() {
+        IncomingObject incObj = createIncObj("on");
+        RequestToServiceMap.Entry e = requestToServiceMap.getEntry(incObj);
+        assertTrue(e.getService().getClass().getName().equals(SubscriptionOnSMPPService.class.getName()));
+    }
+
+    @Test
+    public void testRequestMappingOff() {
+        IncomingObject incObj = createIncObj("off");
+        RequestToServiceMap.Entry e = requestToServiceMap.getEntry(incObj);
+        assertTrue(e.getService().getClass().getName().equals(SubscriptionOffSMPPService.class.getName()));
+    }
+
+    @Test
+    public void testServiceHelp() {
+        IncomingObject incObj = createIncObj("asdas");
+        SMPPService service = requestToServiceMap.getDefaultService();
+        assertTrue(service.serve(new SMPPRequest("Default request", incObj, new Properties())));
+    }
+
+    @Test
+    public void testServiceOn() {
+        IncomingObject incObj = createIncObj("on");
+        RequestToServiceMap.Entry e = requestToServiceMap.getEntry(incObj);
+        SMPPService service = e.getService();
+        assertNotNull(service);
+        assertTrue(service.serve(e.getPattern().createRequest(incObj)));
+    }
+
+    @Test
+    public void testServiceOff() {
+        IncomingObject incObj = createIncObj("off");
+        RequestToServiceMap.Entry e = requestToServiceMap.getEntry(incObj);
+        SMPPService service = e.getService();
+        assertNotNull(service);
+        assertTrue(service.serve(e.getPattern().createRequest(incObj)));
+    }
+    @Test
+    public void testServiceTryOffAgain() {
+
+        IncomingObject incObj = createIncObj("off");
+        RequestToServiceMap.Entry e = requestToServiceMap.getEntry(incObj);
+        SMPPService service = e.getService();
+        assertNotNull(service);
+        assertFalse(service.serve(e.getPattern().createRequest(incObj)));
+    }
+
+    @AfterClass
+    public static void shutdown() {
+    }
+
+    private class TestIncomingObject implements IncomingObject{
+        private Message message;
+        public TestIncomingObject(Message message) {
+            this.message = message;
+        }
+        public Message getMessage() {
+            return message;
+        }
+
+        public boolean isResponded() {
+            return false;
+        }
+
+        public void respond(int status) throws SMPPException {
+        }
+
+    }
+    private IncomingObject createIncObj(String text) {
+        Message message = new Message();
+        message.setDestinationAddress("148");
+        message.setSourceAddress("+7913948test");
+        message.setMessageString(text);
+        return new TestIncomingObject(message);
+    }
+}
