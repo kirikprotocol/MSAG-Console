43ca69a28385cfe229ca88c1aeaf2aeb944298a0
alkhal <>
2008-10-07 03:13:34 +0000

    tests

--- ext_smes/SmsQuiz/test/replystats/DataSourceTester.java
+++ ext_smes/SmsQuiz/test/replystats/DataSourceTester.java
@@ -51,7 +51,7 @@ public class DataSourceTester {
     @Test
     public void list() {
         try {
-            int flag=1;
+            int flag=0;
             for (Reply reply:dataSource.list("148",new Date(System.currentTimeMillis()-2000), new Date())){
                 if(reply.getText().equals("getFileAddReply"))
                     if(reply.getOa().equals("+7913testAdd"))
