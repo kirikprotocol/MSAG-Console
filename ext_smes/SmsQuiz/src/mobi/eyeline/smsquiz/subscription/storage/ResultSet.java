6cca36dc1260dfed667016dc5b21037e7aff907c
alkhal <>
2008-10-08 11:03:03 +0000

    change package

--- ext_smes/SmsQuiz/src/mobi/eyeline/smsquiz/subscription/storage/ResultSet.java
+++
@@ -1,8 +0,0 @@
-package mobi.eyeline.smsquiz.subscription.storage;
-
-
-public interface ResultSet {
-    public boolean next() throws StorageException ;
-    public Object get() throws StorageException ;
-    public void close();
-}
