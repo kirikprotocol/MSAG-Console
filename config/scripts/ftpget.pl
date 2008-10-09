214b7f53cd20904d4dae3faf41e93d4eae563834
skv <>
2008-10-09 09:24:22 +0000

    get .csv files from speficied host/dir to specified dir and delete them on remote host

---
+++ config/scripts/ftpget.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use Net::FTP;
+
+use constant HOST=>'phoenix';
+use constant USER=>'starkom';
+use constant PASSWD=>'starkom';
+use constant FTPDIR=>'ftptest';
+use constant DSTDIR=>'.';
+
+my $ftp=Net::FTP->new(HOST,Debug=>0) or die "Cannot connect to ".HOST;
+$ftp->login(USER,PASSWD) or die "Login failed:".$ftp->message;
+$ftp->cwd(FTPDIR) or die "Failed to cwd:".$ftp->message;
+my @l=$ftp->ls();
+chdir(DSTDIR);
+for(@l)
+{
+	next unless /.csv$/;
+	print "get $_\n";
+	$ftp->get($_) || die "Failed to get $_:".$ftp->message;
+	print "delete $_\n";
+	$ftp->delete($_) || die "Failed to rm $_:".$ftp->message;
+}
+$ftp->quit;
