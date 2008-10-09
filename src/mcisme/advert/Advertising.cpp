5c0f583c5b9df509be9e6e3b144bccf0853ff570
shulga <>
2008-10-09 11:21:22 +0000

    Advertising class was transformed to interface

--- src/mcisme/advert/Advertising.cpp
+++ src/mcisme/advert/Advertising.cpp
@@ -1,53 +1,13 @@
-
-// #include all that you need !!!
-
 #include "Advertising.h"
-#include "AdvertisingImpl.h"
 
-namespace scag {
-namespace advert {
-
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-
-static bool  bAdvertisingInited = false;
-static Mutex initAdvertisingLock;
-
-inline unsigned GetLongevity(Advertising*) { return 10; }
-typedef SingletonHolder<AdvertisingImpl> SingleAI;
-
-uint64_t BannerRequest::lastId = 0;		// îáùèé ñ÷åò÷èê 
-Mutex 	 BannerRequest::lastIdMutex;	// ìüşòåêñ äëÿ äîñòóïà ê ñ÷åò÷èêó
+namespace smsc {
+namespace mcisme {
 
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-void Advertising::Init(const std::string& host, int port, int timeout, int maxcount)
-{
-    if (!bAdvertisingInited)
-    {
-        MutexGuard guard(initAdvertisingLock);
-        if (!bAdvertisingInited) {
-            AdvertisingImpl& ai = SingleAI::Instance();
-            ai.init(host, port, timeout, maxcount);
-            bAdvertisingInited = true;
-        }
-    }
-}
-
-
-Advertising& Advertising::Instance()
-{
-    if (!bAdvertisingInited) 
-    {
-        MutexGuard guard(initAdvertisingLock);
-        if (!bAdvertisingInited) 
-            throw SCAGException("Advertising not inited!");
-    }
-    return SingleAI::Instance();
-
-}
-
+uint32_t BannerRequest::lastId = 0;  // ÏÂİÉÊ ÓŞÅÔŞÉË 
+Mutex    BannerRequest::lastIdMutex; // ÍØÀÔÅËÓ ÄÌÑ ÄÏÓÔÕĞÁ Ë ÓŞÅÔŞÉËÕ
 
 } // advert
 } // scag
