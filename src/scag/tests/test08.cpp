63978be4136310f53a73a7826f010d1e07f5e87c
bukind <>
2008-10-15 03:20:00 +0000

    better code timing measurement, docs

--- src/scag/tests/test08.cpp
+++ src/scag/tests/test08.cpp
@@ -1,43 +1,107 @@
-/// a test of seqlock
+/// This test may be used to measure timing of a code of interest.
+/// Simply place your code between "// -- MEASURED CODE" comments below.
+/// If you need some preparation code, put it between "// -- PREPARATION CODE" below.
 
 #include <cstdio>
 #include <cmath>
 #include "scag/util/HRTimer.h"
+
 #include "core/synchronization/Mutex.hpp"
+#include "core/synchronization/EventMonitor.hpp"
+#include "core/threads/Thread.hpp"
 
 using scag2::util::HRTimer;
 using namespace smsc::core::synchronization;
+using namespace smsc::core::threads;
 
-int main()
+std::pair< double, double > calcavgerr( unsigned nexp, hrtime_t* data )
 {
-    Mutex mtx;
-    HRTimer hrt0;
-    hrt0.mark();
-    HRTimer hrt(hrt0);
-    hrt.mark();
-    const unsigned passes = 100000;
-    hrtime_t avg = 0;
-    hrtime_t sig = 0;
-    size_t x;
-    for ( size_t i = 0; i < passes; ++i ) {
-        hrt.get();
-        {
-            MutexGuard mg(mtx);
-            x = i;
-        }
-        const hrtime_t d = hrt.get() / 1000;
-        avg += d;
-        sig += d*d;
+    hrtime_t avg(0), sig(0);
+    for ( unsigned i = 0; i < nexp; ++i ) {
+        avg += data[i];
+        sig += data[i]*data[i];
     }
-    unsigned total = unsigned(hrt0.get()/1000);
-    double perpass = double(total)/passes;
+    double davg = double(avg) / nexp;
+    double dsig = double(sig) / nexp;
+    dsig = std::sqrt( (dsig-davg*davg) / (nexp-1) );
+    return std::make_pair(davg,dsig);
+}
+
+
+struct DummyThread : public Thread
+{
+    DummyThread( Mutex& mtx ) : mtx_(mtx) {}
+    virtual int Execute() {
+        mon_.Lock();
+        MutexGuard mg(mtx_);
+        mon_.wait();
+        return 0;
+    }
+    void Stop() {
+        mon_.notifyAll();
+    }
+private:
+    EventMonitor mon_;
+    Mutex&       mtx_;
+};
+
+
+int main( int argc, char** argv )
+{
+    const unsigned nexp = 20; // number of experiments
+
+    unsigned passes = 1000000;
+    if ( argc > 1 ) {
+        passes = ::strtoul( argv[1], NULL, 10 );
+        if ( passes < 100 ) passes = 100;
+        printf( "total number of passes: %u\n", passes );
+    }
+
+// -- PREPARATION CODE
+    Mutex mtx;
+    // DummyThread dt(mtx);
+// -- END OF PREPARATION CODE
+
+    hrtime_t res[nexp*2];
+    for ( size_t measure = 0; measure < nexp*2; ++measure ) {
+
+        size_t sum = 0;
+        HRTimer hrt;
+        hrt.mark();
+        for ( size_t i = 0; i < passes; ++i ) {
+            // hrt.get();
+            if ( measure >= nexp ) {
+// -- MEASURED CODE
+                // trylocking locked mutex
+                // mtx.TryLock();
+                // locking/unlocking free mutex
+                MutexGuard mg(mtx);
+// -- END OF MEASURED CODE
+            }
+            // some dummy instruction to prevent optimization
+            sum += i*(measure+1);
+        }
+
+        {
+            const hrtime_t d = hrt.get() / 1000;
+            res[measure] = d;
+        }
+    }
+    // unsigned total = unsigned(hrt0.get()/1000);
+    // double perpass = double(total)/passes;
 
     // calculations
-    double davg = double(avg) / passes;
-    double dsig = double(sig) / passes;
-    dsig = std::sqrt( (dsig-davg*davg) / passes );
-    printf( "total number of passes: %u\n", passes );
-    printf( "total time (us): %u, time per pass (us): %g\n", total, perpass );
-    printf( "time for hrt.get (us): %g+-%g\n", davg, dsig );
+    printf( "total number of experiments: %u\n", nexp );
+    printf( "total number of passes per experiment: %u\n", passes );
+    std::pair< double, double > avgerr0 = calcavgerr( nexp, &res[0] );
+    printf( "Empty loop time(us): total=%g+-%g  onepass=%g+-%g\n",
+            avgerr0.first, avgerr0.second, avgerr0.first/passes, avgerr0.second/passes );
+    std::pair< double, double > avgerr = calcavgerr( nexp, &res[nexp] );
+    printf( "Full loop time(us): total=%g+-%g  onepass=%g+-%g\n",
+            avgerr.first, avgerr.second, avgerr.first/passes, avgerr.second/passes );
+    const double avg = avgerr.first - avgerr0.first;
+    const double sig = std::sqrt(avgerr.second*avgerr.second + avgerr0.second*avgerr0.second);
+    printf( "Corrected time (us): total=%g+-%g  onepass=%g+-%g\n",
+            avg, sig, avg/passes, sig/passes );
     return 0;
 }
