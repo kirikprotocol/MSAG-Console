f106e53cbba4d0fb69e68af040ba256997958288
odarchenko <>
2008-10-21 03:45:18 +0000

    base class for LongCallContext

---
+++ src/scag/re/base/LongCallContextBase.h
@@ -0,0 +1,80 @@
+#ifndef _SCAG_RE_BASE_LONGCALLCONTEXTBASE_H
+#define _SCAG_RE_BASE_LONGCALLCONTEXTBASE_H
+
+#include <sys/types.h>
+#include <stack>
+#include <string>
+
+namespace scag2 {
+namespace lcm {
+
+enum LongCallCommandId 
+{
+    PERS_GET = 1,
+    PERS_SET,
+    PERS_DEL,
+    PERS_INC,
+    PERS_INC_MOD,
+    PERS_BATCH,
+    BILL_OPEN,
+    BILL_COMMIT,
+    BILL_ROLLBACK
+};
+
+class LongCallParams
+{
+public:
+    std::string exception;
+    virtual ~LongCallParams() {};
+};
+
+class LongCallInitiator;
+
+class LongCallContextBase
+{
+public:
+    LongCallContextBase(): initiator(NULL), next(NULL), params(NULL), stateMachineContext(NULL), continueExec(false) {}
+
+    LongCallParams* getParams() { return params; }
+
+    void setParams(LongCallParams* p)
+    {
+        if (params != p) delete params;
+        params = p;
+    }
+
+    void freeParams()
+    {
+        if(params) delete params;
+        params = NULL;
+    }
+
+    virtual ~LongCallContextBase()
+    {
+        if (params) {
+          delete params;
+        }
+    }
+
+
+public:
+    uint32_t callCommandId;
+    LongCallInitiator *initiator;
+    LongCallContextBase *next;
+    void *stateMachineContext;
+    bool continueExec;
+
+private:
+    LongCallParams *params;
+};
+
+class LongCallInitiator
+{
+public:
+    virtual void continueExecution(LongCallContextBase* context, bool dropped) = 0;
+};
+
+}
+}
+
+#endif
