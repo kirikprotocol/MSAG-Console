6a4305511a2d863b79a7103c9a2aeccb4f606d8a
shulga <>
2008-11-13 10:25:18 +0000

    adde ERR_ADV_OTHER constant, removed ERR_ADV_ constant

--- src/mcisme/advert/AdvertErrors.h
+++ src/mcisme/advert/AdvertErrors.h
@@ -9,16 +9,15 @@ namespace mcisme {
 
 enum 
 {
-    ERR_ADV_TIMEOUT = 1,    // таймаут сервера 
-    ERR_ADV_SOCKET,         // ошибка сокета
-    ERR_ADV_SOCK_WRITE,     // ошибка записи сокета
-    ERR_ADV_BANNER_LEN,     // баннер от сервера больше max длины
-    ERR_ADV_PACKET_TYPE,    // некорректный тип пакета
-    ERR_ADV_PACKET_LEN,     // неверная длина пакета
-    ERR_ADV_PACKET_MEMBER,  // неверный элемент пакета
-    ERR_ADV_QUEUE_FULL,     // очередь запросов переполнена
-    
-    ERR_ADV_
+  ERR_ADV_TIMEOUT = 1,    // таймаут сервера 
+  ERR_ADV_SOCKET,         // ошибка сокета
+  ERR_ADV_SOCK_WRITE,     // ошибка записи сокета
+  ERR_ADV_BANNER_LEN,     // баннер от сервера больше max длины
+  ERR_ADV_PACKET_TYPE,    // некорректный тип пакета
+  ERR_ADV_PACKET_LEN,     // неверная длина пакета
+  ERR_ADV_PACKET_MEMBER,  // неверный элемент пакета
+  ERR_ADV_QUEUE_FULL,     // очередь запросов переполнена
+  ERR_ADV_OTHER
 };
 
 }}
