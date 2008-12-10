6b83188e597efd4d8330ccd3ec51b660643b0a1e
shulga <>
2008-12-10 11:20:22 +0000

    added dispatchBERollbackRequest method

--- src/mcisme/OutputMessageProcessorsDispatcher.hpp
+++ src/mcisme/OutputMessageProcessorsDispatcher.hpp
@@ -4,6 +4,7 @@
 # include <mcisme/AbntAddr.hpp>
 # include <mcisme/TaskProcessor.h>
 # include <mcisme/Profiler.h>
+# include <mcisme/advert/Advertising.h>
 
 namespace smsc {
 namespace mcisme {
@@ -16,6 +17,7 @@ public:
   virtual ~OutputMessageProcessorsDispatcher() {}
   virtual void dispatchSendMissedCallNotification(const AbntAddr& abnt) = 0;
   virtual void dispatchSendAbntOnlineNotifications(const sms_info* pInfo, const AbonentProfile& abntProfile) = 0;
+  virtual void dispatchBERollbackRequest(const BannerResponseTrace& bannerRespTrace) = 0;
 
   virtual void markMessageProcessorAsFree(OutputMessageProcessor* freeMessageProcessor) = 0;
   virtual void deleteMessageProcessor(OutputMessageProcessor* terminatedMessageProcessor) = 0;
