0111878a5587c997c908e8b2d2c5afdab12b0b8a
shulga <>
2008-12-10 11:21:07 +0000

    removed unused constants

--- src/mcisme/advert/AdvertErrors.h
+++ src/mcisme/advert/AdvertErrors.h
@@ -10,13 +10,6 @@ namespace mcisme {
 enum 
 {
   ERR_ADV_TIMEOUT = 1,    // таймаут сервера 
-  ERR_ADV_SOCKET,         // ошибка сокета
-  ERR_ADV_SOCK_WRITE,     // ошибка записи сокета
-  ERR_ADV_BANNER_LEN,     // баннер от сервера больше max длины
-  ERR_ADV_PACKET_TYPE,    // некорректный тип пакета
-  ERR_ADV_PACKET_LEN,     // неверная длина пакета
-  ERR_ADV_PACKET_MEMBER,  // неверный элемент пакета
-  ERR_ADV_QUEUE_FULL,     // очередь запросов переполнена
   ERR_ADV_NOT_CONNECTED,
   ERR_ADV_OTHER
 };
