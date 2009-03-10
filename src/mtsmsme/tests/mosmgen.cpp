2253ecd25486ce1cfa506366e9ed276c6558c787
vic <>
2009-03-10 04:45:25 +0000

    add test applications

---
+++ src/mtsmsme/tests/mosmgen.cpp
@@ -0,0 +1,223 @@
+#include "mtsmsme/sccp/SccpProcessor.hpp"
+#include "mtsmsme/processor/HLRImpl.hpp"
+#include "mtsmsme/processor/TCO.hpp"
+#include "mtsmsme/processor/TSM.hpp"
+#include "core/threads/Thread.hpp"
+#include "mtsmsme/comp/MoForwardSm.hpp"
+#include "mtsmsme/processor/Message.hpp"
+#include "mtsmsme/processor/util.hpp"
+#include "mtsmsme/processor/ACRepo.hpp"
+#include "logger/Logger.h"
+#include <string>
+#include <vector>
+
+using smsc::mtsmsme::processor::SccpProcessor;
+using smsc::mtsmsme::processor::RequestSender;
+using smsc::mtsmsme::processor::Request;
+using smsc::mtsmsme::processor::SubscriberRegistrator;
+using smsc::mtsmsme::processor::TCO;
+using smsc::mtsmsme::processor::TSM;
+using smsc::mtsmsme::processor::util::packSCCPAddress;
+using smsc::mtsmsme::processor::shortMsgMoRelayContext_v2;
+using smsc::mtsmsme::processor::TrId;
+using smsc::mtsmsme::comp::MoForwardSmReq;
+using smsc::mtsmsme::processor::BeginMsg;
+using smsc::mtsmsme::processor::util::packNumString2BCD91;
+using smsc::mtsmsme::processor::util::dump;
+using smsc::core::threads::Thread;
+using smsc::sms::Address;
+using smsc::logger::Logger;
+using std::vector;
+using std::string;
+
+namespace smsc{namespace mtsmsme{namespace processor{
+unsigned ConvertText27bit(
+  const unsigned char* text, unsigned chars, unsigned char* bit7buf,unsigned* elen,
+  unsigned offset,unsigned buflen);
+}}}
+using smsc::mtsmsme::processor::ConvertText27bit;
+
+static Logger *logger = 0;
+class EmptyRequestSender: public RequestSender {
+  virtual bool send(Request* request)
+  {
+    request->setSendResult(0); return true;
+  }
+};
+class EmptySubscriberRegistrator: public SubscriberRegistrator {
+  public:
+    EmptySubscriberRegistrator(TCO* _tco) : SubscriberRegistrator(_tco) {}
+    virtual void registerSubscriber(Address& imsi, Address& msisdn, Address& mgt, int period) {}
+    virtual int  update(Address& imsi, Address& msisdn, Address& mgt) {return 1;}
+    virtual bool lookup(Address& msisdn, Address& imsi, Address& msc) {return false;}
+};
+class GopotaListener: public SccpProcessor, public Thread {
+  public:
+    GopotaListener(TCO* _tco, SubscriberRegistrator* _reg) : SccpProcessor(_tco,_reg) {}
+    virtual int Execute()
+    {
+      int result;
+      result = Run();
+      smsc_log_error(logger,"SccpListener exit with code: %d", result);
+      return result;
+    }
+  };
+int randint(int min, int max)
+{
+  return min+int((max-min+1)*rand()/(RAND_MAX+1.0));
+}
+static uint64_t invokation_count;
+class StatFlusher: public Thread {
+  private:
+    uint64_t temp_counter;
+    TSMSTAT stat;
+  public:
+    StatFlusher(): temp_counter(invokation_count)
+    {
+      TSM::getCounters(stat);
+    }
+    virtual int Execute()
+    {
+      struct timespec delay = {0, 995000000}; // 955ms
+      while (true)
+      {
+        nanosleep(&delay,0);
+        smsc_log_info(logger,"stat flusher: sent %lld",invokation_count - temp_counter);
+        temp_counter = invokation_count;
+
+        struct timespec delay = { 0, 995000000 }; // 955ms
+        TSMSTAT statnow;
+        TSM::getCounters(statnow);
+        smsc_log_info(logger,"dlg cre/del/cnt = %d/%d/%d",
+            statnow.objcreated - stat.objcreated,
+            statnow.objdeleted - stat.objdeleted,
+            statnow.objcount);
+        stat = statnow;
+        nanosleep(&delay,0);
+      }
+      return 0;
+    }
+};
+int main(int argc, char** argv)
+{
+  try
+  {
+    smsc::logger::Logger::Init();
+    logger = smsc::logger::Logger::getInstance("mosmgen");
+    smsc_log_info(logger, "MO SMS generator");
+    smsc_log_error(logger,"sizeof(MoForwardSmReq)=%d",sizeof(MoForwardSmReq));
+    StatFlusher trener;
+    trener.Start();
+    string msca("791398699815"); // MSC address
+    string sca("79139869990"); // service center address
+//    string sca("791398699812"); // service center address
+    EmptyRequestSender fakeSender;
+    TCO mtsms(10000);
+    EmptySubscriberRegistrator fakeHLR(&mtsms);
+    mtsms.setRequestSender(&fakeSender);
+    GopotaListener listener(&mtsms, &fakeHLR);
+    listener.configure(44, 192, Address(msca.length(), 1, 1, msca.c_str()),
+        Address(".1.1.791398699813"), Address(".1.1.791398699814"));
+    listener.Start();
+    sleep(10);
+    uint32_t smscount = 0;
+    // fill SCCP addresses for SMS sending, FROM = MSC GT, TO = SMSC GT
+    uint8_t cl[20]; uint8_t cllen; uint8_t cd[20]; uint8_t cdlen;
+    cdlen = packSCCPAddress(cd, 1 /* E.164 */, sca.c_str() /* SMSC E.164 */, 8 /* SMSC SSN */);
+    cllen = packSCCPAddress(cl, 1 /* E.164 */, msca.c_str() /* MSC E.164 */, 8 /* MSC SSN */);
+    while(true)
+    {
+      /*
+       pos = snprintf(rndmsto,sizeof(rndmsto),"791398699814%04d",randint(100,9999));
+       rndmsto[pos] = 0;
+       pos = snprintf(rndmsfrom,sizeof(rndmsfrom),"791398699814%04d",randint(0,99));
+       rndmsfrom[pos] = 0;
+       string msto(rndmsto); // B-subsriber e.g. "7913986998140100"
+       string msfrom(rndmsfrom); // A-subscriber e.g. "7913986998140001
+
+       //prepare SMS
+       //FIRST MODE Mega testovajf hren'
+       //0000000    21  2e  0f  91  97  31  89  96  89  41  33  f3  00  08  ff  28
+       //0000020    04  1c  04  35  04  33  04  30  00  20  04  42  04  35  04  41
+       //0000040    04  42  04  3e  04  32  04  30  04  4f  00  20  04  45  04  40
+       //0000060    04  35  04  3d  04  4c  00  21
+       unsigned char smstext[] = { 0x04,0x1c,0x04,0x35,0x04,0x33,0x04,0x30,
+       0x00,0x20,0x04,0x42,0x04,0x35,0x04,0x41,
+       0x04,0x42,0x04,0x3e,0x04,0x32,0x04,0x30,
+       0x04,0x4f,0x00,0x20,0x04,0x45,0x04,0x40,
+       0x04,0x35,0x04,0x3d,0x04,0x4c,0x00,0x21 };
+       OCTET_STRING_DECL(dest,20);
+       ZERO_OCTET_STRING(dest);
+       dest.size = packNumString2BCD91(dest.buf,msto.c_str(), msto.length());
+       vector<unsigned char> ui;
+       ui.push_back(0x11); //MI
+       ui.push_back(0x2E); //TP Message Reference
+       ui.push_back(msto.length()); // TP-Destination-Address length in digits
+       ui.insert(ui.end(),dest.buf,dest.buf+dest.size); //TP-Destination-Address
+       ui.push_back(0); //TP-Protocol-Identifier
+       ui.push_back(0x08); //TP-Data_Coding-Scheme
+       ui.push_back(0xFF); //TP-Validity-Period
+       ui.push_back(sizeof(smstext)); //TP-User-Data-Length
+       ui.insert(ui.end(), smstext, smstext + sizeof(smstext)); // TP-User-Data
+       MoForwardSmReq mosms(sca,msfrom,ui);
+       */
+
+      TSM* tsm = 0;
+      tsm = mtsms.TC_BEGIN(shortMsgMoRelayContext_v2);
+      //mtsms->
+      if (tsm)
+      {
+        char rndmsto[20] = {0};
+        char rndmsfrom[20] = {0};
+        int pos;
+        pos = snprintf(rndmsto,sizeof(rndmsto),"791398699814%04d",randint(100,9999));
+        rndmsto[pos] = 0;
+        pos = snprintf(rndmsfrom,sizeof(rndmsfrom),"791398699814%04d",randint(0,99));
+        rndmsfrom[pos] = 0;
+        string msto(rndmsto); // B-subsriber e.g. "7913986998140100"
+        string msfrom(rndmsfrom); // A-subscriber e.g. "7913986998140001
+        //prepare SMS count in message body
+        char smstextlatin1[20] = {0}; int tpos;
+        tpos = snprintf(smstextlatin1,sizeof(smstextlatin1),"%010d",++smscount);
+        smstextlatin1[tpos] = 0;
+        unsigned char smstext[20] = {0};
+        unsigned int escaped_len = 0; // TP-User-Data-Length
+        //            unsigned maxlen=(unsigned)(ET96MAP_MAX_SIGNAL_INFO_LEN-(pdu_ptr+1-(pdu->signalInfo+1)));
+        int _newbuflen = ConvertText27bit(
+            (unsigned char *)smstextlatin1,strlen(smstextlatin1),
+            smstext,&escaped_len,0,sizeof(smstext));
+
+        OCTET_STRING_DECL(dest,20);
+        ZERO_OCTET_STRING(dest);
+        dest.size = packNumString2BCD91(dest.buf,msto.c_str(), msto.length());
+        vector<unsigned char> ui;
+        ui.push_back(0x11); //MI
+        ui.push_back(0x2E); //TP Message Reference
+        ui.push_back(msto.length()); // TP-Destination-Address length in digits
+        ui.insert(ui.end(),dest.buf,dest.buf+dest.size); //TP-Destination-Address
+        ui.push_back(0); //TP-Protocol-Identifier
+        ui.push_back(0x00); //TP-Data_Coding-Scheme
+        ui.push_back(0xFF); //TP-Validity-Period
+        ui.push_back(escaped_len); //TP-User-Data-Length
+        ui.insert(ui.end(), smstext, smstext + _newbuflen); // TP-User-Data
+        MoForwardSmReq mosms(sca,msfrom,ui);
+
+        tsm->TInvokeReq( 1 /* invoke_id */, 46 /* mo-ForwardSM */, mosms);
+        tsm->TBeginReq(cdlen, cd, cllen, cl);
+        smsc_log_info(logger,"sent %010d",smscount);
+        invokation_count++;
+      }
+      else
+      {
+      //sleep(1);
+      struct timespec delay = { 0, 5000000}; // nonoseconds
+      nanosleep(&delay, 0);
+      }
+    }
+    listener.Stop();
+  } catch (std::exception& ex)
+  {
+    smsc_log_error(logger, " cought unexpected exception [%s]", ex.what());
+  }
+  return 0;
+}
---
+++ src/mtsmsme/tests/mschlr.cpp
@@ -0,0 +1,142 @@
+#include "mtsmsme/sccp/SccpProcessor.hpp"
+#include "core/threads/Thread.hpp"
+#include "mtsmsme/processor/SccpSender.hpp"
+#include "mtsmsme/processor/TCO.hpp"
+#include "mtsmsme/processor/TSM.hpp"
+#include "mtsmsme/processor/HLRImpl.hpp"
+#include "mtsmsme/comp/SendRoutingInfoForSM.hpp"
+#include "mtsmsme/processor/util.hpp"
+#include "sms/sms.h"
+#include <string>
+
+extern std::string hexdmp(const uchar_t* buf, uint32_t bufSz);
+
+#define TRANS_TYPE 0
+#define NUM_PLAN 0x10
+#define ENC_SCHEME 0x01
+#define NATURE_OF_ADDR 0x04
+
+using smsc::mtsmsme::processor::SccpProcessor;
+using smsc::mtsmsme::processor::RequestSender;
+using smsc::mtsmsme::processor::Request;
+using smsc::mtsmsme::processor::SubscriberRegistrator;
+using smsc::core::threads::Thread;
+using smsc::mtsmsme::processor::TCO;
+using smsc::mtsmsme::processor::TSM;
+using smsc::mtsmsme::processor::TSMSTAT;
+using smsc::mtsmsme::processor::shortMsgGatewayContext_v2;
+using smsc::logger::Logger;
+using smsc::mtsmsme::processor::SccpSender;
+using smsc::mtsmsme::comp::SendRoutingInfoForSMReq;
+using smsc::mtsmsme::processor::util::packSCCPAddress;
+using smsc::mtsmsme::processor::util::dump;
+using smsc::sms::Address;
+using std::string;
+
+static Logger *logger = 0;
+class EmptyRequestSender: public RequestSender {
+  virtual bool send(Request* request)
+  {
+//    using namespace smsc::sms;
+//    const char* prop = 0;
+//    unsigned int len = 0;
+//    prop = request->sms.getBinProperty(Tag::SMSC_RAW_SHORTMESSAGE,&len);
+//    smsc_log_info(logger,
+//                           "SMS[%d]={%s}",len,dump(len,(unsigned char *)prop).c_str());
+//sms.setBinProperty(Tag::SMSC_RAW_SHORTMESSAGE,(char*)b.get(),udh_len+1+symbols);
+    request->setSendResult(0); return true;
+  }
+};
+static char etalon[] = "791398699814xxxx";
+static char etalon_imsi[] = "25001389999xxxx";
+static char etalon_msc[] = "791398699812";
+static char sender_msc[] = "791398699815";
+class EmptySubscriberRegistrator: public SubscriberRegistrator {
+  public:
+    EmptySubscriberRegistrator(TCO* _tco) : SubscriberRegistrator(_tco) {}
+    virtual void registerSubscriber(Address& imsi, Address& msisdn, Address& mgt, int period) {}
+    virtual int  update(Address& imsi, Address& msisdn, Address& mgt) {return 1;}
+    virtual bool lookup(Address& msisdn, Address& imsi, Address& msc)
+    {
+      //msisdn must be 791398699814xxx, so imsi will be 250013899999xxx
+      if (strlen(msisdn.value) != strlen(etalon)) return false;
+      if (strncmp(msisdn.value,etalon,strlen(etalon)-4) == 0)
+      {
+        strncpy(etalon_imsi+strlen(etalon_imsi)-4,msisdn.value+strlen(msisdn.value)-4,4);
+        imsi.setValue((uint8_t)strlen(etalon_imsi),etalon_imsi);
+        if (strncmp(msisdn.value,"7913986998140099",strlen(etalon)) <= 0 )
+          msc.setValue((uint8_t) strlen(sender_msc), sender_msc);
+        else
+          msc.setValue((uint8_t) strlen(etalon_msc), etalon_msc);
+        return true;
+      }
+      return false;
+    }
+};
+class GopotaListener: public SccpProcessor, public Thread {
+  public:
+    GopotaListener(TCO* _tco, SubscriberRegistrator* _reg) : SccpProcessor(_tco,_reg) {}
+    virtual int Execute()
+    {
+      int result;
+      result = Run();
+      smsc_log_error(logger,"SccpListener exit with code: %d", result);
+      return result;
+    }
+};
+class DialogueStat: public Thread {
+  private:
+    TSMSTAT stat;
+  public:
+    virtual int Execute()
+    {
+      while (true)
+      {
+        struct timespec delay = { 0, 995000000 }; // 955ms
+        TSMSTAT statnow;
+        TSM::getCounters(statnow);
+        smsc_log_info(logger,"dlg cre/del/cnt = %d/%d/%d",
+            statnow.objcreated - stat.objcreated,
+            statnow.objdeleted - stat.objdeleted,
+            statnow.objcount);
+        stat = statnow;
+        nanosleep(&delay,0);
+      }
+      return 0;
+    }
+  };
+int main(int argc, char** argv)
+{
+  smsc::logger::Logger::Init();
+  logger = smsc::logger::Logger::getInstance("mschlr");
+
+  try
+  {
+    smsc_log_info(logger, "MSC and HLR emulator");
+    using smsc::core::threads::Thread;
+    EmptyRequestSender fakeSender;
+    TCO mtsms(10000);
+    EmptySubscriberRegistrator fakeHLR(&mtsms);
+    mtsms.setRequestSender(&fakeSender);
+    GopotaListener listener(&mtsms,&fakeHLR);
+    DialogueStat stat;
+    listener.configure(43,191,Address(".1.1.791398699812"),
+                               Address(".1.1.791398699813"),
+                               Address(".1.1.791398699814"));
+    listener.Start();
+    stat.Start();
+    int8_t invoke_id = 0;
+    int count = 0;
+    bool pri;
+    sleep(10);
+    while(true)
+    {
+      sleep(60);
+    }
+    listener.Stop();
+  } catch (std::exception& ex)
+  {
+    smsc_log_error(logger, " catched unexpected exception [%s]", ex.what());
+  }
+  return 0;
+}
