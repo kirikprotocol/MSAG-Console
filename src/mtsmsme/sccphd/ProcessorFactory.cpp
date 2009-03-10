f0d6d37311475b54d2eeb69230e2f4580fb925e3
vic <>
2009-03-10 07:48:17 +0000

    HD support

---
+++ src/mtsmsme/sccphd/ProcessorFactory.cpp
@@ -0,0 +1,32 @@
+static char const ident[] = "$Id$";
+#include "mtsmsme/processor/Processor.h"
+#include "mtsmsme/sccphd/SccpHDProcessor.hpp"
+namespace smsc {
+namespace mtsmsme {
+namespace processor {
+
+using smsc::mtsmsme::processor::RequestProcessor;
+static RequestProcessorFactory* factory = 0;
+Mutex flock;
+class RequestProcessorFactoryImpl: public RequestProcessorFactory {
+  public:
+    virtual RequestProcessor* createRequestProcessor();
+};
+RequestProcessorFactory* RequestProcessorFactory::getInstance()
+{
+  if ( factory == 0 ) {
+    MutexGuard g(flock);
+    if ( factory == 0 )
+    {
+      factory = new RequestProcessorFactoryImpl();
+    }
+  }
+  return factory;
+}
+RequestProcessor* RequestProcessorFactoryImpl::createRequestProcessor()
+{
+  return  new SccpHDProcessor();
+}
+} /* namespace processor */
+} /* namespace mtsmsme   */
+} /* namespace smsc      */
