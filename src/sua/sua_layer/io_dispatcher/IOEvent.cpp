c14818678fd415518469aa266bd20a5ace4b8303
shulga <>
2009-03-19 10:47:07 +0000

    moved to eyeline/sua

--- src/sua/sua_layer/io_dispatcher/IOEvent.cpp
+++
@@ -1,8 +0,0 @@
-#include "IOEvent.hpp"
-
-namespace io_dispatcher {
-
-IOEvent::~IOEvent() {}
-
-}
-
--- src/sua/sua_layer/lm_subsystem/lm_commands/LM_Command.cpp
+++
@@ -1,9 +0,0 @@
-#include "LM_Command.hpp"
-
-namespace lm_commands {
-
-LM_Command::LM_Command()
-  : _logger(smsc::logger::Logger::getInstance("lmsubsys"))
-{}
-
-}
