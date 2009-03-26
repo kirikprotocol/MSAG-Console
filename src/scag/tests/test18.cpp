51d1c7e39e07b18a58fe55268bf8a3f470ebd07f
bukind <>
2009-03-26 07:26:35 +0000

    more printout

--- src/scag/tests/test18.cpp
+++ src/scag/tests/test18.cpp
@@ -7,16 +7,17 @@ typedef AbntAddr Key;
 typedef int64_t  Value;
 
 struct test1 {
-    unsigned parent;
-    unsigned left;
-    unsigned right;
-    int8_t   color;
-    Key      key;
-    Value    value;
+    unsigned parent; // 4
+    unsigned left;   // 4
+    unsigned right;  // 4
+    int8_t   color;  // 1
+    Key      key;    // 24
+    Value    value;  // 8
 };
 
 int main()
 {
+    std::cout << "sizeof(key)=" << sizeof(Key) << std::endl;
     std::cout << "sizeof(test1)=" << sizeof(test1) << std::endl;
     return 0;
 }
