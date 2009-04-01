9a4e406657853489ea487924534c69c3a6a585fc
bukind <>
2009-04-01 04:35:30 +0000

    test18

--- src/scag/tests/test18.cpp
+++ src/scag/tests/test18.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include "scag/pvss/profile/AbntAddr.hpp"
+#include "util/int.h"
 
-using namespace scag2::pvss;
-
-typedef AbntAddr Key;
 typedef int64_t  Value;
 
+/*
+#include "scag/pvss/profile/AbntAddr.hpp"
+using namespace scag2::pvss;
+typedef AbntAddr Key;
 struct test1 {
     unsigned parent; // 4
     unsigned left;   // 4
@@ -14,10 +15,24 @@ struct test1 {
     Key      key;    // 24
     Value    value;  // 8
 };
+ */
+
+struct test2 {
+    uint64_t number;
+    uint8_t  typeplan;
+};
+
+struct test3 {
+    uint64_t number;
+    uint8_t  typeplan;
+    Value    value;
+};
 
 int main()
 {
-    std::cout << "sizeof(key)=" << sizeof(Key) << std::endl;
-    std::cout << "sizeof(test1)=" << sizeof(test1) << std::endl;
+    // std::cout << "sizeof(key)=" << sizeof(Key) << std::endl;
+    // std::cout << "sizeof(test1)=" << sizeof(test1) << std::endl;
+    std::cout << "sizeof(test2)=" << sizeof(test2) << std::endl;
+    std::cout << "sizeof(test3)=" << sizeof(test3) << std::endl;
     return 0;
 }
