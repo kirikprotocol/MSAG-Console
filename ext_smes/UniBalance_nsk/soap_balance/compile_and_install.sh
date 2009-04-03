f7caf4a6edbc58205f6653ef463ffb8019246181
ilyagp <>
2009-04-03 05:11:47 +0000

    *** empty log message ***

--- ext_smes/UniBalance_nsk/soap_balance/compile_and_install.sh
+++ ext_smes/UniBalance_nsk/soap_balance/compile_and_install.sh
@@ -1,4 +1,4 @@
 #mvn clean
 mvn -Dmaven.test.skip package
-rm -rf tomcat/webapps/service_webapp/*
-mv service_webapp/target/service_webapp/* tomcat/webapps/service_webapp
+rm -rf tomcat/webapps/service_webapp
+mv service_webapp/target/service_webapp tomcat/webapps/service_webapp
