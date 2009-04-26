748ef515e10ed9f457f62afd3129ec84f51b39a5
gvroman <>
2009-04-26 12:19:36 +0000

    Telephony BCD string

---
+++ src/eyeline/util/TBCDString.cpp
@@ -0,0 +1,177 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/util/TBCDString.hpp"
+
+namespace eyeline {
+namespace util {
+
+/* ************************************************************************* *
+ * class BCDString implementation
+ * ************************************************************************* */
+bool BCDString::validate(const char * str, unsigned slen)
+{
+  bool failed = slen ? false : true;
+  for (unsigned i = 0; i < slen; ++i) {
+    failed |= !validateChar(str[i]);
+  }
+  return !failed;
+}
+//
+unsigned BCDString::pack2Octs(unsigned char* bcd_buf, const char * str,
+                            unsigned slen, bool use_filler/* = true*/)
+{
+  unsigned bcdLen = countOctsLen(slen);
+
+  for (unsigned i = 0; i < slen; ++i) {
+      if (i & 0x01) // i % 2
+          bcd_buf[i/2] |= ((str[i]-'0') << 4);	// fill high semioctet
+      else
+          bcd_buf[i/2] = (str[i]-'0') & 0x0F;	// fill low semioctet
+  }
+  if ((slen % 2) && use_filler)
+      bcd_buf[bcdLen - 1] |= 0xF0;		// add filler to high semioctet
+
+  return bcdLen;
+}
+
+//Converts low semioctet of given byte to TBCD character.
+//Returns zero for non-alphabet character encoding.
+char BCDString::bcd2char(unsigned char bcd)
+{
+  bcd &= 0x0F;
+  return (bcd <= 0x09) ? '0' + bcd : 0;
+}
+
+#define BCD_UNPACK() \
+  unsigned i = 0, k = 0; \
+  for (; i < bcd_len; ++i) { \
+    /* low semioctet */ \
+    str[k] = bcd2char(bcd_buf[i]); \
+    if (!str[k++]) /* illegal char encoding */ \
+      return 0; \
+    /* high semioctet */ \
+    unsigned char sym = (bcd_buf[i] >> 4); \
+    str[k] = bcd2char(bcd_buf[i]); \
+    if (!str[k++]) { \
+      /* either filler or illegal char encoding */ \
+      return (sym == 0x0F) ? k : 0; \
+    } \
+  } \
+  str[k] = 0; return k
+
+//
+unsigned BCDString::unpackOcts(const unsigned char * bcd_buf, 
+                               unsigned bcd_len, char * str)
+{
+  BCD_UNPACK();
+}
+//
+unsigned BCDString::unpackOcts(const unsigned char * bcd_buf,
+                               unsigned bcd_len, std::string & str)
+{
+  str.clear();
+  BCD_UNPACK();
+}
+
+/* ************************************************************************* *
+ * class TBCDString implementation
+ * ************************************************************************* */
+static const char _TBCD_alphabet[] = {
+//  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '*', '#', 'a', 'b', 'c'
+};
+
+bool TBCDString::validateChar(char use_char)
+{
+  if (!BCDString::validateChar(use_char)) {
+    for (unsigned i = 0; i < sizeof(_TBCD_alphabet); ++i) {
+      if (use_char == _TBCD_alphabet[i])
+        return true;
+    }
+  }
+  return false;
+}
+
+bool TBCDString::validate(const char * str, unsigned slen)
+{
+  bool failed = slen ? false : true;
+  for (unsigned i = 0; i < slen; ++i) {
+    failed |= !validateChar(str[i]);
+  }
+  return !failed;
+}
+
+unsigned TBCDString::pack2Octs(unsigned char* bcd_buf, const char * str,
+                            unsigned slen, bool use_filler/* = true*/)
+{
+  unsigned bcdLen = countOctsLen(slen);
+
+  for (unsigned i = 0; i < slen; ++i) {
+      if (i & 0x01) // i % 2
+          bcd_buf[i/2] |= ((str[i]-'0') << 4);	// fill high semioctet
+      else
+          bcd_buf[i/2] = (str[i]-'0') & 0x0F;	// fill low semioctet
+  }
+  if ((slen % 2) && use_filler)
+      bcd_buf[bcdLen - 1] |= 0xF0;		// add filler to high semioctet
+
+  return bcdLen;
+}
+
+//Converts low semioctet of given byte to TBCD character.
+//Returns zero for non-alphabet character encoding.
+char TBCDString::tbcd2char(unsigned char bcd)
+{
+  bcd &= 0x0F;
+  if (bcd <= 0x09)
+    return '0' + bcd;
+  if (bcd == 0x0A)
+    return '*';
+  else if (bcd == 0x0B)
+    return '#';
+  else if (bcd == 0x0C)
+    return 'a';
+  else if (bcd == 0x0D)
+    return 'b';
+  else if (bcd == 0x0E)
+    return 'c';
+  return 0;
+}
+
+#define TBCD_UNPACK() \
+  unsigned i = 0, k = 0; \
+  for (; i < bcd_len; ++i) { \
+    /* low semioctet */ \
+    str[k] = tbcd2char(bcd_buf[i]); \
+    if (!str[k++]) /* illegal char encoding */ \
+      return 0; \
+    /* high semioctet */ \
+    unsigned char sym = (bcd_buf[i] >> 4); \
+    str[k] = tbcd2char(bcd_buf[i]); \
+    if (!str[k++]) { \
+      /* either filler or illegal char encoding */ \
+      return (sym == 0x0F) ? k : 0; \
+    } \
+  } \
+  str[k] = 0; return k
+
+//
+unsigned TBCDString::unpackOcts(const unsigned char * bcd_buf,
+                                unsigned bcd_len, char * str)
+{
+  TBCD_UNPACK();
+}
+//
+unsigned TBCDString::unpackOcts(const unsigned char * bcd_buf,
+                                unsigned bcd_len, std::string & str)
+{
+  str.clear();
+  TBCD_UNPACK();
+}
+
+
+} //util
+} //eyeline
+
---
+++ src/eyeline/util/TBCDString.hpp
@@ -0,0 +1,146 @@
+/* ************************************************************************** *
+ * Telephony and ordinary Binary Coded Decimal Strings.
+ * ************************************************************************** */
+#ifndef __EYELINE_UTIL_TBCD_STRING_HPP
+#ident "@(#)$Id$"
+#define __EYELINE_UTIL_TBCD_STRING_HPP
+
+#include <string>
+
+namespace eyeline {
+namespace util {
+
+// Binary Coded Decimal String is used to represent several
+// digits from 0 through 9, two digits per octet, each digit
+// encoded 0000 to 1001 (0 to 9);
+// by default, 1111 used as filler when there is an odd number
+// of digits.
+
+// bits 8765 of octet n keep encoding of digit 2n
+// bits 4321 of octet n keep encoding of digit 2(n-1)+1
+class BCDString : public std::string {
+protected:
+  //Converts low semioctet of given byte to BCD character.
+  //Returns zero for non-alphabet character encoding.
+  static char bcd2char(unsigned char bcd);
+
+public:
+  //check character for alphabet conformance
+  //returns false in case of failure
+  static bool validateChar(char use_char)
+  {
+    return ((use_char >= '0') && (use_char <= '9'));
+  }
+  //check characters for alphabet conformance
+  //returns false if illegal character is found
+  static bool validate(const char * str, unsigned slen);
+
+  static unsigned countOctsLen(unsigned str_len)
+  {
+    return (str_len + 1)/2;
+  }
+  //NOTE: it's a caller responsibility to validate characters!
+  //Returns number of bytes written into bcd buffer
+  static unsigned pack2Octs(unsigned char* bcd_buf, const char * str,
+                            unsigned slen, bool use_filler = true);
+  //Unpacks and validates input bcd buffer
+  //Returns number of characters unpacked, 0 - in case of failure.
+  static unsigned unpackOcts(const unsigned char * bcd_buf,
+                             unsigned bcd_len, char * str);
+  static unsigned unpackOcts(const unsigned char * bcd_buf,
+                             unsigned bcd_len, std::string & str);
+
+  //check characters for alphabet conformance
+  //returns false if illegal character is found
+  bool validate(void) const
+  {
+    return validate(c_str(), (unsigned)length());
+  }
+  //
+  unsigned countOctsLen(void) const
+  {
+    return countOctsLen((unsigned)length());
+  }
+  //NOTE: it's a caller responsibility to validate characters!
+  //Returns number of bytes written into bcd buffer
+  unsigned pack2Octs(unsigned char* bcd_buf, bool use_filler = true) const
+  {
+    return pack2Octs(bcd_buf, c_str(), (unsigned)length(), use_filler);
+  }
+  //Unpacks and validates input bcd buffer
+  //Returns number of characters unpacked, 0 - in case of failure.
+  unsigned unpackOcts(const unsigned char * bcd_buf, unsigned bcd_len)
+  {
+    return unpackOcts(bcd_buf, bcd_len, *this);
+  }
+};
+
+
+// Telephony Binary Coded Decimal String is used to
+// represent several digits from 0 through 9, *, #, a, b, c, two
+// digits per octet, each digit encoded 0000 to 1001 (0 to 9),
+// 1010 (*), 1011 (#), 1100 (a), 1101 (b) or 1110 (c);
+// by default, 1111 used as filler when there is an odd number
+// of digits.
+
+// bits 8765 of octet n keep encoding of character 2n
+// bits 4321 of octet n keep encoding of character 2(n-1)+1
+class TBCDString : public std::string {
+protected:
+  //Converts low semioctet of given byte to TBCD character.
+  //Returns zero for non-alphabet character encoding.
+  static char tbcd2char(unsigned char bcd);
+
+public:
+  //check character for alphabet conformance
+  //returns false in case of failure
+  static bool validateChar(char use_char);
+  //check characters for alphabet conformance
+  //returns false if illegal character is found
+  static bool validate(const char * str, unsigned slen);
+
+  static unsigned countOctsLen(unsigned str_len)
+  {
+    return (str_len + 1)/2;
+  }
+  //NOTE: it's a caller responsibility to validate characters!
+  static unsigned pack2Octs(unsigned char* bcd_buf, const char * str,
+                            unsigned slen, bool filler = true);
+  //Unpacks and validates input bcd buffer
+  //Returns number of characters unpacked, 0 - in case of failure.
+  static unsigned unpackOcts(const unsigned char * bcd, unsigned bcd_len,
+                             char * str);
+  static unsigned unpackOcts(const unsigned char * bcd_buf,
+                             unsigned bcd_len, std::string & str);
+
+
+  unsigned countOctsLen(void) const
+  {
+    return countOctsLen((unsigned)length());
+  }
+
+  //check characters for alphabet conformance
+  //returns false if illegal character is found
+  bool validate(void) const
+  {
+    return validate(c_str(), (unsigned)length());
+  }
+  //NOTE: it's a caller responsibility to validate characters!
+  //Returns number of bytes written into bcd buffer
+  unsigned pack2Octs(unsigned char* bcd_buf, bool use_filler = true) const
+  {
+    return pack2Octs(bcd_buf, c_str(), (unsigned)length(), use_filler);
+  }
+  //Unpacks and validates input bcd buffer
+  //Returns number of characters unpacked, 0 - in case of failure.
+  unsigned unpackOcts(const unsigned char * bcd_buf, unsigned bcd_len)
+  {
+    return unpackOcts(bcd_buf, bcd_len, *this);
+  }
+};
+
+} //util
+} //eyeline
+
+#endif /* __EYELINE_UTIL_Oct_STRING_HPP */
+
