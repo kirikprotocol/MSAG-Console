91d820c63d8d46b8ffbb4c4e5eb3e14892001781
gvroman <>
2009-04-30 08:27:45 +0000

    minor enhancement in print()

--- src/inman/common/CSVList.hpp
+++ src/inman/common/CSVList.hpp
@@ -86,22 +86,24 @@ public:
         return size();
     }
 
-    int print(std::string & ostr) const
+    int print(std::string & ostr, bool ins_space = true) const
     {
         int i = 0;
         char dlmStr[3];
 
-        dlmStr[0] = _dlm; dlmStr[1] = ' '; dlmStr[2] = 0;
+        dlmStr[0] = _dlm;
+        dlmStr[1] = ins_space ? ' ' : 0;
+        dlmStr[2] = 0;
         CSVList::const_iterator it = CSVList::begin();
         for (; it != CSVList::end(); ++it, ++i)
             format(ostr, "%s%s", i ? dlmStr : "", it->c_str());
         return i;
     }
 
-    std::string print(void) const
+    std::string print(bool ins_space = true) const
     {
         std::string elems;
-        print(elems);
+        print(elems, ins_space);
         return elems;
     }
 };
