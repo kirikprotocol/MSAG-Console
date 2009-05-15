a801d1c6fa3d49d0077c4273ec5ba051201734d3
shulga <>
2009-05-15 09:58:59 +0000

    removed inheritance from TDlgManagerIface for TCAPIndicationsProcessor class

--- src/eyeline/tcap/provd/TCAPIndicationsProcessor.hpp
+++ src/eyeline/tcap/provd/TCAPIndicationsProcessor.hpp
@@ -3,13 +3,14 @@
 # define __EYELINE_TCAP_PROVD_TCAPINDICATIONSPROCESSOR_HPP__
 
 # include "eyeline/tcap/TDialogueHandlingPrimitive.hpp"
+# include "eyeline/tcap/TDialogueIndicationPrimitives.hpp"
 # include "eyeline/tcap/proto/TransactionId.hpp"
 
 namespace eyeline {
 namespace tcap {
 namespace provd {
 
-class TCAPIndicationsProcessor/* : public TDlgManagerIface*/ {
+class TCAPIndicationsProcessor {
 public:
   virtual ~TCAPIndicationsProcessor() {}
   virtual bool updateDialogue(TC_Begin_Ind& tcBeginInd, unsigned int srcLinkNum);
@@ -17,12 +18,8 @@ public:
   virtual bool updateDialogue(TC_End_Ind& tcEndInd, unsigned int srcLinkNum);
   virtual bool updateDialogue(TC_PAbort_Ind& tcPAbortInd, unsigned int srcLinkNum);
   virtual bool updateDialogue(TC_UAbort_Ind& tcUAbortInd, unsigned int srcLinkNum);
-  //  virtual bool updateDialogue(TC_Notice_Ind& tcNoticeInd);
-  virtual bool updateDialogue(TC_L_Cancel_Ind& lCancelInd, unsigned int srcLinkNum);
-  virtual bool updateDialogue(TC_L_Reject_Ind& lRejectInd, unsigned int srcLinkNum);
-protected:
-  void formPAbortRequest(const proto::TransactionId& trnId,
-                         TDialogueHandlingPrimitive::PAbortCause_e pAbortCause);
+
+  virtual bool updateDialogue(TC_Notice_Ind& tcNoticeInd, unsigned int srcLinkNum);
 };
 
 }}}
