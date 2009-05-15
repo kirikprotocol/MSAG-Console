5d67a76fb9bf2c5209f4f400dc3ee227e77e678d
shulga <>
2009-05-15 10:00:35 +0000

    added overloaded updateDialogue methods for TC_L_Cancel_Ind and TC_L_Reject_Ind types

--- src/eyeline/tcap/TDlgHandlerIface.hpp
+++ src/eyeline/tcap/TDlgHandlerIface.hpp
@@ -3,6 +3,7 @@
 # define __EYELINE_TCAP_TDLGHANDLERIFACE_HPP__
 
 # include "eyeline/tcap/TDialogueIndicationPrimitives.hpp"
+# include "eyeline/tcap/TComponentIndicationPrimitives.hpp"
 
 namespace eyeline {
 namespace tcap {
@@ -15,6 +16,10 @@ public:
   virtual bool updateDialogue(TC_PAbort_Ind & use_tmsg) = 0;
   virtual bool updateDialogue(TC_UAbort_Ind & use_tmsg) = 0;
   virtual bool updateDialogue(TC_Notice_Ind & use_tmsg) = 0;
+
+  virtual bool updateDialogue(TC_L_Cancel_Ind & indPrimitive) = 0;
+  virtual bool updateDialogue(TC_L_Reject_Ind & indPrimitive) = 0;
+
 };
 
 } //tcap
