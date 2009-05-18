939fdef8ff34863f0512bc8168a3eaeae0f5d61b
bukind <>
2009-05-18 09:45:17 +0000

    bugfix: perscall sigsegved when receiving ErrorResponse instead of ProfileResponse

--- src/scag/re/actions/impl/PersCall.cpp
+++ src/scag/re/actions/impl/PersCall.cpp
@@ -1,10 +1,28 @@
 #include "PersCall.h"
 #include "scag/lcm/base/LongCallManager2.h"
+#include "scag/pvss/api/packets/ErrorResponse.h"
 
 using namespace scag2::pvss;
 
 
 namespace {
+
+struct ERVisitor : public ResponseVisitor
+{
+    virtual bool visitErrResponse( ErrorResponse& resp ) { return true; }
+    virtual bool visitPingResponse( PingResponse& resp ) { return false; }
+    virtual bool visitAuthResponse( AuthResponse& resp )       { return false; }
+    virtual bool visitProfileResponse( ProfileResponse& resp ) { return false; }
+};
+struct PRVisitor : public ResponseVisitor
+{
+    virtual bool visitErrResponse( ErrorResponse& resp ) { return false; }
+    virtual bool visitPingResponse( PingResponse& resp ) { return false; }
+    virtual bool visitAuthResponse( AuthResponse& resp )       { return false; }
+    virtual bool visitProfileResponse( ProfileResponse& resp ) { return true; }
+};
+
+
 bool loggerInited = false;
 smsc::core::synchronization::Mutex logmutex;
 smsc::logger::Logger* thelogger;
@@ -31,10 +49,20 @@ exc_(std::string(),PvssException::OK), req_(req) {}
 void PersCall::handleResponse( std::auto_ptr< Request > request, std::auto_ptr< Response > response )
 {
     assert( lcc_ );
-    req_.reset( static_cast< ProfileRequest* >(request.release()) );
-    resp_.reset( static_cast< ProfileResponse* >(response.release()));
-    // continue lcm execution
-    lcc_->initiator->continueExecution( lcc_, false );
+    ERVisitor erv;
+    if ( response->visit( erv ) ) {
+        // error
+        PvssException exc( static_cast<const ErrorResponse*>(response.get())->getStatusMessage(),
+                           PvssException::Type(response->getStatus()) );
+        handleError(exc,request);
+    } else {
+        PRVisitor prv;
+        assert( response->visit(prv) );
+        req_.reset( static_cast< ProfileRequest* >(request.release()) );
+        resp_.reset( static_cast< ProfileResponse* >(response.release()));
+        // continue lcm execution
+        lcc_->initiator->continueExecution( lcc_, false );
+    }
     // NOTE: we cannot clear req and resp here, as another thread may already destroy perscall
     // req_.reset(0);
     // resp_.reset(0);
