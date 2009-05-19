2688cb8f428890edd73b9d170d25d2d90903a524
shulga <>
2009-05-19 03:30:13 +0000

    default constructor was added; _ownAddress data member of sccp::SCCPAddress datatype was added

--- src/eyeline/tcap/provd/TCAPIndicationsProcessor.hpp
+++ src/eyeline/tcap/provd/TCAPIndicationsProcessor.hpp
@@ -2,6 +2,7 @@
 # ident "@(#)$Id$"
 # define __EYELINE_TCAP_PROVD_TCAPINDICATIONSPROCESSOR_HPP__
 
+# include "eyeline/sccp/SCCPAddress.hpp"
 # include "eyeline/tcap/TDialogueHandlingPrimitive.hpp"
 # include "eyeline/tcap/TDialogueIndicationPrimitives.hpp"
 # include "eyeline/tcap/proto/TransactionId.hpp"
@@ -12,6 +13,7 @@ namespace provd {
 
 class TCAPIndicationsProcessor {
 public:
+  TCAPIndicationsProcessor();
   virtual ~TCAPIndicationsProcessor() {}
   virtual bool updateDialogue(TC_Begin_Ind& tcBeginInd, unsigned int srcLinkNum);
   virtual bool updateDialogue(TC_Cont_Ind& tcContInd, unsigned int srcLinkNum);
@@ -20,6 +22,8 @@ public:
   virtual bool updateDialogue(TC_UAbort_Ind& tcUAbortInd, unsigned int srcLinkNum);
 
   virtual bool updateDialogue(TC_Notice_Ind& tcNoticeInd, unsigned int srcLinkNum);
+private:
+  sccp::SCCPAddress _ownAddress;
 };
 
 }}}
