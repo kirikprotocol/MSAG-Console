89895820789ac53b78b8c40b1eb9661ec1901253
shulga <>
2009-05-19 03:16:40 +0000

    setOrigAddress, getOrigAddress, setDestAddress, getDestAddress methods was removed

--- src/eyeline/tcap/TDialogueHandlingPrimitive.hpp
+++ src/eyeline/tcap/TDialogueHandlingPrimitive.hpp
@@ -5,7 +5,6 @@
 # ident "@(#)$Id$"
 # define __EYELINE_TCAP_TDIALOGUEHANDLINGPRIMITIVE_HPP__
 
-# include "eyeline/sccp/SCCPAddress.hpp"
 # include "eyeline/tcap/TDialogueId.hpp"
 # include "eyeline/tcap/proto/TCAPMessage.hpp"
 # include "eyeline/tcap/proto/TransactionId.hpp"
@@ -14,18 +13,16 @@
 namespace eyeline {
 namespace tcap {
 
-using eyeline::sccp::SCCPAddress;
 using eyeline::tcap::proto::TCAPMessage;
 using eyeline::ros::ROSComponentsList;
-
 
 //Base class for all TCAP Dialogue handling primitives
 class TDialogueHandlingPrimitive {
 protected:
   TDialogueId   _dlgId;
   TCAPMessage & _msgTC;
-  SCCPAddress   _orgAdr;
-  SCCPAddress   _dstAdr;
+  //  SCCPAddress   _orgAdr;
+  //  SCCPAddress   _dstAdr;
 
 public:
   enum PAbortCause_e {
@@ -48,12 +45,6 @@ public:
 
   void setDialogueId(TDialogueId use_id) { _dlgId = use_id; }
   TDialogueId getDialogueId(void) const { return _dlgId; }
-
-  void setOrigAddress(const SCCPAddress & use_adr)  { _orgAdr = use_adr; }
-  const SCCPAddress & getOrigAddress(void) const    { return _orgAdr; }
-
-  void setDestAddress(const SCCPAddress & use_adr)  { _dstAdr = use_adr; }
-  const SCCPAddress & getDestAddress(void) const    { return _dstAdr; }
 
   void setTransactionId(const proto::TransactionId & use_id)
   {
