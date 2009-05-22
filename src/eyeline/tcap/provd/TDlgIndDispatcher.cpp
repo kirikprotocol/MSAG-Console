908cd33898bfdfa801f9d4e1fa5a69799e1aca58
shulga <>
2009-05-22 11:57:38 +0000

    at convertSuaApiError2TNoticeCause: added namespace prefix to argument type

--- src/eyeline/tcap/provd/TDlgIndDispatcher.cpp
+++ src/eyeline/tcap/provd/TDlgIndDispatcher.cpp
@@ -19,7 +19,7 @@ typedef eyeline::asn1::ASTypeAC::DECResult DECResult;
 
 
 TC_Notice_Ind::ReportCause_e 
-  convertSuaApiError2TNoticeCause(SuaApi::ErrorCode_e sua_rc)
+convertSuaApiError2TNoticeCause(sua::libsua::SuaApi::ErrorCode_e sua_rc)
 {
   //TODO: 
   return eyeline::tcap::TC_Notice_Ind::errSCCPFailure;
