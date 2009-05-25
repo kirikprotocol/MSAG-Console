6c4aa63aa84af6b73afc587359d681b06d1963f0
odarchenko <>
2009-05-25 08:55:51 +0000

    upload operators providers, 135 & 140 services

--- src/scag/pers/upload/uploadIntStorage.cpp
+++ src/scag/pers/upload/uploadIntStorage.cpp
@@ -199,6 +199,9 @@ void extractPageFile( smsc::logger::Logger* logger, PageFile& pf,
             }
             ++readProfiles;
             if (prof.getProperties().GetCount() > 0) {
+              if (type == PT_SERVICE && pkey != 135 && pkey != 140) {
+                continue;
+              }
               ++notEmptyProfiles;
             }
             if (send) {
@@ -211,7 +214,11 @@ void extractPageFile( smsc::logger::Logger* logger, PageFile& pf,
     } catch ( std::exception& e ) {
         smsc_log_error(logger,"exception: %s", e.what());
     }
-    smsc_log_info(logger,"read profiles: %d, not empty profiles: %d, uploaded profiles: %d", readProfiles, notEmptyProfiles, uploadProfiles);
+    if (type == PT_SERVICE) {
+      smsc_log_info(logger,"read profiles: %d, matched profiles: %d, uploaded profiles: %d", readProfiles, notEmptyProfiles, uploadProfiles);
+    } else {
+      smsc_log_info(logger,"read profiles: %d, not empty profiles: %d, uploaded profiles: %d", readProfiles, notEmptyProfiles, uploadProfiles);
+    }
 }
 
 
