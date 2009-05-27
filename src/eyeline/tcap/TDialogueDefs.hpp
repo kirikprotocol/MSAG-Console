5f54324c5d57f1dfab346b5876b08bcc0f2b4268
gvroman <>
2009-05-27 06:31:43 +0000

    TCUserAssociateResult is added

--- src/eyeline/tcap/TDialogueDefs.hpp
+++ src/eyeline/tcap/TDialogueDefs.hpp
@@ -23,7 +23,7 @@ struct PAbort {
 };
 
 struct TDialogueAssociate {
-  enum Result_e { accepted = 0, reject_permanent = 1 };
+  enum Result_e { dlg_accepted = 0, dlg_reject_permanent = 1 };
 
   enum DiagnosticUser_e {
     dsu_null = 0, dsu_no_reason_given = 1, dsu_ac_not_supported = 2
@@ -31,6 +31,16 @@ struct TDialogueAssociate {
   enum DiagnosticProvider_e {
     dsp_null = 0, dsp_no_reason_given = 1, dsp_no_common_dialogue_portion = 2
   };
+};
+
+struct TCUserAssociateResult {
+  bool  _accepted;
+  TDialogueAssociate::DiagnosticUser_e  _rejCause;
+
+  TCUserAssociateResult(bool usr_accepted = true,
+                       TDialogueAssociate::DiagnosticUser_e rej_cause = TDialogueAssociate::dsu_null)
+    : _accepted(usr_accepted), _rejCause(rej_cause)
+  { }
 };
 
 } //tcap
