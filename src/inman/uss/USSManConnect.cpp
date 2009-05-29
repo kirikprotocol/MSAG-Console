6a09226115c27ff000a73d01b0947bcc7c0b3e7f
shulga <>
2009-05-29 08:03:48 +0000

    getRealConnect and markReqProcessorAsCompleted methods was added; added storing of active request processors using connection presented by this object

--- src/inman/uss/USSManConnect.cpp
+++ src/inman/uss/USSManConnect.cpp
@@ -16,7 +16,7 @@ namespace uss {
 bool
 DuplicateRequestChecker::isRequestRegistered(const USSProcSearchCrit& ussProcSearchCrit) const
 {
-  smsc::core::synchronization::MutexGuard mg(_lock);
+  core::synchronization::MutexGuard mg(_lock);
   if ( _registeredRequests.find(ussProcSearchCrit) == _registeredRequests.end() ) return false;
   else return true;
 }
@@ -24,19 +24,19 @@ DuplicateRequestChecker::isRequestRegistered(const USSProcSearchCrit& ussProcSea
 void
 DuplicateRequestChecker::registerRequest(const USSProcSearchCrit& ussProcSearchCrit)
 {
-  smsc::core::synchronization::MutexGuard mg(_lock);
+  core::synchronization::MutexGuard mg(_lock);
   _registeredRequests.insert(ussProcSearchCrit);
 }
 
 void
 DuplicateRequestChecker::unregisterRequest(const USSProcSearchCrit& ussProcSearchCrit)
 {
-  smsc::core::synchronization::MutexGuard mg(_lock);
+  core::synchronization::MutexGuard mg(_lock);
   _registeredRequests.erase(ussProcSearchCrit);
 
 }
 
-USSManConnect::USSManConnect(unsigned conn_id, smsc::logger::Logger* logger,
+USSManConnect::USSManConnect(unsigned conn_id, logger::Logger* logger,
                                      const UssService_CFG & cfg)
   : _logger(logger), _cfg(cfg)
 {
@@ -45,23 +45,22 @@ USSManConnect::USSManConnect(unsigned conn_id, smsc::logger::Logger* logger,
 
 USSManConnect::~USSManConnect()
 {
-  //for (CreatedSearchCritList_t::iterator begin_iter=_searchCritForCreatedReqProcessors.begin(), end_iter=_searchCritForCreatedReqProcessors.end();
-  //       begin_iter != end_iter; ++begin_iter) {
-  //USSRequestProcessor* ussReqProc = 
-  //    smsc::util::RefObjectRegistry<USSRequestProcessor,USSProcSearchCrit>::getInstance().toUnregisterObject(*begin_iter);
-  //delete ussReqProc;
-  //  }
+  MutexGuard synchronize(_activeReqProcLock);
+  while(!_activeReqProcessors.empty()) {
+    active_req_processes_t::iterator iter = _activeReqProcessors.begin();
+    iter->markConnectAsClosed();
+    _activeReqProcessors.erase(iter)
+  }
 }
 
-//##ModelId=4575350D008E
-void USSManConnect::onPacketReceived(smsc::inman::interaction::Connect* conn, // указатель на объект, обслуживающий соединение с клиентом ussman'a
-                                         std::auto_ptr<smsc::inman::interaction::SerializablePacketAC>& recv_cmd) throw(std::exception)
+void USSManConnect::onPacketReceived(interaction::Connect* conn, // указатель на объект, обслуживающий соединение с клиентом ussman'a
+                                     std::auto_ptr<interaction::SerializablePacketAC>& recv_cmd)
 {
-  smsc::inman::interaction::USSPacketAC* requestPacket =
-    static_cast<smsc::inman::interaction::USSPacketAC*>(recv_cmd.get());
+  interaction::USSPacketAC* requestPacket =
+    static_cast<interaction::USSPacketAC*>(recv_cmd.get());
 
-  smsc::inman::interaction::USSRequestMessage* requestObject = 
-    static_cast<smsc::inman::interaction::USSRequestMessage*>(requestPacket->pCmd());
+  interaction::USSRequestMessage* requestObject =
+    static_cast<interaction::USSRequestMessage*>(requestPacket->pCmd());
   smsc_log_info(_logger, "%s: received request=%s", _logId,
                  requestObject->toString().c_str());
 
@@ -76,16 +75,27 @@ void USSManConnect::onPacketReceived(smsc::inman::interaction::Connect* conn, //
     return;
   }
   DuplicateRequestChecker::getInstance().registerRequest(ussProcSearchCrit);
-  USSRequestProcessor* ussReqProc = new USSRequestProcessor(conn, _cfg,
-                                        requestPacket->dialogId(), ussProcSearchCrit, _logger);
+  USSRequestProcessor* ussReqProc =
+    new USSRequestProcessor(this, conn, _cfg,
+                            requestPacket->dialogId(), ussProcSearchCrit, _logger);
+  {
+    MutexGuard synchronize(_activeReqProcLock);
+    _activeReqProcessors.insert(ussReqProc);
+  }
+
   ussReqProc->handleRequest(requestObject);
 }
 
-//##ModelId=45753514006F
-void USSManConnect::onConnectError(smsc::inman::interaction::Connect* conn, std::auto_ptr<CustomException>& p_exc)
+void USSManConnect::onConnectError(interaction::Connect* conn, std::auto_ptr<CustomException>& p_exc)
 {
   smsc_log_debug(_logger, "%s: connect error %s", _logId,
                  p_exc.get() ? p_exc->what() : "unspecified");
+}
+
+void USSManConnect::markReqProcessorAsCompleted(USSRequestProcessor* ussReqProc)
+{
+  MutexGuard synchronize(_activeReqProcLock);
+  _activeReqProcessors.erase(ussReqProc);
 }
 
 }
