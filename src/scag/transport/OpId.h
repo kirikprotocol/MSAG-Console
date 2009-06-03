864ef1c6196f97f1620a4dea0972f78ce7b724b1
bukind <>
2009-06-03 04:58:01 +0000

    operation ids

---
+++ src/scag/transport/OpId.h
@@ -0,0 +1,22 @@
+#ifndef SCAG_TRANSPORT_OPID_H
+#define SCAG_TRANSPORT_OPID_H
+
+#include "util/int.h"
+
+namespace scag2 {
+namespace transport {
+
+typedef uint32_t opid_type;
+inline opid_type invalidOpId() { return opid_type(0); }
+
+}
+}
+
+namespace scag {
+namespace transport {
+typedef scag2::transport::opid_type opid_type;
+using scag2::transport::invalidOpId;
+}
+}
+
+#endif /* !SCAG_TRANSPORT_OPID_H */
