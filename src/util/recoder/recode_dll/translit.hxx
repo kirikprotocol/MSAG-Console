9286bb3c9cf4b451d38b09e302a8aa2e63fb352e
skv <>
2009-06-19 06:15:04 +0000

    some transliteration fixes to sync with MTC docs

--- src/util/recoder/recode_dll/translit.hxx
+++ src/util/recoder/recode_dll/translit.hxx
@@ -167,7 +167,7 @@ const unsigned char* c2t_Cp1251_table[] = {
 (const unsigned char*)"",
 (const unsigned char*)"",
 (const unsigned char*)"",
-(const unsigned char*)"*JO\0Jo", //¨ 168
+(const unsigned char*)"*JO\0Jo", // 168
 (const unsigned char*)"",
 (const unsigned char*)"",
 (const unsigned char*)"",
@@ -183,77 +183,77 @@ const unsigned char* c2t_Cp1251_table[] = {
 (const unsigned char*)"",
 (const unsigned char*)"",
 (const unsigned char*)"",
-(const unsigned char*)"jo", //¸ 184
-(const unsigned char*)"#",  //¹ 185
+(const unsigned char*)"jo", // 184
+(const unsigned char*)"#",  // 185
 (const unsigned char*)"",
 (const unsigned char*)"",
 (const unsigned char*)"",
 (const unsigned char*)"",
 (const unsigned char*)"",
 (const unsigned char*)"",
-(const unsigned char*)"A",//À 192
-(const unsigned char*)"B",//Á 193
-(const unsigned char*)"V",//Â 194
-(const unsigned char*)"G",//Ã 195
-(const unsigned char*)"D",//Ä 196
-(const unsigned char*)"E",//Å 197
-(const unsigned char*)"*ZH\0Zh",//Æ 198
-(const unsigned char*)"Z",//Ç 199
-(const unsigned char*)"I",//È 200
-(const unsigned char*)"J",//É 201
-(const unsigned char*)"K",//Ê 202
-(const unsigned char*)"L",//Ë 203
-(const unsigned char*)"M",//Ì 204
-(const unsigned char*)"N",//Í 205
-(const unsigned char*)"O",//Î 206
-(const unsigned char*)"P",//Ï 207
-(const unsigned char*)"R",//Ð 208
-(const unsigned char*)"S",//Ñ 209
-(const unsigned char*)"T",//Ò 210
-(const unsigned char*)"U",//Ó 211
-(const unsigned char*)"F",//Ô 212
-(const unsigned char*)"H",//Õ 213
-(const unsigned char*)"C",//Ö 214
-(const unsigned char*)"*CH\0Ch",//× 215
-(const unsigned char*)"*SH\0Sh",//Ø 216
-(const unsigned char*)"*SCH\0Sch",//Ù 217
-(const unsigned char*)"'",//Ú 218
-(const unsigned char*)"Y",//Û 219
-(const unsigned char*)"'",//Ü 220
-(const unsigned char*)"E",//Ý 221
-(const unsigned char*)"*JU\0Ju",//Þ 222
-(const unsigned char*)"*JA\0Ja",//ß 223
-(const unsigned char*)"a",//à 224
-(const unsigned char*)"b",//á 225
-(const unsigned char*)"v",//â 226
-(const unsigned char*)"g",//ã 227
-(const unsigned char*)"d",//ä 228
-(const unsigned char*)"e",//å 229
-(const unsigned char*)"zh",//æ 230
-(const unsigned char*)"z",//ç 231
-(const unsigned char*)"i",//è 232
-(const unsigned char*)"j",//é 233
-(const unsigned char*)"k",//ê 234
-(const unsigned char*)"l",//ë 235
-(const unsigned char*)"m",//ì 236
-(const unsigned char*)"n",//í 237
-(const unsigned char*)"o",//î 238
-(const unsigned char*)"p",//ï 239
-(const unsigned char*)"r",//ð 240
-(const unsigned char*)"s",//ñ 241
-(const unsigned char*)"t",//ò 242
-(const unsigned char*)"u",//ó 243
-(const unsigned char*)"f",//ô 244
-(const unsigned char*)"h",//õ 245
-(const unsigned char*)"c",//ö 246
-(const unsigned char*)"ch",//÷ 247
-(const unsigned char*)"sh",//ø 248
-(const unsigned char*)"sch",//ù 249
-(const unsigned char*)"'",//ú 250
-(const unsigned char*)"y",//û 251
-(const unsigned char*)"'",//ü 252
-(const unsigned char*)"e",//ý 253
-(const unsigned char*)"ju",//þ 254
-(const unsigned char*)"ja"//ÿ
+(const unsigned char*)"A",// 192
+(const unsigned char*)"B",// 193
+(const unsigned char*)"V",// 194
+(const unsigned char*)"G",// 195
+(const unsigned char*)"D",// 196
+(const unsigned char*)"E",// 197
+(const unsigned char*)"*ZH\0Zh",// 198
+(const unsigned char*)"Z",// 199
+(const unsigned char*)"I",// 200
+(const unsigned char*)"J",// 201
+(const unsigned char*)"K",// 202
+(const unsigned char*)"L",// 203
+(const unsigned char*)"M",// 204
+(const unsigned char*)"N",// 205
+(const unsigned char*)"O",// 206
+(const unsigned char*)"P",// 207
+(const unsigned char*)"R",// 208
+(const unsigned char*)"S",// 209
+(const unsigned char*)"T",// 210
+(const unsigned char*)"U",// 211
+(const unsigned char*)"F",// 212
+(const unsigned char*)"H",// 213
+(const unsigned char*)"C",// 214
+(const unsigned char*)"*CH\0Ch",// 215
+(const unsigned char*)"*SH\0Sh",// 216
+(const unsigned char*)"*SCH\0Sch",// 217
+(const unsigned char*)"\"",// 218
+(const unsigned char*)"Y",// 219
+(const unsigned char*)"'",// 220
+(const unsigned char*)"E",// 221
+(const unsigned char*)"*JU\0Ju",// 222
+(const unsigned char*)"*JA\0Ja",// 223
+(const unsigned char*)"a",// 224
+(const unsigned char*)"b",// 225
+(const unsigned char*)"v",// 226
+(const unsigned char*)"g",// 227
+(const unsigned char*)"d",// 228
+(const unsigned char*)"e",// 229
+(const unsigned char*)"zh",// 230
+(const unsigned char*)"z",// 231
+(const unsigned char*)"i",// 232
+(const unsigned char*)"j",// 233
+(const unsigned char*)"k",// 234
+(const unsigned char*)"l",// 235
+(const unsigned char*)"m",// 236
+(const unsigned char*)"n",// 237
+(const unsigned char*)"o",// 238
+(const unsigned char*)"p",// 239
+(const unsigned char*)"r",// 240
+(const unsigned char*)"s",// 241
+(const unsigned char*)"t",// 242
+(const unsigned char*)"u",// 243
+(const unsigned char*)"f",// 244
+(const unsigned char*)"h",// 245
+(const unsigned char*)"c",// 246
+(const unsigned char*)"ch",// 247
+(const unsigned char*)"sh",// 248
+(const unsigned char*)"sch",// 249
+(const unsigned char*)"\"",// 250
+(const unsigned char*)"y",// 251
+(const unsigned char*)"'",// 252
+(const unsigned char*)"e",// 253
+(const unsigned char*)"ju",// 254
+(const unsigned char*)"ja"//
 };
 
