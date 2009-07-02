acef808b3260e3c29ce47bca12f1e5eca0a1c93f
shulga <>
2009-07-02 09:59:22 +0000

    new version 0.7.1 - fixed bug in using of already destroyed connection

--- src/inman/uss/ussversion.hpp
+++ src/inman/uss/ussversion.hpp
@@ -4,7 +4,7 @@
 
 const unsigned USSMAN_VER_HIGH    = 0;    //major verison
 const unsigned USSMAN_VER_LOW     = 7;    //minor version
-const unsigned USSMAN_VER_FIX     = 0;    //bug fix version
+const unsigned USSMAN_VER_FIX     = 1;    //bug fix version
 
 #endif /* __SMSC_USSMAN_VERSION__ */
 
