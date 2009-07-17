4f84329db39c1362f8a83acb3b378e3ff16cc005
shulga <>
2009-07-17 05:50:22 +0000

    when can't send multipart message to smsc then make negative response to sme

--- src/eyeline/load_balancer/protocols/smpp/ApplicationMessageHandlers.cpp
+++ src/eyeline/load_balancer/protocols/smpp/ApplicationMessageHandlers.cpp
@@ -1,5 +1,7 @@
+#include <memory>
+
 #include "ApplicationMessageHandlers.hpp"
-#include "eyeline/load_balancer/io_subsystem/SwitchingTable.hpp"
+#include "Exception.hpp"
 
 namespace eyeline {
 namespace load_balancer {
@@ -11,12 +13,19 @@ ApplicationMessageHandlers::handle(const SubmitMulti& message,
                                    const io_subsystem::LinkId& src_link_id,
                                    io_subsystem::IOProcessor& io_processor)
 {
-  if ( message.isSetSarMsgRefNum() &&
-       message.isSetSarSegmentSeqNum() &&
-       message.isSetSarTotalSegments() )
-    forwardMultipartMessageToSmsc(message, src_link_id, io_processor);
-  else
-    forwardMessageToSmsc(message, src_link_id, io_processor);
+  try {
+    if ( message.isSetSarMsgRefNum() &&
+        message.isSetSarSegmentSeqNum() &&
+        message.isSetSarTotalSegments() )
+      forwardMultipartMessageToSmsc(message, src_link_id, io_processor);
+    else
+      forwardMessageToSmsc(message, src_link_id, io_processor);
+  } catch (InvalidMessageParamException& ex) {
+
+    //
+  } catch (...) {
+    // ESME_RSUBMITFAIL
+  }
 }
 
 void
@@ -32,12 +41,25 @@ ApplicationMessageHandlers::handle(const SubmitSm& message,
                                    const io_subsystem::LinkId& src_link_id,
                                    io_subsystem::IOProcessor& io_processor)
 {
-  if ( message.isSetSarMsgRefNum() &&
-       message.isSetSarSegmentSeqNum() &&
-       message.isSetSarTotalSegments() )
-    forwardMultipartMessageToSmsc(message, src_link_id, io_processor);
-  else
-    forwardMessageToSmsc(message, src_link_id, io_processor);
+  std::auto_ptr<SubmitSmResp> responseMessage;
+  try {
+    if ( message.isSetSarMsgRefNum() &&
+        message.isSetSarSegmentSeqNum() &&
+        message.isSetSarTotalSegments() )
+      forwardMultipartMessageToSmsc(message, src_link_id, io_processor);
+    else
+      forwardMessageToSmsc(message, src_link_id, io_processor);
+  } catch (InvalidMessageParamException& ex) {
+    smsc_log_error(_logger, "ApplicationMessageHandlers::handle::: caught InvalidMessageParamException=[%s], can't handle SubmitSm message",
+                   ex.what());
+    responseMessage.reset(message.makeResponse(ESME_RINVOPTPARAMVAL));
+  } catch (std::exception& ex) {
+    smsc_log_error(_logger, "ApplicationMessageHandlers::handle::: caught exception=[%s], can't handle SubmitSm message",
+                   ex.what());
+    responseMessage.reset(message.makeResponse(ESME_RSUBMITFAIL));
+  }
+  if ( responseMessage.get() )
+    io_processor.getLink(src_link_id)->send(*responseMessage);
 }
 
 void
@@ -70,17 +92,30 @@ ApplicationMessageHandlers::handle(const DataSm& message,
                                    const io_subsystem::LinkId& src_link_id,
                                    io_subsystem::IOProcessor& io_processor)
 {
-  io_subsystem::LinkId linkSetId, emptyLinkId;
-  linkSetId = io_processor.getLinkSetIdOwnerForThisLink(src_link_id);
-  if ( linkSetId == emptyLinkId ) {
-    if ( message.isSetSarMsgRefNum() &&
-         message.isSetSarSegmentSeqNum() &&
-         message.isSetSarTotalSegments() )
-      forwardMultipartMessageToSmsc(message, src_link_id, io_processor);
-    else
-      forwardMessageToSmsc(message, src_link_id, io_processor);
-  } else
-    forwardMessageToSme(message, src_link_id, io_processor);
+  std::auto_ptr<DataSmResp> responseMessage;
+  try {
+    io_subsystem::LinkId linkSetId, emptyLinkId;
+    linkSetId = io_processor.getLinkSetIdOwnerForThisLink(src_link_id);
+    if ( linkSetId == emptyLinkId ) {
+      if ( message.isSetSarMsgRefNum() &&
+          message.isSetSarSegmentSeqNum() &&
+          message.isSetSarTotalSegments() )
+        forwardMultipartMessageToSmsc(message, src_link_id, io_processor);
+      else
+        forwardMessageToSmsc(message, src_link_id, io_processor);
+    } else
+      forwardMessageToSme(message, src_link_id, io_processor);
+  } catch (InvalidMessageParamException& ex) {
+    smsc_log_error(_logger, "ApplicationMessageHandlers::handle::: caught InvalidMessageParamException=[%s], can't handle DataSm message",
+                   ex.what());
+    responseMessage.reset(message.makeResponse(ESME_RINVOPTPARAMVAL));
+  } catch (std::exception& ex) {
+    smsc_log_error(_logger, "ApplicationMessageHandlers::handle::: caught exception=[%s], can't handle DataSm message",
+                   ex.what());
+    responseMessage.reset(message.makeResponse(ESME_RSYSERR));
+  }
+  if ( responseMessage.get() )
+    io_processor.getLink(src_link_id)->send(*responseMessage);
 }
 
 void
