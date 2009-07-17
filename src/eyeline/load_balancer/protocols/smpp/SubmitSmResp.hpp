4d1277399ee3afe4c02c303a8276a37b9d6ed0f4
shulga <>
2009-07-17 05:59:53 +0000

    changed inheritance hierarchy - now class SubmitSmResp inherited from SMPPMessage (formely from FastParsableSmppMessage)

--- src/eyeline/load_balancer/protocols/smpp/SubmitSmResp.hpp
+++ src/eyeline/load_balancer/protocols/smpp/SubmitSmResp.hpp
@@ -1,26 +1,32 @@
 #ifndef __EYELINE_LOADBALANCER_PROTOCOLS_SMPP_SUBMITSMRESP_HPP__
 # define __EYELINE_LOADBALANCER_PROTOCOLS_SMPP_SUBMITSMRESP_HPP__
 
-# include "eyeline/utilx/Exception.hpp"
-# include "eyeline/load_balancer/protocols/smpp/FastParsableSmppMessage.hpp"
+# include "eyeline/load_balancer/protocols/smpp/SMPPMessage.hpp"
 
 namespace eyeline {
 namespace load_balancer {
 namespace protocols {
 namespace smpp {
 
-class SubmitSmResp : public FastParsableSmppMessage {
+class SubmitSmResp : public SMPPMessage {
 public:
   SubmitSmResp()
-    : FastParsableSmppMessage(_MSGCODE)
+  : SMPPMessage(_MSGCODE), _rawMessageBody(NULL), _rawMessageBodyLen(0)
   {}
+
+  virtual size_t serialize(io_subsystem::Packet* packet) const;
+  virtual size_t deserialize(const io_subsystem::Packet* packet);
+
+  virtual std::string toString() const;
 
   static const uint32_t _MSGCODE = 0x80000004;
 
 protected:
-  virtual void generateSerializationException() const {
-    throw utilx::SerializationException("SubmitSmResp::serialize::: message body wasn't set");
-  }
+  virtual uint32_t calculateCommandBodyLength() const;
+
+private:
+  const uint8_t* _rawMessageBody;
+  uint32_t _rawMessageBodyLen;
 };
 
 }}}}
