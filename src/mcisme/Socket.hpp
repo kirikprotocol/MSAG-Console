d19517543029fa74a87ac4d3f278f316c29fc70c
shulga <>
2009-08-06 03:20:10 +0000

    method Connect() was renamed to connect()

--- src/mcisme/Socket.hpp
+++ src/mcisme/Socket.hpp
@@ -5,7 +5,7 @@ namespace mcisme {
 
 class Socket : public core::network::Socket {
 public:
-  int Connect();
+  int connect();
   void setNonBlocking(int mode /* 0 - blocking, 1 - non blocking*/);
 };
 
