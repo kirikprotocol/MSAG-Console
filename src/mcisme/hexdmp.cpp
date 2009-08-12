85c63de89b6b105480053feacf162d32ef921629
shulga <>
2009-08-12 08:46:34 +0000

    put into repository

---
+++ src/mcisme/hexdmp.cpp
@@ -0,0 +1,21 @@
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <sys/types.h>
+
+namespace smsc {
+namespace mcisme {
+
+std::string
+hexdmp(const uchar_t* buf, size_t bufSz)
+{
+  std::ostringstream hexBuf;
+  hexBuf.fill('0');
+  hexBuf << std::hex;
+  for (size_t i=0; i<bufSz; ++i)
+    hexBuf << std::setw(2) << (uint32_t) buf[i];
+
+  return hexBuf.str();
+}
+
+}}
