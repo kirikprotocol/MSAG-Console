585cb675e5ff846b75c6f7dae1868d8bfe937bfb
skv <>
2009-08-14 10:24:41 +0000

    some formatting

--- src/admin/service/Parameter.h
+++ src/admin/service/Parameter.h
@@ -15,67 +15,65 @@ using smsc::core::buffers::Hash;
 class Parameter
 {
 private:
-	char * name;
-	Type type;
+  char * name;
+  Type type;
 
 public:
-	Parameter(const char * const parameterName, Type parameterType)
-	{
-		name = cStringCopy(parameterName);
-		type = parameterType;
-	}
+  Parameter(const char * const parameterName, Type parameterType)
+  {
+    name = cStringCopy(parameterName);
+    type = parameterType;
+  }
 
-	Parameter(const Parameter &copy)
-	{
-		name = cStringCopy(copy.name);
-		type = copy.type;
-	}
+  Parameter(const Parameter &copy)
+  {
+    name = cStringCopy(copy.name);
+    type = copy.type;
+  }
 
-	Parameter()
-	{
-		name = 0;
-		type = undefined;
-	}
+  Parameter()
+  {
+    name = 0;
+    type = undefined;
+  }
 
-	Parameter & operator = (const Parameter & copy)
-	{
-		if (name != 0)
-			delete name;
-		name = cStringCopy(copy.name);
-		type = copy.type;
-		return *this;
-	}
+  Parameter & operator =(const Parameter & copy)
+  {
+    if (name != 0)
+      delete name;
+    name = cStringCopy(copy.name);
+    type = copy.type;
+    return *this;
+  }
 
-	virtual ~Parameter()
-	{
-		if (name != 0)
-			delete name;
-	}
+  virtual ~Parameter()
+  {
+    if (name != 0)
+      delete name;
+  }
 
-	const char * const getName() const
-	{
-		return name;
-	}
+  const char * const getName() const
+  {
+    return name;
+  }
 
-	const Type getType() const
-	{
-		return type;
-	}
+  const Type getType() const
+  {
+    return type;
+  }
 
-	bool operator == (const Parameter & param) const
-	{
-		return    name != 0 && param.name != 0
-		       && type == param.type
-		       && strcmp(name,  param.name) == 0;
-	}
+  bool operator ==(const Parameter & param) const
+  {
+    return name != 0 && param.name != 0 && type == param.type && strcmp(name, param.name) == 0;
+  }
 
-	bool operator != (const Parameter & param) const
-	{
-		return !(*this == param);
-	}
+  bool operator !=(const Parameter & param) const
+  {
+    return !(*this == param);
+  }
 };
 
-typedef Hash<Parameter> Parameters;
+typedef smsc::core::buffers::Hash<Parameter> Parameters;
 
 }
 }
