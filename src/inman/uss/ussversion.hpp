9ef522643ccba986880b4dec6ec2f73eef6da44c
shulga <>
2009-08-25 07:48:09 +0000

    version with fix for MAP dialog leakage

--- src/inman/uss/ussversion.hpp
+++ src/inman/uss/ussversion.hpp
@@ -3,14 +3,14 @@
 #define __SMSC_USSMAN_VERSION__
 
 /* -- NOTE: these defines must be set by release builder -- */
-#define USSMAN_BUILD_NUM   3105
-#define USSMAN_BUILD_DATE  "Jun 18 2009"
+#define USSMAN_BUILD_NUM   3109
+#define USSMAN_BUILD_DATE  "Aug 25 2009"
 /* -- -------------------------------------------------- -- */
 
 /* -- NOTE: these defines are set by developers -- */
 #define USSMAN_VER_MAJOR     0    //major verison
 #define USSMAN_VER_MINOR     7    //minor version
-#define USSMAN_VER_FIX       1    //bug fix version
+#define USSMAN_VER_FIX       2    //bug fix version
 /* -- ----------------------------------------- -- */
 
 #define USSMAN_VENDOR        "Sibinco"
