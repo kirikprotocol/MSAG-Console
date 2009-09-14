b4cd1ea7c92742f42a634d1fe21aef84b8ebc0d7
skv <>
2009-09-14 08:14:20 +0000

    magics!

---
+++ src/eyeline/clustercontroller/protocol/Magics.hpp
@@ -0,0 +1,18 @@
+#ifndef __EYELINE_CLUSTERCONTROLLER_PROTOCOL_MAGICS_HPP__
+#define __EYELINE_CLUSTERCONTROLLER_PROTOCOL_MAGICS_HPP__
+
+namespace eyeline{
+namespace clustercontroller{
+namespace protocol{
+
+enum ProtocolMagics{
+  pmWebApp='WBAP',
+  pmSmsc='SMSC',
+  pmLoadBalancer='LDBL'
+};
+
+}
+}
+}
+
+#endif
