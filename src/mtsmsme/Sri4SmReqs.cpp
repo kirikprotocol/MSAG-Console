d18115fb2748464d899d24d0dd2a5775cd1f1ec5
vic <>
2009-09-14 16:48:33 +0000

    build system dislike file names containig 'sccp'. thinking .cpp :)

--- src/mtsmsme/Sri4SmReqs.cpp
+++ src/mtsmsme/Sri4SmReqs.cpp
@@ -16,6 +16,7 @@ extern std::string hexdmp(const uchar_t* buf, uint32_t bufSz);
 #define NATURE_OF_ADDR 0x04
 
 using smsc::mtsmsme::processor::SccpProcessor;
+using smsc::mtsmsme::processor::SubscriberRegistrator;
 using smsc::core::threads::Thread;
 using smsc::mtsmsme::processor::TCO;
 using smsc::mtsmsme::processor::TSM;
@@ -29,9 +30,16 @@ using smsc::sms::Address;
 using std::string;
 
 static Logger *logger = 0;
+class EmptySubscriberRegistrator: public SubscriberRegistrator {
+  public:
+    EmptySubscriberRegistrator(TCO* _tco) : SubscriberRegistrator(_tco) {}
+    virtual void registerSubscriber(Address& imsi, Address& msisdn, Address& mgt, int period) {}
+    virtual int  update(Address& imsi, Address& msisdn, Address& mgt) {return 1;}
+    virtual bool lookup(Address& msisdn, Address& imsi, Address& msc) { return false;}
+};
 class GopotaListener: public SccpProcessor, public Thread {
   public:
-    GopotaListener(TCO* _tco) : SccpProcessor(_tco) {}
+    GopotaListener(TCO* _tco, SubscriberRegistrator* _reg) : SccpProcessor(_tco, _reg){}
     virtual int Execute()
     {
       int result;
@@ -50,8 +58,9 @@ int main(int argc, char** argv)
   {
     smsc_log_info(logger, "Send Routing Info For SM generator");
     using smsc::core::threads::Thread;
-    TCO* mtsms = new TCO(10);
-    GopotaListener* listener = new GopotaListener(mtsms);
+    TCO mtsms(10);
+    EmptySubscriberRegistrator fakeHLR(&mtsms);
+    GopotaListener* listener = new GopotaListener(&mtsms,&fakeHLR);
     listener->configure(43,191,Address(".1.1.791398699812"),
                                Address(".1.1.791398699813"),
                                Address(".1.1.791398699813"));
@@ -70,7 +79,7 @@ int main(int argc, char** argv)
       cllen = packSCCPAddress(cl, 1 /* E.164 */, sca.c_str() /* SMSC E.164 */, 8 /* SMSC SSN */);
       cdlen = packSCCPAddress(cd, 1 /* E.164 */, ms.c_str() /* MS   E.164 */, 6 /* MS   SSN */);
       TSM* tsm = 0;
-      tsm = mtsms->TC_BEGIN(shortMsgGatewayContext_v2);
+      tsm = mtsms.TC_BEGIN(shortMsgGatewayContext_v2);
       if (tsm)
       {
         SendRoutingInfoForSMReq* inv = new SendRoutingInfoForSMReq(ms, pri, sca);
