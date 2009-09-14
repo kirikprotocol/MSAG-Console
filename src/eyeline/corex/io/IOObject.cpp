36d376377393dc71257547218a0764b60ded8198
shulga <>
2009-09-14 11:41:58 +0000

    fixed error in set/clear of nonblock flags for file descriptor

--- src/eyeline/corex/io/IOObject.cpp
+++ src/eyeline/corex/io/IOObject.cpp
@@ -21,16 +21,16 @@ static int turn_Off_SIGPIPE_Delivery = _turn_Off_SIGPIPE_Delivery();
 void
 IOObject::setNonBlocking(int fd, bool on)
 {
-  int flags;
-  if ( fcntl(fd, F_GETFL, &flags) < 0 )
+  int flags = fcntl(fd, F_GETFL, 0);
+  if ( flags < 0 )
     throw smsc::util::SystemError("IOObject::setNonBlocking::: call to fcntl(F_GETFL) failed");
   if ( on ) {
     flags |= O_NONBLOCK;
-    if ( fcntl(fd, F_SETFL, &flags) < 0 )
+    if ( fcntl(fd, F_SETFL, flags) < 0 )
       throw smsc::util::SystemError("IOObject::setNonBlocking::: call to fcntl(F_SETFL) failed");
   } else {
     flags &= ~O_NONBLOCK;
-    if ( fcntl(fd, F_SETFL, &flags) < 0 )
+    if ( fcntl(fd, F_SETFL, flags) < 0 )
       throw smsc::util::SystemError("IOObject::setNonBlocking::: call to fcntl(F_SETFL) failed");
   }
 }
