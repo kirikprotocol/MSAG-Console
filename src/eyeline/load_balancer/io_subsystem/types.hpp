20f909c5c4830da322d0ac1de2f92d43e183d963
shulga <>
2009-09-16 07:55:23 +0000

    typedef for IOProcessorRefPtr was added

--- src/eyeline/load_balancer/io_subsystem/types.hpp
+++ src/eyeline/load_balancer/io_subsystem/types.hpp
@@ -14,10 +14,11 @@ namespace io_subsystem {
 
 class Link;
 class LinkSet;
+class IOProcessor;
 
 typedef smsc::core::buffers::RefPtr<Link, smsc::core::synchronization::Mutex> LinkRefPtr;
 typedef smsc::core::buffers::RefPtr<LinkSet, smsc::core::synchronization::Mutex> LinkSetRefPtr;
-
+typedef smsc::core::buffers::RefPtr<IOProcessor, smsc::core::synchronization::Mutex> IOProcessorRefPtr;
 }}}
 
 #endif
