52d14fadacc822ca46b45752b6fc9a66eae3eedf
alkhal <>
2009-09-17 04:04:52 +0000

    replace generation blocks in CreateDB

--- ext_smes/InfoSme/java/ru/novosoft/smsc/infosme/backend/siebel/CreateDB.java
+++ ext_smes/InfoSme/java/ru/novosoft/smsc/infosme/backend/siebel/CreateDB.java
@@ -43,15 +43,15 @@ public class CreateDB {
       }finally{
         if(is != null) {
           try{
-          is.close();
+            is.close();
           }catch(Throwable e){}
         }
       }
     } else {
-        props.setProperty("jdbc.source", "jdbc:mysql://mangal/summer_edu");
-        props.setProperty("jdbc.driver", "com.mysql.jdbc.Driver");
-        props.setProperty("jdbc.user", "student");
-        props.setProperty("jdbc.pass", "student");
+      props.setProperty("jdbc.source", "jdbc:mysql://mangal/summer_edu");
+      props.setProperty("jdbc.driver", "com.mysql.jdbc.Driver");
+      props.setProperty("jdbc.user", "student");
+      props.setProperty("jdbc.pass", "student");
     }
     if(arguments.containsKey("-s")) {
       size = Integer.parseInt((String)arguments.get("-s"));
@@ -67,10 +67,10 @@ public class CreateDB {
   }
 
   public static void help() {
-      System.out.println("Usage: CreateDB (create|remove) (args)");
-      System.out.println("Args:");
-      System.out.println("-f config file (custom)");
-      System.out.println("-s number of task(custom, default: '2')");
+    System.out.println("Usage: CreateDB (create|remove) (args)");
+    System.out.println("Args:");
+    System.out.println("-f config file (custom)");
+    System.out.println("-s number of task(custom, default: '2')");
   }
 
   public static void createAll(Properties jdbcProp) {
@@ -84,50 +84,14 @@ public class CreateDB {
       try{
         connection = pool.getConnection();
         connection.setAutoCommit(false);
-        try {
-          prepStatement = connection.prepareStatement("replace into SMS_MAIL_PARAMS (ROW_ID, WAVE_INT_ID, CAMPAIGN_INT_ID, " +
-              "CREATED, LAST_UPD, PRIORITY, FLASH_FLG, SAVE_FLG, BEEP_FLG, EXP_PERIOD, CTRL_STATUS) values (?,?,?,?,?,?,?,?,?,?,?)");
 
-          int count = 0;
-          System.out.println("Creation of SMS_MAIL_PARAMS table...");
-          for(int i=0;i<size;i++) {
-            prepStatement.setString(1,Integer.toString(i));
-            prepStatement.setString(2,Integer.toString(i));
-            prepStatement.setString(3,Integer.toString(i));
-            prepStatement.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
-            prepStatement.setTimestamp(5, new Timestamp(System.currentTimeMillis()));
-            prepStatement.setString(6, Integer.toString(i+1));
-            prepStatement.setString(7,"Y");
-            prepStatement.setString(8,"Y");
-            prepStatement.setString(9,"N");
-            prepStatement.setString(10, Integer.toString(i+1));
-            prepStatement.setString(11, SiebelTask.Status.ENQUEUED.toString());
-
-            prepStatement.addBatch();
-            count++;
-            if(count == 1000) {
-              count = 0;
-              prepStatement.executeBatch();
-            }
-          }
-          if(count != 0) {
-            prepStatement.executeBatch();
-          }
-          connection.commit();
-
-        } finally {
-          if(prepStatement != null) {
-            prepStatement.close();
-          }
-        }
-        System.out.println("Completed: "+size);
         try {
           prepStatement = connection.prepareStatement("replace into SMS_MAIL (ROW_ID, WAVE_INT_ID, " +
               "CLC_INT_ID, CREATED, LAST_UPD, MSISDN, MESSAGE, MESSAGE_STATE, SMSC_STAT_CODE, SMSC_STAT_VAL) values (?,?,?,?,?,?,?,?,?,?)");
           int count = 0;
 
           System.out.println("Creation of SMS_MAIL table...");
-          for(int i=0;i<(10000*size);i++) {
+          for(int i=0;i<(1000*size);i++) {
             prepStatement.setString(1,Integer.toString(i));
             prepStatement.setString(2,Integer.toString(i%size));
             prepStatement.setString(3,Integer.toString(i));
@@ -166,8 +130,47 @@ public class CreateDB {
             prepStatement.close();
           }
         }
-        System.out.println("Completed: "+(10000*size));
+        System.out.println("Completed: "+(1000*size));
         System.out.println();
+
+        try {
+          prepStatement = connection.prepareStatement("replace into SMS_MAIL_PARAMS (ROW_ID, WAVE_INT_ID, CAMPAIGN_INT_ID, " +
+              "CREATED, LAST_UPD, PRIORITY, FLASH_FLG, SAVE_FLG, BEEP_FLG, EXP_PERIOD, CTRL_STATUS) values (?,?,?,?,?,?,?,?,?,?,?)");
+
+          int count = 0;
+          System.out.println("Creation of SMS_MAIL_PARAMS table...");
+          for(int i=0;i<size;i++) {
+            prepStatement.setString(1,Integer.toString(i));
+            prepStatement.setString(2,Integer.toString(i));
+            prepStatement.setString(3,Integer.toString(i));
+            prepStatement.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+            prepStatement.setTimestamp(5, new Timestamp(System.currentTimeMillis()));
+            prepStatement.setString(6, Integer.toString(i+1));
+            prepStatement.setString(7,"Y");
+            prepStatement.setString(8,"Y");
+            prepStatement.setString(9,"N");
+            prepStatement.setString(10, Integer.toString(i+1));
+            prepStatement.setString(11, SiebelTask.Status.ENQUEUED.toString());
+
+            prepStatement.addBatch();
+            count++;
+            if(count == 1000) {
+              count = 0;
+              prepStatement.executeBatch();
+            }
+          }
+          if(count != 0) {
+            prepStatement.executeBatch();
+          }
+          connection.commit();
+
+        } finally {
+          if(prepStatement != null) {
+            prepStatement.close();
+          }
+        }
+        System.out.println("Completed: "+size);
+
         System.out.println("Finished");
       }catch (Throwable e) {
         e.printStackTrace();
