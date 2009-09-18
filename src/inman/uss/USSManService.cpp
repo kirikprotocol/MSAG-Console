b586b378bf5dd37e951488499c2e13104059201b
shulga <>
2009-09-18 08:08:09 +0000

    minor fix

--- src/inman/uss/USSManService.cpp
+++ src/inman/uss/USSManService.cpp
@@ -114,7 +114,7 @@ USSManService::onConnectOpening(smsc::inman::interaction::Server* srv,
                  _logId, sock->getSocket());
   USSManConnect *ussCon = new USSManConnect(conn->getId(), _logger, _cfg);
   if (ussCon) {
-    smsc_log_info(_logger, "%s: New connect[%u] was created", _logId, sockId);
+    smsc_log_info(_logger, "%s: New connect[%u] was created", _logId, sock->getSocket());
     smsc::core::synchronization::MutexGuard guard(_mutex);
     _ussConnects.insert(UssConnMap_t::value_type(sock->getSocket(), ussCon));
     conn->addListener(ussCon);
