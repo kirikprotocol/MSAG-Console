482c4ea0bb86de1e7e22f6a9f4de2021f23f5427
artem <>
2009-09-28 08:34:21 +0000

    Siebel integration for InfoSme

--- ext_smes/InfoSme/java/ru/novosoft/smsc/infosme/backend/siebel/SiebelFinalStateThread.java
+++ ext_smes/InfoSme/java/ru/novosoft/smsc/infosme/backend/siebel/SiebelFinalStateThread.java
@@ -16,26 +16,32 @@ import ru.novosoft.smsc.infosme.backend.Message;
  * Time: 15:45:33
  * To change this template use File | Settings | File Templates.
  */
-public class SiebelFinalStateThread extends Thread {
+public class SiebelFinalStateThread {
 
   private static final Category log_ = Category.getInstance(SiebelFinalStateThread.class);
   private static final String rollingExtension = ".csv";
   private String path_;
-  private Pattern  recSep_;
+  private Pattern recSep_;
   private String processedPath_;
   private DataSource dataSource_;
   private boolean started_;
+  private Thread currentThread;
 
-  public SiebelFinalStateThread( String path, String processedPath, SiebelDataProvider provider ) throws IOException {
+  public SiebelFinalStateThread(String path, String processedPath, SiebelDataProvider provider) throws IOException {
     this.path_ = path;
     this.recSep_ = Pattern.compile(",");
     this.processedPath_ = processedPath;
-    this.dataSource_ = new DataSourceAdapter( provider, log_ );
+    this.dataSource_ = new DataSourceAdapter(provider, log_);
     checkPath();
   }
 
   public void shutdown() {
     started_ = false;
+    currentThread.interrupt();
+    try {
+      currentThread.join();
+    } catch (InterruptedException e) {
+    }
   }
 
   public boolean isOnline() {
@@ -43,49 +49,52 @@ public class SiebelFinalStateThread extends Thread {
   }
 
   public void start() {
-    if(!started_) {
-      super.start();
-    }
-  }
-
-  public synchronized void run() {
-    started_ = true;
-    log_.info("final state thread is started");
-    try {
-      while ( started_ ) {
-        File dir = checkPath();
-        String[] files = dir.list();
-        for ( int i = 0; i < files.length; ++i ) {
-          if ( files[i].endsWith( rollingExtension ) ) {
-            processFile(files[i]);
+    if (!started_) {
+      currentThread = new Thread(new Runnable() {
+        public synchronized void run() {
+          started_ = true;
+          log_.info("final state thread is started");
+          try {
+            while (started_) {
+              File dir = checkPath();
+              String[] files = dir.list();
+              for (int i = 0; i < files.length; ++i) {
+                if (files[i].endsWith(rollingExtension)) {
+                  processFile(files[i]);
+                }
+              }
+              try {
+                Thread.sleep(10000);
+              } catch (InterruptedException e) {
+              }
+            }
+            log_.info("final state thread is shutdowned");
+          } catch (Exception exc) {
+            log_.error("exc in run: " + exc.getMessage());
+            started_ = false;
           }
         }
-        try{
-          Thread.sleep(10000);
-        }catch(InterruptedException e){}
-    }
-      log_.info("final state thread is shutdowned");
-    } catch ( Exception exc ) {
-      log_.error("exc in run: " + exc.getMessage() );
-      started_ = false;
+      });
+      currentThread.start();
     }
   }
 
-  public void processFile( String fileName ) {
-    File fd = new File(path_,fileName);
+
+  public void processFile(String fileName) {
+    File fd = new File(path_, fileName);
     BufferedReader is = null;
     try {
-      is = new BufferedReader( new FileReader(fd) );
+      is = new BufferedReader(new FileReader(fd));
       String line = is.readLine();
-      if ( line == null ) throw new IOException("heading line is not found");
+      if (line == null) throw new IOException("heading line is not found");
       int totalRecords = 0;
       int processedRecords = 0;
       TreeMap map = new TreeMap();
-      while ( started_ && ( line = is.readLine()) != null ) {
+      while (started_ && (line = is.readLine()) != null) {
         // parse line: date,state,taskId,msgId,smppStatus,abnt,userData,taskName
         ++totalRecords;
-        String[] fields = recSep_.split(line,8);
-          /*
+        String[] fields = recSep_.split(line, 8);
+        /*
         if ( log_.isDebugEnabled() ) {
           StringBuffer sb = new StringBuffer();
           for ( int i = 0; i < fields.length; ++i ) {
@@ -95,24 +104,24 @@ public class SiebelFinalStateThread extends Thread {
           log_.debug("line parsed into " + fields.length + " pieces:" + sb.toString());
         }
         */
-        if ( fields.length < 8 ) {
+        if (fields.length < 8) {
           // not all fields specified
-          if (log_.isDebugEnabled() ) {
+          if (log_.isDebugEnabled()) {
             log_.debug("message " + line + " cannot be parsed");
           }
           continue;
         }
-        if ( fields[6].length() != 0 ) {
+        if (fields[6].length() != 0) {
           Message.State state = Message.State.getById(new Integer(fields[1]).intValue());
           SiebelMessage.State siebelState = stateToSiebelState(state);
           String smppCodeDescription = ""; // FIXME: ask Artem where to take smpp description from smpp code
-          map.put( fields[6], new SiebelMessage.DeliveryState(siebelState,fields[4],smppCodeDescription) );
-          if ( (totalRecords - processedRecords) > 100 ) {
-            dataSource_.saveFinalStates( map );
+          map.put(fields[6], new SiebelMessage.DeliveryState(siebelState, fields[4], smppCodeDescription));
+          if ((totalRecords - processedRecords) > 100) {
+            dataSource_.saveFinalStates(map);
             map.clear();
             processedRecords = totalRecords;
           }
-        } else if ( fields[1].equals("0") && fields[3].equals("0") ) {
+        } else if (fields[1].equals("0") && fields[3].equals("0")) {
           // end of messages in the task
           if (log_.isInfoEnabled()) {
             log_.info("all messages have been processed for task " + fields[7]);
@@ -122,31 +131,31 @@ public class SiebelFinalStateThread extends Thread {
             map.clear();
             processedRecords = totalRecords;
           }
-          dataSource_.taskHasFinished( fields[7] );
+          dataSource_.taskHasFinished(fields[7]);
         } else {
           // if (log_.isDebugEnabled() ) {
           //   log_.debug("message " + line + ": userData is empty" );
           // }
         }
       }
-      if ( map.size() > 0 ) {
-        dataSource_.saveFinalStates( map );
+      if (map.size() > 0) {
+        dataSource_.saveFinalStates(map);
       }
       is.close();
       is = null;
       // renaming the file
-      fd.renameTo( new File(processedPath_,fileName) );
-      if ( log_.isInfoEnabled() ) {
-        log_.info("file processed " + fileName + ", msg records=" + totalRecords );
+      fd.renameTo(new File(processedPath_, fileName));
+      if (log_.isInfoEnabled()) {
+        log_.info("file processed " + fileName + ", msg records=" + totalRecords);
       }
-    } catch ( IOException e ) {
-      log_.error( "exc processing file " + fileName + ": " + e.getMessage() );
+    } catch (IOException e) {
+      log_.error("exc processing file " + fileName + ": " + e.getMessage());
     } finally {
-      if ( is != null ) {
+      if (is != null) {
         try {
           is.close();
-        } catch ( IOException e ) {
-          log_.error( "exc: " + e.getMessage());
+        } catch (IOException e) {
+          log_.error("exc: " + e.getMessage());
         }
       }
     }
@@ -154,24 +163,23 @@ public class SiebelFinalStateThread extends Thread {
 
   private File checkPath() throws IOException {
     File dir = new File(path_);
-    if ( !dir.exists() ) {
-      throw new IOException( "path " + path_ + " does not exist" );
-    } else if ( ! dir.isDirectory() ) {
-      throw new IOException( "path " + path_ + " is not a directory" );
+    if (!dir.exists()) {
+      throw new IOException("path " + path_ + " does not exist");
+    } else if (!dir.isDirectory()) {
+      throw new IOException("path " + path_ + " is not a directory");
     }
     return dir;
   }
 
-  private SiebelMessage.State stateToSiebelState( Message.State state )
-  {
+  private SiebelMessage.State stateToSiebelState(Message.State state) {
     SiebelMessage.State siebelState;
-    if ( state == Message.State.DELIVERED ) {
+    if (state == Message.State.DELIVERED) {
       siebelState = SiebelMessage.State.DELIVERED;
-    } else if ( state == Message.State.EXPIRED ) {
+    } else if (state == Message.State.EXPIRED) {
       siebelState = SiebelMessage.State.EXPIRED;
-    } else if ( state == Message.State.DELETED ) {
+    } else if (state == Message.State.DELETED) {
       siebelState = SiebelMessage.State.DELETED;
-    } else if ( state == Message.State.FAILED ) {
+    } else if (state == Message.State.FAILED) {
       siebelState = SiebelMessage.State.ERROR;
     } else {
       siebelState = SiebelMessage.State.UNKNOWN;
