232381cc01dd4517c85db77fbb0ac6846c4fec1e
vic <>
2009-10-07 09:37:58 +0000

    test result file name changed

--- src/mtsmsme/tests/testrunner.cpp
+++ src/mtsmsme/tests/testrunner.cpp
@@ -1,26 +1,30 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TextTestRunner.h>
-#include <cppunit/CompilerOutputter.h>
-#include <stdio.h>
+#include <cppunit/XmlOutputter.h>
+#include <fstream>
+
 int main(int argc, char* argv[])
 {
+  // Define the file that will store the XML output.
+  std::ofstream outputFile("cpptestresults.xml");
+
+  // Create a test runner to execute the test cases you have defined (in other source code).
+  CppUnit::TextTestRunner runner;
+
   // Get the top level suite from the registry
-  //CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
 
   // Adds the test to the list of test to run
-  //CppUnit::TextTestRunner runner;
-  //runner.addTest(suite);
+  runner.addTest( registry.makeTest() );
+
+  // Specify XML output and inform the test runner of this format.
+  CppUnit::XmlOutputter* outputter =
+    new CppUnit::XmlOutputter(&runner.result(), outputFile);
+  runner.setOutputter(outputter);
 
   // Run the tests.
-  //bool wasSucessful = runner.run();
-
+  bool wasSuccessful = runner.run( "", false, true, false );
+  outputFile.close();
   // Return error code 1 if the one of test failed.
-  //return wasSucessful ? 0 : 1;
-
-  CppUnit::TextTestRunner runner;
-  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
-  runner.addTest( registry.makeTest() );
-  //runner.setOutputter(new CppUnit::CompilerOutputter( &runner.result(),std::cerr ));
-  bool wasSuccessful = runner.run( "", false );
   return wasSuccessful;
 }
