c01cf3aefb060d86bee25f3d6d00f2520b66bc72
vic <>
2009-10-09 02:50:58 +0000

    test add

--- src/mtsmsme/comp/tests/tst2.hpp
+++ src/mtsmsme/comp/tests/tst2.hpp
@@ -7,11 +7,13 @@ std::string tsname() { return "smsc::mtsmsme::comp::tests";}
 class AmericaTestFixture : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE (AmericaTestFixture);
   CPPUNIT_TEST (AmericaTest);
+  CPPUNIT_TEST (ulprint);
   CPPUNIT_TEST_SUITE_END ();
 public:
   void setUp(){}
   void tearDown(){}
 protected:
   void AmericaTest (void);
+  void ulprint(void);
 private:
 };
