22e06f75e6c8f49149de8472272d4cdc59cef75e
bukind <>
2009-10-14 05:44:18 +0000

    origAddress removed

--- src/infosme/SmscConnector.cpp
+++ src/infosme/SmscConnector.cpp
@@ -224,10 +224,12 @@ smsc::sme::SmeConfig SmscConnector::readSmeConfig( ConfigView& config )
         const std::string systemType = ::cgetString(config,"systemType","InfoSme system type wasn't defined !");
         rv.setSystemType(systemType);
     } catch (smsc::util::config::ConfigException&) {}
+    /*
     try {
         rv.origAddr = ::cgetString(config,"origAddress",
                                    "InfoSme originating address wasn't defined !");
     } catch (smsc::util::config::ConfigException&) {}
+     */
     try {
         rv.interfaceVersion = config.getInt("interfaceVersion","InfoSme interface version wasn't defined!");
     } catch (smsc::util::config::ConfigException&) {}
