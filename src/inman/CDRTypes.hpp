db5002c10d5e5ff86f6011b164375316082be910
gvroman <>
2009-12-11 08:01:18 +0000

    Typed used in CDR

---
+++ src/inman/CDRTypes.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************* *
+ * Definition of types used in SM/USSD Collected Data Records.
+ * ************************************************************************* */
+#ifndef __SMSC_INMAN_CDR_TYPES
+#ident "@(#)$Id$"
+#define __SMSC_INMAN_CDR_TYPES
+
+#include "util/TonNpiAddress.hpp"
+
+namespace smsc  {
+namespace inman {
+
+const unsigned MAX_ESERVICE_TYPE_LENGTH = 5;
+const unsigned MAX_SMESYSID_TYPE_LENGTH = 15;
+const unsigned MAX_ROUTE_ID_TYPE_LENGTH = 32;
+
+//SMPP DATA_SM service type
+typedef smsc::core::buffers::FixedLengthString<MAX_ESERVICE_TYPE_LENGTH>
+  SMPPServiceType;
+//SM Entity system id
+typedef smsc::core::buffers::FixedLengthString<MAX_SMESYSID_TYPE_LENGTH>
+  SMESysId;
+//SM Route ident 
+typedef smsc::core::buffers::FixedLengthString<MAX_ROUTE_ID_TYPE_LENGTH>
+  SMRouteId;
+
+} //inman
+} //smsc
+
+#endif /* __SMSC_INMAN_CDR_TYPES */
+
