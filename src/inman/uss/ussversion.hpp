17094c52fd2af9b9b9d0128e17ff9c4e360431ac
shulga <>
2010-02-01 08:55:13 +0000

    *** empty log message ***

--- src/inman/uss/ussversion.hpp
+++ src/inman/uss/ussversion.hpp
@@ -3,14 +3,14 @@
 #define __SMSC_USSMAN_VERSION__
 
 /* -- NOTE: these defines must be set by release builder -- */
-#define USSMAN_BUILD_NUM   3109
-#define USSMAN_BUILD_DATE  "Aug 25 2009"
+#define USSMAN_BUILD_NUM   3118
+#define USSMAN_BUILD_DATE  "Feb 01 2010"
 /* -- -------------------------------------------------- -- */
 
 /* -- NOTE: these defines are set by developers -- */
 #define USSMAN_VER_MAJOR     0    //major verison
 #define USSMAN_VER_MINOR     7    //minor version
-#define USSMAN_VER_FIX       2    //bug fix version
+#define USSMAN_VER_FIX       3    //bug fix version
 /* -- ----------------------------------------- -- */
 
 #define USSMAN_VENDOR        "Sibinco"
