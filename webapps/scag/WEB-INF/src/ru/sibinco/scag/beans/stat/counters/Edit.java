10fc7c62c330e193c8538dd9132152f1a74eadaa
makar <>
2010-02-12 08:54:38 +0000

    SNMP counters & C/A tables update

--- webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/stat/counters/Edit.java
+++ webapps/scag/WEB-INF/src/ru/sibinco/scag/beans/stat/counters/Edit.java
@@ -47,7 +47,7 @@ public class Edit extends EditBean
 
     public void process(final HttpServletRequest request, final HttpServletResponse response) throws SCAGJspException
     {
-        logger.debug("Counters process() called"); // TODO: remove it
+        logger.debug("Counters process() called, mbSave=" + getMbSave()); // TODO: remove it
         requestParams = request.getParameterMap();
         super.process(request, response);
 
