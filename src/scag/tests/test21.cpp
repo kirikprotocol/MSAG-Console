e21544c3f14be0ceea84268f9549453689ef025f
bukind <>
2010-04-14 05:35:12 +0000

    more tests

--- src/scag/tests/test21.cpp
+++ src/scag/tests/test21.cpp
@@ -3,21 +3,22 @@
 template <class T> struct A
 {
     A() {
-        std::cout << "ctor" << std::endl;
+        std::cout << "A::A()" << std::endl;
     }
     void process() {
-        std::cout << "A" << std::endl;
+        std::cout << "A<T>::process()" << std::endl;
     }
 };
 
 template <> void A<int>::process()
 {
-    std::cout << "A<int>" << std::endl;
+    std::cout << "A<int>::process()" << std::endl;
 }
 
 
 int main()
 {
+    std::cout << "Possibility to specialize one member of a class template only" << std::endl;
     A<float> af;
     af.process();
     A<int> ai;
---
+++ src/scag/tests/test22.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+
+namespace outer {
+
+namespace inner {
+
+struct A {
+    virtual void exec() {
+        std::cout << "A::exec()" << std::endl;
+    }
+};
+
+}
+
+struct B : public inner::A 
+{
+    void exec() {
+        std::cout << "B::exec()" << std::endl;
+    }
+};
+
+struct C {
+    void exec()
+    {
+        // A test on how to refer to base class method.
+        // It was found that both cases are ok.
+
+        // 1.
+        b.inner::A::exec();
+
+        // 2.
+        b.A::exec();
+    }
+    B b;
+};
+
+}
+
+int main()
+{
+    outer::C c;
+    c.exec();
+    return 0;
+}
