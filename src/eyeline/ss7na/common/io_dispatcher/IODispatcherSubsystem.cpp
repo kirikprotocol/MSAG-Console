8aa6dec3c27ec1fdd898e94319e8141ad3472b5b
shulga <>
2010-05-06 04:47:49 +0000

    added method updateStateMachinesCount

--- src/eyeline/ss7na/common/io_dispatcher/IODispatcherSubsystem.cpp
+++ src/eyeline/ss7na/common/io_dispatcher/IODispatcherSubsystem.cpp
@@ -46,4 +46,20 @@ IODispatcherSubsystem::initialize()
   Reconnector::init();
 }
 
+void
+IODispatcherSubsystem::updateStateMachinesCount(unsigned new_state_machines_count)
+{
+  if ( new_state_machines_count <= _stateMachinesCount )
+    return;
+
+  unsigned delta = new_state_machines_count - _stateMachinesCount;
+  _stateMachinesCount = new_state_machines_count;
+  for(size_t i = 0; i < delta; i++) {
+    IOEventProcessor *ioEventProcessor = new IOEventProcessor(_cMgr);
+
+    smsc_log_debug(_logger, "IODispatcherSubsystem::updateStateMachinesCount::: start IOEventProcessor");
+    _threadsPool.startTask(ioEventProcessor);
+  }
+}
+
 }}}}
