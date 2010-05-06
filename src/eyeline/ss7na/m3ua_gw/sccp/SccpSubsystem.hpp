ed9a15930df322bad53864876d1092fcd954ceab
shulga <>
2010-05-06 05:18:02 +0000

    removed inheritance from utilx::runtime_cfg::ParameterObserver; added methods addSccpUserPolicy/addMTP3Policy; removed method applyParametersChange; method makeAddressFamilyPrefix made as public

--- src/eyeline/ss7na/m3ua_gw/sccp/SccpSubsystem.hpp
+++ src/eyeline/ss7na/m3ua_gw/sccp/SccpSubsystem.hpp
@@ -2,11 +2,11 @@
 # define __EYELINE_SS7NA_M3UAGW_SCCP_SCCPSUBSYSTEM_HPP__
 
 # include <netinet/in.h>
+# include <list>
 # include <string>
 
 # include "eyeline/utilx/Singleton.hpp"
 # include "eyeline/utilx/ThreadSpecificData.hpp"
-# include "eyeline/utilx/runtime_cfg/ParameterObserver.hpp"
 # include "eyeline/utilx/runtime_cfg/RuntimeConfig.hpp"
 
 # include "eyeline/ss7na/common/types.hpp"
@@ -19,26 +19,27 @@ namespace m3ua_gw {
 namespace sccp {
 
 class SccpSubsystem : public common::ApplicationSubsystem,
-                      public utilx::runtime_cfg::ParameterObserver,
                       public utilx::Singleton<SccpSubsystem*> {
 public:
   virtual void stop();
   virtual void initialize(utilx::runtime_cfg::RuntimeConfig& rconfig);
 
-  using ParameterObserver::addParameterEventHandler;
-  using ParameterObserver::changeParameterEventHandler;
-  //  using ParameterObserver::removeParameterEventHandler;
+  void reinitConnectAcceptor();
 
-  virtual void addParameterEventHandler(const utilx::runtime_cfg::CompositeParameter& context,
-                                        utilx::runtime_cfg::Parameter* added_parameter);
-  virtual void changeParameterEventHandler(const utilx::runtime_cfg::CompositeParameter& context,
-                                           const utilx::runtime_cfg::Parameter& modified_parameter);
+  static std::string makeAddressFamilyPrefix(const std::string& gt_mask_value);
 
+  void addSccpUserPolicy(const std::string& route_id,
+                         const std::list<std::string>& sccp_users,
+                         const std::string& traffic_mode);
+
+  void addMTP3Policy(const std::string& route_id,
+                     common::point_code_t lpc,
+                     const std::list<common::point_code_t>& dpcs,
+                     const std::string& traffic_mode);
 private:
   SccpSubsystem();
   friend class utilx::Singleton<SccpSubsystem*>;
 
-  void applyParametersChange();
   void fillInTranslationTable(utilx::runtime_cfg::RuntimeConfig& rconfig);
   void processRouteInfoToSccpUser(utilx::runtime_cfg::CompositeParameter* translation_entry_param,
                                   router::GTTranslationTable* translation_table,
@@ -48,7 +49,7 @@ private:
 
   void initializeConnectAcceptor(const std::string& localHost,
                                  in_port_t localPort);
-  std::string makeAddressFamilyPrefix(const std::string& gt_mask_value) const;
+
   void addSccpUserPolicy(const std::string& route_id,
                          utilx::runtime_cfg::CompositeParameter* sccp_users_param,
                          const std::string& traffic_mode);
@@ -56,8 +57,6 @@ private:
                      common::point_code_t lpc,
                      utilx::runtime_cfg::CompositeParameter* dpcs_param,
                      const std::string& traffic_mode);
-
-  static utilx::ThreadSpecificData<bool> _threadScopeModificationFlag;
 
   char _acceptorName[128];
 };
