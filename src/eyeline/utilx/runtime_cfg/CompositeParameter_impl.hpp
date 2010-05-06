f851de34096bdb3481c98ac053af1ea45564dbdc
shulga <>
2010-05-06 04:35:53 +0000

    getIterator was modified - added passing reference to iterated container as third argument of Iterator's constructor

--- src/eyeline/utilx/runtime_cfg/CompositeParameter_impl.hpp
+++ src/eyeline/utilx/runtime_cfg/CompositeParameter_impl.hpp
@@ -1,51 +1,51 @@
 template<> inline
 const CompositeParameter*
-CompositeParameter::getParameter<CompositeParameter>(const std::string& parameterName) const
+CompositeParameter::getParameter<CompositeParameter>(const std::string& param_name) const
 {
-  composite_parameters_t::const_iterator iter = _compositeParameters.find(parameterName);
+  composite_parameters_t::const_iterator iter = _compositeParameters.find(param_name);
   if ( iter == _compositeParameters.end() ) return NULL;
   return iter->second;
 }
 
 template<> inline
 const Parameter*
-CompositeParameter::getParameter<Parameter>(const std::string& parameterName) const
+CompositeParameter::getParameter<Parameter>(const std::string& param_name) const
 {
-  parameters_t::const_iterator iter = _parameters.find(parameterName);
+  parameters_t::const_iterator iter = _parameters.find(param_name);
   if ( iter == _parameters.end() ) return NULL;
   return iter->second;
 }
 
 template<> inline
 CompositeParameter*
-CompositeParameter::getParameter<CompositeParameter>(const std::string& parameterName)
+CompositeParameter::getParameter<CompositeParameter>(const std::string& param_name)
 {
-  composite_parameters_t::const_iterator iter = _compositeParameters.find(parameterName);
+  composite_parameters_t::const_iterator iter = _compositeParameters.find(param_name);
   if ( iter == _compositeParameters.end() ) return NULL;
   return iter->second;
 }
 
 template<> inline
 Parameter*
-CompositeParameter::getParameter<Parameter>(const std::string& parameterName)
+CompositeParameter::getParameter<Parameter>(const std::string& param_name)
 {
-  parameters_t::const_iterator iter = _parameters.find(parameterName);
+  parameters_t::const_iterator iter = _parameters.find(param_name);
   if ( iter == _parameters.end() ) return NULL;
   return iter->second;
 }
 
 template<> inline
 CompositeParameter::Iterator<Parameter>
-CompositeParameter::getIterator<Parameter>(const std::string& parameterName)
+CompositeParameter::getIterator<Parameter>(const std::string& param_name)
 {
-  std::pair<parameters_t::iterator,  parameters_t::iterator> interval = _parameters.equal_range(parameterName);
-  return Iterator<Parameter>(interval.first, interval.second);
+  std::pair<parameters_t::iterator,  parameters_t::iterator> interval = _parameters.equal_range(param_name);
+  return Iterator<Parameter>(interval.first, interval.second, _parameters);
 }
 
 template<> inline
 CompositeParameter::Iterator<CompositeParameter>
-CompositeParameter::getIterator<CompositeParameter>(const std::string& parameterName)
+CompositeParameter::getIterator<CompositeParameter>(const std::string& param_name)
 {
-  std::pair<composite_parameters_t::iterator,  composite_parameters_t::iterator> interval = _compositeParameters.equal_range(parameterName);
-  return Iterator<CompositeParameter>(interval.first, interval.second);
+  std::pair<composite_parameters_t::iterator,  composite_parameters_t::iterator> interval = _compositeParameters.equal_range(param_name);
+  return Iterator<CompositeParameter>(interval.first, interval.second, _compositeParameters);
 }
