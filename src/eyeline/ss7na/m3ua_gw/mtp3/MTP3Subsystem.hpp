cf306b3bc26c59789aee4858ec20d4d92225a5d6
shulga <>
2010-05-06 05:10:23 +0000

    removed inheritance from utilx::runtime_cfg::ParameterObserver; added methods activateLinkToSGP/addMtp3Route

--- src/eyeline/ss7na/m3ua_gw/mtp3/MTP3Subsystem.hpp
+++ src/eyeline/ss7na/m3ua_gw/mtp3/MTP3Subsystem.hpp
@@ -1,17 +1,19 @@
 #ifndef __EYELINE_SS7NA_M3UAGW_MTP3_M3UASTACK_MTP3SUBSYSTEM_HPP__
 # define __EYELINE_SS7NA_M3UAGW_MTP3_M3UASTACK_MTP3SUBSYSTEM_HPP__
 
+# include <set>
 # include <vector>
 # include <string>
+# include <netinet/in.h>
 
 # include "core/synchronization/EventMonitor.hpp"
 
 # include "eyeline/utilx/Singleton.hpp"
-# include "eyeline/utilx/runtime_cfg/ParameterObserver.hpp"
 # include "eyeline/utilx/runtime_cfg/RuntimeConfig.hpp"
 
 # include "eyeline/ss7na/common/LinkId.hpp"
 # include "eyeline/ss7na/common/ApplicationSubsystem.hpp"
+# include "eyeline/ss7na/common/types.hpp"
 
 namespace eyeline {
 namespace ss7na {
@@ -19,26 +21,19 @@ namespace m3ua_gw {
 namespace mtp3 {
 
 class MTP3Subsystem : public common::ApplicationSubsystem,
-                      public utilx::runtime_cfg::ParameterObserver,
                       public utilx::Singleton<MTP3Subsystem*> {
 public:
   virtual void stop();
   virtual void initialize(utilx::runtime_cfg::RuntimeConfig& rconfig);
   virtual void waitForCompletion();
 
-  using ParameterObserver::addParameterEventHandler;
-  using ParameterObserver::changeParameterEventHandler;
-  using ParameterObserver::removeParameterEventHandler;
-
-  virtual void changeParameterEventHandler(const utilx::runtime_cfg::CompositeParameter& context,
-                                           const utilx::runtime_cfg::Parameter& modified_parameter);
-  virtual void addParameterEventHandler(const utilx::runtime_cfg::CompositeParameter& context,
-                                        utilx::runtime_cfg::Parameter* added_parameter);
-  virtual utilx::runtime_cfg::CompositeParameter*
-  addParameterEventHandler(const utilx::runtime_cfg::CompositeParameter& context,
-                           utilx::runtime_cfg::CompositeParameter* added_parameter);
-
   virtual void notifyLinkShutdownCompletion();
+
+  void activateLinkToSGP(const common::LinkId& link_id,
+                         const std::vector<std::string>& r_addr,
+                         in_port_t r_port,
+                         const std::vector<std::string>& l_addr,
+                         in_port_t l_port);
 
 protected:
   MTP3Subsystem();
@@ -56,6 +51,8 @@ private:
   void configurePoints(utilx::runtime_cfg::RuntimeConfig& rconfig);
   void fillInRoutingTable(utilx::runtime_cfg::RuntimeConfig& rconfig);
   void activateLinksToSGP(utilx::runtime_cfg::RuntimeConfig& rconfig);
+  void addMtp3Route(common::point_code_t lpc, common::point_code_t dpc,
+                    const common::LinkId& sgp_link_id);
 };
 
 }}}}
