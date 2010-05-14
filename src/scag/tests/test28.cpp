c66ec4390a6f8621eef2541f2e06a986789092c3
bukind <>
2010-05-14 08:52:27 +0000

    test SMSCFILELINE

---
+++ src/scag/tests/test28.cpp
@@ -0,0 +1,11 @@
+#include "util/fileline.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << SMSCFILELINE << std::endl;
+    std::cout << SMSCFILELINE << std::endl;
+    std::cout << SMSCFILELINE << std::endl;
+    std::cout << SMSCFILELINE << std::endl;
+    return 0;
+}
