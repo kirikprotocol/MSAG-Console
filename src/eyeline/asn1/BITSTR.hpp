fc33601862073935c0f8e20e0b76935a07797447
gvroman <>
2010-06-18 06:29:59 +0000

    basic array functionality is signled out

--- src/eyeline/asn1/BITSTR.hpp
+++ src/eyeline/asn1/BITSTR.hpp
@@ -10,27 +10,60 @@
 namespace eyeline {
 namespace asn1 {
 
-using eyeline::util::BITArray_T;
+/* ---------------------------------------------------------- *
+ * Template for BIT STRING with maximum number of bits = 255.
+ * ---------------------------------------------------------- */
+static const uint8_t _BITSTR_TINY_DFLT_SZ = 16*8; //in bits
 
-static const uint16_t _BITSTR_DFLT_SZ = 256; //in bits
+typedef eyeline::util::BITArrayExtension_T<uint8_t> BITArrayTiny;
 
-//Template for BIT STRING with known maximum number of bits.
-//Whole BITSTR is allocated on stack at once
-template <uint16_t _MaxBitsTArg>
-class BITSTR_T : public BITArray_T<uint16_t, _MaxBitsTArg> {
+template <uint8_t _StackBitsTArg = _BITSTR_TINY_DFLT_SZ>
+class BITSTRTiny_T : public eyeline::util::BITArray_T<uint8_t, _StackBitsTArg> {
 public:
-  BITSTR_T(uint16_t num_bits = 0)
-    : BITArray_T<uint16_t, _MaxBitsTArg>(num_bits)
+  typedef eyeline::util::BITArrayExtension_T<uint8_t> ArrayType;
+  typedef eyeline::util::BITArrayExtension_T<uint8_t>::size_type size_type;
+
+  explicit BITSTRTiny_T(uint8_t num_bits = 0)
+    : eyeline::util::BITArray_T<uint8_t, _StackBitsTArg>(num_bits)
   { }
-  BITSTR_T(const BITSTR_T & use_arr)
-    : BITArray_T<uint16_t, _MaxBitsTArg>(use_arr)
+  explicit BITSTRTiny_T(const uint8_t use_bits, uint8_t num_bits)
+    : eyeline::util::BITArray_T<uint8_t, _StackBitsTArg>(use_bits, num_bits)
+  { }
+  BITSTRTiny_T(const BITSTR_T & use_arr)
+    : eyeline::util::BITArray_T<uint8_t, _StackBitsTArg>(use_arr)
+  { }
+  ~BITSTRTiny_T()
   { }
 };
+typedef BITSTRTiny_T<_BITSTR_TINY_DFLT_SZ> BITSTRTiny;
 
-//Generic BIT STRING, if number of bits excesses _BITSTR_DFLT_SZ
-//bit array is allocated on heap
+
+/* ---------------------------------------------------------- *
+ * Template for BIT STRING with maximum number of bits = 64k.
+ * ---------------------------------------------------------- */
+static const uint16_t _BITSTR_DFLT_SZ = 256*8; //in bits
+
+typedef eyeline::util::BITArrayExtension_T<uint16_t> BITArray64k;
+
+template <uint16_t _StackBitsTArg = _BITSTR_LRG_DFLT_SZ>
+class BITSTR_T : public eyeline::util::BITArray_T<uint16_t, _StackBitsTArg> {
+public:
+  typedef eyeline::util::BITArrayExtension_T<uint16_t> ArrayType;
+  typedef eyeline::util::BITArrayExtension_T<uint16_t>::size_type size_type;
+
+  explicit BITSTR_T(uint16_t num_bits = 0)
+    : eyeline::util::BITArray_T<uint16_t, _StackBitsTArg>(num_bits)
+  { }
+  explicit BITSTR_T(const uint16_t use_bits, uint16_t num_bits)
+    : eyeline::util::BITArray_T<uint16_t, _StackBitsTArg>(use_bits, num_bits)
+  { }
+  BITSTR_T(const BITSTR_T & use_arr)
+    : eyeline::util::BITArray_T<uint16_t, _StackBitsTArg>(use_arr)
+  { }
+  ~BITSTR_T()
+  { }
+};
 typedef BITSTR_T<_BITSTR_DFLT_SZ> BITSTR;
-
 
 } //asn1
 } //eyeline
--- src/eyeline/asn1/OCTSTR.hpp
+++ src/eyeline/asn1/OCTSTR.hpp
@@ -5,22 +5,66 @@
 #ident "@(#)$Id$"
 #define __ABSTRACT_SYNTAX_OCTSTR_DEFS__
 
-#include <inttypes.h>
-#include <sys/types.h>
 #include "eyeline/util/LWArray.hpp"
+#include "eyeline/util/LWArrayTraitsInt.hpp"
 
 namespace eyeline {
 namespace asn1 {
 
-static const uint8_t _OCTSTR_DFLT_SZ = 255;
-static const uint8_t _OCTSTR_ARR_DFLT_SZ = 32;
+/* ------------------------------------------------------------- *
+ * Template for OCTET STRING with maximum number of octets = 255.
+ * ------------------------------------------------------------- */
+typedef eyeline::util::LWArrayExtension_T<uint8_t, uint8_t> OCTArrayTiny;
 
-//Default unbounded representation, though overall number of octets is limited to 64K
-typedef eyeline::util::LWArray_T<uint8_t, uint16_t, _OCTSTR_DFLT_SZ> OCTSTR;
+static const uint8_t _OCTSTR_TINY_DFLT_SZ = 32;
 
-//Default constrained representation, overall number of octets is limited to 255
-typedef eyeline::util::LWArray_T<uint8_t, uint8_t, _OCTSTR_ARR_DFLT_SZ> OCTSTR_ARRAYED;
+template <uint8_t _SZ_TArg = _OCTSTR_TINY_DFLT_SZ>
+class OCTSTR_TINY_T : public eyeline::util::LWArray_T<uint8_t, uint8_t, _SZ_TArg> {
+public:
+  typedef eyeline::util::LWArrayExtension_T<uint8_t, uint8_t> ArrayType;
+  typedef eyeline::util::LWArrayExtension_T<uint8_t, uint8_t>::size_type size_type;
 
+  explicit OCTSTR_TINY_T(uint8_t num_to_reserve = 0)
+    : eyeline::util::LWArray_T<uint8_t, uint8_t, _SZ_TArg>(num_to_reserve)
+  { }
+  explicit OCTSTR_TINY_T(const uint8_t * use_buf, uint8_t num_elem)
+    : eyeline::util::LWArray_T<uint8_t, uint8_t, _SZ_TArg>(use_buf, num_elem)
+  { }
+  OCTSTR_TINY_T(const OCTSTR_TINY_T & use_arr)
+    : eyeline::util::LWArray_T<uint8_t, uint8_t, _SZ_TArg>(use_arr)
+  { }
+  ~OCTSTR_TINY_T()
+  { }
+};
+typedef OCTSTR_TINY_T<_OCTSTR_TINY_DFLT_SZ> OCTSTR_TINY;
+
+
+/* ------------------------------------------------------------- *
+ * Template for OCTET STRING with maximum number of octets = 64k.
+ * ------------------------------------------------------------- */
+typedef eyeline::util::LWArrayExtension_T<uint8_t, uint16_t> OCTArray64K;
+
+static const uint16_t _OCTSTR_DFLT_SZ = 255;
+
+template <uint16_t _SZ_TArg = _OCTSTR_TINY_DFLT_SZ>
+class OCTSTR_T : public eyeline::util::LWArray_T<uint8_t, uint16_t, _SZ_TArg> {
+public:
+  typedef eyeline::util::LWArrayExtension_T<uint8_t, uint16_t> ArrayType;
+  typedef eyeline::util::LWArrayExtension_T<uint8_t, uint16_t>::size_type size_type;
+
+  explicit OCTSTR_T(uint16_t num_to_reserve = 0)
+    : eyeline::util::LWArray_T<uint8_t, uint16_t, _SZ_TArg>(num_to_reserve)
+  { }
+  explicit OCTSTR_T(const uint16_t * use_buf, uint8_t num_elem)
+    : eyeline::util::LWArray_T<uint8_t, uint16_t, _SZ_TArg>(use_buf, num_elem)
+  { }
+  OCTSTR_T(const OCTSTR_T & use_arr)
+    : eyeline::util::LWArray_T<uint8_t, uint16_t, _SZ_TArg>(use_arr)
+  { }
+  ~OCTSTR_T()
+  { }
+};
+typedef OCTSTR_T<_OCTSTR_DFLT_SZ> OCTSTR;
 
 } //asn1
 } //eyeline
