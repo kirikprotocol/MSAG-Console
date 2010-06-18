6177c58a53aab0f334a40ff8b12b5d27265ede0b
gvroman <>
2010-06-18 09:58:58 +0000

    TCAP Continue message encoder

---
+++ src/eyeline/tcap/proto/enc/TEMsgTContinue.cpp
@@ -0,0 +1,17 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/tcap/proto/enc/TEMsgTContinue.hpp"
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+namespace enc {
+
+const asn1::ASTagging
+  TETContinue::_typeTags(asn1::ASTag::tagApplication,
+                    5, asn1::ASTagging::tagsIMPLICIT);
+
+}}}}
+
