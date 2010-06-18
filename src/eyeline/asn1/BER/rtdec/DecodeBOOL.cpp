592a3a4d34195eb7be407a524c4ad364c5be6590
gvroman <>
2010-06-18 07:30:00 +0000

    BOOLEAN type decoder

---
+++ src/eyeline/asn1/BER/rtdec/DecodeBOOL.cpp
@@ -0,0 +1,54 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/asn1/BER/rtdec/DecodeBOOL.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+/* ************************************************************************* *
+ * Decodes by BER the BOOLEAN value according to X.690 cl. 8.19
+ * ************************************************************************* */
+DECResult DecoderOfBOOL::decodeVAL(
+                    const TLVProperty * val_prop,
+                    const uint8_t * use_enc, TSLength max_len,
+                    TSGroupBER::Rule_e use_rule/* = TSGroupBER::ruleBER*/,
+                    bool relaxed_rule/* = false*/)
+  /*throw(std::exception)*/
+{
+  if (!val_prop)
+    throw smsc::util::Exception("ber::DecoderOfBOOL::decodeVal(): V-part properties isn't decoded");
+  if (!_dVal)
+    throw smsc::util::Exception("ber::DecoderOfBOOL::decodeVal(): value isn't set");
+
+  TSLength  valLen = val_prop->_valLen;
+  if (val_prop->isIndefinite()) { //check DER/CER restrictions
+    if (!relaxed_rule && (use_rule != TSGroupBER::ruleBER))
+      return DECResult(DECResult::decBadEncoding);
+
+    if (!valLen) {
+      DECResult reoc = searchEOC(use_enc, max_len);
+      if (!reoc.isOk())
+        return reoc;
+      valLen = reoc.nbytes;
+    }
+  }
+  if (valLen > 1)
+    return DECResult(DECResult::decBadEncoding);
+  //check DER/CER restrictions
+  if (*use_enc && (*use_enc != 0xFF) && !relaxed_rule
+      && ((use_rule == TSGroupBER::ruleCER)
+          || (use_rule == TSGroupBER::ruleDER)))
+    return DECResult(DECResult::decBadEncoding);
+
+  *_dVal = (*use_enc != 0);
+  return DECResult(DECResult::decOk, 1);
+}
+
+} //ber
+} //asn1
+} //eyeline
+
+
---
+++ src/eyeline/asn1/BER/rtdec/DecodeBOOL.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************* *
+ * BER Decoder: BOOLEAN type decoder.
+ * ************************************************************************* */
+#ifndef __ASN1_BER_DECODER_BOOL
+#ident "@(#)$Id: "
+#define __ASN1_BER_DECODER_BOOL
+
+#include "eyeline/asn1/BER/rtdec/TLVDecoder.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+/* ************************************************************************* *
+ * Encodes by BER/DER/CER the BOOLEAN value according to X.690
+ * clause 8.2 (with appropriate DER/CER restrctions).
+ * ************************************************************************* */
+class DecoderOfBOOL : public TypeValueDecoderAC {
+private:
+  bool * _dVal;
+
+protected:
+  // -- ------------------------------------------------- --
+  // -- ValueDecoderIface abstract methods are to implement
+  // -- ------------------------------------------------- --
+  virtual DECResult decodeVAL(const TLVProperty * val_prop,
+                              const uint8_t * use_enc, TSLength max_len,
+                              TSGroupBER::Rule_e use_rule = TSGroupBER::ruleBER,
+                              bool relaxed_rule = false)
+    /*throw(std::exception)*/;
+
+  //Tagged type referencing BOOLEAN.
+  // NOTE: eff_tags is a complete effective tagging of type!
+  DecoderOfBOOL(const ASTagging & eff_tags,
+                  TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : TypeValueDecoderAC(eff_tags, use_rule)
+    , _dVal(0)
+  { }
+
+public:
+  //Untagged BOOLEAN type encoder constructor. 
+  explicit DecoderOfBOOL(TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : TypeValueDecoderAC(asn1::_tagsBOOL, use_rule)
+    , _dVal(0)
+  { }
+  //Tagged BOOLEAN type encoder constructor
+  DecoderOfBOOL(const ASTag & use_tag, ASTagging::Environment_e tag_env,
+                     TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : TypeValueDecoderAC(use_tag, tag_env, asn1::_tagsBOOL, use_rule)
+    , _dVal(0)
+  { }
+  ~DecoderOfBOOL()
+  { }
+
+  void setValue(bool & use_val) { _dVal = &use_val; }
+};
+
+
+} //ber
+} //asn1
+} //eyeline
+
+#endif /* __ASN1_BER_DECODER_BOOL */
+
