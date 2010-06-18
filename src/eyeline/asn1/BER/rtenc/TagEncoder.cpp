54503d58f6e5ea7cb92040c7efecce3e88cf151a
gvroman <>
2010-06-18 07:19:06 +0000

    Tag encoding routines

---
+++ src/eyeline/asn1/BER/rtenc/TagEncoder.cpp
@@ -0,0 +1,71 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/util/IntConv.hpp"
+#include "eyeline/asn1/BER/rtenc/TagEncoder.hpp"
+#include "eyeline/asn1/BER/rtenc/EncodeIdents_impl.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+/* ************************************************************************* *
+ * Estimates length of BER encoding of ASN.1 Tag:
+ * Returns  number of bytes of resulted encoding or zero.
+ * ************************************************************************* */
+uint8_t estimate_tag(const ASTag & use_tag)
+{
+  if (use_tag._tagValue <= 30)  //short tag form
+    return 1;
+  return estimate_identifier(use_tag._tagValue);
+}
+
+/* ************************************************************************* *
+ * Composes the 'Tag-octets' according to X.690 clause 8.1.2.
+ * Returns zero in case of insuffient buffer is provided.
+ * NOTE: 'use_enc' & 'max_len' argument shouldn't be zero.
+ * ************************************************************************* */
+uint8_t compose_toc(const ASTag & use_tag, bool is_constructed,
+                                   uint8_t * use_enc, uint8_t max_len)
+{
+  uint8_t rval = 0;
+  if (use_tag._tagValue <= 30) {
+    //short tag form, compose 1st byte
+    *use_enc = (uint8_t)use_tag._tagValue;
+  } else {
+    //long tag form (tag > 30)
+    if (!(rval = encode_identifier(use_tag._tagValue, use_enc + 1, max_len - 1)))
+      return 0;
+    //compose 1st byte
+    *use_enc = 0x1F;
+  }
+  //complete 1st byte: class & constructedness
+  *use_enc |= (uint8_t)use_tag._tagClass << 6;
+  if (is_constructed)
+    *use_enc |= 0x20;
+  ++rval;
+  return rval;
+}
+
+/* ************************************************************************* *
+ * Encodes by BER the ASN.1 Tag according to X.690 clause 8.1.2.
+ * ************************************************************************* */
+ENCResult encode_tag(const ASTag & use_tag, bool is_constructed, 
+                     uint8_t * use_enc, TSLength max_len)
+{
+  ENCResult rval(ENCResult::encOk);
+
+  if (!max_len || !use_enc) {
+    rval.status = ENCResult::encBadArg;
+  } else if (!(rval.nbytes = compose_toc(use_tag, is_constructed, use_enc,
+                                                 DOWNCAST_UNSIGNED(max_len, uint8_t)))) {
+    rval.status = ENCResult::encMoreMem;
+  }
+  return rval;
+}
+
+} //ber
+} //asn1
+} //eyeline
+
---
+++ src/eyeline/asn1/BER/rtenc/TagEncoder.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************* *
+ * BER Encoder: Tag encoding methods.
+ * ************************************************************************* */
+#ifndef __ASN1_BER_TAG_ENCODER
+#ident "@(#)$Id$"
+#define __ASN1_BER_TAG_ENCODER
+
+#include "eyeline/asn1/ASNTags.hpp"
+#include "eyeline/asn1/TransferSyntax.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+using eyeline::asn1::ASTag;
+using eyeline::asn1::TSLength;
+using eyeline::asn1::ENCResult;
+
+/* ************************************************************************* *
+ * Estimates length of BER encoding of ASN.1 Tag.
+ * Returns number of bytes of resulted encoding.
+ * ************************************************************************* */
+extern uint8_t estimate_tag(const ASTag & use_tag);
+
+/* ************************************************************************* *
+ * Composes the 'Tag-octets' according to X.690 clause 8.1.2.
+ * Returns zero in case of insuffient buffer is provided.
+ * NOTE: 'use_enc' & 'max_len' arguments shouldn't be zero.
+ * ************************************************************************* */
+extern uint8_t compose_toc(const ASTag & use_tag, bool is_constructed,
+                                   uint8_t * use_enc, uint8_t max_len);
+
+/* ************************************************************************* *
+ * Encodes by BER the ASN.1 Tag according to X.690 clause 8.1.2.
+ * ************************************************************************* */
+extern ENCResult encode_tag(const ASTag & use_tag, bool is_constructed,
+                            uint8_t * use_enc, TSLength max_len);
+
+
+} //ber
+} //asn1
+} //eyeline
+
+#endif /* __ASN1_BER_TAG_ENCODER */
+
