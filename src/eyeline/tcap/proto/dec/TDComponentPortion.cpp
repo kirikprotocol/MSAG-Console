79a7c1e729207d91c43f91a5de1adce47c4b8a03
gvroman <>
2010-06-18 10:08:23 +0000

    TCAP Message component portion decoder

---
+++ src/eyeline/tcap/proto/dec/TDComponentPortion.cpp
@@ -0,0 +1,16 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/tcap/proto/dec/TDComponentPortion.hpp"
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+namespace dec {
+
+const asn1::ASTag
+  TDComponentPortion::_typeTag(asn1::ASTag::tagApplication, 12);
+
+}}}}
+
---
+++ src/eyeline/tcap/proto/dec/TDComponentPortion.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************* *
+ * TCAP Message Decoder: TCAP ComponentPortion type decoder.
+ * ************************************************************************* */
+#ifndef __EYELINE_TCAP_PROTO_DEC_COMPONENT_PORTION_HPP
+#ident "@(#)$Id$"
+#define __EYELINE_TCAP_PROTO_DEC_COMPONENT_PORTION_HPP
+
+#include "eyeline/tcap//TComponentDefs.hpp"
+#include "eyeline/tcap/proto/dec/TDComponent.hpp"
+#include "eyeline/asn1/BER/rtdec/DecodeSeqOfLinked.hpp"
+
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+namespace dec {
+
+//ComponentPortion is defined in IMPLICIT tagging environment as follow:
+//
+//ComponentPortion ::= [APPLICATION 12] SEQUENCE SIZE (1..MAX) OF Component
+
+class TDComponentPortion : public asn1::ber::DecoderOfSeqOfLinked_T<ros::ROSPdu, TDComponent> {
+public:
+  static const asn1::ASTag _typeTag; //[APPLICATION 12] IMPLICIT
+
+  explicit TDComponentPortion(TSGroupBER::Rule_e use_rule = TSGroupBER::ruleDER)
+    : asn1::ber::DecoderOfSeqOfLinked_T<ros::ROSPdu, TDComponent>(
+            _typeTag, asn1::ASTagging::tagsIMPLICIT, TSGroupBER::getTSRule(use_rule))
+  { }
+  ~TDComponentPortion()
+  { }
+};
+
+
+}}}}
+
+#endif /* __EYELINE_TCAP_PROTO_DEC_COMPONENT_PORTION_HPP */
+
