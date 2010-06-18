6eb723895758143588bcf2c617947760553127cb
gvroman <>
2010-06-18 07:32:35 +0000

    NULL type decoder

---
+++ src/eyeline/asn1/BER/rtdec/DecodeNULL.cpp
@@ -0,0 +1,52 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/asn1/BER/rtdec/DecodeNULL.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+/* ************************************************************************* *
+ * Class DecoderOfNULL implementation:
+ * ************************************************************************* */
+
+//NOTE: encoding of NULL type value has the same form for all BER
+//family rules: primitive encoding(0b) with definite LD form
+
+// -- ************************************* --
+// -- ValueDecoderIface interface methods
+// -- ************************************* --
+
+DECResult DecoderOfNULL::decodeVAL(const TLVProperty * val_prop,
+                    const uint8_t * use_enc, TSLength max_len,
+                    TSGroupBER::Rule_e use_rule/* = TSGroupBER::ruleBER*/,
+                    bool relaxed_rule/* = false*/)
+  /*throw(BERDecoderException)*/
+{
+  if (!val_prop)
+    throw smsc::util::Exception("ber::DecoderOfNULL::decodeVAL(): V-part properties isn't decoded");
+
+  DECResult rval(DECResult::decBadEncoding);
+  TSLength  valLen = val_prop->_valLen;
+
+  if (val_prop->isIndefinite()) { //check DER/CER restrictions
+    if (!relaxed_rule && (use_rule != TSGroupBER::ruleBER))
+      return rval;
+
+    DECResult reoc = searchEOC(use_enc, max_len);
+    if (!reoc.isOk())
+      return reoc;
+    valLen = reoc.nbytes;
+  }
+  if (!valLen)
+    rval.status = DECResult::decOk;
+  return rval;
+}
+
+
+} //ber
+} //asn1
+} //eyeline
+
---
+++ src/eyeline/asn1/BER/rtdec/DecodeNULL.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************* *
+ * BER Decoder: NULL type decoder.
+ * ************************************************************************* */
+#ifndef __ASN1_BER_DECODER_NULL
+#ident "@(#)$Id: "
+#define __ASN1_BER_DECODER_NULL
+
+#include "eyeline/asn1/BER/rtdec/TLVDecoder.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+/* ************************************************************************* *
+ * Encodes by BER/DER/CER the BOOLEAN value according to X.690
+ * clause 8.8 (with appropriate DER/CER restrctions).
+ * NOTE: if ASTagging is not set the standard [UNIVERSAL 5] tag goes to
+ * resulting TLV encoding.
+ * ************************************************************************* */
+class DecoderOfNULL : public TypeValueDecoderAC {
+protected:
+  // -- ************************************* --
+  // -- ValueDecoderIface interface methods
+  // -- ************************************* --
+  virtual DECResult decodeVAL(const TLVProperty * val_prop,
+                              const uint8_t * use_enc, TSLength max_len,
+                              TSGroupBER::Rule_e use_rule = TSGroupBER::ruleBER,
+                              bool relaxed_rule = false)
+    /*throw(BERDecoderException)*/;
+
+public:
+  //Constructor for NULL type
+  DecoderOfNULL(TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : TypeValueDecoderAC(asn1::_tagsNULL, use_rule)
+  { }
+  //Constructor for tagged NULL type
+  DecoderOfNULL(const ASTag & use_tag, ASTagging::Environment_e tag_env,
+                TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : TypeValueDecoderAC(ASTagging(use_tag, tag_env, asn1::_tagsNULL), use_rule)
+  { }
+  ~DecoderOfNULL()
+  { }
+};
+
+} //ber
+} //asn1
+} //eyeline
+
+#endif /* __ASN1_BER_DECODER_NULL */
+
