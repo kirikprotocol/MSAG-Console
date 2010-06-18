37bb07b8a29465fefa32ef24d1b40decdd0e3b6b
gvroman <>
2010-06-18 07:43:33 +0000

    CHOICE type decoder

---
+++ src/eyeline/asn1/BER/rtdec/DecodeChoice.cpp
@@ -0,0 +1,74 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/asn1/BER/rtdec/DecodeChoice.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+/* ************************************************************************* *
+ * Class DecoderOfChoiceAC implementation:
+ * ************************************************************************* */
+DECResult DecoderOfChoiceAC::decodeElement(
+                                const uint8_t * use_enc, TSLength max_len,
+                                bool relaxed_rule)
+  /*throw(std::exception)*/
+{
+  TLParser  vtl;
+  DECResult rval = vtl.decodeBOC(use_enc, max_len);
+  if (!rval.isOk(relaxed_rule))
+    return rval;
+
+  ElementDecoderAC::EDAResult edRes = _elDec->processElement(vtl._tag);
+  if (!edRes.isOk()) {
+    rval.status = DECResult::decBadEncoding;
+  } else {
+    TypeDecoderAC * tDec =  prepareAlternative(edRes._alt->getUId());
+
+    if (edRes._alt->getEnv() != EDAlternative::atgExplicit)
+      tDec->setOutermostTL(vtl);
+    rval += tDec->decode(use_enc + rval.nbytes, max_len - rval.nbytes);
+
+    if (rval.isOk(relaxed_rule)) {
+      markDecodedAlternative(edRes._alt->getUId());
+      //in case of explicitly tagged alternative eat EOC's 
+      if ((edRes._alt->getEnv() == EDAlternative::atgExplicit)
+          && vtl.isIndefinite()) {
+        if (checkEOC(use_enc + rval.nbytes, max_len - rval.nbytes))
+          rval.nbytes += 2; 
+        else
+          rval.status = DECResult::decBadEncoding;
+      }
+    }
+  }
+  return rval;
+}
+
+//NOTE: in case of untagged CHOICE, val_prop may be == 0
+DECResult DecoderOfChoiceAC::decodeVAL(
+                      const TLVProperty * val_prop,
+                      const uint8_t * use_enc, TSLength max_len,
+                      TSGroupBER::Rule_e use_rule/* = TSGroupBER::ruleBER*/,
+                      bool relaxed_rule/* = false*/)
+    /*throw(std::exception)*/
+{
+  if (val_prop && val_prop->isDefinite())
+    max_len = val_prop->_valLen;
+
+  DECResult rval = decodeElement(use_enc, max_len, relaxed_rule);
+
+  if (val_prop && val_prop->isIndefinite()) {
+    if (checkEOC(use_enc + rval.nbytes, max_len - rval.nbytes))
+      rval.nbytes += 2; 
+    else
+      rval.status = DECResult::decBadEncoding;
+  }
+  return rval;
+}
+
+} //ber
+} //asn1
+} //eyeline
+
---
+++ src/eyeline/asn1/BER/rtdec/DecodeChoice.hpp
@@ -0,0 +1,158 @@
+/* ************************************************************************* *
+ * BER Decoder: CHOICE type decoder.
+ * ************************************************************************* */
+#ifndef __ASN1_BER_DECODER_CHOICE
+#ident "@(#)$Id$"
+#define __ASN1_BER_DECODER_CHOICE
+
+#include "eyeline/asn1/BER/rtdec/ElementDecoderOfCHC.hpp"
+#include "eyeline/asn1/BER/rtdec/TLVDecoder.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+class DecoderOfChoiceAC : public TypeValueDecoderAC {
+private:
+  TaggingOptions        _altTags; //taggings of CHOICE alternatives
+  CHCElementDecoderAC * _elDec;   //actually it's just a reference to a successor
+                                  //member, so its copying constructor MUST properly
+                                  //that pointer.
+protected:
+
+  DECResult decodeElement(const uint8_t * use_enc, TSLength max_len,
+                          bool relaxed_rule) /*throw(std::exception)*/;
+
+  // -- ************************************************* --
+  // -- ValueDecoderIface abstract methods are to implement
+  // -- ************************************************* --
+  virtual DECResult decodeVAL(const TLVProperty * val_prop,
+                      const uint8_t * use_enc, TSLength max_len,
+                      TSGroupBER::Rule_e use_rule = TSGroupBER::ruleBER,
+                      bool relaxed_rule = false)
+    /*throw(std::exception)*/;
+
+  // ----------------------------------------
+  // -- DecoderOfChoiceAC interface methods
+  // ----------------------------------------
+  //Allocates alternative data structure and initializes associated TypeDecoderAC
+  virtual TypeDecoderAC * prepareAlternative(uint16_t unique_idx)
+    /*throw(throw(std::exception)) */ = 0;
+  //Perfoms actions finalizing alternative decoding
+  virtual void markDecodedAlternative(uint16_t unique_idx)
+    /*throw(throw(std::exception)) */ { return; }
+
+  void setElementDecoder(CHCElementDecoderAC & elm_dec) { _elDec = &elm_dec; }
+
+  //NOTE: copying constructor of successsor MUST properly set _elDec
+  //      by calling setElementDecoder()
+  DecoderOfChoiceAC(const DecoderOfChoiceAC & use_obj)
+    : TypeValueDecoderAC(use_obj), _elDec(0)
+  {
+    setOptions(_altTags);
+  }
+
+public:
+  DecoderOfChoiceAC(CHCElementDecoderAC & use_eldec,
+                    TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : TypeValueDecoderAC(&_altTags, use_rule), _elDec(&use_eldec)
+  {
+    //NOTE.1: in case of untagged CHOICE, tagging of canonical alternative
+    //        MUST BE added by setAlternative() to alternative's tagging
+    //        options in successor's constructor in order to support CER !!!
+  }
+  // NOTE: eff_tags is a complete effective tagging of type!
+  DecoderOfChoiceAC(CHCElementDecoderAC & use_eldec, const ASTagging & eff_tags,
+                    TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : TypeValueDecoderAC(eff_tags, _altTags, use_rule), _elDec(&use_eldec)
+  { }
+  ~DecoderOfChoiceAC()
+  { }
+
+  //Sets tagged alternative of some type
+  void setAlternative(uint16_t unique_idx, const ASTag & fld_tag, ASTagging::Environment_e fld_env)
+    /*throw(std::exception)*/
+  {
+    if (!isTagged()) //untagged CHOICE
+      _altTags.addTagging(fld_tag, fld_env);
+    _elDec->setAlternative(unique_idx, fld_tag, fld_env, EDAlternative::altOPTIONAL);
+  }
+  //Sets untagged alternative of ordinary type
+  void setAlternative(uint16_t unique_idx, const ASTag & fld_tag)
+    /*throw(std::exception)*/
+  {
+    if (!isTagged()) //untagged CHOICE
+      _altTags.addTagging(fld_tag, ASTagging::tagsIMPLICIT);
+    _elDec->setAlternative(unique_idx, fld_tag, EDAlternative::altOPTIONAL);
+  }
+  //Sets untagged alternative of ANY/Opentype
+  void setAlternative(uint16_t unique_idx)
+    /* throw(std::exception)*/
+  {
+    if (!isTagged()) //untagged CHOICE
+      _altTags.addTagging(asn1::_tagANYTYPE, ASTagging::tagsIMPLICIT);
+    _elDec->setAlternative(unique_idx, EDAlternative::altOPTIONAL);
+  }
+  //Sets untagged alternative of untagged CHOICE type
+  void setAlternative(uint16_t unique_idx, const TaggingOptions & use_tag_opts)
+    /*throw(std::exception)*/
+  {
+    if (!isTagged()) //untagged CHOICE
+      _altTags.addOptions(use_tag_opts);
+    _elDec->setAlternative(unique_idx, use_tag_opts, EDAlternative::altOPTIONAL);
+  }
+  //Sets alternative for unknown extension additions entry
+  void setUnkExtension(uint16_t unique_idx) /* throw(std::exception)*/
+  {
+    if (!isTagged()) //untagged CHOICE
+      _altTags.addTagging(asn1::_tagUNI0, ASTagging::tagsIMPLICIT);
+    _elDec->setUnkExtension(unique_idx);
+  }
+};
+
+
+template <uint16_t _SizeTArg>
+class DecoderOfChoice_T : public DecoderOfChoiceAC {
+protected:
+  CHCElementDecoder_T<_SizeTArg>  _chcDec;
+
+  // ----------------------------------------
+  // -- DecoderOfChoiceAC interface methods
+  // ----------------------------------------
+  //Allocates alternative data structure and initializes associated TypeDecoderAC
+  //virtual TypeDecoderAC * prepareAlternative(uint16_t unique_idx)
+  //  /*throw(throw(std::exception)) */ = 0;
+  //Perfoms actions finalizing alternative decoding
+  //virtual void markDecodedAlternative(uint16_t unique_idx)
+  //  /*throw(throw(std::exception)) */ { return; }
+
+public:
+  // constructor for untagged CHOICE
+  DecoderOfChoice_T(TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : DecoderOfChoiceAC(_chcDec, use_rule)
+  {
+    //NOTE.1: in case of untagged CHOICE, tagging of canonical alternative
+    //        MUST BE added by setAlternative() to alternative's tagging
+    //        options in successor's constructor in order to support CER !!!
+  }
+  // constructor for tagged CHOICE
+  DecoderOfChoice_T(const ASTag & use_tag, ASTagging::Environment_e tag_env,
+                  TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+    : DecoderOfChoiceAC(_chcDec, ASTagging(use_tag, tag_env), use_rule)
+  { }
+  //
+  DecoderOfChoice_T(const DecoderOfChoice_T & use_obj)
+    : DecoderOfChoiceAC(use_obj), _chcDec(use_obj._chcDec)
+  {
+    setElementDecoder(_chcDec);
+  }
+  ~DecoderOfChoice_T()
+  { }
+};
+
+} //ber
+} //asn1
+} //eyeline
+
+#endif /* __ASN1_BER_ENCODER_CHOICE */
+
