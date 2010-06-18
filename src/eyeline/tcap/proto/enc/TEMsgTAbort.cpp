eb6351e9d576318068d11b1cc82746ab6363b5cb
gvroman <>
2010-06-18 09:58:06 +0000

    TCAP Abort message encoder

---
+++ src/eyeline/tcap/proto/enc/TEMsgTAbort.cpp
@@ -0,0 +1,17 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/tcap/proto/enc/TEMsgTAbort.hpp"
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+namespace enc {
+
+const asn1::ASTagging
+  TETAbort::_typeTags(asn1::ASTag::tagApplication,
+                    7, asn1::ASTagging::tagsIMPLICIT);
+
+}}}}
+
