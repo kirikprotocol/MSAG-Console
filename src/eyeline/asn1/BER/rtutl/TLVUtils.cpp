5a188ebae45296a7e390b31303caddc8cc639fbc
gvroman <>
2010-06-18 06:59:21 +0000

    TLV parsing utility routines

---
+++ src/eyeline/asn1/BER/rtutl/TLVUtils.cpp
@@ -0,0 +1,101 @@
+#ifndef MOD_IDENT_OFF
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_OFF */
+
+#include "eyeline/asn1/BER/rtutl/TLVUtils.hpp"
+#include "eyeline/asn1/BER/rtutl/DecodeIdents_impl.hpp"
+#include "eyeline/asn1/BER/rtutl/DecodeIntCOC_impl.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+/* ************************************************************************* *
+ * Class TLParser implementation:
+ * ************************************************************************* */
+DECResult TLParser::decode_tag(ASTag & use_tag, bool & is_constructed, 
+                     const uint8_t * use_enc, TSLength max_len)
+{
+  DECResult rval(DECResult::decOk);
+
+  if (!max_len) {
+    rval.status = DECResult::decMoreInput;
+    return rval;
+  }
+
+  //decompose tag value
+  rval.nbytes = 1;
+  if ((use_enc[0] & 0x1F) < 0x1F) { //short tag
+    use_tag._tagValue = use_enc[0] & 0x1F;
+  } else {                          //long tag
+    rval += decode_identifier(use_tag._tagValue, use_enc + 1, max_len - 1);
+  }
+  //decompose class & constructedness
+  is_constructed = ((use_enc[0] & 0x20) != 0);
+  use_tag._tagClass = static_cast<ASTag::TagClass_e>(use_enc[0] & 0xC0);
+
+  return rval;
+}
+
+//Decodes 'Length determinant'
+DECResult TLParser::decode_ld(LDeterminant & use_ld,
+                             const uint8_t * use_enc, TSLength max_len)
+{
+  if (!max_len)
+    return DECResult(DECResult::decMoreInput);
+
+  DECResult rval(DECResult::decOk, 1);
+
+  if (use_enc[0] < 0x80) { //definite, short form
+    use_ld._ldForm = LDeterminant::frmDefinite;
+    use_ld._valLen = use_enc[0];
+    return rval;
+  }
+  if (use_enc[0] == 0x80) { //indefinite
+    use_ld._ldForm = LDeterminant::frmIndefinite;
+    use_ld._valLen = 0;
+    return rval;
+  }
+  //if (enc[0] > 0x80) //definite, long form
+  TSLength ldLen = use_enc[0] & 0x7F;
+  if (ldLen > (max_len - 1)) {
+    rval.status = DECResult::decMoreInput;
+    return rval;
+  }
+  return rval += decodeCOC_UINTEGER(use_ld._valLen, use_enc + 1, ldLen);
+}
+
+//Decodes 'begin-of-content' octets ('T'+'L') of TLV encoding
+//NOTE: result may be DECResult::decOkRelaxed!
+DECResult TLParser::decodeBOC(const uint8_t * use_enc, TSLength max_len)
+{
+  DECResult rval = decodeTOC(use_enc, max_len);
+  if (rval.isOk())
+    rval += decodeLOC(use_enc + rval.nbytes, max_len - rval.nbytes);
+  return rval;
+}
+
+//Decodes 'end-of-content' octets of TLV encoding
+DECResult TLParser::decodeEOC(const uint8_t * use_enc, TSLength max_len) const
+{
+  DECResult rval(DECResult::decOk);
+  if (isDefinite())
+    return rval;
+
+  if (max_len < 2) {
+    rval.nbytes += 1;
+    rval.status = use_enc[0] ?
+                  DECResult::decBadEncoding : DECResult::decMoreInput;
+  } else {
+    rval.nbytes += 2;
+
+    rval.status = !(use_enc[0] || use_enc[1]) ? 
+                  DECResult::decBadEncoding : DECResult::decMoreInput;
+  }
+  return rval;
+}
+
+} //ber
+} //asn1
+} //eyeline
+
---
+++ src/eyeline/asn1/BER/rtutl/TLVUtils.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************* *
+ * BER Runtime: Various TLV utilities.
+ * ************************************************************************* */
+#ifndef __ASN1_BER_TLV_UTILS
+#ident "@(#)$Id$"
+#define __ASN1_BER_TLV_UTILS
+
+#include "eyeline/asn1/ASNTags.hpp"
+#include "eyeline/asn1/BER/rtutl/TLVProperties.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+using eyeline::asn1::ASTag;
+using eyeline::asn1::TSLength;
+using eyeline::asn1::DECResult;
+
+/* ************************************************************************* *
+ * 'TL'-part decomposer 
+ * ************************************************************************* */
+struct TLParser : public TLVStruct {
+  ASTag   _tag;
+
+  TLParser() : TLVStruct()
+  { }
+  ~TLParser()
+  { }
+
+  //Decodes by BER the ASN.1 Tag according to X.690 clause 8.1.2.
+  static DECResult decode_tag(ASTag & use_tag, bool & is_constructed,
+                              const uint8_t * use_enc, TSLength max_len);
+
+  //Decodes 'Length determinant'
+  static DECResult decode_ld(LDeterminant & use_ld,
+                             const uint8_t * use_enc, TSLength max_len);
+
+  //Decodes 'Tag' octets of TLV encoding
+  DECResult decodeTOC(const uint8_t * use_enc, TSLength max_len) /*throw()*/
+  {
+    return decode_tag(_tag, _isConstructed, use_enc, max_len);
+  }
+  //Decodes 'Length' octets of TLV encoding
+  DECResult decodeLOC(const uint8_t * use_enc, TSLength max_len) /*throw()*/
+  {
+    return decode_ld(*this, use_enc, max_len);
+  }
+
+  //Decodes 'begin-of-content' octets ('T'+'L') of TLV encoding
+  //NOTE: result may be DECResult::decOkRelaxed!
+  DECResult decodeBOC(const uint8_t * use_enc, TSLength max_len) /*throw()*/;
+  //Decodes 'end-of-content' octets of TLV encoding
+  DECResult decodeEOC(const uint8_t * use_enc, TSLength max_len) const /*throw()*/;
+};
+
+
+} //ber
+} //asn1
+} //eyeline
+
+#endif /* __ASN1_BER_TLV_UTILS */
+
