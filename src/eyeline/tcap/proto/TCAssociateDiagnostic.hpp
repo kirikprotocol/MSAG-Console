78765fb8709e78b4e7948569cf3869f3de9751fc
gvroman <>
2010-06-18 09:38:27 +0000

    code cleanUp

--- src/eyeline/tcap/proto/TCAssociateDiagnostic.hpp
+++ src/eyeline/tcap/proto/TCAssociateDiagnostic.hpp
@@ -13,45 +13,38 @@ namespace proto {
 
 using eyeline::tcap::TDialogueAssociate;
 
-class AssociateSourceDiagnostic : public TDialogueAssociate {
-public:
-  enum DiagnosticKind_e {
-    dlgSrvUser = 0x01, dlgSrvPrvd = 0x02
+struct AssociateSourceDiagnostic : public TDialogueAssociate {
+  enum SourceKind_e {
+    dlgSrvUser = 0x00, dlgSrvPrvd = 0x01
   };
+  SourceKind_e  _sdKind;
+  Diagnostic_t  _sdResult;
 
-protected:
-  DiagnosticKind_e  _sdKind;
-  union {
-    DiagnosticUser_e     user;
-    DiagnosticProvider_e prvd;
-  } _dsDiagnostic;
-
-public:
   AssociateSourceDiagnostic()
   {
-    setUserDiagnostic(dsu_null); //default: accepted by user 
+    setUserDiagnostic(dsu_null); //default: accepted by user
   }
 
   void setUserDiagnostic(DiagnosticUser_e use_diagnostic = dsu_null)
   {
     _sdKind = dlgSrvUser;
-    _dsDiagnostic.user = use_diagnostic;
+    _sdResult = use_diagnostic;
   }
   void setPrvdDiagnostic(DiagnosticProvider_e use_diagnostic = dsp_null)
   {
     _sdKind = dlgSrvPrvd;
-    _dsDiagnostic.prvd = use_diagnostic;
+    _sdResult = use_diagnostic;
   }
 
-  DiagnosticKind_e diagnosticKind(void) const { return _sdKind; }
+  SourceKind_e getKind(void) const { return _sdKind; }
 
   DiagnosticUser_e getUserDiagnostic(void) const
   {
-    return _dsDiagnostic.user;
+    return static_cast<DiagnosticUser_e>(_sdResult);
   }
   DiagnosticProvider_e getPrvdDiagnostic(void) const
   {
-    return _dsDiagnostic.prvd;
+    return static_cast<DiagnosticProvider_e>(_sdResult);
   }
 };
 
