601b9a9ce67def3972457d0c58502ac3ef3aa757
gvroman <>
2010-06-18 06:33:05 +0000

    EXTERNAL type uses EmbeddedEncoding helper

--- src/eyeline/asn1/ASExternal.hpp
+++ src/eyeline/asn1/ASExternal.hpp
@@ -5,152 +5,204 @@
 #ident "@(#)$Id$"
 #define __ABSTRACT_SYNTAX_EXTERNAL_DEFS__
 
-#include "eyeline/asn1/AbstractSyntax.hpp"
-#include "eyeline/util/MaxSizeof.hpp"
+#include <string>
+
+#include "eyeline/asn1/EncodedOID.hpp"
+#include "eyeline/asn1/EmbdEncoding.hpp"
 
 namespace eyeline {
 namespace asn1 {
 
-//EXTERNAL type value
-class ASExternalValue {
-public:
-  enum ValueKind_e { valNone = 0, valAS, valEnc };
-
-  //encoding of indirectly referenced type
-  struct IRefEncoding {
-    uint32_t  typeId;
-    BITBuffer typeEnc;
-
-    IRefEncoding() : typeId(0)
-    { }
-    ~IRefEncoding()
-    { }
+class DataIdentification {
+  enum IdMask_e {
+    maskAS = 0x01, maskPC = 0x02, maskTS = 0x04
+  };
+  enum Kind_e {
+      idFixed = 0     //Data is the value of a fixed ASN.1 type
+                      //(and hence known to both sender and receiver)
+    , idASyntax = maskAS    //Data is identified by abstract syntaz with
+                      //predefined transfer syntax
+    , idPrsContext = maskPC //The negotiated OSI presentation context identifies
+                      //the abstract and transfer syntaxes
+    , idTSyntax = maskTS    //The data is the value of a fixed ASN.1 type
+                      //(and hence known to both sender and receiver)
+                      //represented according to given transfer syntax
+    , idSyntaxes  = (maskAS | maskTS)  //Data is identified by both abstract
+                      //syntax and transfer syntax
+    , idCtxNegotiation = (maskPC | maskTS) //Data is identified by abstract 
+                      //syntax, predefined for given presentation context,
+                      //and transfer syntax
   };
 
-private:
-  uint8_t objMem[eyeline::util::MaxSizeOf2_T<AbstractSyntaxRfp, IRefEncoding>::VALUE];
-
 protected:
-  ValueKind_e _kind;
-  std::string _descr;
-  union {
-    void              * ptr;
-    AbstractSyntaxRfp * pAs;  //direct typeRef (OID)
-    IRefEncoding      * pEnc; //indirect typeref (some integer ID)
-  } _pVal;
-
-  void resetDescr(const char * use_descr = NULL)
-  {
-    if (use_descr)
-      _descr = use_descr;
-    else
-      _descr.clear();
-  }
-  void resetObj(ValueKind_e use_kind = valNone)
-  {
-    if (_pVal.ptr ) {
-      if (_kind == valAS)
-        _pVal.pAs->~AbstractSyntaxRfp();
-      else if (_kind == valEnc)
-        _pVal.pEnc->~IRefEncoding();
-      _pVal.ptr = 0;
-    }
-    if ((_kind = use_kind) == valAS)
-      _pVal.pAs = new(objMem)AbstractSyntaxRfp();
-    else if (_kind == valEnc)
-      _pVal.pEnc = new(objMem)IRefEncoding();
-  }
+  Kind_e    _kind;
 
 public:
-  ASExternalValue() : _kind(valNone)
+  DataIdentification() : _kind(idFixed)
+  { }
+
+  Kind_e getKind(void) const { return _kind; }
+
+  //returns true if data identification uses AbstractSyntax OID
+  bool hasASyntaxOID(void) const { return (_kind & (unsigned)maskAS) != 0; }
+  //returns true if data identification uses TransferSyntax OID
+  bool hasTSyntaxOID(void) const { return (_kind & (unsigned)maskTS) != 0; }
+  //returns true if data identification uses presentation context Id
+  bool hasPrsContextId(void) const { return (_kind & (unsigned)maskPC) != 0; }
+
+  void setASyntaxOID(void)
   {
-    _pVal.ptr = 0;
+     _kind = static_cast<Kind_e>((unsigned)_kind | maskAS);
   }
-  ASExternalValue(const ASExternalValue & use_ext)
-    : _kind(valNone)
+  void setTSyntaxOID(void)
   {
-    _pVal.ptr = 0;
-    if (use_ext._kind == valEnc)
-      setEncoding(*(use_ext._pVal.pEnc), use_ext._descr.c_str());
-    else if (use_ext._kind == valAS)
-      setASyntax(*(use_ext._pVal.pAs), use_ext._descr.c_str());
-    else
-      resetObj(valNone);
+     _kind = static_cast<Kind_e>((unsigned)_kind | maskTS);
+  }
+  void setPrsContext(void)
+  {
+     _kind = static_cast<Kind_e>((unsigned)_kind | maskPC);
   }
 
-  ASExternalValue(const AbstractSyntaxRfp & use_astyp, const char * use_descr = NULL)
-    : _kind(valNone)
-  {
-    _pVal.ptr = 0;
-    setASyntax(use_astyp, use_descr);
-  }
-  ASExternalValue(uint32_t use_irId, const BITBuffer & use_enc, const char * use_descr = NULL)
-    :  _kind(valNone)
-  {
-    _pVal.ptr = 0;
-    setEncoding(use_irId, use_enc, use_descr);
-  }
+  void identifyByFixed(void)          { _kind = idFixed; }
+  void identifyByASyntax(void)        { _kind = idASyntax; }
+  void identifyByPrsContext(void)     { _kind = idPrsContext; }
+  void identifyByTSyntax(void)        { _kind = idTSyntax; }
+  void identifyBySyntaxes(void)       { _kind = idSyntaxes; }
+  void identifyByCtxNegotiation(void) { _kind = idCtxNegotiation; }
 
-  ~ASExternalValue()
-  {
-    resetObj(valNone);
-  }
-
-  void setASyntax(const AbstractSyntaxRfp & use_astyp, const char * use_descr = NULL)
-  {
-    resetObj(valAS);
-    *_pVal.pAs = use_astyp;
-    resetDescr(use_descr);
-  }
-  void setEncoding(uint32_t use_irId, const BITBuffer & use_enc, const char * use_descr = NULL)
-  {
-    resetObj(valEnc);
-    _pVal.pEnc->typeId = use_irId;
-    _pVal.pEnc->typeEnc = use_enc;
-    resetDescr(use_descr);
-  }
-  void setEncoding(const IRefEncoding & use_enc, const char * use_descr = NULL)
-  {
-    resetObj(valEnc);
-    *_pVal.pEnc = use_enc;
-    resetDescr(use_descr);
-  }
-
-  ValueKind_e Kind(void) const { return _kind; }
-  //
-  const std::string & getDescriptor(void) const { return _descr; }
-  //
-  const AbstractSyntaxRfp * getASyntax(void) const
-  {
-    return (_kind == valAS) ? _pVal.pAs : 0;
-  }
-  //
-  const IRefEncoding * getEncoding(void) const
-  {
-    return (_kind == valEnc) ? _pVal.pEnc : 0;
-  }
+  void resetIdentification(void) { _kind = idFixed; }
 };
 
 
-//ASN.1 1997 EXTERNAL type implementation
-class ASExternal : public ASExternalValue, public ASTypeAC {
+/* -------------------------------------------------------------------------- *
+ * Generic associated type for all ASN types dealing with externally defined
+ * data values (Empbedded Presentation Data Value, X.680 cl.33)
+ * -------------------------------------------------------------------------- */
+class EmbeddedPDV {
 public:
-  ASExternal()
-    : ASExternalValue(), ASTypeAC(ASTag::tagUniversal, 8)
+  DataIdentification  _ident;
+  EmbeddedEncoding    _enc;
+  EncodedOID          _asOid;
+  EncodedOID          _tsOid;
+  int32_t             _prsCtxId;
+
+  EmbeddedPDV() : _prsCtxId(0)
   { }
-  ASExternal(const AbstractSyntaxRfp & use_astyp, const char * use_descr = NULL)
-    : ASExternalValue(use_astyp, use_descr), ASTypeAC(ASTag::tagUniversal, 8)
+  ~EmbeddedPDV()
   { }
-  ASExternal(uint32_t use_uid, const BITBuffer & use_enc, const char * use_descr = NULL)
-    : ASExternalValue(use_uid, use_enc, use_descr), ASTypeAC(ASTag::tagUniversal, 8)
-  { }
-  ASExternal(const ASExternalValue & use_val)
-    : ASExternalValue(use_val), ASTypeAC(ASTag::tagUniversal, 8)
-  { }
-  virtual ~ASExternal()
-  { }
+
+  void identifyByFixed(void)
+  {
+    _ident.resetIdentification();
+    _asOid.reset();
+    _tsOid.reset();
+    _prsCtxId = 0;
+  }
+
+  void identifyByASyntax(const EncodedOID & as_oid)
+  {
+    _ident.identifyByASyntax();
+    _asOid = as_oid;
+    _tsOid.reset();
+    _prsCtxId = 0;
+  }
+  void identifyByPrsContext(int32_t prs_ctx_id)
+  {
+    _ident.identifyByPrsContext();
+    _asOid.reset();
+    _tsOid.reset();
+    _prsCtxId = prs_ctx_id;
+  }
+  void identifyByTSyntax(const EncodedOID & ts_oid)
+  {
+    _ident.identifyByTSyntax();
+    _asOid.reset();
+    _tsOid = ts_oid;
+    _prsCtxId = 0;
+  }
+  void identifyBySyntaxes(const EncodedOID & as_oid, const EncodedOID & ts_oid)
+  {
+    _ident.identifyBySyntaxes();
+    _asOid = as_oid;
+    _prsCtxId = 0;
+    _tsOid = ts_oid;
+  }
+  void identifyByCtxNegotiation(int32_t prs_ctx_id, const EncodedOID & ts_oid)
+  {
+    _ident.identifyByCtxNegotiation();
+    _asOid.reset();
+    _tsOid = ts_oid;
+    _prsCtxId = prs_ctx_id; 
+  }
 };
 
+/* -------------------------------------------------------------------------- *
+ * ASN.1 type EXTERNAL - deals with types defined by some AbstractSyntax
+ * (X.680 cl.34).
+ * -------------------------------------------------------------------------- */
+class ASExternal {
+protected:
+  //NOTE: idFixed & TransferSyntaxOID can't be used as value identification!!!
+  DataIdentification  _ident;
+
+public:
+  EmbeddedEncoding    _enc;
+  EncodedOID          _asOid;
+  int32_t             _prsCtxId;
+  std::string         _descr;
+
+  ASExternal() : _prsCtxId(0)
+  { }
+  ~ASExternal()
+  { }
+
+  void reset(void)
+  {
+    _ident.identifyByFixed();
+    _enc.reset();
+    _asOid.reset();
+    _prsCtxId = 0;
+    _descr.clear();
+  }
+
+  const DataIdentification & getIdentification(void) const { return _ident; }
+
+  //returns true if data identification uses AbstractSyntax OID
+  bool hasASyntaxOID(void) const { return _ident.hasASyntaxOID(); }
+  //returns true if data identification uses presentation context Id
+  bool hasPrsContextId(void) const { return _ident.hasPrsContextId(); }
+
+  void identifyByASyntax(void)      { _ident.identifyByASyntax(); }
+  void identifyByASyntax(const EncodedOID & as_oid)
+  {
+    _ident.identifyByASyntax();
+    _asOid = as_oid;
+    _prsCtxId = 0;
+  }
+
+  void identifyByPrsContext(void)   { _ident.identifyByPrsContext(); }
+  void identifyByPrsContext(int32_t prs_ctx_id)
+  {
+    _ident.identifyByPrsContext();
+    _asOid.reset();
+    _prsCtxId = prs_ctx_id;
+  }
+
+  void setASyntaxOID(void) { _ident.setASyntaxOID(); }
+  void setASyntaxOID(const EncodedOID & as_oid)
+  {
+    _ident.setASyntaxOID();
+    _asOid = as_oid;
+  }
+
+  void setPrsContext(void) { _ident.setPrsContext(); }
+  void setPrsContext(int32_t prs_ctx_id)
+  {
+    _ident.setPrsContext();
+    _prsCtxId = prs_ctx_id;
+  }
+
+};
 
 } //asn1
 } //eyeline
