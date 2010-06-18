5020d4aba1789620d794f0245ece17a6477d8ff2
gvroman <>
2010-06-18 09:43:40 +0000

    TCAP Begin message

---
+++ src/eyeline/tcap/proto/TMsgBegin.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** *
+ * TCAP Begin message (according to
+ * itu-t(0) recommendation(0) q(17) 773 modules(2) messages(1) version3(3)).
+ * ************************************************************************** */
+#ifndef __TCAP_MESSAGE_BEGIN_DEFS_HPP
+#ident "@(#)$Id$"
+#define __TCAP_MESSAGE_BEGIN_DEFS_HPP
+
+#include "eyeline/tcap/TComponentDefs.hpp"
+#include "eyeline/tcap/proto/TCDlgPortion.hpp"
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+
+/* Begin message is defined in IMPLICIT tagging environment as follow:
+Begin ::= [APPLICATION 2] SEQUENCE {
+    otid             OrigTransactionID,
+    dialoguePortion  DialoguePortion OPTIONAL,
+    components       ComponentPortion
+}
+-- NOTE: When the dialoguePortion is present it may contain TCAP structured
+--       dialogue AARQ-APdu
+*/
+struct TMsgBegin {
+  uint32_t        _orgTrId;
+  TCDlgPortion    _dlgPart; //Optional
+  TComponentsList _compPart;
+
+  explicit TMsgBegin() : _orgTrId(0)
+  { }
+  ~TMsgBegin()
+  { }
+
+  //Verifies that dialoguePortion contains allowed Structured Dialogue PDU
+  bool verifyPdu(void) const
+  {
+    return (_dlgPart.empty() || (_dlgPart.getDLG() && _dlgPart.getDLG()->getAARQ()));
+  }
+
+  const TCPduAARQ * getAARQ(void) const
+  {
+    return _dlgPart.getDLG()  ? _dlgPart.getDLG()->getAARQ() : NULL;
+  }
+};
+
+} //proto
+} //tcap
+} //eyeline
+
+#endif /* __TCAP_MESSAGE_BEGIN_DEFS_HPP */
+
