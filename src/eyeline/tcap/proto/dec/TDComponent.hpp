a45a488883fab1d35cc200890093461f497c7331
gvroman <>
2010-06-18 10:07:58 +0000

    TCAP Message component decoder

---
+++ src/eyeline/tcap/proto/dec/TDComponent.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************* *
+ * TCAP Message Decoder: TCAP Component type encoder.
+ * ************************************************************************* */
+#ifndef __EYELINE_TCAP_PROTO_DEC_COMPONENT_HPP__
+#ident "@(#)$Id$"
+#define __EYELINE_TCAP_PROTO_DEC_COMPONENT_HPP__
+
+#include "eyeline/ros/proto/dec/RDRosPdu.hpp"
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+namespace dec {
+
+using eyeline::asn1::ber::TSGroupBER;
+
+typedef eyeline::ros::proto::dec::RDRosPdu TDComponent;
+
+}}}}
+
+#endif /* __EYELINE_TCAP_PROTO_DEC_COMPONENT_HPP__ */
+
