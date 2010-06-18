2fdd32e7e8dbea5342c865e37ac23d54c7b487a3
gvroman <>
2010-06-18 09:23:27 +0000

    T_L_Reject, T_R_Reject are added

--- src/eyeline/tcap/TComponentIndicationPrimitives.hpp
+++ src/eyeline/tcap/TComponentIndicationPrimitives.hpp
@@ -1,9 +1,11 @@
+/* ************************************************************************** *
+ * TComponent sublayer: component handling indications.
+ * ************************************************************************** */
 #ifndef __EYELINE_TCAP_TCOMPONENTINDICATIONPRIMITIVES_HPP__
 # ident "@(#)$Id$"
 # define __EYELINE_TCAP_TCOMPONENTINDICATIONPRIMITIVES_HPP__
 
-# include "eyeline/utilx/Exception.hpp"
-# include "eyeline/tcap/TDialogueId.hpp"
+# include "eyeline/tcap/TDialogueDefs.hpp"
 # include "eyeline/tcap/TComponentDefs.hpp"
 
 namespace eyeline {
@@ -12,40 +14,71 @@ namespace tcap {
 using eyeline::ros::RejectProblem;
 
 class TComponentIndicationPrimitive {
+protected:
+  TDialogueId   _dlgId;
+  InvokeId      _invokeId;
+  bool          _hasInvId;
+
 public:
   TComponentIndicationPrimitive()
-    : _isSetDlgId(false), _isSetInvokeId(false) {}
-
-  TDialogueId getDialogueId(void) const {
-    if ( _isSetDlgId )
-      return _dlgId;
-
-    throw utilx::FieldNotSetException("TComponentIndicationPrimitive::getDialogueId::: dialogueId is not set");
-  }
-
-  InvokeId getInvokeId(void) const {
-    if ( _isSetInvokeId )
-      return _invokeId;
-
-    throw utilx::FieldNotSetException("TComponentIndicationPrimitive::getInvokeId::: invokeId is not set");
-  }
-
-protected:
-  TDialogueId _dlgId;
-  InvokeId _invokeId;
-  bool _isSetDlgId, _isSetInvokeId;
+    : _dlgId(0), _invokeId(0), _hasInvId(false)
+  { }
+  ~TComponentIndicationPrimitive()
+  { }
+  //
+  TDialogueId getDialogueId(void) const { return _dlgId; }
+  //
+  bool hasInvokeId(void) const { return _hasInvId; }
+  //
+  InvokeId getInvokeId(void) const { return _invokeId; }
 };
 
 
-class TC_L_Cancel_Ind : public TComponentIndicationPrimitive {
+//NOTE: InvokeId is optional!
+class TC_L_Cancel_Ind : public TComponentIndicationPrimitive { 
+public:
+  TC_L_Cancel_Ind()
+  { }
+  ~TC_L_Cancel_Ind()
+  { }
 };
 
+//NOTE: InvokeId is optional!
 class TC_L_Reject_Ind : public TComponentIndicationPrimitive {
 protected:
   RejectProblem   _rejProblem;
 
 public:
   TC_L_Reject_Ind()
+  { }
+  ~TC_L_Reject_Ind()
+  { }
+
+  const RejectProblem & getProblem(void) const { return _rejProblem; }
+};
+
+class TC_R_Reject_Ind : public TComponentIndicationPrimitive {
+protected:
+  RejectProblem   _rejProblem;
+
+public:
+  TC_R_Reject_Ind()
+  { }
+  ~TC_R_Reject_Ind()
+  { }
+
+  const RejectProblem & getProblem(void) const { return _rejProblem; }
+};
+
+//NOTE: InvokeId is mandatory!
+class TC_U_Reject_Ind : public TComponentIndicationPrimitive {
+protected:
+  RejectProblem   _rejProblem;
+
+public:
+  TC_U_Reject_Ind()
+  { }
+  ~TC_U_Reject_Ind()
   { }
 
   const RejectProblem & getProblem(void) const { return _rejProblem; }
