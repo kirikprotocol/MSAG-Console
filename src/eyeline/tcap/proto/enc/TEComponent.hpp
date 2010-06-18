904896b10ea7c8bded023a54297cdd913d68ce3c
gvroman <>
2010-06-18 09:56:52 +0000

    TCAP Message component encoder

---
+++ src/eyeline/tcap/proto/enc/TEComponent.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************* *
+ * TCAP Message Encoder: TCAP Component type encoder.
+ * ************************************************************************* */
+#ifndef __EYELINE_TCAP_PROTO_ENC_COMPONENT_HPP__
+#ident "@(#)$Id$"
+#define __EYELINE_TCAP_PROTO_ENC_COMPONENT_HPP__
+
+#include "eyeline/ros/proto/enc/RERosPdu.hpp"
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+namespace enc {
+
+using eyeline::asn1::ber::TSGroupBER;
+
+typedef eyeline::ros::proto::enc::RERosPdu TEComponent;
+
+}}}}
+
+#endif /* __EYELINE_TCAP_PROTO_ENC_COMPONENT_HPP__ */
+
