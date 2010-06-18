db658e70d64610315d20d6b818c1ea2f05693fc9
gvroman <>
2010-06-18 06:25:59 +0000

    Max subIds num defined

--- src/eyeline/asn1/ObjectID.hpp
+++ src/eyeline/asn1/ObjectID.hpp
@@ -13,10 +13,11 @@ namespace asn1 {
 
 typedef uint16_t SubIdType;
 
-static const uint8_t _ObjectID_DFLT_SZ = 16;
-
+static const uint8_t _ObjectID_DFLT_SUBIDS_NUM = 16;
 //NOTE: Overall number of subIdentifiers is limited to 255
-typedef eyeline::util::LWArray_T<SubIdType, uint8_t, _ObjectID_DFLT_SZ> RelativeOID;
+static const uint8_t _ObjectID_MAX_SUBIDS_NUM = 255;
+
+typedef eyeline::util::LWArray_T<SubIdType, uint8_t, _ObjectID_DFLT_SUBIDS_NUM> RelativeOID;
 
 class ObjectID : public RelativeOID {
 public:
