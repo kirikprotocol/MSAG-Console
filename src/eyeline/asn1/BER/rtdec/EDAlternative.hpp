9360eaa81980b6a7bedbe5f62a3eb06835225585
gvroman <>
2010-06-18 07:37:06 +0000

    Constructed type elements generic decoder

---
+++ src/eyeline/asn1/BER/rtdec/EDAlternative.hpp
@@ -0,0 +1,164 @@
+/* ************************************************************************* *
+ * BER Decoder: Structured type ElementDecoder alternative class.
+ * ************************************************************************* */
+#ifndef __ASN1_BER_DECODER_ELEMENTS_DECODER_ALT_DEF
+#ident "@(#)$Id$"
+#define __ASN1_BER_DECODER_ELEMENTS_DECODER_ALT_DEF
+
+#include "eyeline/asn1/BER/rtutl/TypeTagging.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+//ElementDecoder alternative
+class EDAlternative {
+public:
+  enum Kind_e {
+      altNone = 0
+    , altUExtension //uknon extension additions entry, has _tagUNI0 assigned
+    , altOpentype   //untagged ANY/OpenType, has _tagANYTYPE
+    , altChoice     //untagged CHOICE (several options possible)
+    , altElement    //ordinary element occurence (has tag defined)
+  };
+  enum Blocking_e {
+      blockNone = 0           //recurring alternative, doesn't affect others 
+    , blockItself = 0x01      //single occurence alternative, doesn't affect others 
+    , blockPreceeding = 0x02  //alternative blocks all preceeding alternatives
+    , blockFollowing = 0x04   //alternative blocks all following alternatives
+    , blockALL = 0x07         //alternative blocks all following alternatives
+  };
+  enum Tagging_e {
+      atgUntagged = 0 //alternative is identified by tag that is outermost tag
+                      //of its complete tagging
+    , atgImplicit     //alternative is identified by tag that replaces outermost
+                      //tag of its complete tagging
+    , atgExplicit     //alternative is identified by tag that isn't part of its
+                      //complete tagging
+  };
+  enum Presence_e { altMANDATORY = 0, altOPTIONAL = 1 };
+
+private:
+  union {
+    void * _aligner;
+    uint8_t _buf[sizeof(ASTag)];
+  } _memTag;
+
+protected:
+  Kind_e          _kind;
+  uint16_t        _uniqueId;  //
+  bool            _optional;  //presence mode
+  Tagging_e       _tagEnv;
+  union {
+    const void *  _none;
+    const ASTag * _single;    //in case of altOpentype == _tagUNI0, 
+    const TaggingOptions::TagsMAP * _opts; //tagging options of associated type
+  } _tag;                     
+
+  void setTag(const ASTag & use_tag)
+  {
+    _tag._single = new (_memTag._buf)ASTag(use_tag);
+  }
+
+public:
+  //default constructor for containers
+  EDAlternative()
+    : _kind(altNone), _uniqueId(0)
+    , _optional(true), _tagEnv(atgUntagged)
+  {
+    _tag._none = _memTag._aligner = 0;
+  }
+  //alternative of Unknown_extensions_entry
+  EDAlternative(uint16_t unique_id)
+    : _kind(altUExtension), _uniqueId(unique_id)
+    , _optional(true), _tagEnv(atgUntagged)
+  {
+    setTag(asn1::_tagUNI0);
+  }
+  //untagged alternative of Opentype/ANY
+  EDAlternative(uint16_t unique_id, Presence_e use_presence)
+    : _kind(altOpentype), _uniqueId(unique_id)
+    , _optional(use_presence != altMANDATORY), _tagEnv(atgUntagged)
+  {
+    setTag(asn1::_tagANYTYPE);
+  }
+  //untagged alternative of CHOICE option 
+  EDAlternative(uint16_t unique_id, const TaggingOptions::TagsMAP & use_tag_opts,
+                Presence_e use_presence = altMANDATORY)
+    : _kind(altChoice), _uniqueId(unique_id)
+    , _optional(use_presence != altMANDATORY), _tagEnv(atgUntagged)
+  {
+    _tag._opts = &use_tag_opts;
+  }
+  //untagged alternative of ordinary type, that has tag defined
+  EDAlternative(uint16_t unique_id, const ASTag & use_tag,
+                Presence_e use_presence = altMANDATORY)
+    : _kind(altElement), _uniqueId(unique_id)
+    , _optional(use_presence != altMANDATORY), _tagEnv(atgUntagged)
+  {
+    setTag(use_tag);
+  }
+  //tagged alternative of arbitrary type
+  EDAlternative(uint16_t unique_id, const ASTag & use_tag, ASTagging::Environment_e tag_env,
+                Presence_e use_presence = altMANDATORY)
+    : _kind(altElement), _uniqueId(unique_id)
+    , _optional(use_presence != altMANDATORY)
+    , _tagEnv((tag_env == ASTagging::tagsEXPLICIT) ? atgExplicit : atgImplicit)
+  {
+    setTag(use_tag);
+  }
+  ~EDAlternative()
+  { }
+
+  Kind_e getKind(void) const { return _kind; }
+  //
+  bool empty(void) const { return _kind == altNone; }
+  //
+  bool isChoice(void) const { return _kind == altChoice; }
+  //
+  bool isOpentype(void) const { return _kind == altOpentype; }
+  //
+  bool isUnkExtension(void) const { return _kind == altUExtension; }
+  //
+  uint16_t  getUId(void) const { return _uniqueId; }
+  //
+  Tagging_e getEnv(void) const { return _tagEnv; }
+  //
+  const ASTag * getTag(void) const { return isChoice() ? NULL : _tag._single; }
+  //
+  const TaggingOptions::TagsMAP * getTagOpts(void) const
+  {
+    return isChoice() ? _tag._opts : NULL;
+  }
+  //
+  bool isOptional(void) const { return _optional; }
+
+  //
+  uint16_t numTags(void) const
+  {
+    if (_kind == altChoice)
+      return (uint16_t)(_tag._opts->size());
+    return _tag._single ? 1 : 0;
+  }
+
+  void resetTag(const ASTag & use_tag)
+  {
+    if ((_kind != altChoice) && _tag._single)
+      _tag._single->~ASTag();
+    setTag(use_tag);
+  }
+
+  //
+  bool operator<(const EDAlternative & cmp_obj) const
+  {
+    return _uniqueId < cmp_obj._uniqueId;
+  }
+};
+
+} //ber
+} //asn1
+} //eyeline
+
+#endif /* __ASN1_BER_DECODER_ELEMENTS_DECODER_ALT_DEF */
+
+
---
+++ src/eyeline/asn1/BER/rtdec/ElementDecoder.hpp
@@ -0,0 +1,246 @@
+/* ************************************************************************* *
+ * BER Decoder: base class of all element decoders of structured types
+ * (SEQUENCE, SET, CHOICE, SEQUENCE OF, SET OF).
+ * ************************************************************************* */
+#ifndef __ASN1_BER_DECODER_ELEMENTS_DECODER
+#ident "@(#)$Id$"
+#define __ASN1_BER_DECODER_ELEMENTS_DECODER
+
+#include "util/Exception.hpp"
+#include "eyeline/asn1/BER/rtdec/EDAlternative.hpp"
+
+namespace eyeline {
+namespace asn1 {
+namespace ber {
+
+typedef eyeline::util::LWArrayExtension_T<EDAlternative, uint16_t> EDAlternativesArray;
+
+class ElementDecoderAC {
+public:
+  enum TDState_e { //TagDecoder state
+      tdEmpty = 0   //
+    , tdInit        //TagDecoder is being initialized
+    , tdBuilt       //TagDecoder is built and ready for use
+    , tdProcess     //TagDecoder is in process of decoding
+  };
+
+  enum EDAStatus_e { //Element Alternative determination status
+      edaOk = 0
+    , edaUnknown          //unknown alternative found or TagDecoder isn't built yet
+    , edaMisplaced        //misplaced alternative
+    , edaMultiple         //multiply occured alternative
+    , edaMisplacedExt     //misplaced unknown extension
+    , edaMisssed          //at least one mandatory element is missed
+  };
+
+  struct EDAResult { //Element Alternative determination result
+    EDAStatus_e           _status;
+    const EDAlternative * _alt;
+
+    EDAResult(EDAStatus_e use_status = ElementDecoderAC::edaOk,
+              const EDAlternative * use_alt = 0)
+      : _status(use_status), _alt(use_alt)
+    { }
+    ~EDAResult()
+    { }
+
+    bool isOk(void) const { return (_status == ElementDecoderAC::edaOk); }
+  };
+
+  //
+  class DuplicateUIdException : public smsc::util::Exception {
+  public:
+    DuplicateUIdException(uint16_t unique_id)
+      : smsc::util::Exception("ber::ElementDecoderAC::setAlternative(): "
+                              "alternative[%u] is already defined", unique_id)
+    { }
+  };
+  //
+  class UndefinedUIdException : public smsc::util::Exception {
+  public:
+    UndefinedUIdException(uint16_t unique_id)
+      : smsc::util::Exception("ber::ElementDecoderAC::buildTagDecoder(): "
+                              "alternative[%u] is not defined", unique_id)
+    { }
+  };
+  //
+  class DuplicateTagException : public smsc::util::Exception {
+  public:
+    DuplicateTagException(const ASTag & use_tag, uint16_t alt_uid1, uint16_t alt_uid2)
+      : smsc::util::Exception("ber::ElementDecoderAC::buildTagDecoder(): "
+                              "duplicate tag '%s' of alternatives %u, %u",
+                              use_tag.toString().c_str(), alt_uid1, alt_uid2)
+    { }
+  };
+  //
+  class AmbiguousCtxException : public smsc::util::Exception {
+  public:
+    AmbiguousCtxException(uint16_t alt_uid)
+      : smsc::util::Exception("ber::ElementDecoderAC::buildTagDecoder(): "
+                              "OpenType alternative %u is used in ambiguous context",
+                              alt_uid)
+    { }
+  };
+
+protected:
+  TDState_e   _tdState;
+  //ElementDecoderAC initialization data
+  bool        _extensible;  //elements order is extensible - unknown elements
+                            //may be present as special pseudo-alternative
+
+  EDAlternativesArray *   _altsArr; //ElementAlternatives storage, actually it's
+                                    //just a reference to a successor member, so
+                                    //its copying constructor MUST properly set
+                                    //that pointer.
+  // ----------------------------------------------------------
+  // Initialization methods for successors
+  // ----------------------------------------------------------
+  void insertAlternative(const EDAlternative & use_alt)
+    /* throw(std::exception)*/
+  {
+    if (!_altsArr->at(use_alt.getUId()).empty())
+      throw DuplicateUIdException(use_alt.getUId());
+    _altsArr->at(use_alt.getUId()) = use_alt;
+    if (use_alt.isUnkExtension())
+      _extensible = true;
+    if (_tdState == tdEmpty)
+      _tdState = tdInit;
+  }
+
+  void setAltStorage(EDAlternativesArray & alt_store) { _altsArr = &alt_store; }
+
+  //NOTE: copying constructor of successsor MUST properly set _alrArr
+  ElementDecoderAC(const ElementDecoderAC & use_obj)
+    : _tdState(use_obj._tdState), _extensible(use_obj._extensible)
+    , _altsArr(0)
+  { }
+
+  // ----------------------------------------------------------
+  // ElementDecoderAC Interface methods (protected)
+  // ----------------------------------------------------------
+  //Returns blocking mode for alternative with given UId
+  virtual uint8_t getBlocking(uint16_t alt_uid) const /*throw(std::exception)*/ = 0;
+  //Generates TagDecoder initialization data
+  virtual void buildTagDecoder(void) /* throw(std::exception)*/ = 0;
+  //Reverts TagDecoder runtime data to its 'just-built' state
+  virtual void resetTagDecoder(void) /*throw()*/ = 0;
+  //Destroys all TagDecoder data
+  virtual void eraseTagDecoder(void) /*throw()*/ = 0;
+  //Checks if occurence of alternative with given tag is legal at current
+  //ElementDecoderAC state updating it if necessary.
+  virtual EDAResult processElementTag(const ASTag & use_tag) /*throw(std::exception)*/ = 0;
+  //Verifies that ElementDecoderAC is in complete state - all mandatory
+  //alternatives are processed according to its decoding order.
+  virtual EDAResult verifyTDCompletion(void) const /*throw()*/ = 0;
+
+public:
+  ElementDecoderAC(EDAlternativesArray & alt_store)
+    : _tdState(tdEmpty), _extensible(false), _altsArr(&alt_store)
+  { }
+  virtual ~ElementDecoderAC()
+  { }
+
+  // ----------------------------------------------------------
+  // Initialization methods
+  // ----------------------------------------------------------
+
+  //Sets tagged alternative of some type
+  void setAlternative(uint16_t unique_idx, const ASTag & use_tag,
+                      ASTagging::Environment_e tag_env,
+                      EDAlternative::Presence_e use_presence)
+    /* throw(std::exception)*/
+  {
+    insertAlternative(EDAlternative(unique_idx, use_tag, tag_env, use_presence));
+  }
+  //Sets untagged alternative of ordinary type
+  void setAlternative(uint16_t unique_idx, const ASTag & use_tag,
+                      EDAlternative::Presence_e use_presence)
+    /* throw(std::exception)*/
+  {
+    insertAlternative(EDAlternative(unique_idx, use_tag, use_presence));
+  }
+  //Sets untagged alternative of ANY/Opentype
+  void setAlternative(uint16_t unique_idx, EDAlternative::Presence_e use_presence)
+    /* throw(std::exception)*/
+  {
+     insertAlternative(EDAlternative(unique_idx, use_presence));
+  }
+
+  //Sets untagged alternative of untagged CHOICE type
+  void setAlternative(uint16_t unique_idx, const TaggingOptions & use_tag_opts,
+                      EDAlternative::Presence_e use_presence)
+    /* throw(std::exception)*/
+  {
+     insertAlternative(EDAlternative(unique_idx, use_tag_opts.getMap(),use_presence));
+  }
+  //Sets alternative for unknown extension additions entry
+  void setUnkExtension(uint16_t unique_idx) /* throw(std::exception)*/
+  {
+    insertAlternative(EDAlternative(unique_idx));
+  }
+
+  // ----------------------------------------------------------
+  // Runtime processing methods
+  // ----------------------------------------------------------
+
+  //Checks if occurence of alternative with given tag is legal at current
+  //ElementDecoderAC state updating it if necessary.
+  EDAResult processElement(const ASTag & use_tag) /*throw(std::exception)*/
+  {
+    if (_tdState == tdInit) {
+      buildTagDecoder();
+      _tdState = tdBuilt;
+    }
+    _tdState = tdProcess;
+    return processElementTag(use_tag);
+  }
+
+  //Verifies that ElementDecoderAC is in complete state - all mandatory
+  //alternatives are processed according to its decoding order.
+  EDAResult verifyCompletion(void) const /*throw()*/
+  {
+    return (_tdState <= tdBuilt) ? EDAResult(edaUnknown) : verifyTDCompletion();
+  }
+
+  //Reverts ElementDecoderAC to its initialized state, making it ready for next decoding.
+  void reset(void) /*throw()*/
+  {
+    if (_tdState == tdProcess) {
+      resetTagDecoder();
+      _tdState = tdBuilt;
+    }
+  }
+
+  //ElementDecoderAC to its 'just-allocated' state, erases all runtime and
+  //initialization data.
+  void erase(void) /*throw()*/
+  {
+    while (_tdState > tdEmpty) {
+      switch (_tdState) {
+      case tdProcess: {
+        resetTagDecoder(); _tdState = tdBuilt; 
+      } break;
+
+      case tdBuilt: {
+        eraseTagDecoder();  _tdState = tdInit;
+      } break;
+
+      case tdInit: {
+        _altsArr->clear();
+        _extensible =  false;
+        _tdState = tdEmpty;
+      } break;
+
+      default:;
+      } //eosw
+    }
+  }
+};
+
+
+} //ber
+} //asn1
+} //eyeline
+
+#endif /* __ASN1_BER_DECODER_ELEMENTS_DECODER */
+
