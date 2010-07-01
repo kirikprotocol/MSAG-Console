82f795f4dced3d59137a285c59aabe358b4e1b50
alkhal <>
2010-07-01 11:15:54 +0000

    smsc config

--- webapps/smsc_cc/java/ru/novosoft/smsc/admin/smsc_config/InstanceSettings.java
+++ webapps/smsc_cc/java/ru/novosoft/smsc/admin/smsc_config/InstanceSettings.java
@@ -1,11 +1,260 @@
 package ru.novosoft.smsc.admin.smsc_config;
 
+import ru.novosoft.smsc.util.config.ConfigException;
+import ru.novosoft.smsc.util.config.XmlConfig;
+import ru.novosoft.smsc.util.config.XmlConfigSection;
+
 /**
  * Структура с настройками для конкретного инстанца СМСЦ
- *
+ * <p/>
  * Список настроек смотри в ТЗ.
- * 
+ *
  * @author Artem Snopkov
  */
-public class InstanceSettings {
+public class InstanceSettings implements Cloneable {
+
+  private String adminHost;
+
+  private int adminPort;
+
+  private String corePerfHost;
+
+  private int corePerfPort;
+
+  private String smePerfHost;
+
+  private int smePerfPort;
+
+  private String messageStoreStatsDir;
+
+  private String messageStoreArchDir;
+
+  private String localStoreFilename;
+
+  private String localStoreMessIdFile;
+
+  private String smppHost;
+
+  private int smppPort;
+
+  private String agentHost;
+
+  private int agentPort;
+
+  private String cacheDir;
+
+  private int instanceN;
+
+  InstanceSettings(int instanceN, XmlConfig c) throws ConfigException {
+    this.instanceN = instanceN;
+    load(c);
+  }
+
+  protected void load(XmlConfig c) throws ConfigException {
+    assert c != null : "Config is null";
+
+    XmlConfigSection s = c.getSection("admin");
+    adminHost = s.getString("host" + instanceN);
+    adminPort = s.getInt("port" + instanceN);
+    s = c.getSection("core").getSection("performance");
+    corePerfHost = s.getString("host" + instanceN);
+    corePerfPort = s.getInt("port" + instanceN);
+    s = c.getSection("core").getSection("smeperformance");
+    smePerfHost = s.getString("host" + instanceN);
+    smePerfPort = s.getInt("port" + instanceN);
+    s = c.getSection("MessageStore");
+    messageStoreStatsDir = s.getString("statisticsDir" + instanceN);
+    messageStoreArchDir = s.getString("archiveDir" + instanceN);
+    s = c.getSection("MessageStore").getSection("LocalStore");
+    localStoreFilename = s.getString("filename" + instanceN);
+    localStoreMessIdFile = s.getString("msgidfile" + instanceN);
+    s = c.getSection("smpp");
+    smppHost = s.getString("host" + instanceN);
+    smppPort = s.getInt("port" + instanceN);
+    s = c.getSection("profiler").getSection("notify");
+    agentHost = s.getString("host" + instanceN);
+    agentPort = s.getInt("port" + instanceN);
+    cacheDir = s.getString("dir" + instanceN);
+
+
+  }
+
+  protected void save(XmlConfig c) throws ConfigException {
+    XmlConfigSection s = c.getOrCreateSection("admin");
+    s.setString("host" + instanceN, adminHost);
+    s.setInt("port" + instanceN, adminPort);
+    XmlConfigSection core = c.getOrCreateSection("core");
+    s = core.getOrCreateSection("performance");
+    s.setString("host" + instanceN, corePerfHost);
+    s.setInt("port" + instanceN, corePerfPort);
+
+    s = core.getOrCreateSection("smeperformance");
+    s.setString("host" + instanceN, smePerfHost);
+    s.setInt("port" + instanceN, smePerfPort);
+
+    s = c.getOrCreateSection("MessageStore");
+    s.setString("statisticsDir" + instanceN, messageStoreStatsDir);
+    s.setString("archiveDir" + instanceN, messageStoreArchDir);
+
+    s = s.getOrCreateSection("LocalStore");
+    s.setString("filename" + instanceN, localStoreFilename);
+    s.setString("msgidfile" + instanceN, localStoreMessIdFile);
+
+    s = c.getOrCreateSection("smpp");
+    s.setString("host" + instanceN, smppHost);
+    s.setInt("port" + instanceN, smppPort);
+
+    s = c.getOrCreateSection("profiler").getOrCreateSection("notify");
+    s.setString("host" + instanceN, agentHost);
+    s.setInt("port" + instanceN, agentPort);
+    s.setString("dir" + instanceN, cacheDir);
+
+  }
+
+  public String getAdminHost() {
+    return adminHost;
+  }
+
+  public void setAdminHost(String adminHost) {
+    this.adminHost = adminHost;
+  }
+
+  public int getAdminPort() {
+    return adminPort;
+  }
+
+  public void setAdminPort(int adminPort) {
+    this.adminPort = adminPort;
+  }
+
+  public String getCorePerfHost() {
+    return corePerfHost;
+  }
+
+  public void setCorePerfHost(String corePerfHost) {
+    this.corePerfHost = corePerfHost;
+  }
+
+  public int getCorePerfPort() {
+    return corePerfPort;
+  }
+
+  public void setCorePerfPort(int corePerfPort) {
+    this.corePerfPort = corePerfPort;
+  }
+
+  public String getSmePerfHost() {
+    return smePerfHost;
+  }
+
+  public void setSmePerfHost(String smePerfHost) {
+    this.smePerfHost = smePerfHost;
+  }
+
+  public int getSmePerfPort() {
+    return smePerfPort;
+  }
+
+  public void setSmePerfPort(int smePerfPort) {
+    this.smePerfPort = smePerfPort;
+  }
+
+  public String getMessageStoreStatsDir() {
+    return messageStoreStatsDir;
+  }
+
+  public void setMessageStoreStatsDir(String messageStoreStatsDir) {
+    this.messageStoreStatsDir = messageStoreStatsDir;
+  }
+
+  public String getMessageStoreArchDir() {
+    return messageStoreArchDir;
+  }
+
+  public void setMessageStoreArchDir(String messageStoreArchDir) {
+    this.messageStoreArchDir = messageStoreArchDir;
+  }
+
+  public String getLocalStoreFilename() {
+    return localStoreFilename;
+  }
+
+  public void setLocalStoreFilename(String localStoreFilename) {
+    this.localStoreFilename = localStoreFilename;
+  }
+
+  public String getLocalStoreMessIdFile() {
+    return localStoreMessIdFile;
+  }
+
+  public void setLocalStoreMessIdFile(String localStoreMessIdFile) {
+    this.localStoreMessIdFile = localStoreMessIdFile;
+  }
+
+  public String getSmppHost() {
+    return smppHost;
+  }
+
+  public void setSmppHost(String smppHost) {
+    this.smppHost = smppHost;
+  }
+
+  public int getSmppPort() {
+    return smppPort;
+  }
+
+  public void setSmppPort(int smppPort) {
+    this.smppPort = smppPort;
+  }
+
+  public String getAgentHost() {
+    return agentHost;
+  }
+
+  public void setAgentHost(String agentHost) {
+    this.agentHost = agentHost;
+  }
+
+  public int getAgentPort() {
+    return agentPort;
+  }
+
+  public void setAgentPort(int agentPort) {
+    this.agentPort = agentPort;
+  }
+
+  public String getCacheDir() {
+    return cacheDir;
+  }
+
+  public void setCacheDir(String cacheDir) {
+    this.cacheDir = cacheDir;
+  }
+
+  public int getInstanceN() {
+    return instanceN;
+  }
+
+
+  public Object clone() throws CloneNotSupportedException {
+    InstanceSettings s = (InstanceSettings) super.clone();
+    s.adminHost = adminHost;
+    s.adminPort = adminPort;
+    s.corePerfHost = corePerfHost;
+    s.corePerfPort = corePerfPort;
+    s.smePerfHost = smePerfHost;
+    s.smePerfPort = smePerfPort;
+    s.messageStoreStatsDir = messageStoreStatsDir;
+    s.messageStoreArchDir = messageStoreArchDir;
+    s.localStoreFilename = localStoreFilename;
+    s.localStoreMessIdFile = localStoreMessIdFile;
+    s.smppHost = smppHost;
+    s.smppPort = smppPort;
+    s.agentHost = agentHost;
+    s.agentPort = adminPort;
+    s.cacheDir = cacheDir;
+    s.instanceN = instanceN;
+    return s;
+  }
+
 }
--- webapps/smsc_cc/java/ru/novosoft/smsc/admin/smsc_config/SmscConfig.java
+++ webapps/smsc_cc/java/ru/novosoft/smsc/admin/smsc_config/SmscConfig.java
@@ -1,5 +1,10 @@
 package ru.novosoft.smsc.admin.smsc_config;
 
+import org.apache.log4j.Logger;
+import ru.novosoft.smsc.admin.AdminException;
+import ru.novosoft.smsc.util.config.ConfigException;
+import ru.novosoft.smsc.util.config.XmlConfig;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -11,26 +16,83 @@ import java.io.OutputStream;
  */
 class SmscConfig {
 
-  public void load(InputStream is) {
+  private static final Logger logger = Logger.getLogger(SmscConfig.class);
 
+  private XmlConfig config = new XmlConfig();
+
+  private CommonSettings commonSettings;
+
+  private InstanceSettings[] instanceSettings;
+
+  void load(InputStream is) throws AdminException {
+    try {
+      config.clear();
+      config.load(is);
+      commonSettings = new CommonSettings(config);
+      commonSettings.load(config);
+
+      int instancesCount = config.getSection("cluster").getInt("nodesCount");
+      instanceSettings = new InstanceSettings[instancesCount];
+      for (int i = 0; i < instancesCount; i++) {
+        instanceSettings[i] = new InstanceSettings(i, config);
+      }
+    } catch (ConfigException e) {
+      logger.error(e, e);
+      throw new AdminException(e.getMessage());
+    }
   }
 
-  public void save(OutputStream os) {
-
+  void save(OutputStream os) throws AdminException {
+    try {
+      commonSettings.save(config);
+      for (InstanceSettings is : instanceSettings) {
+        is.save(config);
+      }
+      config.save(os);
+    } catch (ConfigException e) {
+      logger.error(e, e);
+      throw new AdminException(e.getMessage());
+    }
   }
 
-  public CommonSettings getCommonSettings() {
-    return null;
+  void backup(OutputStream os) throws AdminException {
+    try {
+      config.save(os);
+    } catch (ConfigException e) {
+      logger.error(e, e);
+      throw new AdminException(e.getMessage());
+    }
   }
 
-  public int getSmscInstancesCount() {
-    return 0;
+  CommonSettings getCommonSettings() {
+    return commonSettings;
   }
 
-  public InstanceSettings getInstanceSettings(int instanceNumber) {
-    return null;
+  public void setCommonSettings(CommonSettings commonSettings) {
+    this.commonSettings = commonSettings;
   }
 
-  
+  int getSmscInstancesCount() {
+    return instanceSettings.length;
+  }
 
+  InstanceSettings getInstanceSettings(int instanceNumber) {
+    if (instanceNumber < 0 || instanceNumber > instanceSettings.length - 1) {
+      throw new IllegalArgumentException("Illegal instance number: " + instanceNumber + ". Must be in [0," + (instanceSettings.length - 1) + "]");
+    }
+    return instanceSettings[instanceNumber];
+  }
+
+  InstanceSettings[] getAllInstanceSettings() {
+    return instanceSettings;
+  }
+
+  public void setInstanceSettings(InstanceSettings instanceSettings) {
+    int instanceNumber = instanceSettings.getInstanceN();
+    if (instanceNumber < 0 || instanceNumber > this.instanceSettings.length - 1) {
+      throw new IllegalArgumentException("Illegal instance number: " + instanceNumber + ". Must be in [0," + (this.instanceSettings.length - 1) + "]");
+    }
+
+    this.instanceSettings[instanceNumber] = instanceSettings;
+  }
 }
