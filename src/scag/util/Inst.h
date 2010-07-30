242f62415cfaf4372b08db4658b1e982d4040a91
bukind <>
2010-07-30 09:03:52 +0000

    missed include

--- src/scag/util/Inst.h
+++ src/scag/util/Inst.h
@@ -2,6 +2,7 @@
 #define __INST_H__
 
 #include <string>
+#include <cstdio>
 
 namespace scag {
 namespace util {
@@ -13,7 +14,7 @@ public:
     Inst() : run_(true) { check("/tmp/scag.pid"); }
     Inst(const char* fname) : run_(true) { check(fname); }
     
-    ~Inst() { if(run_) remove(filename.c_str()); }
+    ~Inst() { if(run_) std::remove(filename.c_str()); }
     bool run(){return run_;}
     
 protected:
