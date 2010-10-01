6874dfb0ee05b5f44b3b1c565b263c42066d4a4f
gvroman <>
2010-10-01 06:43:10 +0000

    default rule is corrected to DER

--- src/eyeline/asn1/BER/rtenc/EncodeENUM.hpp
+++ src/eyeline/asn1/BER/rtenc/EncodeENUM.hpp
@@ -17,17 +17,17 @@ protected:
   //Tagged type referencing INTEGER. 
   // NOTE: eff_tags is a complete effective tagging of type!
   EncoderOfENUM(const ASTagging & eff_tags,
-                  TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+                  TransferSyntax::Rule_e use_rule = TransferSyntax::ruleDER)
     : EncoderOfINTEGER(eff_tags, use_rule)
   { }
 
 public:
-  EncoderOfENUM(TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+  EncoderOfENUM(TransferSyntax::Rule_e use_rule = TransferSyntax::ruleDER)
     : EncoderOfINTEGER(asn1::_tagsENUM, use_rule)
   { }
   //Tagged ENUMERATED type encoder constructor
   EncoderOfENUM(const ASTag & use_tag, ASTagging::Environment_e tag_env,
-                TransferSyntax::Rule_e use_rule = TransferSyntax::ruleBER)
+                TransferSyntax::Rule_e use_rule = TransferSyntax::ruleDER)
     : EncoderOfINTEGER(ASTagging(use_tag, tag_env, asn1::_tagsENUM), use_rule)
   { }
   ~EncoderOfENUM()
