00038fa825e4d209ebede3daef9a818df1d44dff
bukind <>
2010-10-13 05:06:54 +0000

    std forgotten

--- src/scag/tests/test32.cpp
+++ src/scag/tests/test32.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-#define showenv(x) std::cout << x << "=" << ( getenv(x) ? getenv(x) : "<not set>" ) << std::endl
+#define showenv(x) std::cout << x << "=" << ( std::getenv(x) ? std::getenv(x) : "<not set>" ) << std::endl
     showenv("LC_ALL");
     showenv("LC_CTYPE");
     showenv("LANG");
