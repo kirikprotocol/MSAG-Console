dd6e654c60651892ea575ff34bf4350393a36063
gvroman <>
2010-10-18 10:51:55 +0000

    *** empty log message ***

---
+++ src/eyeline/tcap/proto/enc/TEProtocolVersion.cpp
@@ -0,0 +1,17 @@
+#ifdef MOD_IDENT_ON
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_ON */
+
+#include "eyeline/tcap/proto/enc/TEProtocolVersion.hpp"
+
+namespace eyeline {
+namespace tcap {
+namespace proto {
+namespace enc {
+
+const asn1::ASTag
+  TEProtocolVersion::_typeTag(asn1::ASTag::tagContextSpecific, 0);
+
+
+}}}}
+
