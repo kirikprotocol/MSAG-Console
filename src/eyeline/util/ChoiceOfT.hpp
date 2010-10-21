7268d2ed2b045d73c95aa007daaf372093664cdf
gvroman <>
2010-10-21 06:41:53 +0000

    ChoiceOf6_T is added

--- src/eyeline/util/ChoiceOfT.hpp
+++ src/eyeline/util/ChoiceOfT.hpp
@@ -55,6 +55,8 @@ public:
 
   uint16_t getChoiceIdx(void) const { return _union.getIdx(); }
 
+  bool empty(void) const { return _union.empty(); }
+
   void clear() { _union.clear(); }
 
 protected:
@@ -138,6 +140,8 @@ public:
   { }
 
   uint16_t getChoiceIdx(void) const { return _union.getIdx(); }
+
+  bool empty(void) const { return _union.empty(); }
 
   void clear() { _union.clear(); }
 
@@ -225,6 +229,8 @@ public:
   { }
 
   uint16_t getChoiceIdx(void) const { return _union.getIdx(); }
+
+  bool empty(void) const { return _union.empty(); }
 
   void clear() { _union.clear(); }
 
@@ -316,6 +322,8 @@ public:
 
   uint16_t getChoiceIdx(void) const { return _union.getIdx(); }
 
+  bool empty(void) const { return _union.empty(); }
+
   void clear() { _union.clear(); }
 
 protected:
@@ -352,6 +360,105 @@ public:
     : ChoiceOf5_T<_TArg1, _TArg2, _TArg3, _TArg4, _TArg5, _AlternativeTArg, _ConstAlternativeTArg>()
   { }
   ~ChoiceOfBased5_T()
+  { }
+
+  _BaseTArg * get(void)
+  {
+    return static_cast<_BaseTArg *>(this->_union.empty() ? 0 : this->_union.get());
+  }
+  const _BaseTArg * get(void) const
+  {
+    return static_cast<const _BaseTArg *>(this->_union.empty() ? 0 : this->_union.get());
+  }
+};
+
+
+/* ************************************************************************* *
+ * Helper template class for CHOICE of six types ...
+ * ************************************************************************* */
+template <
+  class _TArg1, class _TArg2, class _TArg3, class _TArg4, class _TArg5, class _TArg6
+  , template <class> class _AlternativeTArg = UnionAlternative_T
+  , template <class> class _ConstAlternativeTArg = UnionConstAlternative_T
+>
+class ChoiceOf6_T {
+public:
+  template < class _TArg >
+  class Alternative_T : public _AlternativeTArg<_TArg> {
+  protected:
+    friend class ChoiceOf6_T;
+
+    Alternative_T(UnionStorageAC & use_store, uint16_t use_altIdx)
+      : _AlternativeTArg<_TArg>(use_store, use_altIdx)
+    { }
+
+  public:
+    ~Alternative_T()
+    { }
+  };
+
+  template < class _TArg >
+  class ConstAlternative_T : public _ConstAlternativeTArg<_TArg> {
+  protected:
+    friend class ChoiceOf6_T;
+
+    ConstAlternative_T(const UnionStorageAC & use_store, uint16_t use_altIdx)
+      : _ConstAlternativeTArg<_TArg>(use_store, use_altIdx)
+    { }
+
+  public:
+    ~ConstAlternative_T()
+    { }
+  };
+
+  ChoiceOf6_T()
+  { }
+  ~ChoiceOf6_T()
+  { }
+
+  uint16_t getChoiceIdx(void) const { return _union.getIdx(); }
+
+  bool empty(void) const { return _union.empty(); }
+
+  void clear() { _union.clear(); }
+
+protected:
+  UnionStorageOf6_T<_TArg1, _TArg2, _TArg3, _TArg4, _TArg5, _TArg6> _union;
+
+  Alternative_T<_TArg1>      alternative0()        { return Alternative_T<_TArg1>(_union, 0); }
+  ConstAlternative_T<_TArg1> alternative0() const  { return ConstAlternative_T<_TArg1>(_union, 0); }
+
+  Alternative_T<_TArg2>      alternative1()        { return Alternative_T<_TArg2>(_union, 1); }
+  ConstAlternative_T<_TArg2> alternative1() const  { return ConstAlternative_T<_TArg2>(_union, 1); }
+
+  Alternative_T<_TArg3>      alternative2()        { return Alternative_T<_TArg3>(_union, 2); }
+  ConstAlternative_T<_TArg3> alternative2() const  { return ConstAlternative_T<_TArg3>(_union, 2); }
+
+  Alternative_T<_TArg4>      alternative3()        { return Alternative_T<_TArg4>(_union, 3); }
+  ConstAlternative_T<_TArg4> alternative3() const  { return ConstAlternative_T<_TArg4>(_union, 3); }
+
+  Alternative_T<_TArg5>      alternative4()        { return Alternative_T<_TArg5>(_union, 4); }
+  ConstAlternative_T<_TArg5> alternative4() const  { return ConstAlternative_T<_TArg5>(_union, 4); }
+
+  Alternative_T<_TArg6>      alternative5()        { return Alternative_T<_TArg6>(_union, 5); }
+  ConstAlternative_T<_TArg6> alternative5() const  { return ConstAlternative_T<_TArg6>(_union, 5); }
+};
+
+/* ------------------------------------------------------------------------- *
+ * Helper template for CHOICE of six types, which have same base type ...
+ * ------------------------------------------------------------------------- */
+template <
+  class _BaseTArg, class _TArg1, class _TArg2, class _TArg3, class _TArg4, class _TArg5, class _TArg6
+  , template <class> class _AlternativeTArg = UnionAlternative_T
+  , template <class> class _ConstAlternativeTArg = UnionConstAlternative_T
+>
+class ChoiceOfBased6_T
+  : public ChoiceOf6_T<_TArg1, _TArg2, _TArg3, _TArg4, _TArg5, _TArg6, _AlternativeTArg, _ConstAlternativeTArg> {
+public:
+  ChoiceOfBased6_T()
+    : ChoiceOf6_T<_TArg1, _TArg2, _TArg3, _TArg4, _TArg5, _TArg6, _AlternativeTArg, _ConstAlternativeTArg>()
+  { }
+  ~ChoiceOfBased6_T()
   { }
 
   _BaseTArg * get(void)
--- src/eyeline/util/UnionT.hpp
+++ src/eyeline/util/UnionT.hpp
@@ -318,6 +318,69 @@ public:
   virtual const void * get(void) const { return _mem._buf; }
 };
 
+
+/* ************************************************************************* *
+ * Helper template class for union of six types ...
+ * ************************************************************************* */
+//
+template <
+  class _TArg1, class _TArg2, class _TArg3, class _TArg4, class _TArg5, class _TArg6
+>
+class UnionStorageOf6_T : public UnionStorageAC {
+protected:
+  union {
+    void *  _aligner;
+    uint8_t _buf[util::MaxSizeOf6_T<_TArg1, _TArg2, _TArg3, _TArg4, _TArg5, _TArg6>::VALUE];
+  } _mem;
+
+public:
+  UnionStorageOf6_T() : UnionStorageAC()
+  {
+    _mem._aligner = 0;
+  }
+  UnionStorageOf6_T(const UnionStorageOf6_T & cp_obj) : UnionStorageAC(cp_obj)
+  {
+    if (!_altIdx) {
+      new (_mem._buf)_TArg1(*(const _TArg1 *)cp_obj.get());
+    } else if (_altIdx == 1) {
+      new (_mem._buf)_TArg2(*(const _TArg2 *)cp_obj.get());
+    } else if (_altIdx == 2) {
+      new (_mem._buf)_TArg3(*(const _TArg3 *)cp_obj.get());
+    } else if (_altIdx == 3) {
+      new (_mem._buf)_TArg4(*(const _TArg4 *)cp_obj.get());
+    } else if (_altIdx == 4) {
+      new (_mem._buf)_TArg5(*(const _TArg5 *)cp_obj.get());
+    } else if (_altIdx == 5) {
+      new (_mem._buf)_TArg6(*(const _TArg6 *)cp_obj.get());
+    }
+      _mem._aligner = 0;
+  }
+  virtual ~UnionStorageOf6_T()
+  {
+    clear();
+  }
+
+  virtual void clear(void)
+  {
+    if (!_altIdx) {
+      ((_TArg1*)_mem._buf)->~_TArg1();
+    } else if (_altIdx == 1) {
+      ((_TArg2*)_mem._buf)->~_TArg2();
+    } else if (_altIdx == 2) {
+      ((_TArg3*)_mem._buf)->~_TArg3();
+    } else if (_altIdx == 3) {
+      ((_TArg4*)_mem._buf)->~_TArg4();
+    } else if (_altIdx == 4) {
+      ((_TArg5*)_mem._buf)->~_TArg5();
+    } if (_altIdx == 5) {
+      ((_TArg6*)_mem._buf)->~_TArg6();
+    }
+    _altIdx = _notAnIdx;
+  }
+  virtual void * get(void) { return _mem._buf; }
+  virtual const void * get(void) const { return _mem._buf; }
+};
+
 } //util
 } //eyeline
 
