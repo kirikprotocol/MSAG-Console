f388cf326cc4ff44977cdd9f2eb50fb32f6391ad
bukind <>
2010-10-29 04:39:44 +0000

    user roles stub

--- src/informer/data/UserInfo.h
+++ src/informer/data/UserInfo.h
@@ -4,11 +4,20 @@
 namespace eyeline {
 namespace informer {
 
+typedef enum
+{
+        USERROLE_ADMIN = 1,
+        USERROLE_USER = 2
+} UserRole;
+
+
 class UserInfo
 {
 public:
     const char* getPassword() const { return "password"; }
     const char* getUserId() const { return "user"; }
+
+    bool hasRole( UserRole role ) const { return USERROLE_USER; }
 };
 
 } // informer
