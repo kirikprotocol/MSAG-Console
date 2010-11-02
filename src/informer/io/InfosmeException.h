0dc4dadafb4f42bbf525ae56f8c8008bc0f38703
bukind <>
2010-11-02 10:08:26 +0000

    exception codes

--- src/informer/io/InfosmeException.h
+++ src/informer/io/InfosmeException.h
@@ -22,15 +22,27 @@ static inline char* getStrerror(int err,char* buf,size_t buflen) {
 namespace eyeline {
 namespace informer {
 
+typedef enum
+{
+    EXC_GENERIC = 0,
+    EXC_DLVLIMITEXCEED,    // number of deliveries exceeded
+} ErrorCode;
+
+
 class InfosmeException : public smsc::util::Exception
 {
 public:
-    InfosmeException( const char* fmt, ... )
+    InfosmeException( ErrorCode code, const char* fmt, ... ) :
+    code_(code)
     {
         SMSC_UTIL_EX_FILL(fmt);
     }
+    ErrorCode getCode() const { return code_; }
+
 protected:
     InfosmeException() {}
+
+    ErrorCode code_;
 };
 
 
@@ -39,6 +51,7 @@ class ErrnoException : public InfosmeException
 public:
     ErrnoException( int err, const char* fmt, ...)
     {
+        code_ = EXC_GENERIC;
         SMSC_UTIL_EX_FILL(fmt);
         char ebuf[100];
         message += ": ";
@@ -53,6 +66,7 @@ public:
     FileDataException( size_t pos, const char* fmt, ... ) :
     pos_(pos)
     {
+        code_ = EXC_GENERIC;
         SMSC_UTIL_EX_FILL(fmt);
         char ebuf[30];
         sprintf(ebuf," at %llu",static_cast<unsigned long long>(pos));
