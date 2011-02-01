de90ef3dd0cc49884d69b4fea84af4c25aac8bd0
vic <>
2011-02-01 09:18:40 +0000

    multipart test tool

--- src/mtsmsme/emule/msc-mosm-mpart.cpp
+++ src/mtsmsme/emule/msc-mosm-mpart.cpp
@@ -226,7 +226,7 @@ int main(int argc, char** argv)
       }
       else
       {
-      struct timespec delay = { 0, 500000000}; // nonoseconds
+      struct timespec delay = { 0, 500000000}; // Nanoseconds
       nanosleep(&delay, 0);
       }
     }
