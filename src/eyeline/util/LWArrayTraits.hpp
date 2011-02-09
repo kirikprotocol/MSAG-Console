8d75c4c18bcc3963b7ebf84efc8dd7fb8ac53e30
gvroman <>
2011-02-09 08:37:37 +0000

    shift traits is corrected

--- src/eyeline/util/LWArrayTraits.hpp
+++ src/eyeline/util/LWArrayTraits.hpp
@@ -47,25 +47,37 @@ struct LWArrayTraits {
     }
   }
   //Copies elements starting from first to last
+  //NOTE: source and destination MUST NOT OVERLAP
+  //NOTE: all ('num_elem') elements of destination MUST BE CONSTRUCTED prior to this call!
   template < typename _SizeTypeArg /* must be an unsigned integer type */ >
   static void copy(_TArg * use_dst, const _TArg * use_src, _SizeTypeArg num_elem)
   {
-    for (_SizeTypeArg i = 0; i < num_elem; ++i)
-      use_dst[i] = use_src[i];
+    for (_SizeTypeArg i = 0; i < num_elem; ++i) {
+      use_dst[i].~_TArg();
+      new (use_dst + i)_TArg(use_src[i]);
+    }
   }
-  //shifts number of elements to right
+  //shifts number of elements to right.
+  //NOTE: all elements in range ['num_elems', 'num_elem' + 'shift_sz']  MUST BE DESTROYED
+  //      prior to this call!
   template < typename _SizeTypeArg /* must be an unsigned integer type */ >
   static void shift_right(_TArg * use_buf, _SizeTypeArg num_elem, const _SizeTypeArg shift_sz)
   { //copy elements starting from last to first
-    for (_SizeTypeArg i = 0; i < num_elem; ++i)
-      new (use_buf + num_elem - 1 - i + shift_sz)_TArg(((const _TArg *)use_buf)[num_elem - 1 - i]);
+    _SizeTypeArg i = num_elem;
+    while (i > 0) {
+      --i;
+      new (use_buf + i + shift_sz)_TArg(use_buf[i]);
+    }
   }
-  //shifts number of elements to left
+  //shifts number of elements to left (cuts off first 'shift_sz' elements)
+  //NOTE: all ('num_elem') elements MUST BE CONSTRUCTED prior to this call!
   template < typename _SizeTypeArg /* must be an unsigned integer type */ >
   static void shift_left(_TArg * use_buf, _SizeTypeArg num_elem, const _SizeTypeArg shift_sz)
   { //copy elements starting from first to last
-    for (_SizeTypeArg i = 0; i < (num_elem - shift_sz); ++i)
-      use_buf[i] = use_buf[i + shift_sz];
+    for (_SizeTypeArg i = 0; i < num_elem - shift_sz; ++i) {
+      use_buf[i].~_TArg();
+      new (use_buf + i)_TArg(use_buf[shift_sz + i]);
+    }
   }
 };
 
