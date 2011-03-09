10de650b70af80d063c60bdc1f710814928fbeab
alkhal <>
2011-03-09 07:22:57 +0000

    informer web: syncronize errors in Authenticator

--- webapps/informer/java/mobi/eyeline/informer/web/auth/impl/AuthenticatorImpl.java
+++ webapps/informer/java/mobi/eyeline/informer/web/auth/impl/AuthenticatorImpl.java
@@ -10,12 +10,16 @@ import org.apache.log4j.Logger;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class AuthenticatorImpl implements Authenticator {
 
   private static final Logger logger = Logger.getLogger(AuthenticatorImpl.class);
 
   private final Users users;
+
+  private final Lock mapLock = new ReentrantLock();
 
   private Map<String, Error> errors = new LinkedHashMap<String, Error>(100) {
     private static final int MAX_ENTRIES = 100;
@@ -32,22 +36,27 @@ public class AuthenticatorImpl implements Authenticator {
   public synchronized InformerPrincipal authenticate(String login, String password) {
     try {
       User u = users.getUser(login);
-      if (u == null) {
-        errors.put(login, Error.NOT_FOUND);
-        logger.warn("AuthenticatorImpl.authenticate('" + login + "', '" + password + "') FAILED - User not found.");
-        return null;
+      try{
+        mapLock.lock();
+        if (u == null) {
+          errors.put(login, Error.NOT_FOUND);
+          logger.warn("AuthenticatorImpl.authenticate('" + login + "', '" + password + "') FAILED - User not found.");
+          return null;
+        }
+        if (User.Status.ENABLED != u.getStatus()) {
+          errors.put(login, Error.USER_BLOCKED);
+          logger.warn("AuthenticatorImpl.authenticate('" + login + "', '" + password + "') FAILED - User not ENABLED.");
+          return null;
+        }
+        if (!password.equals(u.getPassword())) {
+          errors.put(login, Error.WRONG_PASSWORD);
+          logger.warn("AuthenticatorImpl.authenticate('" + login + "', '" + password + "') FAILED - Incorrect password.");
+          return null;
+        }
+        errors.remove(login);
+      }finally {
+        mapLock.unlock();
       }
-      if (User.Status.ENABLED != u.getStatus()) {
-        errors.put(login, Error.USER_BLOCKED);
-        logger.warn("AuthenticatorImpl.authenticate('" + login + "', '" + password + "') FAILED - User not ENABLED.");
-        return null;
-      }
-      if (!password.equals(u.getPassword())) {
-        errors.put(login, Error.WRONG_PASSWORD);
-        logger.warn("AuthenticatorImpl.authenticate('" + login + "', '" + password + "') FAILED - Incorrect password.");
-        return null;
-      }
-      errors.remove(login);
       return new InformerPrincipal(login, password, u.getRoles());
 
     } catch (Exception e) {
@@ -59,7 +68,12 @@ public class AuthenticatorImpl implements Authenticator {
 
   @Override
   public Error getError(String login) {
-    return errors.get(login);
+    try{
+      mapLock.lock();
+      return errors.get(login);
+    }finally {
+      mapLock.unlock();
+    }
   }
 
 }
