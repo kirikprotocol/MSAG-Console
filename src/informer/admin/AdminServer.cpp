beadb124990e1dcc2363fcabbb3febc00268e65f
bukind <>
2011-03-15 06:07:37 +0000

    informer-build-20110315

--- src/informer/admin/AdminServer.cpp
+++ src/informer/admin/AdminServer.cpp
@@ -158,7 +158,7 @@ void AdminServer::handle(const messages::SendTestSms& cmd)
     code=-EXC_GENERIC;
   }
   resp.setRespCode(code);
-  smsc_log_debug(log,"sendTestSms resp: %s",msg.toString().c_str());
+  smsc_log_debug(log,"sendTestSms resp: %s",resp.toString().c_str());
   enqueueCommand(cmd.messageGetConnId(),resp,proto,false);
 }
 
