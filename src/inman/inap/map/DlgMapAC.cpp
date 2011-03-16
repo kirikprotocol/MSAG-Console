97e407185c4032796f29619f1c1b6d0ea229b31c
gvroman <>
2011-03-16 11:26:01 +0000

    Primitive MAP dialog core

---
+++ src/inman/inap/map/DlgMapAC.cpp
@@ -0,0 +1,328 @@
+#ifdef MOD_IDENT_ON
+static char const ident[] = "@(#)$Id$";
+#endif /* MOD_IDENT_ON */
+
+#include "inman/inap/map/DlgMapAC.hpp"
+using smsc::core::synchronization::MutexGuard;
+
+#include "inman/comp/MapOpErrors.hpp"
+using smsc::inman::comp::_RCS_MAPOpErrors;
+using smsc::inman::comp::MAPServiceRC;
+using smsc::inman::comp::_RCS_MAPService;
+
+#include "inman/inap/TCAPErrors.hpp"
+using smsc::inman::inap::TC_PAbortCause;
+using smsc::inman::inap::_RCS_TC_PAbort;
+using smsc::inman::inap::TC_UAbortCause;
+using smsc::inman::inap::_RCS_TC_UAbort;
+using smsc::inman::inap::_RCS_TC_Report;
+
+namespace smsc {
+namespace inman {
+namespace inap {
+
+/* ************************************************************************** *
+ * class MapDialogAC implementation:
+ * ************************************************************************** */
+MapDialogAC::MapDialogAC(const char * use_ident, Logger * use_log/* = NULL*/)
+  : _delThis(false), _logPfx(use_ident), _logger(use_log), _tcDlg(NULL)
+{
+  _ctrState.value = 0;
+  if (!_logger)
+    _logger = Logger::getInstance("smsc.inman.inap");
+  _thisRefs.addRef(MapDialogAC::refIdItself);
+  strcpy(_logId, _logPfx);
+}
+
+MapDialogAC::~MapDialogAC()
+{
+  MutexGuard  grd(_sync);
+  _thisRefs.unRef(MapDialogAC::refIdItself);
+  _delThis = false;
+  unbindTCDialog();
+
+  if (_resHdl.get()) {
+    smsc_log_warn(_logger, "%s: result handler still binded!", _logId);
+    _resHdl.Reset(NULL);
+  }
+  if (_thisRefs.hasRefs()) {
+    smsc_log_error(_logger, "%s: ~MapDialogAC() refs present: {%s}", _logId, _thisRefs.toString().c_str());
+  }
+  smsc_log_debug(_logger, "%s: destroyed", _logId);
+}
+
+void MapDialogAC::releaseThis(void)
+{
+  bool doDel = false;
+  {
+    MutexGuard  grd(_sync);
+    _thisRefs.unRef(MapDialogAC::refIdItself);
+    _delThis = true;
+    doDel = !_thisRefs.hasRefs();
+  }
+  if (doDel)
+    delete this;
+}
+
+void MapDialogAC::endDialog(bool do_release/* = true*/)
+{
+  MutexGuard  grd(_sync);
+  endTCDialog();
+  if (do_release)
+    unbindTCDialog();
+}
+
+bool MapDialogAC::unbindUser(void)
+{
+  MutexGuard  grd(_sync);
+  if (_resHdl.Unref()) {
+    _thisRefs.unRef(MapDialogAC::refIdHandler);
+    return true;
+  }
+  return false;
+}
+
+/* ------------------------------------------------------------------------ *
+ * Private/protected methods
+ * ------------------------------------------------------------------------ */
+//Unlocks result handler and destroys this object if it's released
+void MapDialogAC::unRefHdl(void)
+{
+  bool doDel = false;
+  {
+    MutexGuard tmp(_sync);
+    if (_resHdl.get())
+      _resHdl->Awake();
+    _resHdl.UnLock();
+    _thisRefs.unRef(MapDialogAC::refIdHandler);
+    doDel = _delThis && !_thisRefs.hasRefs();
+  }
+  if (doDel)
+    delete this;
+}
+
+/* ------------------------------------------------------------------------ *
+ * NOTE: _sync must be locked upon entry to following methods
+ * ------------------------------------------------------------------------ */
+void MapDialogAC::bindUser(MapDlgUserIface & res_hdl)
+{
+  _resHdl.Reset(&res_hdl);
+  _thisRefs.addRef(MapDialogAC::refIdHandler);
+}
+
+//Binds TC Dialog
+void MapDialogAC::bindTCDialog(uint16_t timeout_sec/* = 0*/)
+{
+  //xompose logId string, that depends on TC Dialog id.
+  snprintf(_logId, sizeof(_logId)-1, "%s[%u:%Xh]", _logPfx,
+           (unsigned)_tcDlg->getId().tcInstId, (unsigned)_tcDlg->getId().dlgId);
+
+  if (timeout_sec)
+    _tcDlg->setInvokeTimeout(timeout_sec);
+
+  _tcDlg->bindUser(this);
+  _thisRefs.addRef(MapDialogAC::refIdTCDialog);
+}
+
+//Locks result handler.
+//Returns false if result handler is not set.
+bool MapDialogAC::doRefHdl(void)
+{
+  if (_resHdl.Lock()) {
+    _thisRefs.addRef(MapDialogAC::refIdHandler);
+    return true;
+  }
+  return false;
+}
+
+//ends TC dialog
+void MapDialogAC::endTCDialog(void)
+{
+  if (_tcDlg) {
+    if (!_tcDlg->checkState(TCDlgState::dlgClosed)) {
+      smsc_log_debug(_logger, "%s: T_END_REQ, state: 0x%x", _logId, _ctrState.value);
+      Dialog::Ending doEnd = (_ctrState.s.ctrInited == MapDialogAC::operInited) ?
+                              Dialog::endUAbort : Dialog::endPrearranged;
+      try {
+        _tcDlg->endDialog(doEnd);
+      } catch (const std::exception & exc) {
+        smsc_log_error(_logger, "%s: TC_END_REQ %s", _logId, exc.what());
+      }
+    }
+    _tcDlg->releaseAllInvokes();
+  }
+}
+
+//ends TC dialog and releases TC dialog object
+void MapDialogAC::unbindTCDialog(void)
+{
+  while (_tcDlg && !_tcDlg->unbindUser()) { //TCDlg refers this object
+    //prevent object from being deleted by other thread
+    _thisRefs.addRef(MapDialogAC::refIdItself);
+    _sync.wait();
+    _thisRefs.unRef(MapDialogAC::refIdItself);
+  }
+  endTCDialog();
+
+  if (_tcDlg) {
+    rlseTCDialog();
+    _tcDlg = NULL;
+    _thisRefs.unRef(MapDialogAC::refIdTCDialog);
+  }
+}
+
+
+/* ------------------------------------------------------------------------ *
+ * DialogListener interface
+ * ------------------------------------------------------------------------ */
+void MapDialogAC::onDialogInvoke(Invoke * op, bool lastComp)
+{
+  MutexGuard  grd(_sync);
+  smsc_log_warn(_logger, "%s: unexoected TC_INVOKE_IND as %s", _logId, op->idStr().c_str());
+}
+
+void MapDialogAC::onDialogContinue(bool compPresent)
+{
+  MutexGuard  grd(_sync);
+  if (!compPresent) {
+    smsc_log_warn(_logger, "%s: missing component in TC_CONT_IND", _logId);
+  } //else wait for ongoing Invoke result/error
+}
+
+//TCAP indicates DialogPAbort: either due to TC layer error on one of sides or
+//because of local TC dialog timeout is expired (no T_END_IND from remote peer).
+void MapDialogAC::onDialogPAbort(uint8_t abortCause)
+{
+  {
+    MutexGuard  grd(_sync);
+    _ctrState.s.ctrAborted = 1;
+    smsc_log_error(_logger, "%s: state 0x%x, P_ABORT: '%s'", _logId,
+                    _ctrState.value, _RCS_TC_PAbort->code2Txt(abortCause));
+    unbindTCDialog();
+    if (!doRefHdl())
+      return;
+  }
+  //may call releaseThis()
+  _resHdl->onDialogEnd(_RCS_TC_PAbort->mkhash(abortCause));
+  unRefHdl(); //die if requested(
+}
+
+//SCF sent DialogUAbort (some logic error on SCF side).
+void MapDialogAC::onDialogUAbort(uint16_t abortInfo_len, uint8_t *pAbortInfo,
+                                    uint16_t userInfo_len, uint8_t *pUserInfo)
+{
+  uint32_t abortCause = (abortInfo_len == 1) ?
+                        *pAbortInfo : TC_UAbortCause::userDefinedAS;
+  {
+    MutexGuard  grd(_sync);
+    _ctrState.s.ctrAborted = 1;
+    smsc_log_error(_logger, "%s: state 0x%x, U_ABORT: '%s'", _logId,
+                    _ctrState.value, _RCS_TC_UAbort->code2Txt(abortCause));
+    unbindTCDialog();
+    if (!doRefHdl())
+      return;
+  }
+  //may call releaseThis()
+  _resHdl->onDialogEnd(_RCS_TC_UAbort->mkhash(abortCause));
+  unRefHdl(); //die if requested(
+}
+
+//Underlying layer unable to deliver message, just abort dialog
+void MapDialogAC::onDialogNotice(uint8_t reportCause,
+                        TcapEntity::TCEntityKind comp_kind/* = TcapEntity::tceNone*/,
+                        uint8_t invId/* = 0*/, uint8_t opCode/* = 0*/)
+{
+  {
+    MutexGuard  grd(_sync);
+    _ctrState.s.ctrAborted = 1;
+    std::string dstr;
+    if (comp_kind != TcapEntity::tceNone) {
+      format(dstr, "Invoke[%u]", invId);
+      switch (comp_kind) {
+      case TcapEntity::tceError:      dstr += ".Error"; break;
+      case TcapEntity::tceResult:     dstr += ".Result"; break;
+      case TcapEntity::tceResultNL:   dstr += ".ResultNL"; break;
+      default:;
+      }
+      dstr += " not delivered.";
+    }
+    smsc_log_error(_logger, "%s: state 0x%x, NOTICE_IND: '%s', %s", _logId,
+                   _ctrState.value, _RCS_TC_Report->code2Txt(reportCause),
+                   dstr.c_str());
+    unbindTCDialog();
+    if (!doRefHdl())
+      return;
+  }
+  //may call releaseThis()
+  _resHdl->onDialogEnd(_RCS_TC_Report->mkhash(reportCause));
+  unRefHdl(); //die if requested(
+}
+
+
+//Remote peer sent DialogEnd, it's either succsesfull contract completion,
+//or some logic error (f.ex. timeout expiration) on remote side.
+void MapDialogAC::onDialogREnd(bool compPresent)
+{
+  RCHash  errcode = 0;
+  {
+    MutexGuard  grd(_sync);
+    _ctrState.s.ctrFinished = 1;
+    if (compPresent)
+      return; //wait for ongoing Invoke result/error
+
+    unbindTCDialog();
+    if (!_ctrState.s.ctrResulted) {
+      smsc_log_error(_logger, "%s: T_END_IND, state 0x%x", _logId, _ctrState.value);
+      errcode = _RCS_MAPService->mkhash(MAPServiceRC::noServiceResponse);
+    }
+    if (!doRefHdl())
+      return;
+  }
+  //may call releaseThis()
+  _resHdl->onDialogEnd(errcode);
+  unRefHdl(); //die if requested(
+}
+
+/* ------------------------------------------------------------------------ *
+ * InvokeListener interface
+ * ------------------------------------------------------------------------ */
+
+//Called if Operation got ResultError
+void MapDialogAC::onInvokeError(InvokeRFP pInv, TcapEntity * resE)
+{
+  {
+    MutexGuard  grd(_sync);
+    smsc_log_error(_logger, "%s: %s got a returnError(%u)", _logId,
+                   pInv->idStr().c_str(), (unsigned)resE->getOpcode());
+
+    _ctrState.s.ctrInited = MapDialogAC::operDone;
+    unbindTCDialog();
+    if (!doRefHdl())
+      return;
+  }
+  //may call releaseThis()
+  _resHdl->onDialogEnd(_RCS_MAPOpErrors->mkhash(resE->getOpcode()));
+  unRefHdl(); //die if requested(
+}
+
+//Called if Operation got L_CANCEL, possibly while waiting result
+void MapDialogAC::onInvokeLCancel(InvokeRFP pInv)
+{
+  {
+    MutexGuard  grd(_sync);
+    smsc_log_error(_logger, "%s: %s got a LCancel", _logId, pInv->idStr().c_str());
+    _ctrState.s.ctrInited = MapDialogAC::operFailed;
+    unbindTCDialog();
+    if (!doRefHdl())
+      return;
+  }
+  //may call releaseThis()
+  _resHdl->onDialogEnd(_RCS_MAPService->mkhash(MAPServiceRC::noServiceResponse));
+  unRefHdl(); //die if requested(
+}
+
+
+} //inap
+} //inman
+} //smsc
+
---
+++ src/inman/inap/map/DlgMapAC.hpp
@@ -0,0 +1,152 @@
+/* ************************************************************************* *
+ * Primitive MAP dialog basic class (performs a single ROS request).
+ * ************************************************************************* */
+#ifndef __SMSC_INMAN_INAP_MAP_DIALOG__
+#ifndef __GNUC__
+#ident "@(#)$Id$"
+#endif
+#define __SMSC_INMAN_INAP_MAP_DIALOG__
+
+#include "util/URCdb/URCRegistry.hpp"
+#include "core/synchronization/EventMonitor.hpp"
+
+#include "inman/common/RefCountersMaskT.hpp"
+#include "inman/inap/dialog.hpp"
+
+namespace smsc {
+namespace inman {
+namespace inap {
+
+using smsc::util::RCHash;
+using smsc::core::synchronization::EventMonitor;
+
+class MapDlgUserIface {
+protected:
+  virtual ~MapDlgUserIface() //forbid interface destruction
+  { }
+
+public: 
+  //if err_code != 0, no result has been got from MAP service,
+  //NOTE: MAP dialog may be safely released but not deleted from this callback!
+  virtual void onDialogEnd(RCHash err_code = 0) = 0;
+  //
+  virtual void Awake(void) = 0;
+};
+
+
+//Primitive MAP dialog:
+//initiates a single ROS request to remote side,
+//awiats result, in case of success reports it to MAP User.
+class MapDialogAC : protected TCDialogUserITF {
+public:
+  static const size_t _maxIdentSZ = 32;
+
+  //NOTE: destructor blocks until last reference to this object is reset.
+  virtual ~MapDialogAC();
+
+  //Marks dialog as released. Object will be destroyed by thread that will
+  //reset the last reference. if no references present this method immediately
+  //calls destructor.
+  //NOTE: calling this method is mutually exclusive with explicit destructor call.
+  void releaseThis(void);
+
+  //Ends/Aborts MAP dialog. Releases TCAP dialog if requested.
+  //NOTE: method may block on TCAP dialog releasing.
+  void endDialog(bool do_release = true);
+
+  //Attempts to unbind MAP User.
+  //Returns true on succsess, or false if this object has established reference
+  //to handler (waits for its mutex). In that case, handler should wait on its
+  //mutex and then repeat this call.
+  bool unbindUser(void);
+
+protected:
+  explicit MapDialogAC(const char * use_ident, Logger * use_log = NULL);
+
+  // -----------------------------------------
+  // -- MapDialogAC interface methods:
+  // -----------------------------------------
+  //Releases TC dialog object
+  virtual void  rlseTCDialog(void) = 0;
+
+  friend class smsc::inman::inap::Dialog;
+  // -----------------------------------------
+  // -- TCDialogUserITF interface methods:
+  // -----------------------------------------
+  virtual void Awake(void) { _sync.notify(); }
+  //DialogListener methods
+  virtual void onDialogInvoke(Invoke * op, bool lastComp);
+  virtual void onDialogContinue(bool compPresent);
+  virtual void onDialogPAbort(uint8_t abortCause);
+  virtual void onDialogREnd(bool compPresent);
+  virtual void onDialogUAbort(uint16_t abortInfo_len, uint8_t * pAbortInfo,
+                              uint16_t userInfo_len, uint8_t * pUserInfo);
+  virtual void onDialogNotice(uint8_t reportCause,
+                              TcapEntity::TCEntityKind comp_kind = TcapEntity::tceNone,
+                              uint8_t invId = 0, uint8_t opCode = 0);
+
+  //InvokeListener methods
+  virtual void onInvokeResultNL(InvokeRFP pInv, TcapEntity * res) = 0;
+  virtual void onInvokeResult(InvokeRFP pInv, TcapEntity * res) = 0;
+  virtual void onInvokeError(InvokeRFP pInv, TcapEntity * resE);
+  virtual void onInvokeLCancel(InvokeRFP pInv);
+
+private:
+  volatile  bool  _delThis;  //delete 'this' on last reference reset
+
+protected:
+  enum ROSState_e { operNone = 0, operInited = 1, operFailed = 2, operDone = 3 };
+
+  union ContractState_u {
+    unsigned char value;
+    struct  {
+      unsigned char ctrInited : 2;
+      unsigned char ctrResulted : 2; // operInited - ResultNL, operDone - ResultL
+      unsigned char ctrAborted : 1;
+      unsigned char ctrFinished : 1;
+    } s;
+  };
+
+  //Ids of external entities, which may refer this object
+  enum RefEntities_e {
+      refIdItself = 0x00
+    , refIdHandler = 0x01
+    , refIdTCDialog = 0x02
+  };
+
+  typedef smsc::util::RefCountersMask_T<3, 2> RefCountersMask;
+  typedef smsc::core::synchronization::MTRefWrapper_T<MapDlgUserIface>
+    MapUserRef;
+
+  mutable EventMonitor  _sync;
+  ContractState_u   _ctrState;
+  RefCountersMask   _thisRefs;
+
+  const char *      _logPfx;
+  char              _logId[_maxIdentSZ + sizeof("[%u:%Xh]") + 2*sizeof(unsigned)*3 + 1];
+
+  MapUserRef        _resHdl;
+  Logger *          _logger;
+  Dialog *          _tcDlg;    //TCAP dialog
+
+  //Sets MAP user for this dialog
+  void bindUser(MapDlgUserIface & res_hdl);
+  //Binds TC Dialog
+  void bindTCDialog(uint16_t timeout_sec = 0);
+  //ends TC dialog
+  void endTCDialog(void);
+  //ends TC dialog and releases TC dialog object
+  void unbindTCDialog(void);
+  //Locks result handler.
+  //Returns false if result handler is not set.
+  bool doRefHdl(void);
+  //Unlocks result handler and destroys this object if it's released
+  void unRefHdl(void);
+};
+
+} //inap
+} //inman
+} //smsc
+
+#endif /* __SMSC_INMAN_INAP_MAP_DIALOG__ */
+
