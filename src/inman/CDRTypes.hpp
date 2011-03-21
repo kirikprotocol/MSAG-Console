97ab710927fdd41db197c48c983227773015113d
gvroman <>
2011-03-21 11:49:23 +0000

    corrected r1.4

--- src/inman/CDRTypes.hpp
+++ src/inman/CDRTypes.hpp
@@ -12,18 +12,18 @@
 namespace smsc  {
 namespace inman {
 
-const unsigned MAX_ESERVICE_TYPE_LENGTH = 6;
-const unsigned MAX_SMESYSID_TYPE_LENGTH = 16;
-const unsigned MAX_ROUTE_ID_TYPE_LENGTH = 33;
+const unsigned MAX_ESERVICE_TYPE_LENGTH = 5;
+const unsigned MAX_SMESYSID_TYPE_LENGTH = 15;
+const unsigned MAX_ROUTE_ID_TYPE_LENGTH = 32;
 
 //SMPP DATA_SM service type
-typedef smsc::core::buffers::FixedLengthString<MAX_ESERVICE_TYPE_LENGTH>
+typedef smsc::core::buffers::FixedLengthString<MAX_ESERVICE_TYPE_LENGTH + 1>
   SMPPServiceType;
 //SM Entity system id
-typedef smsc::core::buffers::FixedLengthString<MAX_SMESYSID_TYPE_LENGTH>
+typedef smsc::core::buffers::FixedLengthString<MAX_SMESYSID_TYPE_LENGTH + 1>
   SMESysId;
 //SM Route ident
-typedef smsc::core::buffers::FixedLengthString<MAX_ROUTE_ID_TYPE_LENGTH>
+typedef smsc::core::buffers::FixedLengthString<MAX_ROUTE_ID_TYPE_LENGTH + 1>
   SMRouteId;
 
 } //inman
