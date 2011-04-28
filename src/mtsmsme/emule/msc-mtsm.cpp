53e556800b879621b5cb6d7666a0b7d73e7da79f
vic <>
2011-04-28 09:27:42 +0000

    *** empty log message ***

--- src/mtsmsme/emule/msc-mtsm.cpp
+++ src/mtsmsme/emule/msc-mtsm.cpp
@@ -7,6 +7,9 @@ static char const ident[] = "$Id";
 #include "mtsmsme/processor/HLRImpl.hpp"
 #include "mtsmsme/comp/SendRoutingInfoForSM.hpp"
 #include "mtsmsme/processor/util.hpp"
+#include "util/config/Manager.h"
+#include "util/config/ConfigView.h"
+#include "util/config/ConfigException.h"
 #include "sms/sms.h"
 #include <string>
 
@@ -31,12 +34,18 @@ using smsc::mtsmsme::processor::SccpSender;
 using smsc::mtsmsme::comp::SendRoutingInfoForSMReq;
 using smsc::mtsmsme::processor::util::packSCCPAddress;
 using smsc::mtsmsme::processor::util::dump;
+using smsc::util::config::Manager;
+using smsc::util::config::ConfigView;
+using smsc::util::config::ConfigException;
 using smsc::sms::Address;
 using std::string;
 
-static char msca[] = "791398699874"; // MSC address
-static char vlra[] = "79139860004"; //VLR address
-static char hlra[] = "79139860004"; //HLR address
+static char msca[20] = "791398699874"; // MSC address
+static char vlra[20] = "79139860004"; //VLR address
+static char hlra[20] = "79139860004"; //HLR address
+static int  ssn = 191; //SSN
+static int  userid = 43; //common part user id
+static int  delay = 8; // MT SMS time processing
 
 static Logger *logger = 0;
 class EmptyRequestSender: public RequestSender {
@@ -87,6 +96,69 @@ class DialogueStat: public Thread {
       return 0;
     }
   };
+class ToolConfig {
+  private:
+    int cfgdelay;
+    Logger* logger;
+  public:
+    ToolConfig(Logger* _logger):logger(_logger){}
+    void read(Manager& manager)
+    {
+      if (!manager.findSection("msc-mtsm"))
+        throw ConfigException("\'msc-mtsm\' section is missed");
+
+      ConfigView view(manager, "msc-mtsm");
+
+      try { cfgdelay = view.getInt("delay");
+      } catch (ConfigException& exc) {
+        throw ConfigException("\'delay\' is unknown or missing");
+      }
+      delay = cfgdelay;
+    }
+};
+class SccpConfig {
+  private:
+    Logger* logger;
+  public:
+    SccpConfig(Logger* _logger):logger(_logger){}
+    void read(Manager& manager)
+    {
+      char* tmp_str;
+
+      if (!manager.findSection("sccp"))
+        throw ConfigException("\'sccp\' section is missed");
+
+      ConfigView sccpConfig(manager, "sccp");
+
+      try { userid = sccpConfig.getInt("user_id");
+      } catch (ConfigException& exc) {
+        throw ConfigException("\'user_id\' is unknown or missing");
+      }
+
+      try { ssn = sccpConfig.getInt("user_ssn");
+      } catch (ConfigException& exc) {
+        throw ConfigException("\'user_ssn\' is unknown or missing");
+      }
+
+      try { tmp_str = sccpConfig.getString("msc_gt");
+            strcpy(msca,tmp_str);
+      } catch (ConfigException& exc) {
+        throw ConfigException("\'msc_gt\' is unknown or missing");
+      }
+
+      try { tmp_str = sccpConfig.getString("vlr_gt");
+            strcpy(vlra,tmp_str);
+      } catch (ConfigException& exc) {
+        throw ConfigException("\'vlr_gt\' is unknown or missing");
+      }
+
+      try { tmp_str = sccpConfig.getString("hlr_gt");
+            strcpy(hlra,tmp_str);
+      } catch (ConfigException& exc) {
+        throw ConfigException("\'hlr_gt\' is unknown or missing");
+      }
+    }
+};
 int main(int argc, char** argv)
 {
   smsc::logger::Logger::Init();
@@ -94,7 +166,12 @@ int main(int argc, char** argv)
 
   try
   {
-    smsc_log_info(logger, "MSC and HLR emulator");
+    smsc_log_info(logger, "MT SMS receiver");
+    Manager::init("config.xml");
+    Manager& manager = Manager::getInstance();
+    ToolConfig config(logger);
+    config.read(manager);
+
     using smsc::core::threads::Thread;
     EmptyRequestSender fakeSender;
     TCO mtsms(10000);
@@ -102,7 +179,7 @@ int main(int argc, char** argv)
     mtsms.setRequestSender(&fakeSender);
     GopotaListener listener(&mtsms,&fakeHLR);
     DialogueStat stat;
-    listener.configure(43, 191, Address((uint8_t)strlen(msca), 1, 1, msca),
+    listener.configure(ssn, userid, Address((uint8_t)strlen(msca), 1, 1, msca),
         Address((uint8_t)strlen(vlra), 1, 1, vlra),
         Address((uint8_t)strlen(hlra), 1, 1, hlra));
     listener.Start();
