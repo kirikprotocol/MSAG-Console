8ef307f78a8843f41ea713e15dfe52bcc3f7b3f6
bukind <>
2011-05-23 04:24:11 +0000

    typos

--- src/scag/re/actions/impl/ActionGenReceiptId.cpp
+++ src/scag/re/actions/impl/ActionGenReceiptId.cpp
@@ -32,7 +32,7 @@ bool ActionGenReceiptId::run( ActionContext& context )
     if (!property) {
         throw SCAGException("Action '%s': Invalid property for %s", opname(),id_.getName());
     }
-    static msectime_type startMsec = 0;
+    static util::msectime_type startMsec = 0;
     static smsc::core::synchronization::Mutex lock;
     if (!startMsec) {
         smsc::core::synchronization::MutexGuard mg(lock);
@@ -42,14 +42,14 @@ bool ActionGenReceiptId::run( ActionContext& context )
             t.tm_year = 2010 - 1900;
             t.tm_mday = 1;
             t.tm_isdst = -1;
-            startMsec = msectime_type(mktime(&t)) * 1000;
+            startMsec = util::msectime_type(mktime(&t)) * 1000;
         }
     }
-    const msectime_type msec = currentTimeMillis();
-    msectime_type diff = msec - startMsec;
+    const util::msectime_type msec = util::currentTimeMillis();
+    util::msectime_type diff = msec - startMsec;
     {
         smsc::core::synchronization::MutexGuard mg(lock);    
-        static msectime_type curVal = 0;
+        static util::msectime_type curVal = 0;
         if ( diff < curVal ) { diff = ++curVal; }
         else { curVal = diff; }
     }
