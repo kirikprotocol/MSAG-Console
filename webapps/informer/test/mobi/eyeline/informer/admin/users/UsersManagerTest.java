4c68915dd4993464f249dac3694889a4af176085
alkhal <>
2011-06-15 04:56:25 +0000

    informer web: add cp removing test

--- webapps/informer/test/mobi/eyeline/informer/admin/users/UsersManagerTest.java
+++ webapps/informer/test/mobi/eyeline/informer/admin/users/UsersManagerTest.java
@@ -3,39 +3,31 @@ package mobi.eyeline.informer.admin.users;
 import mobi.eyeline.informer.admin.filesystem.FileSystem;
 import mobi.eyeline.informer.admin.infosme.Infosme;
 import mobi.eyeline.informer.admin.infosme.TestInfosme;
-import mobi.eyeline.informer.admin.regions.RegionsManager;
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import testutils.TestUtils;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * author: alkhal
  */
 public class UsersManagerTest {
 
-  private static File configFile, backupDir;
+  private File configFile, backupDir;
 
-  private static Infosme infosme;
-  private static UsersManager usersManager;
-
-  @BeforeClass
-  public static void init() throws Exception {
-    configFile = TestUtils.exportResourceToRandomFile(UsersManagerTest.class.getResourceAsStream("users.xml"), ".user");
-    backupDir = TestUtils.createRandomDir(".users.backup");
-  }
+  private Infosme infosme;
+  private UsersManager usersManager;
 
   @Before
   public void before() throws Exception {
+    configFile = TestUtils.exportResourceToRandomFile(UsersManagerTest.class.getResourceAsStream("users.xml"), ".user");
+    backupDir = TestUtils.createRandomDir(".users.backup");
     infosme = new TestInfosme();
     usersManager = new TestUsersManager(infosme, configFile, backupDir, FileSystem.getFSForSingleInst());
     for(User u : usersManager.getUsers()) {
@@ -46,7 +38,6 @@ public class UsersManagerTest {
   @Test
   public void loadSave() throws Exception{
     Collection<User> old = usersManager.getUsers();
-    Collection<User> newU = new ArrayList<User>(old.size());
     for(User u : usersManager.getUsers()) {
       usersManager.updateUser(new User(u));
     }
@@ -66,11 +57,41 @@ public class UsersManagerTest {
     }
   }
 
+  @Test
+  public void testRemovingCPR() throws Exception {
+    List<User> users = usersManager.getUsers();
+    if(users.isEmpty()) {
+      fail("Users are empty. Nothing to test");
+    }
+    User u = users.get(0);
+    List<UserCPsettings> cpSettings = u.getCpSettings();
+    if(cpSettings.isEmpty()) {
+      fail("Settings are empty. Nothing to test");
+    }
+    UserCPsettings cps = cpSettings.remove(0);
+
+    u.setCpSettings(cpSettings);
+    usersManager.updateUser(u);
+
+    usersManager = new TestUsersManager(infosme, configFile, backupDir, FileSystem.getFSForSingleInst());
+
+    List<UserCPsettings> cpSettings1 = usersManager.getUser(u.getLogin()).getCpSettings();
+
+    assertEquals(cpSettings.size(), cpSettings1.size());
+
+    for(UserCPsettings cps1 : cpSettings1) {
+      if(cps1.getHashId().equals(cps.getHashId())) {
+       fail("Settings is not removed");
+      }
+    }
+
+  }
+
 
 
   @SuppressWarnings({"ResultOfMethodCallIgnored"})
-  @AfterClass
-  public static void shutdown() {
+  @After
+  public void shutdown() {
     if(configFile != null) {
       configFile.delete();
     }
