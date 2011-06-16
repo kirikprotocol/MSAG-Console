0160e6f62442a0cb79b15602e89731f0db764654
gvroman <>
2011-06-16 09:09:32 +0000

    migrated to native C99 int types

--- src/inman/interaction/serializer/SerializeIntegers.cpp
+++ src/inman/interaction/serializer/SerializeIntegers.cpp
@@ -2,11 +2,8 @@
 static char const ident[] = "@(#)$Id$";
 #endif /* MOD_IDENT_ON */
 
-#include <arpa/inet.h>
-
-#include "util/Uint64Converter.h"
-
 #include "inman/interaction/serializer/SerializeIntegers.hpp"
+#include "util/NetHostConverter.hpp"
 
 namespace smsc  {
 namespace inman {
@@ -16,27 +13,27 @@ namespace interaction {
  * unsigned ints serialization
  * -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const uint64_t & in_val) throw(SerializerException)
+  operator<<(PacketBufferAC & out_buf, const unsigned long long & in_val) throw(SerializerException)
 {
-  uint64_t nval = smsc::util::Uint64Converter::toNetworkOrder(in_val);
-  if (out_buf.Append((unsigned char*)&nval, 8) < 8)
-    throw BufferOverflowException("uint64_t", 8);
+  unsigned long long nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(unsigned long long)) < sizeof(unsigned long long))
+    throw BufferOverflowException("unsigned long long", (unsigned long)sizeof(unsigned long long));
   return out_buf;
 }
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, uint64_t & out_val) throw(SerializerException)
+  operator>>(PacketBufferAC & in_buf, unsigned long long & out_val) throw(SerializerException)
 {
-  uint64_t nval;
-  if (in_buf.Read((unsigned char*)&nval, 8) < 8)
-    throw BufferDataException(in_buf.getPos(), " >> uint64_t: corrupted data");
+  smsc::util::AlignedInt_T<unsigned long long> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(unsigned long long)) < sizeof(unsigned long long))
+    throw BufferDataException(in_buf.getPos(), " >> unsigned long long: corrupted data");
 
-  out_val = smsc::util::Uint64Converter::toHostOrder(nval);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return in_buf;
 }
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, uint64_t & out_val) throw(SerializerException)
+  operator>>(const PacketBufferAC & in_buf, unsigned long long & out_val) throw(SerializerException)
 {
   PacketBufferCursor  curs(in_buf, 0);
   curs >> out_val;
@@ -44,38 +41,40 @@ PacketBufferAC::size_type
 }
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <uint64_t > (uint64_t & out_val) throw(SerializerException)
+  PacketBufferCursor::operator>> <unsigned long long > (unsigned long long & out_val) throw(SerializerException)
 {
-  uint64_t nval;
-  if (_pckBuf.Copy((unsigned char*)&nval, _pckBuf.getPos() + _curOfs, 8) < 8)
-    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> uint64_t: corrupted data");
+  smsc::util::AlignedInt_T<unsigned long long> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(unsigned long long)) < sizeof(unsigned long long))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> unsigned long long: corrupted data");
 
-  _curOfs += 8;
-  out_val = smsc::util::Uint64Converter::toHostOrder(nval);
+  _curOfs += (PacketBufferAC::size_type)sizeof(unsigned long long);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return *this;
 }
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const uint32_t & in_val) throw(SerializerException)
+  operator<<(PacketBufferAC & out_buf, const unsigned long & in_val) throw(SerializerException)
 {
-  uint32_t nval = htonl(in_val);
-  if (out_buf.Append((unsigned char*)&nval, 4) < 4)
-    throw BufferOverflowException("uint32_t", 4);
+  unsigned long nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(unsigned long)) < sizeof(unsigned long))
+    throw BufferOverflowException("unsigned long", (unsigned long)sizeof(unsigned long));
   return out_buf;
 }
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, uint32_t & out_val) throw(SerializerException)
+  operator>>(PacketBufferAC & in_buf, unsigned long & out_val) throw(SerializerException)
 {
-  if (in_buf.Read((unsigned char*)&out_val, 4) < 4)
-    throw BufferDataException(in_buf.getPos(), " >> uint32_t: corrupted data");
-  out_val = ntohl(out_val);
+  smsc::util::AlignedInt_T<unsigned long> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(unsigned long)) < sizeof(unsigned long))
+    throw BufferDataException(in_buf.getPos(), " >> unsigned long: corrupted data");
+
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return in_buf;
 }
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, uint32_t & out_val) throw(SerializerException)
+  operator>>(const PacketBufferAC & in_buf, unsigned long & out_val) throw(SerializerException)
 {
   PacketBufferCursor  curs(in_buf, 0);
   curs >> out_val;
@@ -83,38 +82,40 @@ PacketBufferAC::size_type
 }
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <uint32_t > (uint32_t & out_val)
-    throw(SerializerException)
+  PacketBufferCursor::operator>> <unsigned long > (unsigned long & out_val) throw(SerializerException)
 {
-  if (_pckBuf.Copy((unsigned char*)&out_val, _pckBuf.getPos() + _curOfs, 4) < 4)
-    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> uint32_t: corrupted data");
+  smsc::util::AlignedInt_T<unsigned long> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(unsigned long)) < sizeof(unsigned long))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> unsigned long: corrupted data");
 
-  _curOfs += 4;
-  out_val = ntohl(out_val);
+  _curOfs += (PacketBufferAC::size_type)sizeof(unsigned long);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return *this;
 }
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const uint16_t & in_val) throw(SerializerException)
+  operator<<(PacketBufferAC & out_buf, const unsigned int & in_val) throw(SerializerException)
 {
-  uint16_t nval = htons(in_val);
-  if (out_buf.Append((unsigned char*)&nval, 2) < 2)
-    throw BufferOverflowException("uint16_t", 2);
+  unsigned int nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(unsigned int)) < sizeof(unsigned int))
+    throw BufferOverflowException("unsigned int", (unsigned long)sizeof(unsigned int));
   return out_buf;
 }
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, uint16_t & out_val) throw(SerializerException)
+  operator>>(PacketBufferAC & in_buf, unsigned int & out_val) throw(SerializerException)
 {
-  if (in_buf.Read((unsigned char*)&out_val, 2) < 2)
-    throw BufferDataException(in_buf.getPos(), " >> uint16_t: corrupted data");
-  out_val = ntohs(out_val);
+  smsc::util::AlignedInt_T<unsigned int> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(unsigned int)) < sizeof(unsigned int))
+    throw BufferDataException(in_buf.getPos(), " >> unsigned int: corrupted data");
+
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return in_buf;
 }
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, uint16_t & out_val) throw(SerializerException)
+  operator>>(const PacketBufferAC & in_buf, unsigned int & out_val) throw(SerializerException)
 {
   PacketBufferCursor  curs(in_buf, 0);
   curs >> out_val;
@@ -122,41 +123,84 @@ PacketBufferAC::size_type
 }
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <uint16_t > (uint16_t & out_val)
-    throw(SerializerException)
+  PacketBufferCursor::operator>> <unsigned int > (unsigned int & out_val) throw(SerializerException)
 {
-  if (_pckBuf.Copy((unsigned char*)&out_val, _pckBuf.getPos() + _curOfs, 2) < 2)
-    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> uint16_t: corrupted data");
-  _curOfs += 2;
-  out_val = ntohs(out_val);
+  smsc::util::AlignedInt_T<unsigned int> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(unsigned int)) < sizeof(unsigned int))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> unsigned int: corrupted data");
+
+  _curOfs += (PacketBufferAC::size_type)sizeof(unsigned int);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return *this;
 }
+
+/* -------------------------------------------------------------------------- */
+PacketBufferAC &
+  operator<<(PacketBufferAC & out_buf, const unsigned short & in_val) throw(SerializerException)
+{
+  unsigned short nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(unsigned short)) < sizeof(unsigned short))
+    throw BufferOverflowException("unsigned short", (unsigned long)sizeof(unsigned short));
+  return out_buf;
+}
+//
+PacketBufferAC &
+  operator>>(PacketBufferAC & in_buf, unsigned short & out_val) throw(SerializerException)
+{
+  smsc::util::AlignedInt_T<unsigned short> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(unsigned short)) < sizeof(unsigned short))
+    throw BufferDataException(in_buf.getPos(), " >> unsigned short: corrupted data");
+
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
+  return in_buf;
+}
+//
+PacketBufferAC::size_type
+  operator>>(const PacketBufferAC & in_buf, unsigned short & out_val) throw(SerializerException)
+{
+  PacketBufferCursor  curs(in_buf, 0);
+  curs >> out_val;
+  return curs._curOfs;
+}
+//
+template <> PacketBufferCursor &
+  PacketBufferCursor::operator>> <unsigned short > (unsigned short & out_val) throw(SerializerException)
+{
+  smsc::util::AlignedInt_T<unsigned short> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(unsigned short)) < sizeof(unsigned short))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> unsigned short: corrupted data");
+
+  _curOfs += (PacketBufferAC::size_type)sizeof(unsigned short);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
+  return *this;
+}
+
 
 /* -------------------------------------------------------------------------- *
  * signed ints serialization
  * -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const int64_t & in_val) throw(SerializerException)
+  operator<<(PacketBufferAC & out_buf, const long long & in_val) throw(SerializerException)
 {
-  uint64_t nval = smsc::util::Uint64Converter::toNetworkOrder((uint64_t)in_val);
-  if (out_buf.Append((unsigned char*)&nval, 8) < 8)
-    throw BufferOverflowException("int64_t", 8);
+  long long nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(long long)) < sizeof(long long))
+    throw BufferOverflowException("long long", (unsigned long)sizeof(long long));
   return out_buf;
 }
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, int64_t & out_val) throw(SerializerException)
+  operator>>(PacketBufferAC & in_buf, long long & out_val) throw(SerializerException)
 {
-  uint64_t nval;
-  if (in_buf.Read((unsigned char*)&nval, 8) < 8)
-    throw BufferDataException(in_buf.getPos(), " >> int64_t: corrupted data");
+  smsc::util::AlignedInt_T<long long> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(long long)) < sizeof(long long))
+    throw BufferDataException(in_buf.getPos(), " >> long long: corrupted data");
 
-  out_val = (int64_t)smsc::util::Uint64Converter::toHostOrder(nval);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return in_buf;
 }
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, int64_t & out_val) throw(SerializerException)
+  operator>>(const PacketBufferAC & in_buf, long long & out_val) throw(SerializerException)
 {
   PacketBufferCursor  curs(in_buf, 0);
   curs >> out_val;
@@ -164,38 +208,40 @@ PacketBufferAC::size_type
 }
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <int64_t > (int64_t & out_val) throw(SerializerException)
+  PacketBufferCursor::operator>> <long long > (long long & out_val) throw(SerializerException)
 {
-  uint64_t nval;
-  if (_pckBuf.Copy((unsigned char*)&nval, _pckBuf.getPos() + _curOfs, 8) < 8)
-    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> int64_t: corrupted data");
+  smsc::util::AlignedInt_T<long long> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(long long)) < sizeof(long long))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> long long: corrupted data");
 
-  _curOfs += 8;
-  out_val = (int64_t)smsc::util::Uint64Converter::toHostOrder(nval);
+  _curOfs += (PacketBufferAC::size_type)sizeof(long long);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return *this;
 }
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const int32_t & in_val) throw(SerializerException)
+  operator<<(PacketBufferAC & out_buf, const long & in_val) throw(SerializerException)
 {
-  uint32_t nval = htonl((uint32_t)in_val);
-  if (out_buf.Append((unsigned char*)&nval, 4) < 4)
-    throw BufferOverflowException("int32_t", 4);
+  long nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(long)) < sizeof(long))
+    throw BufferOverflowException("long", (unsigned long)sizeof(long));
   return out_buf;
 }
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, int32_t & out_val) throw(SerializerException)
+  operator>>(PacketBufferAC & in_buf, long & out_val) throw(SerializerException)
 {
-  if (in_buf.Read((unsigned char*)&out_val, 4) < 4)
-    throw BufferDataException(in_buf.getPos(), " >> int32_t: corrupted data");
-  out_val = (int32_t)ntohl((uint32_t)out_val);
+  smsc::util::AlignedInt_T<long> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(long)) < sizeof(long))
+    throw BufferDataException(in_buf.getPos(), " >> long: corrupted data");
+
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return in_buf;
 }
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, int32_t & out_val) throw(SerializerException)
+  operator>>(const PacketBufferAC & in_buf, long & out_val) throw(SerializerException)
 {
   PacketBufferCursor  curs(in_buf, 0);
   curs >> out_val;
@@ -203,37 +249,40 @@ PacketBufferAC::size_type
 }
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <int32_t > (int32_t & out_val) throw(SerializerException)
+  PacketBufferCursor::operator>> <long > (long & out_val) throw(SerializerException)
 {
-  if (_pckBuf.Copy((unsigned char*)&out_val, _pckBuf.getPos() + _curOfs, 4) < 4)
-    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> int32_t: corrupted data");
+  smsc::util::AlignedInt_T<long> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(long)) < sizeof(long))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> long: corrupted data");
 
-  _curOfs += 4;
-  out_val = (int32_t)ntohl((uint32_t)out_val);
+  _curOfs += (PacketBufferAC::size_type)sizeof(long);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return *this;
 }
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const int16_t & in_val) throw(SerializerException)
+  operator<<(PacketBufferAC & out_buf, const int & in_val) throw(SerializerException)
 {
-  uint16_t nval = htons((uint16_t)in_val);
-  if (out_buf.Append((unsigned char*)&nval, 2) < 2)
-    throw BufferOverflowException("int16_t", 2);
+  int nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(int)) < sizeof(int))
+    throw BufferOverflowException("int", (unsigned long)sizeof(int));
   return out_buf;
 }
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, int16_t & out_val) throw(SerializerException)
+  operator>>(PacketBufferAC & in_buf, int & out_val) throw(SerializerException)
 {
-  if (in_buf.Read((unsigned char*)&out_val, 2) < 2)
-    throw BufferDataException(in_buf.getPos(), " >> int16_t: corrupted data");
-  out_val = (int16_t)ntohs((uint16_t)out_val);
+  smsc::util::AlignedInt_T<int> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(int)) < sizeof(int))
+    throw BufferDataException(in_buf.getPos(), " >> int: corrupted data");
+
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return in_buf;
 }
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, int16_t & out_val) throw(SerializerException)
+  operator>>(const PacketBufferAC & in_buf, int & out_val) throw(SerializerException)
 {
   PacketBufferCursor  curs(in_buf, 0);
   curs >> out_val;
@@ -241,13 +290,55 @@ PacketBufferAC::size_type
 }
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <int16_t > (int16_t & out_val) throw(SerializerException)
+  PacketBufferCursor::operator>> <int > (int & out_val) throw(SerializerException)
 {
-  if (_pckBuf.Copy((unsigned char*)&out_val, _pckBuf.getPos() + _curOfs, 2) < 2)
-    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> int16_t: corrupted data");
+  smsc::util::AlignedInt_T<int> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(int)) < sizeof(int))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> int: corrupted data");
 
-  _curOfs += 2;
-  out_val = (int16_t)ntohs((uint16_t)out_val);
+  _curOfs += (PacketBufferAC::size_type)sizeof(int);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
+  return *this;
+}
+
+/* -------------------------------------------------------------------------- */
+PacketBufferAC &
+  operator<<(PacketBufferAC & out_buf, const short & in_val) throw(SerializerException)
+{
+  short nval = smsc::util::NetHostConverter::toNetworkOrder(in_val);
+  if (out_buf.Append((unsigned char*)&nval, (PacketBufferAC::size_type)sizeof(short)) < sizeof(short))
+    throw BufferOverflowException("short", (unsigned long)sizeof(short));
+  return out_buf;
+}
+//
+PacketBufferAC &
+  operator>>(PacketBufferAC & in_buf, short & out_val) throw(SerializerException)
+{
+  smsc::util::AlignedInt_T<short> nval;
+  if (in_buf.Read(nval._buf, (PacketBufferAC::size_type)sizeof(short)) < sizeof(short))
+    throw BufferDataException(in_buf.getPos(), " >> short: corrupted data");
+
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
+  return in_buf;
+}
+//
+PacketBufferAC::size_type
+  operator>>(const PacketBufferAC & in_buf, short & out_val) throw(SerializerException)
+{
+  PacketBufferCursor  curs(in_buf, 0);
+  curs >> out_val;
+  return curs._curOfs;
+}
+//
+template <> PacketBufferCursor &
+  PacketBufferCursor::operator>> <short > (short & out_val) throw(SerializerException)
+{
+  smsc::util::AlignedInt_T<short> nval;
+  if (_pckBuf.Copy(nval._buf, _pckBuf.getPos() + _curOfs, (PacketBufferAC::size_type)sizeof(short)) < sizeof(short))
+    throw BufferDataException(_pckBuf.getPos() + _curOfs, " >> short: corrupted data");
+
+  _curOfs += (PacketBufferAC::size_type)sizeof(short);
+  out_val = smsc::util::NetHostConverter::toHostOrder(nval._ival);
   return *this;
 }
 
--- src/inman/interaction/serializer/SerializeIntegers.hpp
+++ src/inman/interaction/serializer/SerializeIntegers.hpp
@@ -1,13 +1,12 @@
 /* ************************************************************************** *
- * Definition of serializer operators ('<<' and'>>') for Integer and bool types.
+ * Definition of serializer operators ('<<' and'>>') for native C99 integer 
+ * and bool types.
  * ************************************************************************** */
 #ifndef __SMSC_INMAN_INTERACTION_SERIALIZER_INTS_BOOL
 #ifndef __GNUC__
 #ident "@(#)$Id$"
 #endif
 #define __SMSC_INMAN_INTERACTION_SERIALIZER_INTS_BOOL
-
-#include <inttypes.h>
 
 #include "inman/interaction/serializer/SerializerDefs.hpp"
 
@@ -18,83 +17,109 @@ namespace interaction {
  * unsigned ints serialization
  * -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const uint64_t & in_val) throw(SerializerException);
+  operator<<(PacketBufferAC & out_buf, const unsigned long long & in_val) throw(SerializerException);
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, uint64_t & out_val) throw(SerializerException);
+  operator>>(PacketBufferAC & in_buf, unsigned long long & out_val) throw(SerializerException);
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, uint64_t & out_val) throw(SerializerException);
+  operator>>(const PacketBufferAC & in_buf, unsigned long long & out_val) throw(SerializerException);
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <uint64_t > (uint64_t & out_val) throw(SerializerException);
+  PacketBufferCursor::operator>> <unsigned long long > (unsigned long long & out_val) throw(SerializerException);
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const uint32_t & in_val) throw(SerializerException);
+  operator<<(PacketBufferAC & out_buf, const unsigned long & in_val) throw(SerializerException);
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, uint32_t & out_val) throw(SerializerException);
+  operator>>(PacketBufferAC & in_buf, unsigned long & out_val) throw(SerializerException);
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, uint32_t & out_val) throw(SerializerException);
+  operator>>(const PacketBufferAC & in_buf, unsigned long & out_val) throw(SerializerException);
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <uint32_t > (uint32_t & out_val) throw(SerializerException);
+  PacketBufferCursor::operator>> <unsigned long > (unsigned long & out_val) throw(SerializerException);
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const uint16_t & in_val) throw(SerializerException);
+  operator<<(PacketBufferAC & out_buf, const unsigned int & in_val) throw(SerializerException);
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, uint16_t & out_val) throw(SerializerException);
+  operator>>(PacketBufferAC & in_buf, unsigned int & out_val) throw(SerializerException);
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, uint16_t & out_val) throw(SerializerException);
+  operator>>(const PacketBufferAC & in_buf, unsigned int & out_val) throw(SerializerException);
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <uint16_t > (uint16_t & out_val) throw(SerializerException);
+  PacketBufferCursor::operator>> <unsigned int > (unsigned int & out_val) throw(SerializerException);
+
+/* -------------------------------------------------------------------------- */
+PacketBufferAC &
+  operator<<(PacketBufferAC & out_buf, const unsigned short & in_val) throw(SerializerException);
+//
+PacketBufferAC &
+  operator>>(PacketBufferAC & in_buf, unsigned short & out_val) throw(SerializerException);
+//
+PacketBufferAC::size_type
+  operator>>(const PacketBufferAC & in_buf, unsigned short & out_val) throw(SerializerException);
+//
+template <> PacketBufferCursor &
+  PacketBufferCursor::operator>> <unsigned short > (unsigned short & out_val) throw(SerializerException);
 
 /* -------------------------------------------------------------------------- *
  * signed ints serialization
  * -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const int64_t & in_val) throw(SerializerException);
+  operator<<(PacketBufferAC & out_buf, const long long & in_val) throw(SerializerException);
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, int64_t & out_val) throw(SerializerException);
+  operator>>(PacketBufferAC & in_buf, long long & out_val) throw(SerializerException);
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, int64_t & out_val) throw(SerializerException);
+  operator>>(const PacketBufferAC & in_buf, long long & out_val) throw(SerializerException);
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <int64_t > (int64_t & out_val) throw(SerializerException);
+  PacketBufferCursor::operator>> <long long > (long long & out_val) throw(SerializerException);
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const int32_t & in_val) throw(SerializerException);
+  operator<<(PacketBufferAC & out_buf, const long & in_val) throw(SerializerException);
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, int32_t & out_val) throw(SerializerException);
+  operator>>(PacketBufferAC & in_buf, long & out_val) throw(SerializerException);
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, int32_t & out_val) throw(SerializerException);
+  operator>>(const PacketBufferAC & in_buf, long & out_val) throw(SerializerException);
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <int32_t > (int32_t & out_val) throw(SerializerException);
+  PacketBufferCursor::operator>> <long > (long & out_val) throw(SerializerException);
 
 /* -------------------------------------------------------------------------- */
 PacketBufferAC &
-  operator<<(PacketBufferAC & out_buf, const int16_t & in_val) throw(SerializerException);
+  operator<<(PacketBufferAC & out_buf, const int & in_val) throw(SerializerException);
 //
 PacketBufferAC &
-  operator>>(PacketBufferAC & in_buf, int16_t & out_val) throw(SerializerException);
+  operator>>(PacketBufferAC & in_buf, int & out_val) throw(SerializerException);
 //
 PacketBufferAC::size_type
-  operator>>(const PacketBufferAC & in_buf, int16_t & out_val) throw(SerializerException);
+  operator>>(const PacketBufferAC & in_buf, int & out_val) throw(SerializerException);
 //
 template <> PacketBufferCursor &
-  PacketBufferCursor::operator>> <int16_t > (int16_t & out_val) throw(SerializerException);
+  PacketBufferCursor::operator>> <int > (int & out_val) throw(SerializerException);
+
+/* -------------------------------------------------------------------------- */
+PacketBufferAC &
+  operator<<(PacketBufferAC & out_buf, const short & in_val) throw(SerializerException);
+//
+PacketBufferAC &
+  operator>>(PacketBufferAC & in_buf, short & out_val) throw(SerializerException);
+//
+PacketBufferAC::size_type
+  operator>>(const PacketBufferAC & in_buf, short & out_val) throw(SerializerException);
+//
+template <> PacketBufferCursor &
+  PacketBufferCursor::operator>> <short > (short & out_val) throw(SerializerException);
 
 /* -------------------------------------------------------------------------- *
  * char serialization
