1dfb41f5e4ce9fbc5b3561b863c2997555aa9565
shulga <>
2011-06-24 03:22:40 +0000

    added enumeration constant CONTINUE_READ_PACKET

--- src/mcisme/advert/AdvertErrors.h
+++ src/mcisme/advert/AdvertErrors.h
@@ -7,11 +7,13 @@
 namespace smsc {
 namespace mcisme {
 
-enum 
+enum banner_read_stat
 {
+  BANNER_OK = 0,
   ERR_ADV_TIMEOUT = 1,    // таймаут сервера 
-  ERR_ADV_NOT_CONNECTED,
-  ERR_ADV_OTHER
+  ERR_ADV_NOT_CONNECTED = 2,
+  ERR_ADV_OTHER = 3,
+  CONTINUE_READ_PACKET
 };
 
 }}
