e05a6f80524567c496c87161947e6d1b2e033d2b
shulga <>
2011-06-30 16:55:44 +0000

    Refactoring was made (move up method)

--- src/mcisme/advert/BEProtocolV2SimpleClient.cpp
+++ src/mcisme/advert/BEProtocolV2SimpleClient.cpp
@@ -1,68 +1,18 @@
-#include <mcisme/Exceptions.hpp>
+#include "mcisme/Exceptions.hpp"
 #include "BEProtocolV2SimpleClient.hpp"
-#include "AdvertErrors.h"
 
 namespace smsc {
 namespace mcisme {
 
-std::string
-hexdmp(const uchar_t* buf, size_t bufSz);
-
 BEProtocolV2SimpleClient::BEProtocolV2SimpleClient(const std::string& host, int port)
-  : AdvertisingImpl(host, port), _pos(0), _totalPacketLen(0)
+  : AdvertisingImpl(host, port)
 {}
 
-uint32_t
-BEProtocolV2SimpleClient::readPacket()
-{
-  try
-  {
-    int bytes_to_read;
-    if (!_totalPacketLen)
-      bytes_to_read = CMD_HEADER_SIZE - _pos;
-    else
-      bytes_to_read = _totalPacketLen - _pos;
-
-    int read_sz = readFromSocket(_buf + _pos, bytes_to_read, "BEProtocolV2SimpleClient::readPacket");
-    _pos += read_sz;
-    if (!_totalPacketLen)
-    {
-      if (_pos < CMD_HEADER_SIZE)
-        return 0;
-
-      uint32_t *s = (uint32_t*)_buf;
-      _totalPacketLen = ntohl(s[1]) + CMD_HEADER_SIZE;
-
-      if (_totalPacketLen > sizeof(_buf))
-      {
-        smsc_log_warn(_logger, "BEProtocolV2SimpleClient::readPacket::: got too long packet, totalPacketLen==%d", _totalPacketLen);
-        _totalPacketLen = 0; _pos = 0;
-        generateUnrecoveredProtocolError();
-      }
-
-      return 0;
-    }
-
-    if (_pos == _totalPacketLen)
-    {
-      // we have read total packet
-      uint32_t totalPacketLen = _totalPacketLen;
-      _pos=0; _totalPacketLen = 0;
-      return totalPacketLen;
-    }
-
-    return 0;
-  } catch (NetworkException& ex) {
-    _pos=0; _totalPacketLen = 0;
-    throw;
-  }
-}
-
 void
-BEProtocolV2SimpleClient::parseBannerResponse(const char* packet, uint32_t totalPacketLen,
-                                              uint32_t* gotTransactId, std::string* banner,
-                                              uint32_t* bannerId, uint32_t* ownerId,
-                                              uint32_t* rotatorId)
+BEProtocolV2SimpleClient::parseBannerResponse(const char* packet, uint32_t total_packet_len,
+                                              uint32_t* transaction_id, std::string* banner,
+                                              int32_t* banner_id, uint32_t* owner_id,
+                                              uint32_t* rotator_id, std::string* /*service_name*/)
 {
   uint32_t *s = (uint32_t*)packet;
   uint32_t cmdType = ntohl(s[0]);
@@ -74,122 +24,46 @@ BEProtocolV2SimpleClient::parseBannerResponse(const char* packet, uint32_t total
 
   uint32_t offset = CMD_HEADER_SIZE;
 
-  while (offset < totalPacketLen) {
+  while (offset < total_packet_len) {
     const char* curPtrInBuf = packet + offset;
     uint32_t tag, len;
-    offset += extractParamHeader(curPtrInBuf, totalPacketLen - offset, &tag, &len);
+    offset += extractParamHeader(curPtrInBuf, total_packet_len - offset, &tag, &len);
     curPtrInBuf = packet + offset;
     switch (tag) {
     case TRANSACTION_ID_TAG:
-      offset += extractParamValue(curPtrInBuf, totalPacketLen - offset, gotTransactId);
+      offset += extractParamValue(curPtrInBuf, total_packet_len - offset, transaction_id);
       break;
     case BANNER_ID_TAG:
-      offset += extractParamValue(curPtrInBuf, totalPacketLen - offset, bannerId);
+      offset += extractParamValue(curPtrInBuf, total_packet_len - offset, reinterpret_cast<uint32_t*>(banner_id));
       break;
     case OWNER_ID_TAG:
-      offset += extractParamValue(curPtrInBuf, totalPacketLen - offset, ownerId);
+      offset += extractParamValue(curPtrInBuf, total_packet_len - offset, owner_id);
       break;
     case ROTATOR_ID_TAG:
-      offset += extractParamValue(curPtrInBuf, totalPacketLen - offset, rotatorId);
+      offset += extractParamValue(curPtrInBuf, total_packet_len - offset, rotator_id);
       break;
     case BANNER_BODY_TAG:
-      offset += extractParamValue(curPtrInBuf, totalPacketLen - offset, banner, len);
+      offset += extractParamValue(curPtrInBuf, total_packet_len - offset, banner, len);
       break;
     case CLICK_PRICE_TAG:
       {
         uint32_t clickPrice;
-        offset += extractParamValue(curPtrInBuf, totalPacketLen - offset, &clickPrice);
+        offset += extractParamValue(curPtrInBuf, total_packet_len - offset, &clickPrice);
       }
       break;
     case CHARSET_TAG:
       {
         uint32_t charset;
-        offset += extractParamValue(curPtrInBuf, totalPacketLen - offset, &charset);
+        offset += extractParamValue(curPtrInBuf, total_packet_len - offset, &charset);
       }
       break;
     default:
-      smsc_log_error(_logger, "BEProtocolV2SimpleClient::parseBannerResponse::: got unknown parameter type %d", tag);
+      smsc_log_error(_logger, "BEProtocolV2SimpleClient::parseBannerResponse::: got unknown parameter type 0x%X", tag);
       throw ProtocolError();
     }
   }
-}
-
-uint32_t
-BEProtocolV2SimpleClient::extractParamValue(const char* curPtrInBuf, uint32_t restSizeOfBuff,
-                                            uint32_t* value)
-{
-  if ( restSizeOfBuff < sizeof(uint32_t) ) {
-    smsc_log_error(_logger, "BEProtocolV2SimpleClient::extractParamValue(uint32_r)::: buffer to small");
-    throw ProtocolError();
-  }
-
-  memcpy(reinterpret_cast<uint8_t*>(value), curPtrInBuf, sizeof(uint32_t));
-
-  *value = ntohl(*value);
-
-  return static_cast<uint32_t>(sizeof(uint32_t));
-}
-
-uint32_t
-BEProtocolV2SimpleClient::extractParamValue(const char* curPtrInBuf, uint32_t restSizeOfBuff,
-                                            std::string* value, uint32_t paramLen)
-{
-  if ( restSizeOfBuff < paramLen ) {
-    smsc_log_error(_logger, "BEProtocolV2SimpleClient::extractParamValue(uint32_r)::: buffer to small");
-    throw ProtocolError();
-  }
-
-  value->assign(curPtrInBuf, paramLen);
-
-  return paramLen;
-}
-
-uint32_t
-BEProtocolV2SimpleClient::extractParamHeader(const char* curPtrInBuf,
-                                             uint32_t restSizeOfBuff,
-                                             uint32_t* tag,
-                                             uint32_t* len)
-{
-  if ( restSizeOfBuff < PARAM_HDR_SZ ) {
-    smsc_log_error(_logger, "BEProtocolV2SimpleClient::extractTag::: buffer to small");
-    throw ProtocolError();
-  }
-
-  memcpy(reinterpret_cast<uint8_t*>(tag), curPtrInBuf, sizeof(uint32_t));
-  *tag = ntohl(*tag);
-
-  memcpy(reinterpret_cast<uint8_t*>(len), curPtrInBuf + sizeof(uint32_t), sizeof(uint32_t));
-  *len = ntohl(*len);
-
-  return PARAM_HDR_SZ;
-}
-
-banner_read_stat
-BEProtocolV2SimpleClient::readAdvert(std::string* banner,
-                                     BannerResponseTrace* banner_resp_trace)
-{
-  smsc_log_debug(_logger, "BEProtocolV2SimpleClient::readAdvert");
-
-  static BannerResponseTrace emptyBannerResponseTrace;
-  *banner_resp_trace = emptyBannerResponseTrace;
-  try {
-    uint32_t totalPacketLen = readPacket();
-    if (!totalPacketLen)
-      return CONTINUE_READ_PACKET;
-
-    smsc_log_debug(_logger, "BEProtocolV2SimpleClient::readAdvert::: got packet from BE: [%s]", hexdmp((uchar_t*)_buf, totalPacketLen).c_str());
-    parseBannerResponse(_buf, totalPacketLen, &banner_resp_trace->transactionId,
-                        banner, &banner_resp_trace->bannerId, &banner_resp_trace->ownerId,
-                        &banner_resp_trace->rotatorId);
-
-    smsc_log_info(_logger, "got BannerResponse: transactionId=%u, bannerId=%u, ownerId=%u, rotatorId=%u",
-                  banner_resp_trace->transactionId, banner_resp_trace->bannerId, banner_resp_trace->ownerId, banner_resp_trace->rotatorId);
-  } catch (ProtocolError& ex) {
-    smsc_log_error(_logger, "BEProtocolV2SimpleClient::readAdvert::: caught ProtocolError exception");
-    return ERR_ADV_OTHER;
-  }
-
-  return BANNER_OK;
+  smsc_log_info(_logger, "got BannerResponse: transactionId=%u,bannerId=%d,ownerId=%u,rotatorId=%u",
+                *transaction_id, *banner_id, *owner_id, *rotator_id);
 }
 
 uint32_t
@@ -260,7 +134,7 @@ BEProtocolV2SimpleClient::prepareBannerReqCmd(util::SerializationBuffer* req /* 
 uint32_t
 BEProtocolV2SimpleClient::prepareErrorInfoCmd(util::SerializationBuffer* req /* �����*/,
                                               const BannerRequest& par /*��������� ������� ������*/,
-                                              uint32_t errCode)
+                                              uint32_t err_code)
 {
   uint32_t totalPacketSize = prepareHeader(CMD_ROLLBACK_REQ, CMD_ROLLBACK_REQ_BODY_LEN, req);
 
@@ -287,23 +161,9 @@ BEProtocolV2SimpleClient::prepareErrorInfoCmd(util::SerializationBuffer* req /* 
   // error code
   req->WriteNetInt32(ERROR_CODE_TAG);
   req->WriteNetInt32(static_cast<uint32_t>(sizeof(uint32_t)));
-  req->WriteNetInt32(errCode);
+  req->WriteNetInt32(err_code);
 
   return totalPacketSize;
-}
-
-void
-BEProtocolV2SimpleClient::sendErrorInfo(const BannerRequest& banReq,
-                                        int rc)
-{
-  util::SerializationBuffer req;
-  int len;
-  if (rc == ERR_ADV_TIMEOUT)
-    len = prepareErrorInfoCmd(&req, banReq, BANNER_RESPONSE_TIMEOUT);
-  else
-    len = prepareErrorInfoCmd(&req, banReq, BANNER_OTHER_ERROR);
-
-  writeToSocket(req.getBuffer(), len, "BEProtocolV2SimpleClient::sendErrorInfo");
 }
 
 }}
--- src/mcisme/advert/BEProtocolV2SimpleClient.hpp
+++ src/mcisme/advert/BEProtocolV2SimpleClient.hpp
@@ -1,10 +1,10 @@
 #ifndef __MCISME_ADVERT_BEPROTOCOLV2SIMPLECLIENT_HPP__
 # define __MCISME_ADVERT_BEPROTOCOLV2SIMPLECLIENT_HPP__
 
-# include <mcisme/advert/AdvertisingImpl.h>
-# include <core/buffers/TmpBuf.hpp>
-# include <util/BufferSerialization.hpp>
-# include <util/Exception.hpp>
+# include "mcisme/advert/AdvertisingImpl.h"
+# include "core/buffers/TmpBuf.hpp"
+# include "util/BufferSerialization.hpp"
+# include "util/Exception.hpp"
 
 namespace smsc {
 namespace mcisme {
@@ -15,41 +15,19 @@ public:
 
   static const unsigned int PROTOCOL_VERSION = 2;
 
-  virtual banner_read_stat readAdvert(std::string* banner, BannerResponseTrace* banner_resp_trace);
-
-  virtual void sendErrorInfo(const BannerRequest& banReq,
-                             int rc);
-
 protected:
-  virtual uint32_t prepareBannerReqCmd(util::SerializationBuffer* req /* буфер*/,
-                                       BannerRequest* par /*параметры запроса банера*/);
+  virtual uint32_t prepareBannerReqCmd(util::SerializationBuffer* req,
+                                       BannerRequest* par);
 
-  uint32_t prepareErrorInfoCmd(util::SerializationBuffer* req /* буфер*/,
-                               const BannerRequest& par /*параметры запроса банера*/,
-                               uint32_t errCode);
+  virtual uint32_t prepareErrorInfoCmd(util::SerializationBuffer* req,
+                                       const BannerRequest& par,
+                                       uint32_t err_code);
 
-  uint32_t readPacket();
+  virtual void parseBannerResponse(const char* packet, uint32_t total_packet_len,
+                                   uint32_t* transaction_id, std::string* banner,
+                                   int32_t* banner_id, uint32_t* owner_id,
+                                   uint32_t* rotator_id, std::string* /*service_name*/);
 
-  int extractBanner(core::buffers::TmpBuf<char, MAX_PACKET_LEN>& incomingPacketBuf,
-                    std::string* banner);
-
-  void parseBannerResponse(const char* packet, uint32_t totalPacketLen,
-                           uint32_t* gotTransactId, std::string* banner,
-                           uint32_t* bannerId, uint32_t* ownerId,
-                           uint32_t* rotatorId);
-
-  uint32_t extractParamValue(const char* curPtrInBuf, uint32_t restSizeOfBuff,
-                             uint32_t* value);
-
-  uint32_t extractParamValue(const char* curPtrInBuf, uint32_t restSizeOfBuff,
-                             std::string* value, uint32_t paramLen);
-
-  uint32_t extractParamHeader(const char* curPtrInBuf,
-                              uint32_t restSizeOfBuff,
-                              uint32_t* tag,
-                              uint32_t* len);
-
-  static const unsigned int PARAM_HDR_SZ = sizeof(uint32_t) + sizeof(uint32_t);
   // общая длина фиксированных параметров в getBannerReq
   static const unsigned int GET_BANNER_REQ_BODY_LEN =
     PARAM_HDR_SZ + sizeof(uint32_t) + // transaction id
@@ -69,13 +47,6 @@ protected:
     PARAM_HDR_SZ + sizeof(uint32_t) + // rotator id
     PARAM_HDR_SZ + sizeof(uint32_t); // error code
 
-  static const unsigned int GET_BANNER_RESP_BODY_LEN = 
-    PARAM_HDR_SZ + sizeof(uint32_t) + // transaction id
-    PARAM_HDR_SZ + sizeof(uint32_t) + // banner id
-    PARAM_HDR_SZ + sizeof(uint32_t) + // owner id
-    PARAM_HDR_SZ + sizeof(uint32_t) + // rotator id
-    PARAM_HDR_SZ + sizeof(uint32_t); // click price
-
   enum {
     CMD_BANNER_REQ = 0x01,      // запрос от клиента к серверу
     CMD_BANNER_RSP = 0x02,          // ответ  от сервера
@@ -88,20 +59,6 @@ protected:
     EXT_INFO_BODY_TAG = 0x0A, CLICK_PRICE_TAG = 0x0B, FLAGS_TAG = 0x0C, ERROR_CODE_TAG = 0x0D, 
     ROTATOR_ID_TAG = 0x0E, OWNER_ID_TAG = 0x0F
   };
-
-  enum {
-    BANNER_RESPONSE_TIMEOUT = -100,
-    BANNER_OTHER_ERROR = -101
-  };
-
-  class ProtocolError : public smsc::util::Exception {
-  public:
-  };
-
-private:
-  char _buf[MAX_PACKET_LEN];
-  int _pos;
-  uint32_t _totalPacketLen;
 };
 
 }}
