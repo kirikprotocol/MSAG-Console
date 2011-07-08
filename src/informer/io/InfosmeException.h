2a2b639c8b03e7a30ec4df62e80d6dd349dd6ee7
bukind <>
2011-07-08 09:17:13 +0000

    informer-build-20110708

--- src/informer/io/InfosmeException.h
+++ src/informer/io/InfosmeException.h
@@ -4,13 +4,13 @@
 #include "util/Exception.hpp"
 
 static inline char* getStrerror(int err,char* buf,size_t buflen) {
-#ifdef linux
-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE>=600) && !_GNU_SOURCE
+#if defined(linux) || defined(__MACH__)
+// #if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE>=600) && !_GNU_SOURCE
     strerror_r(err,buf,buflen);
     return buf;
-#else
-    return strerror_r(err,buf,buflen);
-#endif
+// #else
+//     return strerror_r(err,buf,buflen);
+// #endif
 #else
     snprintf(buf,buflen,"errno=%d",err);
     return buf;
