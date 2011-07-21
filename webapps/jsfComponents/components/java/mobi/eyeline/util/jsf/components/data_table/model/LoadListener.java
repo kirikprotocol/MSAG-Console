f0a0e42ab2dc9916fbe9c9f155e12b5cec4ebec6
artem <>
2011-07-21 06:42:30 +0000

    Add LoadListener

---
+++ webapps/jsfComponents/components/java/mobi/eyeline/util/jsf/components/data_table/model/LoadListener.java
@@ -0,0 +1,47 @@
+package mobi.eyeline.util.jsf.components.data_table.model;
+
+
+/**
+* author: Aleksandr Khalitov
+*/
+public class LoadListener {
+
+  private int current;
+  private int total = Integer.MAX_VALUE;
+
+  private ModelException loadError;
+
+  private boolean finished;
+
+  public int getCurrent() {
+    return current;
+  }
+
+  public void setCurrent(int current) {
+    this.current = current;
+  }
+
+  public int getTotal() {
+    return total;
+  }
+
+  public void setTotal(int total) {
+    this.total = total;
+  }
+
+  public ModelException getLoadError() {
+    return loadError;
+  }
+
+  public void setLoadError(ModelException loadError) {
+    this.loadError = loadError;
+  }
+
+  public boolean isFinished() {
+    return finished;
+  }
+
+  public void setFinished(boolean finished) {
+    this.finished = finished;
+  }
+}
