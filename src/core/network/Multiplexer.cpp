7be07f9772b4e509928fc978912c202897f88566
ksv <>
2011-07-28 12:30:05 +0000

    Https tuning: due to multimasking of revent it seems to better check I/O signals first instead of errors to avoid of lost the tail of message.

--- src/core/network/Multiplexer.cpp
+++ src/core/network/Multiplexer.cpp
@@ -29,15 +29,19 @@ int Multiplexer::checkState(int mode,SockArray& ready,SockArray& error,int timeo
   int err=POLLNVAL|POLLERR|POLLHUP;
   for(int i=0;i<fds.Count();i++)
   {
-    if(fds[i].revents&err)
-    {
-      error.Push(sockets[i]);
-      continue;
-    }
-    if(fds[i].revents&mask)
-    {
-      ready.Push(sockets[i]);
-    }
+/*
+ * (xom 28.07.11) due to multimasking of revent
+ * it seems to better check I/O signals first instead of errors
+ * to avoid of lost the tail of message
+ */
+	if(fds[i].revents&mask) {
+		ready.Push(sockets[i]);
+		continue;
+	}
+	if(fds[i].revents&err) {
+		error.Push(sockets[i]);
+		continue;
+	}
   }
   return ready.Count()+error.Count();
 }
