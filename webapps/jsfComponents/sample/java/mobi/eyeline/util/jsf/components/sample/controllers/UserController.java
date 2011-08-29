be7299e8dbfb43ce388ed4cd19f0c323907fe63f
alkhal <>
2011-08-29 07:09:59 +0000

    jsf components: add version to sample

---
+++ webapps/jsfComponents/sample/java/mobi/eyeline/util/jsf/components/sample/controllers/UserController.java
@@ -0,0 +1,16 @@
+package mobi.eyeline.util.jsf.components.sample.controllers;
+
+import mobi.eyeline.util.jsf.components.Version;
+
+/**
+ * author: Aleksandr Khalitov
+ */
+public class UserController {
+
+  public static final String version = Version.version;
+
+  public String getVersion() {
+    return version;
+  }
+
+}
