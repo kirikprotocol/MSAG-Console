3ba3520e4c0627ca1fd3cce8765532c16dce5c2b
gvroman <>
2011-09-02 10:05:30 +0000

    command line arguments is implemented

--- src/inman/inman.cpp
+++ src/inman/inman.cpp
@@ -2,13 +2,14 @@
 static char const ident[] = "@(#)$Id$";
 #endif /* MOD_IDENT_ON */
 
-#include <assert.h>
-
 #include "inman/version.hpp"
 #include "inman/URCRegInit.hpp"
+#include "inman/services/ICSRegistry.hpp"
 #include "inman/services/SvcHost.hpp"
 using smsc::logger::Logger;
 using smsc::inman::ICServiceAC;
+using smsc::inman::ICSState;
+using smsc::inman::ICSLoadupsReg;
 using smsc::inman::SVCHost;
 using smsc::inman::SVCHostProducer;
 using smsc::inman::ICSHostCfgReader;
@@ -56,21 +57,79 @@ void Mutex::reportContention( const char* from,
 }
 }
 }
-#endif
+#endif /* CHECKCONTENTION */
 
 
+struct PhaseParms {
+  const char *  _nmPhase;
+  int           _failureRC;
+};
+
+//Matches the ICSState values
+static PhaseParms _phaseParms[] = {
+    { NULL, 0}
+  , { "Configuration", -1}
+  , { "Initialization", -2}
+  , { "Startup", -3}
+};
+
+static void log_phase_ok(ICSState phase_id, Logger * use_log)
+{
+  smsc_log_info(use_log, "INMan: %s phase complete.", _phaseParms[phase_id]._nmPhase);
+}
+
+static int log_phase_fatal(ICSState phase_id, Logger * use_log, const char * err_cause)
+{
+  if (err_cause) {
+    fprintf(stderr, "INMan: %s\n", err_cause);
+    smsc_log_fatal(use_log, "INMan: %s", err_cause);
+  }
+  fprintf(stderr, "INMan: %s phase failure. Exiting.\n", _phaseParms[phase_id]._nmPhase);
+  smsc_log_fatal(use_log, "INMan: %s phase failure. Exiting.", _phaseParms[phase_id]._nmPhase);
+  return _phaseParms[phase_id]._failureRC;
+}
+
+static const char _hlpMsg[] =
+ "USAGE: %s [-help | -list-uid] | [[-parse|-init] config_file_name]\n"
+ "  -help      print help on command line arguments\n"
+ "  -list-uid  print supported ICService's UIds\n"
+ "  -parse     parse services configuration and exit\n"
+ "  -init      perform services initialization phase and exit\n"
+ "  NOTE:      by default config_file_name is 'config.xml'\n";
 int main(int argc, char** argv)
 {
     int     rval = 0;
-    char *  cfgFile = (char*)"config.xml";
+    const char *  cfgFile = (const char*)"config.xml";
+    ICSState  tgtPhase = ICServiceAC::icsStStarted;
+
+    if (argc > 1) { //parse command line args
+      if (*argv[1] == '-') {
+        if (!strcmp(argv[1], "-help")) {
+          fprintf(stderr, _hlpMsg, argv[0]);
+          return 0;
+        }
+        if (!strcmp(argv[1], "-list-uid")) {
+          fprintf(stderr, "Known UIds: %s\n", ICSLoadupsReg::get().knownUIds().toString().c_str());
+          return 0;
+        }
+        if (!strcmp(argv[1], "-parse")) {
+          tgtPhase = ICServiceAC::icsStConfig;
+        } else if (!strcmp(argv[1], "-init")) {
+          tgtPhase = ICServiceAC::icsStInited;
+        } else {
+          fprintf(stderr, _hlpMsg, argv[0]);
+          return -1;
+        }
+        if (argc > 2)
+          cfgFile = argv[2];
+      } else
+        cfgFile = argv[1];
+    }
 
     tzset();
     URCRegistryGlobalInit();
     Logger::Init();
     inmanLogger = Logger::getInstance("smsc.inman");
-
-    if (argc > 1)
-        cfgFile = argv[1];
 
     std::string prodVer(_getProductVersionStr());
     {
@@ -99,41 +158,50 @@ int main(int argc, char** argv)
           throw ConfigException("SVCHost configuration is incomplete");
 
         _svcHost = (SVCHost*)hostProd.newService(0, inmanLogger);
-        smsc_log_info(inmanLogger, "INMan: configuration processed.\n");
-    } catch (const ConfigException & exc) {
-        smsc_log_fatal(inmanLogger, "INMan: %s", exc.what());
-        smsc_log_fatal(inmanLogger, "Configuration invalid. Exiting.");
-        exit(-1);
+        log_phase_ok(ICServiceAC::icsStConfig, inmanLogger);
+        /* */
+    } catch (const std::exception & exc) {
+      rval = log_phase_fatal(ICServiceAC::icsStConfig, inmanLogger, exc.what());
     } catch (...) {
-      smsc_log_fatal(inmanLogger, "INMan: unknown exception caught");
-      smsc_log_fatal(inmanLogger, "Configuration invalid. Exiting.");
-      exit(-1);
+      rval = log_phase_fatal(ICServiceAC::icsStConfig, inmanLogger, "unknown exception caught");
     }
 
-    try {
-        if (_svcHost->ICSInit() == ICServiceAC::icsRcOk) {
-            smsc_log_info(inmanLogger, "INMan: initialization complete.\n");
-            _runService = 1;
-            if (_svcHost->ICSStart() == ICServiceAC::icsRcOk) {
-                //handle SIGTERM only in main thread
-                sigset(SIGTERM, sighandler);
-            } else {
-                smsc_log_fatal(inmanLogger, "INMan: startup failure. Exiting.");
-                _runService = 0;
-            }
-            while(_runService)
-                usleep(1000 * 200); //sleep 200 ms
-            _svcHost->ICSStop();
-        } else {
-            smsc_log_fatal(inmanLogger, "INMan: initialization failure. Exiting.");
-        }
-    } catch(const std::exception & error) {
-        smsc_log_fatal(inmanLogger, "INMan: %s", error.what() );
-        fprintf(stderr, "Fatal error: %s\n", error.what() );
-        rval = 1;
+    if (tgtPhase > ICServiceAC::icsStConfig) {
+      try {
+        if (_svcHost->ICSInit() == ICServiceAC::icsRcOk)
+          log_phase_ok(ICServiceAC::icsStInited, inmanLogger);
+        else
+          rval = log_phase_fatal(ICServiceAC::icsStInited, inmanLogger, NULL);
+      } catch(const std::exception & error) {
+        rval = log_phase_fatal(ICServiceAC::icsStInited, inmanLogger, error.what());
+      } catch (...) {
+        rval = log_phase_fatal(ICServiceAC::icsStInited, inmanLogger, "unknown exception caught");
+      }
     }
+
+    if (tgtPhase > ICServiceAC::icsStInited) {
+      try {
+        if (_svcHost->ICSStart() == ICServiceAC::icsRcOk) {
+          _runService = 1;
+          //handle SIGTERM only in main thread
+          sigset(SIGTERM, sighandler);
+          log_phase_ok(ICServiceAC::icsStStarted, inmanLogger);
+
+          while(_runService)
+            usleep(1000 * 200); //sleep 200 ms
+          _svcHost->ICSStop();
+        } else {
+          rval = log_phase_fatal(ICServiceAC::icsStStarted, inmanLogger, NULL);
+        }
+      } catch(const std::exception & error) {
+        rval = log_phase_fatal(ICServiceAC::icsStStarted, inmanLogger, error.what());
+      } catch (...) {
+        rval = log_phase_fatal(ICServiceAC::icsStStarted, inmanLogger, "unknown exception caught");
+      }
+    }
+
     if (_svcHost)
-        delete _svcHost;
+      delete _svcHost;
     smsc_log_info(inmanLogger, "INMan: shutdown complete");
     return rval;
 }
