2b156f39a6fbb9fa805a53db7899c601a5e1fbf4
artem <>
2011-09-21 07:38:46 +0000

    Code review

--- webapps/informer/java/mobi/eyeline/informer/admin/WebConfig.java
+++ webapps/informer/java/mobi/eyeline/informer/admin/WebConfig.java
@@ -154,6 +154,7 @@ class WebConfig implements ManagedConfigFile<WebConfigSettings> {
         int mT = cp.getInt("maxTimeSec", 0);
         settings.setCpMaxTimeSec(mT);
       }
+      //todo Если я в конфиге в параметре fileFormat напишу какую-нить ерунду, то CpFileFormat кинет IllegalArgumentException, который не перехватывается.
       settings.setCpFileFormat(fF == null || fF.length() == 0 ? CpFileFormat.EYELINE : CpFileFormat.valueOf(fF));
 
       XmlConfigSection cdr = webconfig.getSection("cdr");
