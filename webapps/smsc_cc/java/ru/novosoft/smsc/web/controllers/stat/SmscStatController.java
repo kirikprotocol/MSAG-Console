177d17a565901b0126e52ba4827cea89885512c4
alkhal <>
2011-10-06 09:35:40 +0000

    smsc_cc: sms stat init

--- webapps/smsc_cc/java/ru/novosoft/smsc/web/controllers/stat/SmscStatController.java
+++ webapps/smsc_cc/java/ru/novosoft/smsc/web/controllers/stat/SmscStatController.java
@@ -37,6 +37,12 @@ public class SmscStatController extends SmscController{
 
   public SmscStatController() {
     this.provider = WebContext.getInstance().getSmscStatProvider();
+    Calendar c = Calendar.getInstance();
+    c.set(Calendar.HOUR_OF_DAY, 0);
+    c.set(Calendar.MINUTE, 0);
+    c.set(Calendar.SECOND, 0);
+    c.set(Calendar.MILLISECOND, 0);
+    setFrom(c.getTime());
   }
 
   public StatType getType() {
