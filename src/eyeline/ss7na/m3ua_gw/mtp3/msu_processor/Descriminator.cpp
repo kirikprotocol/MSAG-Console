97e8bb6264389e22a91dc204129169802e1328b7
shulga <>
2011-11-14 05:25:14 +0000

    removed commeted block

--- src/eyeline/ss7na/m3ua_gw/mtp3/msu_processor/Descriminator.cpp
+++ src/eyeline/ss7na/m3ua_gw/mtp3/msu_processor/Descriminator.cpp
@@ -11,25 +11,6 @@ bool
 Descriminator::checkRouteTermination(uint32_t dpc) const
 {
   return PointsDefinitionRegistry::getInstance().exists(dpc);
-//  for(unsigned i = 0; i < _totalNumOfLPCs; ++i) {
-//    if ( _localPointCodes[i] == dpc )
-//      return true;
-//  }
-//  return false;
 }
 
-/*void
-Descriminator::addLocalPointCode(uint32_t pc)
-{
-  if ( _totalNumOfLPCs == _maxNumOfLPCs ) {
-    _maxNumOfLPCs <<= 2;
-    uint32_t* lpcMem = new uint32_t[_maxNumOfLPCs];
-    std::copy(_localPointCodes, _localPointCodes + _totalNumOfLPCs, lpcMem);
-    if (_isBufferOnHeap )
-      delete [] _localPointCodes;
-    _localPointCodes = lpcMem; _isBufferOnHeap = true;
-  }
-  _localPointCodes[_totalNumOfLPCs++] = pc;
-}
-*/
 }}}}}
