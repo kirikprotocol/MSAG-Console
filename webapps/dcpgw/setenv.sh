583b6ea67c5c1efb3a4feb1c2ea7deead51fa916
stepanov <>
2011-11-16 08:53:55 +0000

    no message

--- webapps/dcpgw/setenv.sh
+++ webapps/dcpgw/setenv.sh
@@ -1,11 +1,11 @@
 CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:conf
 
 CLASSPATH=$CLASSPATH:lib/dcpgw.jar
-CLASSPATH=$CLASSPATH:lib/smppapi.jar
-CLASSPATH=$CLASSPATH:lib/smppcharsets.jar
-CLASSPATH=$CLASSPATH:lib/utils.jar
-CLASSPATH=$CLASSPATH:lib/log4j-1.2.16.jar
-CLASSPATH=$CLASSPATH:lib/protogenfw.jar
-CLASSPATH=$CLASSPATH:lib/apm_utils.jar
+#CLASSPATH=$CLASSPATH:lib/smppapi.jar
+#CLASSPATH=$CLASSPATH:lib/smppcharsets.jar
+#CLASSPATH=$CLASSPATH:lib/utils.jar
+#CLASSPATH=$CLASSPATH:lib/log4j-1.2.16.jar
+#CLASSPATH=$CLASSPATH:lib/protogenfw.jar
+#CLASSPATH=$CLASSPATH:lib/apm_utils.jar
 
 #echo $CLASSPATH
