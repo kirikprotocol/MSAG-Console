b03d805399be2ba3a3ab8784ec1ee5260f08ad43
shulga <>
2011-11-16 12:03:59 +0000

    ObjectPtrRegistry was removed; RefObjectRegistry was changed: LOCK object that used in RefPtr is taken from template argument of RefObjectRegistry; ObjectRegistry::lookup now returns result by value instead by reference since in last case there is a potential race condition that can lead to use already deleted object

--- src/eyeline/utilx/ObjectRegistry.hpp
+++ src/eyeline/utilx/ObjectRegistry.hpp
@@ -24,14 +24,7 @@ public:
       return true;
   }
 
-  OBJECT& lookup(const CRITERION& searchCriterion) {
-    smsc::core::synchronization::MutexGuard synchronize(_lock);
-    typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
-    if ( iter != _objectsRegistryMap.end() ) return iter->second;
-    else throw utilx::RegistryKeyNotFound("ObjectRegistry::lookup::: key not found");
-  }
-
-  const OBJECT& lookup(const CRITERION& searchCriterion) const {
+  OBJECT lookup(const CRITERION& searchCriterion) {
     smsc::core::synchronization::MutexGuard synchronize(_lock);
     typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
     if ( iter != _objectsRegistryMap.end() ) return iter->second;
@@ -60,55 +53,6 @@ private:
 
 template <class OBJECT, class CRITERION,
           class LOCK = FakeMutex>
-class ObjectPtrRegistry {
-public:
-  bool exists (const CRITERION& searchCriterion) const {
-    smsc::core::synchronization::MutexGuard synchronize(_lock);
-    if ( _objectsRegistryMap.find(searchCriterion) == _objectsRegistryMap.end() )
-      return false;
-    else
-      return true;
-  }
-
-  OBJECT* lookup(const CRITERION& searchCriterion) {
-    smsc::core::synchronization::MutexGuard synchronize(_lock);
-    typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
-    if ( iter != _objectsRegistryMap.end() ) return iter->second;
-    else NULL;
-  }
-
-  const OBJECT* lookup(const CRITERION& searchCriterion) const {
-    smsc::core::synchronization::MutexGuard synchronize(_lock);
-    typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
-    if ( iter != _objectsRegistryMap.end() ) return iter->second;
-    else NULL;
-  }
-
-  void insert(OBJECT* newObject, const CRITERION& searchCriterion) {
-    smsc::core::synchronization::MutexGuard synchronize(_lock);
-    typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
-    if ( iter == _objectsRegistryMap.end() )
-      _objectsRegistryMap.insert(std::make_pair(searchCriterion, newObject));
-  }
-
-  OBJECT* remove(const CRITERION& searchCriterion) {
-    smsc::core::synchronization::MutexGuard synchronize(_lock);
-    typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
-    if ( iter != _objectsRegistryMap.end() ) {
-      OBJECT* obj = iter->second;
-      _objectsRegistryMap.erase(iter);
-      return obj;
-    } else return NULL;
-  }
-
-private:
-  typedef std::map<CRITERION,OBJECT*> objectRegistryMap_t;
-  objectRegistryMap_t _objectsRegistryMap;
-  mutable LOCK _lock;
-};
-
-template <class OBJECT, class CRITERION,
-          class LOCK = FakeMutex>
 class RefObjectRegistry {
 public:
   bool exists (const CRITERION& searchCriterion) const {
@@ -119,17 +63,17 @@ public:
       return true;
   }
 
-  smsc::core::buffers::RefPtr<OBJECT, smsc::core::synchronization::Mutex>
+  smsc::core::buffers::RefPtr<OBJECT, LOCK>
   lookup(const CRITERION& searchCriterion) {
     smsc::core::synchronization::MutexGuard synchronize(_lock);
     typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
     if ( iter != _objectsRegistryMap.end() )
       return iter->second;
     else
-      return smsc::core::buffers::RefPtr<OBJECT, smsc::core::synchronization::Mutex>();
+      return smsc::core::buffers::RefPtr<OBJECT, LOCK>();
   }
   
-  void insert(const smsc::core::buffers::RefPtr<OBJECT, smsc::core::synchronization::Mutex>& newObject,
+  void insert(const smsc::core::buffers::RefPtr<OBJECT, LOCK>& newObject,
               const CRITERION& searchCriterion) {
     smsc::core::synchronization::MutexGuard synchronize(_lock);
     typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
@@ -137,20 +81,20 @@ public:
       _objectsRegistryMap.insert(std::make_pair(searchCriterion, newObject));
   }
 
-  smsc::core::buffers::RefPtr<OBJECT, smsc::core::synchronization::Mutex>
+  smsc::core::buffers::RefPtr<OBJECT, LOCK>
   remove(const CRITERION& searchCriterion) {
     smsc::core::synchronization::MutexGuard synchronize(_lock);
     typename objectRegistryMap_t::iterator iter = _objectsRegistryMap.find(searchCriterion);
     if ( iter != _objectsRegistryMap.end() ) {
-      smsc::core::buffers::RefPtr<OBJECT, smsc::core::synchronization::Mutex> obj = iter->second;
+      smsc::core::buffers::RefPtr<OBJECT, LOCK> obj = iter->second;
       _objectsRegistryMap.erase(iter);
       return obj;
     } else
-      return smsc::core::buffers::RefPtr<OBJECT, smsc::core::synchronization::Mutex>();
+      return smsc::core::buffers::RefPtr<OBJECT, LOCK>();
   }
 
 private:
-  typedef std::map<CRITERION,smsc::core::buffers::RefPtr<OBJECT, smsc::core::synchronization::Mutex> > objectRegistryMap_t;
+  typedef std::map<CRITERION, smsc::core::buffers::RefPtr<OBJECT, LOCK> > objectRegistryMap_t;
   objectRegistryMap_t _objectsRegistryMap;
   LOCK _lock;
 };
